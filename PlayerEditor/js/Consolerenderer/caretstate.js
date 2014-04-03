53f9f33f96ca05d634c11197e464befe5c2b4ffb
Karl Persson <c12karpe@student.his.se>
2014-04-03 21:41:17 +0200

    Indentation, comment and description fixes
    Some kind of obsession.

--- PlayerEditor/js/Consolerenderer/caretstate.js
+++ PlayerEditor/js/Consolerenderer/caretstate.js
@@ -3,14 +3,13 @@
 //-------------------------------------------------------------------------------------------
 // Implements a caret state object
 //-------------------------------------------------------------------------------------------
-
 function caretstate()
 {
-		this.xk=0;
-		this.yk=0;
-		this.carethidden=0;
-		this.caretblink=0;
-		this.caretcolor=0;
-		this.textcolor=0;
-		this.textbgcolor=0;
+	this.xk=0;
+	this.yk=0;
+	this.carethidden=0;
+	this.caretblink=0;
+	this.caretcolor=0;
+	this.textcolor=0;
+	this.textbgcolor=0;
 }
