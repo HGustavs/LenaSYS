6a67246fcbdbe9281ccc777a68f579d0b7f92753
a12madka <quake.2@hotmail.com>
2014-04-07 09:26:01 +0200

    update on canvas

--- PlayerEditor/canvas/logfile.php
+++ PlayerEditor/canvas/logfile.php
@@ -3,10 +3,11 @@
 $filename="canvas.txt";
 $content = "This is our test file";
 $length = strlen($content);
-if(file_exists($filename)==false){
+echo("creating file");
+//if(file_exists($filename)==false){
 $handle=fopen($filename,"w");
 $write = fwrite($handle, $content, $length);
 $close = fclose($handle);
 echo("File Created, Click <a href='$filename'> Here </a> to view it.");
-}
+//}
 ?>
