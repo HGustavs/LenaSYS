a78d304ef6650116ea736dc31e2e6444219e5127
c12johpe <c12johpe@student.his.se>
2014-04-10 14:45:58 +0200

    Broke canvasPlayer

--- PlayerEditor/canvasPlayer/canvasPlayer.js
+++ PlayerEditor/canvasPlayer/canvasPlayer.js
@@ -4,24 +4,50 @@
 function canvasPlayer() 
 {	
 	this.parseLine = function(operation){
+		// console.log(operation);
+		// console.log("-->"+operation.nodeName);
 		for(x = 0; x < operation.length; x++) {
 			
-			console.log(operation[x].nodeName);
+			console.log("operation: "+operation.nodeName);
 			console.log(operation[x].attributes);
 			if(operation[x].attributes != null){
 				console.log("attribute length " + operation[x].attributes.length);
 				if(operation[x].attributes.length == 0){
-					window[operation[x].nodeName]();
+					window["canvasPlayer"][operation[x].nodeName]();
 				}
 				else if(operation[x].attributes.length == 1){
-					window["canvasPlayer." + operation[x].nodeName](operation[x].attributes.item(0).nodeValue);
+					window["canvasPlayer"][operation[x].nodeName](operation[x].attributes.item(0).nodeValue);
 				}
 				else if(operation[x].attributes.length == 2){
-					window[operation[x].nodeName](operation[x].attributes.item(0).nodeValue,operation[x].attributes.item(1).nodeValue);
+					window["canvasPlayer"][operation[x].nodeName](operation[x].attributes.item(0).nodeValue,operation[x].attributes.item(1).nodeValue);
 				}
 			}	
 		}	
 	}
+	
+function beginpath() {
+		ctx.lineWidth = 5;
+		console.log("func beginpath");
+		ctx.beginPath();
+}
+
+function moveto(x, y) {
+	console.log("func moveTo" + x +" " +y);
+	ctx.moveTo(x,y);
+	
+}
+
+function lineto(x, y) {
+	console.log("func lineTo");
+	ctx.lineTo(x, y);
+	
+}
+
+function stroke() {
+	console.log("func stroke");
+	ctx.stroke();
+	
+}
 	
 	var c = document.getElementById('Canvas');
 	var ctx = c.getContext("2d");
@@ -47,13 +73,14 @@ function canvasPlayer()
   	xmlDoc=xmlhttp.responseXML;
 //	  
   	timesteps = xmlDoc.getElementsByTagName("script")[0].childNodes;
-	  console.log(timesteps);
+	  console.log(JSON.stringify(timesteps));
 	  console.log(timesteps.length);
 	  //tStep = xmlDoc.getElementsByTagName("timestep");
 	  var totalTime = 0;
 	for(i = 0; i < timesteps.length; i++) {		 
  		//if(i > xmlDoc.getElementsByTagName("timestep").length) break;
-		operation = xmlDoc.getElementsByTagName("timestep")[i].childNodes; 
+		operation = xmlDoc.getElementsByTagName("timestep");
+		if(operation[i]){
 		delay = operation[i].getAttribute("delay");
 		totalTime += parseInt(delay);
 		
@@ -61,9 +88,16 @@ function canvasPlayer()
 		
 		//operation = tStep[i].childNodes;
 		//this.parseLine(operation);
-		setTimeout(function(){canvasPlayer.parseLine(operation)},parseInt(totalTime));
+		nodes = operation[i].childNodes;
 		
+		this.parseLine(nodes);
+		// for(j=0; j<nodes.length; j++) {
+			// console.log(nodes[j].nodeName);
+			// this.parseLine(nodes[j]);
+		// }
+
 		
+		}
 	}
 	
 	
