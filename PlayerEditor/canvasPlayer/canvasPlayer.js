8559c71c5a05e437044f237ffdddbb448336f7cd
Karl Persson <c12karpe@student.his.se>
2014-04-10 16:08:01 +0200

    Operation CanvasPlayer Cleanup
    Comments, indentation fixes and removed console logging.

--- PlayerEditor/canvasPlayer/canvasPlayer.js
+++ PlayerEditor/canvasPlayer/canvasPlayer.js
@@ -1,99 +1,95 @@
-// JavaScript Document
-
-
+// Canvas player
 function canvasPlayer() 
 {	
-	this.parseLine = function(operation){
-		// console.log(operation);
-		 
-		for(x = 0; x < operation.length; x++) {
-			
-			console.log(operation[x].attributes);
-			if(operation[x].attributes != null){
-				console.log("attribute length " + operation[x].attributes.length);
-				if(operation[x].nodeName.substring(0,1) == "#") continue;
-				console.log("operation: "+operation[x].nodeName);
-				if(operation[x].attributes.length == 0){
-					this[operation[x].nodeName]();
+	// Execute timestep nodes
+	this.executeTimestep = function(nodes){
+		// Step through nodes
+		for(x = 0; x < nodes.length; x++) {
+			console.log(nodes[x].attributes);
+
+			if(nodes[x].attributes != null){
+				console.log("attribute length " + nodes[x].attributes.length);
+				// Continue if invalid node (i.e. #text)
+				if(nodes[x].nodeName.substring(0,1) == "#") continue;
+				console.log("nodes: "+nodes[x].nodeName);
+
+				// Check number of arguments and call canvas function
+				// TODO: Add for functions with more arguments
+				if(nodes[x].attributes.length == 0){
+					this[nodes[x].nodeName]();
 				}
-				else if(operation[x].attributes.length == 1){
-					this[operation[x].nodeName](operation[x].attributes.item(0).nodeValue);
+				else if(nodes[x].attributes.length == 1){
+					this[nodes[x].nodeName](nodes[x].attributes.item(0).nodeValue);
 				}
-				else if(operation[x].attributes.length == 2){
-					//window["canvasPlayer"][operation[x].nodeName](operation[x].attributes.item(0).nodeValue, operation[x].attributes.item(1).nodeValue);
-					this[operation[x].nodeName](operation[x].attributes.item(0).nodeValue, operation[x].attributes.item(1).nodeValue);
+				else if(nodes[x].attributes.length == 2){
+					this[nodes[x].nodeName](nodes[x].attributes.item(0).nodeValue, nodes[x].attributes.item(1).nodeValue);
 				}
 			}	
 		}	
 	}
 	
-this.beginpath = function() {
-		ctx.lineWidth = 5;
-		console.log("func beginpath");
-		ctx.beginPath();
-}
+	/*
+	 * Canvas functions
+	 */
+	this.beginpath = function() {
+			ctx.lineWidth = 5;
+			ctx.beginPath();
+	}
 
-this.moveto = function(x, y) {
-	console.log("func moveTo" + x +" " +y);
-	ctx.moveTo(x,y);
-	
-}
+	this.moveto = function(x, y) {
+		ctx.moveTo(x,y);
+	}
 
-this.lineto = function(x, y) {
-	console.log("func lineTo");
-	ctx.lineTo(x, y);
-	
-}
+	this.lineto = function(x, y) {
+		ctx.lineTo(x, y);
+	}
 
-this.stroke = function() {
-	console.log("func stroke");
-	ctx.stroke();
+	this.stroke = function() {
+		ctx.stroke();
+	}
 	
-}
-	
+
+	/*
+	 *
+	 * Start running XML
+	 *
+	 */
 	var c = document.getElementById('Canvas');
 	var ctx = c.getContext("2d");
 	var delay = 0;
 	
-	console.log('asfsaf');
-	
-	//this.loadXMLDoc = function(Doc) 
-//	{
-	console.log('loadXML');
 	if (window.XMLHttpRequest){   
 		  // code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp=new XMLHttpRequest();
-		console.log('thisone');
 	}else {	
 		  // code for IE6, IE5
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  
 	}
 	  
+	// Open XML
 	xmlhttp.open("GET","canvas.xml",false);
   	xmlhttp.send();
   	xmlDoc=xmlhttp.responseXML;
 	
+  	// Load timesteps
   	timesteps = xmlDoc.getElementsByTagName("script")[0].childNodes;
-	  console.log(JSON.stringify(timesteps));
-	  console.log(timesteps.length);
-	  var totalTime = 0;
-	for(i = 0; i < timesteps.length; i++) {		 
-		operation = xmlDoc.getElementsByTagName("timestep");
-		if(operation[i]){
-		delay = operation[i].getAttribute("delay");
-		totalTime += parseInt(delay);
-		
-		console.log("delay " + totalTime);
-		
-		nodes = operation[i].childNodes;
-		setTimeout(parseLine, totalTime, nodes);
-		
+	var totalTime = 0;
 
-		
+	// Step through timesteps
+	for(i = 0; i < timesteps.length; i++){		 
+		timestepElements = xmlDoc.getElementsByTagName("timestep");
+
+		// Check for elements
+		if(timestepElements[i]){
+			// Fetch delay
+			delay = timestepElements[i].getAttribute("delay");
+			totalTime += parseInt(delay);
+			
+			// Fetch timestep nodes
+			nodes = timestepElements[i].childNodes;
+
+			// Execute timestep nodes after specified delay
+			setTimeout(executeTimestep, totalTime, nodes);
 		}
-	}
-	
-	
-	
+	}	
 }
