4d6ac563f41196abefc3dbb6c74a860a12f98699
h12linbe <h12linbe@student.his.se>
2014-04-11 11:39:11 +0200

    Implemented login-screen for #93
    Added new css-file to DuggaSys in folder css (duggasys.css)
    Added new JS-file to DuggaSys (startpage.js)
    Implemented the login-screen in basic.php located in DuggaSys

--- DuggaSys/Courselist.php
+++ DuggaSys/Courselist.php
@@ -1,7 +1,7 @@
 <html>
-<html>
 	<head>
 			<link type="text/css" href="../CodeViewer/css/codeviewer.css" rel="stylesheet" />	
+            <link type="text/css" href="../DuggaSys/css/duggasys.css" rel="stylesheet" />	
 			<script type="text/javascript" src="../CodeViewer/js/jquery-1.5.1.min.js"></script>
 			<script type="text/javascript" src="duggasys.js"></script>
 
