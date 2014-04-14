c2bfa22b32a72a4d3fa01d2c4ed0e1e56330dfd1
e12johka <MinMac@212-25-128-122.eduroam.his.se>
2014-04-14 12:04:20 +0200

    Changed tables in database
    Fixed conflicts on table section

--- Shared/SQL/init_db.sql
+++ Shared/SQL/init_db.sql
@@ -71,45 +71,53 @@ INSERT INTO section(coursename,sectionname,kind,cversion,sectionpos,appuser) val
 /* Code Example contains a list of the code examples for a version of a course in the database */
 /* Version of sections and examples corresponds roughly to year or semester that the course was given. */
 CREATE TABLE codeexample(
-		exampleid		MEDIUMINT NOT NULL AUTO_INCREMENT,
-		coursename		VARCHAR(64),
-		sectionid		MEDIUMINT NOT NULL,
-		examplename		VARCHAR(64),
+		exampleid			MEDIUMINT NOT NULL AUTO_INCREMENT,
+		cid					INT NOT NULL,
+		sectionid			MEDIUMINT NOT NULL,
+		examplename			VARCHAR(64),
 		wordlist			VARCHAR(64),
-		runlink			  VARCHAR(64),
-		pos						INTEGER,
+		runlink			  	VARCHAR(64),
+		pos					INTEGER,
 		cversion			INTEGER,
-		ts 						TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser						VARCHAR(64),
-		PRIMARY KEY(exampleno)		
+		updated 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid					INT NOT NULL,
+		PRIMARY KEY(exampleid),
+		FOREIGN KEY (cid) REFERENCES cource (cid),
+		FOREIGN KEY (sectionid) REFERENCES section (sectionid),
+		FOREIGN KEY (uid) REFERENCES user (uid)
+		
+	
 );
 
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Events 1","JS","",0,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Events 2","JS","",1,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Callback 1","GLSL","Culf.html",2,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Callback 2","GLSL","Dulf.html",3,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Callback 3","GLSL","",4,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="Javascript"),"Callback 4","JS","Fulf.html",5,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="HTML5"),"Design 1","GLSL","Gulf.html",0,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="HTML5"),"Design 2","JS","Hulf.html",1,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="HTML5"),"Design 3","JS","Iulf.html",2,"Creationscript",2013);
-INSERT INTO codeexample(coursename,sectionno,examplename,wordlist,runlink,pos,appuser,cversion) values ("Webbprogrammering",(select sectionno from section where coursename="Webbprogrammering" and sectionname="HTML5"),"Design 4","JS","Julf.html",3,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Events 1","JS","",0,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Events 2","JS","",1,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Callback 1","GLSL","Culf.html",2,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Callback 2","GLSL","Dulf.html",3,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Callback 3","GLSL","",4,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="Javascript"),"Callback 4","JS","Fulf.html",5,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="HTML5"),"Design 1","GLSL","Gulf.html",0,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="HTML5"),"Design 2","JS","Hulf.html",1,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="HTML5"),"Design 3","JS","Iulf.html",2,"Creationscript",2013);
+INSERT INTO codeexample(courseid,sectionid,examplename,wordlist,runlink,pos,uid,cversion) values (1,(select sectionid from section where courseid= 1 and sectionname="HTML5"),"Design 4","JS","Julf.html",3,"Creationscript",2013);
 
 
 /* boxes with information in a certain example */
 CREATE TABLE box(
 		boxid				INTEGER NOT NULL AUTO_INCREMENT,
-		exampleno 			INTEGER NOT NULL,
+		exampleid 			INTEGER NOT NULL,
 		boxcontent			VARCHAR(39),
-		descno				INT DEFAULT '0',
-		fileno				MEDIUMINT  DEFAULT '0',					
+		descid				INT DEFAULT '0',
+		fileid				MEDIUMINT  DEFAULT '0',					
 		settings			VARCHAR(1024),
-		PRIMARY KEY(boxid)		
+		PRIMARY KEY(boxid),
+		FOREIGN KEY (exampleid) REFERENCES codeexample (exampleid),
+		FOREIGN KEY (descid) REFERENCES descriptionsection (descid),
+		FOREIGN KEY (fileid) references filelist (fileid)
 );
 
-INSERT INTO box(exampleno,boxcontent,descno,fileno,settings) VALUES (1,"Document",1,1,"[viktig=1]");
-INSERT INTO box(exampleno,boxcontent,descno,fileno,settings) VALUES (1,"Document",1,2,"[viktig=1]");
-INSERT INTO box(exampleno,boxcontent,descno,fileno,settings) VALUES (1,"Document",1,3,"[viktig=1]");
+INSERT INTO box(exampleid,boxcontent,descid,fileid,settings) VALUES (1,"Document",1,1,"[viktig=1]");
+INSERT INTO box(exampleid,boxcontent,descid,fileid,settings) VALUES (1,"Document",1,2,"[viktig=1]");
+INSERT INTO box(exampleid,boxcontent,descid,fileid,settings) VALUES (1,"Document",1,3,"[viktig=1]");
 
 
 /* template with information about a certain template */
@@ -128,89 +136,99 @@ INSERT INTO template(templateid,stylesheet,boxid) VALUES (2,"template2.css",5);
 
 
 /* improw contains a list of the important rows for a certain example */
-
 CREATE TABLE improw(
-		exampleno 		INTEGER,
+		impid		  		MEDIUMINT NOT NULL AUTO_INCREMENT,
+		exampleid 			INTEGER NOT NULL,
 		istart				INTEGER,
-		iend					INTEGER,
+		iend				INTEGER,
 		irowdesc			VARCHAR(1024),
-		ts	 					TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser				VARCHAR(64),
-		impno		  MEDIUMINT NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY(impno)		
+		updated	 			TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid					INT NOT NULL,
+		PRIMARY KEY(impid),
+		FOREIGN KEY (uid) REFERENCES user (uid),
+		FOREIGN KEY (exampleid) REFERENCES codeexample (exampleid)
 );
 	
-INSERT INTO improw(exampleno,istart,iend,appuser) VALUES (3,6,8,"Creationscript");
-INSERT INTO improw(exampleno,istart,iend,appuser) VALUES (3,15,19,"Creationscript");
-INSERT INTO improw(exampleno,istart,iend,appuser) VALUES (3,10,12,"Creationscript");
+INSERT INTO improw(exampleid,istart,iend,uid) VALUES (3,6,8,1);
+INSERT INTO improw(exampleid,istart,iend,uid) VALUES (5,15,19,1);
+INSERT INTO improw(exampleid,istart,iend,uid) VALUES (7,10,12,2);
 
+
+/*filelist contains a list of shortcuts to files */
 CREATE TABLE filelist(
-		exampleno			INTEGER,
-		fileno		  	MEDIUMINT NOT NULL AUTO_INCREMENT,
+		fileid		  		MEDIUMINT NOT NULL AUTO_INCREMENT,
+		exampleid			INTEGER NOT NULL,
 		filename			VARCHAR(1024),
-		pos						INTEGER,
-		ts	 					TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser				VARCHAR(64),
-		PRIMARY KEY(fileno)		
+		pos					INTEGER,
+		updated	 			TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid					INT NOT NULL,
+		PRIMARY KEY(fileid),
+		FOREIGN KEY (exampleid) REFERENCES codeexample (exampleid),
+		FOREIGN KEY (uid) REFERENCES user (uid)
 );
 	
-INSERT INTO filelist(exampleno,filename,pos,appuser) VALUES (1,"js1.js",1,"Creationscript");
-INSERT INTO filelist(exampleno,filename,pos,appuser) VALUES (2,"js2.js",1,"Creationscript");
-INSERT INTO filelist(exampleno,filename,pos,appuser) VALUES (3,"js3.js",1,"Creationscript");
+INSERT INTO filelist(exampleid,filename,pos,uid) VALUES (1,"js1.js",1,1);
+INSERT INTO filelist(exampleid,filename,pos,uid) VALUES (2,"js2.js",1,1);
+INSERT INTO filelist(exampleid,filename,pos,uid) VALUES (3,"js3.js",1,2);
+
 
 CREATE TABLE descriptionsection(
-		exampleno			INTEGER,
-		segment				VARCHAR(1024),
-		pos						INTEGER,
-		ts	 					TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser				VARCHAR(64),
-		descno		  		MEDIUMINT NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY(descno)		
+		descid		  		MEDIUMINT NOT NULL AUTO_INCREMENT,
+		exampleid			INTEGER NOT NULL,
+		segment				VARCHAR(64000),
+		updated	 			TIMESTAMP 	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid					INTEGER NOT NULL,
+		PRIMARY KEY(descid),
+		FOREIGN KEY (exampleid) REFERENCES codeexample (exampleid),
+		FOREIGN KEY (uid) REFERENCES user (uid)
 );
 	
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (1,"<b>Events 1</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (2,"<b>Events 2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (3,"<b>Callback 1</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (4,"<b>Callback 2 S2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (5,"<b>Callback 3</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (6,"<b>Callback 4</b>This is the seond section of the description<b>Even More</b>This is even more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (7,"<b>Design 1</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (8,"<b>Design 2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (9,"<b>Design 3</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
-INSERT INTO descriptionsection(exampleno,segment,pos,appuser) VALUES (10,"<b>Design 4</b>This is the seond section of the description<b>Even More</b>This is even more text",1,"Creationscript");
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (1,"<b>Events 1</b>This is the first section of the description<b>More</b>This is more text",1,1);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (2,"<b>Events 2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,1);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (3,"<b>Callback 1</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (4,"<b>Callback 2 S2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,1);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (5,"<b>Callback 3</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (6,"<b>Callback 4</b>This is the seond section of the description<b>Even More</b>This is even more text",1,2);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (7,"<b>Design 1</b>This is the first section of the description<b>More</b>This is more text",1,"Creationscript");
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (8,"<b>Design 2</b>This is the seond section of the description<b>Even More</b>This is even more text",1,2);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (9,"<b>Design 3</b>This is the first section of the description<b>More</b>This is more text",1,2);
+INSERT INTO descriptionsection(exampleid,segment,uid) VALUES (10,"<b>Design 4</b>This is the seond section of the description<b>Even More</b>This is even more text",1,2);
 
 /* Wordlist contains a list of keywords for a certain programming language or file type */
 
 CREATE TABLE wordlist(
-		wordno		  	MEDIUMINT NOT NULL AUTO_INCREMENT,
+		wordid		  		MEDIUMINT NOT NULL AUTO_INCREMENT,
 		wordlist			VARCHAR(64),
-		word 					VARCHAR(64),
-		description		VARCHAR(256),
-		ts 						TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser						VARCHAR(64),
-		PRIMARY KEY(wordno)				
+		word 				VARCHAR(64),
+		description			VARCHAR(256),
+		updated 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid					INT NOT NULL,
+		PRIMARY KEY(wordid),
+		FOREIGN KEY (uid) REFERENCES user (uid)
 );
 
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("JS","for","Creationscript");
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("JS","if","Creationscript");
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("JS","var","Creationscript");
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("JS","function","Creationscript");
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("GLSL","vec3","Creationscript");
-INSERT INTO wordlist(wordlist,word,appuser) VALUES ("GLSL","dot","Creationscript");
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("JS","for",1);
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("JS","if",1);
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("JS","var",1);
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("JS","function",2);
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("GLSL","vec3",2);
+INSERT INTO wordlist(wordlist,word,uid) VALUES ("GLSL","dot",2);
 
 /* Wordlist contains a list of important words for a certain code example */
 
 CREATE TABLE impwordlist(
-		wordno		  	MEDIUMINT NOT NULL AUTO_INCREMENT,
-		exampleno			INTEGER,
-		word 					VARCHAR(64),
+		wordid		  	MEDIUMINT NOT NULL AUTO_INCREMENT,
+		exampleid		INTEGER NOT NULL,
+		word 			VARCHAR(64),
 		description		VARCHAR(256),
-		ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		appuser						VARCHAR(64),
-		PRIMARY KEY(wordno)				
+		UPDATED 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		uid				INTEGER NOT NULL,
+		PRIMARY KEY(wordid),
+		FOREIGN KEY (exampleid) REFERENCES codeexample (exampleid),
+		FOREIGN KEY (uid) REFERENCES user (uid)
 );
 
-INSERT INTO impwordlist(exampleno,word,appuser) values (3,"event","Creationscript");
-INSERT INTO impwordlist(exampleno,word,appuser) values (3,"elem","Creationscript");
-INSERT INTO impwordlist(exampleno,word,appuser) values (3,"pageY","Creationscript");
+INSERT INTO impwordlist(exampleid,word,uid) values (3,"event",1);
+INSERT INTO impwordlist(exampleid,word,uid) values (3,"elem",1);
+INSERT INTO impwordlist(exampleid,word,uid) values (3,"pageY",2);
 
