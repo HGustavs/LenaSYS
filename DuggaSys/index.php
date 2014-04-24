eacf6d9c1c61c927cd47c6916ee655560fab4c1c
Andreas <adde992+github@gmail.com>
2014-04-24 09:23:17 +0200

    Include the new stylesheet
    Some files will have to be updated to return this page to its former
    glory.

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -3,6 +3,7 @@
 	<head>
 			<link type="text/css" href="../CodeViewer/css/codeviewer.css" rel="stylesheet" />	
 			<link type="text/css" href="../DuggaSys/css/duggasys.css" rel="stylesheet" />	
+			<link type="text/css" href="../DuggaSys/css/style.css" rel="stylesheet" />
 			<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
 			<script type="text/javascript" src="startpage.js"></script>
 
