b8f34b9082c3a21aec19f0dbea2356d9b44a38c4
b12erifr <erik_frick@hotmail.com>
2014-04-24 11:49:24 +0200

    First virsion of Quiz (dugga system)
    *Dugga folder changed to quiz
    *Frontend
    *Run quiz
    *Edit quiz
    *Studentlist for quiz
    
    *Backend
    *getPages: loading pages in the pages folder. Loading and print a page
    to content div depending on what the browserURL says.
    *getTest: Controlling user acces for the requested quiz.
    
    *CSS
    *header style
    *div#content style
    *table style
    *button style
    etc.

---
+++ DuggaSys/quiz/pages/quiz.php
@@ -0,0 +1,10 @@
+<div>
+	<h2>Vem är den bästa fotbollsspelaren?</h2>
+<form>
+	<input type="checkbox" name="vehicle" value="1">Zlatan<br>
+	<input type="checkbox" name="vehicle" value="2">Hysen<br>
+	<input type="checkbox" name="vehicle" value="3">Aslan<br>
+	<input type="checkbox" name="vehicle" value="4">Åstrand<br><br>
+	<button type='submit' class='default'>Submit</button>
+</form>
+</div>
