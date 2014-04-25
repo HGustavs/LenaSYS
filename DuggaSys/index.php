6a43eb1ec2bd769bb745f21c03ae5aa8109b4937
Andreas <adde992+github@gmail.com>
2014-04-25 09:18:07 +0200

    Fix an issue with starting sessions

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -15,7 +18,6 @@ setupLogin();
 include_once("../../coursesyspw.php");	
 include_once("basic.php");
 
-session_start();
 dbConnect();
 
 courselist();
