c18c0b304963b7a8309e3e81eca30c182bfe1115
h12linbe <h12linbe@student.his.se>
2014-04-25 10:27:20 +0200

    Changed some design
    Made update to css file
    Made update to basic.php
    Made update to index.php

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -1,26 +1,43 @@
 <!DOCTYPE html>
 <html>
-	<head>
-			<link type="text/css" href="../CodeViewer/css/codeviewer.css" rel="stylesheet" />	
-			<link type="text/css" href="../DuggaSys/css/duggasys.css" rel="stylesheet" />	
-			<link type="text/css" href="../DuggaSys/css/style.css" rel="stylesheet" />
-			<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
-			<script type="text/javascript" src="startpage.js"></script>
+<head>
+		<!--<link type="text/css" href="../CodeViewer/css/codeviewer.css" rel="stylesheet" />-->	
+		<link type="text/css" href="../DuggaSys/css/duggasys.css" rel="stylesheet" />	
+		<link type="text/css" href="../DuggaSys/css/style.css" rel="stylesheet" />
+		<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="startpage.js"></script>
 
-<script>
-setupLogin();
-</script>
-	</head>
-	<body>
+		<script>
+			setupLogin();
+		</script>
+</head>
+<body>
+    <header>
+    	<nav id="navigate">
+        	<!--<img src="css/svg/Up.svg">-->
+        </nav>
+        <div id="title">
+        	<h1>LenaSYS</h1>
+        </div>
+        <nav id="user">
+        	<span>Inloggad: Test</span>
+        	<img src="../CodeViewer/icons/Man.svg" onclick="loginbox();">
+        </nav>
+    </header>
+    <div id="content">
 <?php
-include_once("../../coursesyspw.php");	
-include_once("basic.php");
+	include_once("../../coursesyspw.php");	
+	include_once("basic.php");
 
-session_start();
-dbConnect();
+	session_start();
+	dbConnect();
 
-courselist();
-loginwins();
+	courselist();
+	
+?>
+	</div>
+<?php
+	loginwins();
 ?>			
-			</body>
+</body>
 </html>
