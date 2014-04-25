35d62ab7f41443bbf56ed53baa2d210c8f11bf8f
a12madka <quake.2@hotmail.com>
2014-04-25 09:42:27 +0200

    Fixed logfile will save new file for every day,time.

--- PlayerEditor/CanvasWrapper/logfile.php
+++ PlayerEditor/CanvasWrapper/logfile.php
@@ -1,10 +1,12 @@
 <?php
-	$filename="canvas.xml";
+	
+	$filename=date('d-m-Y G.i:s') . ".xml";
 	$content = $_POST['string'];
 	
 	echo("creating file");
 	if(isset($content)) {
-		$fh=fopen($filename,"w");
+		$path=("/home/StudentHome/2012/a12madka/public_html/GitHub/LenaSYS_2014/PlayerEditor/CanvasWrapper/logs");
+		$fh=fopen($path."/".$filename,'w');
 		fwrite($fh, $content);
 		fclose($fh);
 		echo("File Created, Click <a href='$filename'> Here </a> to view it.");
