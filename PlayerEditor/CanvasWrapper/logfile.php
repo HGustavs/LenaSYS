a9a46bbf401c9ce0b92b08b84e067cfd72966c38
a12madka <quake.2@hotmail.com>
2014-04-25 11:16:41 +0200

    Update on logfile for canvaswrapper

--- PlayerEditor/CanvasWrapper/logfile.php
+++ PlayerEditor/CanvasWrapper/logfile.php
@@ -5,8 +5,8 @@
 	
 	echo("creating file");
 	if(isset($content)) {
-		$path=("/home/StudentHome/2012/a12madka/public_html/GitHub/LenaSYS_2014/PlayerEditor/CanvasWrapper/logs");
-		$fh=fopen($path."/".$filename,'w');
+		$fh=fopen("logs/".$filename,'w');
+		
 		fwrite($fh, $content);
 		fclose($fh);
 		echo("File Created, Click <a href='$filename'> Here </a> to view it.");
