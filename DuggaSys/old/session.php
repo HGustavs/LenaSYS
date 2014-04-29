21f5b5381222868e3273ffdd9c419d82ea1c07a3
b12erifr <erik_frick@hotmail.com>
2014-04-29 15:28:25 +0200

    Update! DugaSys - Quiz
    Polished functionality...

--- DuggaSys/old/session.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<pre>";
-print_r(session_get_cookie_params());
-echo "</pre>";
-?>
--- DuggaSys/quiz/pages/noid.php
+++ DuggaSys/quiz/pages/noid.php
@@ -1,4 +1,11 @@
+<?php session_start(); ?>
 <div class='alert alert-danger'>
     <strong>Oh snap!</strong>
     <p>Dead end, please go back and come back with some ID.</p>
 </div>
+
+<?php if(isset($_SESSION['user']['permission']) && ($_SESSION['user']['permission'] < 3)) {?>
+	<script type="text/javascript">
+		changeURL('menu');
+	</script>
+<?php } ?>
