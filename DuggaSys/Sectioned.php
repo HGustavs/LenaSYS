61e209d90296b7081ce299560a5ee3896ac2b606
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-04-30 01:11:04 +0200

    Optimized drag and drop update

--- DuggaSys/Sectioned.php
+++ DuggaSys/Sectioned.php
@@ -61,21 +61,33 @@ session_start();
 										?>
 										<script>
 										$(function() {
+											// Initialize timer object
+											var timer = null;
 											// Placeholder
 											$( "#Sectionlist" ).sortable({
 												opacity: 0.5,
 												cursor: "move",
 												items: "> span",
 												update: function() {
-													// Pass course ID to check write access
-													var array = $(this).sortable("serialize") + "&courseid=" + '<?php echo $courseID; ?>';
-													$.post("entryupdate.php", array, function(theResponse){
-														$("#dragupdate").html(theResponse);
-														$("#dragupdate").slideDown('slow');
-														setTimeout(function(){
-														  $("#dragupdate").slideUp("slow", function () { });
-														}, 2000);
-													});
+													// Check if timer was initialized
+													if (timer != null) {
+														// Clear the timer
+														clearInterval(timer);
+													}
+													var serialized = $(this).sortable("serialize");
+													timer = setTimeout(function(){
+														// Pass course ID to check write access
+														var array = serialized + "&courseid=" + '<?php echo $courseID; ?>';
+														$.post("entryupdate.php", array, function(theResponse){
+															$("#dragupdate").html(theResponse);
+															$("#dragupdate").slideDown('slow');
+															setTimeout(function(){
+															  $("#dragupdate").slideUp("slow", function () { });
+															}, 2000);
+															timer = null;
+														});
+													}, 4000);
+													
 												}
 											});
 										});
