2873d491b9279a3e56581ec04e0c40489f325e13
a12antgu <a12antgu@student.his.se>
2014-05-05 09:24:46 +0200

    Version 1.4

--- CodeViewer/js/codeviewer.js
+++ CodeViewer/js/codeviewer.js
@@ -147,17 +147,7 @@ function styleReset()
 document.addEventListener("drop", function(e) {
     // cancel drop
     e.preventDefault();
-   
 });
-document.addEventListener("paste", function(e) {
-    // cancel paste
-    e.preventDefault();
-    // get text representation of clipboard
-    var text = e.clipboardData.getData("text/plain");
-    // insert text manually
-    document.execCommand("insertText", false, text);
-});
-
 document.addEventListener("paste", function(e) {
     // cancel paste
     e.preventDefault();
