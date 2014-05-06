13c65e97dee5d1ba522efa56d7ccf8c8e5431b3a
c12danla <daniellarsson91@gmail.com>
2014-05-06 10:39:34 +0200

    Implemented themes
    Added functionality to make it possible to change themes. Colors has
    been moved from codeviewer.css to separate theme files.

--- CodeViewer/basic.php
+++ CodeViewer/basic.php
@@ -9,67 +9,69 @@ include "../Shared/basic.php";
 		{	
 				// Body if we are allowed to run code viewer
 				echo '<body style="margin: 0; padding: 0;" onload="setup();">';
-				echo '<span id="forwdrop" class="dropdown forwdrop "><div class="dropdownback">Forw</div></span>';
-				echo '<span id="backwdrop" class="dropdown backwdrop "><div class="dropdownback">Backw</div></span>';
-				echo '<span id="hotdogdrop" class="dropdown showmobile" style="overflow:scroll;">';
+				echo '<span id="forwdrop" class="dropdown dropdownStyle forwdrop "><div class="dropdownback dropdownbackStyle">Forw</div></span>';
+				echo '<span id="backwdrop" class="dropdown dropdownStyle backwdrop "><div class="dropdownback dropdownbackStyle">Backw</div></span>';
+				echo '<span id="hotdogdrop" class="hotdogdropStyle dropdown dropdownStyle showmobile" style="overflow:auto;">';
 					echo '<table cellspacing="0" class="showmobile"><tr>';
-						echo '<td class="butto " title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
-						echo '<td class="butto beforebutton " id="beforebutton" title="Previous example" onmousedown="SkipBDown();" onmouseup="SkipBUp();" onclick="SkipB();"><img src="new icons/backward_button.svg" /></td>';
-						echo '<td class="butto afterbutton " id="afterbutton" title="Next example" onmousedown="SkipFDown();" onmouseup="SkipFUp();" onclick="SkipF();""><img src="new icons/forward_button.svg" /></td>';
-						echo '<td class="butto playbutton " id="playbutton" title="Open demo" onclick="Play();"><img src="new icons/play_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle " title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle beforebutton " id="beforebutton" title="Previous example" onmousedown="SkipBDown();" onmouseup="SkipBUp();" onclick="SkipB();"><img src="new icons/backward_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle afterbutton " id="afterbutton" title="Next example" onmousedown="SkipFDown();" onmouseup="SkipFUp();" onclick="SkipF();""><img src="new icons/forward_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle playbutton " id="playbutton" title="Open demo" onclick="Play();"><img src="new icons/play_button.svg" /></td>';
 					echo '</tr>';
-						echo '<tr><td class="butto " title="Show code" onclick="" colspan="3">Code</td><td class="butto">IMG</td></tr>';
-						echo '<tr><td class="butto " title="Show description" onclick="" colspan="3">Description</td><td class="butto">IMG</td></tr>';
-						echo '<tr><td class="butto " title="Show JS" onclick="" colspan="3">JS</td><td class="butto">IMG</td></tr>';
-						echo '<tr><td class="butto " title="Show rownumbers" onclick="" colspan="3">Show rownumbers</td><td class="butto">IMG</td></tr>';
-						echo '<tr><td class="butto " title="Settings" onclick="" colspan="4">Settings</td></tr>';
-						echo '<tr><td class="butto " title="Change to desktop site" onclick="" colspan="4">Desktop site</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Show code" onclick="" colspan="3">Code</td><td class="butto">IMG</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Show description" onclick="" colspan="3">Description</td><td class="butto">IMG</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Show JS" onclick="" colspan="3">JS</td><td class="butto">IMG</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Show rownumbers" onclick="" colspan="3">Show rownumbers</td><td class="butto">IMG</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Settings" onclick="" colspan="4">Settings</td></tr>';
+						echo '<tr><td class="mbutto mbuttoStyle " title="Change to desktop site" onclick="" colspan="4">Desktop site</td></tr>';
 					echo '</table>';
 			echo '</span>';				
 				if($kind){
-						echo '<span id="codedrop" class="dropdown codedrop " style="overflow:scroll;"><div class="dropdownback">Code viewer Code File Selector</div></span>';
-						echo '<span id="docudrop" class="dropdown docudrop " style="overflow:scroll;"><div class="dropdownback">General settings</div></span>';
+						echo '<span id="codedrop" class="dropdown dropdownStyle codedrop codedropStyle " style="overflow:auto;"><div class="dropdownback dropdownbackStyle">Code viewer Code File Selector</div></span>';
+						echo '<span id="docudrop" class="dropdown dropdownStyle docudrop docudropStyle" style="overflow:auto;"><div class="dropdownback dropdownbackStyle">General settings</div></span>';
+						echo '<span id="themedrop" class="dropdown dropdownStyle themedrop themedropStyle" style="overflow:auto;"><div class="dropdownback dropdownbackStyle">Theme</div></span>';
+						
 				}
 
-				echo '<div id="buttomenu">';
+				echo '<div id="buttomenu" class="buttomenuStyle">';
 				echo '<table cellspacing="2"><tr>';
 				if($kind){
 
 								echo '<td class="buttospacer">&nbsp;</td>';
-								echo '<td class="menutext"><span id="exampleSection">Foo</span>&nbsp;:&nbsp;<span id="exampleName" contenteditable="true">Example Code Page</td>';
-								echo '<td class="butto showdesktop" title="Settings" id="hidesettings"><img src="new icons/general_settings_button.svg" /></td>';
-								echo '<td class="butto showdesktop" title="Select codesource" id="hidecode"><img src="new icons/list_codefiles.svg" /></td>';
-								
+								echo '<td class="menutext menutextStyle"><span id="exampleSection">Foo</span>&nbsp;:&nbsp;<span id="exampleName" contenteditable="true">Example Code Page</td>';
+								echo '<td class="mbutto mbuttoStyle showdesktop" title="Settings" id="hidesettings"><img src="new icons/general_settings_button.svg" /></td>';
+								echo '<td class="mbutto mbuttoStyle showdesktop" title="Select codesource" id="hidecode"><img src="new icons/list_codefiles.svg" /></td>';
+								echo '<td class="mbutto mbuttoStyle showdesktop" title="Themes" id="hidetheme" onclick="Theme();"><img src="new icons/theme_button.svg" /></td>';
 								
 				}else{
-								echo '<td class="menutext"><span id="exampleSection">Foo</span>&nbsp;:&nbsp;<span id="exampleName">Example Code Page</td>';
+								echo '<td class="menutext menutextStyle"><span id="exampleSection">Foo</span>&nbsp;:&nbsp;<span id="exampleName">Example Code Page</td>';
 				}		
-						echo '<td class="butto showmobile" title="Menu" id="hidehotdog"><img src="new icons/hotdog_button.svg" /></td>';
-						echo '<td class="butto showdesktop" title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
-						echo '<td class="butto showdesktop" id="numberbutton" title="Show/hide linenumbers" onclick="fadelinenumbers();"><img src="new icons/numbers_button.svg" /></td>';
-						echo '<td class="butto beforebutton showdesktop" id="beforebutton" title="Previous example" onmousedown="SkipBDown();" onmouseup="SkipBUp();" onclick="SkipB();"><img src="new icons/backward_button.svg" /></td>';
-						echo '<td class="butto afterbutton showdesktop" id="afterbutton" title="Next example" onmousedown="SkipFDown();" onmouseup="SkipFUp();" onclick="SkipF();"><img src="new icons/forward_button.svg" /></td>';
-						echo '<td class="butto playbutton showdesktop" id="playbutton" title="Open demo" onclick="Play();"><img src="new icons/play_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle showmobile" title="Menu" id="hidehotdog"><img src="new icons/hotdog_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle showdesktop" title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle showdesktop" id="numberbutton" title="Show/hide linenumbers" onclick="fadelinenumbers();"><img src="new icons/numbers_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle beforebutton showdesktop" id="beforebutton" title="Previous example" onmousedown="SkipBDown();" onmouseup="SkipBUp();" onclick="SkipB();"><img src="new icons/backward_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle afterbutton showdesktop" id="afterbutton" title="Next example" onmousedown="SkipFDown();" onmouseup="SkipFUp();" onclick="SkipF();"><img src="new icons/forward_button.svg" /></td>';
+						echo '<td class="mbutto mbuttoStyle playbutton showdesktop" id="playbutton" title="Open demo" onclick="Play();"><img src="new icons/play_button.svg" /></td>';
 						echo '<td class="buttospacer">&nbsp;</td>';
 
 				echo '</table></div>';
 				echo '<div id="div2" style="width:100%; position: absolute; top: 50px; bottom: 0px; background-color:#def">';
-				echo '<div id="docucontent"';
+				echo '<div id="docucontent" class="docucontentStyle"';
 				if($kind){
 						echo ' contenteditable="true" >';				
 				}else{ 
 						echo '>';
 				} 
-				echo '<div style="left:20px" class="warning">';
+				echo '<div style="left:20px" class="warning warningStyle">';
 						echo 'Please wait while content loads<br/>';
 						echo '<img src="new icons/loadingJS.gif" /><br/>';
 						echo 'Do not forget to enable Javascript<br/>';
 						echo '</div>';
 						echo '</div>';
-						echo '<div id="codebox">';
+						echo '<div id="codebox" class="codeboxStyle">';
 				//		echo '<span id="playlinkErrorMsg" onclick="$(this).hide();"></span>';
 						echo '<div id="infobox" class="codeview">';
-						echo '<div style="left:300px" class="warning">';
+						echo '<div style="left:300px" class="warning warningStyle">';
 							echo 'Please wait while content loads<br/>';
 							echo '<img src="new icons/loadingJS.gif" /><br/>';
 							echo 'Do not forget to enable Javascript<br/>';
