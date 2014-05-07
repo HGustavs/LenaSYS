44bd06c2d9f7b82a1a379573c484a0efe9e8f713
Manfred Johansson <b12davjo@student.his.se>
2014-05-07 15:40:04 +0200

    Remove old javascript in index.php

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -1,19 +1,15 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
-<head>	
+<head>
 		<link type="text/css" href="css/style.css" rel="stylesheet" />
 		<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="js/function.js"></script>
 		<script src="header/header.js"></script>
 </head>
 <body>
-
-	<script type="text/javascript">
-	window.onload = function() {
-		changeURL("menulist");
-	}
-	</script>
-
     <header>
     	  <div class="load"></div>
     </header>
