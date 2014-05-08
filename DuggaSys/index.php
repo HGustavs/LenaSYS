275bc38d75a5f0118988127880ac9bc53f7ed996
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-05-08 02:17:27 +0200

    Added jQuery UI to index.php

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -6,6 +6,7 @@
 <head>
 		<link type="text/css" href="css/style.css" rel="stylesheet" />
 		<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 		<script type="text/javascript" src="js/function.js"></script>
 		<script src="header/header.js"></script>
 </head>
