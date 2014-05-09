47a482850b5cde9052ec4357a715caa4fcc440d8
Manfred Johansson <b12davjo@student.his.se>
2014-05-09 14:28:09 +0200

    Minor updates on course create form

--- DuggaSys/js/ajax.js
+++ DuggaSys/js/ajax.js
@@ -4,22 +4,38 @@
  		$.ajax({
  			dataType: "json",
 			type: "POST",
-			url:"ajax/createNewCourse.php",
+			url: "ajax/createNewCourse.php",
 			data: {
 				coursename: document.newCourse.coursename.value,
 				coursecode: document.newCourse.coursecode.value,
 				visib: document.newCourse.visib.value
 			},
 			success:function(data) {
-				console.log(data.cid);
 				if (data.cid>0) {
 					changeURL("sectioned?courseid="+data.cid);	
-				};
+				} else if(data==="no access") {
+					alert("ap ap ap!");	
+				}
 				
 			},
 			error:function() {
-				console.log("error");
+				console.log("Something went wrong");
 			}
 		});
+
+$.ajax({
+		dataType: "json",
+		type: "POST",
+		url: 'addstudent_ajax.php',
+		data: {
+		string: $("#string").val()
+	},
+		success: function (data) {
+		//console.log(data);
+		showPopUp('show', data)
+	},
+	});
+
+
  	};
  }
