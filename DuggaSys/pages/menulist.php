b064c3e5f58c848c0ec94216fc4758b96bb352a3
Andreas <adde992+github@gmail.com>
2014-05-09 19:21:21 +0200

    Fix crash in menulist
    Menulist was using an old table that has been removed from the database.
    Moved the code over to PDO while fixing it.

--- DuggaSys/pages/menulist.php
+++ DuggaSys/pages/menulist.php
@@ -3,7 +3,7 @@ session_start();
 include_once(dirname(__file__)."/../../../coursesyspw.php");	
 include_once(dirname(__file__)."/../../shared/database.php");
 
-dbConnect();
+pdoConnect();
 ?>
 <div style="margin-bottom:7px; float:right;"><input style="cursor:pointer;" onclick="changeURL('newCourseForm')" class='submit-button' type='button' value='Add course'></div>
 <table class="course-table" cellspacing="0">
@@ -13,10 +13,17 @@ dbConnect();
 	<?php
 		//$counter is used with the styling mechanism
 		$counter = 0;
-		$querystring="SELECT course.coursename,max(cversion) AS version, cid AS id, visibility FROM course LEFT OUTER JOIN section ON course.coursename=section.coursename GROUP BY course.coursename";
-		$result=mysql_query($querystring);
-		if (!$result) err("SQL Query Error: ".mysql_error(),"Database Password Check Error");
-		while ($row = mysql_fetch_assoc($result)){
+		$query=$pdo->query("SELECT course.coursename,cid AS id,visibility FROM course ORDER BY coursename");
+		$result=$query->execute();
+		if (!$result) {
+			$errorinfo = $query->errorInfo();
+			$errormsg = $errorinfo[2];
+			$error_sqlcode = $errorinfo[0];
+			$error_myerr = $errorinfo[1];
+			// Should this really be here? This is information leakage.
+			err("SQL Query Error: ". $error_sqlcode . " :" . $error_myerr . " " . $errormsg);
+		}
+		while ($row = $query->fetch(PDO::FETCH_ASSOC)){
 			//Sets $color to either true or false (different colors) depending on row counter.
 			$color=($counter % 2 == 0) ? '#f0f0f0' : '#e3e3e3';
 			$counter++;
