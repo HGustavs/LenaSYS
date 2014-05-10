73db789437dd75ddec674f9230362a607d7f667f
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-05-10 14:55:01 +0200

    Cleaned up duggasys.js

--- DuggaSys/duggasys.js
+++ DuggaSys/duggasys.js
@@ -11,100 +11,52 @@ function returnedSection(data)
 		// For now we only have two kinds of sections
 		for(i=0;i<data['entries'].length;i++){
 			if (parseInt(data['entries'][i]['visible']) === 1 || sessionkind === true) {
-				switch(parseInt(data['entries'][i]['kind'])) {
-					case 0:
-						// Styling for header row
-						if (parseInt(data['entries'][i]['visible']) === 0) {
-							str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
-						} else {
+				if (parseInt(data['entries'][i]['visible']) === 0) {
+					str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
+				} else {
+					switch(parseInt(data['entries'][i]['kind'])) {
+						case 0:
+							// Styling for header row
 							str+="<span class='bigg' id='Entry_"+data['entries'][i]['lid']+"'>";
-						}
-						if(sessionkind===true){
-							str+="<span id='SE"+data['entries'][i]['lid']+"' >"+data['entries'][i]['entryname']+"</span>";
-							str+="<span class='smallishbutt'>";
-							str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";											
-							str+="</span>";
-						}else{
-							str+="<span id='SE"+data['entries'][i]['lid']+"'>"+data['entries'][i]['entryname']+"</span>";						
-						}
-						str+="</span>";
-						break;
-					case 1:
-						//Styling for section row
-						if (parseInt(data['entries'][i]['visible']) === 0) {
-							str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
-						} else {
+							break;
+						case 1:
+							//Styling for section row
 							str+="<span class='butt' id='Entry_"+data['entries'][i]['lid']+"'>";
-						}
-
-						// If we are allowed to edit
-						if(sessionkind===true){
-							str+="<span id='SE"+data['entries'][i]['lid']+"'>"+data['entries'][i]['entryname']+"</span>";
-							str+="<span class='smallbutt'>";
-							str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";
-							str+="</span>";
-						}else{
-							str+="<span id='SE"+data['entries'][i]['lid']+"'>"+data['entries'][i]['entryname']+"</span>";						
-						}
-						
-						// End of butt span
-						str+="</span>"
-						break;
-					case 2:
-						// Styling for example row
-						if (parseInt(data['entries'][i]['visible']) === 0) {
-							str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
-						} else {
+							break;
+						case 2:
+							// Styling for example row
 							str+="<span class='example' id='Entry_"+data['entries'][i]['lid']+"'>";
-						}
-						if(sessionkind===true){
-								str+="<span id='EX"+data['entries'][i]['lid']+"'><a id='section-list' href='../CodeViewer/EditorV30.php?exampleid=" + data['entries'][i]['code_id'] + "&courseid=" + data.coursename + "'>"+data['entries'][i]['entryname']+"</a></span>";
-								str+="<span class='smallbutt'>";
-								str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";											
-								str+="</span>"
-						}else{
-								str+="<a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a>";		
-						}
-						str+="</span>";
-						break;
-					case 3:
-						// Styling for test row
-						if (parseInt(data['entries'][i]['visible']) === 0) {
-							str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
-						} else {
+							break;
+						case 3:
+							// Styling for test row
 							str+="<span class='test' id='Entry_"+data['entries'][i]['lid']+"'>";
-						}
-						if(sessionkind===true){
-								str+="<span id='EX"+data['entries'][i]['lid']+"'><a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a></span>";
-								str+="<span class='smallbutt'>";
-								str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";										
-								str+="</span>"
-						}else{
-								str+="<a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a>";		
-						}
-						str+="</span>";
-						break;
-					default:
-					case 4:
-						// Styling for 'others' row
-						if (parseInt(data['entries'][i]['visible']) === 0) {
-							str+="<span class='hidden' id='Entry_"+data['entries'][i]['lid']+"'>";
-						} else {
+							break;
+						default:
+						case 4:
+							// Styling for 'others' row
 							str+="<span class='norm' id='Entry_"+data['entries'][i]['lid']+"'>";
-						}
-						if(sessionkind===true){
-								str+="<span id='EX"+data['entries'][i]['lid']+"'><a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a></span>";
-								str+="<span style='float:right;' class='smallbutt'>";
-								str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";											
-								str+="</span>"
-						}else{
-								str+="<a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a>";		
-						}
-						str+="</span>";
-						break;
+							break;
 					}
 				}
+				
+				if(sessionkind) {
+					if (parseInt(data['entries'][i]['kind']) < 2) {
+						str+="<span>"+data['entries'][i]['entryname']+"</span>";
+						str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";
+					} else {
+						str+="<span><a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a></span>";
+						str+="<img id='table-img-coggwheel' src='css/images/general_settings_button_darkgrey.svg' />";	
+					}
+				} else {
+					if (parseInt(data['entries'][i]['kind']) < 2) {
+						str+="<span>"+data['entries'][i]['entryname']+"</span>";
+					} else {
+						str+="<span><a id='section-list' href="+data['entries'][i]['link']+">"+data['entries'][i]['entryname']+"</a></span>";
+					}
+				}
+				str+="</span>";
 			}
+		}
 			
 			var slist=document.getElementById('Sectionlist');
 			slist.innerHTML=str;
