178adddae88ea4f8b51d2ab352966ac89c9a3b9d
Andreas <adde992+github@gmail.com>
2014-05-11 22:51:09 +0200

    Added support for event logging
    It is now possible to log important events to the database (such as
    failed logins and other audit information).

--- DuggaSys/header/ajax/login.php
+++ DuggaSys/header/ajax/login.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 include_once dirname(__FILE__) . "/../../../../coursesyspw.php";
-include_once dirname(__FILE__) . "/../../../shared/sessions.php";
+include_once dirname(__FILE__) . "/../../../Shared/sessions.php";
+include_once dirname(__FILE__) . "/../../../Shared/functions.php";
 
 if(array_key_exists('username', $_POST) && array_key_exists('password', $_POST)) {
 	$username = $_POST['username'];
@@ -24,6 +25,14 @@ if(login($username, $password, $savelogin)) {
 	// one, notify the login script of this.
 	if($_SESSION['newpw'] === true)
 		$res["newpw"] = true;
+} else {
+	// There's no user logged in so there's no user to associate this event with.
+	log_message(
+		NULL,
+		sprintf("Failed login attempt for user %s",
+			htmlentities($username)
+		)
+	);
 }
 
 // Return the data as JSON
