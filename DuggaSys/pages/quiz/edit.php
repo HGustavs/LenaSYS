e0bf1f3532f7cdd9e136be7c10e7a4b6343dd61e
Manfred Johansson <b12davjo@student.his.se>
2014-05-14 16:55:57 +0200

    Add datetimepicker to application
    The need for date and time rules out JQuery’s own date picker so one
    with time and date is now i use.

--- DuggaSys/pages/quiz/edit.php
+++ DuggaSys/pages/quiz/edit.php
@@ -1,52 +1,20 @@
 <?php session_start(); ?>
 
 
-	<script>
-		$(function() {
-			$( ".datepicker" ).datepicker({
-				showButtonPanel: true
-			});
-		    var next = 1;
-		    $(".add-more").click(function(e){
-		        e.preventDefault();
-		        var addto = "#field" + next;
-		        var addRemove = "#field" + (next);
-		        next = next + 1;
-		        var newIn = '<input autocomplete="off" class="option form-control" id="field' + next + '" name="field' + next + '" type="text">';
-		        var newInput = $(newIn);
-		        var removeBtn = '<button id="remove' + (next - 1) + '" class="option remove-me" >-</button></div><div id="field">';
-		        var removeButton = $(removeBtn);
-		        $(addto).after(newInput);
-		        $(addRemove).after(removeButton);
-		        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-		        $("#count").val(next);  
-		        
-		            $('.remove-me').click(function(e){
-		                e.preventDefault();
-		                var fieldNum = this.id.charAt(this.id.length-1);
-		                var fieldID = "#field" + fieldNum;
-		                $(this).remove();
-		                $(fieldID).remove();
-		            });
-		    });
-		});
-		function showVariant() {
-			$(".variant1").css('display','block');
-		}
-	</script>
+	
 	<div id='create'>
 		<form role='form'>
 			<div class='form-group'>
 				<label>Dugganame</label>
 				<input type="text" class='form-control' />
 				<div class='form-group'>
-					<label>Template parameters (?)</label>
+					<label>Template parameters, saves as string (max 2000 characters)</label>
 					<textarea class='form-control' rows='5'></textarea>
 				</div>
 				<div class='form-group'>
 					<label>Answer</label>
 						<div class="input-append">
-		                    <div id="field"><input autocomplete="off" class='option form-control' id="field1" name="prof1" type="text" data-items="8"/><button id="b1" class="option add-more" type="button">+</button></div>
+		                    <div id="field"><input autocomplete="off" class='option form-control' id="field1" name="prof1" type="text" data-items="8"/></div>
 		                </div>
 				</div>
 				<div class='form-group'>		
@@ -64,11 +32,11 @@
 				</div>
 				<div class='form-group'>
 					<label>Releasedate</label>
-					<input class='form-control datepicker'>
+					<input type="text" class='form-control datetimepicker' />
 				</div>
 				<div class='form-group'>
 					<label>Deadline</label>
-					<input class='form-control datepicker'>
+					<input type="text" class='form-control datetimepicker' />
 				</div>
 				<div class='form-group'>		
 					<label>
@@ -77,6 +45,46 @@
 				</div>
 				
 			</div>
-			<button onclick="alert('create')" class='default'>Submit</button>
+			<button onclick="submitNewQuiz(1, 'w')" class='default'>Submit</button>
 		</form>
 	</div>
+<script>
+	$(function() {
+		
+	    var next = 1;
+	    /*
+	    $(".add-more").click(function(e){
+	        e.preventDefault();
+	        var addto = "#field" + next;
+	        var addRemove = "#field" + (next);
+	        next = next + 1;
+	        var newIn = '<input autocomplete="off" class="option form-control" id="field' + next + '" name="field' + next + '" type="text">';
+	        var newInput = $(newIn);
+	        var removeBtn = '<button id="remove' + (next - 1) + '" class="option remove-me" >-</button></div><div id="field">';
+	        var removeButton = $(removeBtn);
+	        $(addto).after(newInput);
+	        $(addRemove).after(removeButton);
+	        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+	        $("#count").val(next);  
+	        
+	            $('.remove-me').click(function(e){
+	                e.preventDefault();
+	                var fieldNum = this.id.charAt(this.id.length-1);
+	                var fieldID = "#field" + fieldNum;
+	                $(this).remove();
+	                $(fieldID).remove();
+	            });
+	    });
+		*/
+	});
+	function showVariant() {
+		$(".variant1").css('display','block');
+	}
+</script>
+<link rel="stylesheet" type="text/css" href="css/jquery.datetimepicker.css"/ >
+<script src="js/ajax.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/jquery.datetimepicker.js"></script>
+<script type="text/javascript">
+	$('.datetimepicker').datetimepicker();
+</script>
