868181d41ff32b010174f9ff4d5446117c57ccc9
Andreas <adde992+github@gmail.com>
2014-05-15 09:36:43 +0200

    Add access restrictions to some of the pages in the system
    There really needs to be a way of adding GET or POST parameters to ajax-loaded
    pages though, to provide some way of making sure people that aren't allowed to
    access the pages are completely unable to see the forms.

--- DuggaSys/pages/addstudent.php
+++ DuggaSys/pages/addstudent.php
@@ -1,3 +1,8 @@
+<?php
+include_once(dirname(__FILE__) . "/../../Shared/sessions.php");
+session_start();
+if(checklogin()) {
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -14,11 +19,9 @@
 		<br>
 		<textarea placeholder="SSN, Name, email" name="string" id="string" cols="30"></textarea>
 		<br>
-		<input type="button" value="Lägg till student" class="btn btn-login" onclick="passPopUp();"/>
+		<input type="button" value="Add student" class="btn btn-login" onclick="passPopUp();"/>
 		<a href="students.php"><input type="button" class="btn btn-cancel" value="Cancel"/></a>
 	</form>
-
-
 		<div id="light" class="white_content">
 		</div>
 </div>
@@ -26,3 +29,6 @@
 
 </body>
 </html>
+<?php
+}
+?>
