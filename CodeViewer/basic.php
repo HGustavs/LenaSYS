f548cf62f8bb5c78a5587c3a928bf0983fefc644
a12antgu <a12antgu@student.his.se>
2014-05-20 09:59:47 +0200

    mobile version commit buggfix

--- CodeViewer/basic.php
+++ CodeViewer/basic.php
@@ -12,29 +12,7 @@ include "../Shared/basic.php";
 				echo '<span id="themedrop" class="dropdown dropdownStyle themedrop themedropStyle" style="overflow:auto;"><div class="dropdownback dropdownbackStyle">Theme</div></span>';
 				echo '<span id="forwdrop" class="dropdown dropdownStyle forwdrop "><div class="dropdownback dropdownbackStyle">Forw</div></span>';
 				echo '<span id="backwdrop" class="dropdown dropdownStyle backwdrop "><div class="dropdownback dropdownbackStyle">Backw</div></span>';
-
-				echo '<span id="hotdogdrop" class="hotdogdropStyle dropdown dropdownStyle showmobile" style="overflow:auto;">';
-					echo '<div id="table-wrapper"><div id="table-scroll">';
-					echo '<table cellspacing="0" class="showmobile"><tr>';
-						echo '<td class="mbutto mbuttoStyle " title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
-						echo '<td class="mbutto mbuttoStyle beforebutton " id="beforebutton" title="Previous example" onmousedown="SkipBDown();" onmouseup="SkipBUp();" onclick="SkipB();"><img src="new icons/backward_button.svg" /></td>';
-						echo '<td class="mbutto mbuttoStyle afterbutton " id="afterbutton" title="Next example" onmousedown="SkipFDown();" onmouseup="SkipFUp();" onclick="SkipF();""><img src="new icons/forward_button.svg" /></td>';
-						echo '<td class="mbutto mbuttoStyle playbutton " id="playbutton" title="Open demo" onclick="Play();"><img src="new icons/play_button.svg" /></td>';
-					echo '</tr>';	
-						echo '<tr><td class="mbutto mbuttoStyle " title="Show code" onclick="" colspan="4">Code<img src="new icons/hotdogTabButton2.svg" /></td></tr>';
- 						echo '<tr><td class="mbutto mbuttoStyle " title="Show description" onclick="" colspan="4">Description<img src="new icons/hotdogTabButton2.svg" /></td></tr>';
- 						echo '<tr><td class="mbutto mbuttoStyle " title="Show JS" onclick="" colspan="4">JS<img src="new icons/hotdogTabButton2.svg" /></td></tr>';
- 						echo '<tr><td id="numberbuttonMobile" class="mbutto mbuttoStyle " title="Show rownumbers" onclick="fadelinenumbers();" colspan="4">Show rownumbers<img src="new icons/hotdogTabButton.svg" /></td></tr>';
-						echo '<tr><td class="mbutto mbuttoStyle " title="Settings" onclick="" colspan="4">Settings</td></tr>';
-						echo '<tr><td class="mbutto mbuttoStyle " title="Change to desktop site" onclick="" colspan="4">Desktop site</td></tr>';
-						echo '<tr><td class="mbutto mbuttoStyle " title="Chose themes" onclick="mobileTheme()" colspan="4">Theme </td></tr>';
-						    echo '<tr><td class="mbutto mbuttoStyleLight mobilethemebutton themeicon subbutton" colspan="4" onclick="selectTheme(&quot;black&quot;);"><img src="new icons/theme_black.svg"><span>Black background</span></td></tr>';
-							echo '<tr><td class="mbutto mbuttoStyleLight mobilethemebutton themeicon subbutton" colspan="4" onclick="selectTheme(&quot;white&quot;);"><img src="new icons/theme_white.svg"><span>White background</span></td></tr>';
-							echo '<tr><td class="mbutto mbuttoStyleLight mobilethemebutton themeicon subbutton" colspan="4" onclick="selectTheme(&quot;colorblind&quot;);"><img src="new icons/theme_blind.svg"><span>Colorblind</span></td></tr>';
-							//echo '</td></tr>';
-					echo '</table>';
-					echo '</div></div>';
-			echo '</span>';				
+			
 				if($kind){
 						echo '<span id="codedrop" class="dropdown dropdownStyle codedrop codedropStyle " style="overflow:auto;"><div class="dropdownback dropdownbackStyle">Code viewer Code File Selector</div></span>';
 						echo '<span id="docudrop" class="dropdown dropdownStyle docudrop docudropStyle" style="overflow:auto;"><div class="dropdownback dropdownbackStyle">General settings</div></span>';
@@ -44,13 +22,11 @@ include "../Shared/basic.php";
 				echo '<div id="buttomenu" class="buttomenuStyle">';
 				echo '<table cellspacing="2"><tr>';
 				echo '<td class="mbutto mbuttoStyle showdesktop" title="Back to list" onclick="Up();"><img src="new icons/home_button.svg" /></td>';
-			
-								echo '<td class="buttospacer">&nbsp;</td>';
-								echo '<td class="menutext menutextStyle"><span id="exampleSection">Foo </span><span id="exampleName" contenteditable="true">Example Code Page</td>';
-								echo '<td><span id="feedbacksection"></span></td>';
-								echo '<td class="mbutto mbuttoStyle showdesktop" id="savebutton" title="Save" onclick="Save();"><img src="new icons/save_button.svg" /></td>';
-								echo '<td class="mbutto mbuttoStyle showdesktop" title="Settings" id="hidesettings"><img src="new icons/general_settings_button.svg" /></td>';
-								
+				if($kind){		
+					echo '<td class="buttospacer">&nbsp;</td>';
+					echo '<td class="menutext menutextStyle"><span id="exampleSection">Foo </span><span id="exampleName" contenteditable="true">Example Code Page</td>';								echo '<td><span id="feedbacksection"></span></td>';
+					echo '<td class="mbutto mbuttoStyle showdesktop" id="savebutton" title="Save" onclick="Save();"><img src="new icons/save_button.svg" /></td>';
+					echo '<td class="mbutto mbuttoStyle showdesktop" title="Settings" id="hidesettings"><img src="new icons/general_settings_button.svg" /></td>';
 				}else{
 					echo '<td class="menutext menutextStyle"><span id="exampleSection">Foo</span>&nbsp;:&nbsp;<span id="exampleName">Example Code Page</td>';
 				}		
@@ -64,36 +40,6 @@ include "../Shared/basic.php";
 
 				echo '</table></div>';
 				echo '<div id="div2" style="width:100%; position: absolute; top: 50px; bottom: 0px;">';
-
-				if($kind){
-             /*       echo '<div id="buttomenu2">';
-                    echo '<table cellspacing="2"><tr>';
-                    echo '<td class="butto2" title="Remove formatting" onclick="styleReset();"><img src="new icons/reset_button.svg" /></td>';
-                    echo '<td class="butto2" title="Heading" onclick="styleHeader();"><img src="new icons/boldtext_button.svg" /></td>';
-                    echo '<td class="butto2" title="Code example" onclick="styleCode();"><img src="new icons/quote_button.svg" /></td>';
-                    echo '<td class="butto2" id="hideimage" title="Select image"><img src="new icons/picture_button.svg" /></td>';
-                    echo '<td class="butto2" title="Save" onclick="Save();"><img src="new icons/save_button.svg" /></td>';
-                    echo '</tr></table></div>';  */
-           //      echo '<div id="docucontent" class="docucontentStyle" contenteditable="true">';
-				}
-            else{
-            //   echo '<div id="docucontent" class="docucontentStyle">';
-            }
-        		/*    echo '<div style="left:20px" class="warning">';
-						echo 'Please wait while content loads<br/>';
-						echo '<img src="new icons/loadingJS.gif" /><br/>';
-						echo 'Do not forget to enable Javascript<br/>';
-						echo '</div>';
-						echo '</div>';
-						echo '<div id="codebox" class="codeboxStyle">';
-						echo '<div id="infobox" class="codeview">';
-						echo '<div style="left:300px" class="warning warningStyle">';
-							echo 'Please wait while content loads<br/>';
-							echo '<img src="new icons/loadingJS.gif" /><br/>';
-							echo 'Do not forget to enable Javascript<br/>';
-					echo '</div>';
-				echo '</div>';
-		echo '</div>';
-	*/	echo '</div>';
+			echo '</div>';
 		}
 ?>
