fe0a6584fed44fe5f686662eb68561f9041048fa
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-05-20 17:43:48 +0200

    Added placeholder when there are no results

--- DuggaSys/js/pagination.js
+++ DuggaSys/js/pagination.js
@@ -6,6 +6,7 @@
 
 function pagination() {
 	var items = [];
+	// Amount of cells in a row
 	var cells = 5;
 	var show_per_page = 5;
 	
@@ -30,41 +31,52 @@ function pagination() {
 
 	this.showContent = function() {
 		var table = document.getElementById("contentlist");
-		for (i = (this.currentPage * this.show_per_page); i < (this.show_per_page * (this.currentPage + 1)); i++) {
-			if (this.items.entries[i]) {
-				var row = table.insertRow(i + 1);
-				if (parseInt(this.items.entries[i]["grade"]) >= 3) {
-					row.className = "green";
-				} else if (parseInt(this.items.entries[i]["grade"]) < 3) {
-					row.className = "red";
-				}
-				for (j = 0; j < cells; j++) {
-					var cell = row.insertCell(j);
-					switch (j) {
-						case 0:
-							cell.innerHTML = this.items.entries[i]["coursename"];
-							break;
-						case 1:
-							cell.innerHTML = this.items.entries[i]["coursecode"];
-							break;
-						case 2:
-							cell.innerHTML = this.items.entries[i]["name"];
-							break;
-						case 3:
-							cell.innerHTML = this.items.entries[i]["submitted"];
-							break;
-						case 4:
-							if (parseInt(this.items.entries[i]["grade"]) >= 3) {
-								cell.innerHTML = this.items.entries[i]["grade"];
-							} else if (parseInt(this.items.entries[i]["grade"]) < 3) {
-								cell.innerHTML = "U";
-							}
-							break;
+		// If there are any items to print
+		if (pagination.number_of_items > 0) {
+			// Print items currentPage * show_per_page to show_per_page * (currentPage + 1)
+			for (i = (this.currentPage * this.show_per_page); i < (this.show_per_page * (this.currentPage + 1)); i++) {
+				// If item is defined
+				if (this.items.entries[i]) {
+					// Insert row below <th>
+					var row = table.insertRow(i + 1);
+					
+					if (parseInt(this.items.entries[i]["grade"]) >= 3) {
+						row.className = "green";
+					} else if (parseInt(this.items.entries[i]["grade"]) < 3) {
+						row.className = "red";
 					}
+					for (j = 0; j < cells; j++) {
+						var cell = row.insertCell(j);
+						switch (j) {
+							case 0:
+								cell.innerHTML = this.items.entries[i]["coursename"];
+								break;
+							case 1:
+								cell.innerHTML = this.items.entries[i]["coursecode"];
+								break;
+							case 2:
+								cell.innerHTML = this.items.entries[i]["name"];
+								break;
+							case 3:
+								cell.innerHTML = this.items.entries[i]["submitted"];
+								break;
+							case 4:
+								if (parseInt(this.items.entries[i]["grade"]) >= 3) {
+									cell.innerHTML = this.items.entries[i]["grade"];
+								} else if (parseInt(this.items.entries[i]["grade"]) < 3) {
+									cell.innerHTML = "U";
+								}
+								break;
+						}
+					}
+				} else {
+					// No more items to print
+					break;
 				}
-			} else {
-				break;
 			}
+		} else {
+			$('#content').empty();
+			$('#content').append("<div class='no_results'>There are currently no results available</div>")
 		}
 	}
 
