0fd014d2a341d008609ec6a979aa4fb8da8b5a7f
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-05-21 17:00:34 +0200

    Fixed some pagination bugs

--- DuggaSys/js/pagination.js
+++ DuggaSys/js/pagination.js
@@ -1,15 +1,15 @@
 function pagination() {
-	var items = [];
+	this.items = [];
 	// Amount of cells in a row
-	var cells = 6;
-	var show_per_page = 5;
+	this.cells = 6;
+	this.show_per_page = 5;
 	
-	var number_of_items = 0;
-	var number_of_pages = 0;
+	this.number_of_items = 0;
+	this.number_of_pages = 0;
 	
-	var max_pages = 5;
+	this.max_pages = 5;
 	
-	var currentPage = 0;
+	this.currentPage = 0;
 	
 	this.renderPages = function() {
 		$('#pages').empty();
@@ -40,7 +40,7 @@ function pagination() {
 			}
 		}
 		$('#pages').append("<div class='page' onClick='pagination.next()'>Next</div>");
-		if (this.max_pages < this.number_of_pages) {
+		if (this.max_pages < this.number_of_pages && this.currentPage + 1 != this.number_of_pages) {
 			$('#pages').append("<div class='page' onClick='pagination.goToPage("+(this.number_of_pages-1)+")'>Last page</div>");
 		}
 	}
@@ -81,7 +81,7 @@ function pagination() {
 					} else if (parseInt(this.items.entries[i]["grade"]) < 3) {
 						row.className = "red";
 					}
-					for (j = 0; j < cells; j++) {
+					for (j = 0; j < this.cells; j++) {
 						var cell = row.insertCell(j);
 						switch (j) {
 							case 0:
@@ -146,10 +146,6 @@ function getResults(pagination) {
 			} else {
 				pagination.items = data;
 				pagination.number_of_items = pagination.items.entries.length;
-				pagination.cells = 5;
-				pagination.max_pages = 5;
-				pagination.show_per_page = 5;
-				pagination.currentPage = 0;
 				pagination.number_of_pages = Math.ceil(pagination.number_of_items/pagination.show_per_page);
 				if (pagination.number_of_pages > 1) {
 					$('#content').append("<div id='pages'></div>");
