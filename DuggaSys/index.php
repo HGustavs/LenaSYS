53a746ceffda8bb2ba1d8d95f245a10bc43afa63
a12aleny <a12aleny@student.his.se>
2014-05-24 16:20:06 +0200

    Nothing to see here
    Rawr

--- DuggaSys/index.php
+++ DuggaSys/index.php
@@ -9,8 +9,19 @@
 	<script src="js/function.js"></script>
     <script type="text/javascript" src="js/section.js"></script>
 	<script src="header/header.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+<script>!window.jQuery && document.write('<script src="jquery-1.4.3.min.js"><\/script>')</script>
+<script src="jquery.raptorize.1.0.js"></script>
+<script type="text/javascript">
+     $(window).load(function() {
+      $('.myButton').raptorize({
+        'enterOn' : 'timer', 
+        'delayTime' : 3000 
+   });
+});
+</script>
 </head>
-<body>
+<body class="myButton">
 	<header>
 		<div class="load"></div>
 	</header>
