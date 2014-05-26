d5f828c0ca7b49745d4b7367fc6aa58a22ea3d6f
Linus Esbjörnsson <unclebubba1887@gmail.com>
2014-05-26 14:30:55 +0200

    Fixed studentlist not showing all students

--- DuggaSys/ajax/studentlist_results.php
+++ DuggaSys/ajax/studentlist_results.php
@@ -21,13 +21,25 @@
 				if (!$result) err("SQL Query Error: ".$pdo->errorInfo(),"Field Querying Error!");
 				
 				$link = null;
+				$dugganame = null;
+				$start = null;
+				$deadline = null;
+				$gradesystem = null;
+				$correctAnswer = null;
 				foreach($query->fetchAll() as $row) {
+				
+					$dugganame = $row['name'];
+					$start = $row['release'];
+					$deadline = $row['deadline'];
+					$gradesystem = $row['gradesystem'];
+					$correctAnswer = $row['answer'];
+				
 					$quizQuery = $pdo->prepare("SELECT * FROM userAnswer WHERE testID = :1");
 					$quizQuery -> bindParam(':1', $row['id']);
 					$answers=$quizQuery->execute();
 					foreach($quizQuery->fetchAll() as $quizRow) {
-						if (!in_array($quizRow['testID'], $completed)) {
-							array_push($completed, $quizRow['testID']);
+						if (!in_array($quizRow['uid'], $completed)) {
+							array_push($completed, $quizRow['uid']);
 						}
 						
 						$userQuery = $pdo->prepare("SELECT * FROM user WHERE uid = :1");
@@ -55,37 +67,37 @@
 					
 				}
 				
-				$query = $pdo->prepare("SELECT * FROM quiz WHERE id = :1");
-				$query -> bindParam(':1', $_POST['quizid']);
+				$query = $pdo->prepare("SELECT * FROM user_course WHERE cid = :1");
+				$query -> bindParam(':1', $_POST['courseid']);
 				$result=$query->execute();
 				if (!$result) err("SQL Query Error: ".$pdo->errorInfo(),"Field Querying Error!");
 				
-				$link = null;
 				foreach($query->fetchAll() as $row) {
-					if (!in_array($row['id'], $completed)) {
+					if (!in_array($row['uid'], $completed)) {
+						array_push($completed, $row['uid']);
 						$expired = false;
 						$date = new DateTime("now");
-						$deadline = new DateTime($row['deadline']);
-						if ($date >= $deadline) {
+						$parsedDeadline = new DateTime($deadline);
+						if ($date >= $parsedDeadline) {
 							$expired = true;
 						}
 						
 						$userQuery = $pdo->prepare("SELECT * FROM user WHERE uid = :1");
-						$userQuery -> bindParam(':1', $quizRow['uid']);
+						$userQuery -> bindParam(':1', $row['uid']);
 						$users=$userQuery->execute();
 						foreach($userQuery->fetchAll() as $userRow) {
 							array_push(
 								$entries,
 								array(
 									'username' => $userRow['username'],
-									'name' => $row['name'],
-									'start' => $row['release'],
-									'deadline' => $row['deadline'],
+									'name' => $dugganame,
+									'start' => $start,
+									'deadline' => $deadline,
 									'submitted' => "",
 									'grade' => "",
-									'gradesystem' => $row['gradesystem'],
+									'gradesystem' => $gradesystem,
 									'answer' => "",
-									'correctAnswer' => $row['answer'],
+									'correctAnswer' => $correctAnswer,
 									'link' => $link,
 									'expired' => $expired
 								)
