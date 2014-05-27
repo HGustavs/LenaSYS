ad3e226517124efedc5d0f5fcef061a32614ad14
h12linbe <h12linbe@student.his.se>
2014-05-27 10:45:31 +0200

    Updated studentlist
    Added some option value for grade and some ajax that need to be
    connected. Need to look at "submitted" in the database also since this
    is updated every time you update a row in the database table but when
    changing the grade should not change the "submitted" date.

---
+++ DuggaSys/ajax/updateStudentGrade.php
@@ -0,0 +1,31 @@
+<?php 
+session_start(); 
+
+include_once(dirname(__file__)."/../../Shared/sessions.php");
+include_once(dirname(__file__)."/../../Shared/courses.php");
+
+if (checklogin()) {
+	if (isSuperUser($_SESSION["uid"]) || hasAccess($_SESSION["uid"], $_POST["courseid"], 'w')) {
+		include_once(dirname(__file__)."/../../../coursesyspw.php");
+		include_once(dirname(__file__)."/../../Shared/database.php");
+		pdoConnect();
+		$success = true;
+		$stmt = $pdo -> prepare('UPDATE `useranswer` SET `grade` = :3 WHERE `testID` = :1 AND `uid` = :2  ');
+		$stmt -> bindParam(':1', $_POST["quizid"]);
+		$stmt -> bindParam(':2', $_POST["uid"]);
+		$stmt -> bindParam(':3', $_POST["grade"]);
+		
+		if (!$stmt -> execute()) {
+			$success = FALSE;
+		}
+
+		echo json_encode("Successfully updated student grade!");
+		
+	} else {
+		echo json_encode("No write access");
+	}
+} else {
+	echo json_encode("No access");
+}
+
+?>
