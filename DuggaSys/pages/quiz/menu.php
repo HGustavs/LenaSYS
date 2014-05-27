48307b16555f1f926ab535084c95155fc77937cf
Manfred Johansson <b12davjo@student.his.se>
2014-05-27 11:25:11 +0200

    Added security updates on quiz menu

--- DuggaSys/pages/quiz/menu.php
+++ DuggaSys/pages/quiz/menu.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php session_start(); 
+include_once(dirname(__file__)."/../../../Shared/sessions.php");	
+if (checklogin()) {	
+?>
 <script src="js/ajax.js"></script>
 <script>
 page.title("Quiz menu");
@@ -6,10 +9,19 @@ page.title("Quiz menu");
 <div class='middle center'>
 	<p id="admin_title"></p>
 	<button class='default' onclick="changeURL('quiz/quiz?quizid='+getUrlVars().quizid);">Test</button>
+	
+	<?php if (hasAccess($_SESSION["uid"], $_POST["courseid"], "w")) { ?>
 	<button class='default' onclick="changeURL('quiz/edit?courseid=' + qs.courseid + '&quizid='+qs.quizid);">Edit</button>
+	<?php } ?>
+	
 	<button class='default' onclick="changeURL('quiz/studentlist?courseid=' + qs.courseid + '&quizid=' + qs.quizid);">Studentlist</button>
 </div>
 <script type="text/javascript">
 	var qs = getUrlVars();
 	getQuizNameForMenu(qs.quizid, qs.courseid);
 </script>
+<?php
+} else {
+include(dirname(__file__)."/../404.php");
+}
+?>
