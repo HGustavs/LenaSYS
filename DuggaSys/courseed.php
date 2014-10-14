4c6a57ce6099225ccb3383b466065a13b3506abb
Marcus Brohede <a97marbr@gmail.com>
2014-10-14 13:39:18 +0200

    i.e. fix to force edge mode

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -7,6 +7,7 @@ pdoConnect();
 <!DOCTYPE html>
 <html>
 <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Course Editor</title>
 		
