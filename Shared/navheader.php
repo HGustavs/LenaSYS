b23cd53b3ef0547dc7cc674e72a0c49be06ec410
a13chrju <ChristopherJubell@hotmail.com>
2015-04-01 17:18:41 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -1,12 +1,12 @@
-	<!-- Navigation Header! -->
+<!-- Navigation Header! -->
 	<!-- New Version includes code for Code Viewer -->
 	
 	<header>
 				<?php
-					echo "<table width='100%'><tr>";
+					echo "<div style='height:60px'>";
 					
 					include_once "../Shared/basic.php";
-					
+				
 					// Set the target for the 'Upward' Button
 					if($noup=="COURSE"){
 							echo "<td class='navButt'><a href='courseed.php'><img src='../Shared/icons/Up.svg'></a></td>";
@@ -20,7 +20,7 @@
 				
 					// Either generate code viewer specific nav menu or a spacer
 					if(isset($codeviewer)){
-							echo "<td class='navButt' id='beforebutton' title='Previous example' onmousedown='Skip(\"bd\");' onmouseup='Skip(\"bu\");' onclick='Skip(\"b\");'><img src='../Shared/icons/backward_button.svg'></td>";
+							echo "<table class='tableclassen'><td class='navButt' id='beforebutton' title='Previous example' onmousedown='Skip(\"bd\");' onmouseup='Skip(\"bu\");' onclick='Skip(\"b\");'><img src='../Shared/icons/backward_button.svg'></td>";
 							echo "<td class='navButt' id='afterbutton' title='Next example' onmousedown='Skip(\"fd\");' onmouseup='Skip(\"fu\");' onclick='Skip(\"f\");'><img src='../Shared/icons/forward_button.svg' /></td>";
 							echo "<td class='navButt' id='playbutton' title='Open demo' onclick='Play();'><img src='../Shared/icons/play_button.svg' /></td>";
 							if(checklogin()) {
@@ -35,19 +35,21 @@
 									echo "<span id='exampleName'>Exname</span>";
 							echo "</td>";					
 					}else{
-							echo "<td class='navSpacer'></td>";
+							echo "<td class='navSpacer'></td></table>";
 					}
 
 					if(checklogin()) {
-							echo "<td class='navName'><label id='userName'>".$_SESSION['loginname']."</label></td>";		
-							echo "<td id='loginbutton' class='loggedin'><img src='../Shared/icons/Man.svg' onclick='processLogout(\"";
+								
+							echo "<div id='loginbutton' class='loggedin'><img src='../Shared/icons/Man.svg' onclick='processLogout(\"";
 							echo $loginvar;
-							echo "\");' /></td>";
+							echo "\");' /></div>";
+							echo "<div class='navName'><label id='userName'>".$_SESSION['loginname']."</label></div>";
 					}else{
-							echo "<td class='navName'><label id='userName'>Guest</label></td>";		
-							echo "<td id='loginbutton' class='loggedout'><img src='../Shared/icons/Man.svg' onclick='showLoginPopup();' /></td>";
+									
+							echo "<div id='loginbutton' class='loggedout'><img src='../Shared/icons/Man.svg' onclick='showLoginPopup();' /></div>";
+							echo "<div class='navName'><label id='userName'>Guest</label></div>";	
 					}
 
-					echo "</tr></table>";
+					echo "</div>";
 				?>
 	</header>
