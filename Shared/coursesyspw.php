481f1a9ad3bfa08c17f2c9fea41b32d26cab2762
c12jenek <c12jenek@student.his.se>
2015-04-02 09:55:33 +0200

    Update coursesyspw.php

--- Shared/coursesyspw.php
+++ Shared/coursesyspw.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_USER","write your database user name here");
-define("DB_PASSWORD","write your password here");
+define("DB_USER","vadsomhelstannat");
+define("DB_PASSWORD","annatpassword");
 define("DB_HOST","localhost");
-define("DB_NAME","write your database name here");
+define("DB_NAME","imperious");
 
 ?>
