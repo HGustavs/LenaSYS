2206799d4b4b2145fa97a36b43a1a8026a61c839
a13chrju <ChristopherJubell@hotmail.com>
2015-04-08 13:52:22 +0200

    Update dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -2,6 +2,20 @@
 // changeCSS: Changes the CSS and remembers the index of the CSS.
 //            This allows us to set and remove whole CSS files
 //----------------------------------------------------------------------------------
+var status = 0;
+function toggleloginnewpass(){
+
+	if(status == 0){
+		$("#newpassword").css("display", "block");
+		$("#login").css("display", "none");
+		status++;
+	}
+	else if(status == 1){
+		$("#newpassword").css("display", "none");
+		$("#login").css("display", "block");
+		status= 0;
+	}
+}
 
 function changeCSS(cssFile, index)
 {
