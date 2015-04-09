814e447cc07c15537624b30746e95712a097f3f6
a13erisc <a13erisc@student.his.se>
2015-04-09 16:20:17 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -9,15 +9,15 @@
 					
 					// Set the target for the 'Upward' Button
 					if($noup=="COURSE"){
-							echo "<td class='navButt'><a href='../DuggaSys/courseed.php'><img src='../Shared/icons/Home.svg'></a></td>";
+							echo "<td class='navButt' id='home' title='Home'><a href='../DuggaSys/courseed.php'><img src='../Shared/icons/Home.svg'></a></td>";
 					}else if($noup=="SECTION"){
 							$cid=getOPG('cid');
 							$coursevers=getOPG('coursevers');
-							echo "<td class='navButt'><a href='../DuggaSys/sectioned.php?courseid=".$cid."&coursevers=".$coursevers."'><img src='../Shared/icons/Home.svg'></a></td>";
+							echo "<td class='navButt' id='home' title='Home'><a href='../DuggaSys/sectioned.php?courseid=".$cid."&coursevers=".$coursevers."'><img src='../Shared/icons/Home.svg'></a></td>";
 					}else if($noup=="CodeViewToSECTION"){
 							$cid=getOPG('cid');
 							$coursevers=getOPG('coursevers');
-							echo "<td class='navButt'><a href='../DuggaSys/sectioned.php?courseid=".$cid."&coursevers=".$coursevers."'><img src='../Shared/icons/Home.svg'></a></td>";
+							echo "<td class='navButt' id='home' title='Home'><a href='../DuggaSys/sectioned.php?courseid=".$cid."&coursevers=".$coursevers."'><img src='../Shared/icons/Home.svg'></a></td>";
 					}else {
 						// None!
 					}
