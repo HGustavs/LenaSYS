9aaaa4a4ab7320049517eed94bd7590c4f74b8e6
a13antda <a13antda@student.his.se>
2015-04-10 15:08:31 +0200

    syntax error fix

--- Shared/SQL/init_db.sql
+++ Shared/SQL/init_db.sql
@@ -239,7 +239,6 @@ INSERT INTO codeexample(cid,examplename,runlink,uid,cversion,templateid) values 
 INSERT INTO codeexample(cid,examplename,runlink,uid,cversion) values (1,"Design 2","Hulf.html",2,2013);
 INSERT INTO codeexample(cid,examplename,runlink,uid,cversion) values (1,"Design 3","Iulf.html",1,2013);
 INSERT INTO codeexample(cid,examplename,runlink,uid,cversion) values (1,"Design 4","Julf.html",1,2013);
-INSERT INTO codeexample(cid,sectionname,examplename,runlink,uid,cversion,exampleid,templateid) values (1,"HelloWorld-Html","HTML","html1.html",2,2013,1);
 INSERT INTO codeexample(cid,sectionname,examplename,runlink,uid,cversion,templateid,afterid,beforeid) values (1,"HTMLex2","HTML","html2.html",2,2013,1,12,13);
  
 /* improw contains a list of the important rows for a certain example */
@@ -313,8 +312,6 @@ INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,filename) VALUES (1
 INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,segment) VALUES (2,1,"Title","Document","[viktig=1]","<b>Events 1</b>This is the first section of the description<b>More</b>This is more text");
 INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,filename,wordlistid) VALUES (1,12,"TitleA","Code","[viktig=1]","html1.html",1);
 INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,segment,wordlistid) VALUES (2,12,"TitleB","Document","[viktig=1]","<title>page title</title>",1);
-INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,filename,wordlistid) VALUES (1,13,"TitleA","Code","[viktig=1]","html2.html",1);
-INSERT INTO box(boxid,exampleid,boxtitle,boxcontent,settings,segment,wordlistid) VALUES (2,13,"TitleB","Document","[viktig=1]","Styling HTML with CSS",1);
 
 /* improw contains a list of the important rows for a certain example */
 CREATE TABLE improw(
