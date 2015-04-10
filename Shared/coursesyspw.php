e02cc003775979cf52b736bb20195ac5494eb7ae
a13denli <a13denli@gmail.com>
2015-04-10 16:21:30 +0200

    Create coursesyspw.php

---
+++ Shared/coursesyspw.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_USER","Enter user name here");
+define("DB_PASSWORD","Enter password here");
+define("DB_HOST","Enter host name here");
+define("DB_NAME","Enter database name here");
+
+?>
