15e0f16eb28dc4baf3cfc00b22604196a4642bad
c12jenek <c12jenek@student.his.se>
2015-04-10 15:00:06 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -17,20 +17,8 @@
 			}else {
 				// None!
 			}
-		
-			// Either generate code viewer specific nav menu or a spacer
-			if(isset($codeviewer)){
-					echo "<td class='navButt' id='templatebutton' title='Change Template' onclick='openTemplateWindow();'><img src='../Shared/icons/choose_template.svg' /></td>";
-					echo "<td class='navButt' id='beforebutton' title='Previous example' onmousedown='Skip(\"bd\");' onmouseup='Skip(\"bu\");' onclick='Skip(\"b\");'><img src='../Shared/icons/backward_button.svg'></td>";
-					echo "<td class='navButt' id='afterbutton' title='Next example' onmousedown='Skip(\"fd\");' onmouseup='Skip(\"fu\");' onclick='Skip(\"f\");'><img src='../Shared/icons/forward_button.svg' /></td>";
-					echo "<td class='navButt' id='playbutton' title='Open demo' onclick='Play();'><img src='../Shared/icons/play_button.svg' /></td>";
-					if(checklogin()) {
-							echo "<td class='navButt' id='settings' title='Edit example settings' onclick='displayEditExample();' ><img src='../Shared/icons/general_settings_button.svg' /></td>";
-					}
-			}
-
 	
-		// Either generate code viewer specific nav menu or a spacer
+			// Either generate code viewer specific nav menu or a spacer
 			if(isset($codeviewer)){
 					echo "<td class='navButt' id='beforebutton' title='Previous example' onmousedown='Skip(\"bd\");' onmouseup='Skip(\"bu\");' onclick='Skip(\"b\");'><img src='../Shared/icons/backward_button.svg'></td>";
 					echo "<td class='navButt' id='afterbutton' title='Next example' onmousedown='Skip(\"fd\");' onmouseup='Skip(\"fu\");' onclick='Skip(\"f\");'><img src='../Shared/icons/forward_button.svg' /></td>";
@@ -60,6 +48,6 @@
 			}
 
 
-echo "</tr></table>";
-?>
+		echo "</tr></table>";
+	?>
 </header>
