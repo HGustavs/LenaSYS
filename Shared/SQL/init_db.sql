9620efbcccd516221d55d946efc013af359e3f29
a13antda <a13antda@student.his.se>
2015-04-10 11:52:28 +0200

    Update init_db.sql

--- Shared/SQL/init_db.sql
+++ Shared/SQL/init_db.sql
@@ -3,7 +3,6 @@ create database imperious;
 use imperious;
 /* user contains the users of the system and related  information */
 
-
 CREATE TABLE user(
 		uid					INT UNSIGNED NOT NULL AUTO_INCREMENT,
 		username		VARCHAR(80) NOT NULL UNIQUE,
@@ -65,9 +64,15 @@ CREATE TABLE user_course(
 		FOREIGN KEY (cid) REFERENCES course (cid) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO user_course(uid,cid,access) values (1,1,"W");
+/* test data */
+INSERT INTO user_course(uid,cid,access) values (1,2,"W");
+INSERT INTO user_course(uid,cid,access) values (1,1,"R");
+INSERT INTO user_course(uid,cid,access) values (1,3,"R");
 INSERT INTO user_course(uid,cid,access) values (2,1,"R");
-INSERT INTO user_course(uid,cid,access) values (1,2,"R");
+INSERT INTO user_course(uid,cid,access) values (2,2,"R");
+INSERT INTO user_course(uid,cid,access) values (2,3,"R");
+INSERT INTO user_course(uid,cid,access) values (3,3,"R");
+
 
 CREATE TABLE listentries (
 	lid 					INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -400,27 +405,34 @@ CREATE TABLE subparts(
 	FOREIGN KEY (cid) REFERENCES course (cid)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB;
 
-INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta1',3,1,'u345');
-INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta2',3,1.5,'u345');
-INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta3',3,1.5,'u345');
-INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('projektuppgift',3,3.5,'ug');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta2',1,1,'u345');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta',2,2,'u345');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta',1,2,'u345');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('projektuppgift',2,3,'ug');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('projektuppgift',3,5,'u345');
+INSERT INTO subparts(partname,cid,parthp,difgrade) VALUES ('hemtenta',3,2,'u345');
 
 /**
  * this table for many to mny between user and subparts. 
  */ 
 CREATE TABLE partresult (
-    uid	INT UNSIGNED NOT NULL,
+    cid INT UNSIGNED NOT NULL,
+	uid	INT UNSIGNED NOT NULL,
 	partname varchar(50),
 	grade varchar(2) DEFAULT NULL,
-	PRIMARY KEY(partname, uid),
-	FOREIGN KEY (partname) REFERENCES subparts (partname),
+	PRIMARY KEY(partname, cid, uid),
+	FOREIGN KEY (partname,cid) REFERENCES subparts (partname,cid),
 	FOREIGN KEY (uid) REFERENCES user (uid)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB;
 
-INSERT INTO partresult(uid,partname,grade) VALUES (2,'hemtenta1',4);
-INSERT INTO partresult(uid,partname,grade) VALUES (2,'hemtenta2',3);
-INSERT INTO partresult(uid,partname,grade) VALUES (3,'hemtenta3','u');
-INSERT INTO partresult(uid,partname,grade) VALUES (3,'projektuppgift','g');
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (1,1,'hemtenta2',4);
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (2,1,'hemtenta',3);
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (1,2,'hemtenta','u');
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (2,2,'projektuppgift','g');
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (3,3,'hemtenta',5);
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (3,3,'projektuppgift',5);
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (1,3,'hemtenta2',4);
+INSERT INTO partresult(cid,uid,partname,grade) VALUES (1,3,'hemtenta',3);
 
 /**
  * this table many to many relation between class and course. 
@@ -470,13 +482,6 @@ CREATE TABLE studentresultat (
 INSERT INTO programkurs VALUES (45,'WEBUG12h','DA135G','Datakommunikation - Introduktion G1N 7,5 hp','87524',5,NULL,'20132'),(46,'WEBUG12h','SD140G','Studieteknik G1N 1,5 hp','85621',4,NULL,'20122'),(47,'WEBUG12h','DA147G','Grundläggande programmering med C++ G1N 7,5 hp','87520',5,NULL,'20122'),(48,'WEBUG12h','IT116G','Informationssäkerhet - Introduktion G1N 7,5 hp','87510',5,NULL,'20142'),(49,'WEBUG12h','DA133G','Webbutveckling - datorgrafik G1N 7,5 hp','87518',4,NULL,'20122'),(50,'WEBUG12h','DA121G','Datorns grunder G1N 7,5 hp','87514',4,NULL,'20122'),(51,'WEBUG12h','DA330G','Webbprogrammering G1F 7,5 hp','87547',5,NULL,'20132'),(52,'WEBUG12h','DA523G','Webbteknologi - forskning och utveckling G2F 7,5 hp','87568',5,NULL,'20142'),(53,'WEBUG12h','DA524G','Webbutveckling - content management och drift G2F 7,5 hp','87569',4,NULL,'20142'),(54,'WEBUG12h','DA322G','Operativsystem G1F 7,5 hp','87531',4,NULL,'20142'),(55,'WEBUG12h','IS130G','IT i organisationer - Introduktion G1N 7,5 hp','88317',4,NULL,'20132'),(56,'WEBUG12h','IS317G','Databaskonstruktion G1F 7,5 hp','88344',4,NULL,'20132'),(57,'WEBUG12h','KB111G','Interaktion, design och användbarhet I G1N 7,5 hp','88417',5,NULL,'20122'),(58,'WEBUG12h','DA348G','Objektorienterad programmering G1F 7,5 hp','97543',1,NULL,'20131'),(59,'WEBUG12h','MA113G','Algebra och logik G1N 7,5 hp','93612',1,NULL,'20141'),(60,'WEBUG12h','DA338G','Projekt i webbutveckling G1F 15 hp','97545',2,NULL,'20141'),(61,'WEBUG12h','DA345G','Examensarbete i datalogi med inriktning mot webbutveckling G2E 30 hp','97560',1,NULL,'20151'),(62,'WEBUG12h','DV123G','Webbutveckling - webbplatsdesign G1N 7,5 hp','97703',1,NULL,'20131'),(63,'WEBUG12h','DV313G','Webbutveckling - XML API G1F 7,5 hp','97737',2,NULL,'20131'),(64,'WEBUG12h','DV318G','Programvaruutveckling - programvaruprojekt G1F 15 hp','97744',2,NULL,'20141'),(65,'WEBUG12h','DV316G','Programvaruutveckling G1F 7,5 hp','97745',1,NULL,'20141'),(66,'WEBUG12h','IS114G','Databassystem G1N 7,5 hp','98324',2,NULL,'20131'),(67,'WEBUG13h','DA147G','Grundläggande programmering med C++ G1N 7,5 hp','87501',5,NULL,'20132');
 INSERT INTO studentresultat VALUES (1,'111111-1111',NULL,'IT111G','H14',5.0,NULL),(2,'111111-1111',NULL,'IT115G','H14',7.5,NULL),(3,'111111-1111',NULL,'IT118G','H14',7.5,NULL),(4,'111111-1111',NULL,'IT120G','H14',0.0,NULL),(5,'111111-1111',NULL,'IT108G','V15',0.0,NULL),(6,'111111-1111',NULL,'IT121G','V15',0.0,NULL),(7,'111111-1111',NULL,'IT308G','V15',0.0,NULL);
 
-delimiter //
-
-create trigger calcStudentHp after update on partresult
-for each row begin
-	update user_course,user set user_course.result = (select sum(subpart.parthp) from partresult,user,course_user where partresult.cid=user.cid and user.cid=course_user.cid) where user.uid=user_course.uid and ;
-end;
-//
 */
 
 /**
@@ -491,14 +496,17 @@ create view studentresult as
 		inner join partresult on partresult.partname = subparts.partname
 		inner join user_course on user_course.cid = subparts.cid
 		inner join user on user.uid = partresult.uid and user.uid = user_course.uid and user_course.uid = partresult.uid
-			and partresult.grade != "u"
-			group by user_course.uid;
+			and partresult.grade != 'u'
+			group by user.uid;
+
+/* updatesd info in user table */
 
 update user set firstname="Toddler", lastname="Kong" where username="Toddler";
 update user set firstname="Johan", lastname="Grimling" where username="Grimling";
 update user set ssn="810101-5567" where username="Grimling";
 update user set ssn="444444-5447" where username="Toddler";
 update user set password=password("Kong") where username="Toddler";
+update user set password=password("Banan123") where username="Tester";
 update user set superuser=1 where username="Toddler";
 /*
 // Code for testing Code Viewer
