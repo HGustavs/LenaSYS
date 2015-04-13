fe787f4a8bd22abbd8df71f591a99998433c828b
c12jenek <c12jenek@student.his.se>
2015-04-13 09:47:34 +0200

    Update database.php
    Fixed login bug

--- Shared/database.php
+++ Shared/database.php
@@ -45,11 +45,10 @@ function pdoConnect()
 	global $pdo;
 	try {
 		$pdo = new PDO(
-		'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8',
-		 DB_USER,
-		DB_PASSWORD
-	);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+			'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8',
+			DB_USER,
+			DB_PASSWORD
+		);
 	} catch (PDOException $e) {
 		echo "Failed to get DB handle: " . $e->getMessage() . "</br>";
 		exit;
