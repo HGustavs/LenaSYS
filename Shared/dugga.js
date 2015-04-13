0bdf87b00cabd15389f3d4927cbe3600472dcbcc
a12jiyba <a12jiyba@users.noreply.github.com>
2015-04-13 14:46:51 +0200

    Update dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -135,7 +135,7 @@ function navigateTo(prefix,file)
 {
 		surl=window.location.href;
 		surl=surl.substring(0,surl.lastIndexOf("/")); 
-		window.location.href = surl+"/codeupload/"+file;
+		window.location.href = surl+prefix+file;
 }
 
 
