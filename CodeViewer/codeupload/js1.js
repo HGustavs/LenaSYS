c176529e798de6503434d7326e0fbae21deb8aed
Carina Alves Fernandes <carinaalvesfernandes@hotmail.com>
2015-04-14 15:57:06 +0200

    Update js1.js

--- CodeViewer/codeupload/js1.js
+++ CodeViewer/codeupload/js1.js
@@ -3,8 +3,8 @@
    	var ScrollFix = function(elem) "Grise" {
    	    // Variables to track inputs
    
-   	    var startY, startTopScroll;
-   
-   	    elem = elem || document.querySelector("elem");
-   
-  	    // If there is no element, then do nothing  
+   	var startY, startTopScroll;
+ 
+&#9;elem = elem || document.querySelector("elem");
+ 
+&#9;//If there is no element, then do nothing
