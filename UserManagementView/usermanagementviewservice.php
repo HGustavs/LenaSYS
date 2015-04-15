1d9e13edf3e007036f0e615001aa6f759d2fb94b
c13marme <c13marme@student.his.se>
2015-04-15 10:11:16 +0200

    Update usermanagementviewservice.php

--- UserManagementView/usermanagementviewservice.php
+++ UserManagementView/usermanagementviewservice.php
@@ -14,6 +14,7 @@
 	pdoConnect();
 	session_start();
 
+	
 	$pnr = getOP('pnr');
 	$studyprogram = getOP('username');
 	
@@ -27,121 +28,66 @@
 	
 
 	
-	$hr="";
 	if(isSuperUser($userid)){
-			$ha=true;
+			$superUser=true;
 	}else{
-			$ha=false;
+			$superUser=false;
 	}
 
-	$debug="NONE!";	
-
-	//-------------- ----------------------------------------------------------------------------------
-	// Services
-	//------------------------------------------------------------------------------------------------
-
-	$data = array();
-	//Just a random variable to test SQL query $sql1
-
-
-	if ($pnr != null) {
-		array_push($data, array($studyprogram));
-
-		//SQL query to print out information 
-		$sql = "SELECT user.firstname,user.lastname,user.ssn,user.username,course.coursecode,course.coursename FROM user INNER JOIN user_course INNER JOIN course WHERE user_course.uid='".$userid."' and user.uid='".$userid."' and course.cid = user_course.cid ;";
-		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		$sth = $pdo->prepare($sql);
-		$sth->execute();
-		$res = $sth->fetchAll();
-		array_push($data, $res);
-		
-		
-		
-		/*
-		$sql2 = "SELECT course.cid,course.coursename,programcourse.class FROM course INNER JOIN course INNER JOIN programcourse WHERE course.cid = '".$cid."' and programcourse.cid = '".$cid."';";
-		$sth2 = $pdo->prepare($sql2);
-		$sth2->execute();
-		$res2 = $sth2->fetchAll();
-		array_push($data, $res2);
-		*/
-
-		/*
-		$sql3 = "SELECT course.coursename FROM course INNER JOIN programcourse WHERE course.cid = '".$cid."' and programcourse.cid = '".$cid."';";
-		$sth3 = $pdo->prepare($sql3);
-		$sth3->execute();
-		$res3 = $sth3->fetchAll();
-		array_push($data, $res3);
-		*/
-		
-		/*
-		$sql5 = "SELECT course.coursename FROM course INNER JOIN programcourse WHERE course.cid = '".$cid."' and programcourse.cid = '".$cid."';";
-		$sth5 = $pdo->prepare($sql5);
-		$sth5->execute();
-		$res5 = $sth5->fetchAll();
-		array_push($data, $res5);
-		*/
-
-	} else if ($studyprogram != null) {
-		
-		array_push($data, array($studyprogram));
-			//$sql = "SELECT * FROM studentresultat WHERE pnr='".$pnr."';";
-			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			$sth = $pdo->prepare($sql);
-			$sth->execute();
-			$res = $sth->fetchAll();
-			array_push($data, $res);
-
-	}
-
-	$array = array(
-		"data" => $data,
-		"debug" => $debug,
-		'writeaccess' => $ha,
-		'readaccess' => $hr,
-	);
-
-	header('Content-type: application/json');
 	
-	echo $userid;
-	echo json_encode($data); 
+	$debug="NONE!";
+	
+	//------------------------------------------------------------------------------------------------
+	// Queries
+	//------------------------------------------------------------------------------------------------
+	
+	//queries teachers
+	
+	//queries student
+	$titleQuery = "SELECT CONCAT(firstname, ' ', lastname) AS fullname, class FROM user WHERE user.uid = '".$userid."';";
+	//$progressbarQuery = "";
+	//$yearQuery = "";
+	
+	//------------------------------------------------------------------------------------------------
+	// Retrieve information
+	//------------------------------------------------------------------------------------------------
+	
+	$retrievedData =  null;
+	
+	if($superUser) {
+		//retrieve teacher data
+	}
+	else {
+			//retrieve student data
+			
+			$titleData = array();
+			
+			$query = $pdo->prepare($titleQuery);
+			
+			if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error reading data from user ". $error[2]; 
+			} else {
+				
+				foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
+					array_push(
+						$titleData,
+						array(
+							'fullname' => $row['fullname'],
+							'class' => $row['class']
+							)
+					);
+				}
+			}
+			
+			$retrievedData = array(
+				'titleData' => $titleData,
+				'debug' => $debug
+			);
+			
+	}
+	
+	echo json_encode($retrievedData);
 
-	 //Datastrukturen fÃ¶r en student (JSON) skall ha fÃ¶ljande format:
-	 /*
-
-		namn: "Svensson Sven",
-		pnr : "791230-1153",
-		kull: "h12",
-		resultat : [
-			kurs { 
-					kursid : "DV123G",
-					resultat: "5.0"  			
-			},
-			kurs { etc }
-		]
-	 
-	*/
-
-	/* Datastrukturen fÃ¶r en kull (JSON) skall ha fÃ¶ljande format:
-	 * 
-	 
-		{ "kull":
-			{"kullid" : "h12"},
-			"ar" : [ { 
-				"period" : "LP4",
-				"period_start" : "2014-09-01",
-				"peroid_slut"  : "2014-11-01",
-				"kurser" : [
-					"kursid" : "DV123G",
-					"kursansvarig" : "Marcus Brohede",
-					"examinator" : "Anders Dahlbom",
-					"poang" : "7.5",
-					"kurshemsida" : "",
-					"sciosida" : "",
-					"forkunskapskrav" : [
-						kursid : ""]
-				] }
-			]
-		}
-	 
-	 */
+	
 ?>
