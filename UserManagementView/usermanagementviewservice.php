68f8ed1c6f838afe3b73327c8b895d79fa1f94f4
a13freas <a13freas@student.his.se>
2015-04-15 13:23:31 +0200

    Update usermanagementviewservice.php

--- UserManagementView/usermanagementviewservice.php
+++ UserManagementView/usermanagementviewservice.php
@@ -45,8 +45,9 @@
 	
 	//queries student
 	$titleQuery = "SELECT CONCAT(firstname, ' ', lastname) AS fullname, class FROM user WHERE user.uid = '".$userid."';";
-	//$progressbarQuery = "";
-	//$yearQuery = "";
+	$progressbarQuery = "SELECT SUM(user_course.result) AS finishedHP, class.hp as totalHP FROM user_course, course, class, user 
+						WHERE user_course.uid = '".$userid."' AND user_course.cid = course.cid AND user.class = class.class";
+	$coursesQuery = "SELECT course.coursename, user_course.result, course.hp, concat(user.firstname,' ',user.lastname) AS coordinator, course.courseHttpPage FROM user_course, course, user WHERE user_course.uid = '".$userid."' AND user_course.cid = course.cid AND user.uid=course.creator";
 	
 	//------------------------------------------------------------------------------------------------
 	// Retrieve information
@@ -60,6 +61,7 @@
 	else {
 			//retrieve student data
 			
+			// data for title
 			$titleData = array();
 			
 			$query = $pdo->prepare($titleQuery);
@@ -80,8 +82,55 @@
 				}
 			}
 			
+			// data for progressbar
+			$progress = array();
+			
+			$query = $pdo->prepare($progressbarQuery);
+			
+			if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error reading data from user ". $error[2]; 
+			} else {
+				foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
+					array_push(
+						$progress,
+						array(
+							'finishedHP' => $row['finishedHP'],
+							'totalHP' => $row['totalHP']
+							)
+					);
+				}
+			}
+			
+			// data for course
+			$courses = array();
+			
+			$query = $pdo->prepare($coursesQuery);
+			
+			if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error reading data from user ". $error[2]; 
+			} else {
+				foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
+					array_push(
+						$courses,
+						array(
+							'coursename' => $row['coursename'],
+							'result' => $row['result'],
+							'hp' => $row['hp'],
+							'coordinator' => $row['coordinator'],
+							'courseHttpPage' => $row['courseHttpPage']
+							)
+					);
+				}
+			}
+			
+			
+			
 			$retrievedData = array(
 				'titleData' => $titleData,
+				'progress' => $progress,
+				'courses' => $courses,
 				'debug' => $debug
 			);
 			
