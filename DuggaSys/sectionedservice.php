ef5ee946e6f3fcf27492831940a46597387f3ac8
Christopher Jubell <ChristopherJubell@hotmail.com>
2015-04-16 10:12:42 +0200

    Update sectionedservice.php

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -75,20 +75,6 @@ if(checklogin()){
 								$debug="Error updating entries".$error[2];
 							}
 					}
-			}else if(strcmp($opt,"ORDERUPDATE")===0){
-					$orderarr=explode(",",$order);
-					foreach ($orderarr as $key => $value){
-							$armin=explode("XX",$value);
-							$query = $pdo->prepare("UPDATE listentries set pos=:pos,moment=:moment WHERE lid=:lid;");
-							$query->bindParam(':lid', $armin[1]);
-							$query->bindParam(':pos', $armin[0]);
-							$query->bindParam(':moment', $armin[2]);
-							//$query->bindParam(':moment', $moment);
-							if(!$query->execute()) {
-								$error=$query->errorInfo();
-								$debug="Error updating entries".$error[2];
-							}
-					}
 			}else if(strcmp($opt,"UPDATE")===0){
 					$query = $pdo->prepare("UPDATE listentries set moment=:moment,entryname=:entryname,kind=:kind,link=:link,visible=:visible,gradesystem=:gradesys WHERE lid=:lid;");
 					$query->bindParam(':lid', $sectid);
@@ -106,7 +92,7 @@ if(checklogin()){
 						$error=$query->errorInfo();
 						$debug="Error updating entries".$error[2];
 					}
-			}
+			}	
 	}
 
 }
@@ -275,3 +261,4 @@ $array = array(
 
 echo json_encode($array);
 ?>
+
