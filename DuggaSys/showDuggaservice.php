92cdb3f420e9dc8893b05383b5e60da052cc4bdb
Christopher Jubell <ChristopherJubell@hotmail.com>
2015-04-28 10:43:48 +0200

    Update showDuggaservice.php

--- DuggaSys/showDuggaservice.php
+++ DuggaSys/showDuggaservice.php
@@ -69,12 +69,13 @@ if(checklogin()){
 					}
 					else{
 						// Update Dugga!
-						$query = $pdo->prepare("UPDATE userAnswer SET useranswer=:useranswer WHERE uid=:uid AND cid=:cid AND moment=:moment AND vers=:coursevers;");
+						$query = $pdo->prepare("UPDATE userAnswer SET useranswer=:useranswer, timeSpent=:timeSpent WHERE uid=:uid AND cid=:cid AND moment=:moment AND vers=:coursevers;");
 						$query->bindParam(':cid', $courseid);
 						$query->bindParam(':coursevers', $coursevers);
 						$query->bindParam(':uid', $userid);
 						$query->bindParam(':moment', $moment);
 						$query->bindParam(':useranswer', $answer);
+						$query->bindParam(':timeSpent', $timeSpent);
 					}
 					
 					if(!$query->execute()) {
