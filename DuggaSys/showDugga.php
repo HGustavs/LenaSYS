1ca069a96d744f9a145e3857a40e5cdb8ba9b339
a12majca <danielmajapalmqvist@Daniel--Maja-Palmqvists-MacBook-Pro.local>
2015-04-29 10:18:36 +0200

    added meta viewport tag

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Dugga Viewer</title>
