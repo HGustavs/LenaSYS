3ba282dee90d5e461cc1a7b531a220fa1e22159b
c13emmjo <c13emmjo@student.his.se>
2015-04-29 12:07:27 +0200

    did a blankspace before the hp

--- UserManagementView/usermanagementview.js
+++ UserManagementView/usermanagementview.js
@@ -165,7 +165,7 @@ function createHTMLForCourse(data)
 	
 	
 	courseHtmlStr += '<div class="course_name"><p>' + coursename + '</p></div>';
-	courseHtmlStr += '<div class="course_progressbar"> <div class="progress"><div class="progress-bar progress-bar-warning" id="" role="progressbar" style="width:' + parseFloat(procent) + '%"></div></div><p class="points">' + parseFloat(result) + ' / ' + hp + "hp" + '</p></div>';
+	courseHtmlStr += '<div class="course_progressbar"> <div class="progress"><div class="progress-bar progress-bar-warning" id="" role="progressbar" style="width:' + parseFloat(procent) + '%"></div></div><p class="points">' + parseFloat(result) + ' / ' + hp + " hp" + '</p></div>';
 	courseHtmlStr += '<div class="course_link"><a href="' + course_link + '">Kurslänk</a></div>';
 	courseHtmlStr += '<div class="course_reponsible">' + course_responsible + '<a href="mailto:""><img src="envelope.png" id="mail-icon" width="13" height="10" alt="mail"></a></div>';
 	courseHtmlStr += '<div class="course_alert"></div>';
