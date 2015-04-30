f7af52040e3e35d28067cbb8ab949160f1cfcc03
a13denli <a13denli@gmail.com>
2015-04-30 09:51:34 +0200

    Update courseed.php

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -14,7 +14,7 @@ pdoConnect();
 		
 	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
 	<link type="text/css" href="../Shared/css/responsive.css" rel="stylesheet">
-   <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
+  <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
 	
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
