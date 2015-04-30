921582643cd0cfe15649b35f3502af3caa21eb7b
b12erijo <b12erijo@student.his.se>
2015-04-30 11:01:47 +0200

    Removed redundant AJAX Calls The redundant calls should now be removed and as far as i see nothing has broken from it.

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -106,11 +106,6 @@ function saveDuggaResult(citstr)
 		showReceiptPopup();
 }
 
-function readDugga()
-{
-		AJAXService("GETPARAM",{},"PDUGGA");
-}
-
 //----------------------------------------------------------------------------------
 // changeURL: Patch-in for changeURL from project 2014 code
 //----------------------------------------------------------------------------------
@@ -219,13 +214,19 @@ function AJAXService(opt,apara,kind)
 						alert("Illegal characters removed in " + key);
 					}
 				}
+				// Informs the user that his input contained nothing.
+				if(apara[key] == "") {
+					alert("Your input contained nothing in " + key);
+				}				
 				para+="&"+key+"="+encodeURIComponent(htmlEntities(apara[key]));
 			}
 		}
 		// Informs the user that his input contained nothing.
 		if(apara[key] == "") {
 			alert("Your input contained nothing in " + key);
-		}					
+		}			
+		para+="&"+key+"="+encodeURIComponent(htmlEntities(apara[key]));
+		console.log("Para: " + para);
 	}
 				
 	if(kind=="COURSE"){
