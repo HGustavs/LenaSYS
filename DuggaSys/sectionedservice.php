b1496070cc1c4dc469ad982b8c4e805f8f84243d
a12majca <a12majca@student.his.se>
2015-05-04 10:01:04 +0200

    Update sectionedservice.php
    there was a problem with adding new items after the weekly merge. now it is reverted to an old version (that works)...

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -189,9 +189,7 @@ foreach($query->fetchAll() as $row) {
 
 
 $entries=array();
-// If user has read access!
-$reada = (checklogin() && hasAccess($userid, $courseid, 'r'));
-if($reada){
+if($hr){
 	$query = $pdo->prepare("SELECT lid,moment,entryname,pos,kind,link,visible,code_id,gradesystem FROM listentries WHERE listentries.cid=:cid and vers=:coursevers ORDER BY pos");
 	$query->bindParam(':cid', $courseid);
 	$query->bindParam(':coursevers', $coursevers);
