a284f90bb0d03c66e10ccae6725a1dd644f3a346
b12simsa <b12simsa@student.his.se>
2015-05-06 14:22:30 +0200

    Update editorService.php

--- CodeViewer/editorService.php
+++ CodeViewer/editorService.php
@@ -3,17 +3,20 @@
 	// editorService - Saves and Reads content for Code Editor
 	//---------------------------------------------------------------------------------------------------------------
 
-	// Check if example exists and then do operation
 	date_default_timezone_set("Europe/Stockholm");
-	// Include basic application services!
+	
+	// Include basic application services
 	include_once ("../../coursesyspw.php");	
 	include_once ("../Shared/sessions.php");
 	include_once ("../Shared/basic.php");
 	include_once ("../Shared/courses.php");
 	include_once ("../Shared/database.php");
+	
 	// Connect to database and start session
 	pdoConnect();
 	session_start();
+	
+	// Global variables
 	$exampleId=getOP('exampleid');
 	$boxId=getOP('boxid');
 	$opt=getOP('opt');
@@ -27,24 +30,30 @@
 	$playlink=getOP('playlink');
 	$debug="NONE!";	
 	
+	// Checks user id
 	if(isset($_SESSION['uid'])){
 		$userid=$_SESSION['uid'];
 	}else{
-		$userid="1";		
+		$userid="1";
 	} 
+	// Checks and sets user rights
 	if(checklogin() && (hasAccess($userid, $courseId, 'w'))){
-		$writeaccess="w";
+		$writeAccess="w";
 	}else{
-		$writeaccess="s";	
+		$writeAccess="s";	
 	}
+	
 	$appuser=(array_key_exists('uid', $_SESSION) ? $_SESSION['uid'] : 0);
+	
 	// Make sure there is an example
-	$cnt=0;
+	$exampleCount = 0;
+	
 	$query = $pdo->prepare( "SELECT exampleid,sectionname,examplename,runlink,cid,cversion,public FROM codeexample WHERE exampleid = :exampleid;");
     $query->bindParam(':exampleid', $exampleId);
-	$query -> execute();	
+	$query->execute();
+	
 	while ($row = $query->fetch(PDO::FETCH_ASSOC)){
-		$cnt++;
+		$exampleCount++;
 		$exampleId=$row['exampleid'];
 		$exampleName=$row['examplename'];
 		$courseID=$row['cid'];
@@ -53,13 +62,14 @@
 		$sectionName=$row['sectionname'];
 		$playlink=$row['runlink'];
 	}	
-	if($cnt>0){
+	
+	// TODO: Handle a situation where there are no examples available
+	if($exampleCount>0){
 		//------------------------------------------------------------------------------------------------
 		// Perform Update Action
 		//------------------------------------------------------------------------------------------------
-
 		if(checklogin() && (hasAccess($_SESSION['uid'], $courseId, 'w') || isSuperUser($_SESSION['uid']))) {
-			$writeaccess="w";
+			$writeAccess="w";
 			if(strcmp('SETTEMPL',$opt)===0){
 				// Add word to wordlist
 				$query = $pdo->prepare( "UPDATE codeexample SET templateid = :templateno WHERE exampleid = :exampleid and cid = :cid and cversion = :cvers;");		
@@ -67,10 +77,9 @@
 				$query->bindParam(':exampleid', $exampleId);
 				$query->bindParam(':cid', $courseId);
 				$query->bindParam(':cversion', $courseVersion);
-				$query -> execute();
+				$query->execute();
 				
-				// We have two boxes. Create two boxes to start with
-
+				// There are at least two boxes, create two boxes to start with
 				if($templateNumber==1||$templateNumber==2) $boxCount=2;
 				if($templateNumber==3||$templateNumber==4) $boxCount=3;
 				if($templateNumber==5||$templateNumber==6) $boxCount=4;
@@ -83,11 +92,11 @@
 					$query->bindValue(':boxtitle', 'Title');
 					$query->bindValue(':boxcontent', 'Code');
 					$query->bindValue(':settings', '[viktig=1]');
+					//Should only bind with the file used (if used) and not to one by default
 					$query->bindValue(':filename', 'js1.js');
-					$query -> execute();
+					$query->execute();
 				}
 			}else if(strcmp('EDITEXAMPLE',$opt)===0){
-
 				if(isset($_POST['playlink'])) {$playlink = $_POST['playlink'];}
 				if(isset($_POST['examplename'])) {$exampleName = $_POST['examplename'];}
 				if(isset($_POST['sectionname'])) {$sectionName = $_POST['sectionname'];}
@@ -102,28 +111,26 @@
 				$query->bindParam(':exampleid', $exampleId);
 				$query->bindParam(':cid', $courseId);
 				$query->bindParam(':cvers', $courseVersion);
-				$query -> execute();
+				$query->execute();
 				
-				// Is there a better way to set beforeid and afterid?
+				// TODO: Check for better way to get and set before/afterId
 				if($beforeId!="UNK"){
-						$query = $pdo->prepare( "UPDATE codeexample SET beforeid = :beforeid WHERE exampleid = :exampleid and cid = :cid and cversion = :cvers;");		
-						$query->bindParam(':beforeid', $beforeId);
-						$query->bindParam(':exampleid', $exampleId);
-						$query->bindParam(':cid', $courseId);
-						$query->bindParam(':cvers', $courseVersion);
-						$query -> execute();
+					$query = $pdo->prepare( "UPDATE codeexample SET beforeid = :beforeid WHERE exampleid = :exampleid and cid = :cid and cversion = :cvers;");		
+					$query->bindParam(':beforeid', $beforeId);
+					$query->bindParam(':exampleid', $exampleId);
+					$query->bindParam(':cid', $courseId);
+					$query->bindParam(':cvers', $courseVersion);
+					$query->execute();
 				}
 				if($afterId!="UNK"){
-						
-						$query = $pdo->prepare( "UPDATE codeexample SET afterid = :afterid WHERE exampleid = :exampleid and cid = :cid and cversion = :cvers;");		
-						$query->bindParam(':afterid', $afterId);
-						$query->bindParam(':exampleid', $exampleId);
-						$query->bindParam(':cid', $courseId);
-						$query->bindParam(':cvers', $courseVersion);
-						$query -> execute();
+					$query = $pdo->prepare( "UPDATE codeexample SET afterid = :afterid WHERE exampleid = :exampleid and cid = :cid and cversion = :cvers;");		
+					$query->bindParam(':afterid', $afterId);
+					$query->bindParam(':exampleid', $exampleId);
+					$query->bindParam(':cid', $courseId);
+					$query->bindParam(':cvers', $courseVersion);
+					$query->execute();
 				}
 				
-
 				if(isset($_POST['addedWords'])) {
 					// Converts to array
 					$addedWords = explode(",",$_POST['addedWords']);
@@ -151,22 +158,22 @@
 					}
 				}
 			}else if(strcmp('EDITCONTENT',$opt)===0) {
-				$exampleid = $_POST['exampleid'];
-				$boxid = $_POST['boxid'];
-				$boxtitle = $_POST['boxtitle'];
-				$boxcontent = $_POST['boxcontent'];
+				$exampleId = $_POST['exampleid'];
+				$boxId = $_POST['boxid'];
+				$boxTitle = $_POST['boxtitle'];
+				$boxContent = $_POST['boxcontent'];
 				$wordlist = $_POST['wordlist'];
 				$filename = $_POST['filename'];
 				$addedRows = $_POST['addedRows'];
 				$removedRows = $_POST['removedRows'];
 
 				$query = $pdo->prepare("UPDATE box SET boxtitle=:boxtitle, boxcontent=:boxcontent, filename=:filename, wordlistid=:wordlist WHERE boxid=:boxid AND exampleid=:exampleid;");	
-				$query->bindParam(':boxtitle', $boxtitle);
-				$query->bindParam(':boxcontent', $boxcontent);
+				$query->bindParam(':boxtitle', $boxTitle);
+				$query->bindParam(':boxcontent', $boxContent);
 				$query->bindParam(':wordlist', $wordlist);
 				$query->bindParam(':filename', $filename);
-				$query->bindParam(':boxid', $boxid);
-				$query->bindParam(':exampleid', $exampleid);
+				$query->bindParam(':boxid', $boxId);
+				$query->bindParam(':exampleid', $exampleId);
 				$query->execute();
 
 				if (isset($_POST['addedRows'])) {
@@ -174,8 +181,8 @@
 					foreach ($matches[1] as $match) { 
 						$row = explode(",", $match);
 						$query = $pdo->prepare("INSERT INTO improw(boxid,exampleid,istart,iend,uid) VALUES (:boxid,:exampleid,:istart,:iend,:uid);");
-						$query->bindValue(':boxid', $boxid);
-						$query->bindValue(':exampleid', $exampleid);
+						$query->bindValue(':boxid', $boxId);
+						$query->bindValue(':exampleid', $exampleId);
 						$query->bindValue(':istart', $row[1]);
 						$query->bindValue(':iend', $row[2]);
 						$query->bindValue(':uid', $_SESSION['uid']);
@@ -188,8 +195,8 @@
 					foreach ($matches[1] as $match) { 
 						$row = explode(",", $match);
 						$query = $pdo->prepare("DELETE FROM improw WHERE boxid=:boxid AND istart=:istart AND iend=:iend AND exampleid=:exampleid;");
-						$query->bindValue(':boxid', $boxid);
-						$query->bindValue(':exampleid', $exampleid);
+						$query->bindValue(':boxid', $boxId);
+						$query->bindValue(':exampleid', $exampleId);
 						$query->bindValue(':istart', $row[1]);
 						$query->bindValue(':iend', $row[2]);
 						$query->execute();
@@ -199,14 +206,13 @@
 		}
 		//------------------------------------------------------------------------------------------------
 		// Retrieve Information			
-		//------------------------------------------------------------------------------------------------	
-			
+		//------------------------------------------------------------------------------------------------		
 		// Read exampleid, examplename and runlink etc from codeexample and template
 		$exampleName="";
-		$templateid="";
-		$stylesheet="";
-		$numbox="";
-		$exampleno=0;
+		$templateId="";
+		$styleSheet="";
+		$numBox="";
+		$exampleNumber=0;
 		$playlink="";
 		$public="";
 		$entryname="";
@@ -218,18 +224,18 @@
 
 		while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
 			$exampleName=$row['examplename'];
-			$exampleno=$row['exampleid'];
+			$exampleNumber=$row['exampleid'];
 			$public=$row['public'];
 			$playlink=$row['runlink'];
 			$sectionName=$row['sectionname'];
-			$templateid=$row['templateid'];
-			$stylesheet=$row['stylesheet'];
-			$numbox=$row['numbox'];					
+			$templateId=$row['templateid'];
+			$styleSheet=$row['stylesheet'];
+			$numBox=$row['numbox'];					
 		}
 		
 		// Read ids and names from before/after list
-		$beforeafter = array();
-		$beforeafters = array();
+		$beforeAfter = array();
+		$beforeAfters = array();
 		
 		$query = $pdo->prepare( "select exampleid, sectionname, examplename, beforeid, afterid from codeexample order by exampleid");
 		$query->execute();
@@ -239,52 +245,55 @@
 		//$query->bindParam(':cid', $cid);
 		//$query->bindParam(':cvers', $courseVersion);
 		//$query->execute();
-					
+
 		while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
-			$beforeafter[$row['exampleid']]=array($row['exampleid'],$row['sectionname'],$row['examplename'],$row['beforeid'],$row['afterid']);
-				array_push($beforeafters,array($row['exampleid'],$row['sectionname'],$row['examplename'],$row['beforeid'],$row['afterid']));
+			$beforeAfter[$row['exampleid']]=array($row['exampleid'],$row['sectionname'],$row['examplename'],$row['beforeid'],$row['afterid']);
+				array_push($beforeAfters,array($row['exampleid'],$row['sectionname'],$row['examplename'],$row['beforeid'],$row['afterid']));
 		}  
 
-		// iteration to find after examples - We start with $exampleId and at most 5 are collected
-		$cnt=0;
-		$forward_examples = array();	
-		$currid=$exampleId;
+		// Iteration to find after examples - We start with $exampleId and at most 5 are collected
+		$nextExampleCount = 0;
+		$forwardExamples = array();	
+		$currentId=$exampleId;
+		
 		do{
-			if(isset($beforeafter[$currid])){
-				$currid=$beforeafter[$currid][4];
+			if(isset($beforeAfter[$currentId])){
+				$currentId=$beforeAfter[$currentId][4];
 			}else{
-				$currid=null;
+				$currentId=null;
 			}					
-			if($currid!=null){
-				array_push($forward_examples,$beforeafter[$currid]);
+			if($currentId!=null){
+				array_push($forwardExamples,$beforeAfter[$currentId]);
 			}
-			$cnt++;
-		}while($currid!=null&&$cnt<5);
+			$nextExampleCount++;
+			
+		// Iteration to find before examples - We start with $exampleId and at most 5 are collected
+		}while($currentId!=null&&$nextExampleCount<5);
 
-		// iteration to find before examples - We start with $exampleId and at most 5 are collected 
-		$backward_examples = array();	
-		$currid=$exampleId;
-		$cnt=0;
-		do{
-			if(isset($beforeafter[$currid])){
-				$currid=$beforeafter[$currid][3];
-			}else{
-				$currid=null;
-			}					
-			if($currid!=null){
-				array_push($backward_examples,$beforeafter[$currid]);
-			}
-			$cnt++;
-		}while($currid!=null&&$cnt<5);
+			 
+			$backwardExamples = array();	
+			$currentId=$exampleId;
+			$previousExamplesCount = 0;
+			do{
+				if(isset($beforeAfter[$currentId])){
+					$currentId=$beforeAfter[$currentId][3];
+				}else{
+					$currentId=null;
+				}					
+				if($currentId!=null){
+					array_push($backwardExamples,$beforeAfter[$currentId]);
+				}
+			$previousExamplesCount++;
+		}while($currentId!=null&&$previousExamplesCount<5);
 
 		// Read important lines
-		$imp=array();
+		$importantRows=array();
 		$query = $pdo->prepare("SELECT boxid, istart, iend FROM improw WHERE exampleid = :exampleid ORDER BY istart;");
 		$query->bindParam(':exampleid', $exampleId);
 		$query->execute();
-						
+		
 		while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
-			array_push($imp,array($row['boxid'],$row['istart'],$row['iend']));
+			array_push($importantRows,array($row['boxid'],$row['istart'],$row['iend']));
 		}  
 
 		// Get all words for each wordlist
@@ -297,31 +306,33 @@
 		}
 		
 		// Get all wordlists
-		$wordlists=array();
-		$query =$pdo->prepare( "SELECT wordlistid, wordlistname FROM wordlist ORDER BY wordlistid;");
+		$wordLists=array();
+		$query = $pdo->prepare( "SELECT wordlistid, wordlistname FROM wordlist ORDER BY wordlistid;");
 		$query->execute();
 
 		while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
-			array_push($wordlists,array($row['wordlistid'],$row['wordlistname']));					
+			array_push($wordLists,array($row['wordlistid'],$row['wordlistname']));					
 		} 
 		
-	  // Read important wordlist
-		$impwordlist=array();
+
+		// Read important wordlist
+		$importantWordList=array();
 		$query = $pdo->prepare( "SELECT word,label FROM impwordlist WHERE exampleid = :exampleid ORDER BY word;");
 		$query->bindParam(':exampleid', $exampleId);
 		$query->execute();
-				
+		
 		while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
-			array_push($impwordlist,$row['word']);					
+			array_push($importantWordList,$row['word']);					
 		}  
 		
 		$directories = array();
+		
 		// Read Directory - Codeexamples
 		$codeDir=array();
 		if(file_exists('./codeupload')){
 			$dir = opendir('./codeupload');
 			while (($file = readdir($dir)) !== false) {
-				if(endsWith($file,".js")){
+				if(endsWith($file,".js") || endsWith($file,".html") || endsWith($file,".php")){
 					array_push($codeDir,$file);		
 				}
 			}  
@@ -343,25 +354,25 @@
 		$images=array();
 		if(file_exists('./imgupload')){
 			// Read Directory - Images
-			$img_dir = opendir('./imgupload');
-			while (($img_file = readdir($img_dir)) !== false) {
-				if(endsWith($img_file,".png")){
-					array_push($images,$img_file);
+			$imgDir = opendir('./imgupload');
+			while (($imgFile = readdir($imgDir)) !== false) {
+				if(endsWith($imgFile,".png")){
+					array_push($images,$imgFile);
 				}
 			}		
 		}
 	
-	// Collect information for each box
-	$box=array();   // get the primary keys for all types kind of boxes.
-	$query = $pdo->prepare( "SELECT boxid, boxcontent, boxtitle, filename, wordlistid, segment FROM box WHERE exampleid = :exampleid ORDER BY boxid;");
-	$query->bindParam(':exampleid', $exampleId);
-	$query->execute();
+		// Collect information for each box
+		$box=array();   // Array to be filled with the primary keys to all boxes of the example
+		$query = $pdo->prepare( "SELECT boxid, boxcontent, boxtitle, filename, wordlistid, segment FROM box WHERE exampleid = :exampleid ORDER BY boxid;");
+		$query->bindParam(':exampleid', $exampleId);
+		$query->execute();
 
 	while ($row = $query->FETCH(PDO::FETCH_ASSOC)){
-		$boxcontent=strtoupper($row['boxcontent']);
+		$boxContent=strtoupper($row['boxcontent']);
 		$filename=$row['filename'];
 		$content="";					
-		if(strcmp("DOCUMENT",$boxcontent)===0){
+		if(strcmp("DOCUMENT",$boxContent)===0){
 			if(file_exists('./descupload')){
 				$filename="./descupload/".$filename;
 				$handle = @fopen($filename, "r");
@@ -370,57 +381,61 @@
 						$content=$content.$buffer;
 					}
 					if (!feof($handle)) {
-						$content.="Error: Unexpected end of file ".$descFilename."\n";			    
+						$content.="Error: Unexpected end of file ".$descFilename."\n";			    			    
 					}
 					fclose($handle);
 				}
 			}
-		}else{
-			if(file_exists('./codeupload')){
-				$filename="./codeupload/".$filename;
-				$handle = @fopen($filename, "r");
-				if ($handle) {
-					while (($buffer = fgets($handle, 1024)) !== false) {
-						$content=$content.$buffer;
-					}
-					if (!feof($handle)) {
-						$content.="Error: Unexpected end of file ".$filename."\n";			    
-					}
-					fclose($handle);
-				}
+			//If box is not of Document type, code is assumed
 			}else{
-				$content="No file found!";
+				if(file_exists('./codeupload')){
+					$filename="./codeupload/".$filename;
+					$handle = @fopen($filename, "r");
+					if ($handle) {
+						while (($buffer = fgets($handle, 1024)) !== false) {
+							$content=$content.$buffer;
+						}
+						if (!feof($handle)) {
+							$content.="Error: Unexpected end of file ".$filename."\n";
+						}
+						fclose($handle);
+					}
+				}else{
+					$content="No file found!";
+				}
+			
+
 			}
-		}
-		array_push($box,array($row['boxid'],$boxcontent,$content,$row['wordlistid'],$row['boxtitle'],$row['filename']));
+			array_push($box,array($row['boxid'],$boxContent,$content,$row['wordlistid'],$row['boxtitle'],$row['filename']));
 	}						
 	$array = array(
-		'before' => $backward_examples,
-		'after' => $forward_examples,
-		'templateid' => $templateid,
-		'stylesheet' => $stylesheet,
-		'numbox' => $numbox,
+		'before' => $backwardExamples,
+		'after' => $forwardExamples,
+		'templateid' => $templateId,
+		'stylesheet' => $styleSheet,
+		'numbox' => $numBox,
 		'box' => $box,
-		'improws' => $imp,
-		'impwords' => $impwordlist,
+		'improws' => $importantRows,
+		'impwords' => $importantWordList,
 		'directory' => $directories,
 		'examplename'=> $exampleName,
 		'sectionname'=> $sectionName,
 		'playlink' => $playlink,
-		'exampleno' => $exampleno,
+		'exampleno' => $exampleNumber,
 		'words' => $words,
-		'wordlists' => $wordlists, 
+		'wordlists' => $wordLists, 
 		'images' => $images,
-		'writeaccess' => $writeaccess,
+		'writeaccess' => $writeAccess,
 		'debug' => $debug,
-		'beforeafter' => $beforeafters, 
+		'beforeafter' => $beforeAfters, 
 		'public' => $public
 	);
 	echo json_encode($array);
 	}else{
 		$array = array(
-		 	'debug' => "ID does not exist" 
+		 	'debug' => "ID does not exist or there are no examples" 
 		);		
+		echo "There are no examples to fetch, error occur at line 58 in editorService.php";
 		echo json_encode($array);
 	}
 ?>
