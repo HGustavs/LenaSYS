1695eb5d0a3071e80e73a7be44f0cb58ec6ebaa6
c13andka <andreaskarlsson@live.com>
2015-05-11 09:55:30 +0200

    Update duggaed.php
    Updated indenting

--- DuggaSys/duggaed.php
+++ DuggaSys/duggaed.php
@@ -11,7 +11,7 @@ pdoConnect();
 	<title>Dugga editor</title>
 
 	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
-  <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
+  	<link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
 
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
@@ -44,10 +44,10 @@ pdoConnect();
 	<!--- Edit Dugga Dialog START --->
 	<div id='editDugga' class='loginBox' style='width:464px;display:none;'>
 
-	<div class='loginBoxheader'>
-	<h3>Edit Dugga</h3>
-	<div onclick='closeEditDugga();'>x</div>
-	</div>
+		<div class='loginBoxheader'>
+			<h3>Edit Dugga</h3>
+			<div onclick='closeEditDugga();'>x</div>
+		</div>
 		<div style='padding:5px;'>
 			<input type='hidden' id='did' value='Toddler' /></td>
 			<div class='inputwrapper'><span>Name:</span><input class='textinput' type='text' id='name' value='Name' /></div>
@@ -57,18 +57,18 @@ pdoConnect();
 			<div class='inputwrapper'><span>Release Date:</span><input class='textinput datepicker' type='text' id='release' value='None' /></div>
 			<div class='inputwrapper'><span>Deadline Date:</span><input class='textinput datepicker' type='text' id='deadline' value='None' /></div>
 		</div>
-	<div style='padding:5px;'>
-		<input class='submit-button' type='button' value='Save' onclick='updateDugga();' />
-	</div>
+		<div style='padding:5px;'>
+			<input class='submit-button' type='button' value='Save' onclick='updateDugga();' />
+		</div>
 	</div>
 	<!--- Edit Dugga Dialog END --->
 	
 	<!--- Edit Variant Dialog START --->
 	<div id='editVariant' class='loginBox' style='width:464px;display:none;'>
-	<div class='loginBoxheader'>
-	<h3>Edit Variant</h3>
-	<div onclick='closeWindows();'>x</div>
-	</div>
+		<div class='loginBoxheader'>
+			<h3>Edit Variant</h3>
+			<div onclick='closeWindows();'>x</div>
+		</div>
 		<div style='padding:5px;'>
 			<input type='hidden' id='vid' value='Toddler' />
 			<div class='inputwrapper'><span>Param:</span><input class='textinput' type='text' id='parameter' value='Variant Param' /></div>	
