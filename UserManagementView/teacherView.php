bb21bafb3cd4ef30ccbbdb0e103e3c2ac25f7199
a13antda <a13antda@student.his.se>
2015-05-12 09:57:42 +0200

    Update teacherView.php

--- UserManagementView/teacherView.php
+++ UserManagementView/teacherView.php
@@ -52,54 +52,104 @@
              	<div class="button my-pull-left " onclick="loadData(studyprogram.value,pnr.value);">Search</span></div>
         </div>
     
-        <!-- ProgramName -->
-        <div id="title" class="programName">
-        </div> 
-        
-        
-        <!-- Linegraph -->
-        <div id="graphContainer" class="lineGraph">
-            <canvas id="graph" width="900" height="150">
-            </canvas>
-        </div>
-        
-        
-        <!-- Large progressbar -->
-        <div class="largeProgressbarTV">
-        </div> 
-        
-        
-        <div class="totalInfo">
-			
-            	<!-- Filter (Radiobuttons) -->
-				<div id="radio_buttonToolbar">
-                    <form>
-                    	<input type="radio" id="allStudents" name="filterList" value="allStudents" checked>
-                        	<label for="allStudents"><span>All students</span></label>
-						<input type="radio" id="activeStudents" name="filterList" value="activeStudents">
-                        	<label for="activeStudents"><span>Active students</span></label>
-                    </form>
-				</div>
-                
-                <!-- View over the students how are going that program/course -->
-                <div class="studentInfoWrapper">
-                    <div id="studentslist">
-                    </div>
-				</div>
-		
-            
-            <!-- Change pages buttons-->
-            <div id="teacher_pages"> 
+        <div id="class_view">
+            <!-- ProgramName -->
+            <div id="title" class="programName">
             </div> 
-        
+            
+            
+            <!-- Linegraph -->
+            <div id="graphContainer" class="lineGraph">
+                <canvas id="graph" width="900" height="150">
+                </canvas>
+            </div>
+            
+            
+            <!-- Large progressbar -->
+            <div class="largeProgressbarTV">
+            </div> 
+            
+            
+            <div class="totalInfo">
+    			
+                	<!-- Filter (Radiobuttons) -->
+    				<div id="radio_buttonToolbar">
+                        <form>
+                        	<input type="radio" id="allStudents" name="filterList" value="allStudents" checked>
+                            	<label for="allStudents"><span>Alla studenter</span></label>
+    						<input type="radio" id="activeStudents" name="filterList" value="activeStudents">
+                            	<label for="activeStudents"><span>Aktiva studenter</span></label>
+                        </form>
+    				</div>
+                    
+                    <!-- View over the students how are going that program/course -->
+                    <div class="studentInfoWrapper">
+                        <div id="studentslist">
+                        </div>
+    				</div>
+    		
+                
+                <!-- Change pages buttons-->
+                <div id="teacher_pages"> 
+                </div> 
+            
+            </div>
         </div>
-       
+        <!-- Clickble view so the teacher can se specific student -->
+        <div id="studentView">
+            <div class="student_back_button">Back</div>
+
+            <div id="studentTitle">
+
+            </div>
+            <!-- Students progressbar, completed credits in percent out of total -->
+            <div class="mainProgressBarcontainer">
+                <div id="MainProgress">
+                
+                    <div id="completedMainProgress">
+                    
+                        <div id="ProgressbarG1N">
+                            <div id="completedProgressbarG1N"></div>
+                        </div>
+                    
+                        <div id="ProgressbarG1F">
+                            <div id="completedProgressbarG1F"></div>
+                        </div>
+                    
+                        <div id="ProgressbarG2F">
+                            <div id="completedProgressbarG2F"></div>
+                        </div>
+                    
+                    </div>
+                    
+                </div>
+            </div>
+            
+            <!-- View over the student courses/ school year  -->
+            <div id="YearContainer">
+            
+                
+                <div id="Year1" class="Year">
+                    
+                </div>
+                
+                <div id="Year3" class="Year">
+
+                </div>
+            
+                <div id="Year2" class="Year">
+
+                </div>
+        
+            
+            </div>
+        </div>   
 	</div>
 
 
     <script src="js/teacherView.js"></script>
+    <script src="js/studentView.js"></script>
     <script type="text/javascript" src="js/umvjquery.js"></script>
 
 </body>
-
 </html>
