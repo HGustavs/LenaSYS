28ed0b95a9138c219e8c6a38f113bf0d47770f6a
a13freas <a13freas@student.his.se>
2015-05-12 11:40:29 +0200

    hooverfunktion on progressbars

--- UserManagementView/js/teacherView.js
+++ UserManagementView/js/teacherView.js
@@ -173,7 +173,7 @@ function renderView(data)
 
 			htmlStr += "<div class='studentInfo'>";
 			htmlStr += getStudentInfo(student, renderStudent);
-			htmlStr += getCourseResults(student['results']);
+			htmlStr += getCourseResults(student['results'],j+i);
 
 			htmlStr += "</div>";
 		
@@ -192,10 +192,13 @@ function renderView(data)
 
 
 		$('#radio_buttonToolbar').show();
+		onClick_Students_To_page();
 	}
 	
 	var studentView = document.getElementById("studentslist");
 	studentView.innerHTML = htmlStr;
+	
+	progress_bar_hover(data);
 	
 	//The line graph needs to be redrawn once for the text to appear correctly
 	createLinearGraph(data);
@@ -220,14 +223,7 @@ function getStudentInfo(student, number)
 	htmlStr += "<div class='student_name'><p>" + student['fullname'] + "</p></div>";
 	htmlStr += "<div class='student_ssn'><p>" + student['ssn'] + "</p></div>";
 	htmlStr += "<div class='student_username'><p>" + student['username'] + "</p></div>";
-	
-	if(number % 2 == 0) {
-		htmlStr += "<div class='student_email'><a href='mailto:" + student['email'] + "'>";
-		htmlStr += "<img src='img/envelope_white.svg' id='mail-icon' width='13' height='10' alt='mail'></a></div>";
-	}else {
-		htmlStr += "<div class='student_email'><a href='mailto:" + student['email'] + "'>";
-		htmlStr += "<img src='img/envelope_purple.svg' id='mail-icon' width='13' height='10' alt='mail'></a></div>";
-	}
+	htmlStr += "<div class='student_email'><p>" + student['email'] + "</p></div>";
 		
 	htmlStr += "</div>";
 	
@@ -238,7 +234,8 @@ function getStudentInfo(student, number)
 //	getCourseResults(results) - creates the html representation
 //	of the course results for a student and returns it
 //---------------------------------------------------------------
-function getCourseResults(results)
+
+function getCourseResults(results, studentNumber)
 {
 	var colorGreen = "#50a750";
 	var colorYellow = "#F0AD4E";
@@ -251,10 +248,13 @@ function getCourseResults(results)
 		/* Check that result is not null and set to 0 if so */
 		var course_result = results[i]['result'] == null ? 0 : results[i]['result'];
 		var course_hp	  = results[i]['hp'];
+		var course_name	  = results[i]['coursename'];
 		var procent		  = course_result/course_hp * 100;
 		var color		  = procent < 100 ? colorYellow : colorGreen;
 		
-		htmlStr += "<div class='progress_course_total'>";
+		htmlStr += "<div id='course_"+course_name+"#"+studentNumber+"' style='position:relative; margin:-42px 0px 0px -50px; z-index:1001px;'>";		
+		htmlStr += "</div>";
+		htmlStr += "<div id='"+course_name+"#"+studentNumber+"' class='progress_course_total'>";
 		htmlStr += "<div class='progress_course' style='width:" + procent + "%; background-color: " + color + ";'>";
 		htmlStr += "<p>" + parseFloat(course_result) + "/" + course_hp + "</p>";
 		htmlStr += "</div>";
@@ -268,10 +268,29 @@ function getCourseResults(results)
 	
 }
 
-//---------------------------------------------------------------
-//	Sets the number of change pages buttons depending of how many 
-//  students in class
-//---------------------------------------------------------------
+function progress_bar_hover(data){
+	
+	$('.progress_course_total').on( 'mouseenter',function() {
+		
+			var course = 'course_'+$(this).attr('id');
+
+			var htmlString = "";
+			htmlString += "<p>"+$(this).attr('id').split('#')[0]+"</p>";
+			
+			var hover_name = document.getElementById(course);
+			hover_name.innerHTML = htmlString;
+
+		}).on('mouseleave', function(){
+			
+			var courseout = "course_"+$(this).attr('id');
+
+			var hover_name = document.getElementById(courseout);
+			hover_name.innerHTML = "";
+  
+    });
+}
+
+/* Sets the number of change pages buttons depending of how many students in class*/
 function render_next_pages(calcNumberOfStudents,numberOfStudentsPerPages){
 	var htmlInserts="";
 	var numberOfPage=1;
@@ -292,9 +311,7 @@ function render_next_pages(calcNumberOfStudents,numberOfStudentsPerPages){
 
 }
 
-//---------------------------------------------------------------
-//	navigates between pages
-//---------------------------------------------------------------
+//navigates between pages.
 function navigate_page(){
 	$('.pages').click(function(){
 		
@@ -313,6 +330,7 @@ function navigate_page(){
 //	clearLinearGraph() - clears the line graph representing
 //	all the student results in every course
 //---------------------------------------------------------------
+
 function clearLinearGraph() 
 {
 	var graph = $('#graph');
@@ -350,6 +368,7 @@ function get_student_data(studentid) {
 //	createLinearGraph() - creates the line graph
 //	representing all the student results in every course
 //---------------------------------------------------------------
+
 function createLinearGraph(data)
 {
 	var backgroundcolor_overlay = "#F5F0F5";
