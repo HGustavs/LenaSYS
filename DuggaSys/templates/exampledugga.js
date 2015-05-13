cf80a985048ef77d1c02d4367e505af8e9ec9403
a13denli <a13denli@gmail.com>
2015-05-13 10:32:06 +0200

    Update exampledugga.js

--- DuggaSys/templates/exampledugga.js
+++ DuggaSys/templates/exampledugga.js
@@ -2,7 +2,7 @@ var exampleDugga={
 	exampleParaDugga1 : "{*tal*:*2*}",
 	exampleAnswerDugga1 : "{*danswer*:*00000010 0 2*}",
 	
-	exampleParaDugga2 : "{*color*:*red*,*colorname*:*RÃ¶d*}",
+	exampleParaDugga2 : "{*color*:*red*,*colorname*:*Röd*}",
 	exampleAnswerDugga2 : "Variant",
 	
 	exampleParaDugga3 : "{*linje*:*10,30,19 20 40 20 50 30 50,81 65 50*}",
