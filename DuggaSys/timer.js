9006d4f9843f7eedbee8116cc85028849e57f13a
a13karsi <a13karsi@student.his.se>
2015-05-18 10:16:08 +0200

    Added comments
    The previous version of the file didn't have any comments, added some that may help with understanding the code at first glance.

--- DuggaSys/timer.js
+++ DuggaSys/timer.js
@@ -1,23 +1,44 @@
+//---------------------------------------------------------------------------------------------------------------
+// Timer - Used in dugga's to count the amount of time spent on a dugga
+//---------------------------------------------------------------------------------------------------------------
+
 var Timer = {	
+	// Declare the timer variable, will be accessible from this object in a dugga
 	timer: undefined,
+	
+	// Counts the amount of time spent on a dugga
 	timeSpent: 0,
+	
+	// Called at the start of a dugga to initialize the object
 	startTimer: function(){
 		var self = this;
+		
+		// Sets the update interval of the timer, calls animate timer on increment
 		this.timer = setInterval( function(){self.incrementTimer(); self.animateTimer();}, 1000 );
+		
+		// Call animate timer to initialize ui at 00:00:00
 		this.animateTimer();
 	},
+	
+	// Stops the timer from counting, called at the end of a dugga
 	stopTimer: function(){
 		var self = this;
 		clearInterval(self.timer);
 	},
+	
+	// Increments the time counter by one
 	incrementTimer: function(){
 		this.timeSpent++;
 	},
+	
+	// Updates the user interface
 	animateTimer: function(){
+		// Calculate hours, minutes and seconds based on timespent
 		var hours = Math.floor(this.timeSpent / 3600);
 		var minutes = Math.floor(this.timeSpent / 60);
 		var seconds = this.timeSpent % 60;
 
+		// Create a nice looking clock thing with the information we have
 		var str = "<p>";
 		str += hours + ":";
 		
@@ -31,6 +52,7 @@ var Timer = {
 		}
 		str += seconds;
 
+		// Push new value to ui thing
 		document.getElementById('duggaTimer').innerHTML = str;
 	}
 }	
