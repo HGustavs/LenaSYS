20c91a49dd36130dec3f399b416715024c3cc85b
a13karsi <a13karsi@student.his.se>
2015-05-18 09:59:25 +0200

    Added comments
    The original file was uncommented, now it has comments!

--- DuggaSys/clickcounter.js
+++ DuggaSys/clickcounter.js
@@ -1,14 +1,28 @@
+//---------------------------------------------------------------------------------------------------------------
+// Click counter - Used by highscore system implementations in dugga's to count the number of button clicks
+//---------------------------------------------------------------------------------------------------------------
+
 var ClickCounter = {
+	// Used to count clicks
 	noClicks: 0,
 	
+	// Initializes the noClicks variable, called at the start of a dugga
 	initialize: function() {
 		this.noClicks = 0;	
 	},
+	
+	// Called whenever a dugga should count a mouse click, e.g., when a user presses a button
 	onClick: function() {
+		// Increments the click counter by one
 		this.noClicks++;
+		
+		// Calls animate clicks to directly update the click counter user interface 
 		this.animateClicks();
 	},
+	
+	// Updates the click counter user interface in a dugga, uses the same 
 	animateClicks: function() {
+		// Apply some web magic to change the ui counter
 		var str = "<p>";
 		str += this.noClicks;
 		document.getElementById('duggaTimer').innerHTML = str;
