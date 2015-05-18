939a256bd6ccebd37298821149641884ffcd144a
b12erijo <b12erijo@student.his.se>
2015-05-18 09:40:34 +0200

    Fixed indentation

--- DuggaSys/courseedservice.php
+++ DuggaSys/courseedservice.php
@@ -32,16 +32,16 @@ $coursenamealt=getOP('coursenamealt');
 $coursecode=getOP('coursecode');
 
 if(isset($_SESSION['uid'])){
-		$userid=$_SESSION['uid'];
+	$userid=$_SESSION['uid'];
 }else{
-		$userid="UNK";		
+	$userid="UNK";		
 } 
 
 $hr="";
 if(isSuperUser($userid)){
-		$ha=true;
+	$ha=true;
 }else{
-		$ha=false;
+	$ha=false;
 }
 
 $debug="NONE!";	
@@ -52,43 +52,43 @@ $debug="NONE!";
 
 if($ha){
 
-		// The code for modification using sessions
-		if(strcmp($opt,"DEL")===0){
-		}else if(strcmp($opt,"NEW")===0){
-				$query = $pdo->prepare("INSERT INTO course (coursecode,coursename,visibility,creator) VALUES(:coursecode,:coursename,0,:usrid)");
-				$query->bindParam(':usrid', $userid);
-				$query->bindParam(':coursecode', $coursecode);
-				$query->bindParam(':coursename', $coursename);				
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating entries".$error[2];
-				}
-		}else if(strcmp($opt,"NEWVRS")===0){
-				$query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt);");
-
-				$query->bindParam(':cid', $cid);
-				$query->bindParam(':coursecode', $coursecode);
-				$query->bindParam(':vers', $versid);
-				$query->bindParam(':versname', $versname);				
-				$query->bindParam(':coursename', $coursename);
-				$query->bindParam(':coursenamealt', $coursenamealt);
-
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating entries".$error[2];
-				}
-		}else if(strcmp($opt,"UPDATE")===0){
-				$query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode WHERE cid=:cid;");
-				$query->bindParam(':cid', $cid);
-				$query->bindParam(':coursename', $coursename);
-				$query->bindParam(':visibility', $visibility);
-				$query->bindParam(':coursecode', $coursecode);
-		
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating entries".$error[2];
-				}
+	// The code for modification using sessions
+	if(strcmp($opt,"DEL")===0){
+	}else if(strcmp($opt,"NEW")===0){
+		$query = $pdo->prepare("INSERT INTO course (coursecode,coursename,visibility,creator) VALUES(:coursecode,:coursename,0,:usrid)");
+		$query->bindParam(':usrid', $userid);
+		$query->bindParam(':coursecode', $coursecode);
+		$query->bindParam(':coursename', $coursename);				
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating entries".$error[2];
 		}
+	}else if(strcmp($opt,"NEWVRS")===0){
+		$query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt);");
+
+		$query->bindParam(':cid', $cid);
+		$query->bindParam(':coursecode', $coursecode);
+		$query->bindParam(':vers', $versid);
+		$query->bindParam(':versname', $versname);				
+		$query->bindParam(':coursename', $coursename);
+		$query->bindParam(':coursenamealt', $coursenamealt);
+
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating entries".$error[2];
+		}
+	}else if(strcmp($opt,"UPDATE")===0){
+		$query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode WHERE cid=:cid;");
+		$query->bindParam(':cid', $cid);
+		$query->bindParam(':coursename', $coursename);
+		$query->bindParam(':visibility', $visibility);
+		$query->bindParam(':coursecode', $coursecode);
+		
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating entries".$error[2];
+		}
+	}
 }
 
 //------------------------------------------------------------------------------------------------
@@ -97,48 +97,49 @@ if($ha){
 
 $entries=array();
 if($ha){
-		$query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course WHERE visibility<3 ORDER BY coursename");
+	$query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course WHERE visibility<3 ORDER BY coursename");
 }else{
-		$query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course WHERE visibility>0 and visibility<3 ORDER BY coursename");
+	$query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course WHERE visibility>0 and visibility<3 ORDER BY coursename");
 }
 if(!$query->execute()) {
 	$error=$query->errorInfo();
 	$debug="Error reading courses".$error[2];
 }else{
 	foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
-			array_push(
-				$entries,
-				array(
-					'cid' => $row['cid'],
-					'coursename' => $row['coursename'],
-					'coursecode' => $row['coursecode'],
-					'visibility' => $row['visibility'],
-					'activeversion' => $row['activeversion'],
-					'activeedversion' => $row['activeedversion']
+		array_push(
+			$entries,
+			array(
+				'cid' => $row['cid'],
+				'coursename' => $row['coursename'],
+				'coursecode' => $row['coursecode'],
+				'visibility' => $row['visibility'],
+				'activeversion' => $row['activeversion'],
+				'activeedversion' => $row['activeedversion']
 				)
 			);
-		}
+	}
 } 
 
 $versions=array();
 $query=$pdo->prepare("SELECT cid,coursecode,vers,versname,coursename,coursenamealt FROM vers;");
+
 if(!$query->execute()) {
 	$error=$query->errorInfo();
 	$debug="Error reading courses".$error[2];
 }else{
-		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
-			array_push(
-				$versions,
-				array(
-					'cid' => $row['cid'],
-					'coursecode' => $row['coursecode'],
-					'vers' => $row['vers'],
-					'versname' => $row['versname'],
-					'coursename' => $row['coursename'],
-					'coursenamealt' => $row['coursenamealt']
-				)
-			);
-		}
+	foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
+		array_push(
+			$versions,
+			array(
+				'cid' => $row['cid'],
+				'coursecode' => $row['coursecode'],
+				'vers' => $row['vers'],
+				'versname' => $row['versname'],
+				'coursename' => $row['coursename'],
+				'coursenamealt' => $row['coursenamealt']
+			)
+		);
+	}
 }
 
 $array = array(
@@ -147,7 +148,7 @@ $array = array(
 	"debug" => $debug,
 	'writeaccess' => $ha,
 	'readaccess' => $hr,
-);
+	);
 
 echo json_encode($array);
 
