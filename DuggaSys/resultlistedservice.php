b18a9db484c26be307382c13a7ed043e69052a7a
b13johfr <johanofredriksson@gmail.com>
2015-05-18 10:46:33 +0200

    Update resultlistedservice.php
    Fixed the indentation.
    #1110

--- DuggaSys/resultlistedservice.php
+++ DuggaSys/resultlistedservice.php
@@ -11,15 +11,13 @@ pdoConnect();
 session_start();
 
 if(isset($_SESSION['uid'])){
-		$userid=$_SESSION['uid'];
+	$userid=$_SESSION['uid'];
 }else{
-		$userid="1";		
+	$userid="1";		
 } 
 
 $opt = getOP('opt');
-
 $cid = getOP('cid');
-
 $luid = getOP('luid');
 $vers = getOP('vers');
 $moment = getOP('moment');
@@ -39,70 +37,66 @@ $duggaanswer="";
 
 if(checklogin() && (hasAccess($_SESSION['uid'], $cid, 'w') || isSuperUser($_SESSION['uid']))) {
 
-		if(strcmp($opt,"CHGR")===0){
-				if($ukind=="U"){
-						$query = $pdo->prepare("UPDATE userAnswer SET grade=:mark,creator=:cuser WHERE cid=:cid AND moment=:moment AND vers=:vers AND uid=:uid");
-						$query->bindParam(':mark', $mark);
-						$query->bindParam(':cuser', $userid);
+	if(strcmp($opt,"CHGR")===0){
+		if($ukind=="U"){
+			$query = $pdo->prepare("UPDATE userAnswer SET grade=:mark,creator=:cuser WHERE cid=:cid AND moment=:moment AND vers=:vers AND uid=:uid");
+			$query->bindParam(':mark', $mark);
+			$query->bindParam(':cuser', $userid);
+			$query->bindParam(':cid', $cid);
+			$query->bindParam(':moment', $moment);
+			$query->bindParam(':vers', $vers);
+			$query->bindParam(':uid', $uid);
 
-						$query->bindParam(':cid', $cid);
-						$query->bindParam(':moment', $moment);
-						$query->bindParam(':vers', $vers);
-						$query->bindParam(':uid', $uid);
+			if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error updating entries".$error[2];
+			}				
+		}else if($ukind=="I"){
+			$query = $pdo->prepare("INSERT INTO userAnswer(grade,creator,cid,moment,vers,uid) VALUES(:mark,:cuser,:cid,:moment,:vers,:uid);");
+			$query->bindParam(':mark', $mark);
+			$query->bindParam(':cuser', $userid);
+			$query->bindParam(':cid', $cid);
+			$query->bindParam(':moment', $moment);
+			$query->bindParam(':vers', $vers);
+			$query->bindParam(':uid', $uid);
 
-						if(!$query->execute()) {
-							$error=$query->errorInfo();
-							$debug="Error updating entries".$error[2];
-						}				
-				}else if($ukind=="I"){
-						$query = $pdo->prepare("INSERT INTO userAnswer(grade,creator,cid,moment,vers,uid) VALUES(:mark,:cuser,:cid,:moment,:vers,:uid);");
-						$query->bindParam(':mark', $mark);
-						$query->bindParam(':cuser', $userid);
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating entries".$error[2];
+		}								
+	}
+}
 
-						$query->bindParam(':cid', $cid);
-						$query->bindParam(':moment', $moment);
-						$query->bindParam(':vers', $vers);
-						$query->bindParam(':uid', $uid);
+if(strcmp($opt,"DUGGA")===0){
 
-						if(!$query->execute()) {
-							$error=$query->errorInfo();
-							$debug="Error updating entries".$error[2];
-						}								
-				}
-		}
+	$query = $pdo->prepare("SELECT userAnswer.answer as aws,entryname,quizFile,qrelease,deadline,param FROM userAnswer,listentries,quiz,variant WHERE variant.vid=userAnswer.variant AND userAnswer.cid=listentries.cid AND listentries.cid=quiz.cid AND userAnswer.vers=listentries.vers AND listentries.link=quiz.id AND listentries.lid=userAnswer.moment AND uid=:luid AND userAnswer.moment=:moment AND listentries.cid=:cid AND listentries.vers=:vers;");					
+	$query->bindParam(':cid', $cid);
+	$query->bindParam(':vers', $vers);
+	$query->bindParam(':moment', $moment);
+	$query->bindParam(':luid', $luid);
 
-		if(strcmp($opt,"DUGGA")===0){
-
-					$query = $pdo->prepare("SELECT userAnswer.answer as aws,entryname,quizFile,qrelease,deadline,param FROM userAnswer,listentries,quiz,variant WHERE variant.vid=userAnswer.variant AND userAnswer.cid=listentries.cid AND listentries.cid=quiz.cid AND userAnswer.vers=listentries.vers AND listentries.link=quiz.id AND listentries.lid=userAnswer.moment AND uid=:luid AND userAnswer.moment=:moment AND listentries.cid=:cid AND listentries.vers=:vers;");					
-
-					$query->bindParam(':cid', $cid);
-					$query->bindParam(':vers', $vers);
-					$query->bindParam(':moment', $moment);
-					$query->bindParam(':luid', $luid);
-
-					if(!$query->execute()) {
-						$error=$query->errorInfo();
-						$debug="Error reading entries".$error[2];
-					}
+	if(!$query->execute()) {
+	
+		$error=$query->errorInfo();
+		$debug="Error reading entries".$error[2];
+	}
 		
-					if ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-							$duggatitle=$row['entryname'];
-							$duggafile=$row['quizFile'];
-							$duggarel=$row['qrelease'];
-							$duggadead=$row['deadline'];
+	if ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 							
-							$duggaanswer=$row['aws'];
-							$duggaparam=$row['param'];
-
-							$dugganame="templates/".$duggafile.".js";
+		$duggatitle=$row['entryname'];
+		$duggafile=$row['quizFile'];
+		$duggarel=$row['qrelease'];
+		$duggadead=$row['deadline'];
+		$duggaanswer=$row['aws'];
+		$duggaparam=$row['param'];
+		$dugganame="templates/".$duggafile.".js";
 							
-					  	if(file_exists ( "templates/".$duggafile.".html")){
-							  	$duggapage=file_get_contents("templates/".$duggafile.".html");
-							}
-
-					}
-
+		if(file_exists ( "templates/".$duggafile.".html")){
+		
+			$duggapage=file_get_contents("templates/".$duggafile.".html");
+			}
 		}
+	}
 }
 
 //------------------------------------------------------------------------------------------------
@@ -116,23 +110,26 @@ $lentries=array();
 
 if(checklogin() && (hasAccess($_SESSION['uid'], $cid, 'w') || isSuperUser($_SESSION['uid']))) {
 
-		// Users connected to the current course (irrespective of version)
-		$query = $pdo->prepare("select user_course.cid as cid,user.uid as uid,username,firstname,lastname,ssn from user,user_course where user.uid=user_course.uid and user_course.cid=:cid;");
-		$query->bindParam(':cid', $cid);
-		if(!$query->execute()) {
-			$error=$query->errorInfo();
-			$debug="Error updating entries".$error[2];
-		}
-		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
-			// Create array entry for each course participant
+	// Users connected to the current course (irrespective of version)
+	$query = $pdo->prepare("select user_course.cid as cid,user.uid as uid,username,firstname,lastname,ssn from user,user_course where user.uid=user_course.uid and user_course.cid=:cid;");
+	$query->bindParam(':cid', $cid);
 	
-			$entry = array(
-				'cid' => $row['cid'],
-				'uid' => $row['uid'],
-				'username' => $row['username'],
-				'firstname' => $row['firstname'],
-				'lastname' => $row['lastname'],
-				'ssn' => $row['ssn']
+	if(!$query->execute()) {
+			
+		$error=$query->errorInfo();
+		$debug="Error updating entries".$error[2];
+	}
+		
+	foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
+	// Create array entry for each course participant
+	
+		$entry = array(
+			'cid' => $row['cid'],
+			'uid' => $row['uid'],
+			'username' => $row['username'],
+			'firstname' => $row['firstname'],
+			'lastname' => $row['lastname'],
+			'ssn' => $row['ssn']
 			);
 			array_push($entries, $entry);
 		}
@@ -141,14 +138,19 @@ if(checklogin() && (hasAccess($_SESSION['uid'], $cid, 'w') || isSuperUser($_SESS
 		// All results from current course and vers?
 		$query = $pdo->prepare("select aid,quiz,variant,moment,grade,uid,answer,submitted,vers from userAnswer where cid=:cid;");
 		$query->bindParam(':cid', $cid);
+		
 		if(!$query->execute()) {
+			
 			$error=$query->errorInfo();
 			$debug="Error updating entries".$error[2];
 		}
+		
 		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
+			
 			if(!isset($lentries[$row['uid']])){
-					$lentries[$row['uid']]=array();
+				$lentries[$row['uid']]=array();
 			}
+			
 			array_push(
 				$lentries[$row['uid']],
 				array(
@@ -169,10 +171,12 @@ if(checklogin() && (hasAccess($_SESSION['uid'], $cid, 'w') || isSuperUser($_SESS
 		$query = $pdo->prepare("SELECT lid,moment,entryname,pos,kind,link,visible,code_id,vers,gradesystem FROM listentries WHERE listentries.cid=:cid and (listentries.kind=3 or listentries.kind=4) ORDER BY pos");
 		$query->bindParam(':cid', $cid);
 		$result=$query->execute();
+		
 		if(!$query->execute()) {
 			$error=$query->errorInfo();
 			$debug="Error updating entries".$error[2];
 		}
+		
 		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
 			array_push(
 				$gentries,
@@ -194,10 +198,12 @@ if(checklogin() && (hasAccess($_SESSION['uid'], $cid, 'w') || isSuperUser($_SESS
 		// All extant versions of course
 		$query = $pdo->prepare("SELECT cid,coursecode,vers FROM vers");
 		$result=$query->execute();
+		
 		if(!$query->execute()) {
 			$error=$query->errorInfo();
 			$debug="Error updating entries".$error[2];
 		}
+		
 		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
 			array_push(
 				$sentries,
@@ -222,7 +228,6 @@ $array = array(
 	'duggaparam' => $duggaparam,
 	'duggaanswer' => $duggaanswer
 );
-
 
 echo json_encode($array);
 
