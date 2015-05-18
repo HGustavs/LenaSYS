8df3b76f515dc36d6543b43720500eeb6fdd0d2e
a13denli <a13denli@gmail.com>
2015-05-18 10:30:18 +0200

    Documentation #1101

--- DuggaSys/fileed.php
+++ DuggaSys/fileed.php
@@ -4,21 +4,20 @@ include_once "../../coursesyspw.php";
 include_once "../Shared/sessions.php";
 pdoConnect();
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Dugga editor</title>
 	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
-	 <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
+	<link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">  
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
 	<script src="../Shared/dugga.js"></script>
 	<script src="fileed.js"></script>
-
 </head>
 <body>
-	
 	<?php 
 		$noup="SECTION";
 		$loginvar="FILE";
@@ -26,42 +25,43 @@ pdoConnect();
 		setcookie("loginvar", $loginvar);
 	?>
 		
-<!-- content START -->
+	<!-- content START -->
 	<div id="content" >
 		<div style='height:32px;width:100%;float:right'>
-				<input class='submit-button' type='button' value='Add Link' onclick='createLink();'/>
-				</div>
-				<div id="alllinks" style='width:100%;float:right'>
-					<table class='list' style='margin-bottom:8px;' >
-						<tr><th class='first' style='width:64px;'>ID</th><th>Link URL</th><th style='width:30px' class='last'></th></tr>
-					</table>
-				</div>
-				<div style='height:32px;width:100%;float:right'>
-				<input class='submit-button' type='button' value='Add File' onclick='createFile("GFILE");'/>
-				</div>
-				<div id="allglobalfiles" style='width:100%;float:right'>
-					<table class='list' style='margin-bottom:8px;' >
-						<tr><th class='first' style='width:64px;'>ID</th><th>Global File</th><th style='width:30px' class='last'></th></tr>
-					</table>
-				</div>
-				<div style='height:32px;width:100%;float:right'>
-				<input class='submit-button' type='button' value='Add File' onclick='createFile("MFILE");'/>
-				</div>
-				<div id="allcoursefiles" style='width:100%;float:right'>
-					<table class='list'>
-						<tr><th class='first' style='width:64px;'>ID</th><th>Course Local File</th><th style='width:30px' class='last'></th></tr>
-					</table>
-				</div>
-				<div style='height:32px;width:100%;float:right;display:block'>
-				<input class='submit-button' type='button' value='Add File' onclick='createFile("LFILE");'/>
-				</div>
-				<div id="alllocalfiles" style='width:100%;float:right'>
-					<table class='list'>
-					<tr><th class='first' style='width:64px;'>ID</th><th>Local File</th><th style='width:30px' class='last'></th></tr>
-					</table>
-				</div>
-			<br style="clear:both" />
+			<input class='submit-button' type='button' value='Add Link' onclick='createLink();'/>
+		</div>
+		<div id="alllinks" style='width:100%;float:right'>
+			<table class='list' style='margin-bottom:8px;' >
+				<tr><th class='first' style='width:64px;'>ID</th><th>Link URL</th><th style='width:30px' class='last'></th></tr>
+			</table>
+		</div>
+		<div style='height:32px;width:100%;float:right'>
+			<input class='submit-button' type='button' value='Add File' onclick='createFile("GFILE");'/>
+		</div>
+		<div id="allglobalfiles" style='width:100%;float:right'>
+			<table class='list' style='margin-bottom:8px;' >
+				<tr><th class='first' style='width:64px;'>ID</th><th>Global File</th><th style='width:30px' class='last'></th></tr>
+			</table>
+		</div>
+		<div style='height:32px;width:100%;float:right'>
+			<input class='submit-button' type='button' value='Add File' onclick='createFile("MFILE");'/>
+		</div>
+		<div id="allcoursefiles" style='width:100%;float:right'>
+			<table class='list'>
+				<tr><th class='first' style='width:64px;'>ID</th><th>Course Local File</th><th style='width:30px' class='last'></th></tr>
+			</table>
+		</div>
+		<div style='height:32px;width:100%;float:right;display:block'>
+			<input class='submit-button' type='button' value='Add File' onclick='createFile("LFILE");'/>
+		</div>
+		<div id="alllocalfiles" style='width:100%;float:right'>
+			<table class='list'>
+			<tr><th class='first' style='width:64px;'>ID</th><th>Local File</th><th style='width:30px' class='last'></th></tr>
+			</table>
+		</div>
+		<br style="clear:both" />
 	</div>
+	<!-- content END -->
 	
 	<?php 
 		include '../Shared/loginbox.php';
