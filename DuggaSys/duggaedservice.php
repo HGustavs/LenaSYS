a78636260ff7e1b100199b66168512986cc12cd6
b12erijo <b12erijo@student.his.se>
2015-05-18 10:22:40 +0200

    Update duggaedservice.php
    Fixed indentation and removed some whitespace.

--- DuggaSys/duggaedservice.php
+++ DuggaSys/duggaedservice.php
@@ -11,9 +11,9 @@ pdoConnect();
 session_start();
 
 if(isset($_SESSION['uid'])){
-		$userid=$_SESSION['uid'];
+	$userid=$_SESSION['uid'];
 }else{
-		$userid="1";		
+	$userid="1";		
 } 
 
 $cid = getOP('cid');
@@ -33,72 +33,72 @@ $template = getOP('template');
 $release = getOP('release');
 $deadline = getOP('deadline');
 
-$debug="NONE!";	
+$debug="NONE!";
 
 //------------------------------------------------------------------------------------------------
 // Services
 //------------------------------------------------------------------------------------------------
 
-if(checklogin() && (hasAccess($userid, $cid, 'w') || isSuperUser($userid))) {
+if(checklogin() && (hasAccess($userid, $cid, 'w') || isSuperUser($userid))){
 
-		if(strcmp($opt,"ADDUGGA")===0){
-				$querystring='insert into quiz(cid,autograde,gradesystem,qname,quizFile,creator) values (:cid,1,1,"New Dugga","test.html",:uid)';	
-				$stmt = $pdo->prepare($querystring);
-				$stmt->bindParam(':cid', $cid);
-				$stmt->bindParam(':uid', $userid);
+	if(strcmp($opt,"ADDUGGA")===0){
+		$querystring='insert into quiz(cid,autograde,gradesystem,qname,quizFile,creator) values (:cid,1,1,"New Dugga","test.html",:uid)';	
+		$stmt = $pdo->prepare($querystring);
+		$stmt->bindParam(':cid', $cid);
+		$stmt->bindParam(':uid', $userid);
 		
-				try {
-					$stmt->execute();
-				} catch (PDOException $e) {
+		try{
+			$stmt->execute();
+		}catch (PDOException $e){
 						// Error handling to $debug		
-				}
-		}else if(strcmp($opt,"ADDVARI")===0){
-				$querystring='INSERT INTO variant(quizID,param,variantanswer,creator) values (:qid,"New","Variant",:uid)';	
-				$stmt = $pdo->prepare($querystring);
-				$stmt->bindParam(':qid', $qid);
-				$stmt->bindParam(':uid', $userid);
-		
-				if(!$stmt->execute()) {
-					$error=$stmt->errorInfo();
-					$debug="Error updating entries".$error[2];
-				}
-		}else if(strcmp($opt,"SAVVARI")===0){
-				$query = $pdo->prepare("UPDATE variant SET param=:param, variantanswer=:variantanswer WHERE vid=:vid;");
-				$query->bindParam(':vid', $vid);
-				$query->bindParam(':param', $param);
-				$query->bindParam(':variantanswer', $answer);
-		
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating user".$error[2];
-				}	
-		}else if(strcmp($opt,"DELVARI")===0){
-				$query = $pdo->prepare("DELETE FROM variant WHERE vid=:vid;");
-				$query->bindParam(':vid', $vid);
-
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating user".$error[2];
-				}
-		}else if(strcmp($opt,"SAVDUGGA")===0){
-				$query = $pdo->prepare("UPDATE quiz SET qname=:name,autograde=:autograde,gradesystem=:gradesys,quizFile=:template,qrelease=:release,deadline=:deadline WHERE id=:qid;");
-				$query->bindParam(':qid', $qid);
-				$query->bindParam(':name', $name);
-				$query->bindParam(':autograde', $autograde);
-				$query->bindParam(':gradesys', $gradesys);
-				$query->bindParam(':template', $template);
-
-				if($release=="null") $query->bindValue(':release', null,PDO::PARAM_INT);
-				else $query->bindParam(':release', $release);
-
-				if($deadline=="null") $query->bindValue(':deadline', null,PDO::PARAM_INT);
-				else $query->bindParam(':deadline', $deadline);
-		
-				if(!$query->execute()) {
-					$error=$query->errorInfo();
-					$debug="Error updating user".$error[2];
-				}	
 		}
+	}else if(strcmp($opt,"ADDVARI")===0){
+		$querystring='INSERT INTO variant(quizID,param,variantanswer,creator) values (:qid,"New","Variant",:uid)';	
+		$stmt = $pdo->prepare($querystring);
+		$stmt->bindParam(':qid', $qid);
+		$stmt->bindParam(':uid', $userid);
+		
+		if(!$stmt->execute()) {
+			$error=$stmt->errorInfo();
+			$debug="Error updating entries".$error[2];
+		}
+	}else if(strcmp($opt,"SAVVARI")===0){
+		$query = $pdo->prepare("UPDATE variant SET param=:param, variantanswer=:variantanswer WHERE vid=:vid;");
+		$query->bindParam(':vid', $vid);
+		$query->bindParam(':param', $param);
+		$query->bindParam(':variantanswer', $answer);
+		
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating user".$error[2];
+		}	
+	}else if(strcmp($opt,"DELVARI")===0){
+		$query = $pdo->prepare("DELETE FROM variant WHERE vid=:vid;");
+		$query->bindParam(':vid', $vid);
+
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating user".$error[2];
+		}
+	}else if(strcmp($opt,"SAVDUGGA")===0){
+		$query = $pdo->prepare("UPDATE quiz SET qname=:name,autograde=:autograde,gradesystem=:gradesys,quizFile=:template,qrelease=:release,deadline=:deadline WHERE id=:qid;");
+		$query->bindParam(':qid', $qid);
+		$query->bindParam(':name', $name);
+		$query->bindParam(':autograde', $autograde);
+		$query->bindParam(':gradesys', $gradesys);
+		$query->bindParam(':template', $template);
+
+		if($release=="null") $query->bindValue(':release', null,PDO::PARAM_INT);
+		else $query->bindParam(':release', $release);
+
+		if($deadline=="null") $query->bindValue(':deadline', null,PDO::PARAM_INT);
+		else $query->bindParam(':deadline', $deadline);
+		
+		if(!$query->execute()) {
+			$error=$query->errorInfo();
+			$debug="Error updating user".$error[2];
+		}	
+	}
 }
 
 //------------------------------------------------------------------------------------------------
@@ -109,69 +109,69 @@ $entries=array();
 
 $files=array();
 
-if(checklogin() && (hasAccess($userid, $cid, 'w') || isSuperUser($userid))) {
+if(checklogin() && (hasAccess($userid, $cid, 'w') || isSuperUser($userid))){
 
-		$query = $pdo->prepare("SELECT id,cid,autograde,gradesystem,qname,quizFile,qrelease,deadline,modified FROM quiz WHERE cid=:cid ORDER BY id;");
-		$query->bindParam(':cid', $cid);
-		if(!$query->execute()) {
-			$error=$query->errorInfo();
+	$query = $pdo->prepare("SELECT id,cid,autograde,gradesystem,qname,quizFile,qrelease,deadline,modified FROM quiz WHERE cid=:cid ORDER BY id;");
+	$query->bindParam(':cid', $cid);
+	if(!$query->execute()){
+		$error=$query->errorInfo();
+		$debug="Error updating entries".$error[2];
+	}
+
+	foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
+
+		$queryz = $pdo->prepare("SELECT vid,quizID,param,variantanswer,modified FROM variant WHERE quizID=:qid ORDER BY vid;");
+		$queryz->bindParam(':qid',  $row['id']);
+
+		if(!$queryz->execute()){
+			$error=$queryz->errorInfo();
 			$debug="Error updating entries".$error[2];
 		}
-		
-		foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
-			
-			$queryz = $pdo->prepare("SELECT vid,quizID,param,variantanswer,modified FROM variant WHERE quizID=:qid ORDER BY vid;");
-			$queryz->bindParam(':qid',  $row['id']);
 
-			if(!$queryz->execute()) {
-				$error=$queryz->errorInfo();
-				$debug="Error updating entries".$error[2];
-			}
+		$mass=array();
+		foreach($queryz->fetchAll(PDO::FETCH_ASSOC) as $rowz){
 
-			$mass=array();
-			foreach($queryz->fetchAll(PDO::FETCH_ASSOC) as $rowz){
-
-				$entryz = array(
-					'vid' => $rowz['vid'],
-					'param' => $rowz['param'],
-					'variantanswer' => $rowz['variantanswer'],
-					'modified' => $rowz['modified'],
+			$entryz = array(
+				'vid' => $rowz['vid'],
+				'param' => $rowz['param'],
+				'variantanswer' => $rowz['variantanswer'],
+				'modified' => $rowz['modified'],
 				);
 
-				array_push($mass, $entryz);
-			}
-			
-			$entry = array(
-				'variants' => $mass,
-				'did' => $row['id'],
-				'cid' => $row['cid'],
-				'autograde' => $row['autograde'],
-				'gradesystem' => $row['gradesystem'],
-				'name' => $row['qname'],
-				'template' => $row['quizFile'],
-				'release' => $row['qrelease'],	
-				'deadline' => $row['deadline'],				
-				'modified' => $row['modified']				
+			array_push($mass, $entryz);
+		}
+
+		$entry = array(
+			'variants' => $mass,
+			'did' => $row['id'],
+			'cid' => $row['cid'],
+			'autograde' => $row['autograde'],
+			'gradesystem' => $row['gradesystem'],
+			'name' => $row['qname'],
+			'template' => $row['quizFile'],
+			'release' => $row['qrelease'],	
+			'deadline' => $row['deadline'],				
+			'modified' => $row['modified']				
 			);
 
-			array_push($entries, $entry);
-		}
+		array_push($entries, $entry);
+	}
 
-		$dir    = './templates';
-		$giles = scandir($dir);
-		$files =array();
-		foreach ($giles as $value){
-				if(endsWith($value,".html")){
-						array_push($files,substr ( $value , 0, strlen($value)-5 ));
-				}		
-		}
+	$dir    = './templates';
+	$giles = scandir($dir);
+	$files =array();
+	foreach ($giles as $value){
+		if(endsWith($value,".html")){
+			array_push($files,substr ( $value , 0, strlen($value)-5 ));
+		}		
+	}
 }
-		
+
 $array = array(
 	'entries' => $entries,
 	'debug' => $debug,
 	'files' => $files
-);
+	);
 
 
 echo json_encode($array);
