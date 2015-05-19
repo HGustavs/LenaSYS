50eef6ee465bf01833cde097afaeac1ebef9f9ba
Christopher Jubell <ChristopherJubell@hotmail.com>
2015-05-19 13:13:34 +0200

    Update resultlisted.php

--- DuggaSys/resultlisted.php
+++ DuggaSys/resultlisted.php
@@ -21,7 +21,7 @@
 	<script src="../Shared/resultlisted.js"></script>
 </head>
 <body>
-	<?php 
+<?php 
 		$noup="SECTION";
 		$loginvar="RESULT";
 		include '../Shared/navheader.php';
@@ -38,5 +38,25 @@
 	<?php 
 		include '../Shared/loginbox.php';
 	?>
+	
+	
+	<!-- Edit list START -->
+	<div id='editlist' class='loginBox' style='width:464px;display:none;'>
+		<div class='loginBoxheader'>
+			<h3>Edit List</h3>
+			<div onclick='closeWindows();'>x</div>
+		</div>		
+		<div style='padding:5px;'>
+			<input type='hidden' id='cid' value='Toddler' />
+			<div class='inputwrapper'><span>Issuer of the list:</span><input class='textinput' type='text' id='listissuer' placeholder='Pernilla kvist' /></div>
+			<div class='inputwrapper'><span>date of issuing list:</span><input class='textinput' type='date' id='dateissue' placeholder='03-12-2015' /></div>
+			<div class='inputwrapper'><span>List Number</span><input type="text" class='textinput' id="listnumber" placeholder='34754'/></div>
+			<div class='inputwrapper'><span>Examination date:</span><input type='date' class='textinput' id='examdate' placeholder='03-12-2015'/></div>
+			<input type='hidden' id='savelistid' value=''/>
+		</div>
+		<div style='padding:5px;'>
+			<input class='submit-button' type='button' value='Save' title='Save changes' onclick='updatelist();' />
+		</div>
+	</div>
 </body>
 </html>
