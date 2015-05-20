e3d9ca8ee2b7e491f20871c6c69fc74302b7b0d1
a13karsi <a13karsi@student.his.se>
2015-05-20 13:21:04 +0200

    Update showDugga.php

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -52,7 +52,7 @@
 	$result = $query->execute();
 	if($row = $query->fetch(PDO::FETCH_ASSOC)){
 		//Give permit if the user is logged in and has access to the course or if it is public
-		$hr = ((checklogin() && hasAccess($userid, $cid, 'r')) || $row['visibility'] != 0  && $userid != "UNK");
+		$hr = ((checklogin() && hasAccess($userid, $cid, 'r')) || $row['visibility'] != 0 && $userid != "UNK");
 		if(!$hr){
 			if (checklogin()){
 				$hr = isSuperUser($userid);
@@ -126,9 +126,9 @@
 					echo "<div class='err'><span style='font-weight:bold;'>Bummer!</span> The link you asked for does not currently exist!</div>";
 				}
 			}else if ($userid=="UNK"){
-				echo "<div class='err'><span style='font-weight:bold;'>Not logged in!</span>You need to be logged in if you want to do duggor. There is a log in button in the top right corner.</div>";
-			}else{
-				echo "<div class='err'><span style='font-weight:bold;'>Bummer!</span> Something went wrong in loading the dugga. Contact LENASys-admin.</div>";
+				echo "<div class='err'><span style='font-weight:bold;'>Inte inloggad!</span> Du måste logga in för att kunna se och genomföra duggor. Klicka på symbolen längst upp till höger.</div>";
+			}else {
+				echo "<div class='err'><span style='font-weight:bold;'>Bummer!</span> Något gick fel vid hämtningen av duggan. Kontakta LENASys-adminsitratör.</div>";
 			}
 		?>
 
@@ -152,7 +152,7 @@
 
 	<!-- Timer START -->
 
-	<div id='duggaTimer'>
+	<div id='scoreElement'>
 
 	</div>
 
