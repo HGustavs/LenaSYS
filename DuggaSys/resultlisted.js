9a97545318bfb8420abaa78c8988184f5f60381b
Christopher Jubell <ChristopherJubell@hotmail.com>
2015-05-20 10:23:07 +0200

    Update resultlisted.js

--- DuggaSys/resultlisted.js
+++ DuggaSys/resultlisted.js
@@ -1,32 +1,44 @@
+/********************************************************************************
+
+   Documentation <-- Top of file contains documentation e.g. call order and in some cases list of important bugs and/or version history
+
+*********************************************************************************
+
+Execution Order
+---------------------
+ #1 returnedResults() is first function to be called through AJAX
+
+-------------==============######## Documentation End ###########==============-------------
+
+*/
+
+/********************************************************************************
+
+   Globals
+
+*********************************************************************************/
+
 var sessionkind = 0;
 var querystring = parseGet();
 var filez;
 var mmx = 0, mmy = 0;
 var msx = 0, msy = 0;
 
-AJAXService("GET", {
-	cid : querystring['cid']
-}, "RESULTLIST");
+AJAXService("GET", {cid : querystring['cid']}, "RESULTLIST");
 
-$(function() {
-	$("#release").datepicker({
-		dateFormat : "yy-mm-dd"
-	});
-	$("#deadline").datepicker({
-		dateFormat : "yy-mm-dd"
-	});
+$(function() 
+{
+	$("#release").datepicker({dateFormat : "yy-mm-dd"});
+	$("#deadline").datepicker({dateFormat : "yy-mm-dd"});
 });
 
-//----------------------------------------
-// Commands:
-//----------------------------------------
 
 //----------------------------------------
-// Renderer
+// Padstring (adds a size to the string, if 2 characters used, you can specify how large it is (includes white spaces))
 //----------------------------------------
 
-function padstring(str, padno, kind) {
-	// Right Padding
+function padstring(str, padno, kind) // Right Padding
+{
 	if (kind == 1) {
 		if (str.length > padno) {
 			newstr = str.substring(0, padno);
@@ -40,10 +52,13 @@ function padstring(str, padno, kind) {
 	}
 	return newstr;
 }
-
-function returnedResults(data) {
-
-	console.log(data);
+//----------------------------------------
+// return from AJAX -  	One row for each student and Print as follows:
+//	                1. Start with ssn row. Add Padding for each of the other columns. 2. Write Name. Write values for each of the columns 3. Write Study Program. Add Padding for each of the other columns.
+//			SSn Padded to 24, Name Padded to 24   --- Results. Perhaps if overflow add more rows? Study Program Padded to 24
+//----------------------------------------
+function returnedResults(data) 
+{
 	str = "";
 	var sidanr = 0;
 
@@ -56,14 +71,11 @@ function returnedResults(data) {
 	//------------------------------------------------------
 	if (data['moments'].length > 0) {
 		for ( u = 0; u < data['moments'].length; u++) {
-			
 			var segment = data['moments'][u];
 			if(segment['kind'] == 4) {
 				sidanr++; //another page increasement
-			
 				results = data['results'];
 
-				// Make checkboxes!
 				var listnr ="";
 				var listresponsible ="";
 				var responsibledate ="";
@@ -110,41 +122,63 @@ function returnedResults(data) {
 						str += "<pre class='Trow'>";
 						str += "&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;&#9472;&#9472;&#9472;&#9472;&#9532;";
 						str += "\n";
-						// One row for each student
-
-						// Print as follows:
-
-						// 1. Start with ssn row. Add Padding for each of the other columns.
-						// 2. Write Name. Write values for each of the columns
-						// 3. Write Study Program. Add Padding for each of the other columns.
-
-						// SSn Padded to 24
-						// Name Padded to 24   --- Results. Perhaps if overflow add more rows?
-						// Study Program Padded to 24
-
 						str+=user['ssn']+"<br/>";
 						str += padstring(user['firstname'] + " " + user['lastname'], 24, 1);
 						
-
+						//--------------------------------------------
 						// Each of the section entries (i.e. moments)
+						//--------------------------------------------
+
+						studres = results[user['uid']]; 	// We have data if there is a set of result elements for this student in this course... otherwise null
+
+						//--------------------------------------------
+						// Each of the section entries (i.e. moments), and the grades of the 'moments' for all students under this course-moment
+						//--------------------------------------------
+						var foundres = null;
+						str += padstring("", 10, 1);
+						if (studres != null) {
 						
-
-								// We have data if there is a set of result elements for this student in this course... otherwise null
-								studres = results[user['uid']];
-								// There are results to display.
-
-									// Standalone Dugga -- we just need to make a dugga entry with the correct marking system.
-									// We are now processing the moment entry in the moment object
-									var foundres = null;
-									str += padstring("", 10, 1);
+							for (var l = 0; l < studres.length; l++) {
+								var resultitem = studres[l];
+							
+								if (resultitem['moment'] == segment['lid']) { //if the students 'moment' for the result is the same as the course-listerie we're under of kind 4 (moment)
+				
+									var gradeletter ="";
+									if(resultitem['grade'] == 1){
+									gradeletter = "U";
+									}
+									else if(resultitem['grade'] == 2){
+									gradeletter = "G";
+									}
+									else if(resultitem['grade'] == 3){
+									gradeletter = "VG";
+									}
+									else if(resultitem['grade'] == 4){
+									gradeletter = '3';
+									}
+									else if(resultitem['grade'] == 5){
+									gradeletter = '4';
+									}
+									else if(resultitem['grade'] == 6){
+									gradeletter = '5';
+									}
+									str += padstring(gradeletter, 6, 1);
+								}
+							}
+						}
+						str += padstring("", 6, 1);
+						str += padstring("", 5, 1);
+						//--------------------------------------------
+						// Each of the section entries (i.e. moments), and the grades of the 'moments' for all students under this course-listerie  we're under of kind 3 (tests)
+						//--------------------------------------------
+						for ( j = 0; j < data['moments'].length; j++) {
+							var moment = data['moments'][j];
+								if ((moment['kind'] == 3)) {
 									if (studres != null) {
-									
+								
 										for (var l = 0; l < studres.length; l++) {
 											var resultitem = studres[l];
-										
-											if (resultitem['moment'] == segment['lid']) {
-												// There is a result to print
-												foundres = resultitem['grade'];
+											if (resultitem['moment'] == moment['lid'] && (moment['moment'] == segment['lid']) ) {
 												var gradeletter ="";
 												if(resultitem['grade'] == 1){
 												gradeletter = "U";
@@ -168,54 +202,13 @@ function returnedResults(data) {
 											}
 										}
 									}
-									if (foundres == null) {
-									
-									}
-								str += padstring("", 6, 1);
-								str += padstring("", 5, 1);
-								
-								for ( j = 0; j < data['moments'].length; j++) {
-									var moment = data['moments'][j];
-
-										if ((moment['kind'] == 3)) {
-											if (studres != null) {
-										
-												for (var l = 0; l < studres.length; l++) {
-													var resultitem = studres[l];
-													if (resultitem['moment'] == moment['lid'] && (moment['moment'] == segment['lid']) ) {
-														// There is a result to print									
-												
-														var gradeletter ="";
-														if(resultitem['grade'] == 1){
-														gradeletter = "U";
-														}
-														else if(resultitem['grade'] == 2){
-														gradeletter = "G";
-														}
-														else if(resultitem['grade'] == 3){
-														gradeletter = "VG";
-														}
-														else if(resultitem['grade'] == 4){
-														gradeletter = '3';
-														}
-														else if(resultitem['grade'] == 5){
-														gradeletter = '4';
-														}
-														else if(resultitem['grade'] == 6){
-														gradeletter = '5';
-														}
-														str += padstring(gradeletter, 6, 1);
-													}
-												}
-											}
-										}
 								}
-				
+						}
 
 						str += "</pre>";
 					}
 				}
-str += "</br></br></br></br>";
+				str += "</br></br></br></br>";
 				var slist = document.getElementById("content");
 				slist.innerHTML = str;
 			}
