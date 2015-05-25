ad0f4ae00ffff28420a73de515e7ad4e727bd163
a13jansa <a13jansa@student.his.se>
2015-05-25 09:58:38 +0200

    Update EditorV50.php

--- CodeViewer/EditorV50.php
+++ CodeViewer/EditorV50.php
@@ -218,7 +218,7 @@ Testing Link:
 				</tr>
 				<tr>
 					<td>Play Link:<input class='form-control textinput' type='text' id='playlink' value='User Name' /></td>
-					<td>Important Words:<input class='form-control textinput' type='text' id='impword' placeholder="<Important word>" /><input style="width:32px; float:none; margin-top:0px;" class='submit-button' type='button' value='+' onclick='editImpWords("+");' /><select style="float:none;" id='impwords'><input style="width:32px; float:none; margin-top:0px;" class='submit-button' type='button' value='-' onclick='editImpWords("-");' /></select></td>			
+					<td>Important Words:<input class='form-control textinput' type='text' id='impword' placeholder="<Important word>" /><input style="width:32px; float:right; margin-top:0px;" class='submit-button' type='button' value='+' onclick='editImpWords("+");' /><select style="float:none;" id='impwords'><input style="width:32px; float:right; margin-top:0px;" class='submit-button' type='button' value='-' onclick='editImpWords("-");' /></select></td>			
 				</tr>	
 			</table>
 			<div id="SeqEdit">
