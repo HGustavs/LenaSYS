c808ad2ae4bd3a53ac9813451fbbfb52c8874c88
Alfons <a13alfbe@student.his.se>
2015-05-26 13:26:58 +0200

    Update EditorV50.php

--- CodeViewer/EditorV50.php
+++ CodeViewer/EditorV50.php
@@ -67,6 +67,7 @@ Testing Link:
 		<link type="text/css" href="../Shared/css/whiteTheme.css" rel="stylesheet" />
 		<link type="text/css" href="../Shared/css/responsive.css" rel="stylesheet" />
 		<link type="text/css" href="../Shared/css/style.css" rel="stylesheet" />
+		<link rel="shortcut icon" href="../Shared/icons/placeholder.ico"/>
 		<script type="text/javascript" src="../Shared/js/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
 		<script type="text/javascript" src="../Shared/dugga.js"></script>
