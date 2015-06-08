775a9d11d1aac0f9b0eca9d0270cda648c5df088
HGustavs <henrik.gustavsson@his.se>
2015-06-08 11:44:18 +0200

    Cleanup of showdoc 2015-06-08.

--- DuggaSys/showdoc.php
+++ DuggaSys/showdoc.php
@@ -55,115 +55,56 @@ if($hr){// If we have access rights, read the file securely to document
 				// Link
 				//--------------------------
 				echo "<script>window.location.replace('".$row['filename']."');</script>";
-			}else if($filekind==2){
-				// Global
-				//--------------------------
-				$file = "templates/".$row['filename'];
-				if(file_exists ( $file)){
+			}else{
 					$filename = $row['filename'];
-					$readfile = true;
-					$file_extension = strtolower(substr(strrchr($filename,"."),1));
-					switch($file_extension){
-						case "pdf": $ctype="application/pdf"; break;
-						case "exe": $ctype="application/octet-stream"; break;
-						case "zip": $ctype="application/zip"; break;
-						case "doc": $ctype="application/msword"; break;
-						case "xls": $ctype="application/vnd.ms-excel"; break;
-						case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
-						case "gif": $ctype="image/gif"; break;
-						case "png": $ctype="image/png"; break;
-						case "jpg": $ctype="image/jpg"; break;
-						default: $ctype="application/force-download";
+	
+					if($filekind==2){
+						// Global
+						$file = "templates/".$filename;
+					}else if($filekind==3){
+						// Course Local
+						$file = "../courses/".$cid."/".$filename;
+					}else if($filekind==4){
+						// Local
+						$file = "../courses/".$cid."/".$coursevers."/".$filename;
+					}else{
+						$file = "UNK";					
 					}
-					header("Content-Type: ".$ctype);
-					header('Content-Type: application/octet-stream');
-					header('Content-Disposition: inline; filename="' .$filename.'"');
-					header('Content-Transfer-Encoding: binary');
-					header('Accept-Ranges: bytes');
-					@readfile($file);
-				}else{
-					$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> The link you asked for does not currently exist!</div>";										  
-				}
-			}else if($filekind==3){
-				// Course Local
-				//--------------------------
-				$file = "../courses/".$cid."/".$row['filename'];
-				if(file_exists ($file)){
-					$filename = $row['filename'];
-					$readfile = true;
-					$file_extension = strtolower(substr(strrchr($filename,"."),1));
-					switch($file_extension){
-						case "pdf": $ctype="application/pdf"; break;
-						case "exe": $ctype="application/octet-stream"; break;
-						case "zip": $ctype="application/zip"; break;
-						case "doc": $ctype="application/msword"; break;
-						case "xls": $ctype="application/vnd.ms-excel"; break;
-						case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
-						case "gif": $ctype="image/gif"; break;
-						case "png": $ctype="image/png"; break;
-						case "jpg": $ctype="image/jpg"; break;
-						default: $ctype="application/force-download";
-					}
-					header("Content-Type: ".$ctype);
-					header('Content-Type: application/octet-stream');
-					header('Content-Disposition: inline; filename="' .$filename.'"');
-					header('Content-Transfer-Encoding: binary');
-					header('Accept-Ranges: bytes');
-					@readfile($file);
-				}else{
-					$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> The link you asked for does not currently exist!</div>";
-				}
-			}else if($filekind==4){
-				// Local
-				$file = "../courses/".$cid."/".$row['filename'];
-				if(file_exists ($file)){
-					$filename = $row['filename'];
-					$readfile = true;
-					$file_extension = strtolower(substr(strrchr($filename,"."),1));
-					switch($file_extension){
-						case "pdf": $ctype="application/pdf"; break;
-						case "exe": $ctype="application/octet-stream"; break;
-						case "zip": $ctype="application/zip"; break;
-						case "doc": $ctype="application/msword"; break;
-						case "xls": $ctype="application/vnd.ms-excel"; break;
-						case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
-						case "gif": $ctype="image/gif"; break;
-						case "png": $ctype="image/png"; break;
-						case "jpg": $ctype="image/jpg"; break;
-						default: $ctype="application/force-download";
-					}
-					header("Content-Type: ".$ctype);
-					header('Content-Type: application/octet-stream');
-					header('Content-Disposition: inline; filename="' .$filename.'"');
-					header('Content-Transfer-Encoding: binary');
-					header('Accept-Ranges: bytes');
-					@readfile($file);
-				}else{
-					$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> The link you asked for does not currently exist!</div>";										  
-				}
+			
+					if(file_exists ( $file)){
+							$file_extension = strtolower(substr(strrchr($filename,"."),1));
+							
+							if($file_extension=="html"){
+									$bummer=file_get_contents($file);
+							}else{
+									$readfile = true;
+									switch($file_extension){
+										case "pdf": $ctype="application/pdf"; break;
+										case "exe": $ctype="application/octet-stream"; break;
+										case "zip": $ctype="application/zip"; break;
+										case "doc": $ctype="application/msword"; break;
+										case "xls": $ctype="application/vnd.ms-excel"; break;
+										case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+										case "gif": $ctype="image/gif"; break;
+										case "png": $ctype="image/png"; break;
+										case "jpg": $ctype="image/jpg"; break;
+										default: $ctype="application/force-download";
+									}
+									header("Content-Type: ".$ctype);
+									header('Content-Type: application/octet-stream');
+									header('Content-Disposition: inline; filename="' .$filename.'"');
+									header('Content-Transfer-Encoding: binary');
+									header('Accept-Ranges: bytes');
+
+									@readfile($file);
+							}
+						}else{
+							$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> The link you asked for does not currently exist!</div>";										  
+						}
 			}
-		}else{
-			$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> You have reached a non-navigable link!</div>";
-			$query = $pdo->prepare("SELECT filename,kind from fileLink WHERE cid=:cid and UPPER(filename)=UPPER(:fname) LIMIT 1;");
-			$temp = substr($fname,6); 
-			$out = substr_replace($fname,"//",6); 
-			$out .= $temp; 
-			$query->bindParam(':cid', $cid);
-			$query->bindParam(':fname', $out);
-			$result = $query->execute();						
-			if($row = $query->fetch(PDO::FETCH_ASSOC)){
-				$filekind=$row['kind'];
-				if($filekind==1){
-					// Link
-					echo "<script>window.location.replace('".$row['filename']."');</script>";
-				}else{
-				}
-			}
-		}
+	}else{
+		$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> You have reached a non-navigable link!</div>";
 	}
-}else{
-	$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> You have reached a non-navigable link!</div>";
-}
 ?>
 <!DOCTYPE html>
 <html>
