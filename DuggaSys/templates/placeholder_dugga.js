9a2bc2547221c0b3881497bfc359b517b40e71af
Marcus Brohede <a97marbr@gmail.com>
2015-10-08 16:18:01 +0200

    Added a place-holder dugga that displays a markdown file with instructions. NB! This dugga should be replaced with a dugga where students can submit files (PDF, ZIP, etc)

---
+++ DuggaSys/templates/placeholder_dugga.js
@@ -0,0 +1,120 @@
+/********************************************************************************
+
+   Documentation 
+
+*********************************************************************************
+
+Example seed
+---------------------
+	Param:  {*tal*:*2*}
+	Answer: {*danswer*:*00000010 0 2*}
+
+-------------==============######## Documentation End ###########==============-------------
+*/
+
+
+//----------------------------------------------------------------------------------
+// Globals
+//----------------------------------------------------------------------------------
+
+var retdata=null;
+var hc=null;
+
+//----------------------------------------------------------------------------------
+// Setup
+//----------------------------------------------------------------------------------
+
+function setup()
+{
+		AJAXService("GETPARAM",{ },"PDUGGA");
+}
+
+//----------------------------------------------------------------------------------
+// returnedDugga: callback from ajax call in setup, data is json
+//----------------------------------------------------------------------------------
+
+function returnedDugga(data)
+{
+	Timer.startTimer();
+	ClickCounter.initialize();
+	if(querystring['highscoremode'] == 1) {
+		if(data['score'] > 0){
+			Timer.score = data['score'];
+		}
+		Timer.showTimer();
+	} else if (querystring['highscoremode'] == 2) {
+		if(data['score'] > 0){
+			ClickCounter.score = data['score'];
+			console.log(ClickCounter.score);
+		}
+		ClickCounter.showClicker();
+	}
+	
+	  if(data['debug']!="NONE!") alert(data['debug']);
+
+		if(data['param']=="UNK"){
+				alert("UNKNOWN DUGGA!");
+		}else{		
+			retdata=jQuery.parseJSON(data['param']);
+			data=null;
+			$.ajax({
+				method: "GET",
+				url: "showdoc.php",
+				data: { cid: retdata.cid, coursevers: retdata.coursevers, fname: retdata.fname }
+			})
+			.done(function( msg ) {
+				document.getElementById('instructions').innerHTML = msg;
+			});
+		}		
+}
+
+//--------------------================############================--------------------
+//                                  Master Functions
+//--------------------================############================--------------------
+
+
+function saveClick()
+{
+	Timer.stopTimer();
+
+	timeUsed = Timer.score;
+	stepsUsed = ClickCounter.score;
+
+	if (querystring['highscoremode'] == 1) {	
+		score = Timer.score;
+	} else if (querystring['highscoremode'] == 2) {
+		score = ClickCounter.score;
+	}
+
+	// Loop through all bits
+	bitstr="";
+	
+	// Duggastr includes only the local information, duggasys adds the dugga number and the rest of the information.
+	saveDuggaResult(bitstr);
+}
+
+function reset()
+{
+	alert("This will remove everything and reset timers and step counters. Giving you a new chance at the highscore.");
+	Timer.stopTimer();
+	Timer.score=0;
+	Timer.startTimer();
+	ClickCounter.initialize();
+
+}
+
+function showFacit(param, uanswer, danswer)
+{
+	var p = jQuery.parseJSON(param);
+	var daJSON = jQuery.parseJSON(danswer);
+	
+}
+
+function closeFacit(){
+
+}
+
+//--------------------================############================--------------------
+//                                  Local Functions
+//--------------------================############================--------------------
+
