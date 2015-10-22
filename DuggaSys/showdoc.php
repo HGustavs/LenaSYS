c89e13a12a78d91b92b449b2dbf0c46b7c66ae61
HGustavs <henrik.gustavsson@his.se>
2015-10-22 15:21:35 +0200

    Path and SQL fix for showdoc

--- DuggaSys/showdoc.php
+++ DuggaSys/showdoc.php
@@ -162,10 +162,11 @@
 					$bummer = "<div class='err'><span style='font-weight:bold;'>Bummer!</span> You have reached a non-navigable link!</div>";
 				}
 			}else if($fname!="UNK"){
-				$query = $pdo->prepare("SELECT filename,kind from fileLink WHERE cid=:cid and UPPER(filename)=UPPER(:fname) LIMIT 1;");
+				$query = $pdo->prepare("SELECT filename,kind from fileLink WHERE cid=:cid and UPPER(filename)=UPPER(:fname) ORDER BY kind DESC LIMIT 1;");
 				$query->bindParam(':cid', $cid);
 				$query->bindParam(':fname', $fname);
-				$result = $query->execute();						
+				echo "<br><br><br><br><br><br><br><br><br><br>".$fname;
+				$result = $query->execute();
 				if($row = $query->fetch(PDO::FETCH_ASSOC)){
 					$filekind=$row['kind'];
 					$filename = $row['filename'];
