9c7e2c8bcc924f82da3998498be723887f46b732
maxun <maxun@10.7.40.27>
2016-04-01 16:39:52 +0200

    Move to top buttons #1912
    fixat position and a ugly pic for the arrow

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -692,7 +692,10 @@ function returnedSection(data)
 
 					}
 	
-					if(data['writeaccess']) str+="<td style='width:24px'><img id='dorf' style='margin:4px' src='../Shared/icons/Cogwheel.svg' onclick='selectItem(\""+item['lid']+"\",\""+item['entryname']+"\",\""+item['kind']+"\",\""+item['visible']+"\",\""+item['link']+"\",\""+momentexists+"\",\""+item['gradesys']+"\",\""+item['highscoremode']+"\");' /></td>";
+					if(data['writeaccess']){
+						str+="<td style='width:24px'><img id='dorf' style='margin:4px' src='../Shared/icons/Cogwheel.svg' onclick='selectItem(\""+item['lid']+"\",\""+item['entryname']+"\",\""+item['kind']+"\",\""+item['visible']+"\",\""+item['link']+"\",\""+momentexists+"\",\""+item['gradesys']+"\",\""+item['highscoremode']+"\");' /></td>";
+						str+="<td style='width:24px'><img id='dorf' style='margin:4px; width:15px; height:15px' src='../Shared/icons/UpArrow.png' onclick='selectItem(\""+item['lid']+"\",\""+item['entryname']+"\",\""+item['kind']+"\",\""+item['visible']+"\",\""+item['link']+"\",\""+momentexists+"\",\""+item['gradesys']+"\",\""+item['highscoremode']+"\");' /></td>";
+					}
 
 					str += "</tr>";
 				}
