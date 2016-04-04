e1a9e811af45c15eccc641702771075ac9b5f48a
Skabunkel <skabunkel@gmail.com>
2016-04-04 10:00:39 +0200

    added the possibility to add teachers

--- DuggaSys/formerTeacher.php
+++ DuggaSys/formerTeacher.php
@@ -2,16 +2,20 @@
 
 	$query = $pdo->prepare('SELECT DISTINCT  user.ssn, user.firstname, user.lastname, user.email FROM user LEFT JOIN user_course ON (user.uid=user_course.uid) WHERE user_course.access="W"');
 	$query->execute();
-	$row = $query->rowCount();
-	$result = $query->fetch();
+	$data = $query->fetchAll();
 	
-	echo("<select>");
+//	echo("<p>");
 	
-//	for($i=0;$i<$row;$i++){
-//		$str = $result[$i]['ssn']+" "+$result[$i]['firstname']+" "+$result[$i]['lastname']+" "+$result[$i]['email'];
-//		$name = $teacher['firstname']+" "+$teacher['lastname'];
-//		echo( "<option value=\""+$str+"\">"+$name+"</option>\n\0");
-//	}
+//	echo($data['ssn'] . " " . $data['firstname'] . " " . $data['lastname'] . " " . $data['email']);
+	
+//	echo("</p>");
+//	$data = $query->fetchAll();
+	
+	echo("<select onchange=\"AddTeatcher(value);\">");
+	echo( "<option selected=\"selected\"value=\"-1\" > Choose a teacher </option>\n\0");
+	foreach($data as $result){
+		echo( "<option value=\"" . $result['ssn'] . " " . $result['firstname'] . " " . $result['lastname'] . " " . $result['email'] . "\" >" . $result['firstname'] . " " . $result['lastname'] .  "</option>\n\0");
+	}
 	
 	// The above code dosn't work.
 	// find a way to write out every teacher in the array.
