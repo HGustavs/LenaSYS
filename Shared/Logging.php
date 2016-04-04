b18857da700df677d9149d5d4e264275887f7478
Viktor Gustavsson <b14vikgu@student.his.se>
2016-04-04 17:22:05 +0200

    Added very basic logging functionality

--- Shared/Logging.php
+++ Shared/Logging.php
@@ -1,4 +1,45 @@
-Logging.php
 <?php
-	
+
+// Open log database and create table if it does not exist
+$log_db = new PDO('sqlite:../log.db');
+$sql = '
+	CREATE TABLE IF NOT EXISTS logEntries (
+		id INTEGER PRIMARY KEY,
+		eventType INTEGER,
+		description TEXT,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+';
+$log_db->exec($sql);
+
+
+//------------------------------------------------------------------------------------------------
+// logEvent
+//------------------------------------------------------------------------------------------------
+//
+// Creates a new log entry in the log database (log.db, located at the root directory)
+//
+
+function logEvent($eventType, $description) {
+	$query = $GLOBALS['log_db']->prepare('INSERT INTO logEntries (eventType, description) VALUES (:eventType, :description)');
+	$query->bindParam(':eventType', $eventType);
+	$query->bindParam(':description', $description);
+	$query->execute();
+}
+
+
+//------------------------------------------------------------------------------------------------
+// EventTypes
+//------------------------------------------------------------------------------------------------
+//
+// Contains constants for log event types
+//
+
+abstract class EventTypes {
+	const DuggaRead = 1;
+	const DuggaWrite = 2;
+	const LoginSuccess = 3;
+	const LoginFail = 4;
+}
+
 ?>
