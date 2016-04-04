b9c85b40340af4de5a1dd4f32e288fcc03dfdfb3
nozagleh <nozagleh@gmail.com>
2016-04-04 14:23:19 +0200

    removed #overlay hiding

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -22,9 +22,9 @@ function toggleloginnewpass(){
 }
 
 function closeWindows(){
-
+	//changed .loginBox to #loginBox to stop lockbox from closing when login box is closed
 	$("#loginBox").css("display", "none");
-	//$("#overlay").css("display","none");
+	//removed "display:none" on #overlay
 	$("#login #username").val("");
 	$("#login #password").val("");
 	
