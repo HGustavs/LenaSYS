bab495c531090dcb3fb29b0f59640daa9954f6ea
a14antpa <iwantusbeta@gmail.com>
2016-04-05 09:39:11 +0200

    logging.php
    Nu är det ett litet L.

---
+++ Shared/logging.php
@@ -0,0 +1,71 @@
+<?php
+
+// Open log database and create tables if they do not exist
+$log_db = new PDO('sqlite:../log.db');
+$sql = '
+	CREATE TABLE IF NOT EXISTS logEntries (
+		id INTEGER PRIMARY KEY,
+		eventType INTEGER,
+		description TEXT,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+	CREATE TABLE IF NOT EXISTS userLogEntries (
+		id INTEGER PRIMARY KEY,
+		uid INTEGER(10),
+		eventType INTEGER,
+		description VARCHAR(50),
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+';
+$log_db->exec($sql);
+
+
+//------------------------------------------------------------------------------------------------
+// logEvent
+//------------------------------------------------------------------------------------------------
+//
+// Creates a new log entry in the log database (log.db, located at the root directory)
+//
+
+function logEvent($eventType, $description) {
+	$query = $GLOBALS['log_db']->prepare('INSERT INTO logEntries (eventType, description) VALUES (:eventType, :description)');
+	$query->bindParam(':eventType', $eventType);
+	$query->bindParam(':description', $description);
+	$query->execute();
+}
+
+
+//------------------------------------------------------------------------------------------------
+// EventTypes
+//------------------------------------------------------------------------------------------------
+//
+// Contains constants for log event types
+//
+
+abstract class EventTypes {
+	const DuggaRead = 1;
+	const DuggaWrite = 2;
+	const LoginSuccess = 3;
+	const LoginFail = 4;
+}
+
+//------------------------------------------------------------------------------------------------
+// logUserEvent
+//------------------------------------------------------------------------------------------------
+//
+//  Creates a new userbased event in the log.db database.
+//
+
+function logUserEvent() {
+	$query = $GLOBALS['log_db']->prepare('INSERT INTO userLogEntries (uid, eventType, description) VALUES (:uid, :eventType, :description)');
+	$query->bindParam(':uid', $uid);
+	$query->bindParam(':eventType', $eventType);
+	$query->bindParam(':description', $description);
+	$query->execute();
+}
+
+
+
+
+
+?>
