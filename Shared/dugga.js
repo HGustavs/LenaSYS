47f895802bdad85c53e5a55e5e8e44f60f0d75f1
Viktor Gustavsson <b14vikgu@student.his.se>
2016-04-05 14:25:37 +0200

    Added mysql date format function to dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -211,6 +211,16 @@ function htmlEntities(str) {
    	return str;
 }
 
+// Converts a javascript date to a MySQL-compliant date string
+function mysqlDateTime(date) {
+	function twoDigits(d) {
+		if(0 <= d && d < 10) return "0" + d.toString();
+		if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+		return d.toString();
+	}
+	return date.getUTCFullYear() + "-" + twoDigits(1 + date.getUTCMonth()) + "-" + twoDigits(date.getUTCDate()) + " " + twoDigits(date.getUTCHours()) + ":" + twoDigits(date.getUTCMinutes()) + ":" + twoDigits(date.getUTCSeconds());
+}
+
 //----------------------------------------------------------------------------------
 // AJAX Service: Generic AJAX Calling Function with Prepared Parameters
 //----------------------------------------------------------------------------------
@@ -218,7 +228,7 @@ function htmlEntities(str) {
 function AJAXService(opt,apara,kind)
 {
 	var para="";
-	var timestamp = Date.now();
+	var timestamp = mysqlDateTime(Date.now());
 	var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
