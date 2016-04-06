0570e1f999fe97089d40cf2890adf663d754e739
Anton Bengtsson Fröberg <b14antfr@student.his.se>
2016-04-06 14:36:10 +0200

    Removed duplicate file "dugga.js"

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -25,7 +25,6 @@ if(isset($_GET['coursename'])){
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
 	<script src="../Shared/dugga.js"></script>
-	<script src="../Shared/dugga.js"></script>
 	<script src="sectioned.js"></script>
 </head>
 <body>
