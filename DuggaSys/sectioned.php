16568894670ad6ba982393c46eb4a019dc523268
Anton Bengtsson Fröberg <b14antfr@student.his.se>
2016-04-06 14:35:03 +0200

    Changed the page title of sections to the course name
    Issue #1843 fixed.

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -3,6 +3,10 @@ session_start();
 include_once "../../coursesyspw.php";
 include_once "../Shared/sessions.php";
 pdoConnect();
+
+if(isset($_GET['coursename'])){
+	$coursename = $_GET['coursename'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +16,7 @@ pdoConnect();
 	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Section Editor</title>
+	<title><?php echo $coursename; ?></title>
 
 	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
 	<!--<link type="text/css" href="../Shared/css/responsive.css" rel="stylesheet">-->
@@ -20,6 +24,7 @@ pdoConnect();
 
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+	<script src="../Shared/dugga.js"></script>
 	<script src="../Shared/dugga.js"></script>
 	<script src="sectioned.js"></script>
 </head>
