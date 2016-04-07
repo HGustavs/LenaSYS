8f83c3bf82fb06ad1134f64977fe7202b784aca9
a14jaksi <jakobsisk@James-Dean.local>
2016-04-07 12:22:40 +0200

    Added thread option (edit/delete/lock) styling

--- Forum/thread.php
+++ Forum/thread.php
@@ -45,20 +45,29 @@ if (file_exists("../.git/refs/heads/master")) {
 		<div id='threadHeader'>
 			<div id="threadTopic">
 					DV12G HT15: PHP Example 1
+
+					<div id="threadOptions">
+						<input class="new-item-button" type="button" value="Delete">
+						<input class="new-item-button" type="button" value="Edit">
+						<input class="new-item-button" type="button" value="Lock">
+					</div>
 			</div>
+
+
 			<div id="threadDescr">
 				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 				<p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 			</div>
 			<div id="threadDetails">
-				Created <span id="threadDate">Mar 3 2016</span> by <span id="threadCreator">John Doe</span>
+				Skapad <span id="threadDate">3 mar 2016</span> av <span id="threadCreator">John Doe</span>
 			</div>
 		</div>
 
 		<div id="threadComments">
 			<div id="threadCommentsTitle">
-				Comments
+				Kommentarer (2)
 			</div>
+
 			<div class="threadComment">
 				<div class="commentDetails">
 					<span id="commentUser">d13berli</span>
@@ -66,7 +75,17 @@ if (file_exists("../.git/refs/heads/master")) {
 				<div class="commentContent">
 						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 				</div>
-				<div class="commentDate"> Mar 4 2016 </div>
+				<div class="commentDate"> 07:30 4 mar 2016 </div>
+			</div>
+
+			<div class="threadComment replyComment">
+				<div class="commentDetails">
+					<span id="commentUser">a13josaf</span>
+				</div>
+				<div class="commentContent">
+						<p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. </p>
+				</div>
+				<div class="commentDate"> 14:10 4 mar 2016 </div>
 			</div>
 		</div>
 	</div>
