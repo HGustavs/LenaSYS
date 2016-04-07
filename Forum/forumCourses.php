b6b6fbc310f6a1515d531218f70f2a920bca0030
a14gusas <a14gusas@student.his.se>
2016-04-07 03:33:54 -0400

    main forum site

--- Forum/forumCourses.php
+++ Forum/forumCourses.php
@@ -1,21 +1,40 @@
 <?php
 require_once(dirname(__FILE__) . '/../Shared/database.php');
 
-	echo"<p style ='color: black'>hej</p>";
 	global $pdo;
 
-	if($pdo == null) {
+	if($pdo == null)
+	{
 		pdoConnect();
 	}
-
-	$query = $pdo->prepare("SELECT distinct coursename FROM course ");
-	if($query->execute() && $query->rowCount() > 0) {
-		for($i=0; $i<$query->rowCount(); $i++){
+	
+	echo "<table style='width: 100%; table-layout: fixed;'>";
+	$query = $pdo->prepare("SELECT distinct coursename FROM course WHERE visibility=1");
+	if($query->execute() && $query->rowCount() > 0)
+	{
+		for($i=1; $i<(($query->rowCount())+1); $i++)
+		{
 			$course = $query->fetch();
-			echo $course["coursename"];
-			echo"<br/>";
+			if($i%2==0)
+			{
+				echo "<tr class='lo' style='height: 32px;'>";
+					echo "<td class='example item' style='cursor:pointer;margin-left:15px;'>";
+						echo "<a style='cursor:pointer;margin-left:15px;'>".$course["coursename"]."</a>";
+					echo "</td>";
+				echo "</tr>";
+			}
+			else{
+				echo "<tr class='hi' style='height: 32px;'>";
+					echo "<td class='example item' style='cursor:pointer;margin-left:15px;'>";
+						echo "<a style='cursor:pointer;margin-left:15px;'>".$course["coursename"]."</a>";
+					echo "</td>";
+				echo "</tr>";
+			}				
 		}
-	} else {
+	}
+	else 
+	{
 
 	}
+	echo "</table>";
 ?>
