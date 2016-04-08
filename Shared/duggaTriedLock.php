fc79a18a5592f353a7e7b090c5482bb12ea885d3
nozagleh <nozagleh@gmail.com>
2016-04-08 09:59:10 +0200

    changed lock to the svg version in the code

--- Shared/duggaTriedLock.php
+++ Shared/duggaTriedLock.php
@@ -1,6 +1,6 @@
 <div id="overlayLock" style="display:block;top:50px;z-index:8500">
 	<div class="loginTransparent">
-		<img src="../Shared/icons/duggaLock.png">
+		<img src="../Shared/icons/duggaLock.svg">
   		<p style="color: #fff">Too many failed attempts<br>Please contact your course supervisor for access to this dugga.</p>
 	</div>
 </div>
