c65e0a4bf417d98436f2a14ec0f9a92832b97c80
nozagleh <nozagleh@gmail.com>
2016-04-08 09:28:12 +0200

    changed duggaTries lock text

--- Shared/duggaTriedLock.php
+++ Shared/duggaTriedLock.php
@@ -1,6 +1,6 @@
 <div id="overlayLock" style="display:block;top:50px;z-index:8500">
 	<div class="loginTransparent">
 		<img src="../Shared/icons/duggaLock.png">
-  		<p style="color: #fff">Too many failed attempts<br>Please contact your course coordinator for access to this dugga.</p>
+  		<p style="color: #fff">Too many failed attempts<br>Please contact your course supervisor for access to this dugga.</p>
 	</div>
 </div>
