b205e75b7e9b04c1b6aad55303355d514c9dec3d
a14antpa <iwantusbeta@gmail.com>
2016-04-27 12:58:12 +0200

    Revert "Revert "added layout""
    This reverts commit 391f65ff47be323f139adac7d761cd4136cd96d4.

--- DuggaSys/options.php
+++ DuggaSys/options.php
@@ -1,5 +1,49 @@
 <?php
+session_start();
+include_once "../../coursesyspw.php";
+include_once "../Shared/sessions.php";
+pdoConnect();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/ico" href="../Shared/icons/favicon.ico"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Options</title>
 
+	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
+	<link type="text/css" href="../Shared/css/responsive.css" rel="stylesheet">
+	<link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+	
+	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
+	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+	<script src="../Shared/dugga.js"></script>
+	<script src="options.js"></script>
+</head>
+<body>
 
+<?php
+
+	$noup="NONE";
+	$loginvar="OPTIONS";
+	include '../Shared/navheader.php';
+	setcookie("loginvar", $loginvar);
+	
+	// Show analytics if user is superuser.
+	if(isset($_SESSION["superuser"]) && $_SESSION["superuser"] == 1){
+?>
+	<!-- content START -->
+	<div id="content">
+			
+	</div>
+	<!-- content END -->
+	
+<?php
+	}	
+	else{
+		header('Location: courseed.php');
+	}
 ?>
