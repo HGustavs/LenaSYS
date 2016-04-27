44f575481b06b75af451834da6c03b87b3a6843f
a14antpa <iwantusbeta@gmail.com>
2016-04-27 14:16:16 +0200

    functionality test

--- DuggaSys/options.js
+++ DuggaSys/options.js
@@ -1,14 +1,3 @@
 $("#mouseLoggingON").onclick({
-	$.ajax({
-		url: 'optionservice.php',
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			option: "mouseMoveLogging" ,
-			value: "true"
-		},
-		success: function(data) {
-			alert("HELLO");
-		}
-	});
+	alert("test");
 });
