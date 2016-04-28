fd8a657d9dc5a2142e9891f635ec3b75530fe00c
unknown <d11karjo@VDI-IIT-P2-M037.hs.local>
2016-04-28 14:35:28 +0200

    Fixes missing G-VG-3-4-5-buttons.

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -232,11 +232,7 @@ if(strcmp($opt,"DUGGA")!==0){
 		}
 
 		// All dugga/moment entries from all versions of course
-
-		$query = $pdo->prepare("SELECT lid,moment,entryname,pos,kind,link,visible,code_id,vers,gradesystem FROM listentries WHERE listentries.cid=:cid AND listentries.vers=:vers AND (listentries.kind=3 OR listentries.kind=4) ORDER BY pos");
-		
-		// Be folk se över denna del.
-		//$query = $pdo->prepare("SELECT lid,moment,entryname,pos,kind,link,visible,code_id,vers,quiz.gradesystem AS quiz_g, listentries.gradesystem AS list_g FROM listentries  LEFT OUTER JOIN quiz ON listentries.link=quiz.id WHERE listentries.cid=:cid AND (listentries.kind=3 OR listentries.kind=4) ORDER BY pos");
+		$query = $pdo->prepare("SELECT lid,moment,entryname,pos,kind,link,visible,code_id,vers,quiz.gradesystem AS quiz_g, listentries.gradesystem AS list_g FROM listentries  LEFT OUTER JOIN quiz ON listentries.link=quiz.id WHERE listentries.cid=:cid AND listentries.vers=:vers AND (listentries.kind=3 OR listentries.kind=4) ORDER BY pos");
 
 		$query->bindParam(':vers', $vers);
 		$query->bindParam(':cid', $cid);
