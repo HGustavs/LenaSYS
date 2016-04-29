e342e463396f9025e7f64160167c3ea2697ec093
a14antpa <iwantusbeta@gmail.com>
2016-04-29 14:30:05 +0200

    test3
    hejhopp

--- DuggaSys/options.js
+++ DuggaSys/options.js
@@ -53,7 +53,7 @@ function setOption(label, value, cb) {
 				value2 = value;
 			}
 			
-			$("#theDiv").html("<div id='toastBar'>Successfully set the"+ label +" option to "+ value2 +"<a onclick='closeToast();'>X</a></div>");
+			$("#theDiv").html("<div id='toastBar'>Successfully set the"+ label +" option to "+ value2 +"<a href='#' onclick='closeToast();'>X</a></div>");
 			
 		}
 	});
