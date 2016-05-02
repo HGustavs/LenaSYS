9a2b2822e82ade139873ddbed8bdea31e40664e0
unknown <d11karjo@VDI-IIT-P2-M007.hs.local>
2016-05-02 14:26:17 +0200

    Fix

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -700,8 +700,6 @@ function processLogin() {
 					console.log("Removed show login bind");
 					$("#loginbutton").click(function(){processLogout();});
 					
-					
-
 					location.reload();
 				}else{
 					console.log("Failed to log in.");
@@ -765,21 +763,6 @@ function hideLoginPopup()
 //----------------------------------------------------------------------------------
 // setupLoginLogoutButton: Set button to login or logout functionality when navheader loads
 //----------------------------------------------------------------------------------
-
-function getCookie(cname) {
-     var name = cname + "=";
-     var ca = document.cookie.split(';');
-     for(var i = 0; i <ca.length; i++) {
-         var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-         if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-     }
-     return "";
- }
 
 function setupLoginLogoutButton(isLoggedIn){
 
