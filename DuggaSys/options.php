a3821423688713cf5b8721463f0e7093ff64b3e2
a14antpa <iwantusbeta@gmail.com>
2016-05-03 13:01:19 +0200

    testing purposes

--- DuggaSys/options.php
+++ DuggaSys/options.php
@@ -58,7 +58,22 @@ pdoConnect();
 						<input type="checkbox"<?= $options['fourthRound'] == '1' ? ' checked' : ''?>>
 					</label>
 				</div>
-				<p>.</p>
+				<div class="setting">
+					<p>Fourth round trip logging</p>
+					<label data-label="fourthRound" class="switch<?= $options['fourthRound'] == '1' ? ' checked' : ''?>">
+						<i class="icon-ok"></i>
+						<i class="icon-remove"></i>
+						<input type="checkbox"<?= $options['fourthRound'] == '1' ? ' checked' : ''?>>
+					</label>
+				</div>
+				<div class="setting">
+					<p>Fourth round trip logging</p>
+					<label data-label="fourthRound" class="switch<?= $options['fourthRound'] == '1' ? ' checked' : ''?>">
+						<i class="icon-ok"></i>
+						<i class="icon-remove"></i>
+						<input type="checkbox"<?= $options['fourthRound'] == '1' ? ' checked' : ''?>>
+					</label>
+				</div>
 			</div>
 	</div>
 	<!-- content END -->
