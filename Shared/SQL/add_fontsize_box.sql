fcacea6dfd8e63949fd4429b0c6b044070fc247a
David Boya <boya.david@gmail.com>
2016-05-09 13:38:59 +0200

    Some SQL updates

--- Shared/SQL/add_fontsize_box.sql
+++ Shared/SQL/add_fontsize_box.sql
@@ -1,6 +1,4 @@
 use imperious;
 
-/* Temporal way to add the new side layout templates to the database */
-INSERT INTO template(templateid,stylesheet, numbox) VALUES (7,"template7.css",4);
-INSERT INTO template(templateid,stylesheet, numbox) VALUES (8,"template8.css",3);
-INSERT INTO template(templateid,stylesheet, numbox) VALUES (9,"template9.css",5);
+/* Add column fontsize to box with the datatype tinyint and default value 9 (px) */
+ALTER TABLE `box` ADD `fontsize` TINYINT NOT NULL DEFAULT '9' AFTER `filename`;
---
+++ Shared/SQL/add_new_templates.sql
@@ -0,0 +1,6 @@
+use imperious;
+
+/* Temporal way to add the new side layout templates to the database */
+INSERT INTO template(templateid,stylesheet, numbox) VALUES (7,"template7.css",4);
+INSERT INTO template(templateid,stylesheet, numbox) VALUES (8,"template8.css",3);
+INSERT INTO template(templateid,stylesheet, numbox) VALUES (9,"template9.css",5);
