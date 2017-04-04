4703115b5a6f518796060e8dc51b10a657a9a376
Niklas Holmgren <a15nikho@student.his.se>
2017-04-04 14:28:45 +0200

    Added a comment field in Edit Dugga View
    A field has been added on the Edit Dugga View where teachers are able to
    write a comment that will show upp for students when they submit a
    dugga.  It uses the column that was created in #2616 for storing the
    comment in the database

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -150,18 +150,32 @@ function saveDuggaResult(citstr)
 		
 		document.getElementById('receipt').value = hexstr;
 		
+		//Get the comment
+ 		var comment = querystring['comment'];
 		
 		var deadline = querystring['deadline']; //Get deadlinedate from URL
 		var currentTime = new Date().toJSON().slice(0,10).replace(/-/g,'-'); //Get current time
 		
 		if(deadline > currentTime){	//Check if deadline has past
+			if(comment == "UNK"){
+ 				document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p>";
+ 			}
+ 			else{
+ 				document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p><p>\n\n"+comment+"\n\n</p>";
+ 			}
 			
-			document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p>";
+			
+			
 		}
 		else{
 			
-			document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p><p>OBS! Du har lämnat in efter deadline. Läraren kommer att rätta dugga vid nästa kurstillfälle.";
-		
+			if(comment == "UNK"){
+ 				document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p><p>OBS! Du har lämnat in efter deadline. Läraren kommer att rätta dugga vid nästa kurstillfälle.";
+ 			}
+ 			else{
+ 				document.getElementById('receiptInfo').innerHTML = "<p>\n\nTeckensträngen är ditt kvitto på att duggan har lämnats in. Spara kvittot på en säker plats.\n\n</p><p>\n\n"+comment+"\n\n</p>";
+ 			}
+			
 		}
 		
 		//alert("Kvitto - Duggasvar\n\n"+"\""+hexstr+"\"\n\nTeckensträngen ovan är ditt kvitto på att duggan har lämnats in.\n\nSpara kvittot på ett säkert ställe.");
@@ -385,7 +399,7 @@ function AJAXService(opt,apara,kind)
 			$.ajax({
 				url: "sectionedservice.php",
 				type: "POST",
-				data: "courseid="+querystring['courseid']+"&coursename="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"&opt="+opt+para,
+				data: "courseid="+querystring['courseid']+"&coursename="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"&comment="+querystring['deadlinecomment']+"&opt="+opt+para,
 				dataType: "json",
 				success: returnedSection
 			});
