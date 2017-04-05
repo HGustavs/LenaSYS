09e1ec5909d83c29a9870b3b5414317e127eef69
a15alica <a15alica@student.his.se>
2017-04-05 15:44:53 +0200

    #2773
    Fixed styling of the instruction and submission forms, deleted the table
    that was used before and added a delete-button for the submission rows.

--- DuggaSys/duggaed.php
+++ DuggaSys/duggaed.php
@@ -67,50 +67,38 @@ pdoConnect();
 			<h3>Edit Variant</h3>
 			<div onclick='closeWindows();'>x</div>
 		</div>
-		<div style='padding:5px;'>
+		<div style='padding:5px;display:flex;'>
 			<input type='hidden' id='vid' value='Toddler' />
-			<div id="leftDivDialog" style="width:67%;float:left;">
-				<div id="formDivDialog" style="width:50%;float:left">
-					<form class="left" id="jsonform" name="jsonform"> <!-- fun form -->
-						<!-- Instruction for assignment -->
-						<div class="inputwrapper" style="height:100px">
-							<fieldset style="width:90%;max-width:250px;">
-								<!-- Is good -->
-								<legend>Instruction file</legend>
-								<!-- <label for="type">Type:</label> -->
-								<select name="type" id="type" style="margin-bottom:-1px;width:160px;">
-									<option value="md">Markdown</option>
-									<option value="html">HTML</option>
-									<option value="pdf">PDF</option>
-								</select><br/>
-								<input type="text" name="filelink" id="filelink" placeholder="File link"><br/>
-								<!-- Is good end. -->
-							</fieldset>
+			<div id="leftDivDialog">
+				<!-- Instruction for assignment -->
+				<div class="inputwrapper" style="height:80px">
+					<fieldset style="width:90%">
+						<legend>Instruction file</legend>
+						<div style="display:flex;flex-wrap:nowrap;flex-direction:row;">
+							<select name="type" id="type" style="flex:1">
+								<option value="md">Markdown</option>
+								<option value="html">HTML</option>
+								<option value="pdf">PDF</option>
+							</select>
+							<input type="text" name="filelink" id="filelink" placeholder="File link" style="flex:2;margin-left:5px;">
 						</div>
-						<!-- Submissions part. -->
-						<!-- Submissions for dugga -->
-						<div class="inputwrapper" style="height:240px">
-							<div id="duggaSubmissionForm">
-								<fieldset id="submissions" name="submissions" style="width:90%;max-width:250px;">
-								<legend>Submissions types</legend>
-								</fieldset>
-								<button type="button" name="addfieldname" id="addfieldname">Add row</button>
-								<button type="button" name="createjson" id="createjson">Create JSON</button>
-							</div>
-						</div>
-					</form>
+					</fieldset>
 				</div>
-				<!-- Table showing ways of submissions -->
-				<div id="tableDivDialog" style="width:48%">
-					<span>Submissions</span>
-					<table>
-						<tr><th style="width:25%">Name</th><th style="width:12%">Filetype</th><th style="width:60%">Instruction</th><th style="width:3%">Del</th></tr>
-						<!-- <tr><td></td><td></td><td></td><td class="delButton">X</td></tr> -->
-					</table>
+				<!-- Submissions for dugga -->
+				<div class="inputwrapper" style="height:240px">
+					<div id="duggaSubmissionForm">
+						<fieldset style="width:90%">
+						<legend>Submissions types</legend>
+							<div id="submissions" style="display:flex;flex-wrap:wrap;flex-direction:row;">
+							</div>
+						</fieldset>
+						<button type="button" name="addfieldname" id="addfieldname">Add row</button>
+						<button type="button" name="createjson" id="createjson">Create JSON</button>
+					</div>
 				</div>
 			</div>
 			<!-- JSON and answer fields -->
-			<div id="rightDivDialog" style="width:33%;float:right;">
+			<div id="rightDivDialog">
 				<div class='inputwrapper' style='height:170px'><span>Param:</span><textarea id='parameter' placeholder='Variant Param' rows="5" style="height:100px"></textarea></div>
 				<div class='inputwrapper' style='height:170px'><span>Answer:</span><textarea id='variantanswer' placeholder='Variant Param' rows="5" style="height:100px"></textarea></div>
 			</div>
