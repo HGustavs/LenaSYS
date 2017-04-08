aaa111534707c680a0a4debeb999da5a5f4d9065
Unknown <frolunda_stenberg@hotmail.com>
2017-04-09 00:00:44 +0200

    fix-for-issue-#2903
    Added the shape "square" as a predefined figure.

--- DuggaSys/diagram_figure.js
+++ DuggaSys/diagram_figure.js
@@ -10,8 +10,10 @@ var numberOfPointsInFigure = 0;
 
 function createFigure() {
 	if(uimode == "CreateFigure" && md == 4) {
-		if(figureMode =="Free") {
-			freeDraw();
+		if(figureMode == "Free") {
+			figureFreeDraw();
+		} else if(figureMode == "Square") {
+			figureSquare();
 		}
 	}
 }
@@ -20,7 +22,7 @@ function createFigure() {
  * Free draw, the user have to click for
  * every point to draw on the canvas.
  */
-function freeDraw() {
+function figureFreeDraw() {
 	p1 = null;
 	if(isFirstPoint) {
 		p2 = points.addpoint(cx, cy, false);
@@ -54,6 +56,26 @@ function freeDraw() {
 			figurePath.addsegment(1, p1, p2);
 			numberOfPointsInFigure++;
 		}
+	}
+}
+
+/**
+ * Draws a square between p1 and p2.
+ */
+function figureSquare() {
+	if(isFirstPoint) {
+		p1 = points.addpoint(cx, cy, false);
+		isFirstPoint = false;
+	} else {
+		p3 = points.addpoint(cx, cy, false);
+		p2 = points.addpoint(points[p1].x, points[p3].y, false);
+		p4 = points.addpoint(points[p3].x, points[p1].y, false);
+		figurePath.addsegment(1, p1, p2);
+		figurePath.addsegment(1, p2, p3);
+		figurePath.addsegment(1, p3, p4);
+		figurePath.addsegment(1, p4, p1);
+		diagram.push(figurePath);
+		cleanUp();
 	}
 }
 
