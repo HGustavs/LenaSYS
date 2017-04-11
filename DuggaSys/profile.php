98a807877e46de95bda0c2533aa76c906792fc70
Carl Andersson <c14caran@student.his.se>
2017-04-11 11:41:37 +0200

    Fix for merge

--- DuggaSys/profile.php
+++ DuggaSys/profile.php
@@ -64,10 +64,19 @@ pdoConnect();
 		
 			<div id="notificationsOnOff" style="margin-left:30px">
 				<h3>Activate notifications</h3>
-				<button type="button" class="profile-element" id="activate_notifications">Activate notifications</button>
+				<?php
+				if (defined('PUSH_NOTIFICATIONS_VAPID_PUBLIC_KEY')) {
+				?>
+				<button type="button" class="profile-element" id="activate_notifications" disabled="disabled">Activate notifications</button>
 				<script>
 					var push_notifications_vapid_public_key = "<?php echo PUSH_NOTIFICATIONS_VAPID_PUBLIC_KEY; ?>";
 				</script>
+				<?php
+				} else {
+					echo "<p>Notifications subsystems not installed, notifications unavailable.</p>";
+				}
+				?>
+
 			</div>
 		</div>
 	</div>
