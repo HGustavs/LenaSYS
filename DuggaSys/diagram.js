77c171f44e08aa787f94e521760b958007877442
Mathias K <b15matki@student.his.se>
2017-04-12 14:38:21 +0200

    Fixed issue 2720
    Enables the user to assign the type "derived" to an object.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -951,8 +951,9 @@ function doubleclick(ev)
 	if(diagram[selobj].inside(cx,cy)){
         openAppearanceDialogMenu();
         document.getElementById('nametext').value = diagram[selobj].name;
-		document.getElementById('fontColor').value = diagram[selobj].fontColor;
-		document.getElementById('font').value = diagram[selobj].font;
+    		document.getElementById('fontColor').value = diagram[selobj].fontColor;
+    		document.getElementById('font').value = diagram[selobj].font;
+    		document.getElementById('attributeType').value = diagram[selobj].attributeType;
   }
 }
 
@@ -1025,9 +1026,9 @@ function mouseupevt(ev){
 				erAttributeA.topLeft=p1;
 				erAttributeA.bottomRight=p2;
 				erAttributeA.centerpoint=p3;
+				erAttributeA.attributeType="";
 				erAttributeA.fontColor="#253";
 				erAttributeA.font="Arial";
-
 				diagram.push(erAttributeA);
 
 				//selecting the newly created attribute and open the dialogmenu.
@@ -1173,19 +1174,19 @@ function dialogForm() {
     }
     if(diagram[selobj].symbolkind==2){
         form.innerHTML = "Attribute name:</br>" +
-        	"<input id='nametext' type='text'></br>" +
-            "Attribute type: </br>" +
-            "<select id ='attributeType'><option value='Primary key'>Primary key</option><option value='Normal'>Normal</option><option value='Multivalue' selected>Multivalue</option><option value='Composite' selected>Composite</option><option value='Drive' selected>Derive</option></select></br>" +
- 			"Font family:<br>" +
-            "<select id ='font'><option value='arial' selected>Arial</option><option value='Courier New'>Courier New</option><option value='Impact'>Impact</option><option value='Calibri'>Calibri</option></select><br>" +
-			"Font color:<br>" +
-			"<select id ='fontColor'><option value='black' selected>Black</option><option value='blue'>Blue</option><option value='Green'>Green</option><option value='grey'>Grey</option><option value='red'>Red</option><option value='yellow'>Yellow</option></select><br>" +
-            "Text size:<br>" +
-			"<select id ='TextSize'><option value=Tiny>Tiny</option><option value=Small>Small</option><option value=Medium>Medium</option><option value=Large>Large</option></select><br>"+
-			"<button type='submit' onclick='setTextSizeEntity()'>TextScale</button>" +
-			"<button type='submit'  class='submit-button' onclick='changeName(form)' style='float:none;display:block;margin:10px auto'>OK</button>" +
-			"<button type='submit' onclick='setType(form)'>setType</button>" +
-			"<button type='button' onclick='closeAppearanceDialogMenu()'>Cancel</button></br>";
+        "<input id='nametext' type='text'></br>" +
+        "Attribute type: </br>" +
+        "<select id ='attributeType'><option value='Primary key'>Primary key</option><option value='Normal'>Normal</option><option value='Multivalue' selected>Multivalue</option><option value='Composite' selected>Composite</option><option value='Drive' selected>Derive</option></select></br>" +
+   			"Font family:<br>" +
+        "<select id ='font'><option value='arial' selected>Arial</option><option value='Courier New'>Courier New</option><option value='Impact'>Impact</option><option value='Calibri'>Calibri</option></select><br>" +
+  			"Font color:<br>" +
+  			"<select id ='fontColor'><option value='black' selected>Black</option><option value='blue'>Blue</option><option value='Green'>Green</option><option value='grey'>Grey</option><option value='red'>Red</option><option value='yellow'>Yellow</option></select><br>" +
+        "Text size:<br>" +
+  			"<select id ='TextSize'><option value=Tiny>Tiny</option><option value=Small>Small</option><option value=Medium>Medium</option><option value=Large>Large</option></select><br>"+
+  			"<button type='submit' onclick='setTextSizeEntity()'>TextScale</button>" +
+  			"<button type='submit'  class='submit-button' onclick='changeName(form)' style='float:none;display:block;margin:10px auto'>OK</button>" +
+  			"<button type='submit' onclick='setType(form)'>setType</button>" +
+  			"<button type='button' onclick='closeAppearanceDialogMenu()'>Cancel</button></br>";
     }
     if(diagram[selobj].symbolkind==3){
         form.innerHTML = "Entity name: </br>" +
@@ -1220,7 +1221,7 @@ function changeName(form){
 	diagram[selobj].name=document.getElementById('nametext').value;
 	diagram[selobj].fontColor=document.getElementById('fontColor').value;
 	diagram[selobj].font=document.getElementById('font').value;
-	console.log(diagram[selobj])
+	diagram[selobj].attributeType=document.getElementById('attributeType').value;
     dimDialogMenu(false);
     updategfx();
 }
