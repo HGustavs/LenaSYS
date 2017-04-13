b03c9c7c951a09de49a64ec07137784b5842c788
Filip Barkestedt <a15filba@student.his.se>
2017-04-13 09:53:22 +0200

    #2979:
    Added an alert and reloads the page when the user session ends.

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -70,6 +70,19 @@ function setExpireCookie(){
 	console.log(expireDate);
 
 }
+
+//----------------------------------------------------------------------------------
+// Sets a cookie that expires at the same time as the user is logged out (when the session ends)
+//----------------------------------------------------------------------------------
+function setExpireCookieLogOut(){
+
+	var expireDate = new Date();
+	expireDate.setTime(expireDate.getTime() + (1 * 2 * 9000000));
+
+	document.cookie = "sessionEndTimeLogOut=expireC; expires="+ expireDate.toGMTString() +"; path=/";
+	console.log(expireDate);
+
+}
 //----------------------------------------------------------------------------------
 function closeWindows(){
 
@@ -631,6 +644,7 @@ function processLogin() {
 				var result = JSON.	parse(data);
 				if(result['login'] == "success") {
 					setExpireCookie();
+					setExpireCookieLogOut();
 					$("#userName").html(result['username']);
 					$("#loginbutton").removeClass("loggedout");
 					$("#loginbutton").addClass("loggedin");
@@ -714,6 +728,7 @@ function setupLoginLogoutButton(isLoggedIn){
 		$("#loginbutton").off("click");
 		$("#loginbutton").click(function(){processLogout();});
 		sessionExpireMessage();
+		sessionExpireLogOut();
 	}
 
 	else{
@@ -808,6 +823,29 @@ function sessionExpireMessage() {
 		}
 	}
 
+//----------------------------------------------------------------------------------
+// Gives an alert when user is timed out (when the session ends)
+//----------------------------------------------------------------------------------
+function sessionExpireLogOut() {
+
+	if(document.cookie.indexOf('sessionEndTimeLogOut=expireC') > -1){
+		var intervalId = setInterval(function() {
+			checkIfExpired();
+		}, 2000);
+	}
+
+	function checkIfExpired() {
+
+		if (document.cookie.indexOf('sessionEndTimeLogOut=expireC') == -1){
+			alert('Your session has expired');
+			// When reloaded the log in icon should change from green to red
+			location.reload();
+			clearInterval(intervalId);
+		}
+
+	}
+}
+	
 //----------------------------------------------------------------------------------
 // A function that handles the onmouseover/onmouseout events on the loginbutton-td, changing the icon-image on hover.
 //----------------------------------------------------------------------------------
