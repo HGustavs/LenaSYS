1bd0fe26a5fef8f91d4910e07d6c730de731bbb6
b14marki <b14marki@student.his.se>
2017-04-18 09:41:44 +0200

    #2451-Overlay
    There is now a overlay in swimlane.php. It is the same principle as
    loginbox.php.

---
+++ Shared/swimlane.php
@@ -0,0 +1,10 @@
+<!-- Overlay -->
+
+  <div id="overlay" style="display:none"></div>
+  
+	<!-- Swimlane Box Start! -->
+
+  <div id="swimlanebox" class="swimlanebox" style="display:none">
+  
+  
+  </div>
