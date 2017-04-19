798617bc2f1e70fdac2b3083d3ba42eff331bf9a
b15linek <b15linek@student.his.se>
2017-04-19 10:59:54 +0200

    Swimlane now shows dugga release and deadline.
    Only if the release and deadline is in the first 9 weeks of the year. Right now it's impossible to know when a course starts so the swimlane starts from the start of the year. Courses needs a start date.

--- Shared/swimlane.php
+++ Shared/swimlane.php
@@ -58,7 +58,8 @@
               echo '<text y="35" x="10" fill="black">' . $row['coursename'] . '</text>';
           }
 
-          $querystring="SELECT entryname FROM listentries WHERE cid=:cid AND kind=4";
+          $querystring="SELECT listentries.entryname, listentries.kind, quiz.qrelease, quiz.deadline 
+                        FROM listentries LEFT JOIN quiz ON  listentries.link = quiz.id WHERE listentries.cid = :cid;";
           $stmt = $pdo->prepare($querystring);
           $stmt->bindParam(':cid', $course);
 
@@ -70,18 +71,48 @@
 
           $white = true;
           $i = 0;
+          $j = 0;
+          $pos = 0;
+          $oldWeek = 0;
           foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-              echo '<text y="50" x="' . (($i * 200) + 230) . '" fill="black">' . $row['entryname'] . '</text>';
-              echo '<rect y="70" x="' . (($i * 200) + 200) . '" width="200" height="700" style="fill:rgb(';
-              if ($white){
-                  echo '255,255,255';
-                  $white = false;
-              } else {
-                  echo '230,230,230';
-                  $white = true;
+              if ($row['kind'] == 4){
+                  $j = 0;
+                  $pos = (($i * 200) + 230);
+                  echo '<text y="50" x="' . $pos . '" fill="black">' . $row['entryname'] . '</text>';
+                  echo '<rect y="70" x="' . $pos . '" width="200" height="700" style="fill:rgb(';
+                  if ($white){
+                      echo '255,255,255';
+                      $white = false;
+                  } else {
+                      echo '230,230,230';
+                      $white = true;
+                  }
+                  echo ')" />';
+                  $i++;
+              } else if ($row['kind'] == 3) {
+                  $deadlinedate = new DateTime($row['deadline']);
+                  $startdate = new DateTime($row['qrelease']);
+                  $deadlineweek = $deadlinedate->format("W");
+                  $startweek = $startdate->format("W");
+                  if ($j == 0) {
+                      $oldWeek = $deadlineweek;
+                  }
+
+                  echo '<line x1="' . ($pos + $j + 10) . '" y1="' . (100 + ($startweek - 1) * 70) .
+                      '" x2="' . ($pos + $j + 30) .
+                      '" y2="' . (100 + ($startweek - 1) * 70) .
+                      '" style="stroke:rgb(15,126,18);stroke-width:2" />';
+                  echo '<line x1="' . ($pos + $j + 20) . '" y1="' . (100 + ($startweek - 1) * 70) .
+                      '" x2="' . ($pos + $j + 20) .
+                      '" y2="' . (100 + ($deadlineweek - 1) * 70) . '" style="stroke:rgb(15,126,18);stroke-width:2" />';
+                  echo '<circle cx="' . ($pos + $j + 20) . '" cy="' . (100 + ($deadlineweek - 1) * 70) .
+                      '" r="10" stroke="green" stroke-width="2" fill="yellow" />';
+                  echo '<text y="' . (130 + (($oldWeek == $deadlineweek) ? $j : 0) + ($deadlineweek - 1) * 70) .
+                      '" x="' . ($pos + $j + 20) . '" fill="black">' . $row['entryname'] . '</text>';
+                  $j += 25;
+                  $oldWeek = $deadlineweek;
+
               }
-              echo ')" />';
-              $i++;
           }
 
           ?>
