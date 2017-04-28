9e540152fbff0ab6a17943cf6789c3f5ae48f794
b15sarha <b15sarha@student.his.se>
2017-04-28 09:49:09 +0200

    testing GitHub email

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -65,7 +65,7 @@ pdoConnect();
 		<div style='padding:5px;'>
 			<input style='float:left;' class='submit-button deleteDugga' type='button' value='Delete' onclick='deleteItem();' /> 
 			<input style='display:none; float:none;' class='submit-button closeDugga' type='button' value='Cancel' onclick='closeWindows(); closeSelect();showSaveButton();' /> 
-			<input style='margin-left:220px; display:none; float:none;' class='submit-button submitDugga' type='button' value='Submit' onclick='newItem();showSaveButton();' /> 
+			<input style='margin-left:220px; display:none; float:right;' class='submit-button submitDugga' type='button' value='Submit' onclick='newItem();showSaveButton();' />
 			<input style='float:right;' class='submit-button updateDugga' type='button' value='Save' onclick='updateItem();' /> 
     </div>
 	</div>
