878baae99dfa1a0610c9571e29872a3195680025
Unknown <a15nikho@student.his.se>
2017-04-28 17:15:04 +0200

    'New Group' button creates a column in usergroup table in the database
    When the button is clicked a pop-up window appears where the teacher has to write a name and when the teacher click on 'Submit' a column is created in the database. This will later be used for the students on grouped.php.

--- DuggaSys/grouped.php
+++ DuggaSys/grouped.php
@@ -49,8 +49,9 @@
 			<div onclick='closeWindows();'>x</div>
 		</div>
 		<div style='padding:5px;'>
-			<div id='inputwrapper-name' class='inputwrapper'><span>Name:</span><input style='float:none; margin-left: 5px;' type='text' class='textinput' id='groupname' placeholder='Name' /></div>
+			<div id='inputwrapper-name' class='inputwrapper'><span>Name:</span><input style='float:none; margin-left: 5px;' type='text' class='textinput' id='name' placeholder='Name' /></div>
 		</div>
+		<p style="display:none; color:red;" id="groupNameError">Du måste ange ett namn för att skapa en grupp.</p>
 		<div style='padding:5px;'>
 			<input style='float:none; display: inline-block;' class='submit-button ' type='button' value='Cancel' onclick='closeWindows();' /> 
 			<input style='margin-left: 40px; float:none; display: inline-block;' class='submit-button' type='button' value='Submit' onclick='createGroup();' /> 
