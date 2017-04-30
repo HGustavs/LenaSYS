1beb543fda10ba405036eb449a09aa9feeb96c64
Unknown <frolunda_stenberg@hotmail.com>
2017-04-30 18:27:00 +0200

    Cleanup [8]
    Cleaned some of the other files.
     - Re-Indented all files.
     - Replaced all tabs with spaces.
     - Added var in some for-loops to make variable local.

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -1,115 +1,101 @@
 <?php
-session_start();
-include_once "../../coursesyspw.php";
-include_once "../Shared/sessions.php";
-pdoConnect();
+    session_start();
+    include_once "../../coursesyspw.php";
+    include_once "../Shared/sessions.php";
+    pdoConnect();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" type="image/ico" href="../Shared/icons/favicon.ico"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome = 1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Section Editor</title>
-
-	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
-	<link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
-
-	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
-	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
-	<script src="../Shared/dugga.js"></script>
-	<script src="diagram.js"></script>
-	<script src="diagram_symbol.js"></script>
-	<script src="diagram_figure.js"></script>
+    <link rel="icon" type="image/ico" href="../Shared/icons/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome = 1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Section Editor</title>
+    <link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
+    <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    <script src="../Shared/js/jquery-1.11.0.min.js"></script>
+    <script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+    <script src="../Shared/dugga.js"></script>
+    <script src="diagram.js"></script>
+    <script src="diagram_symbol.js"></script>
+    <script src="diagram_figure.js"></script>
     <script src="diagram_example.js"></script>
-	<script src="diagram_IOHandler.js"></script>
-
-   <style>
-
-   </style>
-
+    <script src="diagram_IOHandler.js"></script>
 </head>
 <!-- Reads the content from the js-files -->
 <!-- updategfx() must be last -->
 <body onload="initcanvas(); Symbol(); canvassize(); updategfx(); loadDiagram();">
-
-	<?php
-		$noup="COURSE";
-		include '../Shared/navheader.php';
-	?>
-
-	<!-- content START -->
-	<div id="content">
-		<div id="buttonDiv">
-			<button onclick='classmode();'>Create Class</button>
-			<button onclick='attrmode();'>Create Attribute</button>
-			<button onclick='linemode();'>Create Line</button>
-			<button onclick='entitymode();'>Create Entity</button>
-	        <button onclick='relationmode();'>Create Relation</button>
-			<select id='selectFigure' onchange='figuremode()'>
-				<option selected='selected' disabled>Create Figure</option>
-				<option value='Square'>Square</option>
-				<option value='Free'>Free-Draw</option>
-			</select>
-			<button onclick='openAppearanceDialogMenu();'>Change Apperance</button>
-			<button onclick='debugMode();'>Debug</button>
-			<button onclick='clearCanvas(); removeLocal();'>Removed Hashed Diagram</button>
-			<button onclick='eraseSelectedObject();'>Delete Object</button>
-			<button onclick='clearCanvas(); removeLocal();'>Delete All</button>
-			<button id='zoomInButton' class='unpressed' style='right:0; position:fixed; margin-right:120px;'>+</button>
-			<button id='zoomOutButton' class='unpressed' style='right:0; position:fixed; margin-right:100px;'>-</button>
-	        <select id='download' onchange='downloadMode(this)'>
-	        	<option selected='selected' disabled>State</option>
-	       		<option value='getImage'>getImage</option>
-	       		<option value='Save'>Save</option>
-	       		<option value='Load'>Load</option>
-	        </select>
-	        <button><a onclick='SaveFile(this);' class='btn'> <i class='icon-download'></i>Export</a></button>
-	        <input id='fileid' type='file' name='file_name' hidden multiple/>
-	        <input id='buttonid' type='button' value='Import' />
-					Snap to grid:<input type="checkbox" onClick="enableGrid(this)" checked>
-
-			<button id='moveButton' class='unpressed' style='right: 0; position: fixed; margin-right: 10px;'>Start Moving</button><br>
-		</div>
-		<div id="canvasDiv"></div>
-		<div id="consoleDiv">
-			<div id='consloe' style='position:fixed;left:0px;right:0px;bottom:0px;height:133px;background:#dfe;border:1px solid #284;z-index:5000;overflow:scroll;color:#4A6;font-family:lucida console;font-size:13px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'>Application console</div>
-			<div id='valuesCanvas' style='position: fixed; left: 10px; bottom:130px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'></div>
-			<input id='Hide Console' style='position:fixed; right:0; bottom:133px;' type='button' value='Hide Console' onclick='Consolemode(1);' />
-			<input id='Show Console' style='display:none;position:fixed; right:0; bottom:133px;' type='button' value='Show Console' onclick='Consolemode(2);' />
-		</div>
-	</div>
-
-	<!-- The Appearance menu. Default state is display: none; -->
-	<div id="appearance" class='loginBox' style='display:none;'>
-		<div class='loginBoxheader'>
-			<h3>Apperance</h3>
-			<div onclick='closeAppearanceDialogMenu()'>x</div>
-		</div>
-		<div class='table-wrap'>
-			<div id="f01">
-            </div>
-		</div>
-	</div>
-
-	<!-- content END -->
-
-	<?php
-		include '../Shared/loginbox.php';
-	?>
     <?php
-    if(isset($_POST['StringDiagram'])){
-        $str = $_POST['StringDiagram'];
-        $hash = $_POST['Hash'];
-        save($str,$hash);
-    }
-    function save($data,$hash){
-        mkdir("Saves/b", 0777, true);
-        $myfile = fopen("Saves/b/$hash.txt", "w");
-        fwrite($myfile,$data);
-    }
+        $noup = "COURSE";
+        include '../Shared/navheader.php';
+    ?>
+    <!-- content START -->
+    <div id="content">
+        <div id="buttonDiv">
+            <button onclick='classmode();'>Create Class</button>
+            <button onclick='attrmode();'>Create Attribute</button>
+            <button onclick='linemode();'>Create Line</button>
+            <button onclick='entitymode();'>Create Entity</button>
+            <button onclick='relationmode();'>Create Relation</button>
+            <select id='selectFigure' onchange='figuremode()'>
+                <option selected='selected' disabled>Create Figure</option>
+                <option value='Square'>Square</option>
+                <option value='Free'>Free-Draw</option>
+            </select>
+            <button onclick='openAppearanceDialogMenu();'>Change Apperance</button>
+            <button onclick='debugMode();'>Debug</button>
+            <button onclick='clearCanvas(); removeLocal();'>Removed Hashed Diagram</button>
+            <button onclick='eraseSelectedObject();'>Delete Object</button>
+            <button onclick='clearCanvas(); removeLocal();'>Delete All</button>
+            <button id='zoomInButton' class='unpressed' style='right:0; position:fixed; margin-right:120px;'>+</button>
+            <button id='zoomOutButton' class='unpressed' style='right:0; position:fixed; margin-right:100px;'>-</button>
+            <select id='download' onchange='downloadMode(this)'>
+                <option selected='selected' disabled>State</option>
+                <option value='getImage'>getImage</option>
+                <option value='Save'>Save</option>
+                <option value='Load'>Load</option>
+            </select>
+            <button><a onclick='SaveFile(this);' class='btn'><i class='icon-download'></i>Export</a></button>
+            <input id='fileid' type='file' name='file_name' hidden multiple/>
+            <input id='buttonid' type='button' value='Import' />
+            Snap to grid:<input type="checkbox" onClick="enableGrid(this)" checked>
+            <button id='moveButton' class='unpressed' style='right: 0; position: fixed; margin-right: 10px;'>Start Moving</button><br>
+        </div>
+        <div id="canvasDiv"></div>
+        <div id="consoleDiv">
+            <div id='consloe' style='position: fixed; left: 0px; right: 0px; bottom: 0px; height: 133px; background: #dfe; border: 1px solid #284; z-index: 5000; overflow: scroll; color: #4A6; font-family:lucida console; font-size: 13px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'>Application console</div>
+            <div id='valuesCanvas' style='position: fixed; left: 10px; bottom: 130px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'></div>
+            <input id='Hide Console' style='position: fixed; right: 0; bottom: 133px;' type='button' value='Hide Console' onclick='Consolemode(1);' />
+            <input id='Show Console' style='display: none; position: fixed; right: 0; bottom: 133px;' type='button' value='Show Console' onclick='Consolemode(2);' />
+        </div>
+    </div>
+    <!-- The Appearance menu. Default state is display: none; -->
+    <div id="appearance" class='loginBox' style='display: none;'>
+        <div class='loginBoxheader'>
+            <h3>Apperance</h3>
+            <div onclick='closeAppearanceDialogMenu()'>x</div>
+        </div>
+        <div class='table-wrap'>
+            <div id="f01"></div>
+        </div>
+    </div>
+    <!-- content END -->
+    <?php
+        include '../Shared/loginbox.php';
+    ?>
+    <?php
+        if(isset($_POST['StringDiagram'])) {
+            $str = $_POST['StringDiagram'];
+            $hash = $_POST['Hash'];
+            save($str,$hash);
+        }
+        function save($data, $hash) {
+            mkdir("Saves/b", 0777, true);
+            $myfile = fopen("Saves/b/$hash.txt", "w");
+            fwrite($myfile, $data);
+        }
     ?>
 </body>
 </html>
--- DuggaSys/diagram_IOHandler.js
+++ DuggaSys/diagram_IOHandler.js
@@ -1,89 +1,77 @@
-/**
- * Created by Jockus on 2017-04-24.
- */
 var a;
 var c;
 var b;
-function downloadMode(el){
+var ac = [];
+
+function downloadMode(el) {
     var canvas = document.getElementById("content");
     var selectBox = document.getElementById("download");
     download = selectBox.options[selectBox.selectedIndex].value;
-
-    if(download.toString() == "getImage"){
+    if (download.toString() == "getImage") {
         console.log("b");
         getImage();
     }
-    if(download == "Save"){
+    if (download == "Save") {
         Save();
     }
-    if(download == "Load"){
+    if (download == "Load") {
         Load();
-    } if(download == "Export"){
+    }
+    if (download == "Export") {
         SaveFile(el);
     }
 }
 
-function saveToServer(dia){
-
+function saveToServer(dia) {
     //window.open("diagram_IOHandler.php");
-   // $.post("diagram_IOHandler.php",{ string: "ABC" });
+    // $.post("diagram_IOHandler.php",{ string: "ABC" });
     //$.post( "diagram_IOHandler.php",{DataString: a}, function( a ) {
     //    $('#stage').html(a);
     //});
-   $.ajax({
-       url: 'diagram.php',
-       type: 'POST', // GET or POST
+    $.ajax({
+        url: 'diagram.php',
+        type: 'POST', // GET or POST
         data: {StringDiagram : dia, Hash: hashfunction()} // will be in $_POST on PHP side
-       //success: function(data) { // data is the response from your php script
-       //    // This function is called if your AJAX query was successful
-       //    alert("Response is: " + data);
-       //},
-       //error: function() {
-       //    // This callback is called if your AJAX query has failed
-       //    alert("Error!");
-       //}
-   });
-
-
-
+        //success: function(data) { // data is the response from your php script
+        //    // This function is called if your AJAX query was successful
+        //    alert("Response is: " + data);
+        //},
+        //error: function() {
+        //    // This callback is called if your AJAX query has failed
+        //    alert("Error!");
+        //}
+    });
 }
 
-function getImage(){
-
-    window.open( document.getElementById("myCanvas").toDataURL("image/png"), 'Image');
+function getImage() {
+    window.open(document.getElementById("myCanvas").toDataURL("image/png"), 'Image');
 }
 
-var ac = [];
 function Save() {
-    for (i = 0; i < diagram.length; i++){
+    for (var i = 0; i < diagram.length; i++) {
         c[i] = diagram[i].constructor.name;
         c[i] = c[i].replace(/"/g,"");
     }
-
-    var obj = {
-        diagram: diagram,
-        points: points,
-        diagram_names: c
-    };
+    var obj = {diagram:diagram, points:points, diagram_names:c};
     a = JSON.stringify(obj);
     saveToServer(a);
     console.log("State is saved");
-
-
 }
-function SaveFile(el){
+
+function SaveFile(el) {
     Save();
     var data = "text/json;charset=utf-8," + encodeURIComponent(a);
-    el.setAttribute("class",'icon-download');
+    el.setAttribute("class", 'icon-download');
     el.setAttribute("href", "data:" + data);
     el.setAttribute("download", "diagram.txt");
     updategfx();
 }
-function LoadFile(){
+
+function LoadFile() {
     var pp = JSON.parse(a);
     b = pp;
     //diagram fix
-    for (i = 0; i < b.diagram.length; i++) {
+    for (var i = 0; i < b.diagram.length; i++) {
         if (b.diagram_names[i] == "Symbol") {
             b.diagram[i] = Object.assign(new Symbol, b.diagram[i]);
         } else if (b.diagram_names[i] == "Path") {
@@ -91,16 +79,15 @@ function LoadFile(){
         }
     }
     diagram.length = b.diagram.length;
-    for (i = 0; i < b.diagram.length;i++) {
+    for (var i = 0; i < b.diagram.length; i++) {
         diagram[i] = b.diagram[i];
     }
-
     // Points fix
-    for (i = 0; i < b.points.length; i++) {
+    for (var i = 0; i < b.points.length; i++) {
         b.points[i] = Object.assign(new Path, b.points[i]);
     }
     points.length = b.points.length;
-    for (i = 0; i< b.points.length; i++ ){
+    for (var i = 0; i< b.points.length; i++ ) {
         points[i] = b.points[i];
     }
     console.log("State is loaded");
@@ -111,10 +98,9 @@ function LoadFile(){
 function Load() {
     // Implement a JSON.parse() that will unmarshall a b c, so we can add
     // them to their respecive array so it can redraw the desired canvas.
-
     var dia = JSON.parse(a);
-    b= dia;
-    for (i = 0; i < b.diagram.length; i++) {
+    b = dia;
+    for (var i = 0; i < b.diagram.length; i++) {
         if (b.diagram_names[i] == "Symbol") {
             b.diagram[i] = Object.assign(new Symbol, b.diagram[i]);
         } else if (b.diagram_names[i] == "Path") {
@@ -122,16 +108,15 @@ function Load() {
         }
     }
     diagram.length = b.diagram.length;
-    for (i = 0; i < b.diagram.length;i++) {
+    for (var i = 0; i < b.diagram.length; i++) {
         diagram[i] = b.diagram[i];
     }
-
     // Points fix
-    for (i = 0; i < b.points.length; i++) {
+    for (var i = 0; i < b.points.length; i++) {
         b.points[i] = Object.assign(new Path, b.points[i]);
     }
     points.length = b.points.length;
-    for (i = 0; i< b.points.length; i++ ){
+    for (var i = 0; i < b.points.length; i++) {
         points[i] = b.points[i];
     }
     console.log("State is loaded");
