65bda65345be2e37614d27229b23a03e06fade8a
b15matgu <b15matgu@student.his.se>
2017-05-12 13:36:46 +0200

    Issue 3756 - Default size of relationship not even multiple of default size of entity
    Changed default size of objects to match and align to relations.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -60,18 +60,18 @@ var refresh_timer = setRefreshTime();              //  set how often the diagram
 var refresh_lock = false;               // used to set if the digram should stop refreshing or not.
 var attributeTemplate = {           // Defines entity/attribute/relations predefined sizes
   width: 7 * gridSize,
-  height: 4 * gridSize
+  height: 5 * gridSize
 };
 var entityTemplate = {
-  width: 6 * gridSize,
-  height: 3 * gridSize
+  width: 7 * gridSize,
+  height: 5 * gridSize
 };
 var relationTemplate = {
-  width: 8 * gridSize,
-  height: 4 * gridSize
+  width: 7 * gridSize,
+  height: 5 * gridSize
 };
 var classTemplate = {
-  width: 6 * gridSize,
+  width: 7 * gridSize,
   height: 7 * gridSize
 };
 var a = [], b = [], c = [];
