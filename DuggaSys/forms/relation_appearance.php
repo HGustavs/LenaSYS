c47dbb035e478629e933a1cbf6972538da214782
Admir Sahman <privat@admirsahman.se>
2017-05-12 10:34:28 +0200

    Update relation_appearance.php
    This was an old version, but it is now fixed.

--- DuggaSys/forms/relation_appearance.php
+++ DuggaSys/forms/relation_appearance.php
@@ -1,12 +1,12 @@
 Relation name:</br>
-<input onkeypress="changeObjectAppearance('relationType');" id='nametext' type='text'></br>
+<input onkeyup="changeObjectAppearance('relationType');" id='nametext' type='text'></br>
 Relation type: </br>
-<select onClick="changeObjectAppearance('relationType');" id='object_type'>
+<select onclick="changeObjectAppearance('relationType');" id='object_type'>
     <option value='weak'>weak</option>
     <option value='strong' selected>strong</option>
 </select></br>
 Background color:<br>
-<select onClick="changeObjectAppearance('relationType');" id='symbolColor'>
+<select onclick="changeObjectAppearance('relationType');" id='symbolColor'>
     "<option value='#add8e6'>Blue</option>" +
     "<option value='#dfe'>Green</option>" +
     "<option value='#dadada'>Grey</option>" +
@@ -15,14 +15,14 @@ Background color:<br>
     "<option value='#ffffff'>White</option>" +
 </select><br>
 Font family:<br>
-<select onClick="changeObjectAppearance('relationType');" id='font'>
+<select onclick="changeObjectAppearance('relationType');" id='font'>
     <option value='arial' selected>Arial</option>
     <option value='Courier New'>Courier New</option>
     <option value='Impact'>Impact</option>
     <option value='Calibri'>Calibri</option>
 </select><br>
 Font color:<br>
-<select onClick="changeObjectAppearance('relationType');" id='fontColor'>
+<select onclick="changeObjectAppearance('relationType');" id='fontColor'>
     <option value='black' selected>Black</option>
     <option value='blue'>Blue</option>
     <option value='Green'>Green</option>
@@ -31,7 +31,7 @@ Font color:<br>
     <option value='yellow'>Yellow</option>
 </select><br>
 Text size:<br>
-<select onClick="changeObjectAppearance('relationType');" id='TextSize'>
+<select onclick="changeObjectAppearance('relationType');" id='TextSize'>
     <option value='Tiny'>Tiny</option>
     <option value='Small'>Small</option>
     <option value='Medium'>Medium</option>
