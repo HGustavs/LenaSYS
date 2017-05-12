ee8c810505bf0f45f18674d3afc142ba27f0d157
Unknown <a15gunfu@student.his.se>
2017-05-12 11:28:49 +0200

    Fix for issue #3829
    Fixed an OK-button.

--- DuggaSys/forms/global_appearance.php
+++ DuggaSys/forms/global_appearance.php
@@ -4,3 +4,4 @@ require_once('fontcolor_appearance.php');
 require_once('textsize_appearance.php');
 require_once('line-thickness_appearance.php');
 ?>
+<button type='submit' class='submit-button' onclick="closeAppearanceDialogMenu(); changeObjectAppearance(); setType(form);" style='float: none; display: block; margin: 0px auto;'>OK</button>
