d6ad11e4e35f2e15d2398dd3d8823329f6408c89
Carl Andersson <c14caran@student.his.se>
2017-05-16 11:41:38 +0200

    Make even more lenasys-y

--- DuggaSys/profile.php
+++ DuggaSys/profile.php
@@ -36,40 +36,38 @@ pdoConnect();
 	if(checklogin()){
 	?>
 		<div id="content">
-			<div style='width:100%;height:50px; border-bottom: 2px dotted #775886; '>
-				<h1 style='text-align:center;color:#614875;'>Profile</h1>
+			<div style="width:100%;height:50px;">
+				<h1 style="text-align:center;color:#614875;">Profile</h1>
 			</div>
 			<div style="display:flex; flex-wrap:wrap;">
-				<div id="changeChallengeQuestion" style="flex-grow: 1; border-radius: 2px; margin: 10px; box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); padding: 20px;">
+				<div id="changeChallengeQuestion" class="material-box" style="flex-grow: 1;">
 					<h3>Change challenge question</h3>
 					<form method="post">
-						<label for="currentPassword">Current password</label><br/>
+						<label for="currentPassword">Current password</label><br>
 						<input type="password" id="currentPassword" class="form-control textinput" placeholder="Current password" style="width: 300px;"><br><br>
-						<label for="challengeQuestion">Challenge question</label><br/>
+						<label for="challengeQuestion">Challenge question</label><br>
 						<label id="securityQuestionError"></label>
-						<form action="/action_page.php">
-				            <select id="securityQuestion" class="form-control textinput" name="securityQuestions" style="width: 300px;">
-                                <option value="What was the name of your first pet?">What was the name of your first pet?</option>
-                                <option value="What is your favourite sports/e-sports team?">What is your favourite sports/e-sports team?</option>
-                                <option value="What was the best birthday present you ever got?">What was the best birthday present you ever got?</option>
-                                <option value="What is the first name of your favorite childhood friend?">What is the first name of your favorite childhood friend?</option>
-                                <option value="What is the first name of the person you had your second kiss with?">What is the first name of the person you had your second kiss with?</option>
-                                <option value="What was your favorite place to visit as a child?">What was your favorite place to visit as a child?</option>
-                                <option value="Where did you go on your first vacation?">Where did you go on your first vacation?</option>
-				            </select>
-						  <br><br>
-						</form>
-						<label for="challengeAnswer">Challenge answer</label><br/>
-						<input type="password" id="challengeAnswer" class="form-control textinput" placeholder="Answer to question" style="width: 300px;"><br><br>
+						<select id="securityQuestion" class="form-control textinput" name="securityQuestions" style="width: 300px;">
+							<option value="What was the name of your first pet?">What was the name of your first pet?</option>
+							<option value="What is your favourite sports/e-sports team?">What is your favourite sports/e-sports team?</option>
+							<option value="What was the best birthday present you ever got?">What was the best birthday present you ever got?</option>
+							<option value="What is the first name of your favorite childhood friend?">What is the first name of your favorite childhood friend?</option>
+							<option value="What is the first name of the person you had your second kiss with?">What is the first name of the person you had your second kiss with?</option>
+							<option value="What was your favorite place to visit as a child?">What was your favorite place to visit as a child?</option>
+							<option value="Where did you go on your first vacation?">Where did you go on your first vacation?</option>
+						</select>
+						<br><br>
+						<label for="challengeAnswer">Challenge answer</label><br>
+						<input type="text" id="challengeAnswer" class="form-control textinput" placeholder="Answer to question" style="width: 300px;"><br><br>
 						<button type="button" onClick="saveChallenge()" id="saveChallenge" class="submit-button" style="float: none; margin-left: 0;">Save Challenge</button>
 					</form>
-                    <div id="challengeMessage"></div>
+					<div id="challengeMessage"></div>
 				</div>
 			
-				<div id="changePassword" style="flex-grow: 1; border-radius: 2px; margin: 10px; box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); padding: 20px;">
+				<div id="changePassword" class="material-box" style="flex-grow: 1;">
 					<h3>Change password</h3>
-                    <div id="passForm">
-                        <form method="post" name="passwordForm">
+					<div id="passForm">
+						<form method="post" name="passwordForm">
 							<label for="currentPassword2">Current password</label><br>
 							<input type="password" class="form-control textinput" id="currentPassword2" placeholder="Current password" maxlength="72" onkeydown="formEventHandler(event)"><br><br>
 							<label for="newPassword">New password</label><br>
@@ -77,14 +75,14 @@ pdoConnect();
 							Password must be 8 Characters minimum.<br>
 							Password must contain a number, an uppercase and a lowercase letter.<br><br>
 							<label for="newPassword2">Confirm new password</label><br>
-							<input type="text" class="form-control textinput" id="newPassword2" placeholder="New password again" maxlength="72" onkeydown="formEventHandler(event)"><br><br>
-							<button type="button" id="passwordButton" onclick="validatePassword();" class="submit-button" style="float: none; margin-left: 0;">Update password</button><br/><br/>
-                        </form>
-                    </div>
-                    <div id="passwordMessage"></div>
+							<input type="password" class="form-control textinput" id="newPassword2" placeholder="New password again" maxlength="72" onkeydown="formEventHandler(event)"><br><br>
+							<button type="button" id="passwordButton" onclick="validatePassword();" class="submit-button" style="float: none; margin-left: 0;">Update password</button><br><br>
+						</form>
+					</div>
+					<div id="passwordMessage"></div>
 				</div>
 			
-				<div id="notificationsOnOff" style="flex-grow: 1; border-radius: 2px; margin: 10px; box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); padding: 20px;">
+				<div id="notificationsOnOff" class="material-box" style="flex-grow: 1;">
 					<h3>Push notifications</h3>
 					<?php
 					if (defined('PUSH_NOTIFICATIONS_VAPID_PUBLIC_KEY')) {
