14f51c4a10d97b35cf479ddb8593756bae4bd42e
Marcus Brohede <a97marbr@gmail.com>
2017-08-25 17:09:34 +0200

    Correct column name

--- Shared/SQL/deltachange.sql
+++ Shared/SQL/deltachange.sql
@@ -1,3 +1,3 @@
 /* Increased size of parameters and answers for dugga variants */
-ALTER TABLE variant MODIFY params VARCHAR(8256);
-ALTER TABLE variant MODIFY danswer VARCHAR(8256);
+ALTER TABLE variant MODIFY param VARCHAR(8256);
+ALTER TABLE variant MODIFY variantanswer VARCHAR(8256);
