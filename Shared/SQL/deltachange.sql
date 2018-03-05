fcb542ddb99fc9c8b1b34b223e5bc0dcae6bb83b
Marcus Brohede <a97marbr@gmail.com>
2018-03-05 15:35:30 +0100

    SQL update needed to use the new and improved fileedit service

--- Shared/SQL/deltachange.sql
+++ Shared/SQL/deltachange.sql
@@ -1,3 +1,8 @@
 /* Increased size of parameters and answers for dugga variants */
 ALTER TABLE variant MODIFY param VARCHAR(8256);
 ALTER TABLE variant MODIFY variantanswer VARCHAR(8256);
+
+/*New columns for fileLink */
+ALTER TABLE fileLink ADD COLUMN filesiz INT;
+ALTER TABLE fileLink ADD COLUMN uploaddate DATETIME;
+ALTER TABLE fileLink ADD COLUMN vers VARCHAR(8);
