969ded1a7ef1c015e8bc151efab35df94ad33b73
a16adala <a16adala.student.his.se>
2018-04-05 13:07:48 +0200

    Help toolbar fix13

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -194,6 +194,7 @@
                         <div class="drop-down-item">
                             <a href="#" id="Space">Move grid<div id="Space"><i>Blankspace</i></div></a>
                         </div>
+                        <div class="drop-down-divider">
                         <div class="drop-down-item">
                             <a href="#" id="Ctrl">Select multiple objects<div id="Ctrl"><i>Ctrl + leftclick</i></div></a>
                         </div>
