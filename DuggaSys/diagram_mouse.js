a9d0c620f3c8cd73357aece92d0ff88f8199ad35
b16petma <malmquist45@gmail.com>
2018-04-05 09:05:03 +0200

    test

--- DuggaSys/diagram_mouse.js
+++ DuggaSys/diagram_mouse.js
@@ -254,8 +254,10 @@ function mousedownevt(ev) {
         }
     } else {
         md = 4;            // Box select or Create mode.
-        startMouseCoordinateX = currentMouseCoordinateX;
-        startMouseCoordinateY = currentMouseCoordinateY;
+        if(figureType != "Free"){
+            startMouseCoordinateX = currentMouseCoordinateX;
+            startMouseCoordinateY = currentMouseCoordinateY;
+        }
     }
     if (lastSelectedObject >= 0) {
         if(uimode != "MoveAround") {
