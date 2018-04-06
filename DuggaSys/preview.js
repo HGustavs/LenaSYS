fe11b0e20e64128afadab3648787fd31ea2fd071
Setareh Nori <cetapex@gmail.com>
2018-04-06 14:59:16 +0200

    Added pseudocode and a base for preview.js

--- DuggaSys/preview.js
+++ DuggaSys/preview.js
@@ -1,11 +1,14 @@
 
 
 
+//GLOBAL
+var windowIsOpen;  //måste skapa en funktion till denna
+
 
 //funcion for showing popupWindow
 
 function popupWindow(){
-    // En startfunktion
+    initializeName();
 
     alert('hello it works');
 }
@@ -16,25 +19,38 @@ function popupWindow(){
 //Need to implement some variables and function, this is a not working beta-test.
 //Obs the funtion names are just random and will be changed.
 
-/*
-Start <<<<<{
-	sets the soom existing variable windowIsOpen = true;
-	calls the function someName();
+function initializeName() {     //Change name!
+    windowIsOpen = true;
+    run();
 }
- */
 
-/*
-SomeName <<<<<< {
-	while loop that uppdates as long as the windowIsOpen = true{
-		calls the function that handles the users key-events - updateText();
-		checks if the user wants to save the file - saveFile();
-		closeWi(); kolla
-	} when the variable windowIsOpen = false {
-		close the window
-	}
+
+/**************************************************************
+-----------------------------run()-----------------------------
+ The run function will execute when called, the purpose is that
+ as long as the wndow is open a set of different funtions will
+ run along. It will constantly check for updates and if the
+ variable windowIsOpen == false the while-loop will break and
+ the window close.
+ **************************************************************/
+function run(){
+    //Set frame ?
+    while (windowIsOpen == true){
+		//calls the function that handles the users key-events - updateText();
+		//checks if the user wants to save the file - saveFile();
+        updateCode();
+		//closeWi(); kolla
+        reWriteCode();
+	}   //when the variable windowIsOpen = false {
+		//close the window
+	//}
 }
- */
 
+function reWriteCode(){
+    //rewrite the code to the second window
+    //check for updates, if there is any call reWrite()
+    //if not - do nothing
+}
 /*
 Maybe we should create a seperate funtion for the keyIsPressed? Call the keyIsPressed here and in the updateText
 just uppdate the window with the latest update?
@@ -46,19 +62,22 @@ updateText <<<<<{
 }
  */
 
-/*
-saveFile <<<<<<<<<<{	till en början update
-	if the user presses the save button we shold save it with this function
+//----------------------------updateCode----------------------------//
+//For now, we need to rename it and change the function to saveFile() later
+//if the user presses the save button we shold save it with this function
+function updateCode(){
+    //check for updates, if there is any call reWrite()
+    //if not - do nothing
 }
- */
 
-/*
-cancelButton <<<<<<{
-	if the user presses the cancel-button windowIsOpen = false{
-		same thing happens here, the window will close
-	}
+
+function closeWindow(){
+	//if the user presses the cancel-button windowIsOpen = false{
+		windowIsOpen = false;
+	  //  same thing happens here, the window will close
+	//}
 }
- */
+
 
 
 
