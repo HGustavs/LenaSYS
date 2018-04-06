be9f2c53e22681a93deca53ab8a3ac9f656a36d0
a16adala <a16adala.student.his.se>
2018-04-06 11:05:59 +0200

    4335-previewtest1

--- DuggaSys/preview.php
+++ DuggaSys/preview.php
@@ -1,5 +1,24 @@
-<?php
-
-		echo $_GET['prev'];
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" type="image/ico" href="../Shared/icons/favicon.ico"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome = 1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Preview</title>
+        <link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
+        <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+        <script src="../Shared/js/jquery-1.11.0.min.js"></script>
+        <script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+        <script src="../Shared/dugga.js"></script>
+        <script>
+        </script>
+    </head>
+    <body>
+    Hello its me, preview
+        <?php
+		  echo $_GET['prev'];
+        ?>
+        
+    </body>
+</html>
