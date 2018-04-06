17b0f4973a95223273e6b3f785fbcdaf8b87fda9
Setareh Nori <cetapex@gmail.com>
2018-04-06 11:55:23 +0200

    Added some comments in markdown.js, not related to the rest of the file

--- Shared/markdown.js
+++ Shared/markdown.js
@@ -393,10 +393,69 @@ function markdownBlock(inString)
 
 	return inString;
 }
+
+
 //funcion for showing popupWindow
 
 function popupWindow(){
-    
+    // En startfunktion
     
     alert('hello it works');
 }
+
+
+//This is just some comments on what we need to do, feel free to edit och suggest ideas.
+//Need to implement some variables and function, this is a not working beta-test.
+//Obs the funtion names are just random and will be changed.
+
+/*
+Start <<<<<{
+	sets the soom existing variable windowIsOpen = true;
+	calls the function someName();
+}
+ */
+
+/*
+SomeName <<<<<< {
+	while loop that uppdates as long as the windowIsOpen = true{
+		calls the function that handles the users key-events - updateText();
+		checks if the user wants to save the file - saveFile();
+
+	} when the variable windowIsOpen = false {
+		close the window
+	}
+}
+ */
+
+/*
+Maybe we should create a seperate funtion for the keyIsPressed? Call the keyIsPressed here and in the updateText
+just uppdate the window with the latest update?
+updateText <<<<<{
+	when a keyboard is pressed and detected {
+		call a function that pase markdown
+		
+	}
+}
+ */
+
+/*
+saveFile <<<<<<<<<<{
+	if the user presses the save button we shold save it with this function
+}
+ */
+
+/*
+cancelButton <<<<<<{
+	if the user presses the cancel-button windowIsOpen = false{
+		same thing happens here, the window will close
+	}
+}
+ */
+
+
+
+
+
+
+
+
