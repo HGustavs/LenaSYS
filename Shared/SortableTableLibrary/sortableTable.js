c60a59e185155e7135bf6f0e70761577a6788868
a16juleh <a16juleh@student.his.se>
2018-04-09 10:03:51 +0200

    Changed editpopover

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -90,8 +90,8 @@ function clickedInternal(event,clickdobj) {
 
 		popoverelement.style.left = Math.round(lmnt.left+xscroll)+"px";
 		popoverelement.style.top = Math.round(lmnt.top+yscroll)+"px";
-		popoverelement.style.minHeight = Math.round(lmnt.height)+"px";
-		popoverelement.style.minWidth = (Math.round(lmnt.width)+40)+"px";
+		popoverelement.style.minHeight = (Math.round(lmnt.height)-5)+"px";
+		popoverelement.style.minWidth = (Math.round(lmnt.width)+0)+"px";
 		popoverelement.style.display = "flex";	
 	}
 }
