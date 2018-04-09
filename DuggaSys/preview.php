6d6ba1d5c8e2924601874000151606d85ffb5b81
Setareh Nori <cetapex@gmail.com>
2018-04-09 15:38:05 +0200

    Get the right output

--- DuggaSys/preview.php
+++ DuggaSys/preview.php
@@ -93,7 +93,7 @@
                     if(this.readyState == 4 && this.status == 200){
                     }
                         document.getElementById("markdown").innerHTML = //replace markdown with responseText
-                            markdownBlock(str); //get the response data as a string
+                            parseMarkdown(str); //get the response data as a string
                     };
 
                 //Return data from the file
