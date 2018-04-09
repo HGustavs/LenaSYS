0e26f43fa357436851d39fe14bc1d3c4ac27f24b
Setareh Nori <cetapex@gmail.com>
2018-04-09 13:24:15 +0200

    Get an input in the textbox

--- DuggaSys/preview.php
+++ DuggaSys/preview.php
@@ -99,9 +99,9 @@
                 //Return data from the file
                 //The str variable holds the content of the input field
                 //Specifies the type of request
-                xmlhttp.open("POST", "markdown.js" + str, true);
+                xmlhttp.open("POST", "markdown.js", true);
                 //Sends the request to the server
-                xmlhttp.send();
+                xmlhttp.send(str);
             }
         }
 
