0e0df2663964303fa1c9c8dbb2ff2b08ab0c4c34
Setareh Nori <cetapex@gmail.com>
2018-04-10 08:43:21 +0200

    Should write the right output now

--- DuggaSys/preview.php
+++ DuggaSys/preview.php
@@ -76,15 +76,21 @@
         function onload() {
             $(".PreviewWindow").hide();
         }
-
         function showPreview(str) {
             $(".PreviewWindow").show();
             //This function is triggered when key is pressed down in the input field
+            if(str.length == 0){
+                //Here we check if the input field is empty (str.length == 0).
+                // If it is, clear the content of the txtHint placeholder
+                // and exit the function.
 
-
-                        document.getElementById("markdown").innerHTML =
+                document.getElementById("markdown").innerHTML = " ";
+                return;
+            }else {
+                        document.getElementById("markdown").innerHTML = //replace markdown with responseText
                             parseMarkdown(str); //get the response data as a string
 
+            };
         }
 
         function saveCode() {
@@ -104,14 +110,14 @@
 <div class="PreviewWindow">
     <div class="PrevHead">This is the preview window</div>
     <div class="Markdown">
-
-            <textarea onkeyup="showPreview(this.value)" rows="32" cols="40">
-            </textarea>
-
+        <form>
+            <textarea onkeyup="showPreview(this.value)" rows="32" cols="40"></textarea>
+        </form>
     </div>
 
     <div class="MarkdownPrev">
         <span id="markdown"> </span>
+
     </div>
 
     <div class="OptionButtons">
