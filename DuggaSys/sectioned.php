21915babfbc0cbe9f05b623f8f38e21d198378c3
a14oliri <lillzug@hotmail.com>
2018-04-10 15:46:00 +0200

    Added functions to escape-pressed-event-handler in Shared/dugga.js and after creating new items

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -70,7 +70,7 @@ pdoConnect();
       		<div style='padding:5px;'>
       			<input style='display:none; float:left;' class='submit-button deleteDugga' type='button' value='Delete' onclick='deleteItem();' />
       			<input style='display:block; float:left;' class='submit-button closeDugga' type='button' value='Cancel' onclick='closeWindows();' />
-      			<input id="submitBtn" style='margin-left:220px; display:none; float:none;' class='submit-button submitDugga' type='button' value='Submit' onclick='newItem();' />
+      			<input id="submitBtn" style='margin-left:220px; display:none; float:none;' class='submit-button submitDugga' type='button' value='Submit' onclick='newItem(); showSaveButton();' />
       			<input id="saveBtn" style='float:right;' class='submit-button updateDugga' type='button' value='Save' onclick='updateItem();' />
           </div>
       </div>
