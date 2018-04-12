89a85d6166a75ec01c8d345aaa4cd336667d68d4
b16petma <malmquist45@gmail.com>
2018-04-12 11:41:51 +0200

    css fix didnt work as intented, did it in javascript instead

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -638,6 +638,7 @@ $(document).ready(function(){
         canvas.removeEventListener('mousemove', mousemoveposcanvas, false);
         canvas.removeEventListener('mouseup', mouseupcanvas, false);
         $("#moveButton").removeClass("pressed").addClass("unpressed");
+        $("#moveButton").hide();
         if ($(this).hasClass("pressed")){
             $(".buttonsStyle").removeClass("pressed").addClass("unpressed");
             uimode = "normal";
