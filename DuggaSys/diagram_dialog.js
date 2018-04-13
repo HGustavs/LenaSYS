a9c226b3a5751d5c80801ad93d9464bc1333f1d4
a16rasja <a16rasja@student.his.se>
2018-04-13 15:24:57 +0200

    Fixed minor error

--- DuggaSys/diagram_dialog.js
+++ DuggaSys/diagram_dialog.js
@@ -97,7 +97,7 @@ function loadFormIntoElement(element, dir){
 function setSelectedOption(type, value){
   for(var i = 0; i < document.getElementById(type).options.length; i++){
     if(value == document.getElementById(type).options[i].value){
-      console.log("value:" + value + " blir satt i valuet för: " type);
+      console.log("value:" + value + " blir satt i valuet för: " + type);
       document.getElementById(type).value = value;
       document.getElementById(type).options[i].selected = "true";
     }else{
