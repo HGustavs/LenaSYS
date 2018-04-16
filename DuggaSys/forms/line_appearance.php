0bf48ebb03a7e402261ca80645b8f54d18161bc9
a16rasja <a16rasja@student.his.se>
2018-04-16 14:59:17 +0200

    Added select boxes for cardinality, line appearance

--- DuggaSys/forms/line_appearance.php
+++ DuggaSys/forms/line_appearance.php
@@ -1,10 +1,25 @@
 Line type: </br>
 <select onclick="changeObjectAppearance('lineType');" id='object_type'>
-    "<option value='Normal'>Normal</option> +
-    "<option value='Forced'>Forced</option> +
-    "<option value='Derived'>Derived</option> +
+    <option value='Normal'>Normal</option>
+    <option value='Forced'>Forced</option>
+    <option value='Derived'>Derived</option>
 </select></br>
-Line colors:<br>
+Left side: <br/>
+<select onclick="" id="leftSide">
+  <option value="none">None</option>
+  <option value="one">1</option>
+  <option value="n">N</option>
+  <option value="m">M</option>
+</select>
+Right side: <br/>
+<select onclick="" id="rihtSide">
+  <option value="none">None</option>
+  <option value="one">1</option>
+  <option value="n">N</option>
+  <option value="m">M</option>
+</select>
+</select><br/>
+<!--Line colors:<br>
 <select onclick="changeObjectAppearance('attributeType');" id='AttributeLineColor'>
     <option value='#ccefff'>Blue</option>
     <option value='#ddffee'>Green</option>
@@ -15,5 +30,5 @@ Line colors:<br>
     <option value='#ffccff'>Purple</option>
     <option value='#ffffff'>White</option>
     <option value='#000000'>Black</option>
-</select><br>
+</select><br>-->
 <button type='submit' class='submit-button' onclick="changeObjectAppearance('lineType'); setType(form); closeAppearanceDialogMenu();" style='float: none; display: block; margin: 10px auto;'>OK</button>
