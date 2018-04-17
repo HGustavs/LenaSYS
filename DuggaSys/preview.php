f3b7f1a83aaa2de27f8f2b19694ff4406bd50368
a16adala <a16adala.student.his.se>
2018-04-17 14:48:14 +0200

    4532-navBarbugsFix27

--- DuggaSys/preview.php
+++ DuggaSys/preview.php
@@ -293,7 +293,7 @@
                             <span id="h3" onclick="selected();headerVal3()" value="H3">Header 3</span>
                         </div>
 
-                    <span id="boldText" onclick="changeText('**')" title="Bold"><b>B</b></span>
+                    <span id="boldText" onclick="boldText()" title="Bold"><b>B</b></span>
                     <span id="cursiveText" onclick="cursiveText()" title="Italic"><i>i</i></span>
                 </div>
                 <div class="markText">
