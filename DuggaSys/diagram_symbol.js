6b21c2858030b1d5c5eb99d2df5784ff61120baa
a16rasja <a16rasja@student.his.se>
2018-04-17 16:26:06 +0200

    changed fillcolor for the cardinality letters

--- DuggaSys/diagram_symbol.js
+++ DuggaSys/diagram_symbol.js
@@ -722,6 +722,8 @@ this.drawLine = function(x1, y1, x2, y2){
     if((this.cardinality[0].x != null && this.cardinality[0].y != null) ||
         (this.cardinality[1].x != null && this.cardinality[1].y != null)){
         //Updates the x and y position depending on which side the cardinality is on
+        ctx.fillStyle = '#000';
+
         this.cardinality[0].x = x1 > x2 ? x2+10 : x2-10;
         this.cardinality[0].y = y1 > y2 ? y2+10 : y2-10;
         ctx.fillText(this.cardinality[0].value, this.cardinality[0].x, this.cardinality[0].y);
