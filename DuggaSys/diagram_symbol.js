bab3cb21a8f4665eac8b6c82506508b8d040dea6
a16rasja <a16rasja@student.his.se>
2018-04-17 16:32:37 +0200

    Fixed merging issue

--- DuggaSys/diagram_symbol.js
+++ DuggaSys/diagram_symbol.js
@@ -722,7 +722,7 @@ this.drawLine = function(x1, y1, x2, y2){
     if((this.cardinality[0].x != null && this.cardinality[0].y != null) ||
         (this.cardinality[1].x != null && this.cardinality[1].y != null)){
         //Updates the x and y position depending on which side the cardinality is on
-        ctx.fillStyle = '#000';
+        ctx.fillStyle = '#000'; 
 
         this.cardinality[0].x = x1 > x2 ? x2+10 : x2-10;
         this.cardinality[0].y = y1 > y2 ? y2+10 : y2-10;
