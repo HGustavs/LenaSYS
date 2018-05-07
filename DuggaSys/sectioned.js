21d9f1029cc8d4c7aca0d1076e97f394350d52ae
a15magkn <37794262+a15magkn@users.noreply.github.com>
2018-05-07 09:54:22 +0200

    #4584_ver1
    Declared undeclared variables using "use strict".
    "use strict" was later removed.

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -664,7 +664,7 @@ function returnedSection(data) {
 		// Fill section list with information
 		var versionname = "";
 		if (retdata['versions'].length > 0) {
-			for (j = 0; j < retdata['versions'].length; j++) {
+			for (var j = 0; j < retdata['versions'].length; j++) {
 				var itemz = retdata['versions'][j];
 				if (retdata['courseid'] == itemz['cid']) {
 
@@ -677,7 +677,7 @@ function returnedSection(data) {
 			}
 		}
 
-		str = "";
+		var str = "";
 
 		str += "<table class='navheader' style='overflow: hidden; table-layout: fixed;'>"
 			+ "<tr class='trsize nowrap'>"; // This is for anti-stacking buttons
@@ -687,7 +687,7 @@ function returnedSection(data) {
 			var startdate = null;
 			var enddate = null;
 			if (retdata['versions'].length > 0) {
-				for (i = 0; i < retdata['versions'].length; i++) {
+				for (var i = 0; i < retdata['versions'].length; i++) {
 					var item = retdata['versions'][i];
 					if (retdata['courseid'] == item['cid'] && retdata['coursevers'] == item['vers']) {
 						startdate = item['startdate'];
@@ -854,6 +854,7 @@ function returnedSection(data) {
 
 		// hide som elements if to narrow
 		var hiddenInline = "";
+		var showInline = true;
 		if ($(window).width() < 480) {
 			showInline = false;
 			hiddenInline = "none";
@@ -990,7 +991,7 @@ function returnedSection(data) {
 					var submitted;
 					var lastSubmit = null;
 
-					for (jjj = 0; jjj < data['results'].length; jjj++) {
+					for (var jjj = 0; jjj < data['results'].length; jjj++) {
 						var lawtem = data['results'][jjj];
 						if ((lawtem['moment'] == item['lid'])) {
 							grady = lawtem['grade'];
@@ -1166,7 +1167,7 @@ function returnedSection(data) {
 				}
 
 				else if (itemKind == 4) { // Moment
-
+					var strz = "";
 					if (item['gradesys'] == 0) {
 						strz = "";
 					}
