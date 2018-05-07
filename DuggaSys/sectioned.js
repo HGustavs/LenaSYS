6d4bf6c13cdd606a39f1de130889b16545692129
a16linas <>
2018-05-07 08:28:17 +0200

    changed name of variable

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -3,7 +3,7 @@ var retdata;
 var newversid;
 var active_lid;
 var isClickedElementBox = false;
-var duggaCheck;
+var testsAvailable;
 
 // Stores everything that relates to collapsable menus and their state.
 var menuState = {
@@ -1686,7 +1686,7 @@ function fabValidateType(kind) {
 	if (kind == 3){
 		if (retdata['duggor'].length == 0){
 			toggleFabButton();
-			duggaCheck = true;
+			testsAvailable = true;
 			$("#noTestsConfirmBox").css("display", "flex");
 		}
 		else {
@@ -1741,9 +1741,9 @@ $(window).load(function () {
 			} else if (submitButtonDisplay == 'block' && editSectionDisplay == 'flex' && isNameValid() && isTypeValid()) {
 				newItem();
 				showSaveButton();
-			}else if(isTypeValid() && duggaCheck == true){
+			}else if(isTypeValid() && testsAvailable == true){
 				confirmBox("closeConfirmBox");
-				duggaCheck = false;
+				testsAvailable = false;
 			}
 
 		}
