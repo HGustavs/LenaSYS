7f3474cc9636ae9a644aa2605cdf2c9fdd6a718d
Setareh Nori <cetapex@gmail.com>
2018-05-07 13:44:48 +0200

    Some additional space is now added

--- DuggaSys/fileed.php
+++ DuggaSys/fileed.php
@@ -159,7 +159,7 @@ pdoConnect();
         </div>
         <div class="editFileCode">
             <div class="fileText">
-                <textarea id="filecont" oninput="editFile(this.value)" name="filetext" rows="32" cols="79"></textarea>
+                <textarea id="filecont" oninput="editFile(this.value)" name="filetext" ></textarea>
             </div>
         </div>
         <div class="optionButtons">
