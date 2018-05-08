73e7cfd031a2a56acf5b9aba83b65a6571a94e2f
a16rasja <a16rasja@student.his.se>
2018-05-08 14:47:48 +0200

    Fixed problem when loading toolbar

--- DuggaSys/diagram_toolbox.js
+++ DuggaSys/diagram_toolbox.js
@@ -1,11 +1,12 @@
-var val;
+var toolbarState;
 
 function initToolbox(){
     var element = document.getElementById('diagram-toolbar');
     var myCanvas = document.getElementById('myCanvas');
     var bound = myCanvas.getBoundingClientRect();
     element.style.top = (bound.top+"px");
-    val = (localStorage.getItem("toolbarState") != null) ? localStorage.getItem("toolbarState") : 0;
+    toolbarState = (localStorage.getItem("toolbarState") != null) ? localStorage.getItem("toolbarState") : 0;
+    switchToolbar();
     //element.style.height = (bound.bottom-bound.top-200+"px");
     //element.style.height = (400+"px");
 }
@@ -24,26 +25,26 @@ function toggleToolbarMinimize(){
 function switchToolbar(direction){
   var text = ["All", "ER", "UML"];
   if(direction == 'left'){
-    val--;
-    if(val < 0){
-      val = 2;
+    toolbarState--;
+    if(toolbarState < 0){
+      toolbarState = 2;
     }
   }else if(direction == 'right'){
-    val++;
-    if(val > 2){
-      val = 0;
+    toolbarState++;
+    if(toolbarState > 2){
+      toolbarState = 0;
     }
   }
-  document.getElementById('toolbarTypeText').innerHTML = text[val];
-  localStorage.setItem("toolbarState", val)
+  document.getElementById('toolbarTypeText').innerHTML = text[toolbarState];
+  localStorage.setItem("toolbarState", toolbarState);
   //hides irrelevant buttons, and shows relevant buttons
-  if(val == 1){
+  if(toolbarState == 1){
     $(".buttonsStyle").hide();
     $("#linebutton").show();
     $("#attributebutton").show();
     $("#entitybutton").show();
     $("#relationbutton").show();
-  }else if( val == 2){
+  }else if( toolbarState == 2){
     $(".buttonsStyle").hide();
     $("#linebutton").show();
     $("#classbutton").show();
@@ -52,7 +53,7 @@ function switchToolbar(direction){
     $(".buttonsStyle").show();
   }
 
-  document.getElementById('toolbar-switcher').value = val;
+  document.getElementById('toolbar-switcher').value = toolbarState;
 }
 
 
