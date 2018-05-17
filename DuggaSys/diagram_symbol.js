741b18a95055c70b8359a72c2930740d6895379e
b16petma <malmquist45@gmail.com>
2018-05-17 11:40:55 +0200

    test

--- DuggaSys/diagram_symbol.js
+++ DuggaSys/diagram_symbol.js
@@ -266,7 +266,7 @@ function Symbol(kind) {
                 points[this.bottomRight].y = points[this.topLeft].y + relationTemplate.height/2;
             }
             points[this.centerPoint].x = x1 + (points[this.bottomRight].y-points[this.topLeft].y)/2;
-            points[this.centerPoint].y = y1 + (points[this.topLeft].y-points[this.bottomRight].y)/2
+            points[this.centerPoint].y = y1 + (points[this.bottomRight].y-points[this.topLeft].y)/2
             points[this.bottomRight].y = points[this.topLeft].y + (points[this.bottomRight].x - points[this.topLeft].x) * relationTemplate.height/relationTemplate.width;
             // Static size of relation. Makes resizing of relation impossible.
             /*points[this.topLeft].x = points[this.centerPoint].x-relationTemplate.width/2;
