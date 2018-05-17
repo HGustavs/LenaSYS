ab9e7b65ab96ffcbe502850076a103a3ecebd521
b16petma <malmquist45@gmail.com>
2018-05-17 11:43:32 +0200

    test

--- DuggaSys/diagram_symbol.js
+++ DuggaSys/diagram_symbol.js
@@ -265,7 +265,7 @@ function Symbol(kind) {
             if(points[this.bottomRight].y - points[this.topLeft].y < relationTemplate.height/2){
                 points[this.bottomRight].y = points[this.topLeft].y + relationTemplate.height/2;
             }
-            points[this.centerPoint].x = x1 + (points[this.bottomRight].y-points[this.topLeft].y)/2;
+            points[this.centerPoint].x = x1 + (points[this.bottomRight].x-points[this.topLeft].x)/2;
             points[this.centerPoint].y = y1 + (points[this.bottomRight].y-points[this.topLeft].y)/2
             points[this.bottomRight].y = points[this.topLeft].y + (points[this.bottomRight].x - points[this.topLeft].x) * relationTemplate.height/relationTemplate.width;
             // Static size of relation. Makes resizing of relation impossible.
