5ae603e7996b07f8288bde3c55c78016ca0b358a
a16adala <a16adala.student.his.se>
2018-05-22 14:53:54 +0200

    linedash for text in diagram2

--- DuggaSys/diagram_symbol.js
+++ DuggaSys/diagram_symbol.js
@@ -986,7 +986,7 @@ function Symbol(kind) {
         ctx.beginPath();
         if (this.targeted || this.isHovered) {
             ctx.lineWidth = 2;
-            ctx.strokeColor = "F82";
+            ctx.lineDash = "F82";
             ctx.rect(x1, y1, x2-x1, y2-y1);
             ctx.setLineDash([5, 4]);
         }
