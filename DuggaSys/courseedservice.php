f58d9d8ef551885e64b12ebb70f0c3c131b321f6
Filip Olsén <a16filol@student.his.se>
2018-05-22 12:10:17 +0200

    Add version to courses.

--- DuggaSys/courseedservice.php
+++ DuggaSys/courseedservice.php
@@ -73,27 +73,7 @@ if(checklogin()){
 			if(!$query->execute()) {
 				$error=$query->errorInfo();
 				$debug="Error updating entries".$error[2];
-			} else {
-				// Add default groups
-				$courseID = $pdo->lastInsertId();
-				$defaultGroups = array(
-					"I", "II", "III", "IV", "V", "VI", "VII", "VIII",
-					"1", "2", "3", "4", "5", "6", "7", "8",
-					"A", "B", "C", "D", "E", "F", "G", "H",
-				);
-				
-
-				foreach($defaultGroups as $group) {
-					$stmt = $pdo->prepare("INSERT INTO groups(courseID, groupName) VALUES(:courseID, :groupName)");
-					$stmt->bindParam(':courseID', $courseID);
-					$stmt->bindParam(':groupName', $group);
-
-					if (!$stmt->execute()) {
-						$error = $stmt->errorInfo();
-						$debug = "Error adding group " . $error[2];
-					}
-				}
-			}
+			} 
 		}else if(strcmp($opt,"NEWVRS")===0){
 			$query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt);");
 
@@ -107,6 +87,27 @@ if(checklogin()){
 			if(!$query->execute()) {
 				$error=$query->errorInfo();
 				$debug="Error updating entries".$error[2];
+			}else {
+				// Add default groups
+				$defaultGroups = array(
+					"I", "II", "III", "IV", "V", "VI", "VII", "VIII",
+					"1", "2", "3", "4", "5", "6", "7", "8",
+					"A", "B", "C", "D", "E", "F", "G", "H",
+				);
+				
+
+				foreach($defaultGroups as $group) {
+					$stmt = $pdo->prepare("INSERT INTO groups(courseID, vers, groupName) VALUES(:courseID, :vers, :groupName)");
+					
+					$stmt->bindParam(':courseID', $courseID);
+					$stmt->bindParam(':vers', $versid);
+					$stmt->bindParam(':groupName', $group);
+
+					if (!$stmt->execute()) {
+						$error = $stmt->errorInfo();
+						$debug = "Error adding group " . $error[2];
+					}
+				}
 			}
 
 		}else if(strcmp($opt,"UPDATEVRS")===0){
