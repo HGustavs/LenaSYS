8098a73e8f272f169140096595ae6cb3478e36b9
a16rasja <a16rasja@student.his.se>
2018-05-23 15:26:11 +0200

    created a file for appearance menus

---
+++ DuggaSys/diagram_forms.php
@@ -0,0 +1,36 @@
+<?php
+  $form = $_GET['form'];
+
+  //form for attributes
+  if($form == 1){
+
+  }
+  //form for classes
+  else if($form == 2){
+
+  }
+  //form for entities
+  else if($form == 3){
+
+  }
+  //form for figures
+  else if($form == 4){
+
+  }
+  //form for global
+  else if($form == 5){
+
+  }
+  //form for lines
+  else if($form == 6){
+
+  }
+  //form for relations
+  else if($form == 7){
+
+  }
+  //form for text
+  else if($form == 8){
+
+  }
+ ?>
