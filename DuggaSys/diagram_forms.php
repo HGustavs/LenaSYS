315ca62b4773bb834513ef55228c654cc791bbf3
c16carbe <xtheone@live.se>
2018-05-24 09:50:36 +0200

    Modularized the rest of menu types

--- DuggaSys/diagram_forms.php
+++ DuggaSys/diagram_forms.php
@@ -50,8 +50,8 @@
         <option value=\'#ffffff\'>White</option>
         <option value=\'#000000\' selected="true">Black</option>
         </select><br>
-        <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'attributeType\'); setType(); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
         ';
+  $okButton = '<button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'attributeType\'); setType(); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>';
   //form for attributes
   if($form == 1){
       echo'Attribute name:</br>
@@ -65,7 +65,7 @@
       "<option value=\'Composite\'>Composite</option>
       "<option value=\'Drive\'>Derive</option>
       </select></br>
-      '.$backgroundColor.$fontFamily.$fontColor.$textSize.$lineColors;
+      '.$backgroundColor.$fontFamily.$fontColor.$textSize.$lineColors.$okButton;
   }
   //form for classes
   else if($form == 2){
@@ -93,58 +93,7 @@
       <option value=\'Weak\'>Weak</option>
       <option value=\'Strong\' selected="true">Strong</option>
       </select></br>
-      Background color:<br>
-      <select onchange="changeObjectAppearance(\'entityType\');" id =\'symbolColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\' selected="true">White</option>
-      <option value=\'#000000\'>Black</option>
-      </select><br>
-      Font family:<br>
-      <select onchange="changeObjectAppearance(\'entityType\');" id =\'font\'>
-      <option value=\'Arial\' selected="true">Arial</option>
-      <option value=\'Courier New\'>Courier New</option>
-      <option value=\'Impact\'>Impact</option>
-      <option value=\'Calibri\'>Calibri</option>
-      </select><br>
-      Font color:<br>
-      <select onchange="changeObjectAppearance(\'entityType\');" id =\'fontColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\' selected="true">Black</option>
-      </select><br>
-      Text size:<br>
-      <select onchange="changeObjectAppearance(\'entityType\');" id =\'TextSize\'>
-      <option value=\'Tiny\' selected="true">Tiny</option>
-      <option value=\'Small\'>Small</option>
-      <option value=\'Medium\'>Medium</option>
-      <option value=\'Large\'>Large</option>
-      </select><br>
-      Line colors:<br>
-      <select onchange="changeObjectAppearance(\'attributeType\');" id=\'AttributeLineColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\' selected="true">Black</option>
-      </select><br>
-      <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'entityType\'); setType(); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
-      ';
+      '.$backgroundColor.$fontFamily.$fontColor.$textSize.$lineColors.$okButton;
   }
   //form for figures
   else if($form == 4){
@@ -160,19 +109,7 @@
       <option value=\'#BA68C8\'>Purple</option>
       <option value=\'#ffffff\'>White</option>
       <option value=\'#000000\'>Black</option>
-      </select><br>
-      Line color:<br>
-      <select onchange="changeObjectAppearance(\'figureType\');" id=\'figureLineColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\'>Black</option>
-      </select><br>
+      </select><br>'.$lineColors.'
       Opacity:<br>
       <input type="range" id="figureOpacity" oninput="changeObjectAppearance(\'figureType\');" style="width:100%; margin: -2px; padding: 0px;"><br>
       <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'figureType\'); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
@@ -298,96 +235,18 @@
       <option value=\'Weak\'>Weak</option>
       <option value=\'Strong\' selected>Strong</option>
       </select></br>
-      Background color:<br>
-      <select onchange="changeObjectAppearance(\'relationType\');" id=\'symbolColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\' selected="true">White</option>
-      <option value=\'#000000\'>Black</option>
-      </select><br>
-      Font family:<br>
-      <select onchange="changeObjectAppearance(\'relationType\');" id=\'font\'>
-      <option value=\'Arial\' selected>Arial</option>
-      <option value=\'Courier New\'>Courier New</option>
-      <option value=\'Impact\'>Impact</option>
-      <option value=\'Calibri\'>Calibri</option>
-      </select><br>
-      Font color:<br>
-      <select onchange="changeObjectAppearance(\'relationType\');" id=\'fontColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\' selected="true">Black</option>
-      </select><br>
-      Text size:<br>
-      <select onchange="changeObjectAppearance(\'relationType\');" id=\'TextSize\'>
-      <option value=\'Tiny\' selected="true">Tiny</option>
-      <option value=\'Small\'>Small</option>
-      <option value=\'Medium\'>Medium</option>
-      <option value=\'Large\'>Large</option>
-      </select><br>
-      Line colors:<br>
-      <select onchange="changeObjectAppearance(\'attributeType\');" id=\'AttributeLineColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\' selected="true">Black</option>
-      </select><br>
-      <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'relationType\'); setType(form); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
-      ';
+      '.$backgroundColor.$fontFamily.$fontColor.$textSize.$lineColors.$okButton;
   }
   //form for text
   else if($form == 8){
       echo'Text:</br>
       <textarea id="freeText" class="FreeTextTextarea" oninput="changeObjectAppearance(\'textType\')" style="height:100px;resize:none;"></textarea><br>
-      Font family:<br>
-      <select onchange="changeObjectAppearance(\'textType\');" id=\'font\'>
-      "<option value=\'Arial\' selected="true">Arial</option>
-      "<option value=\'Courier New\'>Courier New</option>
-      "<option value=\'Impact\'>Impact</option>
-      "<option value=\'Calibri\'>Calibri</option>
-      </select><br>
-      Font color:<br>
-      <select onchange="changeObjectAppearance(\'textType\');" id=\'fontColor\'>
-      <option value=\'#64B5F6\'>Blue</option>
-      <option value=\'#81C784\'>Green</option>
-      <option value=\'#e6e6e6\'>Grey</option>
-      <option value=\'#E57373\'>Red</option>
-      <option value=\'#FFF176\'>Yellow</option>
-      <option value=\'#FFB74D\'>Orange</option>
-      <option value=\'#BA68C8\'>Purple</option>
-      <option value=\'#ffffff\'>White</option>
-      <option value=\'#000000\' selected="true">Black</option>
-      </select><br>
-      Text alignment:<br>
+      '.$fontFamily.$fontColor
+      .'Text alignment:<br>
       <select onchange="changeObjectAppearance(\'textType\');" id="textAlign">
       <option value="start">Left</option>
       <option value="center" selected="true">Center</option>
       <option value="end">Right</option>
-      </select><br>
-      Text size:<br>
-      <select onchange="changeObjectAppearance(\'textType\');" id=\'TextSize\'>
-      "<option value=\'Tiny\' selected="true">Tiny</option>
-      "<option value=\'Small\'>Small</option>
-      "<option value=\'Medium\'>Medium</option>
-      "<option value=\'Large\'>Large</option>
-      </select><br>
-      <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'textType\'); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
-      ';
+      </select><br>'.$textSize.$okButton;
   }
  ?>
