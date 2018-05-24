bf50f3b4771f9d46b19b3f8e026910f2e0fa3253
c16carbe <xtheone@live.se>
2018-05-24 09:29:00 +0200

    Started modularizing dialog menu items

--- DuggaSys/diagram_forms.php
+++ DuggaSys/diagram_forms.php
@@ -1,6 +1,57 @@
 <?php
   $form = $_GET['form'];
-
+  $backgroundColor = 'Background color:<br>
+        <select onchange="changeObjectAppearance(\'attributeType\');" id=\'symbolColor\'>
+        <option value=\'#64B5F6\'>Blue</option>
+        <option value=\'#81C784\'>Green</option>
+        <option value=\'#e6e6e6\'>Grey</option>
+        <option value=\'#E57373\'>Red</option>
+        <option value=\'#FFF176\'>Yellow</option>
+        <option value=\'#FFB74D\'>Orange</option>
+        <option value=\'#BA68C8\'>Purple</option>
+        <option value=\'#ffffff\' selected="true">White</option>
+        <option value=\'#000000\'>Black</option>
+        </select><br>';
+  $fontFamily = 'Font family:<br>
+        <select onchange="changeObjectAppearance(\'attributeType\');" id=\'font\'>
+        "<option value=\'Arial\' selected="true">Arial</option>
+        "<option value=\'Courier New\'>Courier New</option>
+        "<option value=\'Impact\'>Impact</option>
+        "<option value=\'Calibri\'>Calibri</option>
+        </select><br>';
+  $fontColor = 'Font color:<br>
+          <select onchange="changeObjectAppearance(\'attributeType\');" id=\'fontColor\'>
+          <option value=\'#64B5F6\'>Blue</option>
+          <option value=\'#81C784\'>Green</option>
+          <option value=\'#e6e6e6\'>Grey</option>
+          <option value=\'#E57373\'>Red</option>
+          <option value=\'#FFF176\'>Yellow</option>
+          <option value=\'#FFB74D\'>Orange</option>
+          <option value=\'#BA68C8\'>Purple</option>
+          <option value=\'#ffffff\'>White</option>
+          <option value=\'#000000\' selected="true">Black</option>
+          </select><br>';
+  $textSize = 'Text size:<br>
+        <select onchange="changeObjectAppearance(\'attributeType\');" id=\'TextSize\'>
+        "<option value=\'Tiny\' selected="true">Tiny</option>
+        "<option value=\'Small\'>Small</option>
+        "<option value=\'Medium\'>Medium</option>
+        "<option value=\'Large\'>Large</option>
+        </select><br>';
+  $lineColors = 'Line colors:<br>
+        <select onchange="changeObjectAppearance(\'attributeType\');" id=\'AttributeLineColor\'>
+        <option value=\'#64B5F6\'>Blue</option>
+        <option value=\'#81C784\'>Green</option>
+        <option value=\'#e6e6e6\'>Grey</option>
+        <option value=\'#E57373\'>Red</option>
+        <option value=\'#FFF176\'>Yellow</option>
+        <option value=\'#FFB74D\'>Orange</option>
+        <option value=\'#BA68C8\'>Purple</option>
+        <option value=\'#ffffff\'>White</option>
+        <option value=\'#000000\' selected="true">Black</option>
+        </select><br>
+        <button type=\'submit\' class=\'submit-button\' onclick="changeObjectAppearance(\'attributeType\'); setType(); closeAppearanceDialogMenu();" style=\'float: none; display: block; margin: 10px auto;\'>OK</button>
+        ';
   //form for attributes
   if($form == 1){
       echo'Attribute name:</br>
