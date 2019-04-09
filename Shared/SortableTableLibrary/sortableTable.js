e882a15efd721722a662fa4ced2e766a3c3c58c6
c17julka <c17julka@student.his.se>
2019-04-09 14:18:43 +0200

    Update sortableTable.js

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -437,8 +437,8 @@ function SortableTable(param)
 
           					str += "<td style='white-space:nowrap;' id='"+cellid+"' onclick='clickedInternal(event,this);' class='"+this.tableid+"-"+columnOrder[columnOrderIdx]+"'>"+renderCell(columnOrder[columnOrderIdx],tbl.tblbody[i][columnOrder[columnOrderIdx]],cellid)+"</td>";
 
-                    if (columnOrderIdx <1) {    
-          					mhvstr += "<td style='white-space:nowrap;' id='"+cellid+DELIMITER+"mhv' onclick='clickedInternal(event,this);' class='"+this.tableid+"-"+columnOrder[columnOrderIdx]+"'>"+renderCell(columnOrder[columnOrderIdx],tbl.tblbody[i][columnOrder[columnOrderIdx]],cellid)+"</td>";
+                    if (columnOrderIdx <1) {
+            					mhvstr += "<td style='white-space:nowrap;' id='"+cellid+DELIMITER+"mhv' onclick='clickedInternal(event,this);' class='"+this.tableid+"-"+columnOrder[columnOrderIdx]+"'>"+renderCell(columnOrder[columnOrderIdx],tbl.tblbody[i][columnOrder[columnOrderIdx]],cellid)+"</td>";
                     }
         				}
       			}
