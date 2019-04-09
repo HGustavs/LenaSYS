ae043c3a1852fdbd07f3b872f849707263fedc30
b17nicsv <49198878+b17nicsv@users.noreply.github.com>
2019-04-09 09:18:41 +0200

    testtest

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -14,7 +14,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title id="sectionedPageTitle">Section Editor</title>
+	<title id="sectionedPageTitle">Section Editor hurr durr</title>
 
 	<link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
 	<link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
