7b826f7d57f5516eae80481bfae3a509104c6c2c
a17adajo <a17adajo@student.his.se>
2019-04-11 11:39:31 +0200

    Testing new engine on user table

--- install/SQL/init_db.sql
+++ install/SQL/init_db.sql
@@ -18,7 +18,7 @@ CREATE TABLE user(
 	securityquestionanswer	VARCHAR(256) DEFAULT NULL,
 	requestedpasswordchange	TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
 	PRIMARY KEY (uid)
-) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB;
 
 INSERT INTO user(username,password,newpassword,creator,superuser) values ("Grimling","$2y$12$stG4CWU//NCdnbAQi.KTHO2V0UVDVi89Lx5ShDvIh/d8.J4vO8o8m",0,1,1);
 INSERT INTO user(username,password,newpassword,creator) values ("Toddler","$2y$12$IHb86c8/PFyI5fa9r8B0But7rugtGKtogyp/2X0OuB3GJl9l0iJ.q",0,1); /* Password is Kong */
