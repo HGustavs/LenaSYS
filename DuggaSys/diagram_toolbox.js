75b949ff11eb9b186f8abfae9f7c64d1715c4236
a17celal <a17celal@student.his.se>
2019-04-11 10:00:43 +0200

    Added comments
    Added the comments that should have been in the last merge

--- DuggaSys/diagram_toolbox.js
+++ DuggaSys/diagram_toolbox.js
@@ -40,7 +40,7 @@ function toggleToolbarLayout(){
     }
 }
 
-//function for switching the toolbar state (All, ER, UML)
+//function for switching the toolbar state (All, ER, UML), not sure what the numbers 0 an 3 mean
 function switchToolbar(direction){
   var text = ["All", "ER", "UML", "Free"];
   if(direction == 'left'){
@@ -57,7 +57,7 @@ function switchToolbar(direction){
   document.getElementById('toolbarTypeText').innerHTML = text[toolbarState];
   localStorage.setItem("toolbarState", toolbarState);
   //hides irrelevant buttons, and shows relevant buttons
-  if(toolbarState == 1){
+  if(toolbarState == 1){ // 1=shows ER alternatives in the toolbar
     $(".toolbar-drawer").hide();
     $("#drawerTools").show();
     $("#drawerCreate").show();
@@ -71,7 +71,7 @@ function switchToolbar(direction){
     $("#attributebutton").show();
     $("#entitybutton").show();
     $("#relationbutton").show();
-  }else if( toolbarState == 2){
+  }else if( toolbarState == 2){ // 2=shows UML alternatives in the toolbar
     $(".toolbar-drawer").hide();
     $("#drawerTools").show();
     $("#drawerCreate").show();
@@ -83,7 +83,7 @@ function switchToolbar(direction){
     $(".buttonsStyle").hide();
     $("#linebutton").show();
     $("#classbutton").show();
-  }else if(toolbarState == 3){
+  }else if(toolbarState == 3){ // 3=shows free alternatives in the toolbar
     $(".toolbar-drawer").hide();
     $("#drawerDraw").show();
     $("#drawerUndo").show();
@@ -94,7 +94,7 @@ function switchToolbar(direction){
     $("#squarebutton").show();
     $("#drawfreebutton").show();
   }
-  else{
+  else{ // shows all alternatives in the toolbar
     $(".toolbar-drawer").show();
     $(".label").show();
     $(".buttonsStyle").show();
