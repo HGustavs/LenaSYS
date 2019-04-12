589bf3260e97363af098421a97f7d0de95151653
c17julka <c17julka@student.his.se>
2019-04-12 13:08:19 +0200

    Update sectioned.js

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -734,7 +734,7 @@ function returnedSection(data) {
           kk = 0;
 
         } else if (itemKind === 2) {
-          str += "<td class='example item style='hover {background.color:green;}' " + hideState + "' placeholder='" + momentexists + "' id='I" + item['lid'] + "' ";
+          str += "<td class='example item' style='background-color:green;' " + hideState + "' placeholder='" + momentexists + "' id='I" + item['lid'] + "' ";
           kk++;
 
         } else if (itemKind === 3) {
