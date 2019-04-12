89bfe4636ad1c3b8e653c324dfe267c8bc6f01b4
b17annni <b17annni@users.noreply.github.com>
2019-04-12 11:42:12 +0200

    Update duggaed.js

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -95,6 +95,10 @@ function selectDugga(qid) {
 }
 
 function updateDugga() {
+var dead1 ="";
+var dead2 ="";
+var dead3 ="";
+
 	var did = $("#did").val();
 	var nme = $("#name").val();
 	var autograde = $("#autograde").val();
@@ -110,6 +114,7 @@ function updateDugga() {
       jsondeadline.deadline1=deadline;
       jsondeadline.comment1=$("#deadlinecomments1").val();
   }else{
+		dead1 = "1";
       deadline="UNK";
       jsondeadline.deadline1="";
       jsondeadline.comment1="";
@@ -119,6 +124,7 @@ function updateDugga() {
       jsondeadline.deadline2=$("#deadline2").val()+" "+$("#deadlinet2").val()+":"+$("#deadlinem2").val();
       jsondeadline.comment2=$("#deadlinecomments2").val();
   }else{
+		dead2="2";
       jsondeadline.deadline2="";
       jsondeadline.comment2="";
   }
@@ -127,12 +133,13 @@ function updateDugga() {
       jsondeadline.deadline3=$("#deadline3").val()+" "+$("#deadlinet3").val()+":"+$("#deadlinem3").val();
       jsondeadline.comment3=$("#deadlinecomments3").val();
   }else{
+		dead3="3";
       jsondeadline.deadline3="";
       jsondeadline.comment3="";
 	}
 	
 	if( $("#deadline1").val()!="" || $("#deadline2").val()!="" || $("#deadline3").val()!=""){
-			alert("Missing");
+			alert("Missing: " + dead1 + " " + dead2 + " " + dead3);
 	}
 
   jsondeadline=JSON.stringify(jsondeadline);
