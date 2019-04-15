cec4964d5b59d62c78b2d583025b66a0f06c975a
Anton Erikssen <a17anter@student.his.se>
2019-04-15 16:10:30 +0200

    Update sectioned.js

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -931,8 +931,8 @@ function returnedSection(data) {
           //FIXME
           // create a warning if the dugga is submitted after the set deadline
           if ((status === "pending") && (dateTimeSubmitted > deadline)) {
-            str += "<td style='width:25px;'><img style='width:25px; padding-top:3px' title='This dugga is not guaranteed to be marked due to submition after deadline.' src='../Shared/icons/warningTriangle.svg'/></td>";
             if (hasGraceTimeExpired(myDate, deadline, dateTimeSubmitted)) {
+              str += "<td style='width:25px;'><img style='width:25px; padding-top:3px' title='This dugga is not guaranteed to be marked due to submition after deadline.' src='../Shared/icons/warningTriangle.svg'/></td>";
             }
           }
           else if (status === "pending") {
@@ -1481,7 +1481,7 @@ function hasGraceTimeExpired(myDate, deadline, dateTimeSubmitted) {
   //console.log("Submit dagen är: " , parameterMyDate.getDay(), " timmen är: ", parameterMyDate.getHours(), " minuten är: ", parameterMyDate.getMinutes());
   var graceDeadline = dateTimeSubmitted;
   var testDeadline = new Date(deadline);
-  console.log(testDeadline);
+  console.log(dateTimeSubmitted);
   if (testDeadline.getHours() >= 17) {
     graceDeadline.setHours(7);
     graceDeadline.setMinutes(59);
