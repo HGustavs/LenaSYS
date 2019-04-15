4868b099750ca9737989ebe064d6a49f27269bd7
Mathias Johansson <c17matjo@github.com>
2019-04-15 09:19:49 +0200

    Update checkboxes to work after commit 3d30fb1f

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -811,7 +811,7 @@ function toggleGrid() {
     } else {
         snapToGrid = false;
     }
-    setCheckbox($("a:contains('Snap to grid')"), snapToGrid);
+    setCheckbox($("span:contains('Snap to grid')"), snapToGrid);
 }
 
 function toggleVirtualA4(){
@@ -1249,7 +1249,7 @@ function debugMode() {
         ghostingCrosses = true;
     }
     updateGraphics();
-    setCheckbox($("a:contains('Developer mode')"), !ghostingCrosses);
+    setCheckbox($("span:contains('Developer mode')"), !ghostingCrosses);
 }
 
 /******************************************************************************************
