7e9371f55270f74064b061a5fbb0b35fc11ec55c
pontus göth <37792222+a17pongo@users.noreply.github.com>
2019-04-16 15:55:42 +0200

    Update sortableTable.js

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -503,7 +503,6 @@ function SortableTable(param) {
     localStorage.setItem(this.tableid + DELIMITER + "filtercolnames", JSON.stringify(columnfilter));
 
     this.reRender();
-  }
 
   this.toggleSortStatus = function (col, kind) {
     // Assign currently active table
