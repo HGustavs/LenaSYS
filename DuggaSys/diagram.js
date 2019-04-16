e995e38a009fd79c9185a9d442d5b0e808c54147
a17okaoz <a17okaoz@student.his.se>
2019-04-16 14:08:59 +0200

    Update diagram.js

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2083,8 +2083,8 @@ function pointDistance(point1, point2) {
 
 function mousemoveevt(ev, t) {
     // Get canvasMouse coordinates for both X & Y.
-    canvasMouseX = (ev.clientX - canvas.offsetLeft) * (1 / zoomValue)
-    canvasMouseY = -(ev.clientY - canvas.offsetTop) * (1 / zoomValue)
+    canvasMouseX = Math.round(ev.clientX - canvas.offsetLeft) * (1 / zoomValue)
+    canvasMouseY = Math.round(-(ev.clientY - canvas.offsetTop) * (1 / zoomValue))
     // Call reWrite() to update the canvasMouseX & canvasMouseY on the page.
     reWrite();
     xPos = ev.clientX;
