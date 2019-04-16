8ad6886dc7dad7342f076d551682671a30a11132
Tobias Frenger <a16tobfr@student.his.se>
2019-04-16 14:27:08 +0200

    Update diagram.js
    added .toFixed(2) so it would definitely print out two decimals

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1368,8 +1368,8 @@ function decimalPrecision(value, precision){
 function reWrite() {
     document.getElementById("valuesCanvas").innerHTML = "<p><b>Zoom:</b> "
      + Math.round((zoomValue * 100)) + "%" + "   |   <b>Coordinates:</b> "
-     + "X=" + decimalPrecision(canvasMouseX, 1)
-     + " & Y=" + decimalPrecision(canvasMouseY, 1) + "</p>";
+     + "X=" + decimalPrecision(canvasMouseX, 2).toFixed(2)
+     + " & Y=" + decimalPrecision(canvasMouseY, 2).toFixed(2) + "</p>";
 }
 //----------------------------------------
 // Renderer
@@ -2088,8 +2088,8 @@ function pointDistance(point1, point2) {
 
 function mousemoveevt(ev, t) {
     // Get canvasMouse coordinates for both X & Y.
-    canvasMouseX = (ev.clientX - canvas.offsetLeft) * (1 / zoomValue)
-    canvasMouseY = -(ev.clientY - canvas.offsetTop) * (1 / zoomValue)
+    canvasMouseX = (ev.clientX - canvas.offsetLeft) * (1 / zoomValue);
+    canvasMouseY = -(ev.clientY - canvas.offsetTop) * (1 / zoomValue);
     // Call reWrite() to update the canvasMouseX & canvasMouseY on the page.
     reWrite();
     xPos = ev.clientX;
