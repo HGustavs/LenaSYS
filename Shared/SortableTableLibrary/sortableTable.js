3f8e9d767c2d4651e495a7cda9ecb8f8a1764c36
a17joaap <a17joaap@student.his.se>
2019-04-17 18:09:00 +0200

    Fixed sorting for requestedpasswordchange

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -47,6 +47,7 @@ function sortableInternalSort(a, b) {
 	var ret = 0;
 	//var colname = sortableTable.currentTable.getKeyByValue();
 	var colname = sortableTable.currentTable.getSortcolumn();
+	
 	if ((sortableTable.currentTable.sortkind % 2) == 0) {
 		//alert("Compare: "+a+" "+b);
 		ret = newCompare(a[colname], b[colname]);
@@ -723,8 +724,6 @@ function newCompare(firstCell, secoundCell) {
 	var firstCellTemp;
 	var secoundCellTemp;
 
-	console.log(firstCell);
-	console.log(secoundCell);
 	//Check if the cell is a valid cell in the table.
 	if (colOrder.includes(col)) {
 		//Check if the cells contains a date object.
@@ -750,9 +749,23 @@ function newCompare(firstCell, secoundCell) {
 				}
 			}
 		}
+		
+		if (col === "requestedpasswordchange") {
+			firstCellTemp = JSON.parse(firstCell);
+			secoundCellTemp = JSON.parse(secoundCell);
+			a = firstCellTemp.requested;
+			b = secoundCellTemp.requested;
+			if (status == 0) {
+				a > b ? val = 1 : a < b ? val = -1 : val = 0;
+			} else {
+				a < b ? val = 1 : a > b ? val = -1 : val = 0;
+			}
+			return val;
+		}
 
 		firstCellTemp = $('<div/>').html(firstCellTemp).text();
 		secoundCellTemp = $('<div/>').html(secoundCellTemp).text();
+
 		if (status == 0) {
 			val = secoundCellTemp.toLocaleUpperCase().localeCompare(firstCellTemp.toLocaleUpperCase());
 		} else {
