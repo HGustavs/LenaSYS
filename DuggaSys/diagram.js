9c81e1155105a33f9962ebef93e83ef97153b3db
Oscar Näzell <a14oscna@student.his.se>
2019-04-17 16:54:01 +0200

    further styling test

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -860,7 +860,7 @@ function toggleVirtualA4() {
         toggleA4 = true;
         updateGraphics();
     }
-    $(".drop-down-option:contains('Display Virtual A4')").parent().toggleClass("drop-down-item drop-down-item-disabled");
+    $("#a4-holes-item").toggleClass("drop-down-item drop-down-item-disabled");
     setCheckbox($(".drop-down-option:contains('Display Virtual A4')"), toggleA4);
 }
 
