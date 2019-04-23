ff1c4e6cbc3d20a8ababe30c3c6ea1010652ddff
a17johpe <a17johpe@student.his.se>
2019-04-23 13:56:22 +0200

    Test code

--- DuggaSys/diagram_objects.js
+++ DuggaSys/diagram_objects.js
@@ -1100,6 +1100,9 @@ function Symbol(kind) {
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
         ctx.stroke();
+
+        // TEST
+        console.log(this.getConnectedObjects());
     }
 
     //---------------------------------------------------------------
