48302f29108ca2a6557f140511fb491a1af44215
Oscar <a17oscno@student.his.se>
2019-04-23 13:29:41 +0100

    debugging

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -945,7 +945,6 @@ function smartSearch(splitSearch, row)
         }
       }else {
         if(sortingType <= sortingValue) {
-          console.log("den ena är mindre än den andra!");
           for (colname in row) {
             if (colname == "lid:" + row[lid].lid) {
               var name = "";
@@ -968,6 +967,9 @@ function smartSearch(splitSearch, row)
 }
 
 function rowFilter(row) {
+
+console.log(row);
+
 	// Custom filters that remove rows before an actual search
 	if (!filterList["showTeachers"] && row["FnameLnameSSN"]["access"].toUpperCase().indexOf("W") != -1)
 		return false;
