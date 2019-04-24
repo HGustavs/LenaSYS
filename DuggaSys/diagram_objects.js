414e781eccd79b1b852f0c3652599c3a35f76a17
Tobias Frenger <a16tobfr@student.his.se>
2019-04-24 10:39:04 +0200

    Update diagram_objects.js
    added clarifying comment

--- DuggaSys/diagram_objects.js
+++ DuggaSys/diagram_objects.js
@@ -1147,6 +1147,7 @@ function Symbol(kind) {
                             // checking if the line Y coordinate is the same as the relations middle Y coordinate.
                             if (dtly == relationMidYPoints[j] || dbly == relationMidYPoints[j]) {
                                 if (dtlx == relationMidYPoints[c] || dtrx == relationMidYPoints[c]) {
+                                    // Going through the array even if empty since it otherwise requires that an attribute is connected to the entity in all cases
                                     for (let y = 0; y <= attributeMidPoint.length; y++) {
                                         for (let k = 0; k <= attributeMidPoint.length; k++) {
                                             // Making sure that lines between relations and attributes aren't set to forced.
