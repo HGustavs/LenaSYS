0e2d4ffe6a7dc7ffbce93da52bfbd30cd20b8a2f
Ludwig <a17ludca@student.his.se>
2019-04-24 14:57:45 +0200

    test

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1278,13 +1278,14 @@ function drawSwimlanes() {
       tempNumb = 0;
       addNumb = 2;
     }
-    str += "<rect x='" + (i * (weekwidth + addNumb)) + "' y='" + (15) + "' width='" + (weekwidth+addNumb+tempNumb) + "' height='" + (weekheight * (deadlineEntries.length + 1)) + "' ";
+    var test = weekwidth+addNumb;
+    str += "<rect x='" + (i * test) + "' y='" + (15) + "' width='" + (test+tempNumb) + "' height='" + (weekheight * (deadlineEntries.length + 1)) + "' ";
     if ((i % 2) == 0) {
       str += "fill='#ededed' />";
     } else {
       str += "fill='#ffffff' />";
     }
-    str += "<text x='" + ((i * weekwidth) + (weekwidth * 0.5) + addNumb) + "' y='" + (33) + "' font-family='Arial' font-size='12px' fill='black' text-anchor='middle'>" + (i + 1) + "</text>";
+    str += "<text x='" + ((i * weekwidth) + (test * 0.5) + (tempNumb * 0.5)) + "' y='" + (33) + "' font-family='Arial' font-size='12px' fill='black' text-anchor='middle'>" + (i + 1) + "</text>";
   }
 
   for (var i = 1; i < (deadlineEntries.length + 2); i++) {
