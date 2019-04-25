2a1c0e8087cbf4556d3e0cd26a9836f6aa5f071d
c17julka <c17julka@student.his.se>
2019-04-25 13:50:29 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -51,13 +51,13 @@ function setup() {
 	filt += "</div>";
 	filt += "</span></td>";
 
-  filt += "<td id='searchBar' class='navButt'>";
-  filt += "<input id='searchinput' type='text' name='search' placeholder='Search..'";
-  filt += "onkeyup='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();'/>";
-  filt += "<button id='searchbutton' class='switchContent'";
-  filt += "onclick='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();' type='button'>";
-  filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>";
-  filt += "</button></td>";
+  filt += `<td id="searchBar" class="navButt">`;
+  filt += `<input id="searchinput" type="text" name="search" placeholder="Search..""`;
+  filt += `onkeyup="searchterm=document.getElementById("searchinput").value;searchKeyUp(event);myTable.reRender();"/>`;
+  filt += `<button id="searchbutton" class="switchContent"`;
+  filt += `onclick="searchterm=document.getElementById("searchinput").value;searchKeyUp(event);myTable.reRender();" type="button">`;
+  filt += `<img id="lookingGlassSVG" style="height:18px;" src="../Shared/icons/LookingGlass.svg"/>`;
+  filt += `</button></td>`;
 
 	$("#menuHook").before(filt);
 
