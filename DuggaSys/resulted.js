55d37a659ae55ca29c62b7f750b524adc5327cb6
c17julka <c17julka@student.his.se>
2019-04-25 13:31:47 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -54,8 +54,8 @@ function setup() {
   var sc = document.getElementById('searchinput');
 
   filt += "<td id='searchBar' class='navButt'>";
-  filt += "<input id='searchinput' type='text' name='search' placeholder='Search..'onkeyup='"+sc+".value;searchKeyUp(event);myTable.reRender();'/>";
-  filt += "<button id='searchbutton' class='switchContent'onclick='"+sc+".value;searchKeyUp(event);myTable.reRender();' type='button'>";
+  filt += "<input id='searchinput' type='text' name='search' placeholder='Search..'onkeyup='searchterm="+sc+".value;searchKeyUp(event);myTable.reRender();'/>";
+  filt += "<button id='searchbutton' class='switchContent'onclick='searchterm="+sc+".value;searchKeyUp(event);myTable.reRender();' type='button'>";
   filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>";
   filt += "</button></td>";
 
