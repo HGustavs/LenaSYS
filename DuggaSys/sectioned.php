4fe3325db63c5beab06ae857e8ede49ec7c9703b
b17annni <b17annni@users.noreply.github.com>
2019-04-25 14:29:41 +0200

    Update sectioned.php

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -35,7 +35,7 @@
 	<!-- content START -->
 	<div id="content">
 
-		<div id='TopMenuStatic' style="display:none; position: absolute; top: 0px; left 50px;">
+		<div id='TopMenuStatic' style="display:none; position: absolute; top: 0px; left 5px;">
 
 			<table class='navheader' style='overflow: hidden; table-layout: fixed;'>
 				<tr class='trsize nowrap'>
