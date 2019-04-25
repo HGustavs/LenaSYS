4b4c2f3a0ede41ca0dd66e354d48344c0b0e93bc
Oscar <a17oscno@student.his.se>
2019-04-25 10:49:29 +0100

    testing

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -939,9 +939,13 @@ function smartSearch(splitSearch, row) {
 
 //----------------------------------------------------------------
 // rowFilter <- Callback function that filters rows in the table
-//----------------------------------------------------------------
+var rowFilterVariable = null;
 function rowFilter(row) {
-  console.log(tblbody);
+  if(rowFilterVariable == null){
+    rowFilterVariable = myTable.tblbody;
+  }
+
+  console.log(rowFilterVariable);
 	// Custom filters that remove rows before an actual search
 	if (!filterList["showTeachers"] && row["FnameLnameSSN"]["access"].toUpperCase().indexOf("W") != -1)
 		return false;
