21310cca5d4a52347973899ceb7960f71ab795ac
c17julka <c17julka@student.his.se>
2019-04-25 10:58:16 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -45,18 +45,17 @@ function setup() {
 	filt += "</div>";
 	filt += "</span></td>";
 
-    filt += "<div id='searchBar' class='navButt' style='test-align:right;margin-bottom:15px;'>";
-    filt += "<input id='searchinput' type='text' name='search' placeholder='Search..' onkeyup='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();'/>";
-    filt += "<button id='searchbutton' class='switchContent' onclick='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();' type='button'>";
-    filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>";
-    filt += "</button></div>";
-    
-	filt += "<td id='filter' class='navButt'><span class='dropdown-container' onmouseover='hovers();'>";
-	filt += "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
-	filt += "<div id='dropdowns' class='dropdown-list-container'>";
-	filt += "</div>";
-	filt += "</span></td>";
+	// filt += "<td id='filter' class='navButt'><span class='dropdown-container' onmouseover='hovers();'>";
+	// filt += "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
+	// filt += "<div id='dropdowns' class='dropdown-list-container'>";
+	// filt += "</div>";
+	// filt += "</span></td>";
 
+  filt += "<div id='searchBar' class='navButt' style='test-align:right;margin-bottom:15px;'>";
+  filt += "<input id='searchinput' type='text' name='search' placeholder='Search..' onkeyup='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();'/>";
+  filt += "<button id='searchbutton' class='switchContent' onclick='searchterm=document.getElementById('searchinput').value;searchKeyUp(event);myTable.reRender();' type='button'>";
+  filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>";
+  filt += "</button></div>";
 
 	$("#menuHook").before(filt);
 
