a7a0444749b4d0af991b87a5fea43f097cedc6e7
Anton Erikssen <a17anter@student.his.se>
2019-04-26 11:35:18 +0200

    Update resultedservice.php

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -86,9 +86,8 @@ if($requestType == "mail"){
 			$error=$mailQuery->errorInfo();
 			$debug="Error reading user entries".$error[2];
 		}
-		if ($row = $mailQuery->fetch(PDO::FETCH_ASSOC)) {
-			array_push($emailsArray[$row['email']]);
-		}
+
+			array_push($emailsArray,$mailQuery['email']);
 //		array_push($groups[$row['groupKind']],$row['groupVal']);
 	}
 
