63aa1fa3f9bf41bb6fe1433165ab566180c635df
a17jonro <a17jonro@student.his.se>
2019-04-26 12:04:11 +0200

    Changed resetViewtoOrigin function to accomodate canvas changes

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -75,8 +75,6 @@ var toggleA4Holes = false;          // toggle if a4 holes are drawn
 var crossStrokeStyle1 = "#f64";     // set the color for the crosses.
 var crossFillStyle = "#d51";
 var crossStrokeStyle2 = "#d51";
-var distanceMovedX = 0;             // the distance moved since last use of resetViewToOrigin()
-var distanceMovedY = 0;
 var minEntityX = 100;               //the minimum size for an Enitny are set by the values seen below.
 var minEntityY = 50;
 var hashUpdateTimer = 5000;         // set timer varibale for hash and saving
@@ -1029,8 +1027,6 @@ function canvasSize() {
     canvas.setAttribute("height", heightWindow);
     ctx.clearRect(sx, sy, widthWindow, heightWindow);
     ctx.translate(sx, sy);
-    distanceMovedX = -sx;
-    distanceMovedY = -sy;
     ctx.scale(1, 1);
     ctx.scale(zoomValue, zoomValue);
 }
@@ -1061,13 +1057,12 @@ function updateGraphics() {
 }
 
 //---------------------------------------------------------------------------------
-// resetViewToOrigin: moves the view to origo based on movement done in the canvas 
+// resetViewToOrigin: moves the view to origo 
 //---------------------------------------------------------------------------------
 
 function resetViewToOrigin(){
-    ctx.translate(distanceMovedX, distanceMovedY);
-    distanceMovedX = 0;
-    distanceMovedY = 0;
+    origoOffsetX = 0;
+    origoOffsetY = 0;
     updateGraphics();
 }
 
