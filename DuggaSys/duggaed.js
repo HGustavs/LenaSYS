84a3c0b5c13f0148cb7af4bdaf8b9f9140e49737
c17julka <c17julka@student.his.se>
2019-04-26 10:15:03 +0200

    Update duggaed.js

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -24,7 +24,7 @@ function setup() {
   var filt = "";
 
   filt += "<td id='testSearchContainer' class='navButt'>"
-  filt += "<input id='duggaSearch' class ='searchField' type='search' placeholder='Search...' onkeyup='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'onsearch='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'/>"
+  filt += "<input id='duggaSearch' type='search' placeholder='Search...' onkeyup='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'onsearch='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'/>"
   filt += "<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>"
   filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>"
   filt += "</button>"
@@ -412,13 +412,13 @@ function returnedDugga(data) {
 	if (data['writeaccess']) {
 		$('#quiz').show();
 		$('.fixed-action-button').show();
-		$('.searchField').show();
+		$('#duggaSearch').show();
 		$('#searchbutton').show();
 	}
 	else {
 		$('#quiz').hide();
 		$('.fixed-action-button').hide();
-		$('.searchField').hide();
+		$('#duggaSearch').hide();
 		$('#searchbutton').hide();
 			changeURL("sectioned.php?courseid=" + querystring['cid'] + "&coursename=" + data.coursename + "&coursevers="
 				+ querystring['coursevers'] + "");
