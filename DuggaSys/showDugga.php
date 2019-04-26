16b795cd904a51fca5d11e4dccb40e3ff4cdc24d
c17alepe <c17alepe@student.his.se>
2019-04-26 18:53:53 +0200

    Update showDugga.php

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -207,7 +207,7 @@
 
   <!--<div id='previewpopover' class='previewPopover' style='display:none;'>-->
   <div id='previewpopover' class='loginBoxContainer' style='display:none; align-items:stretch;'>
-    <div style='width:100% max-height:none;' class="loginBox">
+    <div style='width:100%; max-height:none;' class="loginBox">
     		<div class='loginBoxheader'>
     			<h3 style='width:100%;' id='Nameof'>Submission and feedback view</h3><div class='cursorPointer' onclick='closeWindows();'>x</div>
     		</div>
