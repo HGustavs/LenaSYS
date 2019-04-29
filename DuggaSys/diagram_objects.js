18a869940fae2693055d9b5c2b427b4feb826af0
a17johpe <a17johpe@student.his.se>
2019-04-29 14:32:20 +0200

    Added condition to UML line drawing

--- DuggaSys/diagram_objects.js
+++ DuggaSys/diagram_objects.js
@@ -1398,7 +1398,7 @@ function Symbol(kind) {
                 breakpointEndX = x2;
                 }
 
-                if(Math.abs(x1 - x2) < 60) {
+                if((Math.abs(x1 - x2) < 60) && (Math.abs(y1 - y2) < 60)) {
                     breakpointStartX = x1;
                     breakpointStartY = y1;
                     breakpointEndX = x2;
