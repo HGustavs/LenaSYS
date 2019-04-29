f90544d439ea45d5796a93652c2d035a06d924e4
Oscar <a17oscno@student.his.se>
2019-04-29 11:16:55 +0100

    testing

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -77,7 +77,7 @@ if($requestType == "mail"){
 
 	// foreach($visibleUserIDs as $id)
 	// {
-			$studentID = "c92cober";
+			$studentID = $visibleUserIDs[0];
 			$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentID");
 
 			$mailQuery->bindParam(':studentID', $studentID);
