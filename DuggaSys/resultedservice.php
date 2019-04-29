36388954025b89967f80de890713eae9270f1e79
Oscar <a17oscno@student.his.se>
2019-04-29 11:38:09 +0100

    testing

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -78,7 +78,9 @@ if($requestType == "mail"){
 	// foreach($visibleUserIDs as $id)
 	// {
 			$studentID = $visibleUserIDs[0];
-			$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentid");
+			//$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentid");
+
+			$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers");
 
 			$mailQuery->bindParam(':studentid', $studentID);
 			$mailQuery->bindParam(':cid', $courseid);
