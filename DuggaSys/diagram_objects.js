07f68454bf3b41b0051183ad89d0028f807c11e3
a17johpe <a17johpe@student.his.se>
2019-04-29 14:39:48 +0200

    Added comment

--- DuggaSys/diagram_objects.js
+++ DuggaSys/diagram_objects.js
@@ -1398,6 +1398,7 @@ function Symbol(kind) {
                 breakpointEndX = x2;
                 }
 
+                // If start and end points are too close to each other, set breakpoints to same as start and end points
                 if((Math.abs(x1 - x2) < 60) || (Math.abs(y1 - y2) < 60)) {
                     breakpointStartX = x1;
                     breakpointStartY = y1;
