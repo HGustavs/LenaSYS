005f40955fbc666c171f9da18717efd5828e4490
Oscar <a17oscno@student.his.se>
2019-04-29 11:25:19 +0100

    reverting test

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -84,7 +84,7 @@ if($requestType == "mail"){
 			$mailQuery->bindParam(':cid', $courseid);
 			$mailQuery->bindParam(':cvers', $coursevers);
 
-			if($mailQuery->execute()) {
+			if(!$mailQuery->execute()) {
 				echo json_encode($mailQuery);
 				$error=$mailQuery->errorInfo();
 				$debug="Error reading user entries".$error[2];
