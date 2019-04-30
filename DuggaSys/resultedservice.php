516e77f0519c5e9939b8e75f0e17bc622a18cbad
Anton Erikssen <a17anter@student.his.se>
2019-04-30 15:06:49 +0200

    Update resultedservice.php

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -88,6 +88,8 @@ if($requestType == "mail"){
 			$error=$mailQuery->errorInfo();
 			$debug="Error reading user entries".$error[2];
 		}
+
+		var_dump($mailQuery);
 	//	$test += $count
 		array_push($emailsArray, "hej");
 	}
