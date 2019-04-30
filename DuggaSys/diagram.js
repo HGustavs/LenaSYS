5fef4b6f7554443623d29a57d223b3850669f20c
a16jenja <49199287+a16jenja@users.noreply.github.com>
2019-04-30 14:31:14 +0200

    #6377 Fixed issue with cursor pointer when drawing a line between entities
    The mouse cursor was "all-scroll" before but now it's a default mouse pointer when trying to draw lines between entities.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -748,7 +748,7 @@ window.addEventListener("mousemove", function()
     if (indexOfHoveredObject != -1) {
             for (let i = 0; i < diagram.length; i++) {
                 // If the symbol is a line or an umlline or the object is locked the cursor pointer will remain default, otherwise it's of type all-scroll
-                if (diagram[indexOfHoveredObject].symbolkind != 4 && !diagram[indexOfHoveredObject].locked && diagram[indexOfHoveredObject].symbolkind != 7) {
+                if (diagram[indexOfHoveredObject].symbolkind != 4 && !diagram[indexOfHoveredObject].locked && diagram[indexOfHoveredObject].symbolkind != 7 && uimode != "CreateLine") {
                     canvas.style.cursor = "all-scroll";
                 }
             }
