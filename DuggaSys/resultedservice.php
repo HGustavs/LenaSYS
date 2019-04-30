77dccb0c066e06bb2eee7f36c88b15935336c3ef
Anton Erikssen <a17anter@student.his.se>
2019-04-30 10:03:48 +0200

    Update resultedservice.php

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -85,14 +85,14 @@ if($requestType == "mail"){
 
 // //	 foreach($visibleUserIDs as $id)
 // //	 {
-					$studentID = $visibleUserIDs[0];
-			//		$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentid");
- 					$mailQuery = $pdo->prepare("SELECT email FROM user");
+//					$studentID = $visibleUserIDs[0];
+//					$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentid");
+// 			$mailQuery = $pdo->prepare("SELECT user.email FROM user");
 //
 //
-				//	$mailQuery->bindParam(':studentid', $studentID);
-				//	$mailQuery->bindParam(':cid', $courseid);
-				//	$mailQuery->bindParam(':cvers', $coursevers);
+//					$mailQuery->bindParam(':studentid', $studentID);
+//					$mailQuery->bindParam(':cid', $courseid);
+//					$mailQuery->bindParam(':cvers', $coursevers);
 //
 // 			if (!$mailQuery->execute() {
 // 				echo json_encode($mailQuery);
@@ -101,14 +101,14 @@ if($requestType == "mail"){
 // 			}
 //
 // 			//$emailString += $mailQuery + "; ";
- 	array_push($emailsArray,$mailQuery);
+// 	array_push($emailsArray,$mailQuery);
 // //	}
 // 	//		array_push($groups[$row['groupKind']],$row['groupVal']);
 //
 // 	// Seperates the emails with a ;.
- 			$implodedEmails = implode('; ',$emailsArray);
+// 			$implodedEmails = implode('; ',$emailsArray);
 // 	// Returns the emails in a string representation.
- 			echo json_encode($implodedEmails);
+ 			echo json_encode($visibleUserIDs);
 	} else {
 
 //------------------------------------------------------------------------------------------------
