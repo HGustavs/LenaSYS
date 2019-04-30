28509be6fd7cf087ca744f73e6e1c68ab2d1fb0c
aliceanglesjo <a17alian@student.his.se>
2019-04-30 11:16:32 +0200

    Title edited
    Registered and version ID removed, course code added.
    
    Registered seems to be refering to if the course is registered+

--- DuggaSys/courseed.js
+++ DuggaSys/courseed.js
@@ -379,9 +379,9 @@ function returnedCourse(data)
       		} else {
         		str += "<div class='ellipsis' style='margin-right:15px;'>";
 				if(item['registered'] == true || uname=="Guest") {
-          			str += "<span style='margin-right:15px;'><a class='" + textStyle + "' href='sectioned.php?courseid=" + item['cid'] + "&coursename=" + item['coursename'] + "&coursevers=" + item['activeversion'] + "' title='\"" + item['coursename'] + "\" [" + item['activeversion'] + "] [Registered]'>" + item['coursename'] + "</a></span>";
+          			str += "<span style='margin-right:15px;'><a class='" + textStyle + "' href='sectioned.php?courseid=" + item['cid'] + "&coursename=" + item['coursename'] + "&coursevers=" + item['activeversion'] + "' title='\"" + item['coursename'] + "\" [" + item['coursecode'] + "]'>" + item['coursename'] + "</a></span>";
         		}else{
-          			str += "<span style='margin-right:15px;opacity:0.3'><a class='" + textStyle + "' href='sectioned.php?courseid=" + item['cid'] + "&coursename=" + item['coursename'] + "&coursevers=" + item['activeversion'] + "' title='\"" + item['coursename'] + "\" [" + item['activeversion'] + "] [Not registered]'>" + item['coursename'] + "</a></span>";
+          			str += "<span style='margin-right:15px;opacity:0.3'><a class='" + textStyle + "' href='sectioned.php?courseid=" + item['cid'] + "&coursename=" + item['coursename'] + "&coursevers=" + item['activeversion'] + "' title='\"" + item['coursename'] + "\" [" + item['coursecode']+ "] '>" + item['coursename'] + "</a></span>";
         		}
         		str += "</div>";
 			}
