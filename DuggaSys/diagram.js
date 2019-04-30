109d67d8cf89932fbce4e860d82b7f42e57daba6
b17robev <b17robev@student.his.se>
2019-04-30 13:55:26 +0200

    Rouning error solved
    Javascript is still bad at math

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1194,7 +1194,7 @@ function canvasSize() {
 window.addEventListener('resize', canvasSize);
 
 function mod(n, m) {
-  return  Math.round(((n % m) + m)) % m;
+  return  Math.round(Math.round(((n % m) + m)) % m);
 }
 
 //----------------------------------------------------------
