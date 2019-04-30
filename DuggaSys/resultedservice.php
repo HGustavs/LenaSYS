a8d9aa3571276870a16a956fb0868fa40905a2fe
Anton Erikssen <a17anter@student.his.se>
2019-04-30 10:06:42 +0200

    Update resultedservice.php

--- DuggaSys/resultedservice.php
+++ DuggaSys/resultedservice.php
@@ -83,8 +83,8 @@ if($requestType == "mail"){
  	$emailString = "";
  	$studentID = "";
 
-// //	 foreach($visibleUserIDs as $id)
-// //	 {
+// 	 foreach($visibleUserIDs as $id)
+// 	 {
 //					$studentID = $visibleUserIDs[0];
 //					$mailQuery = $pdo->prepare("SELECT user.email FROM user INNER JOIN user_course ON user.uid = user_course.uid WHERE user_course.cid=:cid AND user_course.vers=:cvers AND user.username=:studentid");
 // 			$mailQuery = $pdo->prepare("SELECT user.email FROM user");
@@ -102,13 +102,13 @@ if($requestType == "mail"){
 //
 // 			//$emailString += $mailQuery + "; ";
 // 	array_push($emailsArray,$mailQuery);
-// //	}
-// 	//		array_push($groups[$row['groupKind']],$row['groupVal']);
+// 	}
+// 			array_push($groups[$row['groupKind']],$row['groupVal']);
 //
-// 	// Seperates the emails with a ;.
+// 	 Seperates the emails with a ;.
 // 			$implodedEmails = implode('; ',$emailsArray);
-// 	// Returns the emails in a string representation.
- 			echo json_encode($visibleUserIDs);
+// 	 Returns the emails in a string representation.
+ 			echo json_encode("Hej");
 	} else {
 
 //------------------------------------------------------------------------------------------------
