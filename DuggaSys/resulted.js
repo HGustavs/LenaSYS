329e47b1aadd591f310e6d80b8fc677a4bf45f8e
a17amama <a17amama@student.his.se>
2019-05-02 14:38:32 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -38,7 +38,7 @@ function setup() {
 
 	/*    Add filter menu   */
 	//Commented out this code because of similar solution below due to possible duplicate issue on github--
-  var filt = "";
+ /* var filt = "";
 	filt += "<td id='select' class='navButt'><span class='dropdown-container' onmouseover='hoverc();'>";
 	filt += "<img class='navButt' src='../Shared/icons/tratt_white.svg'>";
 	filt += "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
@@ -52,6 +52,7 @@ function setup() {
 	filt += "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
 	filt += "</div>";
 	filt += "</span></td>";
+	*/
 	
 
   // Add search bar to nav
