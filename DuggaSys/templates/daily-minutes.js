2171dc209dd474a938a249a76cc812f75d50998d
a17fellu <a17fellu@student.his.se>
2019-05-02 01:20:33 +0100

    Update daily-minutes.js

--- DuggaSys/templates/daily-minutes.js
+++ DuggaSys/templates/daily-minutes.js
@@ -122,10 +122,15 @@ function returnedDugga(data)
 			// UNK
 		}
 
+		var windowWidth = $(window).innerWidth();
 		var duggaFiles = data["files"][inParams["moment"]];
 		if($("#submitButtonTable").length != 0) {
-			createFileUploadArea(duggaParams["submissions"]);
-			createSmallerViewportForm(duggaParams["submissions"]);
+			if(windowWidth > 1120){
+				createFileUploadArea(duggaParams["submissions"]);
+			}
+			if(windowWidth < 1120){
+				createSmallerViewportForm(duggaParams["submissions"]);
+			}
 			for (var k=0; k < duggaParams["submissions"].length; k++){
 				findfilevers(duggaFiles, duggaParams["submissions"][k].fieldname,duggaParams["submissions"][k].type, 0);
 	    		if (duggaParams['uploadInstruction'] !== null){
