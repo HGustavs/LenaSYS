02c1e3dc1cf4654b497cf5a39d28bee21a92249d
b17nicsv <49198878+b17nicsv@users.noreply.github.com>
2019-05-02 10:27:16 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -1125,10 +1125,10 @@ function renderColumnFilter(col, status, colname) {
   /* Uses the intVarGradient variable as a counter to make a gradient effect on the column filters. */
   if(intVarGradient % 2 == 0){
     str = "<div class='hi ";
-    intVarGradient = 0;
+    intVarGradient = 1;
   } else {
     str = "<div class='lo ";
-    intVarGradient = 1;
+    intVarGradient = 0;
   }
 	if (status) {
 		str += "checkbox-dugga'>";
