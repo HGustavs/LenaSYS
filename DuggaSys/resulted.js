734d466262eace1c209af9bca610a14ddef40b3b
a17amama <a17amama@student.his.se>
2019-05-02 15:06:21 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -40,8 +40,6 @@ function setup() {
   var filt = "";
 	filt += "<td id='filter' class='navButt'><span class='dropdown-container' onmouseover='hovers();'>";
 	filt += "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
-	filt += "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
-	filt += "</div>";
 	filt += "</span></td>";
 
   // Add search bar to nav
