693d7948a6914c4bb08fdfd2413850f6122f5d99
a17adajo <a17adajo@student.his.se>
2019-05-02 13:53:50 +0200

    Removed old comment

--- Shared/SQL/init_db.sql
+++ Shared/SQL/init_db.sql
@@ -636,8 +636,6 @@ INSERT INTO word(wordlistid, word,label,uid) VALUES (3,"id","D",1);
 alter table user_course alter column result SET DEFAULT 0.0;
 alter table user_course alter column period set default 1;
 alter table user_course alter column term set default 1;
-/* Does not work, already exist a vers in user_course
-alter table user_course add column vers VARCHAR(8);
-*/
+
 alter table codeexample MODIFY runlink VARCHAR(256);
 ALTER TABLE listentries add column groupKind VARCHAR(16) DEFAULT NULL;
