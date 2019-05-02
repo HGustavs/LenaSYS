134394c9af870d9b0e0c557d30fdf9f80aef09f6
b17nicsv <49198878+b17nicsv@users.noreply.github.com>
2019-05-02 11:29:05 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -30,7 +30,7 @@ var duggaArray = [[]];
 var filterList;
 var tableName = "resultTable";
 var tableCellName = "resultTableCell";
-var intVarGradient;
+var intVarGradient = 0;
 
 function setup() {
 	//Benchmarking function
