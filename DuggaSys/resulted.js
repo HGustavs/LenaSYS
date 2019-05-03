e03d1837b43758a82aea138a92e3d409e5bfe847
pontus <pontus.g91@gmail.com>
2019-05-03 13:33:09 +0200

    debugging

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -848,7 +848,7 @@ function renderCell(col, celldata, cellid) {
 			str += "<div class='text-center resultTableText WriteOutTimesGraded'>";
 			if (celldata.ishere === true && celldata.timesGraded !== 0) {
 				str += '(' + celldata.timesGraded + ')';
-				console.log("graded: "+celldata.timesGraded)
+				console.log("graded: "+celldata.timesGraded + "type: " + typeof(celldata.timesGraded))
 			}
 			str += "</div>";
 			str += "</div>";
