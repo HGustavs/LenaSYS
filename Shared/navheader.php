314c45cd4386796bfb255b86482f12263539a3c5
a17amama <a17amama@student.his.se>
2019-05-03 12:25:38 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -60,8 +60,8 @@
 
 						echo "<td id='select' style='display:none;' class='navButt'>";
 						echo   "<span class='dropdown-container' onmouseover='hoverc();' onmouseleave='leavec();'>";
-						echo     "<img class='navButt' src='../Shared/icons/tratt_white.svg'>";
-						echo     "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
+				//		echo     "<img class='navButt' src='../Shared/icons/tratt_white.svg'>";
+					//	echo     "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
   					echo     "<div id='filterOptions'></div>";
 						echo     "</div>";
 	          echo   "</span>";
