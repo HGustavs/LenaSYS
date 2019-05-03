73d8a6a763f692e8d88e081e267369260f81f643
a17amama <a17amama@student.his.se>
2019-05-03 11:48:57 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -38,7 +38,18 @@ function setup() {
 	/*    Add filter menu   */
 	// --Commented out this code because of similar solution below due to possible duplicate issue on github--
   var filt = "";
-	filt += "<td class='navButt'>";
+	filt += "<td id='select' class='navButt'><span class='dropdown-container' onmouseover='hoverc();'>";
+	filt += "<img class='navButt' src='../Shared/icons/tratt_white.svg'>";
+	filt += "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
+	filt += "<div id='columnfilter'></div>"
+	filt += "<div id='customfilter'></div>"
+	filt += "</div>";
+	filt += "</span></td>";
+	filt += "<td id='filter' class='navButt'><span class='dropdown-container' onmouseover='hovers();'>";
+	filt += "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
+	filt += "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
+	filt += "</div>";
+	filt += "</span></td>";
 
   // Add search bar to nav
   filt += `<td id='searchBar' class='navButt'>`;
