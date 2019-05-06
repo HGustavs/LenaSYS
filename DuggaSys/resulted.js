c0dd174fc649e85f246e6fc0fd5c7a33f31ba2ab
pontus <pontus.g91@gmail.com>
2019-05-06 13:07:05 +0200

    test data

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -606,7 +606,7 @@ function returnedResults(data) {
 			if (studentObject != null && studentObject.uid === parseInt(data.duggauser) && studentObject.lid === parseInt(data.duggaid)) {
 				studentObject.grade = parseInt(data.results);
 				studentObject.timesGraded = parseInt(data.timesgraded);
-				console.log(data.timesgraded);
+				console.log(data['timesgraded']);
 				studentObject.gradeExpire = data.duggaexpire;
 				if (data.results > 0) {
 					studentObject.needMarking = false;
