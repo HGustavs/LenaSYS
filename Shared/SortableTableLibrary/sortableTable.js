38130daa36e1b1883cbada01cdd5250024d051f6
johan <johan.haggmark90@gmail.com>
2019-05-07 15:43:14 +0200

    Update sortableTable.js

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -1,4 +1,3 @@
-var filterResulted = [];
 // Keep track of Currently active Table and all sortable tables
 var sortableTable = {
 	currentTable: null,
@@ -273,7 +272,7 @@ function SortableTable(param) {
 	}
 
 	this.reRender = function () {
-		filterResulted = [];
+
 		this.rowIndex = 1;
 		// Local variable that contains html code for main table and local variable that contains magic headings table
 		str = "<table style='border-collapse: collapse;' id='" + this.tableid + DELIMITER + "tbl' class='list list--nomargin'>";
@@ -397,8 +396,7 @@ function SortableTable(param) {
 		for (var i = 0; i < tbl.tblbody.length; i++) {
 			var row = tbl.tblbody[i];
 			if (rowFilter(row)) {
-				filterResulted.push(row['FnameLnameSSN'].username);
-				str += "<tr id='" + this.tableid + DELIMITER + i + "'";
+				str += "<tr id='" + this.tableid + DELIMITER + i + "'"
 				if (this.hasRowHighlight) str += " onmouseover='rowHighlightInternal(event,this)' onmouseout='rowDeHighlightInternal(event,this)'";
 
 				//Check if row contains requestedpasswordchange & set styling accordingly
@@ -659,9 +657,6 @@ function SortableTable(param) {
 			activeFilteringUsername.push(currentRowFilter[i]['FnameLnameSSN'].username);
 		}
 	 }
-	 console.log(activeFilteringUsername);
-	 console.log(filterResulted);
-	 activeFilteringUsername = filterResulted;
 	$.ajax({
 		url: "resultedservice.php",
 		type: "POST",
