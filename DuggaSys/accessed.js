89e8fcb3e5773f3926504b999f26d013babfdf8a
a17fellu <a17fellu@student.his.se>
2019-05-08 16:03:03 +0100

    Removed test console log from file

--- DuggaSys/accessed.js
+++ DuggaSys/accessed.js
@@ -235,7 +235,6 @@ function renderCell(col, celldata, cellid) {
 		str = "<select onchange='changeOpt(event)' id='" + col + "_" + obj.uid + "'>" + makeoptionsItem(obj.vers, filez['courses'], "versname", "vers") + "</select>";
 		var checkSubmission = data => data.uid === obj.uid;
 		if (filez['submissions'].some(checkSubmission)) {
-			console.log("test");
 			str += "<img id='oldSubmissionIcon' title='View old version' src='../Shared/icons/DocumentDark.svg' onclick='showVersion(" + obj.vers + ")'>";
 		};
 	} else if (col == "access") {
