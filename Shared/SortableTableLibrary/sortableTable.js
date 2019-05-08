8a4ef48b260e574f7a34ad1be0107c5f0383861c
b17annni <b17annni@users.noreply.github.com>
2019-05-08 15:24:34 +0200

    Update sortableTable.js

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -119,7 +119,7 @@ function clickedInternal(event, clickdobj) {
 			str += "<div id='input-container' style='flex-grow:1'>";
 			str += estr;
 			str += "</div>";
-			str += "<img id='popovertick' class='icon' src='../Shared/icons/Ok_Red.svg' onclick='updateCellInternal();'>";
+			str += "<img id='popovertick' class='icon' src='../Shared/icons/Ok_Black.svg' onclick='updateCellInternal();'>";
 			str += "<img id='popovercross' class='icon' src='../Shared/icons/Cancel_Green.svg' onclick='clearUpdateCellInternal();'>";
 			var lmnt = cellelement.getBoundingClientRect();
 			var popoverelement = document.getElementById("editpopover");
