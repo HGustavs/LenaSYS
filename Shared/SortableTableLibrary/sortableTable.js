fe7114b9dd6f78284fce72a329fa5358ddc7c064
b17annni <b17annni@users.noreply.github.com>
2019-05-08 22:26:47 +0200

    icon colour

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -119,8 +119,8 @@ function clickedInternal(event, clickdobj) {
 			str += "<div id='input-container' style='flex-grow:1'>";
 			str += estr;
 			str += "</div>";
-			str += "<img id='popovertick' class='icon' src='../Shared/icons/Ok_Black.svg' onclick='updateCellInternal();'>";
-			str += "<img id='popovercross' class='icon' src='../Shared/icons/Cancel_Black.svg' onclick='clearUpdateCellInternal();'>";
+			str += "<img id='popovertick' class='icon' src='../Shared/icons/Ok_Green.svg' onclick='updateCellInternal();'>";
+			str += "<img id='popovercross' class='icon' src='../Shared/icons/Cancel_Red.svg' onclick='clearUpdateCellInternal();'>";
 			var lmnt = cellelement.getBoundingClientRect();
 			var popoverelement = document.getElementById("editpopover");
 
