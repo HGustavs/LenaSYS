f29e567ae3abf9ba81253d1883d51f8721b2a6a5
pontus <pontus.g91@gmail.com>
2019-05-09 08:43:28 +0200

    testing this.value

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -41,7 +41,7 @@
 				<tr class='trsize nowrap'>
 					<td style='display: inline-block;'>
 						<div class='course-dropdown-div'>
-								<select id="courseDropdownTop" class='course-dropdown' onchange='goToVersion(this.id)' >
+								<select id="courseDropdownTop" class='course-dropdown' onchange='goToVersion(this.value)' >
 								</select>
 						</div>
 					</td>
