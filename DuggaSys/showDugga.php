60ed86f94146d6f7aeec60d20918d7caa6230c1d
b17carma <b17carma@student.his.se>
2019-05-10 14:53:53 +0200

    Change contribution to use standard dugga format instead of an iframe since it caused a lot of issues

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -16,6 +16,7 @@
 
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+	<script src="../Shared/SortableTableLibrary/sortableTable.js"></script>
 	<script src="../Shared/dugga.js"></script>
   <script src="../Shared/markdown.js"></script>
 	<script src="timer.js"></script>
