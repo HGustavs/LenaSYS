a0dc56435e5c3e58efd8e14a301d9284fefde3fe
Tobias Frenger <a16tobfr@student.his.se>
2019-05-14 14:34:06 +0200

    Update diagram.php
    fixed uppercase

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -99,11 +99,11 @@
                                 <h4 class="label tlabel" id="labelUndo">Undo/Redo</h4>
                                 <div class="toolbar-drawer" id="drawerUndo">
                                     <!-- Undo -->
-                                    <button class="diagramAction" id="undoButton" onclick='undoDiagram(event)' data="Undo (ctrl + z)">
+                                    <button class="diagramAction" id="undoButton" onclick='undoDiagram(event)' data="Undo (Ctrl + Z)">
                                         <img src="../Shared/icons/undo.svg" style="filter: invert(100%); text-align: center; margin-left: -15px; margin-right: -15px; width: 17px; height: 17px;">
                                     </button>
                                     <!-- Redo -->
-                                    <button class="diagramAction" id="redoButton" onclick='redoDiagram(event)' data="Redo (ctrl + y)">
+                                    <button class="diagramAction" id="redoButton" onclick='redoDiagram(event)' data="Redo (Ctrl + Y)">
                                         <img src="../Shared/icons/redo.svg" style="filter: invert(100%); text-align: center; margin-left: -15px; margin-right: -15px; width: 17px; height: 17px;">
                                     </button>
                                 </div>
