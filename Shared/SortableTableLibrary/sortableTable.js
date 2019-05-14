ee868497a49b8dad78ced409c4cf53727aedf493
Ludwig <a17ludca@student.his.se>
2019-05-14 15:31:42 +0200

    Update sortableTable.js

--- Shared/SortableTableLibrary/sortableTable.js
+++ Shared/SortableTableLibrary/sortableTable.js
@@ -794,7 +794,7 @@ function newCompare(firstCell, secoundCell) {
 			if (firstCell === null || secoundCell === null) {
 				firstCellTemp = firstCell;
 				secoundCellTemp = secoundCell;
-			} else if(firstCell.indexOf(sizeTemp) && secoundCell.indexOf(sizeTemp)){
+			} else if(firstCell.indexOf(sizeTemp) == 1 && secoundCell.indexOf(sizeTemp) == 1){
 				tempTemp1 = firstCell.replace(/\D/g,'');
 				tempTemp2 = secoundCell.replace(/\D/g,'');
 				firstCellTemp = parseInt(tempTemp1, 10);
