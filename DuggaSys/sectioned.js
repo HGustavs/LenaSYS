222edb807ff63905de582525a96d49663847c222
Ludwig <a17ludca@student.his.se>
2019-05-15 14:27:21 +0200

    Update sectioned.js

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1304,7 +1304,9 @@ function drawSwimlanes() {
         }
         var tempVariable = duggalength*daywidth;
         
-        console.log(startday*daywidth, " ", startday, " ", daywidth);
+        if(startday < 0){
+          startday = startday * -1;
+        }
         str += "<rect opacity='0.7' x='" + (startday * daywidth) + "' y='" + (weeky) + "' width='" + (tempVariable) + "' height='" + weekheight + "' fill='" + fillcol + "' />";
         str += "<text x='" + (12) + "' y='" + (weeky + 18) + "' font-family='Arial' font-size='12px' fill='" + textcol + "' text-anchor='left'> <title> " + entry.text + " </title>" + entry.text + "</text>";
       }
