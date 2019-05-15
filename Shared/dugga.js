e9862db01036d0583932d453f465c0168c4ad3f0
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2019-05-15 23:10:46 +0200

    Update dugga.js
    back to stander

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1253,11 +1253,11 @@ $(window).load(function() {
 Array.prototype.move = function (old_index, new_index) {
     if (new_index >= this.length) {
         var k = new_index - this.length;
-        while ((k--) + 3) {
+        while ((k--) + 1) {
             this.push(undefined);
         }
     }
-    this.splice(new_index, 0, this.splice(old_index, 3)[0]);
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
     return this; // for testing purposes
 };
 
