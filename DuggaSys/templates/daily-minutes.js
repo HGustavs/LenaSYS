8391f2b716836d10fc54af3d4ea010d7d2366df0
a17joaap <a17joaap@student.his.se>
2019-05-15 14:09:18 +0200

    Fixed form resetting when resizing, and fixed row size when adding new row

--- DuggaSys/templates/daily-minutes.js
+++ DuggaSys/templates/daily-minutes.js
@@ -22,13 +22,19 @@ Data is saved to the timesheet table in the database, and to DuggaSys/submission
 function setup()
 {
 	inParams = parseGet();
+
 	window.addEventListener('resize', () => {
 		var windowWidth = $(window).innerWidth();
+		var tomten = document.getElementById('tomten');
 		if(windowWidth > 850){
-			createFileUploadArea(duggaParams["submissions"]);		
+			if (tomten.firstChild.lastChild.firstChild.id === 'tsSmallForm') {
+				createFileUploadArea(duggaParams["submissions"]);
+			}
 		} 
 		if(windowWidth <= 850){
-			createSmallerViewportForm(duggaParams["submissions"]);		
+			if (tomten.firstChild.lastChild.firstChild.id === 'tsBigForm') {
+				createSmallerViewportForm(duggaParams["submissions"]);
+			}		
 		}
 	})
 	AJAXService("GETPARAM", { }, "PDUGGA");
@@ -64,7 +70,7 @@ function newRow() {
 		inputValue = document.getElementById("tsRef_"+idx).value;
 		str += "<td><input id='tsRef_"+idx+"' required type='number' name='tsRef_"+idx+"' style='width: 55px' value='"+inputValue+"' /></td>";
 		inputValue = document.getElementById("tsComment_"+idx).value;
-		str += "<td><input id='tsComment_"+idx+"' class='tsCommentColumn' required type='text' name='tsComment_"+idx+"' style='width: 90%' value='"+inputValue+"' /></td>";
+		str += "<td class='tsCommentColumn'><input id='tsComment_"+idx+"' required type='text' name='tsComment_"+idx+"' style='width: 90%' value='"+inputValue+"' /></td>";
 		if (idx > 0) {
 			str += "<td class='tsTableDeleteCell' onclick='deleteRow("+idx+")'><img src='../Shared/icons/Trashcan.svg'></td>";
 		}
@@ -75,7 +81,7 @@ function newRow() {
 	str += generateTimeSheetOptions(inParams["cid"], inParams["moment"], 0);
 	str += "</select></td>";
 	str += "<td><input id='tsRef_"+lastRowIdx+"' required type='number' name='tsRef_"+lastRowIdx+"' style='width: 55px' /></td>";
-	str += "<td><input id='tsComment_"+lastRowIdx+"' class='tsCommentColumn' required type='text' name='tsComment_"+lastRowIdx+"' style='width: 90%' /></td>";
+	str += "<td class='tsCommentColumn'><input id='tsComment_"+lastRowIdx+"' required type='text' name='tsComment_"+lastRowIdx+"' style='width: 90%' /></td>";
 	str += "<td class='tsTableDeleteCell' onclick='deleteRow("+lastRowIdx+")'><img src='../Shared/icons/Trashcan.svg'></td>";
 
 	tsTableBody.innerHTML = str;
@@ -225,6 +231,7 @@ function createFileUploadArea(params){
 		fieldname = params[0].fieldname;
 	}
 
+	form +="<span id='tsBigForm' display='none'></span>";
 	form +="<form enctype='multipart/form-data' method='post' action='filereceive_dugga.php'>";
 	form +="<table class='tsTable'><thead>";
 	form +="<th>Date</th><th>Type</th>";
@@ -268,7 +275,7 @@ function createSmallerViewportForm(params){
 	if (params) {
 		fieldname = params[0].fieldname;
 	}
-
+	form +="<span id='tsSmallForm' display='none'></span>";
 	form +="<form enctype='multipart/form-data' method='post' action='filereceive_dugga.php'>";
 	form +="<div id='smallerViewportForm'>";
 	form +="<table class='tsTable'>";
