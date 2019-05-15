58f6521e4596675be198e9be4f27db0bd76d48b9
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2019-05-15 23:08:43 +0200

    Update dugga.js
    test

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1253,11 +1253,11 @@ $(window).load(function() {
 Array.prototype.move = function (old_index, new_index) {
     if (new_index >= this.length) {
         var k = new_index - this.length;
-        while ((k--) + 1) {
+        while ((k--) + 3) {
             this.push(undefined);
         }
     }
-    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    this.splice(new_index, 0, this.splice(old_index, 3)[0]);
     return this; // for testing purposes
 };
 
