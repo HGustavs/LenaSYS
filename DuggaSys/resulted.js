53fcc933c4be4d30995b5adfdf8f69518600fecf
Oscar <a17oscno@student.his.se>
2019-05-16 11:23:29 +0100

    testing to see if sorting by both first and lastname works

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -314,16 +314,18 @@ function sorttype(t) {
 		$("input[name='sorttype']").prop("checked", false);
 
     myTable.setNameColumn("Fname");
+	}else if (c == 1) {
+  	localStorage.setItem("lena_" + querystring['cid'] + "-" + querystring['coursevers'] + "-sort1", t);
+  	$("input[name='sorttype']").prop("checked", false);
 
-	} else {
+    myTable.setNameColumn("Lname");
+  }else {
 		if (t == -1) {
 			t = localStorage.getItem("lena_" + querystring['cid'] + "-" + querystring['coursevers'] + "-sort2", t);
 			$("#sorttype" + t).prop("checked", true);
 		} else {
 			localStorage.setItem("lena_" + querystring['cid'] + "-" + querystring['coursevers'] + "-sort2", t);
 			$("#sorttype" + t).prop("checked", true);
-
-      myTable.setNameColumn("Lname");
 		}
 	}
 	typechanged = true;
