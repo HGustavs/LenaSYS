9de523636db3b6c5d024b7e3a3070b42bf55f78a
b17annni <b17annni@users.noreply.github.com>
2019-05-17 15:53:17 +0200

    Update fileed.php

--- DuggaSys/fileed.php
+++ DuggaSys/fileed.php
@@ -34,9 +34,9 @@ pdoConnect();
 	<!-- content START -->
 	<div id="content">
 		<div class='titles' style='padding-top:10px;'>
-            <h1 style='flex:1;text-align:center;'>Files</h1>
-            <br>
-            <div id='searchBarMobile' style='test-align:right;margin-bottom:15px;'>
+			<h1 style='flex:1;text-align:center;'>Files</h1>
+    </div>
+    <div id='searchBarMobile' style='test-align:right;margin-bottom:15px;'>
 				<div id='tooltip-mobile' class="tooltip-searchbar">
 					<div class="tooltip-searchbar-box">
 								<b>Keywords:</b> markG, markU, date <br>
@@ -50,7 +50,6 @@ pdoConnect();
 					<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>
 				</button>
 			</div>
-    </div>
     <div style='display:flex;justify-content:space-between;align-items:flex-end;'>
         <div style='display:flex;flex-wrap:wrap;'>
             <div style='white-space:nowrap'>
