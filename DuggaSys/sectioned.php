4693860477919a7c1e10885aec9347a6feeaf3e4
Pontus <pontus.g91@gmail.com>
2019-05-20 11:31:02 +0200

    previous commit also removed not needed styling,replacing results button with icon

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -84,7 +84,7 @@
 
 					<td class='results menuButton' style='display: inline-block;'>
 						<div class='results menuButton'>
-							<a id='resultsBTN' href='' onclick='navigatePage(this.id, "resulted.php");' oncontextmenu='javascript:navigatePage(this.id, "resulted.php");'><input type='button' value='Results' class='submit-button' title='Edit student results' /></a>
+							<img id='versionPlus' value='Results' class="navButt" title='Edit student results' oncontextmenu='javascript:navigatePage(this.id, "resulted.php");' onclick='navigatePage(this.id, "resulted.php");' src='../Shared/icons/PlusS.svg'>
 						</div>
 					</td>
 					<td class='tests menuButton' style='display: inline-block;'>
