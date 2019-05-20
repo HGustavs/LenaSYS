a2a8bf7dfee74c14b7ab8835d8f5ad4197e19d07
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2019-05-20 19:06:54 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -296,7 +296,7 @@ function hovers() {
 }
 
 function leaves() {
-	$('#dropdowns').css('display', 'none').display('3000s');
+	$('#dropdowns').css('display', 'none').Style.display(3000);
 	var col = 0;
 	var dir = 1;
 
