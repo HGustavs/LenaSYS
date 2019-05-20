7f15dbe8bffa0e1413d58a721be9b04315ebd9e1
Pontus <pontus.g91@gmail.com>
2019-05-20 13:29:16 +0200

    changed icon for files

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -92,7 +92,7 @@
 					<td class='tests' style='display: inline-block;'>
 						<div class='tests menuButton'>
 							<a id='testsBTN' value='Tests' href='' onclick='navigatePage(this.id, "duggaed.php");' oncontextmenu='javascript:navigatePage(this.id, "duggaed.php");'>
-								<img id='testsBTN' title='Show tests' class="navButt" src='../Shared/icons/FistV.svg'>
+								<img id='testsBTN' title='Show tests' class="navButt" src='../Shared/icons/files_icon.svg'>
 							</a>
 						</div>
 					</td>
