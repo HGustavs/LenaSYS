af7d1f5fad03048296e28826f5d54389490764d8
b17annni <b17annni@users.noreply.github.com>
2019-05-21 12:41:09 +0200

    more search

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -25,7 +25,7 @@ function setup() {
 
 	
   filt += "<td id='testSearchContainer' class='navButt'>"
-  filt += "<input id='duggaSearch' type='text' placeholder='Search...' onkeyup='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'onsearch='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'/>"
+  filt += "<input id='duggaSearch' type='text' placeholder='Search...' onkeyup='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'onsearch='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById("searchinputMobile").value=document.getElementById("searchinput").value;'/>"
   filt += "<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>"
   filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>"
   filt += "</button>"
