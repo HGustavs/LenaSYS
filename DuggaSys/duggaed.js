0b0d211d39f24528128896611647984c4e522170
b17annni <b17annni@users.noreply.github.com>
2019-05-21 16:07:26 +0200

    Update duggaed.js

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -26,7 +26,7 @@ function setup() {
 	
 	filt += `<td id='testSearchContainer' class='navButt'>`
 	filt += `<input id='duggaSearch' type='text' name='search' placeholder='Search..'`;
-	filt += `onkeyup='searchterm=document.getElementById("duggaSearch").value;searchKeyUp(event);duggaTable.renderTable();'onsearch='searchterm=document.getElementById("duggaSearch").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById("searchinputMobile").value=document.getElementById("searchinput").value;'/>`;
+	filt += `onkeyup='searchterm=document.getElementById("duggaSearch").value;searchKeyUp(event);duggaTable.renderTable();'onsearch='searchterm=document.getElementById("duggaSearch").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById("searchinputMobile").value=document.getElementById("duggaSearch").value;'/>`;
   filt += `<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>`
   filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>`
   filt += `</button>`
