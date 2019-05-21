71d9bce162c575ff758d2c07d807cc1c8c4e0bd1
b17annni <b17annni@users.noreply.github.com>
2019-05-21 15:49:18 +0200

    Update duggaed.js

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -24,8 +24,9 @@ function setup() {
   var filt = "";
 
 	
-  filt += "<td id='testSearchContainer' class='navButt'>"
-	filt += "<input id='duggaSearch' type='text' placeholder='Search..' onkeyup='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();'onsearch='searchterm=document.getElementById(\"duggaSearch\").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById(\"searchinputMobile\").value=document.getElementById(\"duggaSearch\").value;'/>"
+	filt += "<td id='testSearchContainer' class='navButt'>"
+	filt += `<input id='duggaSearch' type='text' name='search' placeholder='Search..'`;
+	filt += `onkeyup='searchterm=document.getElementById("duggaSearch").value;searchKeyUp(event);duggaTable.renderTable();'onsearch='searchterm=document.getElementById("duggaSearch").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById("searchinputMobile").value=document.getElementById("searchinput").value;'/>`;
   filt += "<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>"
   filt += "<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>"
   filt += "</button>"
