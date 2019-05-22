a457a638eb7d8c4746bf6594bd318b07e95f71ed
Oscar <a17oscno@student.his.se>
2019-05-22 10:17:18 +0100

    testing

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -113,7 +113,7 @@
 			<<?php
 			echo "
 			<script type=\"text/javascript\">
-				navigatePage(document.getElementById('resultsBTN'), 'resulted.php');
+				navigatePage('resultsBTN', 'resulted.php');
 			</script>";
 			 ?>
 
