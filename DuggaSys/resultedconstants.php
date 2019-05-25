ec3f3020878372877e9c4f4d0849ddd06af8c3b9
a17alian <a17alian@student.his.se>
2019-05-25 13:26:06 +0200

    Moved const to the correct comment

--- DuggaSys/resultedconstants.php
+++ DuggaSys/resultedconstants.php
@@ -4,7 +4,7 @@ namespace resulted;
 class Constants
 {
 	// POST parameter identifier for a service updating unexported grades 
-	const getunexported_service_name = "getunexported";
-	// POST parameter identifier for a service retrieveing unexported grades
 	const updateunexported_service_name = "updateunexported";
+	// POST parameter identifier for a service retrieveing unexported grades
+	const getunexported_service_name = "getunexported";
 }
