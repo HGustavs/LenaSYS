05e0789d3b4ede2b6269ec7341399025682005e0
a17alian <a17alian@student.his.se>
2019-05-25 13:03:49 +0200

    Added const getunexported_service_name

--- DuggaSys/resultedconstants.php
+++ DuggaSys/resultedconstants.php
@@ -3,6 +3,8 @@ namespace resulted;
 
 class Constants
 {
+	// POST parameter identifier for a service updating unexported grades 
+	const getunexported_service_name = "getunexported";
 	// POST parameter identifier for a service retrieveing unexported grades
 	const updateunexported_service_name = "updateunexported";
 }
