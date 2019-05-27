8cb2ad260d8979bd71100a57dda619315b8820b1
a17amama <a17amama@student.his.se>
2019-05-27 10:45:47 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -66,14 +66,7 @@
 	          echo   "</span>";
 						echo "</td>";
 
-  					echo "<td id='sort' style='display:none' class='navButt'>";
-						echo   "<span onmouseover='hovers();' onmouseleave='leaves();'>";
-  					echo     "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
-						echo     "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
-						echo     "<div id='filterOptions' onmouseover='hovers();' onmouseleave='leaves();'></div>";
-  					echo     "</div>";
-  					echo   "</span>";
-						echo "</td>";
+  				
 
 						echo "</td>";
 
