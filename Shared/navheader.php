f55a427dab90eee9884ae087082eda7fbd2cbcf8
a17amama <a17amama@student.his.se>
2019-05-27 10:31:57 +0200

    Update navheader.php

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -58,18 +58,18 @@
 
 
 						echo "<td id='select' style='display:none;' class='navButt'>";
-						echo   "<span onmouseover='hoverc();' onmouseleave='leaves();'>";
+						echo   "<span onmouseover='hoverc();' onmouseleave='leavec();'>";
 				    echo     "<img class='navButt' src='../Shared/icons/tratt_white.svg'>";
-					  echo     "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
+					  echo     "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
   					echo     "<div id='filterOptions' onmouseover='hoverc();' onmouseleave='leavec();'></div>";
 						echo     "</div>";
 	          echo   "</span>";
 						echo "</td>";
 
   					echo "<td id='sort' style='display:none' class='navButt'>";
-						echo   "<span onmouseover='hoverc();' onmouseleave='leaves();'>";
+						echo   "<span onmouseover='hoverc();' onmouseleave='leavec();'>";
   					echo     "<img class='navButt' src='../Shared/icons/sort_white.svg'>";
-						echo     "<div id='dropdownc' class='dropdown-list-container' style='z-index: 1'>";
+						echo     "<div id='dropdowns' class='dropdown-list-container' style='z-index: 1'>";
 						echo     "<div id='filterOptions' onmouseover='hoverc();' onmouseleave='leavec();'></div>";
 						
   					echo     "</div>";
