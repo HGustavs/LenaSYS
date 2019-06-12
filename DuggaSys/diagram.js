48528b02edcd15d40f67fabcbaf702f5ffd1a37d
a16jenja <49199287+a16jenja@users.noreply.github.com>
2019-06-12 21:25:31 +0200

    Fixed #7646
    Disables the alignment and distribute options when none or only one object is selected.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1528,39 +1528,47 @@ function toggleA4Orientation(event) {
 //-----------------------------------------------------------------------------------
 var selectedItems = false;
 function enableSelectedItemOptions() {
-      if (selected_objects.length > 0) {
-        $("#change-appearance-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#move-selected-front-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#move-selected-back-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#lock-selected-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#delete-object-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#group-objects-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#ungroup-objects-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#align-top-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#align-right-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#align-bottom-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#align-left-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#horizontal-c-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#vertical-c-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#distribute-horizontal-item").removeClass("drop-down-item drop-down-item-disabled");
-        $("#distribute-vertical-item").removeClass("drop-down-item drop-down-item-disabled");
-      } else {
-        $("#change-appearance-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#move-selected-front-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#move-selected-back-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#lock-selected-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#delete-object-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#group-objects-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#ungroup-objects-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#align-top-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#align-right-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#align-bottom-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#align-left-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#horizontal-c-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#vertical-c-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#distribute-horizontal-item").addClass("drop-down-item drop-down-item-disabled");
-        $("#distribute-vertical-item").addClass("drop-down-item drop-down-item-disabled");
-    }
+  if (selected_objects.length > 0 && selected_objects.length <= 1) {
+    $("#change-appearance-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-front-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-back-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#lock-selected-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#delete-object-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#group-objects-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#ungroup-objects-item").removeClass("drop-down-item drop-down-item-disabled");
+  } else if (selected_objects.length >= 2) {
+    $("#change-appearance-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-front-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-back-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#lock-selected-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#delete-object-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#group-objects-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#ungroup-objects-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#align-top-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#align-right-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#align-bottom-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#align-left-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#horizontal-c-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#vertical-c-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#distribute-horizontal-item").removeClass("drop-down-item drop-down-item-disabled");
+    $("#distribute-vertical-item").removeClass("drop-down-item drop-down-item-disabled");
+  } else {
+    $("#change-appearance-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-front-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#move-selected-back-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#lock-selected-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#delete-object-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#group-objects-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#ungroup-objects-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#align-top-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#align-right-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#align-bottom-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#align-left-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#horizontal-c-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#vertical-c-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#distribute-horizontal-item").addClass("drop-down-item drop-down-item-disabled");
+    $("#distribute-vertical-item").addClass("drop-down-item drop-down-item-disabled");
+}
 }
 
 //----------------------------------------------------
@@ -1697,7 +1705,7 @@ function eraseObject(object) {
                 || line.bottomRight == object.centerPoint
                 || (object.hasConnectorFromPoint(line.topLeft) && (object.symbolkind == symbolKind.erEntity || object.symbolkind == symbolKind.erRelation))
                 || (object.hasConnectorFromPoint(line.bottomRight) && (object.symbolkind == symbolKind.erEntity || object.symbolkind == symbolKind.erRelation))
-            );  
+            );
             } else if (object.symbolkind == symbolKind.uml) {
             objectsToDelete = umlLines.filter(
                 umlLine => umlLine.topLeft == object.middleDivider
