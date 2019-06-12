106e20f6ce4d3be5351a4f3805532666c3b329da
niccesvahn <49589618+NicceSvahn@users.noreply.github.com>
2019-06-12 20:04:23 +0200

    test

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -461,13 +461,8 @@ function accessCourse() {
 function verifyCourseVersionInputForm(input) {
   console.log("Testing the verifyCourseVersionInputForm");
   // Verify if course name is <= 100 characters.
-  if (input.versname.length() <= 100){
+  if (input.versname.length >= 100){
     alert('Input exceeded max length for version name (100)');
-    return false;
-  }
-  // Verify if the course code is a number.
-  if (isNaN(input.versid)){
-    alert('Input is not a number. Version ID has to be a number.');
     return false;
   }
   return true;
