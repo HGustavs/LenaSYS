c61fdd52b0413b3ab7c1dc14c1f778863c790e4a
Marcus Brohede <a97marbr@gmail.com>
2020-02-04 22:05:34 +0100

    Bugfix, now html source doesn't get garbled

--- DuggaSys/fileed.js
+++ DuggaSys/fileed.js
@@ -580,6 +580,8 @@ function returnedPreview(data) {
     updatePreview(data);
     //$('#mrkdwntxt').html(data);
     //https://stackoverflow.com/questions/1147359/how-to-decode-html-entities-using-jquery/1395954#1395954
+    data = data.replace(/\</g, "&lt;");
+    data = data.replace(/\>/g, "&gt;");
     decoded = $('<textarea/>').html(data).text();
     document.getElementById("mrkdwntxt").value = decoded;
 }
