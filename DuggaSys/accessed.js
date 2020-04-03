73dc0ea13f301250af357c0e8c80e08254817d31
g18marma <g18marma@student.his.se>
2020-04-03 12:31:27 +0200

    Revert "Revert "ISSUE#7769 Buttons to Divs""
    This reverts commit 4dac62112d069c16ae23b817e5a6f79dee602841.

--- DuggaSys/accessed.js
+++ DuggaSys/accessed.js
@@ -315,12 +315,13 @@ function renderCell(col, celldata, cellid) {
 	} else if (col == "requestedpasswordchange") {
 		
 		if (parseFloat(obj.recent) > 1440) {
-			str = "<input class='submit-button' type='button' value='Reset PW' style='display:block;margin:auto;float:none;'";
+			str = "<div class='submit-button' style='display:block;margin:auto;float:none;'";
 		} else {
-			str = "<input class='submit-button resetpw-button' type='button' value='Reset PW'";
+			str = "<div class='submit-button resetpw-button' style='width:80%; margin-left:20px; background-color: rgb(254, 204, 86); color: #614875'";
 		}
 		str += " onclick='if(confirm(\"Reset password for " + obj.username + "?\")) ";
 		str += "resetPw(\"" + obj.uid + "\",\"" + obj.username + "\"); return false;'>";
+        str += "Reset PW";
 	} else if (col == "groups") {
 		if (obj.groups == null) {
 			tgroups = [];
