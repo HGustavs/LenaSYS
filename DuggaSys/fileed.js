2db9a61c933a2b6f798ce10e25630efd60163eac
a18mahkh <a18mahkh@student.his.se>
2020-04-14 12:42:40 +0200

    #7881 problem solved
    You can sort the files by its kind

--- DuggaSys/fileed.js
+++ DuggaSys/fileed.js
@@ -266,6 +266,26 @@ function renderCell(col, celldata, cellid) {
     }
     return str;
 }
+//---------------------------------------------------------------
+//sortFilesByKind <- Callback function sorts the files by its kind
+//---------------------------------------------------------------
+
+function sortFilesByKind(kind){
+    $("#fileLink table tbody tr").hide();
+    if(kind == "Global"){
+        $( "td:contains('Global')" ).parents("tr").show();
+
+    }if(kind == "CourseLocal"){
+        $( "td:contains('Course local')" ).parents("tr").show();
+
+    }if(kind == "Local"){
+        $( "td:contains('Local')" ).parents("tr").show();
+
+    }else if(kind == "AllFiles"){
+        $("#fileLink table tr").show();
+    }
+
+}
 
 //----------------------------------------------------------------
 // rowFilter <- Callback function that filters rows in the table
