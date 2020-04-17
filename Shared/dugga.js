7b7ebd2eea9d76829341fbb909d5d4e8b1c92293
b18alegu <62876260+b18alegu@users.noreply.github.com>
2020-04-17 13:52:54 +0200

    Update dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -330,11 +330,12 @@ function removeYearFromDate(date){
 
 function setExpireCookie(){
     if(localStorage.getItem("securityquestion") == "set") {
-				var expireDate = new Date();
-				// a test date so you dont have to actually wait 1 hour and 45 minutes
-				//expireDate.setMinutes(expireDate.getMinutes() + 1);
-				expireDate.setMinutes(expireDate.getMinutes() + 105);
-        document.cookie = "sessionEndTime=expireC; expires=" + expireDate.toGMTString() + "; path=/";
+				const expireDate = new Date();
+				// A test date so you dont have to actually wait 1 hour and 45 minutes.
+				// Don't forget to change the one below (setExpireCookieLogOut()) too.
+				//expireDate.setMinutes(expireDate.getMinutes() + 1);	// For testing
+				expireDate.setMinutes(expireDate.getMinutes() + 105);	// For actual use
+        document.cookie = "sessionEndTime=expireC; expires=" + expireDate.toUTCString() + "; path=/";
     }
 }
 
@@ -344,11 +345,10 @@ function setExpireCookie(){
 
 function setExpireCookieLogOut() {
     if (localStorage.getItem("securityquestion") == "set") {
-				var expireDate = new Date();
-				// test date
-				//expireDate.setMinutes(expireDate.getMinutes() + 2);
-				expireDate.setMinutes(expireDate.getMinutes() + 120);
-        document.cookie = "sessionEndTimeLogOut=expireC; expires=" + expireDate.toGMTString() + "; path=/";
+				const expireDate = new Date();
+				//expireDate.setMinutes(expireDate.getMinutes() + 2);	// For testing
+				expireDate.setMinutes(expireDate.getMinutes() + 120);	// For actual use
+        document.cookie = "sessionEndTimeLogOut=expireC; expires=" + expireDate.toUTCString() + "; path=/";
     }
 }
 
