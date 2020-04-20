80b43d5212c56f93a6552f78b09f1d206171fcfb
Anton Svensson <a18antsv@student.his.se>
2020-04-20 02:26:40 +0200

    Positioned HTML-elements relative to canvas container

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -361,8 +361,6 @@
                         <span id="errorMSG"></span>
                     </div>
                 </div>
-                </br>
-                </br>
 
                 <!-- THESE OBJECTS ARE NOT IN THE TOOLBOX OR THE MENU-->
                 <!-- AS THEY PROBABLY SHOULD BE IMPLEMENTED SOMEWHERE WITHIN ISSUE #3750-->
@@ -381,27 +379,14 @@
                 -->
 
             </div>
-
-            <!-- THESE OBJECTS ARE NOT IN THE TOOLBOX OR THE MENU-->
-            <!-- AS THEY PROBABLY SHOULD BE IMPLEMENTED SOMEWHERE WITHIN ISSUE #3750-->
-            <div class="tooltipdialog">
-                <button id='moveButton' class='unpressed' title="Move Around" style="visibility:hidden">
-                    <img src="../Shared/icons/diagram_move_arrows.svg">
-                </button>
-            </div>
             <div id="diagramCanvasContainer">
-               <canvas id="diagramCanvas"></canvas> 
-            </div>
-            <div id="consoleDiv">
-                <!--
-                    Can be used for a later date. Not needed now.
-                <div id='consloe' style='position: fixed; left: 0px; right: 0px; bottom: 0px; height: 133px; background: #dfe; border: 1px solid #284; z-index: 5000; overflow: scroll; color: #4A6; font-family:lucida console; font-size: 13px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'>Application console</div>
-                <input id='Hide Console' style='position: fixed; right: 0; bottom: 133px;' type='button' value='Hide Console' onclick='Consolemode(1);' />
-                <input id='Show Console' style='display: none; position: fixed; right: 0; bottom: 133px;' type='button' value='Show Console' onclick='Consolemode(2);' />
-                -->
-                <div id="valuesCanvas" style="position: fixed">
+               <canvas id="diagramCanvas"></canvas>
+               <button id='moveButton' class='unpressed' title="Move Around">
+                    <img src="../Shared/icons/diagram_move_arrows.svg">
+                </button> 
+                <div id="valuesCanvas">
                 </div>
-                <div id="selectDiv" style="position: fixed">
+                <div id="selectDiv">
                     <span class="tooltipDecrease">
                         <button name="Zoom" id="zoomDecrease" class="zoomButtonStyle" type="button" onclick="changeZoom(-0.1, event);">-</button>
                         <span class="tooltiptextDec">Zoom Out</span>
@@ -415,6 +400,14 @@
                     </span>
                     <span id="zoomV"></span>
                 </div>
+            </div>
+            <div id="consoleDiv">
+                <!--
+                    Can be used for a later date. Not needed now.
+                <div id='consloe' style='position: fixed; left: 0px; right: 0px; bottom: 0px; height: 133px; background: #dfe; border: 1px solid #284; z-index: 5000; overflow: scroll; color: #4A6; font-family:lucida console; font-size: 13px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; cursor: default;'>Application console</div>
+                <input id='Hide Console' style='position: fixed; right: 0; bottom: 133px;' type='button' value='Hide Console' onclick='Consolemode(1);' />
+                <input id='Show Console' style='display: none; position: fixed; right: 0; bottom: 133px;' type='button' value='Show Console' onclick='Consolemode(2);' />
+                -->
             </div>
         </div>
     </div>
