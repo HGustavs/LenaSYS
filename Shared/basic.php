c51cbf713f19cd3134d809d0ddc13a06371a36b8
Carl-Simon [b17carbr] <b17carbr@student.his.se>
2020-04-20 16:15:26 +0200

    Added logging for file adding

--- Shared/basic.php
+++ Shared/basic.php
@@ -315,9 +315,11 @@ abstract class EventTypes {
 	const ServiceClientEnd = 8;
 	const Logout = 9;
 	const pageLoad = 10;
-  const PageNotFound = 11;
-  const RequestNewPW = 12;
-  const CheckSecQuestion = 13;
+  	const PageNotFound = 11;
+  	const RequestNewPW = 12;
+	const CheckSecQuestion = 13;
+	  
+	const AddFile = 15;
 }
 
 //------------------------------------------------------------------------------------------------
