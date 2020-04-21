4ae6ef506a250726992af108de32275a76b2d8a2
Carl-Simon [b17carbr] <b17carbr@student.his.se>
2020-04-21 13:23:17 +0200

    changed how description apears in db

--- DuggaSys/courseedservice.php
+++ DuggaSys/courseedservice.php
@@ -84,7 +84,9 @@ if(checklogin()){
 				$debug="Error updating entries\n".$error[2];
 			} 
 
-			logUserEvent($username, EventTypes::AddCourse, $coursename.",".$coursecode);
+			$description=$coursename." ".$coursecode;
+			logUserEvent($username, EventTypes::AddCourse, $description);
+
 		}else if(strcmp($opt,"NEWVRS")===0){
 			$query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt);");
 
@@ -451,7 +453,9 @@ if(checklogin()){
 				$visibilityName = "Deleted";
 			}
 			
-			logUserEvent($username, EventTypes::EditCourse, $coursename.",".$coursecode.",".$visibilityName);
+			$description=$coursename." ".$coursecode." ".$visibilityName;
+			logUserEvent($username, EventTypes::EditCourse, $description);
+
 		}else if(strcmp($opt,"SETTINGS")===0){
 		$query = $pdo->prepare("INSERT INTO settings (motd,readonly) VALUES (:motd, :readonly);");
 
