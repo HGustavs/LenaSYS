b1efd2d684d249ced3123223c9089ec8555cf15b
a17pioja <a17pioja@users.noreply.github.com>
2020-04-23 12:38:32 +0200

    collab purpose commit

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -928,8 +928,12 @@ function renderCell(col, celldata, cellid) {
 		if (celldata.kind == 4) {
 			str += "dugga-moment ";
 		}
-		else if (celldata.kind != 4 || celldata.kind != 3 || celldata.kind != 2 || celldata.kind != 1 || celldata.kind != 0 || celldata.kind != 5 || celldata.kind != 6) {
+		else if (celldata.uid ==218) {
+			// do nothing
 			str += "dugga-empty ";
+		}
+		else {
+			//str += "dugga-empty ";
 		}
 		if (celldata.grade > 1) {
 			str += "dugga-pass";
