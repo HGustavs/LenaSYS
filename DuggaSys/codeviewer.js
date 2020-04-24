6c3e33d7a2151a32513352ac86da28308bf366cf
a18geoge <georg.gehlin5@gmail.com>
2020-04-24 11:23:47 +0200

    Updated function "addTemplatebox within codeviewer.js
    Updated function "addTemplatebox" within codeviewer.js to better fit the code standard.

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -773,8 +773,8 @@ function addTemplatebox(id) {
 	str += "<div id='" + id + "' class='box'></div>";
 	str += "</div>";
 
-	str = str + $("#div2").html();
-	$("#div2").html(str);
+	str = str + document.getElementById("div2").innerHTML;
+	document.getElementById("div2").innerHTML = str;
 }
 
 //----------------------------------------------------------------------------------
