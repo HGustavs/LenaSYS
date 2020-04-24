90e71d5b10fbda54dc9a8a5bab3ab834c1aef857
Jens Roderus <mongelonas@hotmail.com>
2020-04-24 10:25:19 +0200

    Commit for test on server

--- DuggaSys/fileed.php
+++ DuggaSys/fileed.php
@@ -334,21 +334,23 @@ $codeLinkQuery->execute();
 
 <!--This if-statements is used when fileedit opens from an iframe in codeviewer. -->
 <?php 
-    if($_GET['kind'] != null && $_GET['filename'] != null){
-        echo '<script type="text/javascript">',
-        'loadFile("../courses/1/'.$_GET['filename'].'", "'.$_GET['filename'].'", '.$_GET['kind'].');',
-            '</script>'
-        ;
-		echo '<script type="text/javascript">',
-        'console.log(1);',
-            '</script>'
-        ;
-    }  
-	if($_GET['kind'] == null){
-		echo '<script type="text/javascript">',
-        'console.log(77);',
-            '</script>'
-        ;
+	if (isset($_GET['kind']) && isset($_GET['filename'])) {
+		if($_GET['kind'] != null && $_GET['filename'] != null){
+			echo '<script type="text/javascript">',
+			'loadFile("../courses/1/'.$_GET['filename'].'", "'.$_GET['filename'].'", '.$_GET['kind'].');',
+				'</script>'
+			;
+			echo '<script type="text/javascript">',
+			'console.log(1);',
+				'</script>'
+			;
+		}  
+		if($_GET['kind'] == null){
+			echo '<script type="text/javascript">',
+			'console.log(77);',
+				'</script>'
+			;
+		}
 	}
 	echo '<script type="text/javascript">',
         'console.log(4);',
