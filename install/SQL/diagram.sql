f2659db8ffc6d3bc92c6f40b5807f83a541257ef
a18conch <c_christoforidis@hotmail.com>
2020-04-28 14:09:32 +0200

    added diagram example data (incomplete)

---
+++ install/SQL/diagram.sql
@@ -0,0 +1,2 @@
+/*Insert the dugga into the example course*/
+INSERT INTO listentries (lid,cid,entryname,link,kind,pos,creator,visible,vers,rowcolor) VALUES (9999,7653,'Diagram',16,2,20,1,1,'4000',0);
