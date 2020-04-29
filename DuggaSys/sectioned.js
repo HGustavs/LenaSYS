22fb5ccceaaecb57abaff7375abbc28947afcf20
a17towan <a17towan@student.his.se>
2020-04-29 10:07:40 +0200

    Remaking 7577 again
    Remaking my previous commits on the previous week-branch because conflicts occurred when I tried to merge W-19 into my previous branch for this issue, so I'm remaking it again.

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -116,7 +116,7 @@ function toggleHamburger() {
 // selectItem: Prepare item editing dialog after cog-wheel has been clicked
 //----------------------------------------------------------------------------------
 
-function selectItem(lid, entryname, kind, evisible, elink, moment, gradesys, highscoremode, comments, grptype, deadline) {
+function selectItem(lid, entryname, kind, evisible, elink, moment, gradesys, highscoremode, comments, grptype, deadline, tabs) {
 
   // Variables for the different options and values for the deadlne time dropdown meny.
   var hourArrOptions=["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
@@ -153,7 +153,7 @@ function selectItem(lid, entryname, kind, evisible, elink, moment, gradesys, hig
   $("#gradesys").html(makeoptions(gradesys, ["-", "U-G-VG", "U-G"], [0, 1, 2]));
   $("#type").html(makeoptions(kind, ["Header", "Section", "Code", "Test", "Moment", "Link", "Group Activity", "Message"], [0, 1, 2, 3, 4, 5, 6, 7]));
   $("#visib").html(makeoptions(evisible, ["Hidden", "Public", "Login"], [0, 1, 2]));
-  $("#tabs").html(makeoptions(gradesys, ["0 tabs", "1 tabs", "2 tabs", "3 tabs", "end", "1 tab + end", "2 tabs + end"], [0, 1, 2, 3, 4, 5, 6]));
+  $("#tabs").html(makeoptions(tabs, ["0 tabs", "1 tabs", "2 tabs", "3 tabs", "end", "1 tab + end", "2 tabs + end"], [0, 1, 2, 3, 4, 5, 6]));
   $("#highscoremode").html(makeoptions(highscoremode, ["None", "Time Based", "Click Based"], [0, 1, 2]));
   if(deadline !== undefined){
     $("#deadlinehours").html(makeoptions(deadline.substr(11,2),hourArrOptions,hourArrValue));
@@ -347,7 +347,7 @@ function prepareItem() {
   // Storing tabs in gradesys column!
   var kind = $("#type").val()
   if (kind == 0 || kind == 1 || kind == 2 || kind == 5 || kind == 6 || kind == 7) {
-    param.gradesys = $("#tabs").val();
+    param.tabs = $("#tabs").val();
   } else {
     param.gradesys = $("#gradesys").val();
   }
@@ -358,6 +358,7 @@ function prepareItem() {
   param.highscoremode = $("#highscoremode").val();
   param.sectname = $("#sectionname").val();
   param.visibility = $("#visib").val();
+  param.tabs = $("#tabs").val();
   param.moment = $("#moment").val();
   param.comments = $("#comments").val();
   param.grptype = $("#grptype").val();
@@ -694,7 +695,7 @@ function returnedSection(data) {
         if (itemKind === 3 || itemKind === 4) {
 
           // Styling for quiz row e.g. add a tab spacer
-          if (itemKind === 3) str += "<td style='width:32px;'><div class='spacerLeft'></div></td>";
+          if (itemKind === 3) str += "<td id='indTab' class='tabs" + item["tabs"] + "'><div class='spacerLeft'></div></td>";
           var grady = -1;
           var status = "";
           var marked;
