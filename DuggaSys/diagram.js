4d17eb684bae13b92ca784d4e0e38bbacde6bad8
Anton Svensson <a18antsv@student.his.se>
2020-04-30 01:56:11 +0200

    Added comments for the special case

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1077,6 +1077,10 @@ function copySymbol(symbol) {
             let newPointIndex = 0;
             if(typeof keyContainsDuplicateOldPoint === "undefined") {
 
+                //Special case for ER lines connected to attributes
+                //The second point of an er line connected to an attribute is the attribute's centerPoint
+                //This code finds connected attributes that will be copied and prevents the second point from being duplicated if the attribute will also be copied
+                //If the attribute will not be copied the point should be created
                 if(symbol.symbolkind === symbolKind.line && key === "bottomRight") {
                     const connectedAttribute = symbol.getConnectedObjects().find(object => object.symbolkind === symbolKind.erAttribute);
                     if(typeof connectedAttribute !== "undefined") {
