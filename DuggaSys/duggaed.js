23f8691692261080f3738be1d331c753be0ffc3e
b18alegu <62876260+b18alegu@users.noreply.github.com>
2020-04-30 12:16:27 +0200

    Corrected indentation of the setup() function

--- DuggaSys/duggaed.js
+++ DuggaSys/duggaed.js
@@ -21,22 +21,21 @@ var searchterm = "";
 
 function setup() {
 
-  var filt = "";
-
+	var filt = "";
 
 	filt += `<td id='testSearchContainer' class='navButt'>`
 	filt += `<input id='duggaSearch' type='text' name='search' placeholder='Search..'`;
 	filt += `onkeyup='searchterm=document.getElementById("duggaSearch").value;searchKeyUp(event);duggaTable.renderTable();'onsearch='searchterm=document.getElementById("duggaSearch").value; searchKeyUp(event); duggaTable.renderTable();document.getElementById("searchinputMobile").value=document.getElementById("duggaSearch").value;'/>`;
-  filt += `<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>`
-  filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>`
-  filt += `</button>`
+	filt += `<button id='searchbutton' class='switchContent' onclick='return searchKeyUp(event);' type='button'>`
+	filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'>`
+	filt += `</button>`
 	filt += `</td>`
-  filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>`;
+	filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>`;
 	filt += `</button></td>`;
 
-  $("#menuHook").before(filt);
+	$("#menuHook").before(filt);
 
-  AJAXService("GET", { cid: querystring['courseid'], coursevers: querystring['coursevers'] }, "DUGGA");
+	AJAXService("GET", { cid: querystring['courseid'], coursevers: querystring['coursevers'] }, "DUGGA");
 }
 
 
