0e3eb8c1f211e2aa3ef02c5d0fdc97a5fb784c08
Kasper Witt <62878146+a18kaswi@users.noreply.github.com>
2020-05-05 12:27:13 +0200

    Moved even more, had to load script with and without defer, dont know if bad.

--- install/install.php
+++ install/install.php
@@ -3,6 +3,7 @@
   <link rel="stylesheet" type="text/css" href="CSS/install_style.css">
   <script src="../Shared/js/jquery-1.11.0.min.js"></script>
   <script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
+  <script src="install.js"></script>
   <script src="install.js" defer></script>
 </head>
 <body>
@@ -105,55 +106,6 @@ function setPermissionModalText(){
   `<div>
     ${getPermissionModalText(owner, filePath, operatingSystem)}
   </div>`;
-}
-
-//---------------------------------------------------------------------------------------------------
-// getPermissionModalText, f for functionVariable
-//---------------------------------------------------------------------------------------------------
-function getPermissionModalText(fOwner, fFilePath, fOperatingSystem){
-  permissionModalText = null;
-  
-  permissionModalText = `<h1>--- READ THIS BEFORE YOU START ---</h1><br>
-  ${setFirstPermissionText(fOperatingSystem)}<br><br>
-  Current Owner: ${fOwner}<br>
-  Current OperatingSystem: ${fOperatingSystem}<br><br>
-  To do this run the command: ${setSecondPermissionText(fOperatingSystem, fFilePath)}\<br><br>
-  <input title='I have completed necessary steps' onclick='if(this.checked){haveRead(true)}else{haveRead(false)}' class='startCheckbox' type='checkbox' value='1' autofocus>" +
-  <i>I promise I have done this.</i>`
-
-  return permissionModalText;
-}
-
-//-----------------------------------------------------------------------------------------------------------
-// setFirstPermissionText: Sets the the first text for permission-modal, called by getPermissionModalText() (using fp not own, os)
-//-----------------------------------------------------------------------------------------------------------
-function setFirstPermissionText(fOperatingSystem){
-  var firstText;
-  switch(fOperatingSystem){
-    case "Linux":
-      firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www-data'.";
-      break;
-    case "Darwin":
-      firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www'";
-      break;
-  }
-  return firstText;
-}
-
-//-----------------------------------------------------------------------------------------------------------
-// setSecondPermissionText: Sets the the second text for permission-modal, called by getPermissionModalText() (using fp os not own)
-//-----------------------------------------------------------------------------------------------------------
-function setSecondPermissionText(fOperatingSystem, fFilePath){
-  var secondText;
-  switch(fOperatingSystem){
-    case "Linux":
-      secondText = "sudo chgrp -R www-data " + fFilePath + "<br>" + "sudo chown -R www-data " + fFilePath + "</h2><br>";
-      break;
-    case "Darwin":
-      secondText = "sudo chgrp -R www " + fFilePath + "<br>" + "sudo chown -R www " + fFilePath + "</h2><br>";
-      break;
-  }
-  return secondText;
 }
 </script>
 
