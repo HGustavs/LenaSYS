5fad9b69609d682c9093bb7e12d75d9775211a12
Kasper Witt <62878146+a18kaswi@users.noreply.github.com>
2020-05-05 11:40:22 +0200

    Broke down the permission-script to prepare for move

--- install/install.php
+++ install/install.php
@@ -85,61 +85,85 @@
   var setOperatingSystem = <?php echo json_encode(PHP_OS_FAMILY); ?>;
   var setOwner = <?php echo json_encode($username); ?>;
 </script>
-  
-<!-- Start permission-modal code -->
+
 <script>
-    var modalRead = false; // Have the user read info?
-    var modal = document.getElementById('warning'); // Get the modal
-    var span = document.getElementsByClassName("close")[0]; // Get the button that opens the modal
-    var filePath = setFilePath;
-    var owner = setOwner;
-    var os = setOperatingSystem; // Get O/S of the system running the installer
-    var firstText = setFirstText(os); // Set first text depending on O/S
-    var secondText = setSecondText(os); // Set second text depending on O/S
-    
-    document.getElementById('dialogText').innerHTML=	
-    `<div>
-	    <h1>--- READ THIS BEFORE YOU START ---</h1><br>
-	    ${firstText}<br><br>
-	    Current Owner: ${owner}<br>
-	    Current OS: ${os}<br><br>
-	    To do this run the command: ${secondText}\<br><br>
-	    <input title='I have completed necessary steps' onclick='if(this.checked){haveRead(true)}else{haveRead(false)}' class='startCheckbox' type='checkbox' value='1' autofocus>" +
-      <i>I promise I have done this.</i>
-	  </div>`;
+var modalRead = false; // Have the user read info?
+var modal = document.getElementById('warning'); // Get the modal
+var modalDialogText = document.getElementById('dialogText'); // Get the dialogText of the modal
+var span = document.getElementsByClassName("close")[0]; // Get the button that opens the modal
+var filePath = setFilePath; // Set variable to value from PHP-file.
+var owner = setOwner; // Set variable to value from PHP-file.
+var operatingSystem = setOperatingSystem; // Set variable to value from PHP-file.
+var firstText = setFirstPermissionText(operatingSystem); // Set first text depending on O/S
+var secondText = setSecondPermissionText(operatingSystem); // Set second text depending on O/S
 
-    function haveRead(isTrue) {
-        modalRead = isTrue;
-    }
-    
-    /*************           Function to set the first text           ****************/
-    /*************      Takes O/S of installing system as input       ****************/
-    /************* Easy to extend to new supported operating systems. ****************/
-    function setFirstText(os){
-      var firstText;
-      switch(os){
-        case "Linux":
-          firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www-data'.";
-          break;
-        case "Darwin":
-          firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www'";
-          break;
-      }
-      return firstText;
-    }
+setPermissionModalText();
 
-    function setSecondText(os){
-      var secondText;
-      switch(os){
-        case "Linux":
-          secondText = "sudo chgrp -R www-data " + filePath + "<br>" + "sudo chown -R www-data " + filePath + "</h2><br>";
-          break;
-        case "Darwin":
-          secondText = "sudo chgrp -R www " + filePath + "<br>" + "sudo chown -R www " + filePath + "</h2><br>";
-          break;
-      }
-      return secondText;
-    }
+//---------------------------------------------------------------------------------------------------
+// setPermissionModalText, function to set the text of the permission-modal
+//---------------------------------------------------------------------------------------------------
+function setPermissionModalText(){
+  modalDialogText.innerHTML=	
+  `<div>
+    ${getPermissionModalText(owner, filePath, operatingSystem)}
+  </div>`;
+}
+
+//---------------------------------------------------------------------------------------------------
+// getPermissionModalText, 
+//---------------------------------------------------------------------------------------------------
+function getPermissionModalText(owner, filePath, operatingSystem){
+  permissionModalText = null;
+  
+  permissionModalText = `<h1>--- READ THIS BEFORE YOU START ---</h1><br>
+  ${firstText}<br><br>
+  Current Owner: ${owner}<br>
+  Current OperatingSystem: ${operatingSystem}<br><br>
+  To do this run the command: ${secondText}\<br><br>
+  <input title='I have completed necessary steps' onclick='if(this.checked){haveRead(true)}else{haveRead(false)}' class='startCheckbox' type='checkbox' value='1' autofocus>" +
+  <i>I promise I have done this.</i>`
+
+  return permissionModalText;
+}
+
+//---------------------------------------------------------------------------------------------------
+// haveRead: Used for the permission-modal, called by getPermissionModalText()
+//---------------------------------------------------------------------------------------------------    
+function haveRead(isTrue) {
+    modalRead = isTrue;
+}
+
+//-----------------------------------------------------------------------------------------------------------
+// setFirstPermissionText: Sets the the first text for permission-modal, called by getPermissionModalText()
+//-----------------------------------------------------------------------------------------------------------
+function setFirstPermissionText(operatingSystem){
+  var firstText;
+  switch(operatingSystem){
+    case "Linux":
+      firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www-data'.";
+      break;
+    case "Darwin":
+      firstText = "<h2>Make sure you set ownership of LenaSYS directory to 'www'";
+      break;
+  }
+  return firstText;
+}
+
+//-----------------------------------------------------------------------------------------------------------
+// setSecondPermissionText: Sets the the second text for permission-modal, called by getPermissionModalText()
+//-----------------------------------------------------------------------------------------------------------
+function setSecondPermissionText(operatingSystem){
+  var secondText;
+  switch(operatingSystem){
+    case "Linux":
+      secondText = "sudo chgrp -R www-data " + filePath + "<br>" + "sudo chown -R www-data " + filePath + "</h2><br>";
+      break;
+    case "Darwin":
+      secondText = "sudo chgrp -R www " + filePath + "<br>" + "sudo chown -R www " + filePath + "</h2><br>";
+      break;
+  }
+  return secondText;
+}
 </script>
 
 <div id="header">
