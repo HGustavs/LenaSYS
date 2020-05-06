4e01e59319211608eb82e4e3a1ac23641ad3c34e
nonSH <43380274+nonSH@users.noreply.github.com>
2020-05-06 17:27:53 +0200

    testbjkawrfdadawdawdafgdafaw

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -1002,11 +1002,11 @@ function renderCell(col, celldata, cellid) {
 				//if(celldata.submitted.getTime() !== timeZero.getTime()){
 					if (celldata.grade === null && celldata.submitted.getTime() !== timeZero.getTime()) {
 					str += makeSelect(celldata.gradeSystem, querystring['courseid'], celldata.vers, celldata.lid, celldata.uid, celldata.grade, 'I', celldata.qvariant, celldata.quizId);
-					} 	else if (celldata.grade === -1) {
+					} 	/* else if (celldata.grade === -1) {
 					str += makeSelect(celldata.gradeSystem, querystring['courseid'], celldata.vers, celldata.lid, celldata.uid, celldata.grade, 'IFeedback', celldata.qvariant, celldata.quizId);
-					} 	 else {
+					} */	 else {
 					str += makeSelect(celldata.gradeSystem, querystring['courseid'], celldata.vers, celldata.lid, celldata.uid, celldata.grade, 'U', celldata.qvariant, celldata.quizId);
-					}
+					} 
 				//}
 				str += "<img id='korf' class='fist";
 				if ((celldata.userAnswer === null && !(celldata.quizfile == "feedback_dugga")) || celldata.quizfile == null) { // Always shows fist. Should be re-evaluated
