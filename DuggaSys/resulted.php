f56974a506a9f3b6451a4bb496a7aa4368efd265
a17pioja <a17pioja@student.his.se>
2020-05-07 10:18:54 +0200

    Merge changes2
    Co-Authored-By: a18ismca <a18ismca@users.noreply.github.com>

--- DuggaSys/resulted.php
+++ DuggaSys/resulted.php
@@ -48,7 +48,25 @@ pdoConnect();
   ?>
   <!-- content START -->
 	<div id="content">
-		
+	
+	<div id="resultedLegendContainer">
+		<div id="legendBtn" onclick="showLegend();"> ? </div>
+		<ul class="legendList">
+			<li class="legendListItem"><img src="../Shared/icons/FistV.png" alt="grade"> Show dugga stats</li>
+			<li class="legendListItem"><img src="../Shared/icons/Uh.png" alt="grade"><img src="../Shared/icons/G.png" alt="grade"> Pass</li>
+			<li class="legendListItem"><img src="../Shared/icons/U.png" alt="grade"><img src="../Shared/icons/Gc.png" alt="grade"> Fail</li>
+			<li class="legendListItem"><img src="../Shared/icons/Uc.png" alt="grade"><img src="../Shared/icons/Gc.png" alt="grade"> Pending/not graded</li>
+			<li class="legendListItem"><img src="../Shared/icons/warningTriangle.png" alt="grade"> Submission past deadline</li>
+			<li class="legendListItem"><div> (9) </div> <div>Grading changes</div></li>
+			<li class="legendListItem"><img src="../Shared/icons/not_announced_icon.svg" alt="grade"> Dugga not yet submitted</li>
+			<li class="legendListItem"><div class="dugga-moment"></div> Dugga moment</li>
+			<li class="legendListItem"><div class="dugga-pending-late-submission"></div> Dugga pending: Late submission</li>
+			<li class="legendListItem"><div class="dugga-pending"></div> Dugga pending: Submission in time</li>
+			<li class="legendListItem"><div class="dugga-moment dugga-pass"></div> Moment passed</li>
+			<li class="legendListItem"><div class="dugga-moment dugga-fail"></div> Moment failed</li>
+			<li class="legendListItem"><div class="dugga-moment dugga-assigned"></div> Moment assigned</li>
+		</ul>
+	</div>
     <div id="resultedFormContainer">
 	<div class="titles" style="justify-content:flex-start;">
 			<h1>Edit student results</h1>
