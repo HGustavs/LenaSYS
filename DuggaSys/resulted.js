b85ea00adca781fa4bb97beac51a3b430f9923ce
b18fella <49142651+b18fella@users.noreply.github.com>
2020-05-07 11:42:40 +0200

    Solved bug where last exported date said "undefined"
    Added an if-statement to assure that it won't write it out anyway.
    
    Changed the catch to take in the error message instead.
    
    Co-Authored-By: a18eripi <a18eripi@users.noreply.github.com>

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -686,12 +686,15 @@ function returnedResults(data) {
 // Success return function for LadExport lastGraded
 //----------------------------------------
 function returnedExportedGrades(gradeData){
+	// Tries to write out the last exported date.
+	// If it fails, then log the error.
 	try {
-		document.getElementById('lastExpDate').innerHTML =  gradeData[0].gradeLastExported;
-	  }
-	  catch(err) {
-		console.log("gradeLastExported updated in database");
-	  }
+		if (typeof gradeData[0].gradeLastExported !== 'undefined' && typeof gradeData[0] === 'object') {
+			document.getElementById('lastExpDate').innerHTML = gradeData[0].gradeLastExported;
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 var myTable;
 //----------------------------------------
