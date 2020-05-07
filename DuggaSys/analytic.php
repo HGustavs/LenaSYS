76d3cd183db09c9a834308f14eeffa96d8f47f4c
b18emigu <b18emigu@student.his.se>
2020-05-07 14:27:57 +0200

    Hopefully fixed error on line 80
    I believe the code from previously wasnt even valid PHP code. Should hopefully work better now.
    
    Co-Authored-By: a18felen <a18felen@users.noreply.github.com>

--- DuggaSys/analytic.php
+++ DuggaSys/analytic.php
@@ -53,30 +53,28 @@ $js = array(
 	
 	// Show analytics if user is superuser.
 	if(isset($_SESSION["superuser"]) && $_SESSION["superuser"] == 1){
-?>
-	<!-- content START -->
-	<div id="content">
-		<div class="analytic-buttons">
-			<input class="submit-button" style="float:left" type="button" value="General stats" onclick="loadGeneralStats()">
-			<input class="submit-button" style="float:left" type="button" value="Password guessing" onclick="loadPasswordGuessing()">
-			<input class="submit-button" style="float:left" type="button" value="OS Percentage" onclick="loadOsPercentage()">
-			<input class="submit-button" style="float:left" type="button" value="Browser percentage" onclick="loadBrowserPercentage()">
-			<input class="submit-button" style="float:left" type="button" value="Service usage" onclick="loadServiceUsage()">
-			<input class="submit-button" style="float:left" type="button" value="Service speed" onclick="loadServiceAvgDuration()">
-			<input class="submit-button" style="float:left" type="button" value="Service crashes" onclick="loadServiceCrashes()">
-			<input class="submit-button" style="float:left" type="button" value="File information" onclick="loadFileInformation()">
-			<input class="submit-button" style="float:left" type="button" value="Page information" onclick="loadPageInformation()">
+		echo '
+		<!-- content START -->
+		<div id="content">
+			<div class="analytic-buttons">
+				<input class="submit-button" style="float:left" type="button" value="General stats" onclick="loadGeneralStats()">
+				<input class="submit-button" style="float:left" type="button" value="Password guessing" onclick="loadPasswordGuessing()">
+				<input class="submit-button" style="float:left" type="button" value="OS Percentage" onclick="loadOsPercentage()">
+				<input class="submit-button" style="float:left" type="button" value="Browser percentage" onclick="loadBrowserPercentage()">
+				<input class="submit-button" style="float:left" type="button" value="Service usage" onclick="loadServiceUsage()">
+				<input class="submit-button" style="float:left" type="button" value="Service speed" onclick="loadServiceAvgDuration()">
+				<input class="submit-button" style="float:left" type="button" value="Service crashes" onclick="loadServiceCrashes()">
+				<input class="submit-button" style="float:left" type="button" value="File information" onclick="loadFileInformation()">
+				<input class="submit-button" style="float:left" type="button" value="Page information" onclick="loadPageInformation()">
+			</div>
+				<div id="analytic-info" style="clear: both; padding: 15px;"></div>
+				<div id="canvas-area" style="height: 300px;">
+			</div>
+			
 		</div>
-		<div id="analytic-info" style="clear: both; padding: 15px;"></div>
-		<div id="canvas-area" style="height: 300px;">
-		</div>
-		
-	</div>
-	<!-- content END -->
-	
-<?php
-	}	
-	else{
+		<!-- content END -->
+		';
+	} else {
 		header('Location: courseed.php');
 	}
 ?>
