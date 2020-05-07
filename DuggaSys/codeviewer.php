e0d70b9d5513af3f2e54f2fba40ec03c2f23df8a
Carl-Simon [b17carbr] <b17carbr@student.his.se>
2020-05-07 13:04:53 +0200

    Added logging comments

--- DuggaSys/codeviewer.php
+++ DuggaSys/codeviewer.php
@@ -161,12 +161,12 @@ Testing Link:
 				$userid="00";		// Guest ID is intentionally different from registered users, it begins with a double-zero to indicate guest
 			}
 
-			// Gets username based on uid
+			// Gets username based on uid, USED FOR LOGGING
 			$query = $pdo->prepare( "SELECT username FROM user WHERE uid = :uid");
 			$query->bindParam(':uid', $userid);
 			$query-> execute();
 
-			// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set
+			// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
 			while ($row = $query->fetch(PDO::FETCH_ASSOC)){
 				$username = $row['username'];
 			}
--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -20,12 +20,12 @@ if(isset($_SESSION['uid'])){
 	$userid="guest";
 }
 
-// Gets username based on uid
+// Gets username based on uid, USED FOR LOGGING
 $query = $pdo->prepare( "SELECT username FROM user WHERE uid = :uid");
 $query->bindParam(':uid', $userid);
 $query-> execute();
 
-// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set
+// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
 while ($row = $query->fetch(PDO::FETCH_ASSOC)){
 	$username = $row['username'];
 }
