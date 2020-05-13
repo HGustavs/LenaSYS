d3bb034b5f106d76c244a20cadb129319628bda7
b18alegu <62876260+b18alegu@users.noreply.github.com>
2020-05-13 06:21:10 +0200

    Updated placeholder ssn to a valid one
    Before it was: 999102-5571. Obviously there is no month 91 in a year...

--- DuggaSys/accessed.php
+++ DuggaSys/accessed.php
@@ -87,7 +87,7 @@
       		</div>
       		<div style='padding:5px;'>
       			<input type='hidden' id='uid' value='Toddler' />
-      			<div class='inputwrapper'><span>SSN:</span><input placeholder="999102-5571" class='textinput' type='text' id='addSsn'/></div>
+      			<div class='inputwrapper'><span>SSN:</span><input placeholder="990102-5578" class='textinput' type='text' id='addSsn'/></div>
       			<div class='inputwrapper'><span>First Name:</span><input placeholder="Greger" class='textinput' type='text' id='addFirstname'/></div>
       			<div class='inputwrapper'><span>Last Name:</span><input placeholder="Gregersson" class='textinput' type='text' id='addLastname'/></div>
       			<div class='inputwrapper'><span>CID:</span><input placeholder="91001" class='textinput' id='addCid'></div>
