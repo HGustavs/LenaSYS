414773166da2f647cd23ee014829069471accf31
a18jimol <a18jimol@student.his.se>
2020-05-14 13:39:13 +0200

    Will send coursecode instead of courseid
    When creating a new course-version or editing a existing one the coursecode while now be send and the edit will work

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -449,7 +449,7 @@ function createVersion() {
   param.versname = document.getElementById("versname").value;
   param.motd = document.getElementById("vmotd").value;
   param.copycourse = document.getElementById("copyvers").value;
-  param.coursecode = querystring["courseid"];
+  param.coursecode = retdata.coursecode;
   param.coursename = querystring["coursename"];
   param.makeactive = 2 + $("#makeactive").is(':checked');
   param.startdate = getDateFormat(new Date($("#startdate").val()));
@@ -485,7 +485,7 @@ function updateVersion() {
   param.versid = document.getElementById("eversid").value;
   param.versname = document.getElementById("eversname").value;
   param.copycourse = document.getElementById("copyvers").value;
-  param.coursecode = querystring["courseid"];
+  param.coursecode = retdata.coursecode;
   param.coursename = querystring["coursename"];
   param.makeactive = 2 + $("#emakeactive").is(':checked');
   param.startdate = $("#estartdate").val();
