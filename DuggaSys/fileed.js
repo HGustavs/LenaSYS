1c5513a9b6dc840d9b06fa60b9a7ae49bf7c31c6
a18jenro <a18jenro@student.his.se>
2020-05-15 11:19:24 +0200

    Cleanup

--- DuggaSys/fileed.js
+++ DuggaSys/fileed.js
@@ -32,23 +32,7 @@ var editor;
 var filedata;
 
 function setup() {
-    /*
-    var filt = "";
-    // Add search bar to nav
-    filt += `<td id='searchBar' class='navButt'>`;
-    filt += `<input id='searchinput' type='text' name='search' placeholder='Search..'`;
-    filt += `onkeyup='searchterm=document.getElementById("searchinput").value;searchKeyUp(event);myTable.reRender();'/>`;
-    filt += `<button id='searchbutton' class='switchContent'`;
-    filt += `onclick='searchterm=document.getElementById("searchinput").value;searchKeyUp(event);myTable.reRender();' type='button'>`;
-    filt += `<img id='lookingGlassSVG' style='height:18px;' src='../Shared/icons/LookingGlass.svg'/>`;
-    filt += `</button></td>`;
-
-    $("#menuHook").before(filt);
-    */
-    
-    AJAXService("GET", {
-        cid: querystring['courseid']
-    }, "FILE");
+    AJAXService("GET", {cid: querystring['courseid']}, "FILE");
 }
 
 window.onresize = function () {
