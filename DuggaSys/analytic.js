b816a1b8d1d80413644f517d45f514b862bb0864
a18eliek <a18eliek@student.his.se>
2020-05-19 14:33:16 +0200

    Added null check

--- DuggaSys/analytic.js
+++ DuggaSys/analytic.js
@@ -666,7 +666,10 @@ function loadPageInformation() {
         for (var i = 0; i < data['percentage'][page].length; i++) {
 			numberOfCourses = parseInt(data['percentage'][page].length);
 			
-			data['percentage'][page][i].courseid = data['percentage'][page][i].refer.match('.+?cid=([0-9]+)')[1];
+			var cid = data['percentage'][page][i].refer.match('.+?cid=([0-9]+)');
+			if(cid != null) {
+				data['percentage'][page][i].courseid = cid[1];
+			}
 			
 			courseID.push([
                 data['percentage'][page][i].courseid
