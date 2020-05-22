eb59d52e1e8bfac876f69dc4049f9e7a58222776
Kasper Witt <62878146+a18kaswi@users.noreply.github.com>
2020-05-22 11:36:39 +0200

    Removed redundant connection and rewrote pdoConnect() to connect to postgres

--- Shared/database.php
+++ Shared/database.php
@@ -23,31 +23,17 @@ function err ($errmsg,$hdr='')
 	exit;
 }
 
-function dbConnect()
-{
-	$printHeaderFunction=0;
-	// Send header info to err()?
-	if ($printHeaderFunction) {
-		$hdr = 'Database Connection Error';
-	} else {
-		$hdr = '';
-	}
-
-	// Connect to DB server
-	$OC_db = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or err("Could not connect to database ".mysql_errno(),$hdr);
-	mysql_set_charset('utf8',$OC_db);
-	// Select DB
-	mysql_select_db(DB_NAME) or err("Could not select database \"".DB_NAME."\" error code".mysql_errno(),$hdr);
-}
-
 function pdoConnect()
 {
 	global $pdo;
 	try {
-		$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASSWORD);
-		if(!defined("MYSQL_VERSION")) {
-			define("MYSQL_VERSION",$pdo->query('select version()')->fetchColumn());
-		}
+    $serverName = DB_HOST;
+    $databaseName = DB_NAME;
+    $dbLowerCase = strtolower($databaseName);
+    $username = DB_USER;
+    $password = DB_PASSWORD;
+
+    $pdo = new PDO("pgsql:host=$serverName; dbname=$dbLowerCase; user=$username; password=$password");
 	} catch (PDOException $e) {
 		echo "Failed to get DB handle: " . $e->getMessage() . "</br>";
 		exit;
