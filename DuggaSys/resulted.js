73ba940b75a9b3ac267357c67af29004d18b495c
a18oscte <a18oscte@users.noreply.github.com>
2020-05-25 11:37:50 +0200

    #9578 Fixed bug when the resultedLegendContainer will sometimes be covered by the table column headings.

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -1805,7 +1805,7 @@ $(window).scroll(function() {
 	var scrolled = $(this).scrollLeft();
 	var legendBox = $('#resultedLegendContainer');
 	var $win = $(window);
-	if((scrolled + ladExportWidth) < resultTableWidth){
+	if((scrolled + ladExportWidth) <= resultTableWidth){
 		$('#resultedFormContainer').css({
 			'transform': 'translateX(' + scrolled +'px'+ ')'
 		});
