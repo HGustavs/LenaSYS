6bce9c65efc58eb2d8b2fc84c890ab2d20ef7d8f
a18jimol <a18jimol@student.his.se>
2020-05-27 11:58:56 +0200

    Creating the dropdown in one place for all users that should see it and creating the FAB-buttons in one place

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -610,7 +610,7 @@ function returnedSection(data) {
 
     var str = "";
 
-    if (data['writeaccess']) {
+    if (data['readaccess'] || data['studentteacher'] || data['writeaccess']) {
       // Build dropdowns
       var bstr = "";
       for (var i = 0; i < retdata['versions'].length; i++) {
@@ -628,6 +628,10 @@ function returnedSection(data) {
         if (querystring['coursevers'] == item['vers']) versnr = item['vers'];
       }
 
+    }
+
+    if (data['writeaccess'] || data['studentteacher']) {
+      
       document.getElementById("courseDropdownTop").innerHTML = bstr;
       bstr = "<option value='None'>None</option>" + bstr;
       document.getElementById("copyvers").innerHTML = bstr;
@@ -641,55 +645,6 @@ function returnedSection(data) {
       // Hide FAB / Menu
       document.getElementById("FABStatic").style.display = "None";
       document.getElementById("FABStatic2").style.display = "None";
-    }
-
-    if (data['readaccess']) {
-      // Build dropdowns
-      var bstr = "";
-      for (var i = 0; i < retdata['versions'].length; i++) {
-        var item = retdata['versions'][i];
-        if (retdata['courseid'] == item['cid']) {
-          bstr += "<option value='" + item['vers'] + "'";
-          if (retdata['coursevers'] == item['vers']) {
-            bstr += " selected";
-          }
-          bstr += ">" + item['versname'] + " - " + item['vers'] + "</option>";
-        }
-        // save vers, versname and motd from table vers as global variables.
-        versnme = versionname;
-        if (querystring['coursevers'] == item['vers']) motd = item['motd'];
-        if (querystring['coursevers'] == item['vers']) versnr = item['vers'];
-      }
-
-      bstr = "<option value='None'>None</option>" + bstr;
-      document.getElementById("copyvers").innerHTML = bstr;
-    }
-
-    if (data['studentteacher']) {
-      // Build dropdowns
-      var bstr = "";
-      for (var i = 0; i < retdata['versions'].length; i++) {
-        var item = retdata['versions'][i];
-        if (retdata['courseid'] == item['cid']) {
-          bstr += "<option value='" + item['vers'] + "'";
-          if (retdata['coursevers'] == item['vers']) {
-            bstr += " selected";
-          }
-          bstr += ">" + item['versname'] + " - " + item['vers'] + "</option>";
-        }
-        // save vers, versname and motd from table vers as global variables.
-        versnme = versionname;
-        if (querystring['coursevers'] == item['vers']) motd = item['motd'];
-        if (querystring['coursevers'] == item['vers']) versnr = item['vers'];
-      }
-
-      document.getElementById("courseDropdownTop").innerHTML = bstr;
-      bstr = "<option value='None'>None</option>" + bstr;
-      document.getElementById("copyvers").innerHTML = bstr;
-      // Show FAB / Menu
-      document.getElementById("FABStatic").style.display = "Block";
-      // Show addElement Button
-      document.getElementById("addElement").style.display = "Block";
     }
 
 
