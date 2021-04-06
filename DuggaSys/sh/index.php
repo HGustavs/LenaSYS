4ee89e5e02066f474b185f50e92acbb9a5d1afb9
Philip Karlsson <81557308+b18phika@users.noreply.github.com>
2021-04-06 14:50:35 +0200

    Add local .htaccess-file to redirect non-existent pages to index.php

--- DuggaSys/sh/index.php
+++ DuggaSys/sh/index.php
@@ -3,12 +3,22 @@
 date_default_timezone_set("Europe/Stockholm");
 
 // Include basic application services
-include_once "../Shared/basic.php";
+include_once "../../Shared/basic.php";
 
 $url = getOPG("c");
 $assignment = getOPG("a");
 
+
+$old_url = $_SERVER['REQUEST_URI'];
+$course = end(explode("/", $old_url));
+
+
 echo "|".$url."|".$assignment."|";
+
+if ($course = "Databaskonstruktion") {
+	header("Location: https://dugga.iit.his.se/DuggaSys/sectioned.php?courseid=4&coursevers=82452");
+	exit();
+}
 
 if($assignment != "UNK"){
 	if(($url == "Databaskonstruktion" || $url == "dbk")){
@@ -28,3 +38,5 @@ if($assignment != "UNK"){
 }
 
 echo "404 Course/Assignment does not exist!";
+
+// RedirectMatch "/sh/[^(index.php)]" "/LenaSYS/lenaSYS/sh/index.php"
