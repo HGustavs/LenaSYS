ff672c5e7c981ef71586ad4b572404c01af20675
Gustav Bäckman <a19gusba@student.his.se>
2021-04-07 11:26:04 +0200

    added hash to saveDuggaResult()
    found a better way of adding hash after saving by adding it to dugga.js file instead of each individual dugga.

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -534,6 +534,10 @@ function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
 //----------------------------------------------------------------------------------
 function saveDuggaResult(citstr)
 {
+
+	var hash = generateHash();
+	console.log("asd: " + hash)
+
 	var readonly;
 	$.ajax({
 		url: "courseedservice.php",
@@ -596,6 +600,39 @@ function saveDuggaResult(citstr)
 			showReceiptPopup();
 		}
 	});
+}
+
+function generateHash() {
+    var randNum = getRandomNumber();
+    var hash = createHash(randNum);
+    return decimalToHexString(hash);
+
+	function createHash(num) {
+		var string = num.toString();
+		var hash = 0;
+	
+		if (string.length == 0) return hash;
+	
+		for (i = 0; i < string.length; i++) {
+			char = string.charCodeAt(i);
+			hash = ((hash << 5) - hash) + char;
+			hash = hash & hash;
+		}
+	
+		return hash;
+	}
+	
+	function decimalToHexString(number) {
+		if (number < 0) {
+			number = 0xFFFFFFFF + number + 1;
+		}
+	
+		return number.toString(16).toUpperCase();
+	}
+	
+	function getRandomNumber() {
+		return Math.floor(Math.random() * 1000000) + 100000;
+	}
 }
 
 //----------------------------------------------------------------------------------
