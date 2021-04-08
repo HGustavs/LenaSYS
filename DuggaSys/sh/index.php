c1ba7ca99dde33ce8e0464545b958e0923317298
simalm <simon.studet@gmail.com>
2021-04-08 15:21:29 +0200

    Update w15

--- DuggaSys/sh/index.php
+++ DuggaSys/sh/index.php
@@ -5,13 +5,42 @@ date_default_timezone_set("Europe/Stockholm");
 // Include basic application services
 include_once "../../Shared/basic.php";
 
-$url = getOPG("c");
+$course = getOPG("c");
 $assignment = getOPG("a");
 
-echo "|".$url."|".$assignment."|";
+//====================================================//
+//database values needed are: cid, coursename, activeversion, possibly courseyear?
+//
+//====================================================//
 
-if($assignment != "UNK"){
-	if(($url == "Databaskonstruktion" || $url == "dbk")){
+echo "|".$course."|".$assignment."|";
+
+switch ($course) {
+	case ("dg"):
+	header("Location: /LenaSYS/DuggaSys/sectioned.php?courseid=3&coursename=Datorns%20grunder&coursevers=1337");
+	exit();
+
+	case ("demo"):
+	header("Location: /LenaSYS/DuggaSys/sectioned.php?courseid=1894&coursename=Demo-Course&coursevers=52432");
+	exit();
+
+	case ("se"):
+	header("Location: /LenaSYS/DuggaSys/sectioned.php?courseid=4&coursename=Software%20Engineering&coursevers=1338");
+	exit();
+
+	case ("wp"):
+	header("Location: /LenaSYS/DuggaSys/sectioned.php?courseid=1&coursename=Webbprogrammering&coursevers=45656");
+	exit();
+
+	case ("dgfk"):
+	header("Location: /LenaSYS/DuggaSys/sectioned.php?courseid=2&coursename=Webbutveckling%20-%20datorgrafik&coursevers=97732");
+	exit();
+
+}
+
+
+/*if($assignment != "UNK"){
+	if(($course == "Databaskonstruktion" || $course == "dbk")){
 		if($assignment=="a1"){
 			header("Location: https://dugga.iit.his.se/DuggaSys/showdoc.php?cid=4&coursevers=82452&fname=minimikrav_m1a.md");
 			exit();		
@@ -21,10 +50,10 @@ if($assignment != "UNK"){
 		}
 	}
 }else{
-	if(($url == "Databaskonstruktion" || $url == "dbk")){
+	if(($course == "Databaskonstruktion" || $course == "dbk")){
 		header("Location: https://dugga.iit.his.se/DuggaSys/sectioned.php?courseid=4&coursevers=82452");
 		exit();	
 	}
-}
+}*/
 
 echo "404 Course/Assignment does not exist!";
