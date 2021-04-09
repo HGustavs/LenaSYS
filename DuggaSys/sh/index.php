3391f75cc05bc98369cb2f6a9bfd51f91798760a
simalm <simon.studet@gmail.com>
2021-04-09 15:42:04 +0200

    Changed from NULL to UNK

--- DuggaSys/sh/index.php
+++ DuggaSys/sh/index.php
@@ -21,52 +21,51 @@ SELECT cid, coursename, activeversion, coursecode FROM course WHERE '%course%' L
 
 
 function courseQuery($course){
-global $pdo;
-$c = '"%' . $course . '%"';
-$sql = "SELECT cid, coursename, activeversion, coursecode 
- FROM course 
- WHERE cid LIKE " . $c . " OR coursename LIKE " . $c . 
- " OR activeversion LIKE " . $c . 
- " OR coursecode LIKE " . $c . "
- AND visibility=1";
-$array = array();
+	global $pdo;
+	$c = '"%' . $course . '%"';
+	$sql = "SELECT cid, coursename, activeversion, coursecode 
+	 FROM course 
+	 WHERE cid LIKE " . $c . " OR coursename LIKE " . $c . 
+	 " OR activeversion LIKE " . $c . 
+	 " OR coursecode LIKE " . $c . "
+	 AND visibility=1";
+	$array = array();
 
-foreach ($pdo->query($sql) as $row) {
-	$array['cid'] = $row['cid'];
-	$array['coursename'] = $row['coursename'];
-	$array['coursecode'] = $row['coursecode'];
-	$array['courseservers'] = $row['activeversion'];
-}
-return $array;
-
+	foreach ($pdo->query($sql) as $row) {
+		$array['cid'] = $row['cid'];
+		$array['coursename'] = $row['coursename'];
+		$array['coursecode'] = $row['coursecode'];
+		$array['courseservers'] = $row['activeversion'];
+	}
+	return $array;
 }
 
 function assignmentQuery($assignment){
-global $pdo;
-$a = '"%' . $assignment . '%"';
-$sql = "SELECT filename, cid FROM filelink WHERE filename LIKE " . $a;
-$array = array();
+	global $pdo;
+	$a = '"%' . $assignment . '%"';
+	$sql = "SELECT filename, cid FROM filelink WHERE filename LIKE " . $a;
+	$array = array();
 
-foreach ($pdo->query($sql) as $row) {
-	$array['filename'] = $row['filename'];
-	$array['cid'] = $row['cid'];
-}
-return $array;
+	foreach ($pdo->query($sql) as $row) {
+		$array['filename'] = $row['filename'];
+		$array['cid'] = $row['cid'];
+	}
+	return $array;
 }
 
 function queryToUrl($course, $assignment){
-global $pdo;
-if($course != NULL)
-	$c = courseQuery($course);
-else echo "No such course";
+	global $pdo;
+	if($course != 'UNK')
+		$c = courseQuery($course);
+	else echo "No such course";
 
-if($assignment != NULL){
-	$a = assignmentQuery($assignment);
-	$url = "/LenasSYS/DuggaSys/showdoc.php?cid=" . $a['cid'] ."&coursevers=" . $c['courseservers'] ."&fname=" . $a['filename'];
-}
-else $url = "/LenaSYS/DuggaSys/sectioned.php?courseid=" . $c['cid'] ."&coursename=" . $c['coursename'] . "&coursevers=" .  $c['courseservers'];
+	if($assignment != 'UNK'){
+		$a = assignmentQuery($assignment);
+		$url = "/LenasSYS/DuggaSys/showdoc.php?cid=" . $a['cid'] ."&coursevers=" . $c['courseservers'] ."&fname=" . $a['filename'];
+	}
+	else $url = "/LenaSYS/DuggaSys/sectioned.php?courseid=" . $c['cid'] ."&coursename=" . $c['coursename'] . "&coursevers=" .  $c['courseservers'];
 
-return $url; 
+	return $url; 
 }
 
 header("Location: ". queryToUrl($course, $assignment));
