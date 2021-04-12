b9e38c2bf29493a9b58aa41f09204df588bd140b
a18osken <a18osken@student.his.se>
2021-04-12 14:32:57 +0200

    Update dugga.js
    Fixed a missing bracket that broke dugga.js execution

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1722,6 +1722,7 @@ function findfilevers(filez,cfield,ctype,displaystate,group)
 		tab+="</table>"
 
 		document.getElementById(cfield+"Prev").innerHTML=tab;
+	}
 }
 
 function makeForm(cfield, ctype){
@@ -2016,3 +2017,4 @@ function sendFeedback(entryname){
 function returnedSubmitFeedback(){
 	$('#submitstatus').css({'color':'var(--color-green)',"display": "inline-block"}).text("Feedback saved");
 }
+
