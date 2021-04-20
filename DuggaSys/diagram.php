67c588a5431f547425b330a5700e8bceefeea9bd
a19steis <81557235+a19steis@users.noreply.github.com>
2021-04-20 10:42:43 +0200

    Replaced "enable grid" button
    Replaced the "enable grid" button with a more appropriate svg image.

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -518,7 +518,9 @@
         <div id ="fieldsetBox">
             <fieldset id='cursorModeFieldset'>
                 <p id="text_currentlyPlacing">Now placing: NULL</p><br>
-                <input class="paneButtons" id="gridButton" type="button" value="Enable Grid" onclick='enableGrid()' /><br>
+                <div class="diagramIcons" onclick='enableGrid()'>
+                    <img src="../Shared/icons/diagram_grid.svg"/>
+                </div>
             </fieldset>
             <fieldset id='propertyFieldset'>
                 
