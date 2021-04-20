ca384a5410421589f90740216abfb11bf73405fe
a18willi <a18willi@student.his.se>
2021-04-20 15:40:48 +0200

    Update sectioned.js
    Changed the color of tthe swimlane text to be more readable
    
    Co-Authored-By: Philip Karlsson <81557308+b18phika@users.noreply.github.com>

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1387,7 +1387,7 @@ function drawSwimlanes() {
         //If deadline is older than current, red text for late assigment should be displayed as orange instead
         var deadlineYear = new Date(entry.deadline).getFullYear();
         if(deadlineYear < current.getFullYear()) {
-           textcol = "#ff9933";
+           textcol = "#5072C7";
        
            var yearDifference = current.getFullYear() - deadlineYear;
            var newYear = new Date(entry.deadline);
