f3a06d447367ea368ce0219630c6d0ec0695d984
a18osken <a18osken@student.his.se>
2021-04-21 13:47:50 +0200

    Template Literal
    Added template literal

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1462,7 +1462,7 @@ function drawSwimlanes() {
         str += `<rect opacity='0.7' x='${(startday * daywidth)}' y='${(weeky)}' width='
         ${(tempVariable)}' height='${weekheight}' fill='${fillcol}' />`;
 
-        str += `<text x='" + (12) + "' y='${(weeky + 18)}' font-family='Arial'
+        str += `<text x='${(12)}' y='${(weeky + 18)}' font-family='Arial'
         font-size='12px' fill='${textcol}' text-anchor='left'> <title>${entry.text}
         </title>${entry.text}</text>`;
       }
