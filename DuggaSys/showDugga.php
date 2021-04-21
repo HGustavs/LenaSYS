e64ff8684b77faebee3cb352886a49fda1332e9f
a18osken <a18osken@student.his.se>
2021-04-21 11:55:47 +0200

    Revert "Merge pull request #10239 from HGustavs/G1-2021-W16-#10141"
    This reverts commit d5ce2008842a146183887d1c662c84ce7404088e, reversing
    changes made to f934927da04a6b60b10c705af3e8124ae123e684.

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -53,11 +53,6 @@
 	$moment=getOPG('moment');
 	$courseid=getOPG('courseid');
 	
-	if(isset($_SESSION['hashpassword'])){
-		$hashpassword=$_SESSION['hashpassword'];
-	}else{
-		$hashpassword='UNK';
-	}
 
 	if(isset($_SESSION['uid'])){
 		$userid=$_SESSION['uid'];
@@ -134,50 +129,40 @@ if($cid != "UNK") $_SESSION['courseid'] = $cid;
 		$noup="SECTION";
 		include '../Shared/navheader.php';
 	?>
-	<div id='login_popup'>
-<?php
 
-function hashPassword($password, $hash){
-		global $pdo;
-		$sql = "SELECT hash,password FROM useranswer WHERE '" .$password. "' LIKE password AND '".$hash."' LIKE hash";
-		$query = $pdo->prepare($sql);
-		$query->execute();
-		$count = $query->rowCount();
-			if($count == 0){
-				return false;
-			} else{
-				return true;
-			}
-}
-	
-//Saved Dugga Login
-if($hash!='UNK'){
-	if(!hashPassword($hashpassword, $hash)){
-		echo "<div class='loginBoxContainer' id='hashBox' style='display:block;'>";	
-		echo "<div class='loginBox' style='max-width:400px; margin: 20% auto;'>";
-		echo "<div class='loginBoxheader'>";
-		echo "<h3>Login for Saved Dugga</h3>";
-		echo "<div onclick='hideHashBox()' class='cursorPointer'>x</div>";
-		echo "</div>";
-		echo "<p id='passwordtext'>Enter your password for the hash:</p>";
-		echo "<p id='hash' style='font-weight: bold;'>$hash</p>";
-		echo "<input id='passwordfield' name='password' class='textinput' type='password' placeholder='Password'>";
-		echo "<input type='submit' class='submit-button' value='Confirm' name='Confirm' onclick='checkHashPassword()'>";
-		echo "</div>";
-		echo "</div>";
-		exit();
-	}
-}
-//Remove if you want the password to be persistent.
-$_SESSION['hashpassword'] = 'UNK';
-
-?>
-	</div>
 	<!-- content START -->
 	<div id="content">
 		<?php
 			// Log USERID for Dugga Access
+
+      // commented out because we are unsure about the usage of logs
 			//makeLogEntry($userid,1,$pdo,$cid." ".$vers." ".$quizid." ".$duggafile);
+  
+			//Saved Dugga Login 
+			if($hash!='UNK'){
+				echo "<div class='loginBoxContainer' id='hashBox' style='display:block;'>";	
+				echo "<div class='loginBox' style='max-width:400px; margin: 20% auto;'>";
+				echo "<div class='loginBoxheader'>";
+				echo "<h3>Login for Saved Dugga</h3>";
+				echo "<div onclick='hideHashBox()' class='cursorPointer'>x</div>";
+				echo "</div>";
+				echo "<p>Enter your password for the hash</p>";
+				echo "<input name='password' class='textinput' type='password' placeholder='Password'>";
+				echo "<input type='submit' class='submit-button' value='Confirm' onclick='hideHashBox()'>";
+				echo "</div>";
+				echo "</div>";
+			}
+
+			function hashPassword($password, $hash){
+				if($hash!='UNK'){
+				//exit();
+				//Authentication Function
+				}
+			}
+			//Retrieved from 'password' input field
+			hashPassword($password, $hash);
+
+
 			// Put information in event log irrespective of whether we are allowed to or not.
 			// If we have access rights, read the file securely to document
 			// Visibility: 0 Hidden 1 Public 2 Login 3 Deleted
