f1d8cf46c60c357c3dda4a9618e69fe92ac4ad2f
a17alfja <a17alfja@student.his.se>
2021-04-22 10:32:59 +0200

    Revert "Merge branch 'G1-2021-#10285' of https://github.com/HGustavs/LenaSYS into G1-2021-#10285"
    This reverts commit 4025a01c87dfa1c51da68c3c5c7febeb6a061329, reversing
    changes made to 45898f4ffc5310e65c4955634d010ba6155ef5c0.

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -21,7 +21,6 @@ var iconFlag = false;
 var ishashindb;
 var blockhashgen = false;
 var ishashinurl;
-var itemvalue;
 
 $(function () {  // Used to set the position of the FAB above the cookie message
 	if(localStorage.getItem("cookieMessage")!="off"){
@@ -457,40 +456,6 @@ function setExpireCookieLogOut() {
         document.cookie = "sessionEndTimeLogOut=expireC; expires=" + expireDate.toUTCString() + "; path=/";
     }
 }
-
-//Creates TTL for localstorage //TTL value is in milliseconds
-function setExpireTime(key, value, ttl){
-	const now = new Date();
-
-	//Item is an object which contains the original value
-	//as well as the time when its supposed to expire
-	const item = {
-		value: value,
-		expiry: now.getTime() + ttl,
-	}
-	
-	localStorage.setItem(key, JSON.stringify(item))
-}
-//Lazily expiring the item (Its only checked when retrieved from storage)
-function getExpireTime(key){
-	const itemString = localStorage.getItem(key)
-	
-	if(!itemString){
-		return null
-	}
-	const item = JSON.parse(itemString)
-	const now = new Date()
-
-	if(now.getTime() > item.expiry){
-		console.log(key + " has expired")
-		localStorage.removeItem(key)
-		return null
-	}
-	itemvalue = item.value;
-	console.log("item value: "+itemvalue);
-	return item.value;
-}
-
 
 //----------------------------------------------------------------------------------
 function closeWindows(){
@@ -993,16 +958,15 @@ function AJAXService(opt,apara,kind)
 				success: returnedSection
 			});
 	}else if(kind=="PDUGGA"){
-			if(localStorage.getItem("variantSize") == null) {
-				localStorage.setItem("variantSize", 100);
+		if(localStorage.getItem("variantSize") == null) {
+			localStorage.setItem("variantSize", 100);
+		}
+		var newInt = +localStorage.getItem('variantSize');
+		if(querystring['did'] <= newInt) {
+			if(localStorage.getItem(querystring['did']) == null){
+				localStorage.setItem(querystring['did'], 0);
 			}
-			var newInt = +localStorage.getItem('variantSize');
-			if(querystring['did'] <= newInt) {
-				if(localStorage.getItem(querystring['did']) == null){
-					localStorage.setItem(querystring['did'], 0);
-				}
-			}
-			//var test = JSON.parse(localStorage.getItem(querystring['did']));
+		}
 			$.ajax({
 				url: "showDuggaservice.php",
 				type: "POST",
@@ -1016,16 +980,13 @@ function AJAXService(opt,apara,kind)
 						hash = generateHash();	//Old hash gets replaced by new hash before saving to database.
 					}
 					// Check localstorage variants.
-					var newvariant = data['variant'];
+					var newvariants = data['variant'];    
 					if(localStorage.getItem(querystring['did']) == 0){
-						localStorage.setItem(querystring['did'], newvariant);
-						//The big number below represents 30 days in milliseconds
-						//setExpireTime(querystring['did'], localStorage.getItem(querystring['did']), 5000);
+						localStorage.setItem(querystring['did'], newvariants);
 					}
-					//getExpireTime(querystring['did']);
 					var variantsize = data['variantsize'];
 					localStorage.setItem("variantSize", variantsize);
-				}
+					}
 			});
 	}else if(kind=="RESULT"){
 			$.ajax({
