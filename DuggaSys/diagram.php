9433d9be22043641bd0af4fbc623909711af19fb
a19steis <81557235+a19steis@users.noreply.github.com>
2021-04-22 09:17:00 +0200

    Update
    Fixed indentation

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -81,13 +81,13 @@
             </div>
         </fieldset>
         <fieldset>
-        <legend>Toggle</legend>
-        <div id="gridToggle" class="diagramIcons" onclick='toggleGrid()'>
-                    <img src="../Shared/icons/diagram_grid.svg"/>
-                </div>
-                <div id="rulerToggle" class="diagramIcons active" onclick='toggleRuler()'>
-                    <img src="../Shared/icons/diagram_ruler.svg"/>
-                </div>
+            <legend>Toggle</legend>
+            <div id="gridToggle" class="diagramIcons" onclick='toggleGrid()'>
+                <img src="../Shared/icons/diagram_grid.svg"/>
+            </div>
+            <div id="rulerToggle" class="diagramIcons active" onclick='toggleRuler()'>
+                <img src="../Shared/icons/diagram_ruler.svg"/>
+            </div>
         </fieldset>
     </div>
 
