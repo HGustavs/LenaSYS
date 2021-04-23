22dc175e58ecd699c4eebbed942e0a2b9b06bf3a
b19jenda <b19jenda@student.his.se>
2021-04-23 13:42:07 +0200

    fixed requested changes and added ctrlKey

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -721,7 +721,7 @@ document.addEventListener('keydown', function (e)
     }
     // If the active element in DOM is an "INPUT" "SELECT" "TEXTAREA"
     if ( /INPUT|SELECT|TEXTAREA/.test(document.activeElement.nodeName.toUpperCase())) {
-        if (e.key == keybinds.ENTER.key) {
+        if (e.key == keybinds.ENTER.key && e.ctrlKey == keybinds.ENTER.ctrl) {
             changeState();
             saveProperties();
         }
