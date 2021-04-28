05e5a34c94c338e6230869ed05b0506ac822c282
a16erilj <81558891+a16erilj@users.noreply.github.com>
2021-04-28 17:14:16 +0200

    Update login.php

--- Canvas/login.php
+++ Canvas/login.php
@@ -1,8 +1,41 @@
 <?php
     $token = file_get_contents('../../../secrets/canvas_token.txt', true);
 
-    $url = 'http://canvas.webug.his.se/api/v1/courses?access_token=' . $token;
-    $json = file_get_contents($url);
-    $json_data = json_decode($json);
-    print_r($json_data);
+    function courses(){
+        $token = $GLOBALS["token"];
+        $url = "http://canvas.webug.his.se/api/v1/courses?access_token={$token}";
+        $json = file_get_contents($url);
+        $json_data = json_decode($json, true);
+        $temp = array();
+        foreach ($json_data as $key=>$value) {
+            array_push($temp, $value);
+         }
+        return $temp;
+    }
+
+    function assignments($course){
+        $token = $GLOBALS["token"];
+        $url = "http://canvas.webug.his.se/api/v1/courses/{$course['id']}/assignments?access_token={$token}";
+        $json = file_get_contents($url);
+        $json_data = json_decode($json, true);
+        $temp = array();
+        foreach ($json_data as $key=>$value) {
+            array_push($temp, $value);
+         }
+         return $temp;
+     }
+
+    $courses = courses();
+    foreach ($courses as $course){
+        $assignments = assignments($course);
+        foreach($assignments as $assignment){
+            echo <<<EOL
+            <h3>Course: {$course['name']} <br>
+            Assignment: {$assignment['name']}</h3>
+            URL : {$assignment['html_url']}
+            <hr>
+            EOL;
+        }
+    }
+
 ?>
