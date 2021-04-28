0c5aa21d97d65fd1410de34c48f4e69bc73d9aec
a16erilj <81558891+a16erilj@users.noreply.github.com>
2021-04-28 12:56:13 +0200

    Update login.php

--- Canvas/login.php
+++ Canvas/login.php
@@ -1,3 +1,8 @@
 <?php
+    $token = file_get_contents('../../../secrets/canvas_token.txt', true);
 
+    $url = 'http://canvas.webug.his.se/api/v1/courses?access_token=' . $token;
+    $json = file_get_contents($url);
+    $json_data = json_decode($json);
+    print_r($json_data);
 ?>
