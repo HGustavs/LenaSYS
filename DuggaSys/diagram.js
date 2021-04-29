3a3f849c7aab1d834f9852568488518d7ed0755c
a19karal <62888356+a19karal@users.noreply.github.com>
2021-04-29 15:28:08 +0200

    issue 10540
    fixed issue 10540

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1592,6 +1592,7 @@ function updateSelection(ctxelement)
     }
     // If CTRL is not pressed and a element has been selected.
     else if (ctxelement != null) {
+        clearContextLine();
         // Element not already in context
         if (!context.includes(ctxelement) && context.length < 1) {
             context.push(ctxelement);
