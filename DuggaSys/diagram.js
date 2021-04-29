654e83473a44a816c82ac2fa9667511187df5f6a
a19steis <81557235+a19steis@users.noreply.github.com>
2021-04-29 09:43:32 +0200

    Update
    Fixed a misunderstanding, the function to actually pan works now.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -890,8 +890,13 @@ function mdown(event)
 {
     // If the middle mouse button (mouse3) is pressed set scroll start values
     if(event.button == 1) {
+        pointerState = pointerStates.CLICKED_CONTAINER;
+        sscrollx = scrollx;
+        sscrolly = scrolly;
+        startX = event.clientX;
+        startY = event.clientY;
         event.preventDefault();
-        return false;
+        return;
     }
 
     // React to mouse down on container
