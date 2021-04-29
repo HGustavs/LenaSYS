acc4ce5f54b9191ded21970e050a16c382918d36
b18mohah <62936358+B18mohah@users.noreply.github.com>
2021-04-29 16:00:47 +0200

    Update login.php

--- Canvas/login.php
+++ Canvas/login.php
@@ -1,7 +1,7 @@
 <?php
     $local = "../../../secrets/canvas_token.txt";
     $server =  "";
-    
+    // looks after if the tokens are local or form the server, also search if we have secret canvas file saved ocally.
     if (file_exists($local)){
         $token = file_get_contents($local, true);
     }  elseif (file_exists($server)){
