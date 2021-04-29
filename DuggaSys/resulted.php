7fd3c26c5cb42ada173e90f0ad17eedbb98b3f5a
b19ponka <b19ponka@student.his.se>
2021-04-29 11:24:40 +0200

    Update resulted.js and resulted.php
    Added dropdown for course assignment
    
    Co-Authored-By: a13zanal <62987694+a13zanal@users.noreply.github.com>

--- DuggaSys/resulted.php
+++ DuggaSys/resulted.php
@@ -120,12 +120,12 @@ pdoConnect();
 					<option value="Filter-VG">VG</option>
 					<option value="Filter-U">U</option>
 				</select>
-			</div>
-			<div class="resultedFormsFlex">
+				</div>
+				<div class="resultedFormsFlex">
 					<label>Assignment</label>
-					<select name="assignmentDropdown" id="assignmentDropdown"></select>
-			</div>
-			<div class="resultedFormsFlex">
+					<select id="assignmentDropdown" onchange="updateTable()"></select>
+				</div>
+				<div class="resultedFormsFlex">
 					<label>Examinator</label>
 					<select name="teacherDropdown" id="teacherDropdown" onchange="updateTable()"></select>
 			</div>
