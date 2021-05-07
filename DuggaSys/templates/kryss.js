fd95f084d6e416ebaa4ff336537213ace54d8f06
b18mohah <62936358+B18mohah@users.noreply.github.com>
2021-05-07 11:12:00 +0200

    Update kryss.js
    reset for frågadugga 1

--- DuggaSys/templates/kryss.js
+++ DuggaSys/templates/kryss.js
@@ -117,6 +117,13 @@ function getCheckedBoxes(){
 
 	}
 
+	function reset(){
+		var answers = document.getElementsByName("answers"+idunique);
+		for(i = 0; i < answers.length; i++){
+			answers[i].checked = false;
+		}
+	}
+
 function saveClick()
 {
 	$.ajax({									//Ajax call to see if the new hash have a match with any hash in the database.
