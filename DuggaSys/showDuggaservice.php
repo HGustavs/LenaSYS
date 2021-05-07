05eb30f35246945891b72114a3c1f535d6c12d38
a19rebsa <62889999+a19rebsa@users.noreply.github.com>
2021-05-07 11:31:19 +0200

    Update showDuggaservice.php
    Changed debug to error_log

--- DuggaSys/showDuggaservice.php
+++ DuggaSys/showDuggaservice.php
@@ -120,9 +120,9 @@ $query = $pdo->prepare("SELECT visibility FROM course WHERE cid=:cid");
 $query->bindParam(':cid', $courseid);
 $result = $query->execute();
 if($row = $query->fetch(PDO::FETCH_ASSOC)){
-		$cvisibility=$row['visibility'];
+	$cvisibility=$row['visibility'];
 }else{
-		$debug="Error reading course visibility";
+	error_log("Error reading course visibility", 0);
 }
 // Read visibility of dugga (listentry)
 $query = $pdo->prepare("SELECT visible FROM listentries WHERE cid=:cid and lid=:moment");
@@ -130,9 +130,9 @@ $query->bindParam(':cid', $courseid);
 $query->bindParam(':moment', $moment);
 $result = $query->execute();
 if($row = $query->fetch(PDO::FETCH_ASSOC)){
-		$dvisibility=$row['visible'];
-}else{
-		$debug="Error reading dugga visibility";
+	$dvisibility=$row['visible'];
+}else{	
+	error_log("Error reading course visibility", 0);
 }
 
 // Get type of dugga
