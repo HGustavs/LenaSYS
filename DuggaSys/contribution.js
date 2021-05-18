085b30bb0301f9f7d56e96a96f31a1908bb097e6
b16oskin <oskar.h.nilsson@gmail.com>
2021-05-18 14:36:01 +0200

    Added legend content
    Added legend content

--- DuggaSys/contribution.js
+++ DuggaSys/contribution.js
@@ -304,15 +304,15 @@ function renderLineDiagram(data) {
   str += weekchoice(firstweek);
   str += '</div>';
 
-  str += '<div class="group2" id="lineDiagramLegend" style="display:flex; width:900px; align-items:center; justified-content:left">';
-  str += '<p>Commits:</p>'
-  str += '<div style="width:10px; height:10px; background-color:#F44336;"></div>';
-  str += '<p>Events:</p>'
-  str += '<div style="width:10px; height:10px; background-color:#4DB6AC;"></div>';
-  str += '<p>Comments:</p>'
-  str += '<div style="width:10px; height:10px; background-color:#43A047;"></div>';
-  str += '<p>LOC:</p>'
-  str += '<div style="width:10px; height:10px; background-color:Purple;"></div>';
+  str += '<div class="group2" id="lineDiagramLegend" style="display:flex; width:900px; align-items:center; justify-content:center;">';
+  str += '<div style="display:flex;align-items:center;margin-left:30px;margin-right:30px;"><p>Commits:</p>';
+  str += '<div style="width:15px; height:15px; background-color:#F44336;margin-left:10px;"></div></div>';
+  str += '<div style="display:flex;align-items:center;margin-left:30px;margin-right:30px;"><p>Events:</p>';
+  str += '<div style="width:15px; height:15px; background-color:#4DB6AC;margin-left:10px;"></div></div>';
+  str += '<div style="display:flex;align-items:center;margin-left:30px;margin-right:30px;"><p>Comments:</p>';
+  str += '<div style="width:15px; height:15px; background-color:#43A047;margin-left:10px;"></div></div>';
+  str += '<div style="display:flex; align-items:center;margin-left:30px;margin-right:30px;"><p>LOC:</p>';
+  str += '<div style="width:15px; height:15px; background-color:Purple;margin-left:10px;"></div></div>';
   str += '</div>';
 
 
