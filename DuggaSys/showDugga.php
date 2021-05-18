08c9e1c8f67493374d263798ca1a8c61256a71ce
a19arvka <a19arvka@student.his.se>
2021-05-18 12:26:10 +0200

    Removed old implementation

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -227,9 +227,6 @@ if($hash!='UNK' && !isSuperUser($userid) && !hasAccess($userid, $cid, 'w')){
 							echo "<input class='".$btnDisable." submit-button large-button' type='button' value='Reset' onclick='reset();' />";
 						}
 						echo "</td>";
-						//echo "<td align='right'>";
-						//echo "<input id='loadDuggaButton' class='submit-button large-button' type='button' value='Load Dugga' onclick='showLoadDuggaPopup();' />";
-						//echo "</td>";
 						echo "</tr>";
 						echo "</table>";
 					}
