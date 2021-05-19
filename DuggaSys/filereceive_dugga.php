c436790e1ec999444b9cceec63af6eb1e796db8b
a19simma <81557282+a19simma@users.noreply.github.com>
2021-05-19 12:36:49 +0200

    Changing default to 0 instead of UNK

--- DuggaSys/filereceive_dugga.php
+++ DuggaSys/filereceive_dugga.php
@@ -50,7 +50,7 @@ if(isset($_SESSION['uid'])){
 	$lastname=$_SESSION['lastname'];
 	$firstname=$_SESSION['firstname'];
 }else{
-	$userid="UNK";
+	$userid="0";
 }
 
 // Gets username based on uid. USED FOR LOGGING
