e04d10642dfe5d8f6883740639b6781df9ea5fed
a19phith <philip2leo@gmail.com>
2021-05-25 14:13:44 +0200

    "Fixed" Restrict number of characters allowed when putting labels on lines
    added a max lenght to the input label

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -3516,7 +3516,7 @@ function generateContextProperties()
             });
             str += `</select></label>`;
         }
-        str += `<input id="lineLabel" type="text" placeholder="Label..."`;
+        str += `<input id="lineLabel" maxlength="20" type="text" placeholder="Label..."`;
         if(contextLine[0].label && contextLine[0].label != "") str += `value="${contextLine[0].label}"`;
         str += `/>`;
     }   
