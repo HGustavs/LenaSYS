51f8573263ecc957ba4ebf0a1bd016ab6f6e3e7f
novin ramzi <a20novra@student.his.se>
2022-04-13 13:36:47 +0200

    Added last_time_teacher_visited column in userAnswer table

--- Shared/SQL/alterTables.sql
+++ Shared/SQL/alterTables.sql
@@ -59,3 +59,6 @@ ALTER TABLE userAnswer ADD COLUMN timesAccessed INT(5);
 
 /* Associated with issue #10980 */
 ALTER TABLE userAnswer ADD COLUMN timesAccessed INT(5);
+
+/* Associated with issue #11113 */
+ALTER TABLE userAnswer ADD last_Time_techer_visited TIMESTAMP NULL DEFAULT NULL AFTER timesGraded;
