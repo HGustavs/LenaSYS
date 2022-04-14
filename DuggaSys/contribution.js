931e36b1e2ca5d86c6a02e6e8d5c84c1bf8f2e7a
a19simru <simon.t.rubinstein@protonmail.com>
2022-04-14 10:56:09 +0200

    comment change

--- DuggaSys/contribution.js
+++ DuggaSys/contribution.js
@@ -206,7 +206,7 @@ function getMidsummer(date)
 function getHolidays(date) 
 {
   var holidays = new Array();
-
+/* 
   var easterDate = getEaster(date);
   var goodFridayDate = getGoodFriday(easterDate);
   var easterMondayDate = getEasterMonday(easterDate);
@@ -220,16 +220,16 @@ function getHolidays(date)
   var redDay4 = getYYYYMMDD(new Date(ascensionDayDate));
   var redDay5 = getYYYYMMDD(new Date(Y + "-05-01"));
   var redDay6 = getYYYYMMDD(new Date(Y + "-06-06"));
-  var redDay7 = getYYYYMMDD(new Date(midsummerDate));
+  var redDay7 = getYYYYMMDD(new Date(midsummerDate)); */
 
   //This is old and static but there was a bug that caused line diagram and bar diagram
   //to not render after the change to the new self updating system.
-  /* var redDay1 = getYYYYMMDD(new Date("2019-04-19"));
+  var redDay1 = getYYYYMMDD(new Date("2019-04-19"));
   var redDay2 = getYYYYMMDD(new Date("2019-04-22"));
   var redDay3 = getYYYYMMDD(new Date("2019-05-01"));
   var redDay4 = getYYYYMMDD(new Date("2019-05-30"));
   var redDay5 = getYYYYMMDD(new Date("2019-06-06"));
-  var redDay6 = getYYYYMMDD(new Date("2019-06-21")); */
+  var redDay6 = getYYYYMMDD(new Date("2019-06-21"));
   holidays.push(redDay1,redDay2,redDay3,redDay4,redDay5,redDay6,redDay7);
   return holidays;
 }
