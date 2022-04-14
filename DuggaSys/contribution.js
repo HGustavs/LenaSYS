d52bb6a482eeb144bde91a01e98468fbb53306e5
a19simru <simon.t.rubinstein@protonmail.com>
2022-04-14 21:18:01 +0200

    Deleted some unnececary commented out code

--- DuggaSys/contribution.js
+++ DuggaSys/contribution.js
@@ -1487,54 +1487,6 @@ function renderCellForghContibTable(col, celldata, cellid) {
         ${holidayList[i]}</span></div>`
       }  
       i++;
-
-/*       var Month = holidayList[i].substr(5,2);
-      var DayOfMonth = holidayList[i].substr(8,2); */
-/* 
-      if(Month == 4){
-        if(DayOfMonth >= 1 && DayOfMonth <= 8 && rowNr == 0){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 9 && DayOfMonth <= 15 && rowNr == 1){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 15 && DayOfMonth <= 22 && rowNr == 2){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 22 && DayOfMonth <= 29 && rowNr == 3){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(( DayOfMonth >= 29) && rowNr == 4){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        }
-      }else if(Month == 5){
-        if( DayOfMonth <= 6 && rowNr == 4){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 6 && DayOfMonth <= 13 && rowNr == 5){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 13 && DayOfMonth <= 20 && rowNr == 6){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 20 && DayOfMonth <= 27 && rowNr == 7){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 27 && rowNr == 8){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        }
-      }else if(Month == 6){
-        if(DayOfMonth <= 3 && rowNr == 8){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        } else if(DayOfMonth >= 3 && DayOfMonth <= 10 && rowNr == 9){
-          str += `<div style='display:flex;'><span style='margin:0 4px;flex-grow:1;'>
-          ${holidayList[i]}</span></div>`
-        }
-      } */
-      
     }
 
   }  else {
