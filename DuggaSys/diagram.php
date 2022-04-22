ee16c7bf7204b96ae9dd8990b12d64b1b2d8bc9c
a20muhal <a20muhal@student.his.se>
2022-04-22 11:01:44 +0200

    Dugga css has been moved to Style.css

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -13,8 +13,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome = 1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Diagram</title>
-    <link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
+    
     <link type="text/css" href="./diagram.css" rel="stylesheet">
+    <link type="text/css" href="../Shared/css/style.css" rel="stylesheet">
     <link type="text/css" href="../Shared/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="../Shared/js/jquery-1.11.0.min.js"></script>
