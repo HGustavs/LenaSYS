e354eadfc8e4f58c793f2d2504c3cd4455eeaf18
a20micro <a20micro@student.his.se>
2022-04-25 12:13:16 +0200

    Update showDugga.php

--- DuggaSys/showDugga.php
+++ DuggaSys/showDugga.php
@@ -77,10 +77,10 @@
 	$response->closeCursor();
 
 	#repeat for filelink table, checking if the corresponding file is global or not (if it's global, file is found in ../courses/global/ rather than course specific)
-	$count = $count + 1;
 	$fileLinkResponse = $pdo->prepare("SELECT isGlobal as isGlobal FROM filelink WHERE filename = '$splicedFileName'");
 	#$fileLinkResponse->bindParam(':isGlobal', $isGlobal);
 	$fileLinkResponse->execute();
+	$count = $count + 1;
 
 
 	foreach($fileLinkResponse->fetchAll(PDO::FETCH_ASSOC) as $row)
