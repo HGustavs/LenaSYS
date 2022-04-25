3158bebe76b60343f6093854ea564cb7b4b298ad
novin ramzi <a20novra@student.his.se>
2022-04-25 14:23:53 +0200

    Function striketxt added to add/remove a class name

--- DuggaSys/templates/group-assignment.js
+++ DuggaSys/templates/group-assignment.js
@@ -17,6 +17,16 @@ var elapsedTime = 0;
 
 //------------==========########### STANDARD MANDATORY FUNCTIONS ###########==========------------
 
+function striketxt(a,b){
+	if(document.getElementById(a).className=="unchecked"){
+		document.getElementById(a).classList.remove("unchecked");
+		document.getElementById(b).className="stroked";
+	}else{
+		document.getElementById(b).classList.remove("stroked");
+		document.getElementById(a).className="unchecked";
+	}
+}
+
 function setup()
 {
 	inParams = parseGet();
