73bf9446c39603c64bbe9d52714632aacaa5d9d4
b20fanfa <b20fanfa@student.his.se>
2022-04-26 14:07:06 +0200

    Added dark mode (toggle) on two more files.
    Added dark mode (toggle) on two more files. In the "codewiver.php" file, the code had a different type of structure than that implemented in sectioned.php. There is a comment that explains it.
    
    I also put back "media prefers-color-scheme" on both css files. Leave a comment that they should stay there and not be removed.

--- DuggaSys/courseed.js
+++ DuggaSys/courseed.js
@@ -344,48 +344,48 @@ function createVersion(){
 
 }
 
-//----------------------------------------
-// Dark mode toggle button listener.  
-//----------------------------------------
+// //----------------------------------------
+// // Dark mode toggle button listener.  
+// //----------------------------------------
 
-/*/ The code below is waitng for the page to load, and check when the user changes his/her 
-operative system to either black or white mode . /*/
+// /*/ The code below is waitng for the page to load, and check when the user changes his/her 
+// operative system to either black or white mode . /*/
 
-const themeStylesheet = document.getElementById('themeBlack');
+// const themeStylesheet = document.getElementById('themeBlack');
 
-document.addEventListener('DOMContentLoaded', () => {
-	const storedTheme = localStorage.getItem('themeBlack');
-	if(storedTheme){
-			themeStylesheet.href = storedTheme;
-	}
-	const themeToggle = document.getElementById('theme-toggle');
-	themeToggle.addEventListener('click', () => {
-			// if it's light -> go dark
-			if(themeStylesheet.href.includes('blackTheme')){
-					themeStylesheet.href = "../Shared/css/whiteTheme.css";
-					localStorage.setItem('themeBlack',themeStylesheet.href)
-					// themeToggle.innerText = 'Switch to light mode';
-			} else if(themeStylesheet.href.includes('whiteTheme')) {
-					// if it's dark -> go light
-					themeStylesheet.href = "../Shared/css/blackTheme.css";
-					localStorage.setItem('themeBlack',themeStylesheet.href)
-					// themeToggle.innerText = 'Switch to dark mode';
-			}		
-	})
-})
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const storedTheme = localStorage.getItem('themeBlack');
+// 	if(storedTheme){
+// 			themeStylesheet.href = storedTheme;
+// 	}
+// 	const themeToggle = document.getElementById('theme-toggle');
+// 	themeToggle.addEventListener('click', () => {
+// 			// if it's light -> go dark
+// 			if(themeStylesheet.href.includes('blackTheme')){
+// 					themeStylesheet.href = "../Shared/css/whiteTheme.css";
+// 					localStorage.setItem('themeBlack',themeStylesheet.href)
+// 					// themeToggle.innerText = 'Switch to light mode';
+// 			} else if(themeStylesheet.href.includes('whiteTheme')) {
+// 					// if it's dark -> go light
+// 					themeStylesheet.href = "../Shared/css/blackTheme.css";
+// 					localStorage.setItem('themeBlack',themeStylesheet.href)
+// 					// themeToggle.innerText = 'Switch to dark mode';
+// 			}		
+// 	})
+// })
 
-//It actively checks if the "theme" changes on the operating system and changes colors based on it. It override your preferences.
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-	const newColorScheme = e.matches ? "dark" : "light";
-	if(newColorScheme == "dark") {
-		themeStylesheet.href = "../Shared/css/blackTheme.css";
-		localStorage.setItem('themeBlack',themeStylesheet.href)
-	}
-	else {
-		themeStylesheet.href = "../Shared/css/whiteTheme.css";
-		localStorage.setItem('themeBlack',themeStylesheet.href)
-	}
-});
+// //It actively checks if the "theme" changes on the operating system and changes colors based on it. It override your preferences.
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+// 	const newColorScheme = e.matches ? "dark" : "light";
+// 	if(newColorScheme == "dark") {
+// 		themeStylesheet.href = "../Shared/css/blackTheme.css";
+// 		localStorage.setItem('themeBlack',themeStylesheet.href)
+// 	}
+// 	else {
+// 		themeStylesheet.href = "../Shared/css/whiteTheme.css";
+// 		localStorage.setItem('themeBlack',themeStylesheet.href)
+// 	}
+// });
 
 
 //----------------------------------------
--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -35,6 +35,7 @@ if(isset($_SESSION['uid'])){
 	<link id="themeWhite" type="text/css" href="../Shared/css/whiteTheme.css" rel="stylesheet">
 	<link id="themeBlack" type="text/css" href="../Shared/css/blackTheme.css" rel="stylesheet">
 
+	<script src="darkmodeToggle.js"></script>
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
 	<script src="../Shared/dugga.js"></script>
