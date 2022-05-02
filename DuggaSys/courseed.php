ec80deffc9618ba3daf98d9ba6f3b6ff770dc4d8
a20mtafa <a20mtafa@student.his.se>
2022-05-02 15:43:33 +0200

    Update courseed.php

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -120,7 +120,7 @@ if(isset($_SESSION['uid'])){
 					<select class='selectinput' id='visib'></select>
 				</div>
     		</div>
-    		<div style='padding:20px; float:right;' >
+    		<div style='float:left; padding-top:20px; padding-right:50px;' >
     			<input id='saveCourse' class='submit-button' type='button' value='Save' title='Save changes' onclick="validateForm('editCourse')" />
     		</div>
       </div>
