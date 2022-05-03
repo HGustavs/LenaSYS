886108cedb58e82e5cb84b6376da96034251a73c
novin ramzi <a20novra@student.his.se>
2022-05-03 13:45:58 +0200

    added go to top to resulted.php and .js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -420,3 +420,24 @@ function setDateIntervals(data){
 	document.querySelector("#datepicker-interval-2").value = highest;
 
 }
+
+//------------------------------------------------------------------------------
+// Scroll to top of page function 
+//------------------------------------------------------------------------------
+$(document).ready(function(){
+	$("#scrollUp").on('click', function(event) {
+	  window.scrollTo(0, 0);
+	});
+  });
+  
+  // Show the up-arrow when user has scrolled down 200 pixels on the page
+  window.onscroll = function() {scrollToTop()};
+  function scrollToTop() {
+	var scroll = document.getElementById("fixedScroll");
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+	  scroll.style.display = "block";
+	} else {
+	  scroll.style.display = "none";
+	}
+  }
+  
