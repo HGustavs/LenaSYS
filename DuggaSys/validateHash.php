4eb56151a43dcb47a870b063729d9bf4b5b22ae9
a20micro <a20micro@student.his.se>
2022-05-04 16:26:59 +0200

    Revert "Temp Test"
    This reverts commit 1c6d3c77b28cb7ad9a1c83080891f23d2e79c3b1.

--- DuggaSys/validateHash.php
+++ DuggaSys/validateHash.php
@@ -33,7 +33,7 @@
             $_SESSION["submission-$newcourseid-$newcoursevers-$newduggaid-$newmoment"]=$hash;
             $_SESSION["submission-password-$newcourseid-$newcoursevers-$newduggaid-$newmoment"]=$hashpwd;
             $_SESSION["submission-variant-$newcourseid-$newcoursevers-$newduggaid-$newmoment"]=$variant;
-            $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/LenaSys/sh/?s=$hash";
+            $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/sh/?s=$hash";
             header("Location: $link");
             exit();	
         }else{
