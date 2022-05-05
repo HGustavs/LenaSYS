1e59618d3a360d4f6b1c7148d0f3fdbb5f0e2d32
a20micro <a20micro@student.his.se>
2022-05-05 17:15:24 +0200

    Revert "LastDebugTry"
    This reverts commit 4bd6cc6acf48dfa12205eba6304932b7c03ac7d7.

--- DuggaSys/templates/diagram_dugga.js
+++ DuggaSys/templates/diagram_dugga.js
@@ -48,7 +48,7 @@ function getDiagramData()
  * */
 function uploadFile()
 {
-    /*
+    console.log(hash);
     $.ajax({
         method: "POST",
         url: "filereceive_dugga.php",
@@ -64,7 +64,6 @@ function uploadFile()
             moment: inParams["moment"]
         }
     });
-    */
 }
 /**
  * @description If the user has previus submissions to the current
