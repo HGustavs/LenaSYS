c440430bdc8db70ee39a23ed1f19cd87dee5a440
novin ramzi <a20novra@student.his.se>
2022-05-09 11:37:22 +0200

    removed noneDragbleArea

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -86,18 +86,12 @@ function hideCollapsedMenus() {
 function toggleArrows(id) {
   $('.arrowComp').show();
   $('.arrowRight').hide();
-  $('#selectionDrag'+id).show();
-  $('#NonselectionDragI'+id).hide();
-  
+  $('#selectionDrag'+id).toggle();
   for (var i = 0; i < menuState.arrowIcons.length; i++) {
     if (menuState.arrowIcons[i].indexOf('arrowComp') > -1) {
       $('#' + menuState.arrowIcons[i]).hide();
-      $('#selectionDrag'+id).hide();
-
     } else {
       $('#' + menuState.arrowIcons[i]).show();
-  $('#NonselectionDragI'+id).show();
-
     }
   }
 
@@ -1031,10 +1025,6 @@ function returnedSection(data) {
           // Styling for Section row
           str += "<td style='background-color: #614875;' class='LightBox" + hideState + "'>";
           str += "<div id='selectionDragI"+item['lid']+"' class='dragbleArea'><img alt='pen icon dugga' src='../Shared/icons/select.png'></div>";
-
-          str += "<td style='background-color: #614875;' class='LightBox" + hideState + "'>";
-          str += "<div id='NonselectionDragI"+item['lid']+"' class='dragbleArea'><img alt='pen icon dugga' src='../Shared/icons/nonDragableArea.jpg'></div>";
-
           str += `<td class='section item${hideState}' placeholder='${momentexists}'id='I${item['lid']}' style='cursor:pointer;' `;
           kk = 0;
 
