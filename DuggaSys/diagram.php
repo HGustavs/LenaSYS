d0fcbae74a879d449e62f41ad3e89e54d079c2c1
a19meram <77247277+a19meram@users.noreply.github.com>
2022-05-10 11:40:24 +0200

    Update diagram.php

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -292,7 +292,7 @@
             <defs>
             <pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
 
-                <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-dasharray="0.9 0.9" stroke-width="3"/>
+                <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke="0.9 0.9" stroke-width="3"/>
             </pattern>
             </defs>
             <rect width="100%" height="100%" fill="url(#grid)" />
