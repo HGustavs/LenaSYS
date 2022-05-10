f13324ae4d9c5425bb50ff25e297628f8d8638ff
b19karhj <81624541+b19karhj@users.noreply.github.com>
2022-05-10 10:57:43 +0200

    Boolean removed
    Boolean check seem to be behind the reset of the list. Now removed

--- DuggaSys/fileed.js
+++ DuggaSys/fileed.js
@@ -163,7 +163,7 @@ function uploadFile(kind) {
     var filesToBeUploaded;
     //Boolean to check if file was found during for-loop.
     var fileExists = false;
-    let data = sessionStorage.getItem('booleanCheck')
+    
     
     //For-loop which checkes the selected file/files full name, with already uploaded/existing files.
     for(i = 0; i < document.getElementById("uploadedfile").files.length; i++){ 
@@ -174,17 +174,15 @@ function uploadFile(kind) {
             if(filesToBeUploaded.replace(/\s+/g, '') == fullFileName[j]){
                 //If a file/files have same name and filetype fileExists sets to true so the file will not be uploaded.
                 fileExists = true;
-                sessionStorage.setItem('booleanCheck', 'true');
-                alert(data);
+                
 
                 alert("File with same name and filetyp as "+filesToBeUploaded+" already exist in list. Your file/files will not be uploaded");
-                break;
+                
             }; 
         };
     };
-
-    if(fileExists == false){
-        sessionStorage.removeItem('booleanCheck');
+    
+    
         if (kind == "MFILE") {
             var str = "<option>NONE</option>";
             for (i = 0; i < filez['lfiles'].length; i++) {
@@ -219,7 +217,7 @@ function uploadFile(kind) {
         $("#courseid").val(querystring['courseid']);
         $("#coursevers").val(querystring['coursevers']);
     };
-};
+
 
     
 
