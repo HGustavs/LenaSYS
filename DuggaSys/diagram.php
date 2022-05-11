2a788a5a1d741e0ab00e0ffd3659e006ef818343
a19meram <77247277+a19meram@users.noreply.github.com>
2022-05-11 16:23:50 +0200

    Update diagram.php

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -271,7 +271,7 @@
         <fieldset>
             <legend>Include</legend>
             <div id="erTableToggle" class="diagramIcons" onclick="toggleErTable()">
-                <img src="../Shared/icons/lessThen.jpg"/>
+                <img src="../Shared/icons/diagram_errorCheck.svg"/>
                 <span class="toolTipText"><b>To use less than</b><br>
                     <p>To use less than type & #60; <br><strong>"EX: <& #60;Include>>"</STRONG> </p><br>
                 </span>
