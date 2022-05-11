35a83e977f761117bbc3132ed9292d429bb24ed9
b20idawa <b20idawa@student.his.se>
2022-05-11 14:05:29 +0200

    Changed the announcement icon in hamburgermenu
    Changed the style of announcement icon in the hamburgermenu

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -201,7 +201,7 @@
 
 							//Dropdown Menu For Teachers
 							echo "<div id='hamburgerBox'>";
-							echo "<div id='announcementBurger'><img alt='announcement icon'  class='burgerButt' src='../Shared/icons/new_announcement_icon.svg'><p class='burgerHover'>Announcements</p></div>";
+							echo "<div id='announcementBurger'><img alt='announcement icon'  class='burgerButt' src='../Shared/icons/new_announcement_iconShadow.svg'><p class='burgerHover'>Announcements</p></div>";
 
 							echo "<div id='versionCogBurger' onclick=showEditVersion();><img alt='settings icon'  class='burgerButt' title='Edit the selected version'  src='../Shared/icons/CogwheelWhite.svg'><p  class='burgerHover'>Edit selected version</p></div>";
 
