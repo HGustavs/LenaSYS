e72d85404582d86220872631e4a7c14ec8a628ce
b20idawa <b20idawa@student.his.se>
2022-05-12 11:54:36 +0200

    Changed position for codeburger menu
    Changed position for the codeburger menu

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -5012,7 +5012,7 @@ function showBurgerDropdown(){
 	var burgerPos = document.querySelector('#codeBurger').getBoundingClientRect();
 	menu.style.display = 'block';
 	menu.style.top = burgerPos.top + 50 + 'px';
-	menu.style.left = burgerPos.left + (-100) + 'px';
+	menu.style.left = burgerPos.left + (-90) + 'px';
 }
 
 function closeBurgerMenu() {
