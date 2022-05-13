b127e8f5a3e7eab0f3bb9e16e21df67d585f1b52
a20willa <81626616+a20willa@users.noreply.github.com>
2022-05-13 15:51:40 +0200

    Fixed so that overlay works over linnes

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -527,11 +527,32 @@ function preventDefaults (e)
 function highlight(e) 
 {
 	e.target.closest(".box").classList.add('highlight');
+	var normtext = document.querySelectorAll(".normtext");
+	var impword = document.querySelectorAll(".impword");
+	
+	normtext.forEach(box => {
+		box.style.backgroundColor = 'transparent';
+	  });
+	  
+	impword.forEach(box => {
+		box.style.backgroundColor = 'transparent';
+	  });
 }
 
 function unhighlight(e) 
 {
 	e.target.closest(".box").classList.remove('highlight');
+
+	var normtext = document.querySelectorAll(".normtext");
+	var impword = document.querySelectorAll(".impword");
+	
+	normtext.forEach(box => {
+		box.style.backgroundColor = 'none';
+	  });
+	  
+	impword.forEach(box => {
+		box.style.backgroundColor = 'none';
+	  });
 } 
 
 //---------------------------------------------------------------------------------
