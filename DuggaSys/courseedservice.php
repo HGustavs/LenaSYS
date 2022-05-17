98059ebaa4a4674feabc0f5aa8e95f3a0be63a65
a19simru <simon.t.rubinstein@protonmail.com>
2022-05-17 11:09:37 +0200

    Fixed another memory leak

--- DuggaSys/courseedservice.php
+++ DuggaSys/courseedservice.php
@@ -530,11 +530,39 @@ foreach($queryz->fetchAll(PDO::FETCH_ASSOC) as $row){
 	$userCourse[$row['cid']] = $row['access'];
 }
 
-//Delete Courses that have been marked as deleted.
+//Delete course matterial from courses that have been marked as deleted.
 $deleted = 3;
-$query = $pdo->prepare("DELETE FROM course WHERE visibility=:deleted;");
+$query = $pdo->prepare("DELETE codeexample FROM course,codeexample WHERE course.visibility=:deleted AND codeexample.cid = course.cid;");
 $query->bindParam(':deleted', $deleted);
+ if(!$query->execute()) {
+	$error=$query->errorInfo();
+	$debug="Error reading courses\n".$error[2];
+} 
 
+$query = $pdo->prepare("DELETE listentries FROM course,listentries WHERE course.visibility=:deleted AND listentries.cid = course.cid;");
+$query->bindParam(':deleted', $deleted);
+ if(!$query->execute()) {
+	$error=$query->errorInfo();
+	$debug="Error reading courses\n".$error[2];
+} 
+
+$query = $pdo->prepare("DELETE quiz FROM course,quiz WHERE course.visibility=:deleted AND quiz.cid = course.cid;");
+$query->bindParam(':deleted', $deleted);
+ if(!$query->execute()) {
+	$error=$query->errorInfo();
+	$debug="Error reading courses\n".$error[2];
+} 
+
+$query = $pdo->prepare("DELETE vers FROM course,vers WHERE course.visibility=:deleted AND vers.cid = course.cid;");
+$query->bindParam(':deleted', $deleted);
+ if(!$query->execute()) {
+	$error=$query->errorInfo();
+	$debug="Error reading courses\n".$error[2];
+} 
+
+//Delete Courses that have been marked as deleted.
+$query = $pdo->prepare("DELETE course FROM course WHERE visibility=:deleted;");
+$query->bindParam(':deleted', $deleted);
 if(!$query->execute()) {
 	$error=$query->errorInfo();
 	$debug="Error reading courses\n".$error[2];
