4f63fe2d5850b65fac8e2b865db2cbb68fa0ee86
b19karhj <81624541+b19karhj@users.noreply.github.com>
2022-05-19 15:15:23 +0200

    name change on function call
    name change on function call

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -358,7 +358,7 @@
         </fieldset>
         <fieldset>
             <legend>History</legend>
-            <div id="erTableToggle" class="diagramIcons" onclick="resetDiagram()">
+            <div id="erTableToggle" class="diagramIcons" onclick="resetDiagramAlert()">
                 <img src="../Shared/icons/diagram_Refresh_Button.svg"/>
                 <span class="toolTipText"><b>Reset diagram</b><br>
                     <p>Reset diagram to default state</p><br>
