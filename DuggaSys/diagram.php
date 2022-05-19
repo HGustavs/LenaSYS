609a26063ff70cb18ad99c8daea37f5adec6c895
b19karhj <81624541+b19karhj@users.noreply.github.com>
2022-05-19 09:52:08 +0200

    Tooltip/name
    New name and tooltip for reset diagram button.

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -228,8 +228,8 @@
             <legend>History</legend>
             <div id="erTableToggle" class="diagramIcons" onclick="refreshDiagram()">
                 <img src="../Shared/icons/diagram_Refresh_Button.svg"/>
-                <span class="toolTipText"><b>Refresh Diagram</b><br>
-                    <p>Click to reset the diagram to its original look</p><br>
+                <span class="toolTipText"><b>Reset diagram</b><br>
+                    <p>Reset diagram to default state</p><br>
                 </span>
             </div>
             <div id="stepForwardToggle" class="diagramIcons" onclick="toggleStepForward()">
