3c49baf3804c491b5e0fedaddc4369fe033490af
a21aldsa <a21aldsa@student.his.se>
2023-04-03 14:56:49 +0200

    Revert "seeing if the fontcolor is automatically set to black everytime the css updates"
    This reverts commit f26bec29181503b904f4bd80130f080cc085b392.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10757,7 +10757,7 @@ function updateCSSForAllElements()
                             fontColor.style.fill = `${"#ffffff"}`;
                         } else{
                             fillColor.style.fill = `${element.fill}`;
-                            fontColor.style.fill = `${"#ffffff"}`;
+                            fontColor.style.fill = `${"#000000"}`;
                         }
                     }
                 }
