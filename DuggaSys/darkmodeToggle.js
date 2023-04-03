84a4a00e497fd3dbe85cc46bbc339094780f3c88
a19clasv <a19clasv@student.his.se>
2023-04-03 20:28:02 +0200

    Revert "Change color immediately"
    This reverts commit 144abb56f4ec9018d10edc102411d431120adf1c.

--- DuggaSys/darkmodeToggle.js
+++ DuggaSys/darkmodeToggle.js
@@ -17,18 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // if it's light -> go dark
     if(themeStylesheet.href.includes('blackTheme')){
       themeStylesheet.href = "../Shared/css/style.css";
-      localStorage.setItem('themeBlack', themeStylesheet.href)
-
-      // force immediate change by accessing element
-      themeStylesheet = document.getElementById('themeBlack');
+      localStorage.setItem('themeBlack',themeStylesheet.href)
     } 
     else if(themeStylesheet.href.includes('style')) {
       // if it's dark -> go light
       themeStylesheet.href = "../Shared/css/blackTheme.css";
-      localStorage.setItem('themeBlack', themeStylesheet.href)
-
-      // force immediate change by accessing element
-      themeStylesheet = document.getElementById('themeBlack');
+      localStorage.setItem('themeBlack',themeStylesheet.href)
     }		
   })
 })
