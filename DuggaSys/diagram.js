e60205ff0cca8c01aca6d59d59ad31bbfcfe78ce
a21aldsa <a21aldsa@student.his.se>
2023-04-03 16:24:09 +0200

    added the same check to UML as well

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10746,7 +10746,13 @@ function updateCSSForAllElements()
                             fontColor.style.fill = `${"#ffffff"}`;
                         } else{
                             fillColor.style.fill = `${element.fill}`;
-                            fontColor.style.fill = `${"#000000"}`;
+                            if ((element.fill == "#000000") || (element.fill == "#dc267f")) {
+                                fontColor.style.fill = `${"#ffffff"}`;
+                            }
+                            else{
+
+                                fontColor.style.fill = `${"#000000"}`;
+                            }
                         }
                     }
                 }
@@ -10761,10 +10767,13 @@ function updateCSSForAllElements()
                             fontColor.style.fill = `${"#ffffff"}`;
                         } else{
                             fillColor.style.fill = `${element.fill}`;
-                            if ((element.fill == "#000000") || (element.fill = "#dc267f")) {
+                            if ((element.fill == "#000000") || (element.fill == "#dc267f")) {
                                 fontColor.style.fill = `${"#ffffff"}`;
                             }
-                            fontColor.style.fill = `${"#000000"}`;
+                            else{
+
+                                fontColor.style.fill = `${"#000000"}`;
+                            }
                         }
                     }
                 }
