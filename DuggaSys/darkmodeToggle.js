d463a9da91c64133e55f5066a3fa36a3e7622eaf
a19clasv <a19clasv@student.his.se>
2023-04-03 21:05:59 +0200

    Revert "Button listener outside document listener"
    This reverts commit 6603a7b62fdab4268ea856e96f56880391c5d3d7.

--- DuggaSys/darkmodeToggle.js
+++ DuggaSys/darkmodeToggle.js
@@ -1,34 +1,18 @@
+//----------------------------------------
+// Dark mode toggle button listener.  
+//----------------------------------------
+
 /*/ The code below is waitng for the page to load, and check when the user changes his/her 
 operative system to either black or white mode . /*/
 
 const themeStylesheet = document.getElementById('themeBlack');
-const themeToggle = document.getElementById('theme-toggle');
-
-themeToggle.addEventListener('click', () => {
-  // if it's light -> go dark
-  if (themeStylesheet.href.includes('blackTheme')) {
-    themeStylesheet.href = "../Shared/css/style.css";
-    localStorage.setItem('themeBlack', themeStylesheet.href)
-  }
-  else if (themeStylesheet.href.includes('style')) {
-    // if it's dark -> go light
-    themeStylesheet.href = "../Shared/css/blackTheme.css";
-    localStorage.setItem('themeBlack', themeStylesheet.href)
-  }
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-  const storedTheme = localStorage.getItem('themeBlack');
-  if (storedTheme) {
-    themeStylesheet.href = storedTheme;
-  }
-})
 
 document.addEventListener('DOMContentLoaded', () => {
 	const storedTheme = localStorage.getItem('themeBlack');
 	if(storedTheme){
 		themeStylesheet.href = storedTheme;
 	}
+	const themeToggle = document.getElementById('theme-toggle');
 	themeToggle.addEventListener('click', () => {
     // if it's light -> go dark
     if(themeStylesheet.href.includes('blackTheme')){
