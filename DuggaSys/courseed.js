ee563a1d23d8b9750f74117c8c8f2dee3d0a9148
g21emmka <102578908+g21emmka@users.noreply.github.com>
2023-04-04 16:03:23 +0200

    more regex edits
    had issues with the formatting of the regular expression, hopefully fixed now
    
    Co-Authored-By: c21rebja <102578746+c21rebja@users.noreply.github.com>

--- DuggaSys/courseed.js
+++ DuggaSys/courseed.js
@@ -622,8 +622,7 @@ function showGitInput(show){
 //Validation with REG EXP for Github URL
 function validateGitInput(){
 	var gitUrl = document.getElementById("ncoursegit-url");
-	var reg = new RegExp("/^(https?://)?(github)\.(com)([/\w \.-]*)*\/?(\.git)$/");
-		if((gitUrl.value.match(reg)!=null)){
+		if((gitUrl.value.match(/^(https?:\/\/)?(github)(\.com)([/\w \.-]*)*\/?(\.git)$/)!=null)){
 			console.log("Valid URL!");
 			gitUrl.removeAttribute("style");
 		} else {
