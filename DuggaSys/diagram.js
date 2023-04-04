acfe1876e093e9af7db7ffb27014e09b71059188
a21oligu <oliver.gustafsson22@gmail.com>
2023-04-04 11:58:53 +0200

    color changes are now stored properly with ids in historyLog

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6633,6 +6633,7 @@ function setElementColors(clickedCircleID)
         var color = colors[index];
         for (var i = 0; i < context.length; i++) {
             context[i].fill = color;
+            elementIDs.push(context[i].id)
             /*
             // Change font color to white for contrast, doesn't work for whatever reason but will maybe provide a hint for someone who might want to try to solve it.
             if (clickedCircleID == "BGColorCircle9" || clickedCircleID == "BGColorCircle6") {
@@ -6643,8 +6644,11 @@ function setElementColors(clickedCircleID)
                 //element.id.style.color = "#000000";
             }*/
         }
-        stateMachine.save(StateChangeFactory.ElementAttributesChanged(elementIDs, { fill: color }),
-        StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
+
+        stateMachine.save(
+            StateChangeFactory.ElementAttributesChanged(elementIDs, { fill: color }),
+            StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED
+        );
     } else if (menu.id == "StrokeColorMenu") {  // If stroke button was pressed
         var index = id.replace("strokeColorCircle", "") * 1;
         var color = strokeColors[index];
@@ -6652,8 +6656,10 @@ function setElementColors(clickedCircleID)
             context[i].stroke = color;
             elementIDs[i] = context[i].id;
         }
-        stateMachine.save(StateChangeFactory.ElementAttributesChanged(elementIDs, { stroke: color }),
-        StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
+        stateMachine.save(
+            StateChangeFactory.ElementAttributesChanged(elementIDs, { stroke: color }),
+            StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED
+        );
     } else {
         console.error(`${menu.id} is not a valid ID`);
     }
