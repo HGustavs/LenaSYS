47b8eeebbc5102792a4186409c2a103e48ea9668
a19clasv <a19clasv@student.his.se>
2023-04-05 21:28:15 +0200

    Added updatepos and showdata in edge creation mode
    Added updatepos(0,0) and showdata() in case mouseModes.EDGE_CREATION to hopefully keep the font color

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -1971,11 +1971,14 @@ function mouseMode_onMouseUp(event)
                     makeGhost();
                     // Create ghost line
                     ghostLine = { id: makeRandomID(), fromID: context[0].id, toID: ghostElement.id, kind: "Normal" };
+                    showdata();
+                    updatepos(0,0);
                 }else{   
                     clearContext();
                     ghostElement = null;
                     ghostLine = null;
                     showdata();
+                    updatepos(0,0);
                 }
             }
             break;
