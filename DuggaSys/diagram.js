2d5fe49e68721fe8f8caa4a3ad0333f823c4376e
a21aldsa <a21aldsa@student.his.se>
2023-04-06 15:53:18 +0200

    Update diagram.js
    removed old, commented away, code. and added comments explaining my work

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6248,38 +6248,12 @@ function generateContextProperties()
             if(contextLine[0].endLabel && contextLine[0].endLabel != "") str += `value="${contextLine[0].endLabel}"`;
             str += `/>`;
         }
+        //TODO this code can be refactored to avoid the reuse of if (contextLine[0].startIcon != undefined... in every if statement
         if (contextLine[0].type == 'UML' || contextLine[0].type == 'IE' ) {
             str += `<label style="display: block">Icons:</label> <select id='lineStartIcon' onchange="changeLineProperties()">`;
             str  += `<option value=''>None</option>`;
             //iterate through all the icons assicoated with UML, like Arrow or Black Diamond and add them to the drop down as options
             Object.keys(UMLLineIcons).forEach(icon => {
-                //TODO icon and contextLine[0].startIcon will never be the same:
-                //staricon can, for example, be Triangle, while icon is TRIANGLE
-                //whats worse is that starticon can be White_Diamond while icon is WHITEDIAMOND
-                /* if (contextLine[0].startIcon != undefined && contextLine[0].startIcon == icon){
-                    str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                }else {
-                    str += `<option value='${UMLLineIcons[icon]}'>${UMLLineIcons[icon]}</option>`;
-                } */
-                //fixed Triangle=/=TRIANGLE with toUpperCase()
-                //check if there even is a starticon first
-                /* if (contextLine[0].startIcon != undefined){
-                    console.log("icon is " + icon);
-                    console.log("startIcon is " + contextLine[0].startIcon.toUpperCase());
-                    //check if its white or black diamond since these wont be caught otherwise
-                    if ((contextLine[0].startIcon == "White_Diamond") && icon == "WHITEDIAMOND") {
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                    }
-                    else if ((contextLine[0].startIcon == "Black_Diamond") && icon == "BLACKDIAMOND") {
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                    }
-                    else if (contextLine[0].startIcon.toUpperCase() == icon) {
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                    }
-                    //str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                }else {
-                    str += `<option value='${UMLLineIcons[icon]}'>${UMLLineIcons[icon]}</option>`;
-                } */
                 //this covers Triangle and Arrow.
                 //If the lines in context happen to be matching something in the drop down, it is set as selected.
                 if (contextLine[0].startIcon != undefined && contextLine[0].startIcon.toUpperCase() == icon){
@@ -6300,22 +6274,17 @@ function generateContextProperties()
                     str += `<option value='${UMLLineIcons[icon]}'>${UMLLineIcons[icon]}</option>`;
                 }
             });
-            /* Object.keys(IELineIcons).forEach(icon => {
-                if (contextLine[0].startIcon != undefined && contextLine[0].startIcon == icon){
-                    str += `<option value='${IELineIcons[icon]}' selected>${IELineIcons[icon]}</option>`;
-                }else {
-                    str += `<option value='${IELineIcons[icon]}'>${IELineIcons[icon]}</option>`;
-                }
-            }); */
-            
+            //iterate trough all icons associated with IE. add these icons to the drop down
+            //if the line in context has one of these lines in the starting position, just like for UML, it is automatically selected
             Object.keys(IELineIcons).forEach(icon => {
                 //this only really covers WEAK, since the rest have a inconsistent naming scheme, like ONE_MANY; its also reffered to as 1-M
+                //This means we have to manually check these and others like them
                 if (contextLine[0].startIcon != undefined && contextLine[0].startIcon.toUpperCase() == icon){
                     str += `<option value='${IELineIcons[icon]}' selected>${IELineIcons[icon]}</option>`;
                     console.log("IE icon is " + icon);
                     console.log("IE startIcon is " + contextLine[0].startIcon.toUpperCase());
                 }
-                //icon can be ZERO_MANY while start icon can be 0-M. This means we have to manually check these and others like them
+                //icon can be ZERO_MANY while start icon can be 0-M.
                 else if (contextLine[0].startIcon != undefined && (contextLine[0].startIcon.toUpperCase() == "0-M") && (icon == "ZERO_MANY")) {
                     str += `<option value='${IELineIcons[icon]}' selected>${IELineIcons[icon]}</option>`;
                 }
