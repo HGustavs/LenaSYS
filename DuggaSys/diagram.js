ce1a92c74befdedb2b981ef4c9a0685eff1eabfb
a21aldsa <a21aldsa@student.his.se>
2023-04-06 15:28:09 +0200

    Update diagram.js
    removed unnecessary if statement

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6283,20 +6283,18 @@ function generateContextProperties()
                 //this covers Triangle and Arrow.
                 //TODO i assume the weird icons are IE
                 //If the lines in context happen to be matching something in the drop down, it is set as selected.
-                if (contextLine[0].startIcon != undefined) {
-                    if (contextLine[0].startIcon != undefined && contextLine[0].startIcon.toUpperCase() == icon){
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                        console.log("icon is " + icon);
-                        console.log("startIcon is " + contextLine[0].startIcon.toUpperCase());
-                    }
-                    //white and diamond needs their own if statement since contextLine[0].startIcon can be White_Diamond,
-                    //while icon is WHITEDIAMOND. So I decided the most suitable way is to manually check it.
-                    else if (contextLine[0].startIcon != undefined && (contextLine[0].startIcon == "White_Diamond") && (icon == "WHITEDIAMOND")) {
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                    }
-                    else if (contextLine[0].startIcon != undefined && (contextLine[0].startIcon == "Black_Diamond") && (icon == "BLACKDIAMOND")) {
-                        str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
-                    }
+                if (contextLine[0].startIcon != undefined && contextLine[0].startIcon.toUpperCase() == icon){
+                    str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
+                    console.log("icon is " + icon);
+                    console.log("startIcon is " + contextLine[0].startIcon.toUpperCase());
+                }
+                //white and diamond needs their own if statement since contextLine[0].startIcon can be White_Diamond,
+                //while icon is WHITEDIAMOND. So I decided the most suitable way is to manually check it.
+                else if (contextLine[0].startIcon != undefined && (contextLine[0].startIcon == "White_Diamond") && (icon == "WHITEDIAMOND")) {
+                    str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
+                }
+                else if (contextLine[0].startIcon != undefined && (contextLine[0].startIcon == "Black_Diamond") && (icon == "BLACKDIAMOND")) {
+                    str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
                 }
                 //else, its not matching and the option is just added to the dropdown normally.
                 else {
