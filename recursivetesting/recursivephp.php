a297c70c637fd5c6a628f02e6419f385335fae09
e21krida <e21krida@student.his.se>
2023-04-06 13:11:06 +0200

    Testing php
    Testing a version for fetching the information of a github repository via php.

--- recursivetesting/recursivephp.php
+++ recursivetesting/recursivephp.php
@@ -6,5 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+<body>
+    <?php
+    $opts = [
+        'http' => [
+                'method' => 'GET',
+                'header' => [
+                        'User-Agent: PHP'
+                ]
+        ]
+    ];
+    
+    $context = stream_context_create($opts);
+    $content = file_get_contents("https://api.github.com/repos/HGustavs/Webbprogrammering-Examples/contents/", false, $context);
+    var_dump($content);
+    ?>
+</body>
 </html>
 
