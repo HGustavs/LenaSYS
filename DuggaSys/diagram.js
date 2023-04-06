43ebfe35a3797dc50bbf2834c657c28be7c09ec0
a21aldsa <a21aldsa@student.his.se>
2023-04-06 12:13:19 +0200

    Update diagram.js
    Found the source of the issue. Its a matter of the original author not accounting for the difference in how the names for these icons are stored. The author, effectively, assumed that, for example, White_Diamond would be equal to WHITEDIAMOND. Which is not the case.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6251,35 +6251,10 @@ function generateContextProperties()
         if (contextLine[0].type == 'UML' || contextLine[0].type == 'IE' ) {
             str += `<label style="display: block">Icons:</label> <select id='lineStartIcon' onchange="changeLineProperties()">`;
             str  += `<option value=''>None</option>`;
-            
-            /* str  += `<option `;
-            if (contextLine[0].startIcon && contextLine[0].startIcon != "") {
-                let contextStartIcon = contextLine[0].startIcon;
-                str += `value=`+contextStartIcon+`">`+contextStartIcon;
-                console.log(contextLine[0].startIcon);
-            } else {
-                str += `value=''>None`;
-            }
-            str += `</option>`; */
-            //if the line has a start icon, the drop down will show it 
-            /* if (contextLine[0].startIcon && contextLine[0].startIcon != "") {
-                //if the startIcon dropdown has been generated, set the value of it to match the start icon on the line. 
-                if (document.getElementById("lineStartIcon")) {
-                    let startIconDropDown = document.getElementById("lineStartIcon");
-                    let contextStartIcon = contextLine[0].startIcon;
-                    //iterate through the whole dropdown until the value matches the icon.
-                    //then set that option to be selected.
-                    for (let i = 0; i < startIconDropDown.options.length; i++) {
-                        console.log(startIconDropDown.options[i].value);
-                        if (contextStartIcon == startIconDropDown.options[i].value) {
-                            contextStartIcon.options[i].selected = true;
-                        }
-                    }
-                    console.log(document.getElementById("lineStartIcon").options.length);
-                }
-            } */
-            
             Object.keys(UMLLineIcons).forEach(icon => {
+                //TODO icon and contextLine[0].startIcon will never be the same:
+                //staricon can, for example, be Triangle, while icon is TRIANGLE
+                //whats worse is that starticon can be White_Diamond while icon is WHITEDIAMOND
                 if (contextLine[0].startIcon != undefined && contextLine[0].startIcon == icon){
                     str += `<option value='${UMLLineIcons[icon]}' selected>${UMLLineIcons[icon]}</option>`;
                 }else {
