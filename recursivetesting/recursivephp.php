b8f3ca920aebbd59627e6933ce1b32d07d3b0619
Olle Jörlid <a21olljo@student.his.se>
2023-04-06 16:21:31 +0200

    Testing on other station after API block

--- recursivetesting/recursivephp.php
+++ recursivetesting/recursivephp.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- <script src="recursivejs.js" type="module"></script> -->
+<script src="recursivejs.js" type="module"></script>
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<?php
-$opts = [
-    'http' => [
-        'method' => 'GET',
-        'header' => [
-            'User-Agent: PHP'
-            ]
-            ]
-        ];
-        
-        $context = stream_context_create($opts);
-        $content = file_get_contents("https://api.github.com/repos/HGustavs/Webbprogrammering-Examples/contents/", false, $context);
-       
-		bfs($content);
-		
-        function bfs($url){
-			$contentArr = json_decode($url, true);
-            echo "<table style='border: 1px solid black'>";
-            echo "<td>";
+		<?php
+		bfs("https://api.github.com/repos/HGustavs/Webbprogrammering-Examples/contents/");
+
+		function bfs($url){
+			$opts = [
+				'http' => [
+					'method' => 'GET',
+					'header' => [
+						'User-Agent: PHP'
+						]
+						]
+					];
+			$context = stream_context_create($opts);
+			$content = file_get_contents($url, false, $context);
+			$contentArr = json_decode($content, true);
+			echo "<table style='border: 1px solid black'>";
+			echo "<td>";
 
 			foreach($contentArr as $key => $value) {
 				echo "<tr><td>" . $key . "</td>";
@@ -33,23 +31,23 @@ $opts = [
 					foreach($value as $k2 => $v2) {
 						echo "<td>" . $k2 . "</td>";
 					}
-				} else {
+				} else
 					echo "<td>" . $value . "</td></tr>";
-				}
+
 				if($value["type"] == "dir") {
+					print_r("<br><br>Im sending: <br>");
+					print_r($url . "/" . $value["name"]);
 					// print_r($value);
-					print_r($value["git_url"]);
-					bfs($value["git_url"]);
+					bfs($url . "/" . $value["name"]);
+				} else {
+					// print_r("<br><br>This is a file<br>");
+					// print_r($value["git_url"]);
 				}
 			}
-            
-            echo "</tr>";
-            echo "</table>";
-        }
-        
 
-
-
-        ?> 
+			echo "</tr>";
+			echo "</table>";
+		}
+		?>
     </body>
 </html>
