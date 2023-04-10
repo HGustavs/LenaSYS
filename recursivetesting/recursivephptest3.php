e84c1d74506a8d6905c64181ed2e70b54ef351ce
e21krida <e21krida@student.his.se>
2023-04-10 12:44:58 +0200

    Changed position of one comment
    Simply moved a comment down to make it more readable.

--- recursivetesting/recursivephptest3.php
+++ recursivetesting/recursivephptest3.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- <script src="recursivejs.js" type="module"></script> -->
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -67,7 +65,8 @@
             $path_count = substr_count($item['path'], '/');
             // Determine the class of the table element based on the path count
             $table_class = 'path' . ($path_count + 1);
-            echo '<table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';            // Checks if the fetched item is of type 'file'
+            echo '<table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';           
+            // Checks if the fetched item is of type 'file'
             if ($item['type'] == 'file') {
                 echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>' . $item['size'] . '</td><td><a href="' . $item['download_url'] . '">Download URL</a></td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
                 // Checks if the fetched item is of type 'dir'
