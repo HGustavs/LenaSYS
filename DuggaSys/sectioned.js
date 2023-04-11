db95e6eb7c250eca6f220ff157b92f72031c5bd2
d21emijo <d21emijo@student.his.se>
2023-04-11 15:32:05 +0200

    updated github img
    updated github img to include title and the temporary class "githubPointer"

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1613,11 +1613,9 @@ function returnedSection(data) {
         if (data['writeaccess'] || data['studentteacher']) {
           str += `<td style='width:32px;' class='${makeTextArray(itemKind, ["header", "section", 
           "code", "test", "moment", "link", "group", "message"])} ${hideState}'>`;
-          str += `<img style='max-width: 60%;' class="traschcanDelItemTab" alt='trashcan icon' tabIndex="0" id='dorf' title='Delete item' class='' 
+          str += `<img style='max-width: 60%;' class="githubPointer" alt='gitgub icon' tabIndex="0" id='dorf' title='Github' class='' 
           src='../Shared/icons/githubLink-icon.png' onclick='confirmBox(\"openConfirmBox\", this);'>`;
           str += "</td>";
-
-
         }
 
         
