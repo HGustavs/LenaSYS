218e3f8176b8d2fc926a802ead58fe486705b945
a21aldsa <a21aldsa@student.his.se>
2023-04-11 11:20:20 +0200

    Update diagram.js
    increased the css value "top" to the inline css of spawning UML inheritance

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8302,7 +8302,7 @@ function drawElement(element, ghosted = false)
     else if (element.kind == 'UMLRelation') {
         //div to encapuslate UML element
         str += `<div id='${element.id}'	class='element uml-element' onmousedown='ddown(event);' onmouseenter='mouseEnter();' onmouseleave='mouseLeave();'
-        style='left:0px; top:0px; width:${boxw}px;height:${boxh}px;`;
+        style='left:0px; top:5px; width:${boxw}px;height:${boxh}px;`;
 
         if(context.includes(element)){
             str += `z-index: 1;`;
