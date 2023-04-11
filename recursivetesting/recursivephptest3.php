34bceeff6ee5912c5ab9485cc7399d4147f96dcc
Olle Jörlid <a21olljo@student.his.se>
2023-04-11 09:05:51 +0200

    Improved bfs
    After not sufficient testing on last iteration, this one now uses a more conventional bfs, consisting of a fifo queue which uses a while-loop. Instead of recursively calling the function, this was changed because of api issues, and not wanting to use global variables.

--- recursivetesting/recursivephptest3.php
+++ recursivetesting/recursivephptest3.php
@@ -49,60 +49,55 @@
 <body>
     <?php
     
-    bfs('https://api.github.com/repos/a21olljo/Webbprogrammering-Examples/contents/', [], []);
-
-
-    
+    bfs('https://api.github.com/repos/a21olljo/Webbprogrammering-Examples/contents/');
         
-    function bfs($url, $fifoQueue, $visited)
+    function bfs($url)
     {
-        $opts = [
-            'http' => [
-                'method' => 'GET',
-                'header' => [
-                    'User-Agent: PHP',
-                    // 'Authorization: Bearer YOUR_GITHUB_API_KEY' // Replace YOUR_GITHUB_API_KEY with your actual GitHub API key
+        $visited = array();
+        $fifoQueue = array();
+        
+        array_push($fifoQueue, $url);
+
+        while(!empty($fifoQueue)) {
+            $currentUrl = array_shift($fifoQueue);
+            $opts = [
+                'http' => [
+                    'method' => 'GET',
+                    'header' => [
+                        'User-Agent: PHP',
+                        'Authorization: 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' // Replace YOUR_GITHUB_API_KEY with your actual GitHub API key
+                    ]
                 ]
-            ]
-        ];
+            ];
 
-        $context = stream_context_create($opts);
-        $data = file_get_contents($url, true, $context);
-        $json = json_decode($data, true);
+            $context = stream_context_create($opts);
+            $data = file_get_contents($currentUrl, true, $context);
+            $json = json_decode($data, true);
 
-        // Loops through each item fetched in the JSON data
-        foreach ($json as $item) {
-            // Count the number of slashes in the path
-            $path_count = substr_count($item['path'], '/');
-            // Determine the class of the table element based on the path count
-            $table_class = 'path' . ($path_count + 1);
-            // Checks if the fetched item is of type 'file'
-            if ($item['type'] == 'file') {
-                echo '<table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';
-                echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>' . $item['size'] . '</td><td><a href="' . $item['download_url'] . '">Download URL</a></td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
-                // Checks if the fetched item is of type 'dir'
-            } else if ($item['type'] == 'dir') {
-                echo '<br><table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';
-                echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>-</td><td>NULL</td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
-                array_push($fifoQueue, $item);
-            }
-            echo "</table>";
-        }
-
-
-        foreach ($visited as $item) {
-            if (($key = array_search($item, $fifoQueue)) !== false) {
-                unset($fifoQueue[$key]);
+            // Loops through each item fetched in the JSON data
+            foreach ($json as $item) {
+                // Count the number of slashes in the path
+                $path_count = substr_count($item['path'], '/');
+                // Determine the class of the table element based on the path count
+                $table_class = 'path' . ($path_count + 1);
+                // Checks if the fetched item is of type 'file'
+                if ($item['type'] == 'file') {
+                    echo '<table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';
+                    echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>' . $item['size'] . '</td><td><a href="' . $item['download_url'] . '">Download URL</a></td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
+                    // Checks if the fetched item is of type 'dir'
+                } else if ($item['type'] == 'dir') {
+                    echo '<br><table class="' . $table_class . '"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';
+                    echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>-</td><td>NULL</td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
+                    if (!in_array($item['url'], $visited)){        
+                        array_push($visited, $item['url']);
+                        array_push($fifoQueue, $item['url']);
+                    }
+                }
+                echo "</table>";
             }
         }
+        print_r("<br><br>Done<br>");
 
-        // After filtering out the dirs above the dirs are searched 
-        foreach ($fifoQueue as $item) {
-            echo '<br><table class="dir"><tr><th>Name</th><th>URL</th><th>Type</th><th>Size</th><th>Download URL</th><th>SHA</th><th>Path</th></tr>';
-            echo '<tr><td>' . $item['name'] . '</td><td><a href="' . $item['html_url'] . '">HTML URL</a></td><td>' . $item['type'] . '</td><td>-</td><td>NULL</td><td>' . $item['sha'] . '</td><td>' . $item['path'] . '</td></tr>';
-            array_push($visited, $item);
-            BFS($item['url'], $fifoQueue, $visited);
-        }
     }
     ?>
 </body>
