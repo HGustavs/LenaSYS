5cc3e4a7a225eec6e3287d2b8e04295b2c561901
d21emijo <d21emijo@student.his.se>
2023-04-11 15:00:09 +0200

    test 5
    try again

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1601,9 +1601,18 @@ function returnedSection(data) {
         
         // Trashcan
         if (data['writeaccess'] || data['studentteacher']) {
-          str += `<td style='width:62px;' class='${makeTextArray(itemKind, ["header", "section", 
+          str += `<td style='width:32px;' class='${makeTextArray(itemKind, ["header", "section", 
           "code", "test", "moment", "link", "group", "message"])} ${hideState}'>`;
-          str += `<img  class="traschcanDelItemTab" alt='trashcan icon' tabIndex="0" id='dorf' title='Delete item1337' class='' 
+          str += `<img  class="traschcanDelItemTab" alt='trashcan icon' tabIndex="0" id='dorf' title='Delete item' class='' 
+          src='../Shared/icons/Trashcan.svg' onclick='confirmBox(\"openConfirmBox\", this);'>`;
+          str += "</td>";
+
+
+        }
+        if (data['writeaccess'] || data['studentteacher']) {
+          str += `<td style='width:32px;' class='${makeTextArray(itemKind, ["header", "section", 
+          "code", "test", "moment", "link", "group", "message"])} ${hideState}'>`;
+          str += `<img  class="traschcanDelItemTab" alt='trashcan icon' tabIndex="0" id='dorf' title='Delete item' class='' 
           src='../Shared/icons/Trashcan.svg' onclick='confirmBox(\"openConfirmBox\", this);'>`;
           str += "</td>";
 
