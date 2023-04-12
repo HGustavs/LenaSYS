23faa3838a9626c6be576e0bb43297fbb2ddced9
Alexander Ström <c21alest@student.his.se>
2023-04-12 17:30:04 +0200

    Another try to store all logs

--- Shared/logging.php
+++ Shared/logging.php
@@ -1,11 +1,25 @@
 <?php
 $loggingDataJSON = file_get_contents("php://input"); // JSON string from dugga.js
 
+if(file_exists('log.json')){
+    $fileContent = file_get_contents('log.json');
+    $fileArray = json_decode($fileContent, true);
+    $fileArray[] = $loggingDataJSON;
+    if(file_put_contents('log.json', $fileArray)){
+        echo json_encode('Updated data in log file');
+    }
+}
+else{
+    if(file_put_contents('log.json', $loggingDataJSON)){
+        echo json_encode('Added latest log to log file');
+    }
+}
+
 // Write latest log
-$fp = fopen('logging', 'w');
+$fp = fopen('logging.json', 'w');
 fwrite($fp, $loggingDataJSON);
 fclose($fp);
 
-echo json_encode(['success wirting to log file'=>true]);
+echo json_encode('success writing to log file');
 
 ?>
