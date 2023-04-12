f6c06b4407ef35f350297a54c813f133a4698de0
a21jeaha <a21jeaha@student.his.se>
2023-04-12 07:50:27 +0200

    small adjustment to the test code

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -2810,7 +2810,7 @@ async function logForTesting(opt, apara, kind) {  // IN THE TESTIUNG PHASE
 
 	var  testString = "TEST";
 	try {
-		const response = await fetch(testString, {
+		const response = await fetch(./Shared/dugga.js, {
 			method: "POST", // or 'PUT'
 			headers: {
 				"Content-Type": "application/json",
