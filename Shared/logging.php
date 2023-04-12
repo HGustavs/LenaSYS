f43ee484636b462ff94cc5f69114409b578e7330
Alexander Ström <c21alest@student.his.se>
2023-04-12 15:32:58 +0200

    Test connection with PHP file

--- Shared/logging.php
+++ Shared/logging.php
@@ -1,24 +1,10 @@
 <?php
-$opt = $_POST["opt"]
-$apara = $_POST["apara"]
-$kind = $_POST["kind"]
-$aparaType = $_POST["apara_type"]
-
-echo $opt
-
-$jsonData = [
-    [
-        "test1" => "123",
-    ],
-    [
-        "test2" => "456",
-    ]
-];
-
-$jsonString = json_encode($jsonData, JSON_PRETTY_PRINT);
+$logginDataJSON = file_get_contents("php://input"); // json string
 
 $fp = fopen('logging.json', 'w');
-fwrite($fp, $jsonString);
+fwrite($fp, $logginDataJSON);
 fclose($fp);
+
+echo json_encode(['success'=>true]);
 
 ?>
