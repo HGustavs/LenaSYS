0aa1caf085b1dc5562faab8e89e5c1777d1d415b
Alexander Ström <c21alest@student.his.se>
2023-04-12 16:33:06 +0200

    Now two types of log, latest and all

--- Shared/logging.php
+++ Shared/logging.php
@@ -1,11 +1,19 @@
 <?php
 $logginDataJSON = file_get_contents("php://input"); // JSON string from dugga.js
 
-// Write to file
-$fp = fopen('logging.json', 'w');
-fwrite($fp, $logginDataJSON);
-fclose($fp);
+$logFileName = "log.json"
+$latestLogFileName = "latestlog.json"
 
-echo json_encode(['success wirting to log file'=>true]);
+// Write to log file
+$fileContent = file_get_contents(logFileName);
+$fileContentArray = json_decode($fileContent);
+array_push($fileContentArray, $logginDataJSON);
+$newJSONData = json_encode($fileContentArray);
+file_put_contents(logFileName, $newJSONData);
+
+// Write latest log
+file_put_contents(latestLogFileName, $logginDataJSON);
+
+echo json_encode(['success writing to log file'=>true]);
 
 ?>
