7c57fd389531fbaf089e4f1832c5b8a8c01422a7
a21jeaha <a21jeaha@student.his.se>
2023-04-12 15:13:05 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -1,6 +1,10 @@
 <?php
-    // if ($_GET) {
-    //     echo var_dump($_GET);
-    // }
+    if ($_GET) {
+        echo var_dump($_GET);
+    }
 ?>
+if ($_GET) {
+        echo var_dump($_GET);
+    }
+
 echo "<form action='dugga.js' method='GET'>";
