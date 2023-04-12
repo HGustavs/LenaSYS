a201ef11064c6d05e55818f5f7d77898e047df26
a21aldsa <a21aldsa@student.his.se>
2023-04-12 15:33:00 +0200

    Update diagram.js
    added a logging message for testing a possible solution

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2177,6 +2177,7 @@ function mouseMode_onMouseUp(event)
 
             
             if (ghostElement && event.button == 0) {
+                console.log(entityIsOverlapping(ghostElement.id, event.clientX, event.clientY));
                 addObjectToData(ghostElement);
                 makeGhost();
                 showdata();
