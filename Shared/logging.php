eb9d2355fa913c043767642181a46fd76c6b698c
Alexander Ström <c21alest@student.his.se>
2023-04-12 16:47:24 +0200

    Update logging.php

--- Shared/logging.php
+++ Shared/logging.php
@@ -4,23 +4,23 @@ $loggingDataJSON = file_get_contents("php://input"); // JSON string from dugga.j
 $logFileName = "log.json";
 $latestLogFileName = "latestlog.json";
 
-if(file_exists(logFileName)){
-    $fileContent = file_get_contents(logFileName);
+if(file_exists($logFileName)){
+    $fileContent = file_get_contents($logFileName);
     $fileContentArray = json_decode($fileContent);
     array_push($fileContentArray, $loggingDataJSON);
     $newJSONData = json_encode($fileContentArray);
 }
 else{
-    $newJSONData = loggingDataJSON;
+    $newJSONData = $loggingDataJSON;
 }
 
 // Write to all log file
-$fp = fopen(logFileName, 'w');
+$fp = fopen($logFileName, 'w');
 fwrite($fp, $newJSONData);
 fclose($fp);
 
 // Write latest log
-$fp = fopen(latestLogFileName, 'w');
+$fp = fopen($latestLogFileName, 'w');
 fwrite($fp, $loggingDataJSON);
 fclose($fp);
 
