5dcd00130fb84cb6107573cccdd895afc84fd73c
Alexander Ström <c21alest@student.his.se>
2023-04-12 16:38:33 +0200

    Different method of writing to file

--- Shared/logging.php
+++ Shared/logging.php
@@ -4,16 +4,22 @@ $logginDataJSON = file_get_contents("php://input"); // JSON string from dugga.js
 $logFileName = "log.json"
 $latestLogFileName = "latestlog.json"
 
-// Write to log file
 $fileContent = file_get_contents(logFileName);
 $fileContentArray = json_decode($fileContent);
 array_push($fileContentArray, $logginDataJSON);
 $newJSONData = json_encode($fileContentArray);
-file_put_contents(logFileName, $newJSONData);
+
+// Write to all log file
+$fp = fopen(logFileName, 'w');
+fwrite($fp, $newJSONData);
+fclose($fp);
 
 // Write latest log
-file_put_contents(latestLogFileName, $logginDataJSON);
+$fp = fopen(latestLogFileName, 'w');
+fwrite($fp, $logginDataJSON);
+fclose($fp);
 
-echo json_encode(['success writing to log file'=>true]);
+
+echo json_encode(['success wirting to log file'=>true]);
 
 ?>
