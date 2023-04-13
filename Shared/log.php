a7bfc180a25fbf6036398a263e45ec4b53ce4982
a21jeaha <a21jeaha@student.his.se>
2023-04-13 10:32:59 +0200

    changed the content of log.php, new focus presenting data from json file
    changed the content of log.php, new focus presenting data from json file

--- Shared/log.php
+++ Shared/log.php
@@ -1,22 +1,40 @@
 
 
+
+
+
+
 <?php
-    if(isset ($_POST)){
-        $test = file_get_contents("php://input");
-        $testArray = json_decode($test, true); 
-        foreach($testArray as $value ){
-            echo $value;
-        }
-        // echo $testArray["kind"];
-        // echo json_encode[$test];
-    }
+
+    $url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/logging.json";
+    $jsontext = file_get_contents($url);
+    $arr = json_decode($jsontext);
+
+    print_r($arr);
+
+
+
+
+
+
+
+
+    // if(isset ($_POST)){
+    //     $test = file_get_contents("php://input");
+    //     $testArray = json_decode($test, true); 
+    //     foreach($testArray as $value ){
+    //         echo $value;
+    //     }
+    //     // echo $testArray["kind"];
+    //     // echo json_encode[$test];
+    // }
     
 
-    // if ($_POST['input']) {
-    //     echo $_POST['input'];
-    // }
+    // // if ($_POST['input']) {
+    // //     echo $_POST['input'];
+    // // }
 
-    // echo "<p>";
+    // // echo "<p>";
 ?>
 
 <!-- echo "<form action='dugga.js' method='POST'>"; -->
