f976a1f7c0919f8e974fcdff3f5f8e50d07b46aa
d21emijo <d21emijo@student.his.se>
2023-04-13 15:11:13 +0200

    updated title
    updated title to clarify that there are for 2 diffrent github links

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1624,7 +1624,7 @@ function returnedSection(data) {
         if (itemKind === 4 && data['writeaccess'] || data['studentteacher'])  {
           str += `<td style='width:32px;' class='${makeTextArray(itemKind, ["header", "section", 
           "code", "test", "moment", "link", "group", "message"])} ${hideState}'>`;
-          str += `<img style='max-width: 60%;' class="githubPointer" alt='gitgub icon' tabIndex="0" id='dorf' title='Github' class='' 
+          str += `<img style='max-width: 60%;' class="githubPointer" alt='gitgub icon' tabIndex="0" id='dorf' title='Github repo' class='' 
           src='../Shared/icons/githubLink-icon.png' onclick='confirmBox(\"openConfirmBox\", this);'>`;
           str += "</td>";
         }
