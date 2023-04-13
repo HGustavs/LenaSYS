bfc57761ae543fcc281d82121f9f49c7fa95677d
a21lukpo <a21lukpo@student.his.se>
2023-04-13 15:42:34 +0200

    Reverted changes
    Reverted changes

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -3489,7 +3489,7 @@ function refreshCodeExample(exampelid){
 
   $(document).ready(function(){
     $(".refreshButton").click(function(){
-      var id = $(firstChild).attr('id');
+      var id = $(this).attr('id');
       console.log(id);
     })
   })
