cd085af6d0a779cb42a3e477b20cd3a4dc8dd3e4
d21emijo <d21emijo@student.his.se>
2023-04-13 14:13:40 +0200

    devide github icons
    trying to create diffrent functions depending on where you click the github icon

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1621,7 +1621,7 @@ function returnedSection(data) {
 
         }
         // github icon
-        if (data['writeaccess'] || data['studentteacher']) {
+        if (itemKind === 0 || itemKind === 1 && data['writeaccess'] || data['studentteacher']) {
           str += `<td style='width:32px;' class='${makeTextArray(itemKind, ["header", "section", 
           "code", "test", "moment", "link", "group", "message"])} ${hideState}'>`;
           str += `<img style='max-width: 60%;' class="githubPointer" alt='gitgub icon' tabIndex="0" id='dorf' title='Github' class='' 
