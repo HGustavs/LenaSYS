1f95d3800bd51c2661dd665e0a6a81031aa36f30
a21jeaha <a21jeaha@student.his.se>
2023-04-13 14:26:54 +0200

    Update log.php
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

--- Shared/log.php
+++ Shared/log.php
@@ -82,65 +82,27 @@
             // $dateTime = $arr['dateTime'];           
 
         
-            echo "<table style='width:100%'>";
-                echo "<tr>";
-                    echo "<th> Options </th>";
-                    echo "<th> Parameters </th>";
-                    echo "<th> Kind of Page </th>";
-                    echo "<th> Parameter type </th>";
-                    echo "<th> Date </th>";
-                echo "</tr>";
-                echo "<tr>";
-                    echo "<td>".$opt."</td>";
-                    /*echo "<td>".foreach($apara as $value){
-                        echo $value;
-                    }."</td>";*/
-                    echo "<td>".$apara."</td>";
-                    echo "<td>".$kind."</td>";
-                    echo "<td>".$apara_type."</td>";
-                    echo "<td>".$dateTime."</td>";
-                echo "</tr>";
-            echo "</table>";
+            // // // // echo "<table style='width:100%'>";
+            // // // //     echo "<tr>";
+            // // // //         echo "<th> Options </th>";
+            // // // //         echo "<th> Parameters </th>";
+            // // // //         echo "<th> Kind of Page </th>";
+            // // // //         echo "<th> Parameter type </th>";
+            // // // //         echo "<th> Date </th>";
+            // // // //     echo "</tr>";
+            // // // //     echo "<tr>";
+            // // // //         echo "<td>".$opt."</td>";
+            // // // //         /*echo "<td>".foreach($apara as $value){
+            // // // //             echo $value;
+            // // // //         }."</td>";*/
+            // // // //         echo "<td>".$apara."</td>";
+            // // // //         echo "<td>".$kind."</td>";
+            // // // //         echo "<td>".$apara_type."</td>";
+            // // // //         echo "<td>".$dateTime."</td>";
+            // // // //     echo "</tr>";
+            // // // // echo "</table>";
 
-
-
-            // echo "<table>";
-            //     echo "<td>".$opt;."</td>";
-            //         echo"<tr>";
-            //             foreach($apara as $value){
-            //                 echo "<td>".$value."</td>";
-            //             }
-            //         echo "</tr>";
-                    
-            //         echo "<tr>";
-            //         echo "<td>".$kind."</td>";
-            //         echo "<td>".$apara_type."</td>";
-            //         echo "<td>".$dateTime."</td>";
-            //     echo "</tr>";
-            // echo "</table>";    
-            // print_r($arr);
-
-
-
-
-            // if(isset ($_POST)){
-            //     $test = file_get_contents("php://input");
-            //     $testArray = json_decode($test, true); 
-            //     foreach($testArray as $value ){
-            //         echo $value;
-            //     }
-            //     // echo $testArray["kind"];
-            //     // echo json_encode[$test];
-            // }
-            
-
-            // // if ($_POST['input']) {
-            // //     echo $_POST['input'];
-            // // }
-
-            // // echo "<p>";
         ?>    
     </body>
 
 </html>
-<!-- echo "<form action='dugga.js' method='POST'>"; -->
