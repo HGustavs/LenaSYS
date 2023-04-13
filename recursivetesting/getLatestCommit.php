8aca8e78dc33b6c7c1e2c86666f479cf63d67faa
c21rebja <c21rebja@student.his.se>
2023-04-13 17:09:45 +0200

    Created new file
    Trying to fetch the entire html code from a set url that can later be used to see which the latest commit made was.
    
    Co-Authored-By: g21emmka <102578908+g21emmka@users.noreply.github.com>

---
+++ recursivetesting/getLatestCommit.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+		<script>
+			// Get url from db/courseGitURL instead of hardcoding
+			var url = 'https://github.com/HGustavs/LenaSYS.git';
+			var urledit = url.replace('.git', ''); //remove ending
+			console.log(fetch(urledit));
+
+			
+		</script>
+</head>
+<body>
+    <?php 
+        
+    ?>
+</body>
+</html>
