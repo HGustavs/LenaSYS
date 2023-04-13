a8947d42b6f2570a06ab79abb51320421f8702f5
a21jeaha <a21jeaha@student.his.se>
2023-04-13 10:55:17 +0200

    Update log.php
    structured the information decoded from json. I hope.

--- Shared/log.php
+++ Shared/log.php
@@ -1,40 +1,67 @@
 
+<html>
+    <head>
+        <style>
+            span{
+                padding-left:8px;
+            }
+        </style>
+    </head>
+    <body>
+        <table border='1'>
+        <?php
+
+            $url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/logging.json";
+            $jsontext = file_get_contents($url);
+            $arr = json_decode($jsontext, true);
+
+            $opt = $arr['opt'];
+            $apara[] = $arr['apara'];
+            $kind = $arr['kind'];
+            $apara_type = $arr['apara_type'];
+            $dateTime = $arr['dateTime'];           
+
+            echo "<ul>";
+                echo $opt;
+                foreach($apara as $value){
+                    echo $value;
+                }
+                echo $kind;
+                echo $apara_type;
+                echo $dateTime;
+            echo "</ul>"; 
+
+
+            // print_r($arr);
 
 
 
 
 
-<?php
-
-    $url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/logging.json";
-    $jsontext = file_get_contents($url);
-    $arr = json_decode($jsontext);
-
-    print_r($arr);
 
 
 
 
 
 
+            // if(isset ($_POST)){
+            //     $test = file_get_contents("php://input");
+            //     $testArray = json_decode($test, true); 
+            //     foreach($testArray as $value ){
+            //         echo $value;
+            //     }
+            //     // echo $testArray["kind"];
+            //     // echo json_encode[$test];
+            // }
+            
 
+            // // if ($_POST['input']) {
+            // //     echo $_POST['input'];
+            // // }
 
-    // if(isset ($_POST)){
-    //     $test = file_get_contents("php://input");
-    //     $testArray = json_decode($test, true); 
-    //     foreach($testArray as $value ){
-    //         echo $value;
-    //     }
-    //     // echo $testArray["kind"];
-    //     // echo json_encode[$test];
-    // }
+            // // echo "<p>";
+        ?>    
+    </body>
     
-
-    // // if ($_POST['input']) {
-    // //     echo $_POST['input'];
-    // // }
-
-    // // echo "<p>";
-?>
-
+</html>
 <!-- echo "<form action='dugga.js' method='POST'>"; -->
