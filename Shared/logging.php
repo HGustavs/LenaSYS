16703c28046b1707d520727e5b6d8b3482585fde
Alexander Ström <c21alest@student.his.se>
2023-04-13 10:59:21 +0200

    Success writing to two files, big log and latest

--- Shared/logging.php
+++ Shared/logging.php
@@ -1,27 +1,13 @@
 <?php
 $loggingDataJSON = file_get_contents("php://input"); // JSON string from dugga.js
 
-if(file_exists('log.json')){
-    $fileContent = file_get_contents('log.json');
-    $fileArray = json_decode($fileContent);
-    $fileArray[] = $loggingDataJSON;
-    $fp = fopen('log.json', 'w');
-    fwrite($fp, $fileArray);
-    fclose($fp);
-    echo json_encode('Updated data in log file');
-}
-else{
-     $fp = fopen('log.json', 'w');
-     fwrite($fp, $loggingDataJSON);
-     fclose($fp);
-     echo json_encode('Added latest log to log file');
-}
+$latestLogFile = "logs/latestlog.json";
+$logFile = "logs/log.json";
 
-// Write latest log
-$fp = fopen('latestlog.json', 'w');
-fwrite($fp, $loggingDataJSON);
-fclose($fp);
+// Append log to log file
+file_put_contents($logFile, $loggingDataJSON, FILE_APPEND);
 
-echo json_encode('success writing to log file');
+// Save latest log
+file_put_contents($latestLogFile, $loggingDataJSON);
 
 ?>
