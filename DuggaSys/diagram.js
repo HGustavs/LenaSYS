c1542d574d431f1fde795782c4a99071d251d429
b21matfi <b21matfi@student.his.se>
2023-04-13 13:28:55 +0200

    commented out the while loop
    commented out the while loop to see if that fixes the issue

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -503,15 +503,17 @@ class StateMachine
         // If there is no history => return
         if (this.currentHistoryIndex == -1) {return;}
         else {
-        do {
+            this.currentHistoryIndex--;
+            console.log(this.currentHistoryIndex);
+       /* do {
             // Lower the historyIndex by one
             this.currentHistoryIndex--;
             console.log(this.currentHistoryIndex);
 
-        }while(this.currentHistoryIndex != 0 ||
+        }while(this.currentHistoryIndex === 0 ||
            ( this.currentHistoryIndex > 0
             && this.historyLog[this.currentHistoryIndex] 
-            && this.historyLog[this.currentHistoryIndex - 1].time == this.historyLog[this.currentHistoryIndex].time));
+            && this.historyLog[this.currentHistoryIndex - 1].time == this.historyLog[this.currentHistoryIndex].time));*/
         }
         clearContext();
         clearContextLine();
