e64101aac737c37aaf4077a80f6f7bd9a9730b51
b21hamwe <b21hamwe@student.his.se>
2023-04-14 15:27:57 +0200

    Update dummylog.php
    magic

--- Shared/dummylog.php
+++ Shared/dummylog.php
@@ -1,70 +1,83 @@
-
-
-
- <?php
-
-
-/*$url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/logging.json";
-$jsontext = file_get_contents($url);
-$arr = json_decode($jsontext, true);
-
-@ -34,10 +34,117 @@ $dateTime = $arr['dateTime'];
+<html>
+<head>
+    <style>
+        table, th, td {
+        border:1px solid black;
+    }
+    </style>    
+</head>
+    <body>
+        <?php
+        
+            function debug($o){
+                echo '<pre>';
+                print_r($o);
+                echo '</pre>';
             }
+
             
-        echo "</td>";
-    echo "</tr>";
-    echo "</tr>";*/
+              
+            try {
+	            $log_db = new PDO('sqlite:../../log/loglena6.db');
+            } catch (PDOException $e) {
+	            echo "Failed to connect to the database";
+	            throw $e;
+            }
+            // $log_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            // $log_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);           // en del av error hanteringen.
 
     
-//---------------------------------------------------------------------------------------------------------------
-// connect log database - Open log database
-//---------------------------------------------------------------------------------------------------------------
+            
+            echo "<table style='width:100%'>";
+                foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $column) {
+                    echo "<th>".$column['Field']."</th>";
+                    echo "<script> console.log(".$column['Field']."); </script>"; 
+                    debug($column);
+                }  
+            echo "</table>";
+            
+            
+            // echo "<table style='width:100%'>";
+            //     foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $row) {
+            //         echo "BALALA";
+            //         echo "<th>".$row['uuid']."</th>";
+            //         echo "<script> console.log(".$row['uuid']."); </script>";
+            //         printf("EFTER".$row['Field']); 
 
-    if(!file_exists ('../../log')) {
-        if(!mkdir('../../log')){
-            echo "Error creating folder: log";
-            die;
-        }
-    }
-
-
-//---------------------------------------------------------------------------------------------------------------
-// IF MAKING CHANGES TO SQLite tables, increment this value!
-//---------------------------------------------------------------------------------------------------------------
-$dbVersion = 6;
-//---------------------------------------------------------------------------------------------------------------
-
-try {
-	$log_db = new PDO('sqlite:../../log/loglena6.db');
-} catch (PDOException $e) {
-	echo "Failed to connect to the database";
-	throw $e;
-}
+            //     }  
+            // echo "</table>";
+            
 
 
-$log_db->exec($sql);    
+            $url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/latestlog.json";
+            $jsontext = file_get_contents($url);
+            $arr = json_decode($jsontext, true);
+            
+            
+            // echo "<table style='width:100%'>";
+            //     echo "<tr>";
+            //         echo "<th> Options </th>";
+            //         echo "<th> Parameters </th>";
+            //         echo "<th> Kind of Page </th>";
+            //         echo "<th> Parameter type </th>";
+            //         echo "<th> Date </th>";
+              
+            //     echo "<tr>";
+            //         // Arrays of arrays can be traversed using a nested foreach
+            //         foreach ($arr as $key => $value) {
+            //             echo $value."</br>";
+            //             foreach ($value as $valuekey => $valuevalue) {
+            //             echo "<td>".$valuekey.": ".$valuevalue."</td></br>";
+            //             }
+            //         }
+            //         echo "<br>";
+            //         echo "</tr>";
 
+            
+            //     echo "</tr>";
+            // echo "</table>";
 
+        ?>    
+    </body>
 
-
-$query = $log_db->prepare( "SELECT * FROM serviceLogEntries;");
-$query->bindParam(':id', $id);
-$query->execute();
-
-  while ($row = $query->fetch(PDO::FETCH_ASSOC)){
-      $id=$row['id'];
-      $uuid=$row['uuid'];
-      $eventType=$row['eventType'];
-      $service=$row['service'];
-      $userid=$row['userid'];
-      $timestamp=$row['timestamp'];
-      $userAgent=$row['userAgent'];
-      $operatingSystem=$row['operatingSystem'];
-  }
-
-  $row = $result -> fetch_assoc();
-  printf("%s (%s)\n", $row["id"], $row["uuid"])
-  
-  $result -> free_result();
-
-?>
+</html>
