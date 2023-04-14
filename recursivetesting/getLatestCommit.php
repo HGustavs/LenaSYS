5522087a8ae94184e455760c72354e73acecf43f
g21emmka <102578908+g21emmka@users.noreply.github.com>
2023-04-14 10:39:41 +0200

    Trying to print a single element from the page
    Co-Authored-By: c21rebja <102578746+c21rebja@users.noreply.github.com>

--- recursivetesting/getLatestCommit.php
+++ recursivetesting/getLatestCommit.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-		<script>
-			// Get url from db/courseGitURL instead of hardcoding
-			var url = 'https://github.com/HGustavs/LenaSYS';
-			var urledit = url.replace('.git', ''); //remove ending
+  <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script>
+		// Get url from db/courseGitURL instead of hardcoding
+		//var url = 'https://github.com/HGustavs/LenaSYS';
+		//var urledit = url.replace('.git', ''); //remove ending
 
 			
-		</script>
+	</script>
 </head>
 <body>
-    <?php 
-        echo $page = file_get_contents("https://github.com/HGustavs/LenaSYS");
+	<?php 
 
-        echo print_r($page);
-    ?>
+		// Fails to load latest commit unless clearing cache on reload
+		$page = file_get_contents("https://github.com/HGustavs/LenaSYS"); 
+
+		echo print_r($page.getElementById("spoof-warning"));
+	?>
 </body>
 </html>
