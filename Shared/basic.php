5651039d6328df00132ddb29af07a2d87dd1552e
e21krida <e21krida@student.his.se>
2023-04-14 13:27:39 +0200

    fix

--- Shared/basic.php
+++ Shared/basic.php
@@ -241,7 +241,7 @@ $sql2 = '
 	CREATE TABLE gitRepos ( 
 		repoID INTEGER PRIMARY KEY,
 		repoName VARCHAR(50), 
-    repoURL VARCHAR(500), 
+    repoURL VARCHAR(255), 
     repoFileType VARCHAR(50), 
     repoDownloadURL VARCHAR(255), 
     repoSHA VARCHAR(255), 
