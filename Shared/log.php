1ab9464702e665ac8d1b042dd9377a716e6f753c
a21jeaha <a21jeaha@student.his.se>
2023-04-14 15:36:26 +0200

    Update log.php
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

--- Shared/log.php
+++ Shared/log.php
@@ -30,10 +30,10 @@
             
             echo "<table style='width:100%'>";
                 foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $column) {
-                    echo "<th>".$column['uuid']."</th>";
-                    echo "<th>".$column['service']."</th>";
-                    echo "<script> console.log(".$column['Field']."); </script>"; 
-                    //debug($column);
+                    // echo "<th>".$column['uuid']."</th>";
+                    // echo "<th>".$column['service']."</th>";
+                    // echo "<script> console.log(".$column['Field']."); </script>"; 
+                    debug($column);
                 }  
             echo "</table>";
             
