b2b6cf000e52c4de43667b206c10ad0086ede6f1
e21krida <e21krida@student.his.se>
2023-04-17 12:54:59 +0200

    Further testing of URL
    Co-Authored-By: a21olljo <102599614+a21olljo@users.noreply.github.com>

--- recursivetesting/FetchGithubRepo.php
+++ recursivetesting/FetchGithubRepo.php
@@ -78,7 +78,7 @@
                         // Checks if the fetched item is of type 'file'
                         if ($item['type'] == 'file') {
                             $fileContents = file_get_contents($item['download_url']);
-                            $path = dirname(__FILE__) . '/' . 'Webbprogrammering-Examples' . '/' . $item['path'];
+                            $path = dirname(__FILE__) . '/' . 'Webbprogrammering-Examples/' . $item['path'];
                             echo "<script>console.log('Debug Objects: " . $path . "' );</script>";
                             if (!file_exists((dirname($path)))) {
                                 mkdir(dirname($path), 0777, true);
