c3bf4191864caaf080a5f09050922b4ff30c34b5
g21emmka <102578908+g21emmka@users.noreply.github.com>
2023-04-17 10:41:57 +0200

    Added solution with dom
    Made phph displayed errors and found a solution with php dom to retrive the link
    
    Co-Authored-By: c21rebja <102578746+c21rebja@users.noreply.github.com>

--- recursivetesting/getLatestCommit.php
+++ recursivetesting/getLatestCommit.php
@@ -5,27 +5,37 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-
-	<script>
-
-		// const regexp = /^<a-href=\"(.*?)\/commit\//;
-		// var link = .querySelectorAll('.f6 a');
-		// for(let i = 0; i<link.length; i++) {
-		// 	if(i.match(regexp)) {
-		// 		console.log(link[i]);
-		// 	}
-		// }
-
-	</script>
 </head>
 <body>
 	<?php 
-	  // print_r();
-		// Get the contents of the HTML page
-		echo $html = file_get_contents("https://github.com/HGustavs/LenaSYS"); // Fails to load latest commit unless clearing cache on reload
-	?>
+		// Used to display errors on screen since PHP doesn't do that automatically.
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 
-	
+		$html = file_get_contents("https://github.com/HGustavs/LenaSYS"); // Fails to load latest commit unless clearing cache on reload
+		$dom = new DomDocument;
+		$dom->preserveWhiteSpace = FALSE;
+		
+		// Because of how dom works with html, this is necessary to not fill the screen with errors - the page still prints
+		libxml_use_internal_errors(true); 
+		$dom->loadHTML($html);
+		libxml_use_internal_errors(false);
+
+		//$orgUrl = "HGustavs/LenaSYS/commit"; // temporary until we get the actual url needed
+		$regex = "/^(.*?)\/commit\//";
+
+		// Go through the document
+		$links = $dom->getElementsByTagName('a');
+		foreach ($links as $link) {
+			$value = $link->getAttribute("href");
+			if(preg_match($regex, $value)) {
+				echo "<p>".$value."</p>"; // Print the link if it matches the regex
+			}
+		}
+
+		//print_r($dom);
+	?>
 
 </body>
 </html>
