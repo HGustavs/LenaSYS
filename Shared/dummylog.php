7a0ec9725f462db03e715c19c26ff133edb54431
b21hamwe <b21hamwe@student.his.se>
2023-04-17 09:09:49 +0200

    Update dummylog.php
    blubb blubb

--- Shared/dummylog.php
+++ Shared/dummylog.php
@@ -6,15 +6,7 @@
     }
     </style>    
 </head>
-
     <body>
-    <form id="form1" name="form1" method="post" action="<?php echo $PHP_SELF; ?>">  
-    Table List :  
-            <select dbtable Name='NEW'>  
-            <option value="">--- Select ---</option> 
-
-        <table border='1'>
-
         <?php
         
             function debug($o){
@@ -23,95 +15,137 @@
                 echo '</pre>';
             }
 
-            
-              
+
+
             try {
 	            $log_db = new PDO('sqlite:../../log/loglena6.db');
             } catch (PDOException $e) {
 	            echo "Failed to connect to the database";
 	            throw $e;
             }
-            // $log_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            // $log_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);           // en del av error hanteringen.
+        ?>
+            <span><form id="form1" name="form1" method="post" action="<?php echo $PHP_SELF; ?>">
 
-    
-            //echo "<tr><th>DuggaLoadLogEntries</th><th>exampleLoadLogEntries</th><th>logEntries</th><th>serviceLogEntries</th><th>userHistory</th><th>userLogEntries</th></tr>";
-              
-                //mysql_connect ("localhost","root","");  
-                mysql_select_db ("company");  
-                $select="company";  
-                if (isset ($select)&&$select!=""){  
-                $select=$_POST ['NEW'];  
-            }  
-            ?>  
-            <? 
-            $list=mysql_query("select * from employee order by emp_id asc");  
-            while($row_list=mysql_fetch_assoc($list)){  
-                ?>  
-                    <option value="<? echo $row_list['emp_id']; ?>"<? if($row_list['emp_id']==$select){ echo "selected"; } ?>>  
-                                         <?echo $row_list['emp_name'];?>  
-                    </option>  
-                <?  
-                }  
-                ?>  
-            </select>  
-            <input type="submit" name="Submit" value="Select" />  
-        </form>
+        <?php    
+                echo 'Medlemmar per spann:';     
+                
+                // hämtar namnen på de nuvarande Spannen 
+                echo '<select onchange="submit();" name="SpannNamn" >'; 
+                                                                   
+                echo '<option value="'.$row['Namn'].'"';
 
-            
-           <?php 
+                if(isset($_POST['SpannNamn'])){
+                    if($_POST['SpannNamn']==$row['Namn']) echo " selected ";
+                }
+                echo '>';
+                    echo $row['Namn'];
+                echo '</option>';
+                
+        
+
+            // gathers information from database table userHistory
             echo "<table style='width:100%'>";
-                foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $column) {
-                    echo "<th>".$column['Field']."</th>";
-                    echo "<script> console.log(".$column['Field']."); </script>"; 
-                    debug($column);
+                
+                echo '<tr>';
+                    echo '<th> refer </th>';
+                    echo '<th> userid </th>';
+                    echo '<th> username </th>';
+                    echo '<th> IP </th>';
+                    echo '<th> URLParams </th>';
+                    echo '<th> timestamp </th>';
+                echo '<tr>';
+                
+                foreach($log_db->query('SELECT * FROM userHistory;') as $row) {
+                    echo '<tr>';
+                        echo '<td>'.$row["refer"].'</td>';
+                        echo '<td>'.$row["userid"].'</td>';
+                        echo '<td>'.$row["username"].'</td>';
+                        echo '<td>'.$row["IP"].'</td>';
+                        echo '<td>'.$row["URLParams"].'</td>';
+                        echo '<td>'.$row["timestamp"].'</td>';
+                        echo '</tr>';
+                }  
+            echo "</table>";
+
+            // gathers information from database table userLogEntries
+            echo "<table style='width:100%'>";
+                
+            echo '<tr>';
+                echo '<th> id </th>';
+                echo '<th> uid </th>';
+                echo '<th> username </th>';
+                echo '<th> eventType </th>';
+                echo '<th> description </th>';
+                echo '<th> timestamp </th>';
+                echo '<th> userAgent </th>';
+                echo '<th> remoteAddress </th>';
+            echo '<tr>';
+            
+            foreach($log_db->query('SELECT * FROM userLogEntries;') as $row) {
+                echo '<tr>';
+                    echo '<td>'.$row["id"].'</td>';
+                    echo '<td>'.$row["uid"].'</td>';
+                    echo '<td>'.$row["username"].'</td>';
+                    echo '<td>'.$row["eventType"].'</td>';
+                    echo '<td>'.$row["description"].'</td>';
+                    echo '<td>'.$row["timestamp"].'</td>';
+                    echo '<td>'.$row["userAgent"].'</td>';
+                    echo '<td>'.$row["remoteAddress"].'</td>';
+                    echo '</tr>';
+            }  
+        echo "</table>";
+
+
+
+            // collects information from database table serviceLogEntries
+            echo "<table style='width:100%'>";
+                
+                echo '<tr>';
+                    echo '<th> id </th>';
+                    echo '<th> uuid </th>';
+                    echo '<th> eventType </th>';
+                    echo '<th> service </th>';
+                    echo '<th> userid </th>';
+                    echo '<th> timestamp </th>';
+                    echo '<th> userAgent </th>';
+                    echo '<th> operatingSystem </th>';
+                    echo '<th> info </th>';
+                    echo '<th> referer </th>';
+                    echo '<th> IP </th>';
+                    echo '<th> browser </th>';
+                echo '<tr>';
+                
+                foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $row) {
+                    echo '<tr>';
+                        echo '<td>'.$row["id"].'</td>';
+                        echo '<td>'.$row["uuid"].'</td>';
+                        echo '<td>'.$row["eventType"].'</td>';
+                        echo '<td>'.$row["service"].'</td>';
+                        echo '<td>'.$row["userid"].'</td>';
+                        echo '<td>'.$row["timestamp"].'</td>';
+                        echo '<td>'.$row["userAgent"].'</td>';
+                        echo '<td>'.$row["operatingSystem"].'</td>';
+                        echo '<td>'.$row["info"].'</td>'; //  $info = $opt..$cid..$coursevers..$fid..$filename..$kind;
+                        echo '<td>'.$row["referer"].'</td>';
+                        echo '<td>'.$row["IP"].'</td>';
+                        echo '<td>'.$row["browser"].'</td>';
+                    echo '</tr>';
                 }  
             echo "</table>";
             
+           
             
-            // echo "<table style='width:100%'>";
-            //     foreach($log_db->query('SELECT * FROM serviceLogEntries;') as $row) {
-            //         echo "BALALA";
-            //         echo "<th>".$row['uuid']."</th>";
-            //         echo "<script> console.log(".$row['uuid']."); </script>";
-            //         printf("EFTER".$row['Field']); 
-
-            //     }  
-            // echo "</table>";
-            
-
+           
+           
+           // currently not used, to be removed later on. 
+            /*    
             $url = "https://cms.webug.se/root/G2/students/a21jeaha/LenaSYS/Shared/latestlog.json";
             $jsontext = file_get_contents($url);
             $arr = json_decode($jsontext, true);
+            */
             
-            
-            // echo "<table style='width:100%'>";
-            //     echo "<tr>";
-            //         echo "<th> Options </th>";
-            //         echo "<th> Parameters </th>";
-            //         echo "<th> Kind of Page </th>";
-            //         echo "<th> Parameter type </th>";
-            //         echo "<th> Date </th>";
-              
-            //     echo "<tr>";
-            //         // Arrays of arrays can be traversed using a nested foreach
-            //         foreach ($arr as $key => $value) {
-            //             echo $value."</br>";
-            //             foreach ($value as $valuekey => $valuevalue) {
-            //             echo "<td>".$valuekey.": ".$valuevalue."</td></br>";
-            //             }
-            //         }
-            //         echo "<br>";
-            //         echo "</tr>";
-
-            
-            //     echo "</tr>";
-            // echo "</table>";
-
-           
-
+          
         ?>    
-        </table>
     </body>
 
 </html>
