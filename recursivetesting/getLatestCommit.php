58a39e428876ace7b4c8a26a2f7c75bb4c34ab35
c21rebja <c21rebja@student.his.se>
2023-04-17 16:26:57 +0200

    updated solution to only show the first result
    prints the first link, removing everything but the last numbers representing a commit
    
    Co-Authored-By: g21emmka <102578908+g21emmka@users.noreply.github.com>

--- recursivetesting/getLatestCommit.php
+++ recursivetesting/getLatestCommit.php
@@ -1,41 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<?php 
-		// Used to display errors on screen since PHP doesn't do that automatically.
-		ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
-		error_reporting(E_ALL);
+<?php 
+	// Used to display errors on screen since PHP doesn't do that automatically.
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 
-		$html = file_get_contents("https://github.com/HGustavs/LenaSYS"); // Fails to load latest commit unless clearing cache on reload
-		$dom = new DomDocument;
-		$dom->preserveWhiteSpace = FALSE;
-		
-		// Because of how dom works with html, this is necessary to not fill the screen with errors - the page still prints
-		libxml_use_internal_errors(true); 
-		$dom->loadHTML($html);
-		libxml_use_internal_errors(false);
+	$url = "https://github.com/HGustavs/LenaSYS"; // TODO: This url should be the one you fetch from the database!
 
-		//$orgUrl = "HGustavs/LenaSYS/commit"; // temporary until we get the actual url needed
-		$regex = "/^(.*?)\/commit\//";
+	$html = file_get_contents($url);
+	$dom = new DomDocument;
+	$dom->preserveWhiteSpace = FALSE;
+	
+	// Because of how dom works with html, this is necessary to not fill the screen with errors - the page still prints
+	libxml_use_internal_errors(true); 
+	$dom->loadHTML($html);
+	libxml_use_internal_errors(false);
 
-		// Go through the document
-		$links = $dom->getElementsByTagName('a');
-		foreach ($links as $link) {
-			$value = $link->getAttribute("href");
-			if(preg_match($regex, $value)) {
-				echo "<p>".$value."</p>"; // Print the link if it matches the regex
-			}
+	/* TODO: This regex could be improved to take the explicit name of the user and repo,
+	* for example "/HGustavs/LenaSYS/commit/..."
+	* instead of just taking "/.../.../commit/...". */
+	$regex = "/^(.*?)\/commit\//";
+	
+	$links = $dom->getElementsByTagName('a');
+
+	foreach ($links as $link) {
+		$value = $link->getAttribute("href");
+		if(preg_match($regex, $value)) {
+			$href = $value; // Takes the first matching value and stores it in an array
+			break; //exits the loop since only the first match is necessary
 		}
+	}
 
-		//print_r($dom);
-	?>
-
-</body>
-</html>
+	$latestCommit = preg_replace($regex, "", $href);
+	print_r($latestCommit); // TODO: This is where we could store the value in the db, or similar
+?>
