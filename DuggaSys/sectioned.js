edb7288fd05ebe5f111cec987c05f76715f3c347
b21sebgr <b21sebgr@student.his.se>
2023-04-17 11:05:11 +0200

    Test to find the name
    trying to find the variable to find the name of the file on this coulmn

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1553,6 +1553,7 @@ function returnedSection(data) {
         // Refresh button
         if (itemKind === 2 && data['writeaccess'] || data['studentteacher']) {
           str += `<td style='width:32px;'>`;
+          str += item['entryname'];
           str += `<img style='width:16px' alt='refresh icon' tabIndex='0'
                   id='dorf' class='refreshButton' title='Refresh code example' src='../Shared/icons/refresh.svg'`;
           str += " onclick='refreshCodeExample("+item['exempelid']+")'"
