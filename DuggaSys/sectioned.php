735b61334931a207d7db18b44e75b690991737dd
b21sebgr <b21sebgr@student.his.se>
2023-04-18 14:17:54 +0200

    test

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -36,7 +36,6 @@
 	<script src="../Shared/js/jquery-1.11.0.min.js"></script>
 	<script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
 	<script src="../Shared/dugga.js"></script>
-	<script src="../recursivetesting/FetchGithubRepo.js"></script>
 	<script src="sectioned.js"></script>
 	<script src="backToTop.js"></script>
 	
