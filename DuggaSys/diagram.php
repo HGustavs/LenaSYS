4e837397da2d849639b8b82a110efd0fa7cb3c31
a21nibah <a21nibah@student.his.se>
2023-04-18 09:47:47 +0000

    changed the elementPlacement parameters for the onclick for umlInital and final state buttons in diagram.php

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -392,7 +392,7 @@
                     </span>
                 </div>
                 <!-- UML Initial state selection -->
-                <div id="elementPlacement9" class="diagramIcons toolbarMode" onclick='setElementPlacementType(8); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
+                <div id="elementPlacement9" class="diagramIcons toolbarMode" onclick='setElementPlacementType(9); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
                     <img src="../Shared/icons/diagram_UML_initial_state.svg"/>
                     <span class="toolTipText"><b>UML initial state</b><br>
                         <p>Creates an initial state for UML.</p><br>
@@ -400,7 +400,7 @@
                     </span>
                 </div>
                 <!-- UML Final state selection -->
-                <div id="elementPlacement10" class="diagramIcons toolbarMode" onclick='setElementPlacementType(9); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
+                <div id="elementPlacement10" class="diagramIcons toolbarMode" onclick='setElementPlacementType(10); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
                     <img src="../Shared/icons/diagram_UML_final_state.svg"/>
                     <span class="toolTipText"><b>UML final state</b><br>
                         <p>Creates a final state for UML.</p><br>
