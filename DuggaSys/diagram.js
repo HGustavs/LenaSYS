ad90f0272ba73d7b7553176d841aeccff95e49f3
b21matfi <b21matfi@student.his.se>
2023-04-18 15:16:15 +0200

    Doing testing
    doing some testing to see if this is the properties

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6489,10 +6489,11 @@ function generateContextProperties()
             }
             
                 else if (element.kind ='SDState'){
-                    str +=`<div style='color:white'>TESTER</div>`;
+                    
                 for (const property in element) {
                     switch (property.toLowerCase()) {
                         case 'name':
+                            str +=`<div style='color:white'>TESTER</div>`;
                             str += `<div style='color:white'>Name</div>`;
                             str += `<input id='elementProperty_${property}' type='text' value='${element[property]}' onfocus='propFieldSelected(true)' onblur='propFieldSelected(false)'>`;
                             break;
