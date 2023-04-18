d82a770198d6b11c69f33c9f937e36da33da7b15
c21axepe <c21axepe@student.his.se>
2023-04-18 13:55:16 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -195,7 +195,7 @@
                             echo '<td>'.$row["userid"].'</td>';
                             $timestamp=$row["timestamp"];
                             echo $timestamp;
-                            $timestamp = date('y-m-d', $timestamp);
+                            $timestamp = date('l jS \of F Y h:i:s A', $timestamp);
                             echo '<td>'.$timestamp.'</td>';
                             echo '<td>'.$row["userAgent"].'</td>';
                             echo '<td>'.$row["operatingSystem"].'</td>';
