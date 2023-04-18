632917b07e0e2330809005dcfd7d83bb987ef75b
g21emmka <102578908+g21emmka@users.noreply.github.com>
2023-04-18 14:22:29 +0200

    added the refresh button to the styling
    Added the id's of the refresh button in Media Queries
    
    Co-Authored-By: c21rebja <102578746+c21rebja@users.noreply.github.com>

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -247,9 +247,9 @@
 							// Refresh button for Github repo in hamburger menu
 							echo "<div id='refreshBurger'>";
             	echo "<a id='refreshBTN' title='Refresh Github Repo' value='Refresh' href='#'>";
-							echo "<img alt='refresh icon'  class='burgerButt' src='../Shared/icons/lrefresh.svg'>";
+							echo "<img alt='refresh icon'  class='burgerButt' src='../Shared/icons/refresh.svg'>";
 							echo "</a>";
-							echo "<a class='burgerButtText' href='#' >Refresh Github Repo</a></div>";
+							echo "<a class='burgerButtText' href='#' >Refresh github repo</a></div>";
 					
 							//Adding home button to the teacher burger menu
 							echo "<div id='homeBurgerTeacher'>";
