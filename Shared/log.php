d48e94792ab859c31f4d29b0c60a2d65229c3a84
c21axepe <c21axepe@student.his.se>
2023-04-18 14:11:12 +0200

    Added descriptions

--- Shared/log.php
+++ Shared/log.php
@@ -33,7 +33,7 @@
         <!----------------------------------------------------------------------------------->
         <span><form id="form1" name="form1" method="post" action="<?php echo $PHP_SELF; ?>">
         <?php    
-                date_default_timezone_set('Etc/GMT+2');
+                date_default_timezone_set('Etc/GMT+2'); //Used in serviceLogEntries to convert unix to datetime
 
                 echo 'choose table';
 
@@ -194,7 +194,7 @@
                             echo '<td>'.$row["service"].'</td>';
                             echo '<td>'.$row["userid"].'</td>';
                             $timestamp=$row["timestamp"];
-                            $timestamp = date('Y-m-d h:i:s', $timestamp/1000);
+                            $timestamp = date('Y-m-d h:i:s', $timestamp/1000); //Divide by 1000 cause unix is in miliseconds
                             echo '<td>'.$timestamp.'</td>';
                             echo '<td>'.$row["userAgent"].'</td>';
                             echo '<td>'.$row["operatingSystem"].'</td>';
