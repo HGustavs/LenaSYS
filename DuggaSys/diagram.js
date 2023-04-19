2bd6a35a20059bcad5145e97e15bbf025e47f83f
a21aldsa <a21aldsa@student.his.se>
2023-04-19 16:46:22 +0200

    Update diagram.js
    fixed small syntax error in my code

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8918,12 +8918,12 @@ function drawElement(element, ghosted = false)
         //then make a horizontal line that is the negative width
         //finally, close the path
         str += `<path 
-            d="M'${linew}','${(linew)+5}'
+            d="M${linew},${(linew)+5}
                 q5,-5 0,-5
-                h'${(boxw - (linew * 2))-5}'
+                h${(boxw - (linew * 2))-5}
                 q5,5 5,0
-                v'${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}'
-                h'${(boxw - (linew * 2))*-1}'
+                v${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}
+                h${(boxw - (linew * 2))*-1}
                 z
             "
             stroke-width='${linew}'
