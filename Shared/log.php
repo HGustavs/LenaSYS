699979221ec2fbcfbec5e0bab64cd72eb9c88927
a21oscgu <a21oscgu@student.his.se>
2023-04-19 13:36:14 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -221,7 +221,50 @@
 
             if((isset($_POST['name'])) && ($_POST['name']=='userLogEntries')){
                 // gathers information from database table userLogEntries
-                echo "<table style='width:100%'>";
+                
+                if(isset($_GET['order'])){
+                    $order = $_GET['order'];
+                }
+                else{
+                    $order = 'id';
+                }
+                if(isset($_GET['sort'])){
+                    $sort = $_GET['sort'];
+                }
+                else{
+                    $sort = 'ASC';
+                }
+                $resultSet = $log_db->query('SELECT * FROM userLogEntries ORDER BY $order $sort;');
+                if($resultSet->num_rows > 0){
+
+                    $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
+
+                    echo"
+                    <table border='1'>
+                        <tr>
+                            <th><a href='?order=id&&sort=$sort'>id</a></th>
+                            <th><a href='?order=uid&&sort=$sort'>uid</a></th>
+                    ";
+                    while($rows = $resultSet->fetch_assoc()){
+                        $id = $rows['id'];
+                        $uid = $rows['uid'];
+                        echo"
+                        <tr>
+                            <td>$id</td>
+                            <td>$uid</td>
+                        </tr>
+                        ";
+                    }
+                    echo"
+                        </tr>
+                    </table>
+                    ";
+                }
+                else{
+                    echo "No records returned.";
+                }
+                
+                /*echo "<table style='width:100%'>";
                     
                 echo '<tr>';
                     echo '<th> id </th>';
@@ -246,8 +289,7 @@
                         echo '<td>'.$row["remoteAddress"].'</td>';
                         echo '</tr>';
                 }  
-                echo "</table>";
-
+                echo "</table>";*/
             }            
             
 
