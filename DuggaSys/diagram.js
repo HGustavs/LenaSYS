88fe066ccde60b6387e7e365802cd611cd92d14c
b21matfi <b21matfi@student.his.se>
2023-04-19 14:25:56 +0200

    trying to see old type
    wanting to see what comes out when uml is placed and changed

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2998,7 +2998,7 @@ function changeState()
     const element =  context[0],
           oldType = element.type,
           newType = document.getElementById("typeSelect")?.value || document.getElementById("propertySelect")?.value || undefined;
-
+        console.log(OldType);
     /* If the element has a new type and got lines, then it can't change type. */
     if (newType !== undefined && oldType != newType && elementHasLines(element)) {
         displayMessage("error", `
