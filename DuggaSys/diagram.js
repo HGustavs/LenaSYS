b178deb2c4be882f48e27be44d6f9e0f73125f5d
b21matfi <b21matfi@student.his.se>
2023-04-20 13:16:28 +0200

    Update diagram.js
    changed some code

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8934,7 +8934,7 @@ function drawElement(element, ghosted = false)
             for (var i = 0; i < elemAttri; i++) {
                 str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:${element.attributes[i]}</text>`;
             }
-            
+            str += `</svg>`;
 
             //end of svg for background
             
@@ -8948,7 +8948,7 @@ function drawElement(element, ghosted = false)
             //end of svg for background
             str += `</svg>`;
         }
-        if (elemFunc != 0) {
+        if (elemFunc > 0) {
             str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
             str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
