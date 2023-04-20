7fb5aee87ce4d107368bd79c1be8e11981969dd1
c21rebja <c21rebja@student.his.se>
2023-04-20 17:12:16 +0200

    more comments for tomorrow
    Co-Authored-By: g21emmka <102578908+g21emmka@users.noreply.github.com>

--- recursivetesting/getLatestCommit.php
+++ recursivetesting/getLatestCommit.php
@@ -1,18 +1,8 @@
 <?php 
-
 	// Used to display errors on screen since PHP doesn't do that automatically.
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-
-	//Get data from AJAX call in courseed.js and then runs the function getNewCourseGithub link
-	if(isset($_POST['action'])) 
-	{
-		if($_POST['action'] == 'getCourseID') 
-		{
-			getCourseID($_POST['githubURL']);
-		}
-	};
 
 	// Include basic application services!
 	include_once "../Shared/basic.php";
@@ -22,15 +12,24 @@
 	pdoConnect();
 	session_start();
 
-	//getCourseID("https://github.com/HGustavs/DiagrammingSYS");
+	//Get data from AJAX call in courseed.js and then runs the function getNewCourseGithub link
+	// if(isset($_POST['action'])) 
+	// {
+	// 	if($_POST['action'] == 'getCourseID') 
+	// 	{
+	// 		getCourseID($_POST['githubURL']);
+	// 	}
+	// };
+
+	// DUMMY CODE
+	getCourseID("https://github.com/HGustavs/DiagrammingSYS");
 
 	function getCourseID($githubURL) {
-
 		echo "<table>";
+		
 		// translates the url to the same structure as in mysql
 		// the "/" needs to be "&#47;" for the query to work
 		$newURL = str_replace("/", "&#47;", $githubURL);
-		print_r($newURL);
 
 		// fetching from the database
 		global $pdo;
