8bdf30ec7d5dd1ca46071c7ebf694aa6f41eed02
a21oscgu <a21oscgu@student.his.se>
2023-04-20 11:35:50 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -145,20 +145,20 @@
                     $sort = 'ASC';
                 }*/
 
-                /*$userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries ORDER BY timestamp DESC;');
+                $userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries ORDER BY timestamp DESC;');
                 $userLogEntriesResults = $userLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
                 $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
                 echo"
                 <table border='1'>
                     <tr>
-                        <th><a href='?order=id&&sort=$sort'>id</a></th>
-                        <th><a href='?order=uid&&sort=$sort'>uid</a></th>
-                        <th><a href='?order=username&&sort=$sort'>username</a></th>
-                        <th><a href='?order=eventType&&sort=$sort'>eventType</a></th>
-                        <th><a href='?order=description&&sort=$sort'>description</a></th>
-                        <th><a href='?order=timestamp&&sort=$sort'>timestamp</a></th>
-                        <th><a href='?order=userAgent&&sort=$sort'>userAgent</a></th>
-                        <th><a href='?order=remoteAddress&&sort=$sort'>remoteAddress</a></th>
+                        <th><a href='log.php?order=id&&sort=$sort'>id</a></th>
+                        <th><a href='log.php?order=uid&&sort=$sort'>uid</a></th>
+                        <th><a href='log.php?order=username&&sort=$sort'>username</a></th>
+                        <th><a href='log.php?order=eventType&&sort=$sort'>eventType</a></th>
+                        <th><a href='log.php?order=description&&sort=$sort'>description</a></th>
+                        <th><a href='log.php?order=timestamp&&sort=$sort'>timestamp</a></th>
+                        <th><a href='log.php?order=userAgent&&sort=$sort'>userAgent</a></th>
+                        <th><a href='log.php?order=remoteAddress&&sort=$sort'>remoteAddress</a></th>
                     </tr>
                 ";
                 foreach($userLogEntriesResults as $rows) {
@@ -176,7 +176,7 @@
                 print "<pre>";
                 print_r($userLogEntriesResults);
                 print "</pre>";
-                */
+                /*
                 // gathers information from database table userLogEntries
                 echo "<table style='width:100%'>";
                     echo '<tr>';
@@ -201,7 +201,7 @@
                         echo '<td>'.$row["remoteAddress"].'</td>';
                     echo '</tr>';
                 }  
-                echo "</table>";
+                echo "</table>";*/
             }
 
             if((isset($_POST['name'])) && ($_POST['name']=='serviceLogEntries')){
