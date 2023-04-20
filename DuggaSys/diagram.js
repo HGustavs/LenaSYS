c08b0cdcebdbdf398d4a7892e8758c1b87e97439
a21aldsa <a21aldsa@student.his.se>
2023-04-20 09:07:47 +0200

    Update diagram.js
    adjusted initial drawing position to account for final curve, this also makes the horizontal line shorter of course.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8911,8 +8911,8 @@ function drawElement(element, ghosted = false)
         //svg for SD header, background and text
         str += `<svg width='${boxw}' height='${boxh}'>`;
         str += `<path 
-            d="M${(linew)},${(linew)}
-                h${(boxw - (linew * 2))-15}
+            d="M${(linew)+15},${(linew)}
+                h${(boxw - (linew * 2))-30}
                 a15,15 0 0 1 15,15
                 v${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2))-15}
                 h${(boxw - (linew * 2))*-1}
