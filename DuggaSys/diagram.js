f07a55ac2224e49d882b0c552cb161f127c21985
b21matfi <b21matfi@student.his.se>
2023-04-20 13:11:22 +0200

    changed to a new if statement
    changed so that its in the new if statement

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8934,9 +8934,7 @@ function drawElement(element, ghosted = false)
             for (var i = 0; i < elemAttri; i++) {
                 str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:${element.attributes[i]}</text>`;
             }
-            for (var i = 0; i < elemFunc; i++) {
-                str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
-            }
+            
 
             //end of svg for background
             str += `</svg>`;
@@ -8949,6 +8947,14 @@ function drawElement(element, ghosted = false)
             str += `<text x='5' y='${hboxh + boxh / 2}' dominant-baseline='middle' text-anchor='right'> </text>`;
             //end of svg for background
             str += `</svg>`;
+        }
+        if (elemFunc != 0) {
+            str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
+            str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
+            stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
+            for (var i = 0; i < elemFunc; i++) {
+                str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
+            }
         }
         //end of div for SD content
         str += `</div>`;
