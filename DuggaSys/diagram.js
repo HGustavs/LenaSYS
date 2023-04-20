e096e2c1838e6dd2d2e10bfc7df4b0ed8664166f
b21matfi <b21matfi@student.his.se>
2023-04-20 15:09:15 +0200

    Update diagram.js
    changed so that the top line of the rect disapears and bottom line

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8944,10 +8944,10 @@ function drawElement(element, ghosted = false)
         } else {
             //svg for background
             str += `<svg width='${boxw}' height='${boxh / 2 + (boxh / 2)}' style='border-bottom-left-radius: ${elemFunc == 0 ? boxh / 2 : 0}px; border-bottom-right-radius: ${elemFunc == 0 ? boxh / 2 : 0}px;'>`;
-            str += `<rect x='${linew}' y='${boxh / 2}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh / 2) - (linew * 2)}'
+            str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
             str += `<text x='${xAnchor}' y='${hboxh + boxh * 0 / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:</text>`;
-            if (elemFunc == 0) {
+            if (elemFunc != 0) {
                 str += `<rect x='${linew}' y='${boxh / 2 + (boxh * elemAttri / 2) - linew}' width='${boxw - (linew * 2)}' height='${linew}' fill='${element.stroke}' />`;
               }
             
