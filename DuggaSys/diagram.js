4e73bc17219a17faf475b7b792c9667ea168499a
a21aldsa <a21aldsa@student.his.se>
2023-04-20 08:31:42 +0200

    Update diagram.js
    left out a vital part of creating an arc, should work now

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8919,7 +8919,7 @@ function drawElement(element, ghosted = false)
         //finally, close the path
         str += `<path 
             d="M${linew},${(linew)}
-                A 5 5 0 0 1
+                A 5 5 0 0 1 5,5
                 h${(boxw - (linew * 2))-5}
                 q5,5 5,0
                 v${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}
