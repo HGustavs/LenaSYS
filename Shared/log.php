4bfee2c601b76266ef2fdb1d37be15502d4565cd
a21oscgu <a21oscgu@student.his.se>
2023-04-20 10:39:46 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -148,7 +148,7 @@
                 $order = "id";
                 $sort = "ASC";
 
-                $userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries ORDER BY '{$order}''{$sort}';'); // ORDER BY $order $sort
+                $userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries ORDER BY $order $sort;'); // ORDER BY $order $sort
                 $userLogEntriesResults = $userLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
                 $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
                 echo"
