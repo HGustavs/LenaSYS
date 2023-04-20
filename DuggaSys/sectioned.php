8bfaa0b222363f284e1186809a41a80798674424
d21emijo <d21emijo@student.his.se>
2023-04-20 12:31:44 +0200

    test

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -621,12 +621,12 @@
 							<td align='right'><input class='submit-button' type='button' value='Save' onclick='updateTemplate();' /></td>
 						</tr>
 					</table>
+					<div class='inputwrapper'><span>Name:</span><input class='textinput' type='text' id='hash' disabled placeholder='Name.type' value=''/></div>
+					<div class='inputwrapper'><span>GithubUrl:</span><input class='textinput' type='text' id='hash' placeholder='GitHubDownloadUrl' value=''/></div>
+					<div class='inputwrapper'><span>Filepath:</span><input class='textinput' type='text' id='hash' placeholder='no' value=''/></div>
+					<div class='inputwrapper'><span>Order of items:</span><input class='textinput' type='text' id='hash' placeholder='nope' value=''/></div>
 				</div>
 				
-				<div class='inputwrapper'><span>Name:</span><input class='textinput' type='text' id='hash' disabled placeholder='Name.type' value=''/></div>
-				<div class='inputwrapper'><span>GithubUrl:</span><input class='textinput' type='text' id='hash' placeholder='GitHubDownloadUrl' value=''/></div>
-				<div class='inputwrapper'><span>Filepath:</span><input class='textinput' type='text' id='hash' placeholder='no' value=''/></div>
-				<div class='inputwrapper'><span>Order of items:</span><input class='textinput' type='text' id='hash' placeholder='nope' value=''/></div>
 
 
 				
