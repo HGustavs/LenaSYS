cc99389134e96c24dc3c053895e8d57d37efd29b
b21matfi <b21matfi@student.his.se>
2023-04-20 16:20:28 +0200

    removed svg

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8951,13 +8951,13 @@ function drawElement(element, ghosted = false)
         
         str += `<div style='margin-top: ${-8 * zoomfact}px;'>`;
         if (elemFunc != 0) {
-           str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
+           //str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
            // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
             //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
             for (var i = 0; i < elemFunc; i++) {
                 str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
             }
-            str += `</svg>`;
+          //  str += `</svg>`;
         }
         //end of div for SD content
         str += `</svg>`;
