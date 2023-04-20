1981f9ac80f12ae75dc90d834983499416e91c6a
b21matfi <b21matfi@student.his.se>
2023-04-20 16:24:13 +0200

    Update diagram.js
    changed postion of the function

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8935,6 +8935,15 @@ function drawElement(element, ghosted = false)
             for (var i = 0; i < elemAttri; i++) {
                 str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:${element.attributes[i]}</text>`;
             }
+            if (elemFunc != 0) {
+                //str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
+                // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
+                 //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
+                 for (var i = 0; i < elemFunc; i++) {
+                     str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
+                 }
+               //  str += `</svg>`;
+             }
             str += `<div style='margin-top: ${-8 * zoomfact}px;'>`;
             //end of svg for background
             
@@ -8951,15 +8960,7 @@ function drawElement(element, ghosted = false)
         }
         
        
-        if (elemFunc != 0) {
-           //str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
-           // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
-            //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
-            for (var i = 0; i < elemFunc; i++) {
-                str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
-            }
-          //  str += `</svg>`;
-        }
+       
         //end of div for SD content
         str += `</svg>`;
         str += `</div>`;
