3ab4f6eb6c7db634337b63b001785778c4309990
a21oscgu <a21oscgu@student.his.se>
2023-04-20 10:31:38 +0200

    Update log.php

--- Shared/log.php
+++ Shared/log.php
@@ -131,7 +131,7 @@
 
             if((isset($_POST['name'])) && ($_POST['name']=='userLogEntries')){
 
-                if(isset($_GET['id'])){
+                /*if(isset($_GET['id'])){
                     echo "It works";
                     $order = $_GET['id'];
                 }
@@ -143,8 +143,11 @@
                 }
                 else{
                     $sort = 'ASC';
-                }
+                }*/
                 
+                $order = "id";
+                $sort = "ASC";
+
                 $userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries ORDER BY '$order $sort';'); // ORDER BY $order $sort
                 $userLogEntriesResults = $userLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
                 $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
