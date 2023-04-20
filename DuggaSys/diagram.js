63bf5f3a890a41a77e7f48043c0cc758db66824f
b21matfi <b21matfi@student.his.se>
2023-04-20 11:10:02 +0200

    Update diagram.js
    Added some more code trying to see if this changes

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6529,7 +6529,12 @@ function generateContextProperties()
                             break;
                         case 'attributes':
                             str += `<div style='color:white'>Attributes</div>`;
-                            str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(element[property])}</textarea>`;
+                            const attributes = element[property];
+                            const nonEditablePart = "do: ";
+                            const editablePart = attributes.replace(nonEditablePart, "");
+                            str += `<div>${nonEditablePart}</div>`;
+                            str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(editablePart)}</textarea>`;
+    
                             break;
                         default:
                             break;
