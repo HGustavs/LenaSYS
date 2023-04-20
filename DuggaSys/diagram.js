9ba5a2957cce63606849dc2c6c8ce5d0923f25a7
b21matfi <b21matfi@student.his.se>
2023-04-20 14:22:34 +0200

    rounded corners
    trying to remove rounded corners when the exit is spawned

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8928,7 +8928,7 @@ function drawElement(element, ghosted = false)
         //Draw SD-content if there exist at least one attribute
         if (elemAttri != 0) {
             //svg for background
-            str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}' >`;
+            str += `<svg id='SD-Corner' width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;' >`;
             str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
             for (var i = 0; i < elemAttri; i++) {
@@ -8941,7 +8941,7 @@ function drawElement(element, ghosted = false)
             // Draw SD-content if there are no attributes.
         } else {
             //svg for background
-            str += `<svg width='${boxw}' height='${boxh / 2 + (boxh / 2)}' >`;
+            str += `<svg id='SD-Corner' width='${boxw}' height='${boxh / 2 + (boxh / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
             str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
             str += `<text x='${xAnchor}' y='${hboxh + boxh * 0 / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:</text>`;
@@ -8952,6 +8952,9 @@ function drawElement(element, ghosted = false)
         str += `</div>`;
         str += `<div style='margin-top: ${-8 * zoomfact}px;'>`;
         if (elemFunc != 0) {
+            const sdCorner = document.getElementById("SD-corner");
+            sdCorner.style.border-bottom-left-radius == 0;
+            sdCorner.style.border-bottom-right-radius == 0;
             str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemFunc / 2)}' style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
             str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
