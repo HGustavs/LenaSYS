23cba7703bd122d57f6cd94ef53198f2dc0130ff
b21matfi <b21matfi@student.his.se>
2023-04-20 15:40:53 +0200

    commented some code

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8937,13 +8937,13 @@ function drawElement(element, ghosted = false)
             }
             
             if (elemFunc != 0) {
-                str += `<svg width='${boxw}'  style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
+               // str += `<svg width='${boxw}'  style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
                 // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
                  //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
                  for (var i = 0; i < elemFunc; i++) {
                      str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
                  }
-                 str += `</svg>`;
+                 //str += `</svg>`;
              }
              str += `</div>`;
             str += `</svg>`;
