05b14bea4080e80b1e7c9ed9ff5519e61718aa09
b21matfi <b21matfi@student.his.se>
2023-04-20 15:41:52 +0200

    div placemnt
    changed div placement

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8935,17 +8935,17 @@ function drawElement(element, ghosted = false)
             for (var i = 0; i < elemAttri; i++) {
                 str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>do:${element.attributes[i]}</text>`;
             }
-            
+            str += `</div>`;
             if (elemFunc != 0) {
-               // str += `<svg width='${boxw}'  style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
+                str += `<svg width='${boxw}'  style='border-bottom-left-radius: ${boxh / 2}px; border-bottom-right-radius: ${boxh / 2}px;'>`;
                 // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
                  //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
                  for (var i = 0; i < elemFunc; i++) {
                      str += `<text x='${xAnchor}' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
                  }
-                 //str += `</svg>`;
+                 str += `</svg>`;
              }
-             str += `</div>`;
+             
             str += `</svg>`;
             //end of svg for background
             
