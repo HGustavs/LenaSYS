ef7cd1bb0d82cc8d3d70337dd9d70f12f054fbb5
a21aldsa <a21aldsa@student.his.se>
2023-04-20 08:55:34 +0200

    Update diagram.js
    trying to use a arc w relative length instead of coordinate based

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8918,9 +8918,9 @@ function drawElement(element, ghosted = false)
         //then make a horizontal line that is the negative width
         //finally, close the path
         str += `<path 
-            d="M${linew},${(linew)}
-                h${(boxw - (linew * 2))-5}
-                A 5 5 0 0 1 5,5
+            d="M${(linew)},${(linew)}
+                h${(boxw - (linew * 2))-20}
+                a20,20 0 0 1 20,20
                 v${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2))-5}
                 h${(boxw - (linew * 2))*-1}
                 z
