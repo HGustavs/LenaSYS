534b675d6b8ce7ebccbecbb1a4fe8cbdcb164c16
b21matfi <b21matfi@student.his.se>
2023-04-20 17:01:17 +0200

    Update diagram.js
    fixed syntax error

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8943,7 +8943,7 @@ function drawElement(element, ghosted = false)
                 // str += `<rect x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemFunc / 2) - (linew * 2)}'
                  //stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
                  for (var i = 0; i < elemFunc; i++) {
-                     str += `<text x='${xAnchor}' y='${elemFuncy * i}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
+                     str += `<text x='${xAnchor}' y='${elemFuncY * i}' dominant-baseline='middle' text-anchor='${vAlignment}'>exit:${element.functions[i]}</text>`;
                  }
                  str += `</svg>`;
              }
