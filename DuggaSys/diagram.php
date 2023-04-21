4d011ae47b27bc901b8b1e899dfd52132250efa0
b21matfi <b21matfi@student.his.se>
2023-04-21 15:32:28 +0200

    Update diagram.php
    added the id back and added the class back aswell

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -543,10 +543,10 @@
     <!-- Message prompt -->
     <div id="diagram-message"></div>
     <div id="zoom-container">
-        <div class="diagramZoomIcons">
+        <div id ="zoom-message-box" class="diagramZoomIcons">
             <img width="25%" height="25%" src="../Shared/icons/zoom-message-icon.svg"/>
             <text id ="zoom-message">1x</text>
-        </div>
+    </div>
         <div class="diagramZoomIcons" onclick='zoomin();'>
             <img src="../Shared/icons/diagram_zoomin.svg"/>
             <span class="zoomToolTipText"><b>Zoom IN</b><br>
