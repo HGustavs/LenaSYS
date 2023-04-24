30c40f91423866d5286d8ed97cc0c22fbb603546
b21hamwe <b21hamwe@student.his.se>
2023-04-24 15:42:17 +0200

    Summary was requested...
    additional tries!

--- DuggaSys/codeviewerService.php
+++ DuggaSys/codeviewerService.php
@@ -620,4 +620,18 @@
 
 	logServiceEvent($log_uuid, EventTypes::ServiceServerEnd, "codeviewerService.php",$userid,$info);
 
+
+	$info = array(
+		"opt",$opt,
+		"courseId",$courseId,
+		"courseVersion", $courseVersion,
+		"exampleName", $exampleName,
+		"sectionName", $sectionName,
+		"exampleId", $exampleId 
+	);
+		
+		
+		$json = json_encode($info , JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
+
+
 ?>
