59adf9b24c2d40bff0cf81ee4640813c73fc5af5
b21eriho <erokiyt@gmail.com>
2023-04-24 11:21:18 +0200

    Add text-class to path

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8841,7 +8841,7 @@ function drawElement(element, ghosted = false)
         str += `<div style='width: ${boxw}; height: ${boxh};'>`;
         //svg for SD header, background and text
         str += `<svg width='${boxw}' height='${boxh}'>`;
-        str += `<path 
+        str += `<path class="text" 
             d="M${linew+cornerRadius},${(linew)}
                 h${(boxw - (linew * 2))-(cornerRadius*2)}
                 a${cornerRadius},${cornerRadius} 0 0 1 ${cornerRadius},${cornerRadius}
@@ -8868,7 +8868,7 @@ function drawElement(element, ghosted = false)
         if (elemAttri != 0) {
             //svg for background
             str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}'>`;
-            str += `<path 
+            str += `<path class="text"
                 d="M${linew},${(linew)}
                     h${(boxw - (linew * 2))}
                     v${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2))-cornerRadius}
