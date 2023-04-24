4d461db10ea92bb524e9a1f17c1821efc1b214d8
d21emijo <d21emijo@student.his.se>
2023-04-24 14:22:06 +0200

    test

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -574,7 +574,7 @@
 			</div>
 			<div class='inputwrapper'><span>Name:</span><input class='textinput' type='text' id='hash' placeholder='Name.type' value=''/></div>
 
-			<div class='inputwrapper'><span>file:</span><select class='textinput' type='text' id='' placeholder='Name.type' value=''></div>
+			<div class='inputwrapper'><span>file:</span><select class='textinput' type='text' id='' placeholder='Name.type' value='' /></div>
 			
 			<div class='inputwrapper'><span>GithubUrl:</span><input class='textinput' type='text' id='hash' placeholder='GitHubDownloadUrl' value=''/></div>
 			<div class='inputwrapper'><span>Filepath:</span><input class='textinput' type='text' id='hash' placeholder='no' value=''/></div>
