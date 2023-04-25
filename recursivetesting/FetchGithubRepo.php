ee44f1f9fc4179b2c82e31fa9d47c2a904261d04
e21krida <e21krida@student.his.se>
2023-04-25 12:32:12 +0200

    tested adding of cid

--- recursivetesting/FetchGithubRepo.php
+++ recursivetesting/FetchGithubRepo.php
@@ -157,7 +157,8 @@ function bfs($url, $repository)
                             array_push($fifoQueue, $item['url']);
                         }
                     }
-                    $query = $pdoLite->prepare('INSERT INTO gitFiles (fileName, fileType, fileURL, downloadURL, fileSHA, filePath) VALUES (:fileName, :fileType, , :fileURL, :downloadURL, :fileSHA, :filePath)');
+                    $query = $pdoLite->prepare('INSERT INTO gitFiles (cid, fileName, fileType, fileURL, downloadURL, fileSHA, filePath) VALUES (:cid, :fileName, :fileType, , :fileURL, :downloadURL, :fileSHA, :filePath)');
+                    $query->bindParam(':cid', 1);
                     $query->bindParam(':fileName', $item['name']);
                     $query->bindParam(':fileType', $item['type']);
                     $query->bindParam(':fileURL', $item['url']);
