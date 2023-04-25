f26308a84501e014f7f2949065367630fa295e14
Pontus Kronenberg <a21ponkr@student.his.se>
2023-04-25 14:54:51 +0200

    #13330
    put js to another file

--- Shared/log.php
+++ Shared/log.php
@@ -13,48 +13,12 @@
 <html>
 <head>
 
-<script>
-function searchTable() {
-    var input, filter, table, tr, td, i, j, txtValue;
-    input = document.getElementById("searchInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementsByTagName("table")[0];
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        for (j = 0; j < tr[i].cells.length; j++) {
-            td = tr[i].cells[j];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    break;
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
-}
-
-function filterTable() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementsByTagName("table")[0].innerHTML = this.responseText;
-        }
-    };
-    var filter = document.getElementById("searchInput").value;
-    var table = document.getElementById("tableID").value;
-    xmlhttp.open("GET", "log.php?table=" + table + "&filter=" + filter, true);
-    xmlhttp.send();
-}
-</script>
-
     <style>
         table, th, td {
         border:1px solid black;
     }
-    </style>    
+    </style>
+     <script type="text/javascript" src="logSearch.js"></script>
 </head>
     <body>
         <?php
