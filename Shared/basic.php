ae081485ece7029b5c27ef7880919e8462a77471
e21krida <e21krida@student.his.se>
2023-04-25 13:08:31 +0200

    further testing of database

--- Shared/basic.php
+++ Shared/basic.php
@@ -258,13 +258,14 @@ $metadata_db->exec($sql2);
 
 $sql3 = '
 	CREATE TABLE IF NOT EXISTS gitFiles ( 
-		cid INTEGER PRIMARY KEY,
-		fileName VARCHAR(50) PRIMARY KEY, 
+		cid INTEGER,
+		fileName VARCHAR(50), 
     fileType VARCHAR(50),
 		fileURL VARCHAR(255),
     downloadURL VARCHAR(255), 
     fileSHA VARCHAR(255), 
-    filePath VARCHAR(255)
+    filePath VARCHAR(255),
+		PRIMARY KEY (cid, fileName)
 	);
 '; 
 $metadata_db->exec($sql3);
