f8461d353100cd05f4052e1bb31079f6cd17a27b
b21hamwe <b21hamwe@student.his.se>
2023-04-25 14:59:52 +0200

    betting
    bett av kanelbulle

--- DuggaSys/courseedservice.php
+++ DuggaSys/courseedservice.php
@@ -673,7 +673,7 @@ echo json_encode($array);
 logServiceEvent($log_uuid, EventTypes::ServiceServerEnd, "courseedservice.php",$userid,$info);
 
 
-$info = array(
+/*$info = array(
 	"opt"=>$opt,
 	"cid"=>$cid,
 	"coursename"=> $coursename,
@@ -683,6 +683,6 @@ $info = array(
 );
 	
 	$json = json_encode($info , JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
-	print_r($info);
+	print_r($info);*/
 
 ?>
