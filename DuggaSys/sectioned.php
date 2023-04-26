f7a8ee958afe555d1a2672e5ac83b610d5a4886f
d21emijo <d21emijo@student.his.se>
2023-04-26 22:56:27 +0200

    test

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -15,25 +15,23 @@
 	// $cid = 1;
 
 	function testingtesting(){
-		// $pdolite = new PDO('sqlite:../../githubMetadata/metadata2.db');
-		// $query =  $pdolite->prepare('SELECT fileName FROM gitFiles WHERE cid = :cid;');
-		// $query->bindParam(':cid', 1);
-		// $query->execute();
-		// $simmy = $query->fetch(PDO::FETCH_ASSOC);
-		// echo "<option value='1'>".$simmy['fileName']"</option>";
-		// if($simmy){
-		// 	echo "<p>" . $simmy['fileName'] . "</p>";
+		$pdolite = new PDO('sqlite:../../githubMetadata/metadata2.db');
+		$query =  $pdolite->prepare('SELECT fileName FROM gitFiles WHERE cid = :cid;');
+		$query->bindParam(':cid', 1);
+		$query->execute();
+		$simmy = $query->fetch(PDO::FETCH_ASSOC);
+		echo "<option value='1'>".$simmy['fileName']"</option>";
+		if($simmy){
+			echo "<p>" . $simmy['fileName'] . "</p>";
 
-		// }else{
-			// echo "<p> bad </p>";
-		// }
+		}else{
+			echo "<p> bad </p>";
+		}
 		// foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
 		// 	echo "<option value='1'>".$row['fileName']"</option>";
 
 		// }
-		$text = "Plogen";
-        $pTag = "<p>" . $text . "</p>";
-        return $pTag;
+
 
 	}
 	
