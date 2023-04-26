ed083217a191b3c73979bdbbb18055ae5d7f7da8
Thea <c21theti@student.his.se>
2023-04-26 14:30:23 +0200

    Commented out "type"

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6277,11 +6277,11 @@ function generateContextProperties()
           //Skip diagram type-dropdown if element does not have an UML equivalent, in this case only applies to ER attributes
           //TODO: Find a way to do this dynamically as new diagram types are added
           if (element.kind != 'ERAttr') {
-              str += `<div style='color:white'>Type</div>`;
+              //str += `<div style='color:white'>Type</div>`;
 
               //Create a dropdown menu for diagram type
               var value = Object.values(entityType);
-              //var selected = context[0].type;
+              var selected = context[0].type;
 
               str += '<select id="typeSelect">';
                for (i = 0; i < value.length; i++) {
@@ -6295,18 +6295,18 @@ function generateContextProperties()
            }
 
           //Selected ER type
-          //if (element.type == 'ER') {
+          if (element.type == 'ER') {
               //ID MUST START WITH "elementProperty_"!!!!!1111!!!!!1111 
-             // for (const property in element) {
-                  //switch (property.toLowerCase()) {
-                      //case 'name':
-                         // str += `<div style='color:white'>Name</div>`;
-                         // str += `<input id='elementProperty_${property}' type='text' value='${element[property]}' onfocus='propFieldSelected(true)' onblur='propFieldSelected(false)'>`;
-                         // break;
-                      //default:
-                        //  break;
-                 // }
-             // }
+              for (const property in element) {
+                  switch (property.toLowerCase()) {
+                      case 'name':
+                          str += `<div style='color:white'>Name</div>`;
+                          str += `<input id='elementProperty_${property}' type='text' value='${element[property]}' onfocus='propFieldSelected(true)' onblur='propFieldSelected(false)'>`;
+                          break;
+                      default:
+                          break;
+                  }
+              }
               str += `<div style='color:white'>Variant</div>`;
 
               //Creates drop down for changing state of ER elements
@@ -6335,29 +6335,29 @@ function generateContextProperties()
           }
 
           //Selected UML type
-          //else if (element.type == 'UML') {
+          else if (element.type == 'UML') {
               //If UML entity
-              //if (element.kind == 'UMLEntity') {
+              if (element.kind == 'UMLEntity') {
                   //ID MUST START WITH "elementProperty_"!!!!!1111!!!!!1111 
-                 // for (const property in element) {
-                     // switch (property.toLowerCase()) {
-                         // case 'name':
-                            //  str += `<div style='color:white'>Name</div>`;
-                            //  str += `<input id='elementProperty_${property}' type='text' value='${element[property]}' onfocus='propFieldSelected(true)' onblur='propFieldSelected(false)'>`;
-                            //  break;
-                         // case 'attributes':
-                            //  str += `<div style='color:white'>Attributes</div>`;
-                             // str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(element[property])}</textarea>`;
-                             // break;
-                          //case 'functions':
-                            //  str += `<div style='color:white'>Functions</div>`;
-                            //  str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(element[property])}</textarea>`;
-                            //  break;
-                         // default:
-                           //   break;
-                     // }
-                //  }
-              //}
+                  for (const property in element) {
+                      switch (property.toLowerCase()) {
+                          case 'name':
+                              str += `<div style='color:white'>Name</div>`;
+                              str += `<input id='elementProperty_${property}' type='text' value='${element[property]}' onfocus='propFieldSelected(true)' onblur='propFieldSelected(false)'>`;
+                              break;
+                          case 'attributes':
+                              str += `<div style='color:white'>Attributes</div>`;
+                              str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(element[property])}</textarea>`;
+                              break;
+                          case 'functions':
+                              str += `<div style='color:white'>Functions</div>`;
+                              str += `<textarea id='elementProperty_${property}' rows='4' style='width:98%;resize:none;'>${textboxFormatString(element[property])}</textarea>`;
+                              break;
+                          default:
+                              break;
+                      }
+                  }
+              }
 
               //If UML inheritance
               else if (element.kind = 'UMLRelation') {
