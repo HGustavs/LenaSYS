652c803cb1ca42f7c9b99eee5fdd9be68fb34de5
a21hugni <a21hugni@student.his.se>
2023-04-26 14:48:06 +0200

    Test
    Co-Authored-By: e21krida <102578849+e21krida@users.noreply.github.com>

--- Shared/database.php
+++ Shared/database.php
@@ -36,13 +36,26 @@ function dbConnect()
 	// Select DB
 	mysql_select_db(DB_NAME) or err("Could not select database \"".DB_NAME."\" error code".mysql_errno(),$hdr); */
 
-    global $pdo;
-    try {
-        $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        err('Could not connect to database: '.$e->getMessage(), 'Database Connection Error');
+	    $printHeaderFunction = false;
+    // Send header info to err()?
+    if ($printHeaderFunction) {
+        $hdr = 'Database Connection Error';
+    } else {
+        $hdr = '';
     }
+
+    // Connect to DB server
+    $OC_db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+    if (!$OC_db) {
+        die("Could not connect to database " . mysqli_connect_error());
+    }
+    mysqli_set_charset($OC_db, 'utf8');
+
+    // Select DB
+    if (!mysqli_select_db($OC_db, DB_NAME)) {
+        die("Could not select database \"" . DB_NAME . "\" error code " . mysqli_errno($OC_db));
+    }
+
 }
 
 function pdoConnect()
