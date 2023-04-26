9be92a309f1e5365a501eefd2ec6687c9409431f
b21matfi <b21matfi@student.his.se>
2023-04-26 14:50:52 +0200

    testing so move position

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -310,28 +310,28 @@
                         </span>
                         <div id="togglePlacementTypeButton1" class="placementTypeIcon togglePlacementTypeButton">
                             <img src="../Shared/icons/diagram_toolbar_arrow.svg" alt="An arrow for expanding this menu option"/>
+                            <div id="togglePlacementTypeBox1" class="togglePlacementTypeBox togglePlacementTypeBoxRI"><!--<-- UML functionality start-->
+                                <div class="ERButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(1,1); setElementPlacementType(1); setMouseMode(2);'>
+                                    <img src="../Shared/icons/diagram_relation.svg" alt="ER relation"/>
+                                    <span class="placementTypeToolTipText"><b>ER relation</b><br>
+                                        <p>Change to ER relation</p>
+                                    </span>
+                                </div>
+                                <div class="UMLButton placementTypeBoxIcons" onclick='togglePlacementType(5,1); setElementPlacementType(5); setMouseMode(2);'>
+                                    <img src="../Shared/icons/diagram_inheritance.svg" alt="UML Inheritance"/>
+                                    <span class="placementTypeToolTipText"><b>UML Inheritance</b><br>
+                                        <p>Change to UML inheritance</p>
+                                    </span>
+                                </div>
+                                <div class="IEButton placementTypeBoxIcons" onclick='togglePlacementType(7,1); setElementPlacementType(7); setMouseMode(2);'>
+                                    <img src="../Shared/icons/diagram_IE_inheritance.svg" alt="IE inheritance"/>
+                                    <span class="placementTypeToolTipText"><b>UML Inheritance</b><br>
+                                        <p>Change to IE inheritance</p>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>    
-                    <div id="togglePlacementTypeBox1" class="togglePlacementTypeBox togglePlacementTypeBoxRI"><!--<-- UML functionality start-->
-                        <div class="ERButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(1,1); setElementPlacementType(1); setMouseMode(2);'>
-                            <img src="../Shared/icons/diagram_relation.svg" alt="ER relation"/>
-                            <span class="placementTypeToolTipText"><b>ER relation</b><br>
-                                <p>Change to ER relation</p>
-                            </span>
-                        </div>
-                        <div class="UMLButton placementTypeBoxIcons" onclick='togglePlacementType(5,1); setElementPlacementType(5); setMouseMode(2);'>
-                            <img src="../Shared/icons/diagram_inheritance.svg" alt="UML Inheritance"/>
-                            <span class="placementTypeToolTipText"><b>UML Inheritance</b><br>
-                                <p>Change to UML inheritance</p>
-                            </span>
-                        </div>
-                        <div class="IEButton placementTypeBoxIcons" onclick='togglePlacementType(7,1); setElementPlacementType(7); setMouseMode(2);'>
-                            <img src="../Shared/icons/diagram_IE_inheritance.svg" alt="IE inheritance"/>
-                            <span class="placementTypeToolTipText"><b>UML Inheritance</b><br>
-                                <p>Change to IE inheritance</p>
-                            </span>
-                        </div>
-                    </div>
                 </div>
                 <div>
                     <div id="elementPlacement5"
