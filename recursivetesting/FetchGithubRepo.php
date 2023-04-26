85df0be5229642407b7e890a826558285805bafd
e21krida <e21krida@student.his.se>
2023-04-26 13:31:06 +0200

    Separated inserts to metadata2 database into separate function
    Co-Authored-By: a21lukpo <102577654+a21lukpo@users.noreply.github.com>

--- recursivetesting/FetchGithubRepo.php
+++ recursivetesting/FetchGithubRepo.php
@@ -74,6 +74,19 @@ function insertToFileLink($cid, $item) {
         } 
     }
 }
+
+function insertToMetaData($cid, $item) {
+    global $pdoLite;
+    $query = $pdoLite->prepare('INSERT INTO gitFiles (cid, fileName, fileType, fileURL, downloadURL, fileSHA, filePath) VALUES (:cid, :fileName, :fileType, :fileURL, :downloadURL, :fileSHA, :filePath)');
+    $query->bindParam(':cid', $cid);
+    $query->bindParam(':fileName', $item['name']);
+    $query->bindParam(':fileType', $item['type']);
+    $query->bindParam(':fileURL', $item['url']);
+    $query->bindParam(':downloadURL', $item['download_url']);
+    $query->bindParam(':fileSHA', $item['sha']);
+    $query->bindParam(':filePath', $item['path']);
+    $query->execute();
+}
     
 function bfs($url)
 {
@@ -81,6 +94,7 @@ function bfs($url)
     $fifoQueue = array();
     // TODO remove repository creation bacuse we create folders in course local
     array_push($fifoQueue, $url);
+    global $pdoLite;
     $pdoLite = new PDO('sqlite:../../githubMetadata/metadata2.db');
 
     // TODO link the course with cid, should not be hardcoded 
@@ -127,7 +141,6 @@ function bfs($url)
                         if (!file_exists((dirname($path)))) {
                             mkdir(dirname($path), 0777, true);
                         } 
-
                         insertToFileLink($cid, $item);
                         // Writes the file to the respective folder. 
                         file_put_contents($path, $fileContents);
@@ -144,15 +157,6 @@ function bfs($url)
                             array_push($fifoQueue, $item['url']);
                         }
                     }
-                    $query = $pdoLite->prepare('INSERT INTO gitFiles (cid, fileName, fileType, fileURL, downloadURL, fileSHA, filePath) VALUES (:cid, :fileName, :fileType, :fileURL, :downloadURL, :fileSHA, :filePath)');
-                    $query->bindParam(':cid', $cid);
-                    $query->bindParam(':fileName', $item['name']);
-                    $query->bindParam(':fileType', $item['type']);
-                    $query->bindParam(':fileURL', $item['url']);
-                    $query->bindParam(':downloadURL', $item['download_url']);
-                    $query->bindParam(':fileSHA', $item['sha']);
-                    $query->bindParam(':filePath', $item['path']);
-                    $query->execute();
                     echo "</table>"; 
                 }
             } else {
@@ -177,4 +181,5 @@ function bfs($url)
         }
     }
 }
+
 ?>
