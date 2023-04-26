41b34f35644849f2a81e29243595801f06088d43
g21emmka <102578908+g21emmka@users.noreply.github.com>
2023-04-26 15:42:07 +0200

    added "or rerplace" to overwrite existing values in sqlite
    Co-Authored-By: c21rebja <102578746+c21rebja@users.noreply.github.com>

--- recursivetesting/getLatestCommit.php
+++ recursivetesting/getLatestCommit.php
@@ -69,7 +69,8 @@
 	// Create a new row if it doesn't exist
 	function insertIntoSQLite($url, $cid, $commit) {
 		$pdolite = new PDO('sqlite:../../githubMetadata/metadata2.db');
-		$query = $pdolite->prepare("INSERT INTO gitRepos (cid, repoURL, lastCommit) VALUES (:cid, :repoURL, :commits)"); 
+		// Remove "or replace" later when everything works like it should
+		$query = $pdolite->prepare("INSERT OR REPLACE INTO gitRepos (cid, repoURL, lastCommit) VALUES (:cid, :repoURL, :commits)"); 
 		$query->bindParam(':cid', $cid);
 		$query->bindParam(':repoURL', $url);
 		$query->bindParam(':commits', $commit);
