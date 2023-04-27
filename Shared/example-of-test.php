d98892542e3b380730eb6a7631ea1789cec1ac00
Alexander Ström <c21alest@student.his.se>
2023-04-27 15:02:10 +0200

    Implemented Test 3: call service test

--- Shared/example-of-test.php
+++ Shared/example-of-test.php
@@ -1,10 +1,20 @@
 <?php
 
+session_start();
+
 $postParameter = array(
     'value1' => 'test',
     'value2' => 'test',
     'user' => 'brom',
-    'pwd' => 'password'
+    'pwd' => 'password',
+    'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
+    'serviceData' => serialize(array(
+        'opt' => 'NEW',
+        'username' => 'brom',
+    )),
+    'test1Debug' => false,
+    'test2Debug' => true,
+    'test3Debug' => true,
 );
 
 $curl = curl_init('https://cms.webug.se/root/G2/students/c21alest/LenaSYS/Shared/test.php');
@@ -15,5 +25,9 @@ $curlResponse = curl_exec($curl);
 curl_close($curl);
 
 echo $curlResponse;
+
+//setcookie("loginname", "brom", time() + (86400 * 30), "/");
+//setcookie("username", "brom", time() + (86400 * 30), "/");
+//setcookie("password", "password", time() + (86400 * 30), "/");
 
 ?>
--- Shared/test.php
+++ Shared/test.php
@@ -2,21 +2,30 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+    $test1Result = false;
+    $test2Result = false;
+    $test3Result = false;
+
     // Test 1 assertEqual
-    echo json_encode(assertEqual($_POST['value1'], $_POST['value2']));
+    echo json_encode(assertEqualTest($_POST['value1'], $_POST['value2'], $_POST['test1Debug']));
 
     // Test 2 login
-    echo json_encode(loginTest($_POST['user'], $_POST['pwd']));
+    echo json_encode(loginTest($_POST['user'], $_POST['pwd'], $_POST['test2Debug']));
+
+    // Test 3 callService
+    echo json_encode(callServiceTest($_POST['service'], $_POST['serviceData'], $_POST['test3Debug']));
 
 
 }
 
 // Test 1: assert equal test
-function assertEqual($value1, $value2){
+function assertEqualTest($value1, $value2, $debug){
+
     if (($_POST['value1'] != null) && ($_POST['value2'] != null)){
         $equalTest = assert($value1, $value2);
         if ($equalTest){
             $equalTestResult = "passed";
+            $test1Result = true;
         }
         else{
             $equalTestResult = "failed";
@@ -26,29 +35,83 @@ function assertEqual($value1, $value2){
         $equalTestResult = "failed no valid values";
     }
 
-    return array(
-            'Test 1 (assertEqual)' => $equalTestResult,
+    // If debug true send back detailed info
+    if ($debug == true) {
+        return array(
+            'Test 1 (assertEqual):' => $equalTestResult,
             'value1' => $value1,
             'value2' => $value2
         );
+    }
+    else{
+        return array(
+            'Test 1 (assertEqual):' => $equalTestResult,
+        );
+    }
 }
 
 // Test 2: login test
-function loginTest($user, $pwd){
+function loginTest($user, $pwd, $debug){
+
     // Session includes login functionality
     include_once "../Shared/sessions.php";
-    if (login($user, $pwd, false)) {
+
+    if (login($user, $pwd, true)) {
         $loginTestResult = "passed";
+        $test2Result = true;
     }
     else{
         $loginTestResult = "failed";
     }
 
-    return array(
+    // If debug true send back detailed info
+    if ($debug == true) {
+        return array(
             'Test 2 (login):' => $loginTestResult,
             'username' => $user,
             'password' => $pwd
         );
+    }
+    else{
+        return array(
+            'Test 2 (login):' => "passed",
+        );
+    }
+}
+
+// Test 3: call service test
+function callServiceTest($service, $data, $debug){
+
+    // Make POST request to service
+    $curl = curl_init($service);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, unserialize($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $curlResponse = curl_exec($curl);
+
+    curl_close($curl);
+
+    if ($curl) {
+        $callServiceTestResult = "passed";
+        $test3Result = true;
+    }
+    else{
+        $callServiceTestResult = "failed";
+    }
+
+    // If debug true send back detailed info
+    if ($debug == true) {
+        return array(
+            'Test 3 (callService):' => $curlResponse,
+            'service' => $service,
+            'data' => $sendServiceData
+        );
+    }
+    else{
+        return array(
+            'Test 3 (callService):' => $callServiceTestResult,
+        );
+    }
 }
 
 ?>
