25e8813e8688172682e2ca29cbe60430d1b0763f
b21matfi <b21matfi@student.his.se>
2023-04-27 14:45:06 +0200

    Update diagram.js
    changed x again

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -8954,7 +8954,7 @@ function drawElement(element, ghosted = false)
                     <svg width="100%" height="100%">
                     <rect width="${boxw}px" height="${boxh}px" fill="none" fill-opacity="0" stroke="#000" stroke-width="2" rx="20"/>
                     <rect width="${boxw/2}px" height="${boxh/6}px" fill="#FFF" fill-opacity="1" stroke="#000" stroke-width="2" />   
-                        <text x='${60 * zoomfact}px' y='${40 * zoomfact}px' dominant-baseline='middle' text-anchor='${vAlignment}' font-size="${boxh/30}px">${element.name}</text>
+                        <text x='${80 * zoomfact}px' y='${40 * zoomfact}px' dominant-baseline='middle' text-anchor='${vAlignment}' font-size="${boxh/30}px">${element.name}</text>
                     </svg>
                 </div>`;
     }
