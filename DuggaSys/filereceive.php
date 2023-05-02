77baa67057ab97a34209ce32b456c3c8a2fda1ff
e21krida <e21krida@student.his.se>
2023-05-02 14:44:48 +0200

    debugging

--- DuggaSys/filereceive.php
+++ DuggaSys/filereceive.php
@@ -436,5 +436,5 @@ logServiceEvent($log_uuid, EventTypes::ServiceServerEnd, "filereceive.php", $use
 if (!$error) {
     echo "<meta http-equiv='refresh' content='0;URL=fileed.php?courseid=" . $cid . "&coursevers=" . $vers . "' />";  //update page, redirect to "fileed.php" with the variables sent for course id and version id
 }*/
-echo "<meta http-equiv='refresh' content='0;URL=fileed.php?courseid=" . $cid . "&coursevers=" . $vers . "' />";  //update page, redirect to "fileed.php" with the variables sent for course id and version id;
+// echo "<meta http-equiv='refresh' content='0;URL=fileed.php?courseid=" . $cid . "&coursevers=" . $vers . "' />";  //update page, redirect to "fileed.php" with the variables sent for course id and version id;
 ?>
