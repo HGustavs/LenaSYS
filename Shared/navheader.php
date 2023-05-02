1c4657e365aaaaee275cd384d48494802d89b599
c21rebja <c21rebja@student.his.se>
2023-05-02 14:33:00 +0200

    added a new function in sectioned.js
    to create a connection between getlatestCommit.php and navheader.php
    
    Co-Authored-By: g21emmka <102578908+g21emmka@users.noreply.github.com>

--- Shared/navheader.php
+++ Shared/navheader.php
@@ -11,7 +11,6 @@
 
 			echo "<table class='navheader' id='navheader'><tr id='navbar'>";
 			include_once "../Shared/basic.php";
-			require_once "../recursivetesting/getLatestCommit.php"; // TODO: Change this when changing name of the file!
 
 			// As we always include the navheader - we can add the code that saves the current course ID to the session here.
 			if (isset($_GET['courseid']))
