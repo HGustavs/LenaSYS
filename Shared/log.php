aeebd0047bf95b686a5675fd7b7eca491e390e75
Pontus Kronenberg <a21ponkr@student.his.se>
2023-05-03 19:36:06 +0000

    #13494
    Complete code

--- Shared/log.php
+++ Shared/log.php
@@ -1,10 +1,42 @@
 <html>
 <head>
 
+<?php
+/********************************************************************************
+
+   Documentation 
+   
+*********************************************************************************
+
+ Presents the data housed in the loglena database tables, in a readable fashion.
+
+-------------==============######## Documentation End ###########==============-------------
+*/?>
+
     <style>
         table, th, td {
         border:1px solid black;
         border-collapse: collapse;
+    }
+
+    #set_to_default_button{
+        display: block;
+        margin-top: 2vh;
+        margin-bottom: 2vh;
+    }
+    th{
+        background-color: #614875;
+    }
+    th a{
+        color: white;
+        text-decoration: none;
+    }
+    table tr:nth-child(even){
+        background-color: #f2f2f2;
+    }
+
+    table tr:hover {
+        background-color: #ddd;
     }
     </style>
 </head>
@@ -17,6 +49,11 @@
 	            throw $e;
             }
         ?>
+
+        <!----------------------------------------------------------------------------------->  
+        <!------Creates a dropdown with all tables in the loglena database------------------->
+        <!----------------------------------------------------------------------------------->
+        
         <input type="text" id="searchInput" placeholder="Search...">
         <button type="button" onclick="searchTable()">Search</button>
 
@@ -36,6 +73,147 @@
                     echo"<p>".$row['name']."</p>";
                 }
             echo '</select>';
+
+//---------------------------------------------------------------------------------------------------
+// Present data  <-- Presents the information from each db table 
+//---------------------------------------------------------------------------------------------------
+
+
+            // Gathers information from database table logEntries
+            if((isset($_POST['name'])) && ($_POST['name']=='logEntries')){
+                $logEntriesSql = $log_db->query('SELECT * FROM logEntries');
+                $logEntriesResults = $logEntriesSql->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table border='1'>
+                    <tr>
+                        <th><a href="#" onclick="sortTable(0)">id</a></th>
+                        <th><a href="#" onclick="sortTable(1)">eventype</a></th>
+                        <th><a href="#" onclick="sortTable(2)">description</a></th>
+                        <th><a href="#" onclick="sortTable(3)">userAgent</a></th>
+                        <th><a href="#" onclick="sortTable(4)">timestamp</a></th>
+                    </tr>
+                <?php
+                foreach($logEntriesResults as $rows) {
+                    echo"<tr>";
+                    foreach($rows as $row) {
+                        if ($rows['timestamp'] == $row) {
+                            // Convert timestamp to a date format
+                            $timestamp = date('Y-m-d H:i:s', $row/1000);
+                            echo"<td>".$timestamp."</td>";
+                        } else {
+                            echo"<td>".$row."</td>";
+                        }
+                    }
+                    echo"</tr>";
+                }
+                echo"
+                </table>
+                ";
+            }
+            
+            // Gathers information from database table exampleLoadLogEntries
+            if((isset($_POST['name'])) && ($_POST['name']=='exampleLoadLogEntries')){
+                $exampleLoadLogEntriesSql = $log_db->query('SELECT * FROM exampleLoadLogEntries');
+                $exampleLoadLogEntriesResults = $exampleLoadLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table border='1'>
+                    <tr>
+                        <th><a href="#" onclick="sortTable(0)">id</a></th>
+                        <th><a href="#" onclick="sortTable(1)">type</a></th>
+                        <th><a href="#" onclick="sortTable(2)">courseid</a></th>
+                        <th><a href="#" onclick="sortTable(3)">uid</a></th>
+                        <th><a href="#" onclick="sortTable(4)">username</a></th>
+                        <th><a href="#" onclick="sortTable(5)">exampleid</a></th>
+                        <th><a href="#" onclick="sortTable(6)">timestamp</a></th>
+                    </tr>
+                <?php
+                foreach($exampleLoadLogEntriesResults as $rows) {
+                    echo"<tr>";
+                    foreach($rows as $row) {
+                        if ($rows['timestamp'] == $row) {
+                            // Convert timestamp to a date format
+                            $timestamp = date('Y-m-d H:i:s', $row/1000);
+                            echo"<td>".$timestamp."</td>";
+                        } else {
+                            echo"<td>".$row."</td>";
+                        }
+                    }
+                    echo"</tr>";
+                }
+                echo"
+                </table>
+                ";
+            }
+
+            // Gathers information from database table userHistory
+            if((isset($_POST['name'])) && ($_POST['name']=='userHistory')){
+
+                $userHistorySql = $log_db->query('SELECT * FROM userHistory');
+                $userHistoryResults = $userHistorySql->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table border='1'>
+                    <tr>
+                        <th><a href="#" onclick="sortTable(0)">refer</a></th>
+                        <th><a href="#" onclick="sortTable(1)">userid</a></th>
+                        <th><a href="#" onclick="sortTable(2)">username</a></th>
+                        <th><a href="#" onclick="sortTable(3)">IP</a></th>
+                        <th><a href="#" onclick="sortTable(4)">URLParams</a></th>
+                        <th><a href="#" onclick="sortTable(5)">timestamp</a></th>
+                    </tr>
+                <?php
+                foreach($userHistoryResults as $rows) {
+                    echo"<tr>";
+                    foreach($rows as $row) {
+                        if ($rows['timestamp'] == $row) {
+                            // Convert timestamp to a date format
+                            $timestamp = date('Y-m-d H:i:s', $row/1000);
+                            echo"<td>".$timestamp."</td>";
+                        } else {
+                            echo"<td>".$row."</td>";
+                        }
+                    }
+                    echo"</tr>";
+                }
+                echo"
+                </table>
+                ";
+            }
+
+            // Gathers information from database table userLogEntries
+            if((isset($_POST['name'])) && ($_POST['name']=='userLogEntries')){
+
+                $userLogEntriesSql = $log_db->query('SELECT * FROM userLogEntries');
+                $userLogEntriesResults = $userLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table border='1'>
+                    <tr>
+                        <th><a href="#" onclick="sortTable(0)">id</a></th>
+                        <th><a href="#" onclick="sortTable(1)">uid</a></th>
+                        <th><a href="#" onclick="sortTable(2)">username</a></th>
+                        <th><a href="#" onclick="sortTable(3)">eventType</a></th>
+                        <th><a href="#" onclick="sortTable(4)">description</a></th>
+                        <th><a href="#" onclick="sortTable(5)">timestamp</a></th>
+                        <th><a href="#" onclick="sortTable(6)">userAgent</a></th>
+                        <th><a href="#" onclick="sortTable(7)">remoteAddress</a></th>
+                    </tr>
+                <?php
+                foreach($userLogEntriesResults as $rows) {
+                    echo"<tr>";
+                    foreach($rows as $row) {
+                        if ($rows['timestamp'] == $row) {
+                            // Convert timestamp to a date format
+                            $timestamp = date('Y-m-d H:i:s', $row/1000);
+                            echo"<td>".$timestamp."</td>";
+                        } else {
+                            echo"<td>".$row."</td>";
+                        }
+                    }
+                    echo"</tr>";
+                }
+                echo"
+                </table>
+                ";
+            }
 
             // Gathers information from database table serviceLogEntries
             if((isset($_POST['name'])) && ($_POST['name']=='serviceLogEntries')){
@@ -61,6 +239,42 @@
 
                 <?php
                 foreach($serviceLogEntriesResults as $rows) {
+                    echo"<tr>";
+                    foreach($rows as $row) {
+                        if ($rows['timestamp'] == $row) {
+                            // Convert timestamp to a date format
+                            $timestamp = date('Y-m-d H:i:s', $row/1000);
+                            echo"<td>".$timestamp."</td>";
+                        } else {
+                            echo"<td>".$row."</td>";
+                        }
+                    }
+                    echo"</tr>";
+                }
+                echo"
+                </table>
+                ";
+            }
+
+
+            // Gathers information from database table duggaLoadLogEntries
+            if((isset($_POST['name'])) && ($_POST['name']=='duggaLoadLogEntries')){
+                $duggaLoadLogEntriesSql = $log_db->query('SELECT * FROM duggaLoadLogEntries');
+                $duggaLoadLogEntriesResults = $duggaLoadLogEntriesSql->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table border='1'>
+                    <tr>
+                        <th><a href="#" onclick="sortTable(0)">id</a></th>
+                        <th><a href="#" onclick="sortTable(1)">type</a></th>
+                        <th><a href="#" onclick="sortTable(2)">cid</a></th>
+                        <th><a href="#" onclick="sortTable(3)">uid</a></th>
+                        <th><a href="#" onclick="sortTable(4)">username</a></th>
+                        <th><a href="#" onclick="sortTable(5)">vers</a></th>
+                        <th><a href="#" onclick="sortTable(6)">quizid</a></th>
+                        <th><a href="#" onclick="sortTable(7)">timestamp</a></th>
+                    </tr>
+                <?php
+                foreach($duggaLoadLogEntriesResults as $rows) {
                     echo"<tr>";
                     foreach($rows as $row) {
                         if ($rows['timestamp'] == $row) {
