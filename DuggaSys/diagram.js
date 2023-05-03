ddee0d93275daf5eace8d6c07bcbe4f429e525fe
a21aldsa <a21aldsa@student.his.se>
2023-05-03 14:51:35 +0200

    working on svg for stickman

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9374,7 +9374,7 @@ function drawElement(element, ghosted = false)
         //svg for stickman.
         //str += `<rect width="${boxw}px" height="${boxh}px" fill='${element.fill}' stroke='${element.stroke}' stroke-width="2" />`;
         //str += `<path class="text" d="M${(boxw/2)+linew},${(linew)}"/>`;
-        str += `<circle cx="${(boxw/2)+linew}" cy="${(linew)}" r="${boxw/8}px" fill="red" stroke='${element.stroke}' stroke-width="2"/>`;
+        str += `<circle cx="${(boxw/2)+linew}" cy="${(boxw/8)+linew}" r="${boxw/8}px" fill="red" stroke='${element.stroke}' stroke-width="2"/>`;
         str += `</svg>`;
         str += `</div>`;
         
