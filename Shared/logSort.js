a40a898fb10d58681faf778676aa17cce27cf1ec
Pontus Kronenberg <a21ponkr@student.his.se>
2023-05-03 19:19:27 +0000

    #13494
    parseInt() to convert the string values of the cells to numbers before comparing them. If it is not the id-column, we use the same comparison as before.

--- Shared/logSort.js
+++ Shared/logSort.js
@@ -1,28 +1,55 @@
 function sortTable(columnIndex) {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
+    var table, rows, switching, i, x, y, shouldSwitch, ascending;
+  
+    table = document.getElementsByTagName("table")[0];
     switching = true;
+  
+    // Determine the new sorting direction
+    if (columnIndex === currentSortColumnIndex) {
+      if (currentSortAscending) {
+        ascending = false;
+      } else {
+        ascending = true;
+      }
+      currentSortAscending = ascending;
+    } else {
+      ascending = true;
+      currentSortAscending = true;
+      currentSortColumnIndex = columnIndex;
+    }
+  
+    // Remove the sorting classes from all columns if table is not sorted
+    if (currentSortColumnIndex === -1) {
+      for (i = 0; i < table.rows[0].cells.length; i++) {
+        table.rows[0].cells[i].classList.remove("sorted-ascending");
+        table.rows[0].cells[i].classList.remove("sorted-descending");
+      }
+    }
+  
+    // Set the sorting class for the clicked column
+    if (currentSortColumnIndex !== -1) {
+      table.rows[0].cells[columnIndex].classList.add(ascending ? "sorted-ascending" : "sorted-descending");
+    }
+  
+    // Loop through all rows except the first one (header)
     while (switching) {
       switching = false;
       rows = table.rows;
       for (i = 1; i < rows.length - 1; i++) {
         shouldSwitch = false;
-        x = rows[i].getElementsByTagName("TD")[columnIndex];
-        y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
-        if (columnIndex === currentSortColumnIndex) {
-          if (currentSortOrder === "asc") {
-            if (+x.innerHTML.toLowerCase() < +y.innerHTML.toLowerCase()) {
-              shouldSwitch = true;
-              break;
-            }
-          } else if (currentSortOrder === "desc") {
-            if (+x.innerHTML.toLowerCase() > +y.innerHTML.toLowerCase()) {
-              shouldSwitch = true;
-              break;
-            }
+  
+        // Get the values of the current and next row for the clicked column
+        x = rows[i].getElementsByTagName("td")[columnIndex];
+        y = rows[i + 1].getElementsByTagName("td")[columnIndex];
+  
+        // Compare the values and determine if they should be switched
+        if (columnIndex === 0) { // special case for id-column
+          if (ascending ? parseInt(x.innerHTML) > parseInt(y.innerHTML) : parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
           }
-        } else {
-          if (+x.innerHTML.toLowerCase() > +y.innerHTML.toLowerCase()) {
+        } else { // normal comparison for other columns
+          if (ascending ? x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() : x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
             shouldSwitch = true;
             break;
           }
@@ -33,11 +60,4 @@ function sortTable(columnIndex) {
         switching = true;
       }
     }
-    if (currentSortOrder === "asc") {
-      currentSortOrder = "desc";
-    } else {
-      currentSortOrder = "asc";
-    }
-    currentSortColumnIndex = columnIndex;
-  }
-  
+}
