825455ca3a7a3b0918f0b6dcf7dd9b9b9b82458d
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-03 15:09:37 +0200

    added a short description for the other diagram types while one type is already selected.

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -287,25 +287,30 @@
                             <div class="ERButton placementTypeBoxIcons" onclick='togglePlacementType(0,0); setElementPlacementType(0); setMouseMode(2);'>
                                 <img src="../Shared/icons/diagram_entity.svg" alt="ER entity"/>
                                 <span class="placementTypeToolTipText"><b>ER entity</b><br>
-                                    <p>Change to ER entity is selected</p>
+                                    <p>Change to ER entity</p>
+                                    <p>Each entity represents an object which is a representation of concepts or data.</p>
+                                    <p>The entity only holds the name of the object and if it depends on another object.</p>
                                 </span>
                             </div>
                             <div class="UMLButton placementTypeBoxIcons" onclick='togglePlacementType(4,0); setElementPlacementType(4); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_UML_entity.svg" alt="UML class"/>
                                 <span class="placementTypeToolTipText"><b>UML class</b><br>
-                                    <p>Change to UML class is selected</p>
+                                    <p>Change to UML class</p>
+                                    <p>Each class entity represents its own class along with the attributes and operations held within the class.</p>
                                 </span>
                             </div>
                             <div class="IEButton placementTypeBoxIcons" onclick='togglePlacementType(6,0); setElementPlacementType(6); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_IE_entity.svg" alt="IE entity"/>
                                 <span class="placementTypeToolTipText"><b>IE entity</b><br>
-                                    <p>Change to IE entity is selected </p>
+                                    <p>Change to IE entity</p>
+                                    <p>Each entity represents an object along with its attributes.</p>
+                                    <p>Each entity is represented by a table with a field that shows attributes.</p>
                                 </span>
                             </div>
                             <div class="SDButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(8,0); setElementPlacementType(8); setMouseMode(2);' > <!-- Dummy button, functions like IE-button -->
                                 <img class="SDState-rounded" src="../Shared/icons/diagram_state.svg" alt="Statediagram state"/>
                                 <span class="placementTypeToolTipText"><b>State diagram state</b><br>
-                                  <!--  <p>Change to state diagram state</p> -->
+                                    <p>Change to state diagram state</p>
                                     <p>A state diagram state is a representation of a status a process can have.</p>
                                     <p>Each state represents a unique status that a process can have.</p>
                                 </span>
