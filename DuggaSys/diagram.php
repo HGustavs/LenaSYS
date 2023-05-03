178985e6a785058dac767bd2a38d454426ae562a
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-03 15:17:11 +0200

    added er description when it appears on the left menu

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -137,7 +137,10 @@
                          onmousedown="holdPlacementButtonDown(0)"><!--<-- UML functionality -->
                         <img src="../Shared/icons/diagram_entity.svg" alt="ER entity"/>
                         <span class="toolTipText"><b>ER entity</b><br>
-                            <p>Add an ER entity to the diagram</p><br>
+                            <p>Add an ER entity to the diagram</p>
+                            <p>Each entity represents an object which is a representation of concepts or data.</p>
+                            <p>The entity only holds the name of the object and if it depends on another object.</p>
+                                    <br>
                             <p id="tooltip-PLACE_ENTITY" class="key_tooltip">Keybinding:</p>
                         </span>
                         <div id="togglePlacementTypeButton0" class="placementTypeIcon togglePlacementTypeButton">
@@ -149,7 +152,6 @@
                             <div class="ERButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(0,0); setElementPlacementType(0); setMouseMode(2);'>
                                 <img src="../Shared/icons/diagram_entity.svg" alt="ER entity"/>
                                 <span class="placementTypeToolTipText"><b>ER entity</b><br>
-                                   <!-- <p>Change to ER entity</p>-->
                                     <p>Each entity represents an object which is a representation of concepts or data.</p>
                                     <p>The entity only holds the name of the object and if it depends on another object.</p>
                                 </span>
@@ -207,8 +209,7 @@
                             </div>
                             <div class="UMLButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(4,0); setElementPlacementType(4); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_UML_entity.svg" alt="UML class"/>
-                                <span class="placementTypeToolTipText"><b>UML class</b><br>
-                                    <!--<p>Change to UML class</p>-->
+                                <span class="placementTypeToolTipText"><b>UML class</b><br>                      
                                     <p>Each class entity represents its own class along with the attributes and operations held within the class.</p>
                                 </span>
                             </div>
@@ -266,7 +267,6 @@
                             <div class="IEButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(6,0); setElementPlacementType(6); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_IE_entity.svg" alt="IE entity"/>
                                 <span class="placementTypeToolTipText"><b>IE entity</b><br>
-                                   <!-- <p>Change to IE entity</p> -->
                                     <p>Each entity represents an object along with its attributes.</p>
                                     <p>Each entity is represented by a table with a field that shows attributes.</p>
                                 </span>
@@ -325,7 +325,6 @@
                             <div class="SDButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(8,0); setElementPlacementType(8); setMouseMode(2);' > <!-- Dummy button, functions like IE-button -->
                                 <img class="SDState-rounded" src="../Shared/icons/diagram_state.svg" alt="Statediagram state"/>
                                 <span class="placementTypeToolTipText"><b>State diagram state</b><br>
-                                   <!-- <p>Change to state diagram state</p> -->
                                     <p>A state diagram state is a representation of a status a process can have.</p>
                                     <p>Each state represents a unique status that a process can have.</p>
                                 </span>
