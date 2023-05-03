a8b3748cfea720bb96f2762f9003ad7e1cd88c5a
Pontus Kronenberg <a21ponkr@student.his.se>
2023-05-03 19:14:25 +0000

    #13494
    using numbers instead of strings to sort? maybe good solution?? idk

--- Shared/logSort.js
+++ Shared/logSort.js
@@ -1,63 +1,43 @@
-var currentSortColumnIndex = -1;
-var currentSortAscending = true;
-
-
 function sortTable(columnIndex) {
-    var table, rows, switching, i, x, y, shouldSwitch, ascending;
-  
-    table = document.getElementsByTagName("table")[0];
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
     switching = true;
-  
-    // Determine the new sorting direction
-    if (columnIndex === currentSortColumnIndex) {
-      if (currentSortAscending) {
-        ascending = false;
-      } else {
-        ascending = true;
-      }
-      currentSortAscending = ascending;
-    } else {
-      ascending = true;
-      currentSortAscending = true;
-      currentSortColumnIndex = columnIndex;
-    }
-  
-    // Remove the sorting classes from all columns if table is not sorted
-    if (currentSortColumnIndex === -1) {
-      for (i = 0; i < table.rows[0].cells.length; i++) {
-        table.rows[0].cells[i].classList.remove("sorted-ascending");
-        table.rows[0].cells[i].classList.remove("sorted-descending");
-      }
-    }
-  
-    // Set the sorting class for the clicked column
-    if (currentSortColumnIndex !== -1) {
-      table.rows[0].cells[columnIndex].classList.add(ascending ? "sorted-ascending" : "sorted-descending");
-    }
-  
-    // Loop through all rows except the first one (header)
     while (switching) {
       switching = false;
       rows = table.rows;
       for (i = 1; i < rows.length - 1; i++) {
         shouldSwitch = false;
-  
-        // Get the values of the current and next row for the clicked column
-        x = rows[i].getElementsByTagName("td")[columnIndex];
-        y = rows[i + 1].getElementsByTagName("td")[columnIndex];
-  
-        // Compare the values and determine if they should be switched
-        if (ascending ? x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() : x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
+        x = rows[i].getElementsByTagName("TD")[columnIndex];
+        y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+        if (columnIndex === currentSortColumnIndex) {
+          if (currentSortOrder === "asc") {
+            if (+x.innerHTML.toLowerCase() < +y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          } else if (currentSortOrder === "desc") {
+            if (+x.innerHTML.toLowerCase() > +y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          }
+        } else {
+          if (+x.innerHTML.toLowerCase() > +y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
         }
       }
-  
       if (shouldSwitch) {
-        // Switch the rows and set the switching flag to true
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
       }
     }
+    if (currentSortOrder === "asc") {
+      currentSortOrder = "desc";
+    } else {
+      currentSortOrder = "asc";
+    }
+    currentSortColumnIndex = columnIndex;
   }
   
