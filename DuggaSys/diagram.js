d940e95581c51931a6235b35ea4fc9d20a98f682
a21aldsa <a21aldsa@student.his.se>
2023-05-03 15:47:13 +0200

    tweaking svg for stickman

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9382,8 +9382,8 @@ function drawElement(element, ghosted = false)
                 h${boxw/2}
                 m-${(boxw/4)},0
                 v${boxh/3}
-                l${boxw/4},${boxh-(boxh/8)}
-                m${(boxw/4)*-1},${(boxh-(boxh/8))*-1}
+                l${boxw/2},${boxh-(boxh/8)}
+                m${(boxw/2)*-1},${(boxh-(boxh/8))*-1}
                 l${(boxw/2)*-1},${boxh-(boxh/8)}
             "
             stroke-width='${linew}'
