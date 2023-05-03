31eb937ae6dee6264d0d6b327b02f989a23320a2
a21aldsa <a21aldsa@student.his.se>
2023-05-03 16:42:11 +0200

    tweaking lifeline svg

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9376,7 +9376,7 @@ function drawElement(element, ghosted = false)
         "
         stroke-width='${linew}'
         stroke='${element.stroke}'
-        stroke-dasharray='${linew},${linew}'
+        stroke-dasharray='${linew*5},${linew*5}'
         fill='transparent'
         />`;
         //str += `</svg>`;
