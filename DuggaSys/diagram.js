b0e4c684081cae83280e28317f3414b0c558cfa2
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 16:22:03 +0200

    changed back thing to how they were

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6831,42 +6831,6 @@ function generateContextProperties()
                     str += `<option value='${SDLineIcons[icon]}'>${SDLineIcons[icon]}</option>`;
                 }
             });
-            str += `<label style="display: block">Line Type:</label> <select id='lineType' onchange="changeLineProperties()">`;
-            str += `<option value=''>None</option>`;
-            Object.keys(SDLineType).forEach(type => {
-                if (contextLine[0].innerType != undefined) {
-                    //If the lines in context happen to be matching something in the drop down, it is set as selected.
-                    if (contextLine[0].innerType == type) {
-                        str += `<option value='${SDLineType[type]}' selected>${SDLineType[type]}</option>`;
-                    }
-                    //else, its not matching and the option is just added to the dropdown normally.
-                    else {
-                        str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
-                    }
-                }
-                else {
-                    str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
-                }
-            });
-            str += `<label style="display: block">Icons:</label> <select id='lineType' onchange="changeLineProperties()">`;
-            str += `<option value=''>None</option>`;
-            //iterate through all the icons assicoated with SD, and add them to the drop down as options
-            Object.keys(SDLineType).forEach(type => {
-                if (contextLine[0].innerType != undefined) {
-                    //If the lines in context happen to be matching something in the drop down, it is set as selected.
-                    if (contextLine[0].innerType == type) {
-                        str += `<option value='${SDLineType[type]}' selected>${SDLineType[type]}</option>`;
-                    }
-                    //else, its not matching and the option is just added to the dropdown normally.
-                    else {
-                        str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
-                    }
-                }
-                else {
-                    str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
-                }
-            });
-            /*
             str += `</select>`;
             str += `<label style="display: block">Line Type:</label><select id='lineType' onchange='changeLineProperties()'>`;
             Object.keys(SDLineType).forEach(type => {
@@ -6879,8 +6843,7 @@ function generateContextProperties()
                     str += `<option value='${SDLineType[type]}' >${SDLineType[type]}</option>`;
                 }
             });
-            str += `</select>`;
-            */
+            str += `</select>`; 
         }
         str+=`<br><br><input type="submit" class='saveButton' value="Save" onclick="changeLineProperties();displayMessage(messageTypes.SUCCESS, 'Successfully saved')">`;
       }
