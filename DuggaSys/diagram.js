374c6459f267089ca1e3d971b8422635d1fc9be2
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 16:11:50 +0200

    tried remaking the dropdown for line type

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6833,19 +6833,19 @@ function generateContextProperties()
             });
             str += `<label style="display: block">Line Type:</label> <select id='lineType' onchange="changeLineProperties()">`;
             str += `<option value=''>None</option>`;
-            Object.keys(SDLineIcons).forEach(icon => {
-                if (contextLine[0].endIcon != undefined) {
+            Object.keys(SDLineType).forEach(type => {
+                if (contextLine[0].innerType != undefined) {
                     //If the lines in context happen to be matching something in the drop down, it is set as selected.
-                    if (contextLine[0].endIcon == icon) {
-                        str += `<option value='${SDLineIcons[icon]}' selected>${SDLineIcons[icon]}</option>`;
+                    if (contextLine[0].innerType == type) {
+                        str += `<option value='${SDLineType[type]}' selected>${SDLineType[type]}</option>`;
                     }
                     //else, its not matching and the option is just added to the dropdown normally.
                     else {
-                        str += `<option value='${SDLineIcons[icon]}'>${SDLineIcons[icon]}</option>`;
+                        str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
                     }
                 }
                 else {
-                    str += `<option value='${SDLineIcons[icon]}'>${SDLineIcons[icon]}</option>`;
+                    str += `<option value='${SDLineType[type]}'>${SDLineType[type]}</option>`;
                 }
             });
             /*
