ad17385276d89dff6b74538eb9509aff92397f1a
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 14:31:05 +0200

    added a check that should change line type based on the option menu

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -7676,9 +7676,13 @@ function preProcessLine(line) {
         }
 
 
-        
-        
-        if (isClose(felem.cx, telem.cx, felem.cy, telem.cy, zoomfact)) {
+        if (line.innerType === 'Segment') { 
+            line.innerType = SDLineType.SEGMENT;
+        }
+        else if (line.innerType === 'Straight') {
+            line.innerType = SDLineType.STRAIGHT;
+        }
+        else if (isClose(felem.cx, telem.cx, felem.cy, telem.cy, zoomfact)) {
             line.innerType = SDLineType.STRAIGHT;
         }
         else {
