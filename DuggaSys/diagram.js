c34bc0f5f4137adbea0f77facc0830643970ff04
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 10:23:09 +0200

    removed the diagramToScreenPosition and removed the comment about it

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -3293,7 +3293,6 @@ function clearContextLine()
  * @param {Number} mouseX Pixel position in the x-axis.
  * @param {Number} mouseY Pixel position in the y-axis.
  * @returns {Point} Point containing the calculated coordinates.
- * @see diagramToScreenPosition() For converting the other way.
  */
 function screenToDiagramCoordinates(mouseX,mouseY)
 {
@@ -3319,24 +3318,6 @@ function screenToDiagramCoordinates(mouseX,mouseY)
                     Math.round(((mouseY - 0) / zoomfact - scrolly) + zoomX * scrolly + zoomOrigo.y)
     );
 }
-
-/**
- * @description Converts a coordinate on the canvas into a pixel position on the screen.
- * @param {Number} coordX Coordinate position in the x-axis.
- * @param {Number} coordY Coordinate position in the y-axis.
- * @returns {Point} Point containing the calculated screen position.
- * @depricated TODO : Needs to be updated
- * @see screenToDiagramCoordinates() For converting the other way.
- */
-/** 
-function diagramToScreenPosition(coordX, coordY)
-{
-    console.warn("diagramToScreenPosition() is depricated. It should be updated to use new screenToDiagramCoordinates() algorithm reversed.");
-    return new Point(
-        Math.round((coordX + scrollx) / zoomfact + 0),
-        Math.round((coordY + scrolly) / zoomfact + 0)
-    );
-}*/
 
 /**
  * @description Test weither an enum object contains a certain property value.
