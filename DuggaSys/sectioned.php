e557a7cdb5675c13defbc9eec4180e175edcdd9c
a21rammo <a21rammo@outlook.com>
2023-05-04 16:12:18 +0200

    added button for github moment

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -605,6 +605,8 @@
 			</select></div>
 			<div class='inputwrapper'><span>Filepath:</span><input class='textinput' type='text' id='hash' placeholder='no' value=''/></div>
 			<div class='inputwrapper'><span>Order of items:</span><input class='textinput' type='text' id='hash' placeholder='nope' value=''/></div>
+			<input type='button' class='submit-button' onclick="" value='Save'>
+
 		</div>
 	</div>
 
