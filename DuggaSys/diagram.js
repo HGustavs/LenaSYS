d1d96179dc9145a1deacabc04b7aa27e52b6ebf2
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 14:39:13 +0200

    test

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -7676,12 +7676,14 @@ function preProcessLine(line) {
         }
 
 
-       
-        if (isClose(felem.cx, telem.cx, felem.cy, telem.cy, zoomfact)) {
+        if (line.innerType === 'Segment') {
             line.innerType = SDLineType.SEGMENT;
         }
-        else {
+        else if (isClose(felem.cx, telem.cx, felem.cy, telem.cy, zoomfact)) {
             line.innerType = SDLineType.STRAIGHT;
+        }
+        else {
+            line.innerType = SDLineType.SEGMENT;
         }
         
         
