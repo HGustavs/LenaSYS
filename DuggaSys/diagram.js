b3dfb7059fe993f7adad2e4d0d4d2ed66f533225
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-04 16:37:59 +0200

    changed lineType to innerType

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -3025,7 +3025,7 @@ function changeLineProperties()
         if (line.innerType != lineType.value) {
             lineType.value = lineType.value.trim();
             line.innerType = lineType.value
-            stateMachine.save(StateChangeFactory.ElementAttributesChanged(contextLine[0].id, { lineType: lineType.value }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
+            stateMachine.save(StateChangeFactory.ElementAttributesChanged(contextLine[0].id, { innerType: lineType.value }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
         }
         // Start label, near side
         if (line.startLabel != startLabel.value) {
