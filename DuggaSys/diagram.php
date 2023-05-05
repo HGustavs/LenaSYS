be11682b11334e001db82d625c3f6dc9468c11b9
b21oscpe <b21oscpe@student.his.se>
2023-05-05 16:05:30 +0200

    Update diagram.php

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -307,7 +307,7 @@
                     </div>
                     <div id="diagramPopOut">
                         <div id="togglePlacementTypeBox8" class="togglePlacementTypeBox togglePlacementTypeBoxEntity">
-                            <div class="ERButton placementTypeBoxIcons" onclick='togglePlacementType(0,0); setElementPlacementType(0); setMouseMode(2);'>
+                            <div class="ERButton placementTypeBoxIcons" onclick='togglePlacementType(0,1); setElementPlacementType(0); setMouseMode(2);'>
                                 <img src="../Shared/icons/diagram_entity.svg" alt="ER entity"/>
                                 <span class="placementTypeToolTipText"><b>ER entity</b><br>
                                     <p>Change to ER entity</p>
@@ -315,14 +315,14 @@
                                     <p>The entity only holds the name of the object and if it depends on another object.</p>
                                 </span>
                             </div>
-                            <div class="UMLButton placementTypeBoxIcons" onclick='togglePlacementType(4,0); setElementPlacementType(4); setMouseMode(2);' >
+                            <div class="UMLButton placementTypeBoxIcons" onclick='togglePlacementType(4,1); setElementPlacementType(4); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_UML_entity.svg" alt="UML class"/>
                                 <span class="placementTypeToolTipText"><b>UML class</b><br>
                                     <p>Change to UML class</p>
                                     <p>Each class entity represents its own class along with the attributes and operations held within the class.</p>
                                 </span>
                             </div>
-                            <div class="IEButton placementTypeBoxIcons" onclick='togglePlacementType(6,0); setElementPlacementType(6); setMouseMode(2);' >
+                            <div class="IEButton placementTypeBoxIcons" onclick='togglePlacementType(6,1); setElementPlacementType(6); setMouseMode(2);' >
                                 <img src="../Shared/icons/diagram_IE_entity.svg" alt="IE entity"/>
                                 <span class="placementTypeToolTipText"><b>IE entity</b><br>
                                     <p>Change to IE entity</p>
@@ -330,7 +330,7 @@
                                     <p>Each entity is represented by a table with a field that shows attributes.</p>
                                 </span>
                             </div>
-                            <div class="SDButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(8,0); setElementPlacementType(8); setMouseMode(2);' > <!-- Dummy button, functions like IE-button -->
+                            <div class="SDButton placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(8,1); setElementPlacementType(8); setMouseMode(2);' > <!-- Dummy button, functions like IE-button -->
                                 <img class="SDState-rounded" src="../Shared/icons/diagram_state.svg" alt="Statediagram state"/>
                                 <span class="placementTypeToolTipText"><b>State diagram state</b><br>
                                     <p>A state diagram state is a representation of a status a process can have.</p>
