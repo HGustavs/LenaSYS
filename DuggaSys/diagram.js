e861de4edb9604330add2df9766bb06449e0251e
b21oscpe <b21oscpe@student.his.se>
2023-05-05 14:59:06 +0200

    Update diagram.js

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -5992,15 +5992,7 @@ function togglePlacementType(num,type){
         document.getElementById("elementPlacement7").children.item(1).classList.remove("hiddenToolTiptext");
         document.getElementById("togglePlacementTypeButton7").classList.remove("activeTogglePlacementTypeButton");
         document.getElementById("togglePlacementTypeBox7").classList.remove("activeTogglePlacementTypeBox"); // IE inheritance end
-        
-        // SE start
-        document.getElementById("elementPlacement11").classList.add("hiddenPlacementType"); 
-        document.getElementById("elementPlacement11").children.item(1).classList.add("toolTipText");
-        document.getElementById("elementPlacement11").children.item(1).classList.remove("hiddenToolTiptext");
-        document.getElementById("togglePlacementTypeButton11").classList.remove("activeTogglePlacementTypeButton");
-        document.getElementById("togglePlacementTypeBox11").classList.remove("activeTogglePlacementTypeBox");
-        // SE end
-        
+
     }
     
     // Unhide the currently selected placement type
