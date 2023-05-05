37eff2eeddee15879ff7b3f401a18a48cca42227
b21matfi <b21matfi@student.his.se>
2023-05-05 13:10:01 +0200

    Update diagram.js
    added so it checks if its black theme

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9042,6 +9042,7 @@ function drawElement(element, ghosted = false)
     }
     else if (element.kind == 'UMLFinalState') {
         const ghostAttr = (ghosted) ? `pointer-events: none; opacity: ${ghostLine ? 0 : 0.0};` : "";
+        const theme = document.getElementById("themeBlack");
         str += `<div id="${element.id}" 
                      class="element uml-state"
                      style="width:${boxw}px;height:${boxh}px;${ghostAttr}"
@@ -9059,7 +9060,7 @@ function drawElement(element, ghosted = false)
                             </g>
                         </svg>
                 </div>`;
-                if(element.fill == `${"#000000"}`) {
+                if(element.fill == `${"#000000"}` && theme.href.includes('blackTheme')){
                     element.fill = `${"#FFFFFF"}`;
                 }
 
