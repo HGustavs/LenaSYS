de6a47c27120b3963889e53669df199742dac054
b21oscpe <b21oscpe@student.his.se>
2023-05-05 14:51:09 +0200

    Update diagram.js

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -5969,11 +5969,24 @@ function togglePlacementType(num,type){
         document.getElementById("elementPlacement8").children.item(1).classList.remove("hiddenToolTiptext");
         document.getElementById("togglePlacementTypeButton8").classList.remove("activeTogglePlacementTypeButton");
         document.getElementById("togglePlacementTypeBox8").classList.remove("activeTogglePlacementTypeBox");// SD state end
+        
         document.getElementById("elementPlacement11").classList.add("hiddenPlacementType");// SE start
         document.getElementById("elementPlacement11").children.item(1).classList.add("toolTipText");
         document.getElementById("elementPlacement11").children.item(1).classList.remove("hiddenToolTiptext");
         document.getElementById("togglePlacementTypeButton11").classList.remove("activeTogglePlacementTypeButton");
         document.getElementById("togglePlacementTypeBox11").classList.remove("activeTogglePlacementTypeBox");// SE end
+
+        document.getElementById("elementPlacement12").classList.add("hiddenPlacementType");// SE start
+        document.getElementById("elementPlacement12").children.item(1).classList.add("toolTipText");
+        document.getElementById("elementPlacement12").children.item(1).classList.remove("hiddenToolTiptext");
+        document.getElementById("togglePlacementTypeButton12").classList.remove("activeTogglePlacementTypeButton");
+        document.getElementById("togglePlacementTypeBox12").classList.remove("activeTogglePlacementTypeBox");// SE end
+
+        document.getElementById("elementPlacement13").classList.add("hiddenPlacementType");// SE start
+        document.getElementById("elementPlacement13").children.item(1).classList.add("toolTipText");
+        document.getElementById("elementPlacement13").children.item(1).classList.remove("hiddenToolTiptext");
+        document.getElementById("togglePlacementTypeButton13").classList.remove("activeTogglePlacementTypeButton");
+        document.getElementById("togglePlacementTypeBox13").classList.remove("activeTogglePlacementTypeBox");// SE end
     }
     else if(type==1){
         document.getElementById("elementPlacement1").classList.add("hiddenPlacementType");// ER relation start
