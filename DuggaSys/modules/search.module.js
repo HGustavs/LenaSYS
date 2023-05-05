a5e8a5c348a3199478013423ab7f53f2455c1b23
a21nibah <a21nibah@student.his.se>
2023-05-05 08:56:43 +0000

    added comment to search.js

--- DuggaSys/modules/search.module.js
+++ DuggaSys/modules/search.module.js
@@ -1,3 +1,8 @@
+/**
+ * @description returns the value of a query string sent in a URL.
+ * @param a key of type string.
+ * @returns a string containing the value 
+ */
 function search(key)
 {
     const parameters=new URLSearchParams(window.location.search);
