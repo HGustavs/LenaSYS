b19f803516d0fddf246810465420cf4f4f110080
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-05 13:58:01 +0200

    test

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -536,7 +536,7 @@
                         </div>
                     </div>
                     <div id="diagramPopOut">
-                        <div id="togglePlacementTypeBox9" class="togglePlacementTypeBox togglePlacementTypeBoxEntity"><!--<-- UML functionality start-->
+                        <div id="togglePlacementTypeBox10" class="togglePlacementTypeBox togglePlacementTypeBoxEntity"><!--<-- UML functionality start-->
                             <div class="placementTypeBoxIcons activePlacementType" onclick='togglePlacementType(9,9); setElementPlacementType(9); setMouseMode(2);'>
                                 <img src="../Shared/icons/diagram_UML_initial_state.svg" alt="UML initial state"/>
                                 <span class="placementTypeToolTipText"><b>UML initial state</b><br>
