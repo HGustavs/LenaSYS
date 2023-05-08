2174f2b166acb873a5088c92399a6c7012de4a7a
c21sebar <c21sebar@student.his.se>
2023-05-08 15:07:41 +0200

    update comment to issue

--- Shared/SQL/alterTables.sql
+++ Shared/SQL/alterTables.sql
@@ -63,4 +63,5 @@ ALTER TABLE userAnswer ADD COLUMN timesAccessed INT(5);
 /* Associated with issue #11113 */
 -- ALTER TABLE userAnswer ADD last_Time_techer_visited TIMESTAMP NULL DEFAULT NULL AFTER timesGraded;
 
+/* Associated with issue #13793 */
 ALTER TABLE box ADD COLUMN fileID INTEGER(11);
