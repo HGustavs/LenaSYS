e6773c6cfdb33de9dd69eece0037f18ba3259e9d
b21hamwe <b21hamwe@student.his.se>
2023-05-09 13:51:51 +0200

    Create motherTest.php
    creating an "base" for future test data

---
+++ backend-models/service-tests/motherTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+include_once ".../test1";
+include_once ".../test2";
+include_once ".../test3";
+include_once ".../test4";
+
+pdoConnect();
+*/
+
+echo "
+<table border='1'>
+<tr>
+    <th>test1</th>
+    <th>test2</th>
+    <th>test3</th>
+    <th>test4</th>
+    <th>test5</th>
+</tr>
+";
+
+echo "</table>";
+
+
+?>
