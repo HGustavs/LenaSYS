7c07feb20e4e5265bbae48ae0f0c8f826aacb484
b21hamwe <b21hamwe@student.his.se>
2023-05-09 14:00:07 +0200

    Update motherTest.php
    Added styling standard

--- backend-models/service-tests/motherTest.php
+++ backend-models/service-tests/motherTest.php
@@ -1,3 +1,33 @@
+<html>
+<head>
+<style>
+
+    
+        table, th, td {
+        border:1px solid black;
+        border-collapse: collapse;
+    }
+
+    /*#set_to_default_button{
+        display: block;
+        margin-top: 2vh;
+        margin-bottom: 2vh;
+    }*/
+    th{
+        background-color: #614875;
+    }
+    th a{
+        color: white;
+        text-decoration: none;
+    }
+    table tr:nth-child(even){
+        background-color: #f2f2f2;
+    }
+
+    table tr:hover {
+        background-color: #ddd;
+    }
+    </style>
 <?php
 
 /*
@@ -23,4 +53,6 @@ echo "
 echo "</table>";
 
 
-?>
+            ?>
+   </body>
+</html>
