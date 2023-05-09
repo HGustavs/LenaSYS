f75522ca640377668349911637d0d6805fc94c5a
a21aldsa <a21aldsa@student.his.se>
2023-05-09 14:57:19 +0200

    tweaking the rect

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9471,7 +9471,7 @@ function drawElement(element, ghosted = false)
                 stroke='${element.stroke}'
                 fill='transparent'
             />`;
-            str += `<rect class='text' x='${xAnchor-(textWidth/2)}' y='${boxw-(textheight/2)}' width='${textWidth}' height='${12*zoomfact}' fill='${element.fill}' />`;
+            str += `<rect class='text' x='${xAnchor-(textWidth/2)}' y='${boxw-(textheight/2)}' width='${textWidth}' height='${textheight}' fill='${element.fill}' />`;
             str += `<text class='text' x='${xAnchor}' y='${boxw}' dominant-baseline='middle' text-anchor='${vAlignment}' fill='${element.stroke}'>${element.name}</text>`;
             str += `</g>`;
         }
