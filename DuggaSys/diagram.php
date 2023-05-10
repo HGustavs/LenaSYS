3840323f6911cafd4d400d553fd724c769b43627
b21matfi <b21matfi@student.his.se>
2023-05-10 14:21:35 +0200

    Update diagram.php
    changed the ID again

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -571,7 +571,7 @@
                     </span>
                 </div>              
                 <!-- Sequence diagram object selection -->
-                <div id="elementPlacement13" class="diagramIcons toolbarMode" onclick='setElementPlacementType(2); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
+                <div id="elementPlacement14" class="diagramIcons toolbarMode" onclick='setElementPlacementType(2); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'>
                     <img src="../Shared/icons/diagram_sequence_object.svg" alt="Sequence Object"/>
                     <span class="toolTipText"><b>Sequence Object</b><br>
                         <p>Creates a sequence object.</p>
@@ -582,7 +582,7 @@
                         </span>
                 </div> 
                 <!-- Sequence activation selection -->
-                <div id="elementPlacement14" class="diagramIcons toolbarMode"onclick='setElementPlacementType(13); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'> 
+                <div id="elementPlacement13" class="diagramIcons toolbarMode"onclick='setElementPlacementType(13); setMouseMode(2);' onmouseup='holdPlacementButtonUp();'> 
                     <img src="../Shared/icons/diagram_activation.svg" alt="Sequence activation"/>
                     <span class="toolTipText"><b>Sequence activation</b><br>
                         <p>Creates an activation box.</p>
