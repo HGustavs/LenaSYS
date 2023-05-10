f738e9a41f292a0f26b95c5674316921c26f3475
b21kurar <b21kurar@student.his.se>
2023-05-10 18:50:17 +0200

    edit drop database

--- Shared/testingDB.php
+++ Shared/testingDB.php
@@ -25,17 +25,18 @@ if(!$query->execute()) {
 }
 else{
 	echo "<h3>Database exists already, resetting...</h3>";
+	
+	$query = $pdo->prepare('DROP DATABASE :dbname');
+	$query->bindParam(':dbname', $dbName);
+	if(!$query->execute()) {
+		$error = $query->errorInfo();
+		echo "<h4> Error dropping database: ".$error[2]."</h4>";
+	}
+	else{
+		echo "<h2>Database reset</h2>";
+	}
 }
 
-$query = $pdo->prepare('DROP DATABASE IF EXISTS :dbname');
-$query->bindParam(':dbname', $dbName);
-if(!$query->execute()) {
-	$error = $query->errorInfo();
-	echo "<h4> Error dropping database: ".$error[2]."</h4>";
-}
-else{
-	echo "<h2>Database reset</h2>";
-}
 
 $query = $pdo->prepare("CREATE DATABASE IF NOT EXISTS ".$dbName);
 
