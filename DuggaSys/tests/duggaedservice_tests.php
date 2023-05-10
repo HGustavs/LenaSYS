886eac26943f97059aee99fd732f80926d685a01
AckePe <c21axepe@student.his.se>
2023-05-10 13:23:00 +0200

    Added test data

---
+++ DuggaSys/tests/duggaedservice_tests.php
@@ -0,0 +1,40 @@
+<?php
+
+include "../Shared/test.php";
+
+$testsData = array(
+    'create course test' => array(
+        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
+        'service-data' => serialize(array( // Data that service needs to execute function
+            'opt' => 'NEW',
+            'cid' => '2',
+            'coursevers' => '97732',
+            'username' => 'toddler',
+            'password' => 'Kong'
+        )),
+        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+            'debug',
+            'readonly'
+        )),
+    ),
+    /*
+    'create course test 2' => array(
+        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
+        'service-data' => serialize(array( // Data that service needs to execute function
+            'opt' => 'NEW',
+            'username' => 'usr',
+            'password' => 'pass',
+            'coursecode' => 'IT466G',
+            'coursename' => 'TestCourseFromAPI5',
+            'uid' => '101'
+        )),
+        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+            'none'
+        )),
+    ),
+    */
+);
+
+testHandler($testsData, false); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
