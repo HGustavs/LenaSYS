80f4335b6b2b6078db20d6b8ded6ef5975613fca
b21hamwe <b21hamwe@student.his.se>
2023-05-10 16:33:48 +0200

    added json file
    added a json file,
    and now try to present and parse

--- backend-models/service-tests/motherTest.php
+++ backend-models/service-tests/motherTest.php
@@ -72,7 +72,9 @@ include_once ".../test4";
         <label for='sort_failed'>Failed tests</label>
         <?php    
             //$test = json_decode($_GET["name"]);
-
+            $motherTest_json = file_get_contents('motherTestJSON.json');
+            $decoded_json = json_decode($motherTest_json, false);
+            echo $decoded_json['uid'];
             
             echo 'Choose table: ';
             echo '<select onchange="this.form.submit()" name="name" >';
