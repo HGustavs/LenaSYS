6ebf1bd7b8e1670e16e14ab137814e44c5a587b1
AckePe <c21axepe@student.his.se>
2023-05-10 15:15:08 +0200

    Saving progress
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- Shared/duggaedservice_tests.php
+++ Shared/duggaedservice_tests.php
@@ -52,6 +52,21 @@ $testsData = array(
             'readonly'
             )),
     ),
+
+    'Delete an assignment' => array(
+        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
+        'service-data' => serialize(array( // Data that service needs to execute function
+            'opt' = 'DELDU',
+            'qid' = 'VALUE FROM QUERY',
+            'username' = 'toddler',
+            'password' = 'Kong'
+            )),
+            'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+            'debug',
+            'readonly'
+            )),
+    ), 
     /*
     'create course test 2' => array(
         'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
