a101c80bf49c1c7cbe4ac5b04824503abfbc7aba
a21aldsa <a21aldsa@student.his.se>
2023-05-11 10:54:02 +0200

    slightly simplifying svg for label

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9657,7 +9657,7 @@ function drawElement(element, ghosted = false)
         str += `<path 
             d="M${(linew+sequenceCornerRadius)+(boxw/15)},${linew}
                 v${boxh/10}
-                l${((boxw/40))*-1},${(boxh/10)/2}
+                l${((boxw/40))*-1},${(boxh/20)}
                 H${linew}
             "
             stroke-width='${linew}'
