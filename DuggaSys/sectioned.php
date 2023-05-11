bfb5c7eef1afbefbb1a55760b05a2356e2e51d44
e21krida <e21krida@student.his.se>
2023-05-11 16:09:38 +0200

    testing use of strstr
    Co-Authored-By: a21lukpo <102577654+a21lukpo@users.noreply.github.com>

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -580,7 +580,9 @@
 									echo "<script>console.log('test');</script>";
 									foreach ($dirs as $dir) {
 										$dirname = basename($dir);
-										echo "<option value='$dirname'>$dirname</option>";
+										if(strstr($dirname, 'Examples')) {
+											echo "<option value='$dirname'>$dirname</option>";
+										}
 									}											
 											/*
                         $cid = getOPG('courseid');
