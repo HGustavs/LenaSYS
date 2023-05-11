c22c5b1f6deedc858da489ab09fafaf84e3328dc
b21ricni <b21ricni@student.his.se>
2023-05-11 23:24:58 +0200

    Update codeviewer.js

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -1013,7 +1013,7 @@ function updateContent(file, content, boxnumber)
 				alert("Error when updating content: " + e.message);
 			}
 			console.log("Timeout");
-			setTimeout("location.reload()", 100000); //SETBACK TO 500
+			setTimeout("location.reload()", 500); //SETBACK TO 500
 		}
 	} else {
 		if (box[4] != document.querySelector("#boxtitle2").textContent) {
@@ -1097,7 +1097,7 @@ function createboxmenu(contentid, boxid, type, filepath, filename, filekind) {
 		if (retData['writeaccess'] == "w") {
 			if (type == "DOCUMENT") {
 				str += "<td class='butto2 editcontentbtn showdesktop codedropbutton' id='settings' title='Edit box settings' onclick='displayEditContent(" + boxid + ");' ><img src='../Shared/icons/general_settings_button.svg' /></td>";
-				str += '<td id = "boxtitlewrapper" class="butto2 boxtitlewrap" title="Title"><span id="boxtitle2" class="boxtitleEditable" onblur="updateContent( '+ filename +','+ contentid +',' + boxid + ');" contenteditable>' + retData['box'][boxid - 1][4] + '</span></td>';
+				str += '<td id = "boxtitlewrapper" class="butto2 boxtitlewrap" title="Title"><span id="boxtitle2" class="boxtitleEditable" onblur="updateContent( '+ filename +','+ filekind +',' + boxid + ');" contenteditable>' + retData['box'][boxid - 1][4] + '</span></td>';
 				str += `<div id='iframeBoxes'><td class='butto2 editbtn' onclick='showIframe("${filepath}", "${filename}", ${filekind});'><img title='Edit file' class='markdownIcon' src='../Shared/icons/newMarkdown.svg'></div>`;
 			} else if (type == "CODE") {
 				str += "<td class='butto2 editcontentbtn showdesktop codedropbutton' id='settings' title='Edit box settings' onclick='displayEditContent(" + boxid + ");' ><img src='../Shared/icons/general_settings_button.svg' /></td>";
