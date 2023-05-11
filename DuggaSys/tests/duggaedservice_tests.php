77c84291e60a5fa7050991965ec9f355613037d2
AckePe <c21axepe@student.his.se>
2023-05-11 11:13:13 +0200

    Added inputs and querys for test#1

--- DuggaSys/tests/duggaedservice_tests.php
+++ DuggaSys/tests/duggaedservice_tests.php
@@ -3,27 +3,38 @@
  include "../Shared/test.php";
   
  $testsData = array(
-     'create course test' => array(
+     'create an assignment' => array(
          'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
-         'query-before-test' => "INSERT INTO course (coursecode,coursename,visibility,creator, hp) VALUES('IT401G','MyAPICourse',0,101, 7.5)",
-         'query-after-test' => "DELETE FROM course WHERE coursecode = 'IT478G' AND coursename = 'APICreateCourseTestQuery'",
-         'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
+         //This test has no pre querys since its only an insert
+         /*'query-before-test' => "INSERT INTO course (coursecode,coursename,visibility,creator, hp) VALUES('IT401G','MyAPICourse',0,101, 7.5)",*/
+         'query-after-test' => "DELETE FROM quiz WHERE id = 'UNK'",
+         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
          'service-data' => serialize(array( // Data that service needs to execute function
-             'opt' => 'NEW',
-             'username' => 'usr',
-             'password' => 'pass',
-             'coursecode' => 'IT466G',
-             'coursename' => 'TestCourseFromAPI4',
-             'uid' => '101'
+            'opt' => 'SAVDUGGA',
+            'qid' => 'UNK',
+            'cid' => '1885',
+            'userid' => '2',      // this is automatically added depending on what session is active (if any), we want the value to be 2
+            'coursevers' => '1337',
+            'qname' => 'TestQuiz',
+            'autograde' => '1',
+            'gradesys' => '2',
+            'template' => 'Quiz',
+            'jsondeadline' => '{&quot;deadline1&quot;:&quot;2023-04-27 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;&quot;,&quot;comment3&quot;:&quot;&quot;}',
+            'release' => '2023-04-27 00:00:00',
+            'deadline' => '2023-04-29 00:00:00',
+            'qstart' => '2023-04-17',
+            'username' => '2',
+            'password' => 'Kong'
          )),
          'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
              'debug',
              'readonly'
          )),
      ),
+
      'create course test 2' => array(
          'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
-         'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
+         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
          'service-data' => serialize(array( // Data that service needs to execute function
              'opt' => 'NEW',
              'username' => 'usr',
