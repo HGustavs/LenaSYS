63e196b20be9d1d8033248db30d442a50931e712
b21ricni <b21ricni@student.his.se>
2023-05-11 22:56:45 +0200

    Update codeviewer.js

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -1013,7 +1013,7 @@ function updateContent(file, content, boxnumber)
 				alert("Error when updating content: " + e.message);
 			}
 			console.log("Timeout");
-			setTimeout("location.reload()", 10000); //SETBACK TO 500
+			setTimeout("location.reload()", 100000); //SETBACK TO 500
 		}
 	} else {
 		if (box[4] != document.querySelector("#boxtitle2").textContent) {
