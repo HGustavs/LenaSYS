69b2c822195ebe5faaae88fdae324a5ea8901fc4
a21aldsa <a21aldsa@student.his.se>
2023-05-12 14:25:52 +0200

    Revert "combined two cases in saveProperties"
    This reverts commit 412682e382246aee341aaf86763febe6e8dcb6e8.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2977,7 +2977,6 @@ function saveProperties()
                 }
                 break;
             case 'attributes':
-            case 'functions':
                 //Get string from textarea
                 var elementAttr = child.value;
                 //Create an array from string where newline seperates elements
@@ -2994,7 +2993,7 @@ function saveProperties()
                 propsChanged.attributes = arrElementAttr;
                 break;
         
-            /* case 'functions':
+            case 'functions':
                 //Get string from textarea
                 var elementFunc = child.value;
                 //Create an array from string where newline seperates elements
@@ -3009,7 +3008,7 @@ function saveProperties()
                 arrElementFunc = formatArr;
                 element[propName] = arrElementFunc;
                 propsChanged.functions = arrElementFunc;
-                break; */
+                break;
 
             default:
                 break;
