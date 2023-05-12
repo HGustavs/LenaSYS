16fae2a4d88914d2790123258a6eb8b73c9da8ec
c21sebar <c21sebar@student.his.se>
2023-05-12 13:23:26 +0200

    removed test file
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -507,10 +507,10 @@ if($gradesys=="UNK") $gradesys=0;
 					//TODO rest från 13179, här anropas uppdateringsfunktionen
 				} else if(strcmp($opt,"CREGITEX")===0) {
 
-					echo "<script> console.log('TESTING');</script>";
+					
 					//$parts = explode('/', $url);
 					//count if there is already a codeexample or if we should create a new one.
-				
+					$dirname = "Demo";
 					$query = $pdo->prepare("SELECT COUNT(*) FROM codeexample WHERE cid=:cid AND examplename=:examplename;");
 					$query->bindParam(":cid", $courseid);
 					$query->bindParam(":examplename",$dirname); // $parts[count($parts)-1]
