36b7d861efb6fed5b9116cded188ac5dc32777ff
c21sebar <c21sebar@student.his.se>
2023-05-12 15:40:50 +0200

    testing
    seems to not enter the else if
    still no insert into db
    Checking id ajax data is wrong

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1283,7 +1283,7 @@ function AJAXService(opt,apara,kind)
 		$.ajax({
 			url: "sectionedservice.php",
 			type: "POST",
-			data: "opt="+opt+para,
+			data: opt+para,
 			dataType: "json",
 			success: returnedSection
 
