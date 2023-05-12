7e48903b99d8653e53012f5ace7faec60d540527
a20fanma <a20fanma@student.his.se>
2023-05-12 11:19:21 +0200

    Implemented Test 1
    Created the file and implemented Test 1

---
+++ DuggaSys/tests/accessedservice_tests.php
@@ -0,0 +1,32 @@
+
+<?php
+
+include "../Shared/test.php";
+
+$testsData = array(
+    // Test 1
+    'create access test' => array(
+        'expected-output' => '{"debug":"NONE!", "motd":"UNK"}',
+        'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
+        'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
+        'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
+        'query-after-test-2' => "DELETE FROM user WHERE username = 'testuser1'",
+        'service' => 'https://cms.webug.se/root/G2/a20fanma/LenaSYS/DuggaSys/accessedservice.php',
+        'service-data' => serialize(array( // The data that the service needs to execute
+            'opt' => 'UPDAT', // May change UPDAT to UPDATE
+            'prop' => 'firstname',
+            'val' => 'test',
+            'uid' => 'testuser1',
+            'blop' => '!query-before-test-1! [0][uid]' 
+        )),
+        'filter-output' => serialize(array(
+            'none' // To use all output from service
+        )),
+    ),
+    // Create new test
+);
+
+// Call JSON data, how to view the result
+testHandler($testsData, false);
+
+?>
