8254b3a501f04bac32b313abfca50ea49b6c402a
b21willu <102600930+b21willu@users.noreply.github.com>
2023-05-12 14:35:07 +0200

    Update diagram.js

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -11867,7 +11867,6 @@ function drawSelectionBox(str)
         var tempLines = [];
         if (contextLine.length > 0) {
             for (var i = 0; i < contextLine.length; i++) {
-                console.log('context:', contextLine[i].kind);
                 if (contextLine[i].kind === "DOUBLE") {
                     tempLines.push(document.getElementById(contextLine[i].id + "-1"));
                     tempLines.push(document.getElementById(contextLine[i].id + "-2"));
