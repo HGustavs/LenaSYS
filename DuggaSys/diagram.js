dc9460e2b0adb143295c9312dd2460b1c885e540
b21matfi <b21matfi@student.his.se>
2023-05-12 14:19:44 +0200

    fixed typo error
    fixed typo error

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -6134,7 +6134,7 @@ function togglePlacementType(num,type){
         document.getElementById("togglePlacementTypeButton15").classList.remove("activeTogglePlacementTypeButton");
         document.getElementById("togglePlacementTypeBox15").classList.remove("activeTogglePlacementTypeBox");
     }
-    if (num == 13 || type == 12) {
+    if (num == 13 && type == 12) {
         docuemnt.getElementById("elementPlacement12").classList.add("mouseData");
     }
     
