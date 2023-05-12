79e249dca0f5bb26e9ed08e535a007279499a576
c21sebar <c21sebar@student.his.se>
2023-05-12 12:39:31 +0200

    Update test.php
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- testfiles/test.php
+++ testfiles/test.php
@@ -1,5 +1,13 @@
 <?php
 
+include_once "../Shared/sessions.php";
+include_once "../Shared/basic.php";
+include_once "../recursivetesting/FetchGithubRepo.php";
+
+// Connect to database and start session
+pdoConnect();
+session_start();
+
 $dirname = "..&#47;courses&#47;1895&#47;Github&#47;Demo&#47;Code-example1&#47";
 $query = $pdo->prepare("SELECT COUNT(*) FROM codeexample WHERE cid=:cid AND examplename=:examplename;");
 					$query->bindParam(":cid", $courseid);
