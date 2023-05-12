036f1feb5df4635853a1b0fcf1e9512f886fdeda
c21sebar <c21sebar@student.his.se>
2023-05-12 13:16:07 +0200

    Update test.php
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- testfiles/test.php
+++ testfiles/test.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Website</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+
   </head>
   <body>
     <main>
