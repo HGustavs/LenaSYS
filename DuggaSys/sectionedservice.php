da9da6e39c81bd4cb2ac1fb499de491909c7b4ed
c21sebar <c21sebar@student.his.se>
2023-05-15 13:10:27 +0200

    fixing testing var code block to easier see in db what var we are testing
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -550,10 +550,10 @@ if($gradesys=="UNK") $gradesys=0;
 						}
 						$query2 = $pdo->prepare('INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,"TESTING","TESTINGSQLITE",1,45656,1);');
 						$query2->execute();	
-
+						$varname="success";
 						$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
 						$query3->bindParam(":examplename", $success); 
-						$query3->bindParam(":sectionname", $success); 
+						$query3->bindParam(":sectionname", $varname); 
 						$query3->execute();	
 						/*
 						//select the files that has should be in the codeexample
