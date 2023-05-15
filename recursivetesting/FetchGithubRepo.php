9db909d23129c9e068ad889bc61073f07301d42c
a21henhe <a21henhe@student.his.se>
2023-05-15 12:14:58 +0200

    index file is now fetched using curl

--- recursivetesting/FetchGithubRepo.php
+++ recursivetesting/FetchGithubRepo.php
@@ -86,37 +86,26 @@ function downloadToWebServer($cid, $item)
     
 //Retrieves the content of a repos index-file
 function getIndexFile($url) {
-	$indexFilePath = "/index.txt";
-	$url = $url . $indexFilePath;
+    $indexFilePath = "/index.txt";
+    $url = $url . $indexFilePath;
 
-	// Necessary headers to send with the request, 'User-Agent: PHP' is necessary. 
-	$opts = [
-		'http' => [
-		'method' => 'GET',
-		'header' => [
-		'User-Agent: PHP',
-			// If you wish to avoid the API-fetch limit, below is a comment that implements the ability to send a GitHub token key, 
-			// simply replace 'YOUR_GITHUB_API_KEY' with a working token and un-comment the line to send the token as a header for your request. 
-			// To clarify, the syntax will remain as 'Authorization: Bearer 'YOUR_GITHUB_API_KEY' which is a personal token (Settings -> Developer Settings -> Personal Access Token)
-			// Example: 'Authorization: Bearer ghp_y2h1AzwRlaCpUFzEgafT656bDoNSCQ7Y2GSx'
-			//'Authorization: Bearer YOUR_GITHUB_API_KEY'
-			]
-		]
-	];
-
-	// Starts a stream with the required headers
-	$context = stream_context_create($opts);
-	// Fetches the data with the stream included
-	$data = @file_get_contents($url, true, $context);
-    $json = json_decode($data, true);
-    //Check if url is found
-	if($json['message'] !== "Not Found") {
-        //Get content of JSON-object and split files to individual index of the array
-        $array = file_get_contents($json['download_url']);
-        return explode("\n", $array);
-	} else {
-		return false;
-	}
+    $authorization = 'Authorization: Bearer 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';
+    $c = curl_init($url);
+    curl_setopt($c, CURLOPT_USERAGENT, 'curl/7.48.0');
+    curl_setopt($c, CURLOPT_HEADER, 0);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_HTTPHEADER, array($authorization) );
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+    $http_response_code = curl_getinfo($c, CURLINFO_HTTP_CODE);
+    curl_close($c);
+    
+    if($http_response_code == 200){
+        $response = json_decode(curl_exec($c));
+        return explode("\n", file_get_contents($response->download_url));
+    } else {
+        return false;
+    }
 }
 
 function bfs($url, $cid, $opt) 
@@ -153,41 +142,41 @@ function bfs($url, $cid, $opt)
             $json = json_decode($data, true);
             // Loops through each item fetched in the JSON data
             if ($json) {
-							foreach ($json as $item) {
-								if ($item['type'] == 'file') {
-									//If an index file has been found, check against the content of the index file
-									if($filesToDownload){
-										//If file is part of filestoDownload (Index file)
-										if(in_array($item['name'], $filesToDownload)){
-											// Checks if the fetched item is of type 'file'
-											if($opt == "REFRESH") {
-												insertToMetaData($cid, $item);
-											}
-											else if($opt == "DOWNLOAD") {
-												insertToFileLink($cid, $item);
-												insertToMetaData($cid, $item);
-												downloadToWebserver($cid, $item);  
-											}                 
-										}
-										//Otherwise, fetch and download all files
-									} else {
-											// Checks if the fetched item is of type 'file'
-											if($opt == "REFRESH") {
-												insertToMetaData($cid, $item);
-											}
-											else if($opt == "DOWNLOAD") {
-												insertToFileLink($cid, $item);
-												insertToMetaData($cid, $item);
-												downloadToWebserver($cid, $item);  
-											}      
-									}
-									// Checks if the fetched item is of type 'dir'
-								} else if ($item['type'] == 'dir') {
-									if (!in_array($item['url'], $visited)) {
-										array_push($visited, $item['url']);
-										array_push($fifoQueue, $item['url']);
-									}
-								}
+                foreach ($json as $item) {
+                    if ($item['type'] == 'file') {
+                        //If an index file has been found, check against the content of the index file
+                        if($filesToDownload){
+                            //If file is part of filestoDownload (Index file)
+                            if(!in_array($item['name'], $filesToDownload)){
+                                // Checks if the fetched item is of type 'file'
+                                if($opt == "REFRESH") {
+                                    insertToMetaData($cid, $item);
+                                }
+                                else if($opt == "DOWNLOAD") {
+                                    insertToFileLink($cid, $item);
+                                    insertToMetaData($cid, $item);
+                                    downloadToWebserver($cid, $item);  
+                                }                 
+                            }
+                            //Otherwise, fetch and download all files
+                        } else {
+                                // Checks if the fetched item is of type 'file'
+                                if($opt == "REFRESH") {
+                                    insertToMetaData($cid, $item);
+                                }
+                                else if($opt == "DOWNLOAD") {
+                                    insertToFileLink($cid, $item);
+                                    insertToMetaData($cid, $item);
+                                    downloadToWebserver($cid, $item);  
+                                }      
+                        }
+                        // Checks if the fetched item is of type 'dir'
+                    } else if ($item['type'] == 'dir') {
+                        if (!in_array($item['url'], $visited)) {
+                            array_push($visited, $item['url']);
+                            array_push($fifoQueue, $item['url']);
+                        }
+                    }
               }
             } else {
                 //422: Unprocessable entity
