ad13666290377fac0cadf1f7ab054dc21edb024c
b21hamwe <b21hamwe@student.his.se>
2023-05-15 11:39:14 +0200

    Update motherTest.php
    functionality for checkboxes

--- backend-models/service-tests/motherTest.php
+++ backend-models/service-tests/motherTest.php
@@ -52,10 +52,10 @@ include_once ".../test4";
 
         <span><form id="form1" name="form1" method="post" action="<?php echo $PHP_SELF; ?>">
         
-        <input type="checkbox"  id="sort_passed" name="sort_passed" value="passed_value">
+        <input type="checkbox" onclick="checkPassed()" id="sort_passed" name="sort_passed" value="passed_value">
         <label for='sort_passed'>Passed tests</label>
 
-        <input type="checkbox" id="sort_failed" name="sort_failed" value="failed_value">
+        <input type="checkbox" onclick="checkFailed()" id="sort_failed" name="sort_failed" value="failed_value">
         <label for='sort_failed'>Failed tests</label>
         <?php    
             
@@ -147,8 +147,16 @@ include_once ".../test4";
                 echo '</tbody>';
                 
         
-                
-
+                function checkPassed() {
+                    if(filter_has_var($PHP_SELF,'sort_passed')) {
+                        echo $val == 'passed';
+                   }
+                }
+                function checkFailed() {
+                    if(filter_has_var($PHP_SELF,'sort_failed')) {
+                        echo $val == 'failed';
+                   }
+                }
 
 
                        
