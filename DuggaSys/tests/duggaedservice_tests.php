8b3644ef180af88b872ce2b507872a50bdf8f191
AckePe <c21axepe@student.his.se>
2023-05-15 11:58:04 +0200

    Updated api with new version
    Now variables can be used in querys
    
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice_tests.php
+++ DuggaSys/tests/duggaedservice_tests.php
@@ -47,19 +47,19 @@
          'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
          'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
          'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' = 'SAVDUGGA',
-            'qid' = 'NULL',
-            'name' = 'UpdatedAutomaticTest',
-            'autograde' = '1',
-            'gradesys' = '2',
-            'template' = 'group-assignment',
-            'jsondeadline' = '{&quot;deadline1&quot;:&quot;2023-04-30 0:0&quot;;&quot;comment1&quot;:&quot;&quot;;&quot;deadline2&quot;:&quot;&quot;;&quot;comment2&quot;:&quot;&quot;;&quot;deadline3&quot;:&quot;&quot;;&quot;comment3&quot;:&quot;&quot;}',
-            'groupAssignment' = '1', 
-            'release' = '2023-04-28 00:00:00',
-            'deadline' = '2023-04-30 00:00:00',
-            'qstart' = '2023-04-18',
-            'username' = '2',
-            'password' = 'Kong'
+            'opt' => 'SAVDUGGA',
+            'qid' => 'NULL',
+            'name' => 'UpdatedAutomaticTest',
+            'autograde' => '1',
+            'gradesys' => '2',
+            'template' => 'group-assignment',
+            'jsondeadline' => '{&quot;deadline1&quot;:&quot;2023-04-30 0:0&quot;;&quot;comment1&quot;:&quot;&quot;;&quot;deadline2&quot;:&quot;&quot;;&quot;comment2&quot;:&quot;&quot;;&quot;deadline3&quot;:&quot;&quot;;&quot;comment3&quot;:&quot;&quot;}',
+            'groupAssignment' => '1', 
+            'release' => '2023-04-28 00:00:00',
+            'deadline' => '2023-04-30 00:00:00',
+            'qstart' => '2023-04-18',
+            'username' => '2',
+            'password' => 'Kong'
          )),
          'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
              'debug',
@@ -73,12 +73,13 @@
         VALUES (1885, 0, 1, 'AutomaticTest', 'Quiz', '2023-04-27 00:00:00', '2023-04-29 00:00:00', 2, '1337', '2023-04-17', '{&quot;deadline1&quot;:&quot;2023-04-27 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;&quot;,&quot;comment3&quot;:&quot;&quot;}', 0);",
         'query-before-test-2' => "SELECT MAX(id) FROM quiz",
         'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
+        'query-variables' => "qid",
         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' = 'DELDU',
-            'qid' = '!query-before-test-2! [0][quiz]',
-            'username' = 'toddler',
-            'password' = 'Kong'
+            'opt' => 'DELDU',
+            'qid' => '<!query-before-test-2!> <*[0][quiz]*>',
+            'username' => 'toddler',
+            'password' => 'Kong'
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'debug',
@@ -91,16 +92,17 @@
         'query-before-test-1' => "INSERT INTO quiz (cid,autograde,gradesystem,qname,quizFile,qrelease,deadline,creator,vers,qstart,jsondeadline,group) VALUES(1885, 0, 1, 'AutomaticTest', 'Quiz', '2023-04-27 00:00:00', '2023-04-28 00:00:00', 2, '1337', '2023-04-17', '{&quot,deadline1&quot,:&quot,2023-04-27 0:0&quot,,&quot,comment1&quot,:&quot,&quot,,&quot,deadline2&quot,:&quot,&quot,,&quot,comment2&quot,:&quot,&quot,,&quot,deadline3&quot,:&quot,&quot,,&quot,comment3&quot,:&quot,&quot,}', 0);",
         'query-before-test-2' => "SELECT MAX(id) FROM quiz",
         'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1;",
+        'query-variables' => "qid",
         'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
             'opt' => 'ADDVARI',
             'username' => 'toddler',
             'password' => 'Kong',
-            'qid' => '!query-before-test-2! [0][quiz]',
+            'qid' => '<!query-before-test-2!> <*[0][quiz]*>',
             'userid' => '2',
             'disabled' => '1',
             'param' => '{"type":"md","filelink":"md","gType":"","diagram_File":"Empty canvas","diagram_type":{"ER":true,"UML":false,"IE":false},"extraparam":"","notes":"","submissions":[{"type":"pdf","fieldname":"","instruction":""}],"errorActive":false}',
-            'answer' = 'Bara Text'
+            'answer' => 'Bara Text'
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'debug',
@@ -114,17 +116,20 @@
         VALUES (1885, 0, 1, 'AutomaticTest', 'Quiz', '2023-04-27 00:00:00', '2023-04-28 00:00:00', 2, '1337', '2023-04-17', '{&quot;deadline1&quot;:&quot;2023-04-27 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;&quot;,&quot;comment3&quot;:&quot;&quot;}', 0);",
         'query-before-test-2' => "SELECT MAX(id) FROM quiz",
         'query-before-test-3' => "INSERT INTO variant(quizID,creator,disabled,param,variantanswer) 
-        VALUES ('!query-before-test-2! [0][quiz]', 2, 0, '{&quot;type&quot;:&quot;md&quot;,&quot;filelink&quot;:&quot;md&quot;,&quot;gType&quot;:&quot;&quot;,&quot;diagram_File&quot;:&quot;Empty canvas&quot;,&quot;diagram_type&quot;:{&quot;ER&quot;:true,&quot;UML&quot;:false,&quot;IE&quot;:false},&quot;extraparam&quot;:&quot;&quot;,&quot;notes&quot;:&quot;&quot;,&quot;submissions&quot;:[{&quot;type&quot;:&quot;pdf&quot;,&quot;fieldname&quot;:&quot;&quot;,&quot;instruction&quot;:&quot;&quot;}],&quot;errorActive&quot;:false}', 'some text')",
+        VALUES ('?', 2, 0, '{&quot;type&quot;:&quot;md&quot;,&quot;filelink&quot;:&quot;md&quot;,&quot;gType&quot;:&quot;&quot;,&quot;diagram_File&quot;:&quot;Empty canvas&quot;,&quot;diagram_type&quot;:{&quot;ER&quot;:true,&quot;UML&quot;:false,&quot;IE&quot;:false},&quot;extraparam&quot;:&quot;&quot;,&quot;notes&quot;:&quot;&quot;,&quot;submissions&quot;:[{&quot;type&quot;:&quot;pdf&quot;,&quot;fieldname&quot;:&quot;&quot;,&quot;instruction&quot;:&quot;&quot;}],&quot;errorActive&quot;:false}', 'some text')",
+        'query-before-test-4' => "SELECT MAX(vid) FROM variant",
         'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
+        'query-variables' => "id",
         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' = 'SAVVARI',
-            'vid' = '!query-before-test-2! [0][quiz]',
-            'disabled' = '1',
-            'param' = '{"type":"md","filelink":"","gType":"md","gFilelink":"","diagram_File":"","diagram_type":{"ER":true,"UML":false,"IE":false},"extraparam":"","notes":"","submissions":[{"type":"pdf","fieldname":"","instruction":""},{"type":"pdf","fieldname":"","instruction":""}],"errorActive":false}',
-            'answer' = 'new text',
-            'username' = 'toddler',
-            'password' = 'Kong'
+            'opt' => 'SAVVARI',
+            'id' => '!query-before-test-3!> <*[0][quiz]*>'
+            'vid' => '<!query-before-test-4!> <*[0][variant]*>',
+            'disabled' => '1',
+            'param' => '{"type":"md","filelink":"","gType":"md","gFilelink":"","diagram_File":"","diagram_type":{"ER":true,"UML":false,"IE":false},"extraparam":"","notes":"","submissions":[{"type":"pdf","fieldname":"","instruction":""},{"type":"pdf","fieldname":"","instruction":""}],"errorActive":false}',
+            'answer' => 'new text',
+            'username' => 'toddler',
+            'password' => 'Kong'
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'debug',
@@ -138,18 +143,23 @@
         'query-before-test-2' => "SELECT MAX(id) from quiz",
         'query-before-test-3' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,moment,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES(1885, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 5019, 1, 1, 0, 'UNK');",
         'query-before-test-4' => "SELECT MAX(moment) from listentries",
-	    'query-before-test-5' => "INSERT INTO variant(quizID,creator,disabled,param,variantanswer) VALUES($id, 2, 0, '{&quot,type&quot,:&quot,md&quot,,&quot,filelink&quot,:&quot,md&quot,,&quot,gType&quot,:&quot,&quot,,&quot,diagram_File&quot,:&quot,Empty canvas&quot,,&quot,diagram_type&quot,:{&quot,ER&quot,:true,&quot,UML&quot,:false,&quot,IE&quot,:false},&quot,extraparam&quot,:&quot,&quot,,&quot,notes&quot,:&quot,&quot,,&quot,submissions&quot,:[{&quot,type&quot,:&quot,pdf&quot,,&quot,fieldname&quot,:&quot,&quot,,&quot,instruction&quot,:&quot,&quot,}],&quot,errorActive&quot,:false}', 'some text');",
+	    'query-before-test-5' => "INSERT INTO variant(quizID,creator,disabled,param,variantanswer) VALUES(?, 2, 0, '{&quot,type&quot,:&quot,md&quot,,&quot,filelink&quot,:&quot,md&quot,,&quot,gType&quot,:&quot,&quot,,&quot,diagram_File&quot,:&quot,Empty canvas&quot,,&quot,diagram_type&quot,:{&quot,ER&quot,:true,&quot,UML&quot,:false,&quot,IE&quot,:false},&quot,extraparam&quot,:&quot,&quot,,&quot,notes&quot,:&quot,&quot,,&quot,submissions&quot,:[{&quot,type&quot,:&quot,pdf&quot,,&quot,fieldname&quot,:&quot,&quot,,&quot,instruction&quot,:&quot,&quot,}],&quot,errorActive&quot,:false}', 'some text');",
+        //'query-before-test-6' => "SELECT MAX(id) from quiz",
         'query-before-test-6' => "SELECT MAX(vid) from variant",
-	    'query-before-test-7' => "INSERT INTO userAnswer(cid,moment,quiz,variant) VALUES(1885, $moment, $id, $variant);",
+	    'query-before-test-7' => "INSERT INTO userAnswer(cid,moment,quiz,variant) VALUES(1885, ?, ?, ?);",
 	    'query-after-test-1' => "DELETE FROM variant ORDER BY quizID DESC LIMIT 1;",
 	    'query-after-test-2' => "DELETE FROM listentries WHERE cid = 1885;",
 	    'query-after-test-3' => "DELETE FROM quiz ORDER BY id;",
+        'query-variables' => "id, moment, id2, vid",
         'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
             'opt' => 'DELVARI',
             'username' => 'toddler',
             'password' => 'Kong',
-	        'vid' => '$variant'
+            'id' => '<!query-before-test-2!> <*[0][quiz]*>',
+            'moment' => '<!query-before-test-4!> <*[0][listentries]*>',
+            'id2' => '<!query-before-test-2!> <*[0][quiz]*>',
+	        'vid' => '<!query-before-test-6!> <*[0][variant]*>'
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'debug',
