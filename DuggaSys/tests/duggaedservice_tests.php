2051ac6dd8cdab0c6016f4e998142fcb40fd2ca3
AckePe <c21axepe@student.his.se>
2023-05-15 14:57:55 +0200

    Small changes
    Everything is done except expected output
    
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice_tests.php
+++ DuggaSys/tests/duggaedservice_tests.php
@@ -2,19 +2,10 @@
  
  include "../Shared/test.php";
 
- //NEEDS TO BE DONE:
- //Selecting a value and inputting it in the "send"
- //Multiple inserts
- //Multiple deletes
- //Multiple selects
- //Expected output
- //Check how the database is formed, may have to rewrite some tests now when we have more knowledge about how the tests will work
   
  $testsData = array(
      'create an assignment' => array(
          'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
-         //This test has no pre querys since its only an insert
-         /*'query-before-test' => "INSERT INTO course (coursecode,coursename,visibility,creator, hp) VALUES('IT401G','MyAPICourse',0,101, 7.5)",*/
          'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
          'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
          'service-data' => serialize(array( // Data that service needs to execute function
@@ -115,15 +106,15 @@
         'query-before-test-1' => "INSERT INTO quiz(cid,autograde,gradesystem,qname,quizFile,qrelease,deadline,creator,vers,qstart,jsondeadline,`group`) 
         VALUES (1885, 0, 1, 'AutomaticTest', 'Quiz', '2023-04-27 00:00:00', '2023-04-28 00:00:00', 2, '1337', '2023-04-17', '{&quot;deadline1&quot;:&quot;2023-04-27 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;&quot;,&quot;comment3&quot;:&quot;&quot;}', 0);",
         'query-before-test-2' => "SELECT MAX(id) FROM quiz",
-        'query-before-test-3' => "INSERT INTO variant(quizID,creator,disabled,param,variantanswer) 
+        'query-before-test-3' => "INSERT INTO variant(?,creator,disabled,param,variantanswer) 
         VALUES ('?', 2, 0, '{&quot;type&quot;:&quot;md&quot;,&quot;filelink&quot;:&quot;md&quot;,&quot;gType&quot;:&quot;&quot;,&quot;diagram_File&quot;:&quot;Empty canvas&quot;,&quot;diagram_type&quot;:{&quot;ER&quot;:true,&quot;UML&quot;:false,&quot;IE&quot;:false},&quot;extraparam&quot;:&quot;&quot;,&quot;notes&quot;:&quot;&quot;,&quot;submissions&quot;:[{&quot;type&quot;:&quot;pdf&quot;,&quot;fieldname&quot;:&quot;&quot;,&quot;instruction&quot;:&quot;&quot;}],&quot;errorActive&quot;:false}', 'some text')",
         'query-before-test-4' => "SELECT MAX(vid) FROM variant",
         'query-after-test' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
-        'query-variables' => "id",
+        'query-variables' => "id", //This variable is used in query 3
         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
             'opt' => 'SAVVARI',
-            'id' => '!query-before-test-3!> <*[0][quiz]*>'
+            'id' => '<!query-before-test-2!> <*[0][quiz]*>'
             'vid' => '<!query-before-test-4!> <*[0][variant]*>',
             'disabled' => '1',
             'param' => '{"type":"md","filelink":"","gType":"md","gFilelink":"","diagram_File":"","diagram_type":{"ER":true,"UML":false,"IE":false},"extraparam":"","notes":"","submissions":[{"type":"pdf","fieldname":"","instruction":""},{"type":"pdf","fieldname":"","instruction":""}],"errorActive":false}',
@@ -149,8 +140,8 @@
 	    'query-before-test-7' => "INSERT INTO userAnswer(cid,moment,quiz,variant) VALUES(1885, ?, ?, ?);",
 	    'query-after-test-1' => "DELETE FROM variant ORDER BY quizID DESC LIMIT 1;",
 	    'query-after-test-2' => "DELETE FROM listentries WHERE cid = 1885;",
-	    'query-after-test-3' => "DELETE FROM quiz ORDER BY id;",
-        'query-variables' => "id, moment, id2, vid",
+	    'query-after-test-3' => "DELETE FROM quiz ORDER BY ?;",
+        'query-variables' => "id, moment, id2, vid", //Used in query 7 and 5
         'service' => 'https://cms.webug.se/root/G2/students/c21alest/LenaSYS/DuggaSys/courseedservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
             'opt' => 'DELVARI',
