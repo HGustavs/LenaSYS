6990fd19048caa544850e40df87dadbe349928ef
a21aldsa <a21aldsa@student.his.se>
2023-05-15 16:04:48 +0200

    added back commented out check for text width

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9855,9 +9855,9 @@ function drawElement(element, ghosted = false)
                 fill='transparent'
                 />`;
                 //if the alternative is longer than the avaliable space, add a linebreak.
-                /* if (canvasContext.measureText(element.alternatives[i]).width >= boxw-(linw*2)) {
+                if (canvasContext.measureText(element.alternatives[i]).width >= boxw-(linw*2)) {
                     console.log("alternative needs a linebreak");
-                } */
+                }
                 console.log(element.alternatives[i] + "is " + canvasContext.measureText(element.alternatives[i]).width + "long");
                 str += `<text x='${linew*2}' y='${((boxh/element.alternatives.length)*i)+(texth/1.5)+linew*2}' fill='${actorFontColor}'>${element.alternatives[i]}</text>`;
             }
