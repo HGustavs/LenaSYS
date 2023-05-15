b6429f5153ee1354d75d0476a209112e082579be
c21sebar <c21sebar@student.his.se>
2023-05-15 11:51:35 +0200

    Update sectionedservice.php
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -510,16 +510,18 @@ if($gradesys=="UNK") $gradesys=0;
 					//Server error from here
 					$query = $pdo->prepare("SELECT COUNT(*) FROM codeexample WHERE cid=:cid AND examplename=:examplename;");
 					$query->bindParam(":cid", $courseid);
-					$query->bindParam(":examplename",$dirname); // $parts[count($parts)-1]
+					$query->bindParam(":examplename", $dirname); // $parts[count($parts)-1]
 					$query->exectue();
+
+					$result = $query->fetch(PDO::FETCH_OBJ);
+					$counted = $result->$counted;
 					
 					$query = $pdo->prepare('INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,"TESTING","TESTING",1,45656,1);');
 					$query->execute();	
 					/*
 					
 					
-					$result = $query->fetch(PDO::FETCH_OBJ);
-					$counted = $result->$counted;
+					
 
 					
 
