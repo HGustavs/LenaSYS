0420a3a5ff174c34db0f7946da146b082558b3c0
e21krida <e21krida@student.his.se>
2023-05-15 10:20:08 +0200

    reorganized if-statement
    Co-Authored-By: a21lukpo <102577654+a21lukpo@users.noreply.github.com>

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -594,25 +594,23 @@
 				<?php
 					global $pdo;
 					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['githubInsert'])) {
+					if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['githubInsert']) && !empty($_POST['githubDir'])) {
 						$cid = getOPG('courseid');
 						$feedbackenabled = 0;
-						if(!empty($_POST['githubDir'])) {
-							$query = $pdo->prepare("INSERT INTO listentries (cid, githubDir, creator, feedbackenabled) VALUES (:cid, :githubdir, :creator, :feedbackenabled)");
-							$query->bindParam(':cid', $cid);
-							$query->bindParam(':githubdir', $_POST['githubDir']);
-							$query->bindParam(':creator', $userid);
-							$query->bindParam(':feedbackenabled', $feedbackenabled);
-							try {
-								if($query->execute()) {
-									echo "<script>console.log('insert successful!');</script>";		
-								} else {
-									echo "<script>console.log('insert failed!');</script>";		
-								} 
-							} catch (PDOException $e) {
-								$errorMessage = $e->getMessage();
-								echo "<script>console.log('" . addslashes($errorMessage) . "');</script>";
-							}
+						$query = $pdo->prepare("INSERT INTO listentries (cid, githubDir, creator, feedbackenabled) VALUES (:cid, :githubdir, :creator, :feedbackenabled)");
+						$query->bindParam(':cid', $cid);
+						$query->bindParam(':githubdir', $_POST['githubDir']);
+						$query->bindParam(':creator', $userid);
+						$query->bindParam(':feedbackenabled', $feedbackenabled);
+						try {
+							if($query->execute()) {
+								echo "<script>console.log('insert successful!');</script>";		
+							} else {
+								echo "<script>console.log('insert failed!');</script>";		
+							} 
+						} catch (PDOException $e) {
+							$errorMessage = $e->getMessage();
+							echo "<script>console.log('" . addslashes($errorMessage) . "');</script>";
 						}
 					}
 				?>
