d851262d50bb27fa956304979946972b5c87c5eb
b21ricni <b21ricni@student.his.se>
2023-05-15 16:06:28 +0200

    Added functionallity
    Added Edit title function to editBoxTitle
    Added Function  to check user login

--- DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
+++ DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
@@ -1,4 +1,37 @@
 <?php
     date_default_timezone_set("Europe/Stockholm");
     
+    // Include basic application services
+    include_once "../Shared/sessions.php";
+    include_once "../Shared/basic.php";
+
+    // Connect to database and start session
+    pdoConnect();
+    session_start();
+
+    function checkUserStatusTest(){
+        // Checks user id, if user has none a guest id is set
+        if(isset($_SESSION['uid'])){
+            $userid=$_SESSION['uid'];
+        }else{
+            $userid="1";
+        }
+
+        // Gets username based on uid, USED FOR LOGGING
+        $query = $pdo->prepare( "SELECT username FROM user WHERE uid = :uid");
+        $query->bindParam(':uid', $userid);
+        $query-> execute();
+
+        $log_uuid = getOP('log_uuid');
+        $log_timestamp = getOP('log_timestamp');
+
+        $log_uuid = getOP('log_uuid');
+        $info="opt: ".$opt." courseId: ".$courseId." courseVersion: ".$courseVersion." exampleName: ".$exampleName." sectionName: ".$sectionName." exampleId: ".$exampleId;
+        logServiceEvent($log_uuid, EventTypes::ServiceServerStart, "codeviewerService.php",$userid,$info);
+
+        // This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $username = $row['username'];
+        }
+    }
 ?>
--- DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle.php
+++ DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle.php
@@ -1,4 +1,24 @@
 <?php
     date_default_timezone_set("Europe/Stockholm");
     
+    require __DIR__ . '../Misc/checkUserStatus.php';
+
+    echo checkUserStatusTest();
+
+    if(checklogin() && ($writeAccess=="w" || isSuperUser($_SESSION['uid']))) {
+        if(strcmp('EDITTITLE',$opt)===0) {
+            $exampleid = $_POST['exampleid'];
+            $boxId = $_POST['boxid'];
+            $boxTitle = $_POST['boxtitle'];
+
+            $query = $pdo->prepare("UPDATE box SET boxtitle=:boxtitle WHERE boxid=:boxid AND exampleid=:exampleid;");
+            $query->bindParam(':boxtitle', $boxTitle);
+            $query->bindValue(':exampleid', $exampleId);
+            $query->bindParam(':boxid', $boxId);
+            $query->execute();
+
+            echo json_encode(array('title' => $boxTitle, 'id' => $boxId));
+            return;
+        }
+    }
 ?>
