03f8873cdd2a20f581404292ab1226c89aa0aee7
c21sebar <c21sebar@student.his.se>
2023-05-15 17:08:37 +0200

    fixing db names for testing of ajax dirname
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -506,7 +506,7 @@ if($gradesys=="UNK") $gradesys=0;
 					$varname="Dirname";
 						
 					$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-					$query3->bindParam(":examplename", $success); 
+					$query3->bindParam(":examplename", $varname); 
 					$query3->bindParam(":sectionname", $dirname); 
 					$query3->execute();	
 
@@ -517,7 +517,7 @@ if($gradesys=="UNK") $gradesys=0;
 					$varname="DirnameExplode";
 		
 					$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-					$query3->bindParam(":examplename", $success); 
+					$query3->bindParam(":examplename", $varname); 
 					$query3->bindParam(":sectionname", $dirname); 
 					$query3->execute();	
 					//$dirname = "Code-example1";
