552d5a8027a3259b59634f497f5c789a72977d3a
b21hamwe <b21hamwe@student.his.se>
2023-05-15 14:16:39 +0200

    Update motherTest.php
    Another try for the checkboxes

--- backend-models/service-tests/motherTest.php
+++ backend-models/service-tests/motherTest.php
@@ -51,14 +51,13 @@ include_once ".../test4";
         <button type="button" onclick="searchTable()">Search</button>
 
         <span><form id="form1" name="form1" method="post" action="<?php echo $PHP_SELF; ?>">
-        
-        <input type="checkbox" onchange="checkPassed()" id="sort_passed" name="sort_passed" value="passed_value">
+
+        <input type="checkbox" id="sort_passed" name="sort_passed" value="passed_value">
         <label for='sort_passed'>Passed tests</label>
 
-        <input type="checkbox" onchange="checkFailed()" id="sort_failed" name="sort_failed" value="failed_value">
+        <input type="checkbox" id="sort_failed" name="sort_failed" value="failed_value">
         <label for='sort_failed'>Failed tests</label>
 
-        <tr class="<?php echo ($value['result'] == 'passed') ? 'passed' : 'failed'; ?>">
         <?php    
             
             $motherTest_json = file_get_contents('motherTestJSON.json');
@@ -149,36 +148,51 @@ include_once ".../test4";
                 echo '</tbody>';
                 
         
-                function checkPassed() {
-                    if(filter_has_var($PHP_SELF,'sort_passed')) {
-                        echo $val == 'passed';
-                   }
-                }
-                function checkFailed() {
-                    if(filter_has_var($PHP_SELF,'sort_failed')) {
-                        echo $val == 'failed';
-                   }
-                }
+                function checkPassed($val) {
+                    if($_POST['sort_passed'] && $val == 'passed') {
+                      return true;
+                    }
+                    return false;
+                  }
+                  
+                  function checkFailed($val) {
+                    if($_POST['sort_failed'] && $val == 'failed') {
+                      return true;
+                    }
+                    return false;
+                  }
+
+
+                  foreach($decoded_json['create course test'] as $key => $value){
+                    echo '<tr class="'.($value['result'] == 'passed' ? 'passed' : 'failed').'">';
+                    if (checkPassed($value['result']) || checkFailed($value['result'])) {
+                      echo '<td>'.$value['result'].'</td>';
+                    } else {
+                      echo '<td>'.$value['test_name'].'</td>';
+                    }
+                    echo '</tr>';
+                  }
 
 
             ?>
 
         <script>
+         
             function filterTests() {
-            var passedChecked = document.getElementById("sort_passed").checked;
-            var failedChecked = document.getElementById("sort_failed").checked;
-            var rows = document.getElementsByTagName("tr");
+                var passedChecked = document.getElementById("sort_passed").checked;
+                var failedChecked = document.getElementById("sort_failed").checked;
+                var rows = document.getElementsByTagName("tr");
 
-            for (var i = 1; i < rows.length; i++) { // start at i=1 to skip table header
-                var row = rows[i];
-                if (passedChecked && row.classList.contains("failed")) {
-                row.style.display = "none";
-                } else if (failedChecked && row.classList.contains("passed")) {
-                row.style.display = "none";
-                } else {
-                row.style.display = "";
+                for (var i = 1; i < rows.length; i++) { // start at i=1 to skip table header
+                    var row = rows[i];
+                    if (passedChecked && row.classList.contains("failed")) {
+                    row.style.display = "none";
+                    } else if (failedChecked && row.classList.contains("passed")) {
+                    row.style.display = "none";
+                    } else {
+                    row.style.display = "";
+                    }
                 }
-            }
             }
 
             document.getElementById("sort_passed").addEventListener("change", filterTests);
