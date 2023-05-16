1a4f781614b9d04085189889181762e7a7a395df
b21hamwe <b21hamwe@student.his.se>
2023-05-16 16:10:50 +0200

    Test for checkUserStatusTest
    Test for checkUserStatusTest

---
+++ DuggaSys/MicroservicesBeta/Misc/testLogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TestLogin</title>
+</head>
+<body>
+    <button onclick="showLoginPopUp();">
+        <h3>Login</h3>
+    </button>
+
+    <button onload="checkUserStatusTest();">Call checkUserStatusTest</button>
+</body>
+</html>
+
+<?php
+pdoConnect();
+session_start();
+
+require __DIR__ . '../Misc/checkUserStatus.php';
+
+echo checkUserStatusTest();
+
+?>
