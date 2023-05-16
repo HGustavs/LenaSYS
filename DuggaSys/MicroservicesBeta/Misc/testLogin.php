cdece9bd5e7a82d04cf79db427214f05b4be94d4
b21hamwe <b21hamwe@student.his.se>
2023-05-16 17:13:34 +0200

    Update testLogin.php
    added js script funct. for showloginpopup

--- DuggaSys/MicroservicesBeta/Misc/testLogin.php
+++ DuggaSys/MicroservicesBeta/Misc/testLogin.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TestLogin</title>
 </head>
+
+<?php 
+echo '<script> testLogin_showLoginPopup(); </script>'; 
+?>
+
 <body>
     <button onclick="showLoginPopUp();">
         <h3>Login</h3>
