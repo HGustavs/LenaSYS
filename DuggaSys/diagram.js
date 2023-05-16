65b798ad9202db9e2b650fc63f3b783b7c557b48
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-16 10:09:13 +0200

    test to see if i broke something

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2921,18 +2921,17 @@ function changeState()
             element.state = property;
             stateMachine.save(StateChangeFactory.ElementAttributesChanged(element.id, { state: property }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
         }
-    //Check if type has been changed
-    if (oldType != newType) {
-        var newKind = element.kind;
-        newKind = newKind.replace(oldType, newType);
-        //Update element kind
-        element.kind = newKind;
-        stateMachine.save(StateChangeFactory.ElementAttributesChanged(element.id, { kind: newKind }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
-    }
-    //Update element type
-    element.type = newType;
-    stateMachine.save(StateChangeFactory.ElementAttributesChanged(element.id, { type: newType }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
-    
+        //Check if type has been changed
+        if (oldType != newType) {
+            var newKind = element.kind;
+            newKind = newKind.replace(oldType, newType);
+            //Update element kind
+            element.kind = newKind;
+            stateMachine.save(StateChangeFactory.ElementAttributesChanged(element.id, { kind: newKind }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
+        }
+        //Update element type
+        element.type = newType;
+        stateMachine.save(StateChangeFactory.ElementAttributesChanged(element.id, { type: newType }), StateChange.ChangeTypes.ELEMENT_ATTRIBUTE_CHANGED);
 
     }
     else if(element.type=='IE') {
