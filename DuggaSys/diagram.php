10c59e69fdecdb7f63827ffc5dc0ae28860b168c
b21oscpe <b21oscpe@student.his.se>
2023-05-16 11:55:26 +0200

    test

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -29,7 +29,7 @@
     <script src="../Shared/js/jquery-ui-1.10.4.min.js"></script>
     <script src="../Shared/dugga.js"></script>
     <script src="../Shared/markdown.js"></script>
-    <script type="module" src="diagram.js"></script>
+    <script src="diagram.js"></script>
     <script src="./assets/js/fetchDiagramInfo.js"></script>
 </head>
 <body onload="getData();addAlertOnUnload();" style="overflow: hidden;">
