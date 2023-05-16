709a5b247869e35ed42b5365f02f32bf7ba832be
a20fanma <a20fanma@student.his.se>
2023-05-16 10:31:32 +0200

    Updated accessedservice_tests.php
    Made some changes on expected output

--- DuggaSys/tests/accessedservice_tests.php
+++ DuggaSys/tests/accessedservice_tests.php
@@ -6,7 +6,7 @@ include "../../Shared/test.php";
 $testsData = array(
     // Test 1
     'create access test' => array(
-        'expected-output' => '{"debug":"NONE!", "motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
         'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -25,7 +25,7 @@ $testsData = array(
     ),
     // Test 2
     'create access test 2' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
         'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -44,7 +44,7 @@ $testsData = array(
     ),
     // Test 3
     'create access test 3' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
         'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -63,7 +63,7 @@ $testsData = array(
     ),
     // Test 4
     'create access test 4' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
         'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -82,7 +82,7 @@ $testsData = array(
     ),
     // Test 5
     'create access test 5' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT userid FROM user WHERE username = 'testuser1'", // May change userid to uid
         'query-before-test-2' => "INSERT INTO user(username, pwd) VALUES('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -101,7 +101,7 @@ $testsData = array(
     ),
     // Test 6
     'create access test 6' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'", 
         'query-before-test-2' => "INSERT INTO course(creator, ccoursecode) VALUES(1 , 'testtest')",
         'query-after-test-1' => "DELETE FROM course WHERE cid = 'testtest'", // cid may have another value
@@ -121,7 +121,7 @@ $testsData = array(
     // Test 7
     // Part of Test 6 when the examiner value is NONE!
     'create access test 7' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'", 
         'query-before-test-2' => "INSERT INTO user_course(userid, cid, access) VALUES(1 , 'testtest', 'test')",
         'query-after-test-1' => "DELETE FROM user_course WHERE cid = 'testtest'", // cid may have another value
@@ -140,7 +140,7 @@ $testsData = array(
     ),
     // Test 8
     'create access test 8' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}',
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'", 
         'query-before-test-2' => "INSERT INTO course(creator, coursecode) VALUES(1 , 'testtest')",
         'query-before-test-3' => "INSERT INTO user_course(uid, cid, access) VALUES (2, 'testtest', 'test')",
@@ -161,7 +161,7 @@ $testsData = array(
     ),
     // Test 9
     'create access test 9' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'", 
         'query-before-test-2' => "INSERT INTO course(creator, coursecode) VALUES(1 , 'testtest')",
         'query-before-test-3' => "INSERT INTO user_course(uid, cid, access) VALUES (2, 'testtest', 'test')",
@@ -182,7 +182,7 @@ $testsData = array(
     ),
     // Test 10
     'creat access test 10' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'", 
         'query-before-test-2' => "INSERT INTO course(creator, coursecode) VALUES(1 , 'testtest')",
         'query-before-test-3' => "INSERT INTO user_course(uid, cid, access) VALUES (2, 'testtest', 'test')",
@@ -203,7 +203,7 @@ $testsData = array(
     ),
     // Test 11
     'create access test 11' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-after-test-1' => "DELETE FROM class WHERE class = 'testClass'",
         'service' => 'https://cms.webug.se/root/G2/a20fanma/LenaSYS/DuggaSys/accessedservice.php',
         'service-data' => serialize(array(
@@ -223,7 +223,7 @@ $testsData = array(
     ),
     // Test 12
     'create access test 12' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT uid FROM user WHERE username = 'testuser1'",
         'query-before-test-2' => "INSERT INTO user(username, password) VALUES ('testuser1', 'testpwd')",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
@@ -240,7 +240,7 @@ $testsData = array(
     ),
     // Test 13
     'create access test 13' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "INSERT INTO class(class, responsible, regcode, classcode, hp, tempo, hpProgress) VALUES ('testClass', 2,'testClassName', 12345678, '87654321', 7.5, 100, 1.5)",
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
         'query-after-test-2' => "DELETE FROM class WHERE class = 'testClass'",
@@ -263,7 +263,7 @@ $testsData = array(
     // Test 14
     // Add select and insert
     'create access test 14' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1'",
         'query-after-test-2' => "DELETE FROM class WHERE class = 'testClass'",
         'service' => 'https://cms.webug.se/root/G2/a20fanma/LenaSYS/DuggaSys/accessedservice.php',
@@ -286,7 +286,7 @@ $testsData = array(
     ),
     // Test 15
     'create access test 15' => array(
-        'expected-output' => '{"debug":"NONE!","motd":"UNK"}', // Change this on all tests before DONE!
+        'expected-output' => '{"entries":"UNK", "debug":"UNK", "teachers":"UNK", "classes":"UNK", "courses":"UNK", "groups":"UNK", "queryResult":"UNK", "examiners":"UNK", "submissions":"UNK", "access":"UNK"}',
         'query-before-test-1' => "SELECT cid FROM course WHERE coursecode = 'testtest'",
         'query-before-test-2' => "INSERT INTO course(creator, coursecode) VALUES (1, 'testtest')",
         'query-after-test-1' => "DELETE FROM course WHERE cid = 'testtest'",
