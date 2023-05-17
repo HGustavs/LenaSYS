e9d7fe307762011761d30bc73a0fd97f56ee75bb
b21ricni <b21ricni@student.his.se>
2023-05-17 14:38:42 +0200

    Moving from v6 -> v7
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

---
+++ DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle.php
@@ -0,0 +1,45 @@
+<?php
+    date_default_timezone_set("Europe/Stockholm");
+
+    // Include basic application services
+	include_once ("../../coursesyspw.php");
+	include_once ("../Shared/sessions.php");
+	include_once ("../Shared/basic.php");
+	include_once ("../Shared/courses.php");
+	include_once ("../Shared/database.php");
+
+    // Connect to database and start session
+	pdoConnect();
+	session_start();
+
+    // Global variables
+	$exampleId=getOP('exampleid');
+	$boxId=getOP('boxid');
+	$opt=getOP('opt');
+    
+    require __DIR__ . '../Misc/checkUserStatus.php';
+    require __DIR__ . '/codeViewerRetriveInformation.php';
+
+    echo checkUserStatusTest();
+
+    //Check access
+    if(checklogin() && ($hasWriteAccess==true || $hasSuperAccess==true)){
+    //if(checklogin() && ($writeAccess=="w" || isSuperUser($_SESSION['uid']))) {
+        if(strcmp('EDITTITLE',$opt)===0) {
+            $exampleid = $_POST['exampleid'];
+            $boxId = $_POST['boxid'];
+            $boxTitle = $_POST['boxtitle'];
+
+            $query = $pdo->prepare("UPDATE box SET boxtitle=:boxtitle WHERE boxid=:boxid AND exampleid=:exampleid;");
+            $query->bindParam(':boxtitle', $boxTitle);
+            $query->bindValue(':exampleid', $exampleId);
+            $query->bindParam(':boxid', $boxId);
+            $query->execute();
+
+            echo json_encode(array('title' => $boxTitle, 'id' => $boxId));
+            return;
+        }
+    }
+
+    echo retrieveCodeViewerInformation();
+?>
