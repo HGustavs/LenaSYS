7ab299749971318a293a01a938a7890b6bffda79
a21aldsa <a21aldsa@student.his.se>
2023-05-17 15:19:33 +0200

    adjusting svg for rect

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -9833,12 +9833,11 @@ function drawElement(element, ghosted = false)
                 y='${boxw}'
                 width='${textWidth}'
                 height='${texth}'
-                rx='${sequenceCornerRadius}'
                 stroke-width='${linew}'
                 stroke='${element.stroke}'
                 fill='${element.fill}' 
             />`;
-            str += `<text class='text' x='${xAnchor}' y='${boxw}' dominant-baseline='middle' text-anchor='${vAlignment}' fill='${nonFilledElementPartStrokeColor}'>${element.name}</text>`;
+            str += `<text class='text' x='${xAnchor}' y='${boxw+(texth/2)}' dominant-baseline='middle' text-anchor='${vAlignment}' fill='${nonFilledElementPartStrokeColor}'>${element.name}</text>`;
             str += `</g>`;
         }
         else if (element.actorOrObject == "object") {
