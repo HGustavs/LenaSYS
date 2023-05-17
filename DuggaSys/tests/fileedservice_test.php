23f2513bd08066c07bb5732a659df94dc1874a88
b21kurar <b21kurar@student.his.se>
2023-05-17 17:09:54 +0200

    remake tests

--- DuggaSys/tests/fileedservice_test.php
+++ DuggaSys/tests/fileedservice_test.php
@@ -4,46 +4,48 @@ include_once "../../../coursesyspw.php";
 
 $testsData = array(
 	'Delete file test' => array(
-		'expected-output' => '{"entries":[],"debug":"NONE!","gfiles":[],"lfiles":[],"access":false,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
+		'expected-output' => '{"debug":"The file was deleted.","gfiles":[],"lfiles":[],"access":true,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
 		'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
 		'service-data' => serialize(array( // Data that service needs to execute function
 			'username' => 'brom',
 			'password' => 'password',
-			'cid' => '1234',
+			'cid' => '1894',
+			'coursevers' => '52432',
 			'opt' => 'DELFILE',
-			'coursevers' => 'test',
-			'coursename' => 'test.txt',
-			'kind' => 1,
-			'fid' => 1,
-			'contents' => ''
+			'fid' => '47',
+			'cid' => '1894',
+			'coursevers' => '52432',
+			'filename' => 'helloWorld.html',
+			'kind' => '2',
 		)),
 		'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-			'entries',
 			'debug',
 			'gfiles',
 			'lfiles',
 			'access',
 			'studentteacher',
-			'supeuser',
+			'superuser',
 			'waccess',
 			'supervisor'
 		)),
 	),
-	'Save file test' => array(
-		'expected-output' => '{"entries":[],"debug":"NONE!","gfiles":[],"lfiles":[],"access":false,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
+
+	'Edit file test' => array(
+		'expected-output' => '{"debug":"NONE!","gfiles":[],"lfiles":[],"access":true,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
 		'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
 		'service-data' => serialize(array( // Data that service needs to execute function
 			'username' => 'brom',
 			'password' => 'password',
+			'cid' => '1894',
+			'coursevers' => '52432',
 			'opt' => 'SAVEFILE',
-			'filename' => 'testfilename',
-			'filesize' => 'test',
-			'kindid' => '2',
-			'vers' => 'testvers',
-			'cid' => '' // value from previous query
+			'cid' => '1894',
+			'contents' => "<div class='err'><span style='font-weight:bold;'>Bummer!</span> You have reached a non-navigable link!!!</div>",
+			'filename' => 'helloWorld.html',
+			'filepath' => '../courses/global/helloWorld.html', 
+			'kind' => '2'
 		)),
 		'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-			'entries',
 			'debug',
 			'gfiles',
 			'lfiles',
@@ -55,44 +57,45 @@ $testsData = array(
 		)),
 	),
 
-	'Retrieve file path test' => array( // UNFINISHED
-		'expected-output' => '',
-		'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
-		'service-data' => serialize(array( // Data that service needs to execute function
-			'username' => 'brom',
-			'password' => 'password',
-			'fid' => 1,
-			'expectedPath' => '' // path to file
-		)),
-		'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-		)),
-	),
-
-	'Logging service events test' => array( // I think that this one isn't necessary
-		'expected-output' => '{"entries":[],"debug":"NONE!","gfiles":[],"lfiles":[],"access":false,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
-		'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
-		'service-data' => serialize(array( // Data that service needs to execute function
-			'username' => 'brom',
-			'password' => 'password',
-			'log_uuid' => '123',
-			'user_id' => '1',
-			'script_name' => 'fileedservice.php',
-			'event_type' => 'EventTypes::ServiceServerStart',
-			'info' => 'DELFILE 123 2023-00-00 1 test.txt 2'
+	// Part of 'Retrieve Infromation' for the service, unnecessary
+	// 'Retrieve file path test' => array( // UNFINISHED
+	// 	'expected-output' => '',
+	// 	'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
+	// 	'service-data' => serialize(array( // Data that service needs to execute function
+	// 		'username' => 'brom',
+	// 		'password' => 'password',
+	// 		'fid' => 1,
+	// 		'expectedPath' => '' // path to file
+	// 	)),
+	// 	'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+	// 	)),
+	// )
+		// Don't need to test logging
+	// 'Logging service events test' => array( // I think that this one isn't necessary
+	// 	'expected-output' => '{"entries":[],"debug":"NONE!","gfiles":[],"lfiles":[],"access":false,"studentteacher":false,"superuser":true,"waccess":false,"supervisor":false}',
+	// 	'service' => 'cms.webug.se/root/G2/students/b21kurar/LenaSYS/DuggaSys/fileedservice.php',
+	// 	'service-data' => serialize(array( // Data that service needs to execute function
+	// 		'username' => 'brom',
+	// 		'password' => 'password',
+	// 		'log_uuid' => '123',
+	// 		'user_id' => '1',
+	// 		'script_name' => 'fileedservice.php',
+	// 		'event_type' => 'EventTypes::ServiceServerStart',
+	// 		'info' => 'DELFILE 123 2023-00-00 1 test.txt 2'
 			
-		)),
-		'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-			'entries',
-			'debug',
-			'gfiles',
-			'lfiles',
-			'access',
-			'studentteacher',
-			'supeuser',
-			'waccess',
-			'supervisor'
-		)),
-	),
+	// 	)),
+	// 	'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+	// 		'entries',
+	// 		'debug',
+	// 		'gfiles',
+	// 		'lfiles',
+	// 		'access',
+	// 		'studentteacher',
+	// 		'supeuser',
+	// 		'waccess',
+	// 		'supervisor'
+	// 	)),
+	// ),
 
 );
 
