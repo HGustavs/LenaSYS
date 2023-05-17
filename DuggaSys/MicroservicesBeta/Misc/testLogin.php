2a9b4877d6fc883dc3bf6be270da2ac56aebc76c
b21hamwe <b21hamwe@student.his.se>
2023-05-17 15:27:02 +0200

    Update testLogin.php
    push it
    
    Co-Authored-By: b21ricni <102578233+b21ricni@users.noreply.github.com>

--- DuggaSys/MicroservicesBeta/Misc/testLogin.php
+++ DuggaSys/MicroservicesBeta/Misc/testLogin.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TestLogin</title>
-</head>
+
+
+
 
 <?php 
 pdoConnect();
@@ -20,7 +15,7 @@ require __DIR__ . '../Misc/checkUserStatus.php';
 
 echo checkUserStatusTest();
 ?>
-
+<html lang="en">
 <body onload="checkUserStatusTest();">
     <button onclick="showLoginPopUp();">
         <h3>Login</h3>
