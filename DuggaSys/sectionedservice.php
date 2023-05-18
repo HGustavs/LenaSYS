5fb8b0f0654f906762d169d5a0f5ad31dd9f4054
c21sebar <c21sebar@student.his.se>
2023-05-18 13:37:47 +0200

    Testing all variables on those that do not create a box
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -667,35 +667,46 @@ if($gradesys=="UNK") $gradesys=0;
 										$query3->bindParam(":examplename", $varname); 
 										$query3->bindParam(":sectionname", $varname); 
 										$query3->execute();	
+										$varname="boxid";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $boxid); 
+										$query3->execute();	
+										$varname="exampleid";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $exampleid); 
+										$query3->execute();	
+										$varname="filename";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $filename); 
+										$query3->execute();	
+										$varname="filetype";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $filetype); 
+										$query3->execute();	
+										$varname="wlid";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $wlid); 
+										$query3->execute();	
+										$varname="fontsize";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $fontsize); 
+										$query3->execute();	
+										$varname="setting";	
+										$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
+										$query3->bindParam(":examplename", $varname); 
+										$query3->bindParam(":sectionname", $setting); 
+										$query3->execute();	
 										
 									}
 							
 										
-									$varname="boxid";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $boxid); 
-									$query3->execute();	
-									$varname="exampleid";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $exampleid); 
-									$query3->execute();	
-									$varname="filename";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $filename); 
-									$query3->execute();	
-									$varname="filetype";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $filetype); 
-									$query3->execute();	
-									$varname="wlid";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $wlid); 
-									$query3->execute();		
+										
 
 								}
 
