53eb2cbd96a3605975797b06af001e73307c3794
c21sebar <c21sebar@student.his.se>
2023-05-18 12:55:50 +0200

    testing
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -656,11 +656,6 @@ if($gradesys=="UNK") $gradesys=0;
 									$query->bindParam(":fontsize", $fontsize);
 									$query->execute();
 
-									$varname="fileName";	
-									$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
-									$query3->bindParam(":examplename", $varname); 
-									$query3->bindParam(":sectionname", $filename); 
-									$query3->execute();
 								}
 
 								$link = "UNK";
