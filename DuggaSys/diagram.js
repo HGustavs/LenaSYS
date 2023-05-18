7161a363eb2ef3c70b846ef8275dfd5438891ac4
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-18 15:20:48 +0200

    gav up on changing the size while there was only 1 line

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10026,26 +10026,7 @@ function drawElement(element, ghosted = false)
        
         //div to encapuslate note content
         //Draw note-content if there exist at least one attribute
-        if (elemAttri == 1) {   
-            var NOTEEntityHeight = {
-                id: element.id,
-                height: ((boxh + boxh) / zoomfact)
-            }
-            NOTEHeight.push(NOTEEntityHeight);
-            console.log("elemAttri == 1");
-            //svg for background
-            str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}'>`;
-            str += `<rect class='text' x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}'
-            stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
-            for (var i = 0; i < elemAttri; i++) {
-                str += `<text class='text' x='0.5em' y='${hboxh + boxh * i / 2}' dominant-baseline='middle' text-anchor='right'>${text[i]}</text>`;
-            }
-            //end of svg for background
-            str += `</svg>`;
-            // Draw note-content if there are no attributes.
-        }
-        else if (elemAttri >= 2) {
-            console.log("elemAttri >= 2");
+        if (elemAttri != 0) {
             //svg for background
             str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}'>`;
             str += `<rect class='text' x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}'
