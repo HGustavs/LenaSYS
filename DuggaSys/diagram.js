a4f7d88bcf5fc0b4a1b52c01e738e5defd45fc96
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-18 14:17:46 +0200

    changed /2 to /4

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10036,7 +10036,7 @@ function drawElement(element, ghosted = false)
         //Draw note-content if there exist at least one attribute
         if (elemAttri != 0) {
             //svg for background
-            str += `<svg width='${boxw}' height='${boxh / 2 + (boxh * elemAttri / 2)}'>`;
+            str += `<svg width='${boxw}' height='${boxh / 4 + (boxh * elemAttri / 2)}'>`;
             str += `<rect class='text' x='${linew}' y='${linew}' width='${boxw - (linew * 2)}' height='${boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)}'
             stroke-width='${linew}' stroke='${element.stroke}' fill='${element.fill}' />`;
             for (var i = 0; i < elemAttri; i++) {
