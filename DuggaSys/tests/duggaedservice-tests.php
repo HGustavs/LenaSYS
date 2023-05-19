432ae4d620ff7d12383910cdaf28977d53a045a0
AckePe <c21axepe@student.his.se>
2023-05-19 14:57:48 +0200

    Update duggaedservice-tests.php
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice-tests.php
+++ DuggaSys/tests/duggaedservice-tests.php
@@ -19,18 +19,19 @@ $testsData = array(
                 // Data that service needs to execute function
                 'opt' => 'SAVDUGGA',
                 'qid' => 'UNK',
-                'cid' => '1885',
-                'userid' => '2',
+                'cid' => '2',
+                'nme' => 'test',
+
                 // this is automatically added depending on what session is active (if any), we want the value to be 2
-                'coursevers' => '1337',
-                'qname' => 'TestQuiz',
-                'autograde' => '1',
-                'gradesys' => '2',
-                'template' => 'Quiz',
-                'jsondeadline' => '{&quot;deadline1&quot;:&quot;2023-04-27 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;&quot;,&quot;comment3&quot;:&quot;&quot;}',
-                'release' => '2023-04-27 00:00:00',
-                'deadline' => '2023-04-29 00:00:00',
-                'qstart' => '2023-04-17',
+                'coursevers' => '97732',
+                //'qname' => 'TestQuiz',
+                'autograde' => '0',
+                'gradesys' => '1',
+                'template' => '3d-dugga',
+                'jsondeadline' => '{&quot;deadline1&quot;:&quot;2023-05-02 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;2023-05-02 0:0&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;2023-05-03 0:0&quot;,&quot;comment3&quot;:&quot;&quot;}',
+                'release' => '2023-05-04 0:0',
+                'deadline' => '2023-05-02 0:0',
+                'qstart' => '2023-05-01 0:0',
                 'username' => 'brom',
                 'password' => 'password'
             )
