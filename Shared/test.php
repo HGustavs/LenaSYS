3b3a5c3d9cfecb2f88b9f1e5a5998345df41f562
a21jeaha <a21jeaha@student.his.se>
2023-05-19 09:45:52 +0200

    removed debug code

--- Shared/test.php
+++ Shared/test.php
@@ -256,7 +256,7 @@ function callServiceTest($service, $data, $filter, $QueryReturnJSON, $prettyPrin
     curl_close($curl);
 
     $curlResponseJSON = json_decode($curlResponse, true);
-    echo print_r($curlResponseJSON);
+
     // Only include JSON same as filter
     foreach($filter as $option => $optionArray){
         // If none do not filter
@@ -346,40 +346,6 @@ function assertEqualTest($valueExpected, $valueOuput, $prettyPrint){
     );
 }
 
-// // Test 3: assert equal test
-// function assertEqualTest2($valueExpected, $valueOuput, $prettyPrint){
-
-//     // Expected value is JSON
-//     $valueExpected = json_encode($valueExpected);
-//     $valueOuput = json_encode($valueOuput);
-
-//     if (($valueExpected != null) && ($valueOuput != null)){
-//         $equalTest = ($valueExpected == $valueOuput);
-//         if ($equalTest){
-//             $equalTestResult = "passed";
-//         }
-//         else{
-//             $equalTestResult = "failed";
-//         }
-//     }
-//     else{
-//         $equalTestResult = "failed with error: no valid values to compare";
-//     }
-
-//     if ($prettyPrint) {
-//         echo "<h3> Test 3 (assertEqual): {$equalTestResult} </h3>";
-//         echo "<strong>value expected: </strong>".json_encode($valueExpected, true);
-//         echo "<br>";
-//         echo "<strong>value output: </strong>".json_encode($valueOuput, true);
-//         echo "<br>";
-//         echo "<br>";
-//     }
-//     return array(
-//         'result' => $equalTestResult,
-//         'value-expected' => $valueExpected,
-//         'value-output' => $valueOuput
-//     );
-// }
 
 // Test 3: assert equal test
 function assertEqualTestJP($valueExpected, $valueOuput, $prettyPrint){
