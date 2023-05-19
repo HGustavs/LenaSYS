5f9b1c797c6e169ad52c80711c3c9fc78622d015
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-19 15:38:59 +0200

    made the cut off bigger

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10041,9 +10041,9 @@ function drawElement(element, ghosted = false)
                 d="M${linew},${linew}
                     v${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2))}
                     h${boxw - (linew * 2)}
-                    v-${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)) * 0.8}  
-                    l-${(boxw - (linew * 2))*0.08},-${9.2*zoomfact}  
-                    h-${(boxw - (linew * 2)) * 0.92}
+                    v-${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)) * 0.75}  
+                    l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * elemAttri / 2) - (linew * 2)) * 0.25} 
+                    h-${(boxw - (linew * 2)) * 0.88}
                 "
                 stroke-width='${linew}'
                 stroke='${element.stroke}'
