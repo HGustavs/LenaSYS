abf779c21fe78998bcdc39e0249841a564ccf115
a21jeaha <a21jeaha@student.his.se>
2023-05-19 11:23:11 +0200

    added the field 'kind' to the filter

--- DuggaSys/tests/sectionedservice_Test.php
+++ DuggaSys/tests/sectionedservice_Test.php
@@ -179,6 +179,7 @@ $testsData = array(
                 'entryname',
                 //'lid',
                 'pos',
+                'kind',
                 'moment',
                 //'link',
                 'visible',
