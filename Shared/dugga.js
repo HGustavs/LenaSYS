f2b0d5b7b5d681ad20ef6e92ab504d24358ad4e9
b21ricni <b21ricni@student.his.se>
2023-05-19 15:42:58 +0200

    error?
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1166,7 +1166,7 @@ function AJAXService(opt,apara,kind)
 	}else if(kind=="BOXTITLE"){
 		$.ajax({
 			//url: "codeviewerService.php",
-			url: "../DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle_ms.php",
+			url: "../DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle.php",
 			//url: "../DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php",
 			type: "POST",
 			data: "opt="+opt+para,
