c44ee145f11c255dccdeaf237f8aabc0268d9b8e
AckePe <c21axepe@student.his.se>
2023-05-19 15:02:39 +0200

    Update duggaedservice-tests.php
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice-tests.php
+++ DuggaSys/tests/duggaedservice-tests.php
@@ -8,9 +8,7 @@ $testsData = array(
     This will lead to the DELETE query still deleting something during the test since the delete is based on id, and not on a created specific id. 
     Therefore this test will be commented out for the time being, added if the error is found.*/
     'create an assignment' => array(
-        'expected-output' => '{
-            {"debug":"NONE!","writeaccess":true,"coursecode":"IT118G","coursename":"Webbutveckling - datorgrafik","entries":[{"qname":"Bitdugga1"},{"qname":"Bitdugga2"},{"qname":"colordugga1"},{"qname":"colordugga2"},{"qname":"linjedugga1"},{"qname":"linjedugga2"},{"qname":"dugga1"},{"qname":"dugga2"},{"qname":"Quiz"},{"qname":"Rapport"},{"qname":"HTML CSS Testdugga"},{"qname":"Clipping masking testdugga"},{"qname":"test"}]}
-        }',
+        'expected-output' => '{"debug":"NONE!","writeaccess":true,"coursecode":"IT118G","coursename":"Webbutveckling - datorgrafik","entries":[{"qname":"Bitdugga1"},{"qname":"Bitdugga2"},{"qname":"colordugga1"},{"qname":"colordugga2"},{"qname":"linjedugga1"},{"qname":"linjedugga2"},{"qname":"dugga1"},{"qname":"dugga2"},{"qname":"Quiz"},{"qname":"Rapport"},{"qname":"HTML CSS Testdugga"},{"qname":"Clipping masking testdugga"},{"qname":"test"}]}',
         'query-after-test-1' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
         'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
         'service-data' => serialize(
