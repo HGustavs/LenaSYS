2d569f02c7c5c1bb563549bb1f1fbd4b981f919c
Thea <c21theti@student.his.se>
2023-05-19 15:53:53 +0200

    test
    test

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -4385,6 +4385,12 @@ function toggleDarkmode()
     btn.style.color = "#614875";
     btn.style.fontWeight = "bold";
    }
+   else if (cssUrl == 'blackTheme.css'){
+    btn.style.backgroundColor ="#00FFFF";
+    btn.style.border = "3px solid #614875";
+    btn.style.color = "#614875";
+    btn.style.fontWeight = "bold";
+   }
    else {
     btn.style.backgroundColor ="transparent";
     btn.style.border = "3px solid #614875";
