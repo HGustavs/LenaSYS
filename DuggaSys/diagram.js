84341b2ec411bb899563494fc77200f35e3cff06
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-22 00:51:59 +0200

    number test

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10064,7 +10064,7 @@ function drawElement(element, ghosted = false)
                     h${boxw - (linew * 2)}
                     v-${(boxh / 2 + (boxh * 4 / 2) - (linew * 2)) - (boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}  
                     l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}                    
-                    h-${(boxw - (linew * 2)) * 0.88}
+                    h-${(boxw - (linew * 2)) * 0.885}
                 "
                 stroke-width='${linew}'
                 stroke='${element.stroke}'
