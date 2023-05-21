cc4aefb312ca1ca4c5dc6015c5ba0dc15ffad590
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-22 00:37:11 +0200

    h value changed to match the text box

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10076,7 +10076,7 @@ function drawElement(element, ghosted = false)
                     v${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5} 
                     h${(boxw - (linew * 2)) * 0.11}
                     l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}
-                    h-${(boxw - (linew * 2)) * 0.88}
+                    h-${(boxw - (linew * 2)) * 0.885}
                 "
                 stroke-width='${linew}'
                 stroke='${element.stroke}'
