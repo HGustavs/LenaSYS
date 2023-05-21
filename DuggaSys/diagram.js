e08bc5300bf9ee61bf0e5c179ee8f6368277a790
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-22 00:41:24 +0200

    test

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10074,7 +10074,7 @@ function drawElement(element, ghosted = false)
                 d="M${linew},${linew}                                     
                     h${(boxw - (linew * 2)) * 0.885}
                     v${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5} 
-                    h${(boxw - (linew * 2)) * 0.12}
+                    h${(boxw - (linew * 2)) * 0.115}
                     l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}
                     h-${(boxw - (linew * 2)) * 0.885}
                 "
