e4413e40a4f4d36adb94e96546a5f4e6381af9e4
c21adano <102599332+c21adano@users.noreply.github.com>
2023-05-22 00:21:31 +0200

    last commit didnt carry any of the changes

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -10064,6 +10064,9 @@ function drawElement(element, ghosted = false)
                     h${boxw - (linew * 2)}
                     v-${(boxh / 2 + (boxh * 4 / 2) - (linew * 2)) - (boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}  
                     l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5} 
+                    v${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5} 
+                    h${(boxw - (linew * 2)) * 0.12}
+                    l-${(boxw - (linew * 2)) * 0.12},-${(boxh / 2 + (boxh * 1 / 2) - (linew * 2)) * 0.5}
                     h-${(boxw - (linew * 2)) * 0.885}
                 "
                 stroke-width='${linew}'
