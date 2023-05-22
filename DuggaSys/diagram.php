21df0e45b3f8a86d65b1348ba0a67de965276e17
b21matfi <b21matfi@student.his.se>
2023-05-22 16:35:02 +0200

    added tabindex

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -48,7 +48,7 @@
         <fieldset>
             <legend aria-hidden="true" aria-hidden="true">Modes</legend>
             
-                <div id="mouseMode0" class="diagramIcons toolbarMode active" onclick='setMouseMode(0);'>
+                <div tabindex="0" id="mouseMode0" class="diagramIcons toolbarMode active" onclick='setMouseMode(0);'>
                     <img src="../Shared/icons/diagram_pointer_white.svg" alt="Pointer"/>
                     <span class="toolTipText" id="highestToolTip"><b>Pointer</b><br>
                         <p>Allows you to select and move different elements as well as navigate the workspace</p><br>
