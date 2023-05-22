0e3bfe235711800e16a9fb4c05358f939fb72128
e21krida <e21krida@student.his.se>
2023-05-22 14:38:14 +0200

    reverted test
    Co-Authored-By: a21lukpo <102577654+a21lukpo@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -24,13 +24,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if(isset($_POST['lid'])) {
+/* if(isset($_POST['lid'])) {
 	$fetchedLid = $_POST['lid'];
 	echo "<script>console.log('lid:', " . $fetchedLid . ");</script>";
  } else {
 	echo "<script>console.log('error');</script>";
  }
-
+ */
 
 // Gets username based on uid, USED FOR LOGGING
 $query = $pdo->prepare( "SELECT username FROM user WHERE uid = :uid");
