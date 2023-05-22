de330db36cee990058771cc7147e760ecb302ae1
b21matfi <b21matfi@student.his.se>
2023-05-22 16:23:27 +0200

    changed position of button element

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -47,24 +47,28 @@
     <div id="diagram-toolbar" onmousedown='mdown(event)' onmouseup='tup();'>
         <fieldset>
             <legend aria-hidden="true" aria-hidden="true">Modes</legend>
-            <button>
+            
                 <div id="mouseMode0" class="diagramIcons toolbarMode active" onclick='setMouseMode(0);'>
+                <button>
                     <img src="../Shared/icons/diagram_pointer_white.svg" alt="Pointer"/>
                     <span class="toolTipText" id="highestToolTip"><b>Pointer</b><br>
                         <p>Allows you to select and move different elements as well as navigate the workspace</p><br>
                         <p id="tooltip-POINTER" class="key_tooltip">Keybinding:</p>
                     </span>
+                    </button>
                 </div>
-            </button>
-            <button>
+            
+            
                 <div id="mouseMode1" class="diagramIcons toolbarMode" onclick='setMouseMode(1);'>
+                <button>
                     <img src="../Shared/icons/diagram_box_selection2.svg" alt="Box Selection"/>
                     <span class="toolTipText"><b>Box Selection</b><br>
                         <p>Click and drag to select multiple elements within the selected area</p><br>
                         <p id="tooltip-BOX_SELECTION" class="key_tooltip">Keybinding:</p>
                     </span>
+                    </button>
                 </div>
-            </button>
+           
                 <div>
                     <div id="elementPlacement0"
                          class="ERButton diagramIcons toolbarMode"
