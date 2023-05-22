f285f9648e14cc2873e623c91f79ca590c5dbc49
AckePe <c21axepe@student.his.se>
2023-05-22 14:14:40 +0200

    Update duggaedservice-tests.php
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice-tests.php
+++ DuggaSys/tests/duggaedservice-tests.php
@@ -58,7 +58,7 @@ $testsData = array(
             array(
                 // Data that service needs to execute function
                 'opt' => 'SAVDUGGA',
-                'qid' => '<!query-before-test-1!> <*[0][id]*>',
+                'qid' => '<!query-before-test-1!> <*[0][quiz]*>',
                 'cid' => '2',
                 'nme' => 'TestDugga6',
 
