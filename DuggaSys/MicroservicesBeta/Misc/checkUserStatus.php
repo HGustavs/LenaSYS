1e8089225c50d634406de9bb1f0306d1c49c8154
b21ricni <b21ricni@student.his.se>
2023-05-22 11:38:37 +0200

    okokok
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

--- DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
+++ DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
@@ -2,8 +2,8 @@
     date_default_timezone_set("Europe/Stockholm");
     
     // Include basic application services
-    include_once ("../../../Shared/sessions.php";)
     include_once ("../../../../coursesyspw.php");
+    include_once ("../../../Shared/sessions.php";)
     include_once ("../../../Shared/basic.php";)
     include_once ("../../../Shared/courses.php");
 	include_once ("../../../Shared/database.php");
--- DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle_ms.php
+++ DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle_ms.php
@@ -7,7 +7,7 @@
 	include_once ("../../../Shared/sessions.php");
 	include_once ("../../../Shared/basic.php");
 	
-	//include ('../Misc/checkUserStatus.php';) //Need to check user status
+	include ('../Misc/checkUserStatus.php';) //Need to check user status
 
 	//Remove when ms is working
 	//include_once ("../../../Shared/courses.php");
@@ -22,6 +22,8 @@
 	$boxId=getOP('boxid');
 	$opt=getOP('opt');
 	
+	checkUserStatusTest();
+	/*
 	// Checks user id, if user has none a guest id is set
 	if(isset($_SESSION['uid'])){
 		$userid=$_SESSION['uid'];
@@ -35,14 +37,14 @@
 	$log_uuid = getOP('log_uuid');
 	$info="opt: ".$opt." courseId: ".$courseId." courseVersion: ".$courseVersion." exampleName: ".$exampleName." sectionName: ".$sectionName." exampleId: ".$exampleId;
 	logServiceEvent($log_uuid, EventTypes::ServiceServerStart, "editBoxTitle.php",$userid,$info);
-
+*/
 	$appuser=(array_key_exists('uid', $_SESSION) ? $_SESSION['uid'] : 0);
 
 	$exampleCount = 0;
-
+	/*
   	$query = $pdo->prepare( "SELECT exampleid,sectionname,examplename,runlink,cid,cversion,beforeid,afterid,public FROM codeexample WHERE exampleid = :exampleid;");
   	$query->bindParam(':exampleid', $exampleId);
-	$query->execute();
+	$query->execute();*/
 
 	while ($row = $query->fetch(PDO::FETCH_ASSOC)){
 		$exampleCount++;
@@ -50,7 +52,7 @@
 
 	// TODO: Better handle a situation where there are no examples available
 	if($exampleCount>0){
-		if(checklogin() && ($writeAccess=="w" || isSuperUser($_SESSION['uid']))) {
+		if(checklogin() && ($hasWAccess==true || $hasSuperAccess == true)) {
 
 			if(strcmp('EDITTITLE',$opt)===0) {
 				$exampleid = $_POST['exampleid'];
