0a91156c0a921d93dc8240969df9c8cf08fe96a0
b21ricni <b21ricni@student.his.se>
2023-05-22 12:03:26 +0200

    plz work
    Co-Authored-By: b21hamwe <102579060+b21hamwe@users.noreply.github.com>

--- DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
+++ DuggaSys/MicroservicesBeta/Misc/checkUserStatus.php
@@ -8,8 +8,24 @@
     include_once ("../../../Shared/courses.php");
 	include_once ("../../../Shared/database.php");
 
-    function cUST(){
-        echo "uwu";
+    function cUST($log_uuid){
+        // Checks user id, if user has none a guest id is set
+        if(isset($_SESSION['uid'])){
+            $userid=$_SESSION['uid'];
+        }else{
+            $userid="1";
+        }
+
+        $log_uuid = getOP('log_uuid');
+        $log_timestamp = getOP('log_timestamp');
+
+        $log_uuid = getOP('log_uuid');
+        $info="opt: ".$opt." courseId: ".$courseId." courseVersion: ".$courseVersion." exampleName: ".$exampleName." sectionName: ".$sectionName." exampleId: ".$exampleId;
+        logServiceEvent($log_uuid, EventTypes::ServiceServerStart, "checkUserStatus.php",$userid,$info);
+
+        $appuser=(array_key_exists('uid', $_SESSION) ? $_SESSION['uid'] : 0);
+
+        return $log_uuid;
     }
 
     /*function checkUserStatusTest(){
--- DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle_ms.php
+++ DuggaSys/MicroservicesBeta/codeviewerService/editBoxTitle_ms.php
@@ -17,21 +17,7 @@
 	$boxId=getOP('boxid');
 	$opt=getOP('opt');
 	
-	// Checks user id, if user has none a guest id is set
-	if(isset($_SESSION['uid'])){
-		$userid=$_SESSION['uid'];
-	}else{
-		$userid="1";
-	}
-
-	$log_uuid = getOP('log_uuid');
-	$log_timestamp = getOP('log_timestamp');
-
-	$log_uuid = getOP('log_uuid');
-	$info="opt: ".$opt." courseId: ".$courseId." courseVersion: ".$courseVersion." exampleName: ".$exampleName." sectionName: ".$sectionName." exampleId: ".$exampleId;
-	logServiceEvent($log_uuid, EventTypes::ServiceServerStart, "editBoxTitle.php",$userid,$info);
-
-	$appuser=(array_key_exists('uid', $_SESSION) ? $_SESSION['uid'] : 0);
+	cUST();
 
 	$exampleCount = 0;
 
