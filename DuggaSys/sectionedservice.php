9ef6fdf23ff4a31da939262b30cda91fe2f104bf
c21sebar <c21sebar@student.his.se>
2023-05-22 15:10:40 +0200

    testing
    Co-Authored-By: b21sebgr <102599253+b21sebgr@users.noreply.github.com>

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -694,7 +694,7 @@ if($gradesys=="UNK") $gradesys=0;
 							$query3->bindParam(":sectionname", $exampleName); 
 							$query3->execute();
 
-							$likePattern = $exampleName .'%';
+							$likePattern = '\''.$exampleName .'%'.'\'';
 
 							$varname="TESTING likePattern";	
 							$query3 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (1,:examplename,:sectionname,1,45656,1);");
@@ -703,7 +703,7 @@ if($gradesys=="UNK") $gradesys=0;
 							$query3->execute();
 
 							$pdolite = new PDO('sqlite:../../githubMetadata/metadata2.db');
-							$query = $pdolite->prepare("SELECT * FROM gitFiles WHERE cid = :cid AND fileName LIKE ':fileName';"); 
+							$query = $pdolite->prepare("SELECT * FROM gitFiles WHERE cid = :cid AND fileName LIKE :fileName;"); 
 							$query->bindParam(':cid', $cid);
 							$query->bindParam(':fileName', $likePattern);
 							$query->execute();
