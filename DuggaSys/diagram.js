6f8d41063421b7af93fda544e5a833499a8b9e00
a19clasv <a19clasv@student.his.se>
2023-05-22 09:46:40 +0200

    Subtract 13 from y when resizing ER entities

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2644,8 +2644,8 @@ function mmoving(event)
                 tmp = elementData.y;
                 if(elementData.kind != "EREntity"){
                     elementData.y = screenToDiagramCoordinates(0, (startY - deltaY + 15)).y;
-                }else{//the superstate jumps down 10 pixels when using this node for ER entities.
-                    elementData.y = screenToDiagramCoordinates(0, (startY - deltaY - 10)).y;
+                }else{//the superstate jumps down 13 pixels when using this node for ER entities.
+                    elementData.y = screenToDiagramCoordinates(0, (startY - deltaY - 13)).y;
                 }
                 
                 // Deduct the new position, giving us the total change
