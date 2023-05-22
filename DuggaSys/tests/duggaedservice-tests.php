410e50b526028d23b399cdf03a8edc143fb9b632
AckePe <c21axepe@student.his.se>
2023-05-22 14:36:48 +0200

    Update duggaedservice-tests.php
    Co-Authored-By: b21antbr <102599104+b21antbr@users.noreply.github.com>

--- DuggaSys/tests/duggaedservice-tests.php
+++ DuggaSys/tests/duggaedservice-tests.php
@@ -49,7 +49,7 @@ $testsData = array(
     
     'update an assignment' => array(
         'expected-output' => '{"debug":"NONE!","writeaccess":true,"coursecode":"IT118G","coursename":"Webbutveckling - datorgrafik","entries":[{"qname":"Bitdugga1"},{"qname":"Bitdugga2"},{"qname":"colordugga1"},{"qname":"colordugga2"},{"qname":"linjedugga1"},{"qname":"linjedugga2"},{"qname":"dugga1"},{"qname":"dugga2"},{"qname":"Quiz"},{"qname":"Rapport"},{"qname":"HTML CSS Testdugga"},{"qname":"Clipping masking testdugga"},{"qname":"TestDugga6"}]}',
-        'query-before-test-1' => "INSERT INTO quiz(cid,qid,qname,autograde,gradesys,template,qstart,deadline,jsondeadline,release,coursevers) 
+        'query-before-test-1' => "INSERT INTO quiz(cid,qid,qname,autograde,gradesystem,quizFile,qstart,deadline,jsondeadline,qrelease,vers) 
          VALUES (2, 'UNK', 'TestDugga5', 1, 1, '3d-dugga', '2023-05-01 0:0', '2023-05-02 0:0', '{&quot;deadline1&quot;:&quot;2023-05-02 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;2023-05-03 0:0&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;2023-05-04 0:0&quot;,&quot;comment3&quot;:&quot;&quot;}', '2023-05-06 0:0', 97732);",
         'query-before-test-2' => "SELECT id FROM quiz WHERE qname = 'TestDugga5'",
         //'query-after-test-1' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
