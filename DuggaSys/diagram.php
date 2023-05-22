9db63b1ccc663a15aaf53cdfa799616cae973862
b21matfi <b21matfi@student.his.se>
2023-05-22 16:31:34 +0200

    added style to img

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -49,7 +49,7 @@
             <legend aria-hidden="true" aria-hidden="true">Modes</legend>
             
                 <button id="mouseMode0" class="diagramIcons toolbarMode active" onclick='setMouseMode(0);'>
-                    <img src="../Shared/icons/diagram_pointer_white.svg" alt="Pointer"/>
+                    <img src="../Shared/icons/diagram_pointer_white.svg" alt="Pointer"  style="display: block; width: 100%;"/>
                     <span class="toolTipText" id="highestToolTip"><b>Pointer</b><br>
                         <p>Allows you to select and move different elements as well as navigate the workspace</p><br>
                         <p id="tooltip-POINTER" class="key_tooltip">Keybinding:</p>
