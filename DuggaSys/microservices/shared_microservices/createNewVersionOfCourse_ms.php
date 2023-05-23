6b037a38e3c4e20fd148b5ba071c42d82ca56465
b21hamwe <b21hamwe@student.his.se>
2023-05-23 14:20:48 +0200

    Create createNewVersionOfCourse_ms.php
    Start of microservice

---
+++ DuggaSys/microservices/shared_microservices/createNewVersionOfCourse_ms.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once "../Shared/basic.php";
+include_once "../Shared/sessions.php";
+
+$opt=getOP('opt');
+$cid=getOP('cid');
+$coursecode=getOP('coursecode');
+$coursename=getOP('coursename');
+$coursenamealt=getOP('coursenamealt');
+$versname=getOP('versname');
+$versid=getOP('versid');
+$motd=getOP('motd');
+
+if(strcmp($opt,"NEWVRS")===0){
+    $query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt,startdate,enddate,motd) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt,:startdate,:enddate,:motd);");
+
+    $query->bindParam(':cid', $cid);
+    $query->bindParam(':coursecode', $coursecode);
+    $query->bindParam(':vers', $versid);
+    $query->bindParam(':versname', $versname);
+    $query->bindParam(':coursename', $coursename);
+    $query->bindParam(':coursenamealt', $coursenamealt);
+    $query->bindParam(':motd', $motd);
+
+}
+
+
+            
+?>            
