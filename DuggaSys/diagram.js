8d7fe4d012bd650061ef592fcd330b660a6a9a7c
a19clasv <a19clasv@student.his.se>
2023-05-24 00:23:12 +0200

    Get y from screen coordinates in getRectFromElement

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -3501,7 +3501,7 @@ function getRectFromElement (element)
 {
     return {
         x: element.x,
-        y: element.y,
+        y: screenToDiagramCoordinates(element.x, element.y).y,
         width: element.width,
         height: element.height,
     };
