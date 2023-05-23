dd8eebce0643e60f153577baa9cf13469e40f8db
a21jeaha <a21jeaha@student.his.se>
2023-05-23 14:55:28 +0200

    Update test.php

--- Shared/test.php
+++ Shared/test.php
@@ -63,7 +63,7 @@ function getURL($serviceName){
         
         echo strpos($serviceName, $part);
         
-        if((str_contains($serviceName, $part)==false) && (strcmp($part,"tests") != 0)){
+        if((strchr($serviceName, $part)==false) && (strcmp($part,"tests") != 0)){
             // echo $part;
             $url .= $part.'/';
         }
