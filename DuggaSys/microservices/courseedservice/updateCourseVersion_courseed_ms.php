29213187f7216c410d2e4f11ace9d5bf294ce4d0
a20fanma <a20fanma@student.his.se>
2023-05-23 14:23:25 +0200

    Created micro-service
    Created a micro-service file

---
+++ DuggaSys/microservices/courseedservice/updateCourseVersion_courseed_ms.php
@@ -0,0 +1,37 @@
+<?php
+
+date_default_timezone_set("Europe/Stockholm");
+
+// Include basic application services
+include ('../shared_microservices/getUid_ms.php');
+
+// Connect to database and start session
+pdoConnect();
+session_start();
+
+// Global variables
+$opt=getOP('opt');
+$cid=getOP('cid');
+$coursename=getOP('coursename');
+$versid=getOP('versid');
+$versname=getOP('versname');
+
+getUid();
+
+
+$query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course ORDER BY coursename");
+
+if(strcmp($opt,"UPDATEVRS")===0){
+    $query = $pdo->prepare("UPDATE vers SET versname=:versname WHERE cid=:cid AND coursecode=:coursecode AND vers=:vers;");
+    $query->bindParam(':cid', $courseid);
+    $query->bindParam(':coursecode', $coursecode);
+    $query->bindParam(':vers', $versid);
+    $query->bindParam(':versname', $versname);
+
+    if(!$query->execute()) {
+            $error=$query->errorInfo();
+            $debug="Error updating entries\n".$error[2];
+    }
+}
+
+?>
