f41c87bd6032146ef97a4475cc689d73c326828d
Holkers <a19hakja@student.his.se>
2023-05-24 15:13:35 +0200

    Update updateCourseVersion_courseed_ms.php
    included coursesyspw.php

--- DuggaSys/microservices/courseedService/updateCourseVersion_courseed_ms.php
+++ DuggaSys/microservices/courseedService/updateCourseVersion_courseed_ms.php
@@ -5,7 +5,7 @@ date_default_timezone_set("Europe/Stockholm");
 // Include basic application services!
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
-
+include_once "../../../../coursesyspw.php";
 
 // Connect to database and start session
 pdoConnect();
