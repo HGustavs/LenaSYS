32fb79ebd53aaf7ed686037d77de153a9dc0ed4c
a21oscgu <a21oscgu@student.his.se>
2023-05-24 11:39:10 +0200

    Update dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1018,7 +1018,8 @@ function AJAXService(opt,apara,kind)
 
 	if(kind=="COURSE"){
 			$.ajax({
-				url: "courseedservice.php",
+				//url: "courseedservice.php",
+				url : "../DuggaSys/microservices/shared_microservices/setAsActiveCourse_ms.php",
 				type: "POST",
 				data: "opt="+opt+para,
 				dataType: "json",
