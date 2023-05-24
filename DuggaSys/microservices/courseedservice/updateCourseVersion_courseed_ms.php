0a3a4e75496201524a07be2cc04e44fd410f0595
a20fanma <a20fanma@student.his.se>
2023-05-24 15:43:09 +0200

    Updated the file
    Made some changes in the file

--- DuggaSys/microservices/courseedservice/updateCourseVersion_courseed_ms.php
+++ DuggaSys/microservices/courseedservice/updateCourseVersion_courseed_ms.php
@@ -3,6 +3,8 @@
 date_default_timezone_set("Europe/Stockholm");
 
 // Include basic application services
+include_once "../Shared/basic.php";
+include_once "../Shared/sessions.php";
 include ('../shared_microservices/getUid_ms.php');
 
 // Connect to database and start session
@@ -19,6 +21,24 @@ $versname=getOP('versname');
 
 getUid();
 
+if(isset($_SESSION['uid'])){
+	$userid=$_SESSION['uid'];
+}else{
+	$userid="UNK";
+}
+$ha=null;
+$debug="NONE!";
+
+// Gets username based on uid, USED FOR LOGGING
+$query = $pdo->prepare( "SELECT username FROM user WHERE uid = :uid");
+$query->bindParam(':uid', $userid);
+$query-> execute();
+
+// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
+while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+	$username = $row['username'];
+}
+
 
 $query = $pdo->prepare("SELECT coursename,coursecode,cid,visibility,activeversion,activeedversion FROM course ORDER BY coursename");
 
@@ -34,8 +54,6 @@ if(strcmp($opt,"UPDATEVRS")===0){
             $debug="Error updating entries\n".$error[2];
     }
 
-    echo json_encode(array('cid' => $cid, 'coursecode' => $coursecode, 'vers' => $versid, 'versname' => $versname));
-    return;
 }
 
 ?>
