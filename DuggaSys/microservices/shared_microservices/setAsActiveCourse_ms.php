3e4028f6c8c00d6f9056fac6c263837aa538f156
a21oscgu <a21oscgu@student.his.se>
2023-05-24 11:38:55 +0200

    Update setAsActiveCourse_ms.php

---
+++ DuggaSys/microservices/shared_microservices/setAsActiveCourse_ms.php
@@ -0,0 +1,27 @@
+<?php
+
+	date_default_timezone_set("Europe/Stockholm");
+
+	// Include basic application services!
+	include_once "../../../Shared/basic.php";
+	include_once "../../../Shared/sessions.php";
+
+	// Connect to database and start session
+	pdoConnect();
+	session_start();
+
+	// Global variables
+	$cid=getOP('cid');
+	$versid=getOP('versid');
+	$makeactive=getOP('makeactive');
+
+	if($makeactive==3){
+		$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
+		$query->bindParam(':cid', $cid);
+		$query->bindParam(':vers', $versid);
+		if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error updating entries\n".$error[2];
+		}
+	}
+?>
