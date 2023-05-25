5ce1914ffd5c5f238c84d9f6616285279841ecba
a19clasv <a19clasv@student.his.se>
2023-05-25 09:53:58 +0200

    Adds 300 and doubles height correction for SE types

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -3614,8 +3614,8 @@ function getRectFromElement (element)
                 resizedY += (element.height - preResizeHeight[i].height)/2
             }
             if(element.type == "SE"){
-                resizedY -= (element.height - preResizeHeight[i].height)/4
-                resizedY += 30;
+                resizedY += (element.height - preResizeHeight[i].height)
+                resizedY += 300;
             }
             return {
                 x: element.x,
@@ -3628,7 +3628,7 @@ function getRectFromElement (element)
     let elementY = element.y;
     if(element.type == "SE"){
         let elementY = element.y;
-        elementY += 30;
+        elementY += 300;
     }
     return {
         x: element.x,
