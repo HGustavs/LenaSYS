bfc91c984a321e472be3e080d189dfd57f999703
a20fanma <a20fanma@student.his.se>
2023-05-25 10:18:13 +0200

    Created microservice
    Created a microservice for courseedservice

---
+++ DuggaSys/microservices/courseedservice/setActiveCourseversion_ms.php
@@ -0,0 +1,31 @@
+<?php
+
+date_default_timezone_set("Europe/Stockholm");
+
+// Include basic application services
+include_once "../Shared/basic.php";
+include_once "../Shared/sessions.php";
+include '../shared_microservices/getUid_ms.php';
+
+// Connect to database and start session
+pdoConnect();
+session_start();
+
+// Global variables
+$opt=getOP('opt');
+$cid=getOP('cid');
+$makeactive=getOP('makeactive');
+
+getUid();
+
+if($makeactive==3){
+    $query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
+    $query->bindParam(':cid', $cid);
+    $query->bindParam(':vers', $versid);
+    if(!$query->execute()) {
+            $error=$query->errorInfo();
+            $debug="Error updating entries\n".$error[2];
+    }	
+}
+
+?>
