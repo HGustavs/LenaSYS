2349469b60beed1a0f321e0cbb4d512e6dab30f9
a21jeaha <a21jeaha@student.his.se>
2023-05-25 10:19:17 +0200

    moved micro service from one another bransh to this one

---
+++ DuggaSys/microservices/courseedService/createMOTD_ms.php
@@ -0,0 +1,42 @@
+<?php
+/********************************************************************************
+
+   CreateMOTD_ms.php
+
+*********************************************************************************
+
+    This micro service is called upon when the message of the day is changed in the page associated with courseed.
+    The user must be a super user to be able to do this!
+
+-------------==============######## Documentation End ###########==============-------------
+*/
+
+
+date_default_timezone_set("Europe/Stockholm");
+// include ("../../../Shared/sessions.php");     
+include('../shared_microservices/getUid_ms.php');
+
+// Connect to database and start session.
+pdoConnect();
+session_start();
+
+$opt=getOP('opt');
+$motd=getOP('motd');
+$readonly=getOP('readonly');
+
+// Updates the message of the day 
+if(checklogin() && isSuperUser(getUid())) {
+
+    $query = $pdo->prepare("INSERT INTO settings (motd,readonly) VALUES (:motd, :readonly);");
+
+    $query->bindParam(':motd', $motd);
+    if($readonly == "UNK") {$readonly=0;}
+    $query->bindParam(':readonly', $readonly);
+
+    if(!$query->execute()) {
+        $error=$query->errorInfo();
+        $debug="Error updating entries\n".$error[2];
+    }
+}
+?>
+
--- DuggaSys/microservices/shared_microservices/getUid_ms.php
+++ DuggaSys/microservices/shared_microservices/getUid_ms.php
@@ -11,7 +11,7 @@
         if(isset($_SESSION['uid'])){
             $userid=$_SESSION['uid'];
         }else{
-            $userid="1";
+            $userid="guests";
         }
 
         $log_uuid = getOP('log_uuid');
