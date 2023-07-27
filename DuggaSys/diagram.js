d41b6f8e6b67f72b08d6c92eec662129b2219129
b21willu <102600930+b21willu@users.noreply.github.com>
2023-07-27 11:40:17 +0200

    Update diagram.js
    Changed comparison operator to see if diagramKeys.length is greater than or equal to 1 (because 1 diagram by default is saved).

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -13238,7 +13238,7 @@ function checkAndShowModal() {
         diagramKeys = Object.keys(localDiagrams);
     }
 
-    if (diagramKeys.length === 0) {
+    if (diagramKeys.length >= 1) {
         showModal();
     } else {
         console.log("You have no saves");
