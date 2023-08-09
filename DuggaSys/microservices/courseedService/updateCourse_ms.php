4dec2d291aae363e9f7bd4eeb7a5d299a943dd5b
a21oligu <oliver.gustafsson22@gmail.com>
2023-08-09 23:34:01 +0200

    fixed DB_HOST error

--- DuggaSys/microservices/courseedService/updateCourse_ms.php
+++ DuggaSys/microservices/courseedService/updateCourse_ms.php
@@ -8,6 +8,7 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
+include_once "../../../../coursesyspw.php";
 
 // Connect to database and start session
 pdoConnect();
