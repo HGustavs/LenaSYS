6d4731dc315d0fd53960f813367797d2f1da0e38
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-04-04 13:00:05 +0200

    Updated the CreateCourseVersion microservice

--- DuggaSys/microservices/courseedService/createCourseVersion_ms.php
+++ DuggaSys/microservices/courseedService/createCourseVersion_ms.php
@@ -1,5 +1,10 @@
 <?php
 
+//---------------------------------------------------------------------------------------------------------------
+// The microservice createCourseVersion_ms.php creates a new version of an existing course
+// by inserting a new row into the vers table
+//---------------------------------------------------------------------------------------------------------------
+
 date_default_timezone_set("Europe/Stockholm");
 include_once "../Shared/basic.php";
 include_once "../Shared/sessions.php";
@@ -8,8 +13,6 @@ include ('../shared_microservices/getUid_ms.php');
 // Connect to database and start session.
 pdoConnect();
 session_start();
-
-getUid();
 
 $opt=getOP('opt');
 $cid=getOP('cid');
@@ -20,19 +23,43 @@ $versname=getOP('versname');
 $versid=getOP('versid');
 $motd=getOP('motd');
 
-if(strcmp($opt,"NEWVRS")===0){
+
+if(checklogin() && isSuperUser(getUid()) == true) {
     $query = $pdo->prepare("INSERT INTO vers(cid,coursecode,vers,versname,coursename,coursenamealt,startdate,enddate,motd) values(:cid,:coursecode,:vers,:versname,:coursename,:coursenamealt,:startdate,:enddate,:motd);");
 
-    $query->bindParam(':cid', $cid);
-    $query->bindParam(':coursecode', $coursecode);
-    $query->bindParam(':vers', $versid);
-    $query->bindParam(':versname', $versname);
-    $query->bindParam(':coursename', $coursename);
-    $query->bindParam(':coursenamealt', $coursenamealt);
+	$query->bindParam(':cid', $cid);
+	$query->bindParam(':coursecode', $coursecode);
+	$query->bindParam(':vers', $versid);
+	$query->bindParam(':versname', $versname);
+	$query->bindParam(':coursename', $coursename);
+	$query->bindParam(':coursenamealt', $coursenamealt);
     $query->bindParam(':motd', $motd);
 
+    // if start and end dates are null, insert mysql null value into database
+	if($startdate=="null") $query->bindValue(':startdate', null,PDO::PARAM_INT);
+	else $query->bindParam(':startdate', $startdate);
+	if($enddate=="null") $query->bindValue(':enddate', null,PDO::PARAM_INT);
+	else $query->bindParam(':enddate', $enddate);
+
+	if(!$query->execute()) {
+		$error=$query->errorInfo();
+		$debug="Error inserting entries\n".$error[2];
+	} 
+
+    // if specified, sets the course as active
+    if($makeactive==3){
+		$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
+		$query->bindParam(':cid', $cid);
+		$query->bindParam(':vers', $versid);
+		if(!$query->execute()) {
+				$error=$query->errorInfo();
+				$debug="Error updating entries\n".$error[2];
+		}	
+	}
+
+	// Logging for create a fresh course version
+	$description=$cid." ".$versid;
+	logUserEvent($userid, $username, EventTypes::AddCourseVers, $description);
 }
 
-
-            
 ?>   
