8b93ecf7590b2bce7b9bbc83c4bf04f521917674
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-04-04 09:56:37 +0200

    initiated database connection

--- DuggaSys/microservices/courseedService/createCourseVersion_ms.php
+++ DuggaSys/microservices/courseedService/createCourseVersion_ms.php
@@ -1,8 +1,13 @@
 <?php
 
+date_default_timezone_set("Europe/Stockholm");
 include_once "../Shared/basic.php";
 include_once "../Shared/sessions.php";
 include ('../shared_microservices/getUid_ms.php');
+
+// Connect to database and start session.
+pdoConnect();
+session_start();
 
 getUid();
 
