d25ea9affd977b8978673d2dfe0d2b877cfe2397
c22emigr <c22emigr@student.his.se>
2024-04-04 13:42:36 +0200

    Created accessedservice_test.php
    created php-file for accessedservice test and included necessary files

---
+++ DuggaSys/tests/accessedservice_test.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../../Shared/test.php";   // Include the test file
+include_once "../../../coursesyspw.php";  // Include the logged in user
+
+$testsData = array(   // Test-data is saved on this array that is then tested in test.php file
+
+    
+
+
+
+)
+
+
+?>
