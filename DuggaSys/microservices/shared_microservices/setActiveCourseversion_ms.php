18879e8512b67f48bd57ba88d8ee36cbee487dbd
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-05 12:00:32 +0200

    connected relevant services and database

---
+++ DuggaSys/microservices/shared_microservices/setActiveCourseversion_ms.php
@@ -0,0 +1,12 @@
+<?php
+  date_default_timezone_set ("Europe/Stockholm");
+  //Bacis application services
+  include_once "../../../Shared/basic.php";
+  include_once "../../../Shared/sessions.php";
+  include_once "../shared_microservices/getUid_ms.php";
+  include_once "../shared_microservices/updateTableCourse.php";
+
+  //connect to database and start session
+  pdoConnect();
+  session_start();
+?>
