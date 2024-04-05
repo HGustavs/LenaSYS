063293edb69d95e0b00ace33ed70af681ae74045
c22emigr <c22emigr@student.his.se>
2024-04-05 15:20:11 +0200

    some correct data from documentation

--- DuggaSys/tests/accessedservice_test.php
+++ DuggaSys/tests/accessedservice_test.php
@@ -8,7 +8,7 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
     'create data' => array(  
         'expected-output' => '{"debug":"NONE!","writeaccess":true,"coursecode":"IT118G","coursename":"Webbutveckling - datorgrafik","entries":[{"qname":"Bitdugga1"},{"qname":"Bitdugga2"},{"qname":"colordugga1"},{"qname":"colordugga2"},{"qname":"linjedugga1"},{"qname":"linjedugga2"},{"qname":"dugga1"},{"qname":"dugga2"},{"qname":"Quiz"},{"qname":"Rapport"},{"qname":"HTML CSS Testdugga"},{"qname":"Clipping masking testdugga"},{"qname":"test"}]}',
         //'query-after-test-1' => "DELETE FROM quiz ORDER BY id DESC LIMIT 1",
-        'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',
+        'service' => 'https://cms.webug.se/root/G2/students/c21axepe/LenaSYS/DuggaSys/duggaedservice.php',    
         'service-data' => serialize(
             array(
                 // Data that service needs to execute function
@@ -21,16 +21,16 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
                 'username' => 'c123korv',
                 'addedtime',  // Ska denna vara med?
                 'val' => '3d-dugga',
-                'newusers' => '{&quot;deadline1&quot;:&quot;2023-05-02 0:0&quot;,&quot;comment1&quot;:&quot;&quot;,&quot;deadline2&quot;:&quot;2023-05-02 0:0&quot;,&quot;comment2&quot;:&quot;&quot;,&quot;deadline3&quot;:&quot;2023-05-03 0:0&quot;,&quot;comment3&quot;:&quot;&quot;}',
+                'newusers' => 'a22bagge',
                 'newclass' => 'webug',
                 'coursevers' => 'A1',
                 'teacher' => 'brom',
                 'vers' => '1',
                 'requestedpasswordchange' => '0',
                 'groups' => array(
-                    'groupval',
-                    'groupkind',
-                    'groupint',
+                    'groupval' => 'abc',
+                    'groupkind' => 'abcd',
+                    'groupint' => '2',
                 ), // ARRAY???
                 'gid' => '1',
                 'prop' => 'password',
