631c68bc51f00ab0be51954f4b0eee524affe0bc
f22linbe <f22linbe@student.his.se>
2024-04-10 17:29:49 +0200

    Created retrieveSectionedService_ms.php, no functions yet

---
+++ DuggaSys/microservices/sectionedService/retrieveSectionedService_ms.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "../../../Shared/basic.php";
+    function retrieveSectionedService($pdo, $ha, $debug, $writeAccess, $LastCourseCreated){//CHANGE HERE
+        // Include basic application services! Include more if needed
+        date_default_timezone_set("Europe/Stockholm");
+        include_once "../../../Shared/sessions.php";
+
+        pdoConnect();
+        // Retrieve Information
+        // functions will be added
+    }
