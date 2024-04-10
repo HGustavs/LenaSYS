d2beee1eb8fd385a6e1d3b48622fea5746f01b6a
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-10 14:15:26 +0200

    call updateTableListentries

--- DuggaSys/microservices/sectionedService/reorderListentries_ms.php
+++ DuggaSys/microservices/sectionedService/reorderListentries_ms.php
@@ -14,5 +14,19 @@
   $pos = getOP('pos');
   $moment = getOP('moment');
 
+  //Call the updateTableListentries service to update the pos & moment columns
 
+  $query = $pdo->prepare("CALL updateTableListentries (:courseid, :coursevers, :pos, :moment)");
+  $query->bindParam(':courseid, $courseid');
+  $query->bindParam(':coursevers, $coursevers');
+  $query->bindParam(':pos, $pos');
+  $query->bindParam(':moment, $moment');
+
+  if (!$query->execute()){
+    $error = $query->errorInfo();
+    $debug = "Error updating entries" . $error[2];
+  }
+
+  echo json_encode(array('courseid' => $courseid, 'coursevers' => $coursevers, 'pos' => $pos, 'moment' => $moment, 'debug' => $debug));
+  return;
 ?>
