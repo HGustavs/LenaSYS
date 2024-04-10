f15f85ebe3a8799e327ae4cb7a5600319c09c1f3
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-10 14:17:38 +0200

    fixed fail

--- DuggaSys/microservices/sectionedService/reorderListentries_ms.php
+++ DuggaSys/microservices/sectionedService/reorderListentries_ms.php
@@ -1,7 +1,7 @@
 <?php
   date_default_timezone_set("Europe/Stockholm");
 
-  include_once "../Shared/session.php";
+  include_once "../Shared/sessions.php";
   include_once "../Shared/basic.php";
   include_once "../Shared/microservices/getUid_ms.php";
 
