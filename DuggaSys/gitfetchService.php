3e7ebcc12cab2be5def1542abd4e3086421a928d
a22oscja <a22oscja@student.his.se>
2024-04-11 14:40:24 +0200

    test this plz

--- DuggaSys/gitfetchService.php
+++ DuggaSys/gitfetchService.php
@@ -79,7 +79,7 @@ function insertToMetaData($cid, $item)
     global $pdoLite;
     $query = $pdoLite->prepare('INSERT INTO gitFiles (cid, fileName, fileType, fileURL, downloadURL, fileSHA, filePath) VALUES (:cid, :fileName, :fileType, :fileURL, :downloadURL, :fileSHA, :filePath)');
     $query->bindParam(':cid', $cid);
-    print_r("name = ".$item['name']."type = ".$item['type']."url = ".$item['url']."download_url = ".$item['download_url']."sha = ".$item['sha']."path = ".$item['path']);
+    print_r("\n ---name = ".$item['name']."\n ----type = ".$item['type']."\n ---url = ".$item['url']."\n ---download_url = ".$item['download_url']."\n ---sha = ".$item['sha']."\n ---path = ".$item['path']."\n\n");
     $query->bindParam(':fileName', $item['name']);
     $query->bindParam(':fileType', $item['type']);
     $query->bindParam(':fileURL', $item['url']);
@@ -176,6 +176,7 @@ function bfs($url, $cid, $opt)
                     // Checks if the fetched item is of type 'file'
                     if ($item['type'] == 'file') {
                         //If an index file has been found, check against the content of the index file
+                        if($item['name'] != ".gitignore"){
                         if($filesToIgnore){
                             //If file is not part of files to ignore, resume (Index file)
                             if(!in_array($item['name'], $filesToIgnore)){
@@ -184,8 +185,7 @@ function bfs($url, $cid, $opt)
                                 }
                                 else if($opt == "DOWNLOAD") {
                                     insertToFileLink($cid, $item);
-                                    //work in progress on these two
-                                    //insertToMetaData($cid, $item);
+                                    insertToMetaData($cid, $item);
                                     //downloadToWebserver($cid, $item);  
                                 }                
                             }
@@ -196,11 +196,11 @@ function bfs($url, $cid, $opt)
                                 }
                                 else if($opt == "DOWNLOAD") {
                                     insertToFileLink($cid, $item);
-                                    //work in progress on these two
-                                    //insertToMetaData($cid, $item);
+                                    insertToMetaData($cid, $item);
                                     //downloadToWebserver($cid, $item);  
                                 }      
                         }
+                    }
                         // Checks if the fetched item is of type 'dir'
                     } else if ($item['type'] == 'dir') {
                         if (!in_array($item['url'], $visited)) {
