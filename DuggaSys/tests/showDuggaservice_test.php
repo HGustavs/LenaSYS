fc5d6d03e681691d9af10c2ada9604d40eee027d
Albin <aljo940616@gmail.com>
2024-04-11 10:25:19 +0200

    comment out all tests to have them be fixed in there own issues

--- DuggaSys/tests/showDuggaservice_test.php
+++ DuggaSys/tests/showDuggaservice_test.php
@@ -4,47 +4,47 @@ include "../../Shared/test.php";
 /*Warning! No test works since showDugga doesn't work properly. Expected outputs in this document have not been fixed yet.*/
 
 $testsData = array(
-    'Get active users' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO groupdugga(hash, active_users) VALUES ('hnf3j58s', 5);",
-        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hnf3j58s';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-        'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' => 'UPDATEAU',
-            'hash' => 'hnf3j58s'
-        )),
-        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
-        )),
-    ),
-    'Create active users' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        //Get newly added active users?
-        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'tj7dh2nb';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-        'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' => 'UPDATEAU',
-            'hash' => 'tj7dh2nb',
-            'AUtoken' => '999'
-        )),
-        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
-        )),
-    ),
-    'update active users' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 52);",
-        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-        'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' => 'UPDATEAU',
-            'hash' => 'hjk4ert6',
-            'AUtoken' => '9999'
-        )),
-        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
-        )),
-    ),
+    // 'Get active users' => array(
+    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+    //     'query-before-test-1' => "INSERT INTO groupdugga(hash, active_users) VALUES ('hnf3j58s', 5);",
+    //     'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hnf3j58s';",
+    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+    //     'service-data' => serialize(array( // Data that service needs to execute function
+    //         'opt' => 'UPDATEAU',
+    //         'hash' => 'hnf3j58s'
+    //     )),
+    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+    //         'none'
+    //     )),
+    // ),
+    // 'Create active users' => array(
+    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+    //     //Get newly added active users?
+    //     'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'tj7dh2nb';",
+    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+    //     'service-data' => serialize(array( // Data that service needs to execute function
+    //         'opt' => 'UPDATEAU',
+    //         'hash' => 'tj7dh2nb',
+    //         'AUtoken' => '999'
+    //     )),
+    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+    //         'none'
+    //     )),
+    // ),
+    // 'update active users' => array(
+    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+    //     'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 52);",
+    //     'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
+    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+    //     'service-data' => serialize(array( // Data that service needs to execute function
+    //         'opt' => 'UPDATEAU',
+    //         'hash' => 'hjk4ert6',
+    //         'AUtoken' => '9999'
+    //     )),
+    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+    //         'none'
+    //     )),
+    // ),
     // 'get data from userAnswer (student)' => array(
     //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
     //     'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
@@ -89,53 +89,53 @@ $testsData = array(
     //         'none'
     //     )),
     // ),
-    'update submitted dugga' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
-        'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES(9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
-        'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
-        'variables-query-before-test-4' => "moment",
-        'query-before-test-4' => "INSERT INTO userAnswer(cid,hash,password,moment) VALUES(9999, 'dfg4zxc5', 'asdfasdf', ?);",
-        'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
-        'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
-        'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-        'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' => 'SAVDU',
-            'hash' => 'dfg4zxc5',
-            'haspwd' => 'asdfasdf',
-            'answer' => 'im updated',
-            'moment' => '<!query-before-test-3!><*[0]["listentries"]*>'
-        )),
-        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
-        )),
-    ),
-    'submit dugga' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
-        'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES (9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
-        'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
-        'variables-query-before-test-4' => "moment",
-        'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
-        'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
-        'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-        'service-data' => serialize(array( // Data that service needs to execute function
-            'opt' => 'SAVDU',
-            'cid' => '9999',
-            'coursevers' => '52432',
-            'duggaid' => '1',
-            'moment' => '<!query-before-test-3!><*[0]["listentries"]*>',
-            'variant' => '3',
-            'hash' => 'ghj1ghj2',
-            'haspwd' => 'asddasdd',
-            'answer' => 'NULL'
-        )),
-        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
-        )),
-    ),
+    // 'update submitted dugga' => array(
+    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+    //     'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
+    //     'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES(9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
+    //     'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
+    //     'variables-query-before-test-4' => "moment",
+    //     'query-before-test-4' => "INSERT INTO userAnswer(cid,hash,password,moment) VALUES(9999, 'dfg4zxc5', 'asdfasdf', ?);",
+    //     'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
+    //     'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
+    //     'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
+    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+    //     'service-data' => serialize(array( // Data that service needs to execute function
+    //         'opt' => 'SAVDU',
+    //         'hash' => 'dfg4zxc5',
+    //         'haspwd' => 'asdfasdf',
+    //         'answer' => 'im updated',
+    //         'moment' => '<!query-before-test-3!><*[0]["listentries"]*>'
+    //     )),
+    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+    //         'none'
+    //     )),
+    // ),
+    // 'submit dugga' => array(
+    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+    //     'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
+    //     'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES (9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
+    //     'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
+    //     'variables-query-before-test-4' => "moment",
+    //     'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
+    //     'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
+    //     'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
+    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+    //     'service-data' => serialize(array( // Data that service needs to execute function
+    //         'opt' => 'SAVDU',
+    //         'cid' => '9999',
+    //         'coursevers' => '52432',
+    //         'duggaid' => '1',
+    //         'moment' => '<!query-before-test-3!><*[0]["listentries"]*>',
+    //         'variant' => '3',
+    //         'hash' => 'ghj1ghj2',
+    //         'haspwd' => 'asddasdd',
+    //         'answer' => 'NULL'
+    //     )),
+    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+    //         'none'
+    //     )),
+    // ),
     /* This test doesn't work.
     'super-view data from useranswer on hash (teacher)' => array(
         'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
