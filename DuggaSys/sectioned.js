9216eeec5e2a25fc86d2ba4d243cbd78e5a041a4
a22karja <a22karja@student.his.se>
2024-04-11 09:13:18 +0200

    Bug fix #14444
    Fixed bug where deleted elements would reaper if multiple elemetns were deleted at the same time

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -1949,6 +1949,11 @@ function returnedSection(data) {
 
 
   }
+  
+  //Force elements that are deletet to not show up unless pressing undo delete or reloading the page
+  for(var i = 0; i < delArr.length; i++){
+    document.getElementById("lid"+delArr[i]).style.display="none";
+  }
 
   // Reset checkboxes
   // Prevents a bug if they are checked when for example an item is deleted and the table refreshes
@@ -1987,10 +1992,7 @@ function returnedSection(data) {
     addClasses();
     showMOTD();
   }
-    //Force elements that are deletet to not show up unless pressing undo delete or reloading the page
-    for(var i = delArr.length-1; i == 0; --i){
-      document.getElementById("lid"+delArr[i]).style.display="none";
-    }
+
 }
 
 function openCanvasLink(btnobj) {
