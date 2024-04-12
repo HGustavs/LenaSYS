f5e4ba4594920a9d16fc01d59f02db61c6f366ec
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-12 13:38:52 +0200

    added a consol

--- DuggaSys/tests/showDuggaservice_test.php
+++ DuggaSys/tests/showDuggaservice_test.php
@@ -164,13 +164,14 @@ $testsData = array(
         'query-after-test-2' => "DELETE FROM course WHERE cid = 9999;",
         'service' => 'localhost/LenaSYS/DuggaSys/showDuggaservice.php',
         'service-data' => serialize(array( // Data that service needs to execute function
-            'username' => 'stei',
+            'username' => 'knas',
             'password' => 'password',
             'moment' => '<!query-before-test3!> <*[0][listentries]*>'
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'none'
         )),
+        'console-output' => 'echo "This is a message printed to the console.";'
     ),
     'super-view data on quizname (teacher)' => array(
         'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
