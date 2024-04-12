f91175ca3459d9fd175c707014a8f4bc4115bea2
a22marca <a22marca@student.his.se>
2024-04-12 09:25:38 +0200

    version of the microservice that performs the database query without the use of the function from basic.php

--- DuggaSys/microservices/shared_microservices/logServiceEvent_ms.php
+++ DuggaSys/microservices/shared_microservices/logServiceEvent_ms.php
@@ -2,8 +2,8 @@
 
 /* 
  * logServiceEvent Microservice
- * Typically it can be used to log when a service starts and when it ends by adding
  * EventTypes::ServiceServerStart or EventTypes::ServiceServerEnd as the eventType parameter
+ * to log a service start or end
  * */
 
 include_once ("../../../Shared/basic.php");
@@ -16,51 +16,57 @@ $eventType=getOP('eventtype');
 $service=getOP('service');
 $info=getOP('info');
 $timestamp=getOP('timestamp');
-$userid=getUid();
+$userid=getOP('userid');
 
-// Only for testing purposes, remove before integration
-echo "UUID: " . $uuid;
-echo "eventType: " . $eventType;
-echo "Service: " . $service;
-echo "Info: " . $info;
-echo "timestamp: " . $timestamp;
-echo "userid: " . $userid;
-
-if (is_null($timestamp)) {
+if (strcmp($timestamp,"UNK")==0) {
 	$timestamp = round(microtime(true) * 1000);
 }
 
-$query = $GLOBALS['log_db']->prepare('INSERT INTO serviceLogEntries (uuid, eventType, service, timestamp, userAgent, operatingSystem, browser, userid, info, referer, IP) VALUES (:uuid, :eventType, :service, :timestamp, :userAgent, :operatingSystem, :browser, :userid, :info, :referer, :IP)');
-$query->bindParam(':uuid', $uuid);
-$query->bindParam(':eventType', $eventType);
-$query->bindParam(':service', $service);
-$query->bindParam(':timestamp', $timestamp);
-$query->bindParam(':userid', $userid);
-$query->bindParam(':info', $info);
-$referer="";
+try {
+	$db_path = "../../../log/loglena6.db";
+	$log_db = new PDO('sqlite:'.$db_path);
+    $log_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	try {
+		// Execute the statement
+		$query = $log_db->prepare('INSERT INTO serviceLogEntries (id,uuid, eventType, service, timestamp, userAgent, operatingSystem, browser, userid, info, referer, IP) VALUES (:id,:uuid, :eventType, :service, :timestamp, :userAgent, :operatingSystem, :browser, :userid, :info, :referer, :IP)');
+		$query->bindParam(':id',$id);
+		$query->bindParam(':uuid', $uuid);
+		$query->bindParam(':eventType', $eventType);
+		$query->bindParam(':service', $service);
+		$query->bindParam(':timestamp', $timestamp);
+		$query->bindParam(':userid', $userid);
+		$query->bindParam(':info', $info);
+		$referer="";
 
-if(isset($_SERVER['HTTP_REFERER'])){
-	$referer.=$_SERVER['HTTP_REFERER'];
+		if(isset($_SERVER['HTTP_REFERER'])){
+			$referer.=$_SERVER['HTTP_REFERER'];
+		}
+
+		$IP="";
+		if(isset($_SERVER['REMOTE_ADDR'])){
+			$IP.=$_SERVER['REMOTE_ADDR'];
+		}
+		if(isset($_SERVER['HTTP_CLIENT_IP'])){
+			$IP.=" ".$_SERVER['HTTP_CLIENT_IP'];
+		}
+		if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+			$IP.=" ".$_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+
+		$query->bindParam(':referer', $referer);
+		$query->bindParam(':IP', $IP);
+		$query->bindParam(':userAgent', $_SERVER['HTTP_USER_AGENT']);
+		$currentOS = getOS();
+		$query->bindParam(':operatingSystem', $currentOS);
+		$currentBrowser = getBrowser();
+		$query->bindParam(':browser', $currentBrowser);
+		$query->execute();
+	} catch (PDOException $e) {
+		echo "error executing query: " . $e->getMessage();
+	}
+} catch(PDOException $e) {
+	echo "error declaring pdo";
 }
 
-$IP="";
-if(isset($_SERVER['REMOTE_ADDR'])){
-	$IP.=$_SERVER['REMOTE_ADDR'];
-}
-if(isset($_SERVER['HTTP_CLIENT_IP'])){
-	$IP.=" ".$_SERVER['HTTP_CLIENT_IP'];
-}
-if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
-	$IP.=" ".$_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-
-$query->bindParam(':referer', $referer);
-$query->bindParam(':IP', $IP);
-$query->bindParam(':userAgent', $_SERVER['HTTP_USER_AGENT']);
-$currentOS = getOS();
-$query->bindParam(':operatingSystem', $currentOS);
-$currentBrowser = getBrowser();
-$query->bindParam(':browser', $currentBrowser);
-$query->execute();
 
 ?>
