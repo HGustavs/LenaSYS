075cb2ebb3c8a27d6ad175258105f08d71577d5b
c22emigr <c22emigr@student.his.se>
2024-04-15 16:16:12 +0200

    nope it is not used by our function

--- DuggaSys/microservices/courseedService/changeActiveCourseVersion_courseed_ms.php
+++ DuggaSys/microservices/courseedService/changeActiveCourseVersion_courseed_ms.php
@@ -1,7 +1,7 @@
 <?php
 
 //---------------------------------------------------------------------------------------------------------------
-// The microservice changeActiveVersion_ms.php takes an existing course and changes the activeversion
+// The microservice changeActiveVersion_ms.php takes an existing course and changes content of the activeversion column
 //---------------------------------------------------------------------------------------------------------------
 
 // Set the correct time zone
@@ -10,6 +10,7 @@ date_default_timezone_set("Europe/Stockholm");
 // Basic includes for this ms to work
 include_once "../Shared/basic.php";
 include_once "../Shared/sessions.php";
+include ('../shared_microservices/getUid_ms.php');
 
 
 
@@ -27,19 +28,8 @@ $vers=getOP('vers');
 // Login is checked for function to run
 if(checklogin() && isSuperUser(getUid()) == true) {
 
+
 if(strcmp($opt,"CHGVERS")===0) {
-
-
-	if ($makeactive == 3) {
-		$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
-		$query->bindParam(':cid', $cid);
-		$query->bindParam(':vers', $versid);
-		if (!$query->execute()) {
-			$error = $query->errorInfo();
-			$debug = "Error updating entries\n" . $error[2];
-		}
-	}
-	
 
 		$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
 		$query->bindParam(':cid', $courseid);
