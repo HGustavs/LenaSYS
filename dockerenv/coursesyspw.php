e0c88e888eda20c13744c5c04ae6ee488f86c0de
LindholmLabs <william_lindholm@outlook.com>
2024-04-15 10:02:14 +0200

    Moved dockerenv config from personal repo.

---
+++ dockerenv/coursesyspw.php
@@ -0,0 +1,7 @@
+<?php
+          define("DB_USER","lenasys");
+          define("DB_PASSWORD","password");
+          define("DB_HOST","db");
+          define("DB_NAME","lenasysdb");
+          define("DB_USING_DOCKER","on");
+        ?>
