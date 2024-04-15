cd1866f0bae5e2b9fd1b8b537b66c91ac6e0fc30
Albin <aljo940616@gmail.com>
2024-04-15 20:20:02 +0200

    steal code from a22melst issue 14573

--- DuggaSys/microservices/sectionedService/deleteListentries_ms.php
+++ DuggaSys/microservices/sectionedService/deleteListentries_ms.php
@@ -15,8 +15,16 @@
     if (checklogin()) { //This entire checklogin should be working by using the getUid instead, but for the time being it doesn't.
         if(isSuperUser($userid)) {
             $sectid=getOP('lid');
+            // Delete foreign key references
+            $query = $pdo->prepare("DELETE FROM useranswer WHERE moment=:lid");
+            $query->bindParam(':lid', $sectid);
 
-            // Now delete the row from the listentries table
+            if(!$query->execute()) {
+                if($query->errorInfo()[0] == 23000) {
+                    $debug = "The item could not be deleted.";
+                }
+            }
+            // Delete the listentry
             $query = $pdo->prepare("DELETE FROM listentries WHERE lid = :lid");
             $query->bindParam(':lid', $sectid);
             if (!$query->execute()) {
