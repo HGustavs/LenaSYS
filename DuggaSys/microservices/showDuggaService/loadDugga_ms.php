1b4d9956ec86a96052b1ad8e285c5e915fa627e4
a22marca <a22marca@student.his.se>
2024-04-16 08:50:40 +0200

    removed unnecessary echo

--- DuggaSys/microservices/showDuggaService/loadDugga_ms.php
+++ DuggaSys/microservices/showDuggaService/loadDugga_ms.php
@@ -29,34 +29,34 @@ $newcourseid=array();
 $newcoursevers=array();
 $newduggaid=array();
 
-    if($hash!="UNK"){
-        $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE hash=:hash";
+if($hash!="UNK"){
+    $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE hash=:hash";
+    $query = $pdo->prepare($sql);
+    $query->bindParam(':hash', $hash);
+    $result = $query->execute();
+    $rows = $query->fetchAll();
+
+    //if the hash didn't work then retrive all answers for that moment
+    if($rows == NULL){
+        //changed WHERE key to moment instead of hash since hash isn't working correctly. It appears to work so long as their is an entry for that moment in userAnswer
+        $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE moment=:moment";
         $query = $pdo->prepare($sql);
-        $query->bindParam(':hash', $hash);
-        $result = $query->execute();
+        $query->bindParam(':moment', $moment);
+        $query->execute();
         $rows = $query->fetchAll();
-
-        //if the hash didn't work then retrive all answers for that moment
-        if($rows == NULL){
-            //changed WHERE key to moment instead of hash since hash isn't working correctly. It appears to work so long as their is an entry for that moment in userAnswer
-            $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE moment=:moment";
-            $query = $pdo->prepare($sql);
-            $query->bindParam(':moment', $moment);
-            $query->execute();
-            $rows = $query->fetchAll();
-        }
-
-        foreach($rows as $row){
-            $variant=$row['vid'];
-            $answer=$row['useranswer'];
-            $variantanswer=html_entity_decode($row['variantanswer']);
-            $param=html_entity_decode($row['param']);
-            $newcourseid=$row['cid'];
-            $newcoursevers=$row['vers'];
-            $newduggaid=$row['quiz'];
-        }
-
     }
+
+    foreach($rows as $row){
+        $variant=$row['vid'];
+        $answer=$row['useranswer'];
+        $variantanswer=html_entity_decode($row['variantanswer']);
+        $param=html_entity_decode($row['param']);
+        $newcourseid=$row['cid'];
+        $newcoursevers=$row['vers'];
+        $newduggaid=$row['quiz'];
+    }
+
+}
 
 echo json_encode(
     array(
