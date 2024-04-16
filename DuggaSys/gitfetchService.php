0f4f7d88681ef0d473afda982b39b5da8aa8cb64
a22natth <a22natth@student.his.se>
2024-04-16 09:24:31 +0200

    Comments added
    Comments were added to the code to increase understandability.

--- DuggaSys/gitfetchService.php
+++ DuggaSys/gitfetchService.php
@@ -93,6 +93,7 @@ function downloadToWebServer($cid, $item)
 {
     // Retrieves the contents of each individual file based on the fetched "download_url"
     $fileContents = file_get_contents($item['download_url']);
+    // If unable to get file contents then it is logged into the specified textfile
     if ($fileContents === false) {
         $message = "\n" . date("Y-m-d H:i:s",time()) . " - Error: Failed to get file contents of " . $item['name'] . " from " . $item['download_url'] . "\n";
         $file = '../../LenaSYS/log/gitErrorLog.txt';
@@ -105,6 +106,7 @@ function downloadToWebServer($cid, $item)
     } 
 
     $content = file_put_contents($path, $fileContents);
+    // If unable to get file contents then it is logged into the specified textfile
     if ($content === false) {
         $message = "\n" . date("Y-m-d H:i:s",time()) . " - Error: Failed to put " . $item['name'] . " in " . $path . "\n";
         $file = '../../LenaSYS/log/gitErrorLog.txt';
@@ -176,6 +178,8 @@ function bfs($url, $cid, $opt)
         $context = stream_context_create($opts);
         // Fetches the data with the stream included
         $data = @file_get_contents($currentUrl, true, $context);
+        // If unable to get file contents then it is logged into the specified textfile with
+        // the specific http error code
         if($data === false || !$data) {
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_USERAGENT, 'curl/7.48.0');
