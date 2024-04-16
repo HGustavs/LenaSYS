0482cebf3ed4bb1b7ae9b1fd1209a65c5db37117
a22marca <a22marca@student.his.se>
2024-04-16 17:40:36 +0200

    added pdoConnect and session_start function calls

--- DuggaSys/microservices/sectionedService/updateListentriesTabs_ms.php
+++ DuggaSys/microservices/sectionedService/updateListentriesTabs_ms.php
@@ -4,6 +4,10 @@ date_default_timezone_set("Europe/Stockholm");
 include_once ('../../../Shared/sessions.php');
 include_once "../../../Shared/basic.php";
 
+// Connect to database and start session.
+pdoConnect();
+session_start();
+
 $opt=getOP('opt');
 $sectid=getOP('lid');
 $tabs=getOP('tabs');
