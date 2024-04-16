f30942dd85c7a7a108f5499add80c8a78a292432
a22samka <129301680+a22samka@users.noreply.github.com>
2024-04-16 14:33:12 +0200

    Fixed so that darkmode works with popup
    Added CSS and html so that the popup window changes depending on dark mode or not.

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -144,8 +144,8 @@ if(isset($_SESSION['uid'])){
 				<button id="deleteCourseButton" onclick='openForm()'>Delete</button>
     			<input id='saveCourse' class='submit-button' type='button' value='Save' title='Save changes' onclick="validateForm('editCourse')" />
 					<div class="form-popup" id="myForm">
-					<form action="" class="form-container">
-						<h1 style="font-size:20px; padding-bottom:20px; color:#434343; border-bottom: 2px solid #ccc;">Are you sure? </h1>
+					<form action="" class="form-container DarkModeBackgrounds">
+						<h1 Class="DarkModeText" style="font-size:24px; padding-bottom:50px; padding-top:60px; border-bottom: 2px solid #ccc;">Are you sure? </h1>
 
 
 
