e58f9786d94cbb08dc62476422e6dc12480ddf18
a22danwe <a22danwe@student.his.se>
2024-04-16 15:57:44 +0200

    Changed mounts and file permissions.

--- dockerenv/init.sh
+++ dockerenv/init.sh
@@ -6,9 +6,13 @@ if ! id "www-data" &>/dev/null; then
     useradd -u 33 -g www-data -s /bin/false www-data
 fi
 
-chown www-data:1000 /var/www/html/coursesyspw.php
+# Set appropriate user rights
+umask 000
+chown www-data:www-data /var/www/html/coursesyspw.php
 mkdir /var/www/html/githubMetadata
+chown www-data:www-data /var/www/html/LenaSYS
 chown -R www-data:www-data /var/www/html/githubMetadata
+chmod -R 777 /var/www/html
 
 git config core.fileMode false
 
