80789400f2e39b59dfd209ae50578e3db01af3d9
Albin <aljo940616@gmail.com>
2024-04-16 09:35:55 +0200

    create folder for showDuggaservice micro services and file for updateActiveUsers

---
+++ DuggaSys/microservices/showDuggaservice/updateActiveUsers_ms.php
@@ -0,0 +1,10 @@
+### updateActiveUsers
+Collect active users
+Uses service __selectFromTableUserAnswar__ to _get_ information it requires from __userAnswer__.
+<br>
+
+Uses service __insertIntoTableGroupdugga__ to makes _inserts_ into the table __groupdugga__.
+Uses the services __updateTableGroupdugga__ to change the content of these columns:
+- active_users
+
+<br>
