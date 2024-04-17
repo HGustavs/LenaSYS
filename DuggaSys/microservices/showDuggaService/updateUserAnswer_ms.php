2568cffab15e0907e5e7220463bf8f12a0509efe
a22eriwi <a22eriwi@student.his.se>
2024-04-17 13:16:06 +0200

    added paths

--- DuggaSys/microservices/showDuggaService/updateUserAnswer_ms.php
+++ DuggaSys/microservices/showDuggaService/updateUserAnswer_ms.php
@@ -1,6 +1,10 @@
 <?php
 
+include_once "../../../Shared/sessions.php";
+include_once "../../../Shared/basic.php";
 
+pdoConnect(); // Connect to database and start session
+session_start();
 
 if(isset($dbpwd) && strcmp($hashpwd,$dbpwd) === 0){
 	$query = $pdo->prepare("UPDATE userAnswer SET submitted=NOW(), useranswer=:useranswer, timesSubmitted=timesSubmitted+1 WHERE hash=:hash AND password=:hashpwd;");
