3f68bc45ee0e5f461ff47420264c6f2e7d8b0d68
a22eriwi <a22eriwi@student.his.se>
2024-04-17 14:21:10 +0200

    opt SAVDU strcmp

--- DuggaSys/microservices/showDuggaService/updateUserAnswer_ms.php
+++ DuggaSys/microservices/showDuggaService/updateUserAnswer_ms.php
@@ -16,21 +16,26 @@ $answer=getOP('answer');
 $hash=getOP('hash');
 $hashpwd=getOP('password');
 
-if(checklogin() && isSuperUser(getUid()) == true) {
+if(strcmp($opt,"SAVDU")==0){
+	// Log the dugga write
+	makeLogEntry($userid,2,$pdo,$courseid." ".$coursevers." ".$duggaid." ".$moment." ".$answer);
+	$discription = $courseid." ".$duggaid." ".$moment." ".$answer;
+	if(checklogin() && isSuperUser(getUid()) == true) {
 
-	if(isset($grade)&&($grade > 1)){
-		//if grade equal G, VG, 3, 4, 5, or 6
-		$debug="You have already passed this dugga. You are not required/allowed to submit anything new to this dugga.";
-	}
-	else {
-		if(isset($dbpwd) && strcmp($hashpwd,$dbpwd) === 0){
-			$query = $pdo->prepare("UPDATE userAnswer SET submitted=NOW(), useranswer=:useranswer, timesSubmitted=timesSubmitted+1 WHERE hash=:hash AND password=:hashpwd;");
-			$query->bindParam(':hash', $hash);
-			$query->bindParam(':hashpwd', $hashpwd);
-			$query->bindParam(':useranswer', $answer);
-			if(!$query->execute()) {
-				$error=$query->errorInfo();
-				$debug="Error updating variant (row ".__LINE__.") Error code: ".$error[2];
+		if(isset($grade)&&($grade > 1)){
+			//if grade equal G, VG, 3, 4, 5, or 6
+			$debug="You have already passed this dugga. You are not required/allowed to submit anything new to this dugga.";
+		}
+		else {
+			if(isset($dbpwd) && strcmp($hashpwd,$dbpwd) === 0){
+				$query = $pdo->prepare("UPDATE userAnswer SET submitted=NOW(), useranswer=:useranswer, timesSubmitted=timesSubmitted+1 WHERE hash=:hash AND password=:hashpwd;");
+				$query->bindParam(':hash', $hash);
+				$query->bindParam(':hashpwd', $hashpwd);
+				$query->bindParam(':useranswer', $answer);
+				if(!$query->execute()) {
+					$error=$query->errorInfo();
+					$debug="Error updating variant (row ".__LINE__.") Error code: ".$error[2];
+				}
 			}
 		}
 	}
