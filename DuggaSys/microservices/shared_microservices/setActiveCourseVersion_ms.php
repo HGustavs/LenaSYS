454dd6ee5b83013b74e0f7f870399bcc295dfe3c
Erkan <a22eriwi@student.his.se>
2024-04-18 15:48:47 +0200

    Implemented if statement

--- DuggaSys/microservices/shared_microservices/setActiveCourseVersion_ms.php
+++ DuggaSys/microservices/shared_microservices/setActiveCourseVersion_ms.php
@@ -3,3 +3,17 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
+
+
+if (strcmp($opt, "UPDATEVRS") === 0) {
+    if ($makeactive == 3) {
+        $query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
+        $query->bindParam(':cid', $courseid);
+        $query->bindParam(':vers', $versid);
+
+        if (!$query->execute()) {
+            $error = $query->errorInfo();
+            $debug = "Error updating entries\n" . $error[2];
+        }
+    }
+}
