52ce0ea3851e0459f39fb1fdced54002368a03fa
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-18 12:28:00 +0200

    included timezone and database

---
+++ DuggaSys/microservices/showDuggaService/createDuggaVariant_ms.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set("Europe/Stockholm");
+
+include_once "../../../Shared/sessions.php";
+include_once "../../../Shared/basic.php";
+
+// Connect to database and start session
+pdoConnect();
+session_start();
+
+
+?>
