f888a3b7c71d37fe809ce2d9658924da1281ed99
a22alfna <a22alfna@student.his.se>
2024-04-19 14:56:32 +0200

    Update diagram.php

--- DuggaSys/diagram.php
+++ DuggaSys/diagram.php
@@ -1024,7 +1024,7 @@
                 <button id="darkmodeToggle" class="saveButton" onclick="toggleDarkmode()">Darkmode</button><br><br>
                 <button id="diagramDropDownToggle" class="saveButton" onclick="toggleDiagramDropdown()">Example diagrams </button><br><br>
                 <div class="dropdownContent">
-                    <select id="diagramTypeDropdown">
+                    <select id="diagramTypeDropdown" onchange="checkDropdownOption()">
 			<option value="JSON/EMPTYDiagramMockup.json">Empty board</option>
                         <option value="JSON/StateChartDiagramMockup.json">State chart diagrams</option>
                         <option value="JSON/IEDiagramMockup.json">IE diagrams</option>
