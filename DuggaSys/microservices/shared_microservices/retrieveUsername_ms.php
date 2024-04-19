26cb8231df6f44efa28ac77551ee4d770d30cbd4
Erkan <a22eriwi@student.his.se>
2024-04-19 15:35:22 +0200

    checklogin function added

--- DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
+++ DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
@@ -5,9 +5,17 @@ $query->bindParam(':uid', $userid);
 $query->execute();
 
 // This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-	$username = $row['username'];
+if(checklogin()){
+	if(isset($_SESSION['uid'])){
+		$userid=$_SESSION['uid'];
+		$hasread=hasAccess($userid, $courseid, 'r');
+		$studentTeacher=hasAccess($userid, $courseid, 'st');
+		$haswrite=hasAccess($userid, $courseid, 'w');
+	}else{
+		$userid="guest";
+	}
+	while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+		$username = $row['username'];
+	}
 }
-
-
 ?>
