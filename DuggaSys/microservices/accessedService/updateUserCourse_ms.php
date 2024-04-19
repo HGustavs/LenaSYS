db8fbb919b2486086b153d96f7ee0b1f3bebe634
c22mohel <c22mohel@student.his.se>
2024-04-19 13:36:29 +0200

    makes sure the user has access

--- DuggaSys/microservices/accessedService/updateUserCourse_ms.php
+++ DuggaSys/microservices/accessedService/updateUserCourse_ms.php
@@ -1,1 +1,43 @@
-<?php?>
+<?php
+
+date_default_timezone_set("Europe/Stockholm");
+// Include basic application services!
+include_once "../Shared/sessions.php";
+include_once "../Shared/basic.php";
+// connect to database
+pdoConnect();
+session_start();
+// get userId and courseId
+$userid = getUid(); 
+$cid = getOP('courseid');
+
+
+if (hasAccess($userid, $cid, 'w') || isSuperUser($userid)) {
+	$hasAccess = true;
+} else {
+	$hasAccess = false;
+} 
+
+
+if(checklogin() && $hasAccess) {
+
+
+}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
