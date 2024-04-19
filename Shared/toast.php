cf1614b664d13bf7f7d16bb1bf7ff0bd102b1d03
a22moamy <a22moamy@student.his.se>
2024-04-19 11:11:22 +0200

    Adjusted function to create toast
    toast() function now takes three parameters to create a toast (instead of getting the right values from some test inputs). The toast css is currently added in style.css and blackTheme.css for testing.

--- Shared/toast.php
+++ Shared/toast.php
@@ -1,159 +1,137 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toast TESTING</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script>
-        //let toastAmount = 0;
+<style>
+    /* Toast notifications */
+    #snackbar {
+    visibility: hidden; 
+    min-width: 200px; 
+    max-width: 300px;
+    max-height: 150px;
+    margin-right: 10px; 
+    border-radius: 10px; 
+    padding: 15px;
 
-        function closeToast(){
-            snackbar.className = snackbar.className.replace("show", "");
-        }
-        function myFunction() {
+    text-align: center; 
 
-            // toastAmount++;
-            //console.log("toastAmount: "+toastAmount);
+    position: fixed;
+    right: 5%;
+    top: 20px;
+    z-index: 8001; 
+    /* light mode */
+    background-color: #fecc56;
+    /*  Darkmode background color: #e9e9e9 */
+    color: #000; 
 
-            // Get the snackbar DIV
-            let snackbar = document.getElementById("snackbar");
-            let snackbarText = document.getElementById("snackbarText");
-            let typeIcon = document.getElementById("typeIcon");
-            let duration = document.getElementById("duration").value;
-            let type = document.getElementById("type").value;
-            let text = document.getElementById("text").value;
-        
-            // Changes the toast text to the input text
-            snackbarText.innerHTML = text;
+    display: grid;
+    grid-template-areas: 
+    'icon type close'
+    'text text text';
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.3), 0 5px 20px 0 rgba(0, 0, 0, 0.3);
+    }
 
-            // Decides the type of toast (changes the type icon)
-            switch(type){
-                case "warning":
-                    typeIcon.innerHTML = "warning";
-                    break;
-                case "success":
-                    typeIcon.innerHTML = "check_circle";
-                    break;
-                case "error":
-                    typeIcon.innerHTML = "error";
-                    break;
-            }
-
-            // Add the "show" class to DIV
-            snackbar.className = "show";
-        
-            // Default duration
-            if(duration == "" || duration == null){
-                duration = 3;
-            }
-             // After x amount of seconds, remove the show class from DIV
-            setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, duration*1000);
-            /*             
-            if(toastAmount > 0){
-                toastAmount--;
-            } 
-            */
-        } 
-    </script>
-    <style>
-        #snackbar {
-            visibility: hidden; 
-            min-width: 200px; 
-            margin-right: 10px; 
-            border-radius: 10px; 
-            padding: 15px;
-
-            text-align: center; 
-
-            position: fixed;
-            right: 5%;
-            top: 20px;
-            z-index: 1; 
-
-            background-color: red;
-            color: #fff; 
-
-            display: grid;
-            grid-template-areas: 
-            'type close'
-            'text text';
-            grid-template-columns: 1fr 1fr;
-            grid-template-rows: 1fr 1fr;
-        }
-
-        #snackbar.show {
+    #snackbar.show {
         visibility: visible; 
-        /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
-        However, delay the fade out process for 2.5 seconds */
         -webkit-animation: fadein 0.5s;
         animation: fadein 0.5s;
-        }
+    }
 
-        #snackbarText{
-           font-family: Arial; 
-           grid-area: "text";
-           overflow-y: scroll;
-           width: 200%;
-        }
-        #typeIcon{
-            grid-area: "type";
-            cursor: default;
-        }
+    #snackbarText{
+        font-family: Arial; 
+        grid-area: "text";
+        overflow-y: scroll;
+        width: 300%;
+        height: 85px;
+    }
+    #typeIcon{
+        grid-area: "icon";
+        cursor: default;
+        height: 1.5rem;
+    }
 
-        #closeIcon{
-            grid-area: "close";
-            cursor: pointer;
-            /* margin-right: -90px; */
-        }
+    #typeText{
+        grid-area: "type";
+        font-family: Arial; 
+        font-weight: bold;
+        font-size: 1.2em;
+    }
 
-        @-webkit-keyframes fadein {
+    #closeIcon{
+        grid-area: "close";
+        cursor: pointer;
+    }
+
+    @-webkit-keyframes fadein {
         from {top: 0; opacity: 0;}
         to {top: 30px; opacity: 1;}
-        }
+    }
 
-        @keyframes fadein {
+    @keyframes fadein {
         from {top: 0; opacity: 0;}
         to {top: 30px; opacity: 1;}
-        }
+    }
 
-        @-webkit-keyframes fadeout {
+    @-webkit-keyframes fadeout {
         from {top: 30px; opacity: 1;}
         to {top: 0; opacity: 0;}
-        }
+    }
 
-        @keyframes fadeout {
+    @keyframes fadeout {
         from {top: 30px; opacity: 1;}
         to {top: 0; opacity: 0;}
-        } 
-    </style>
-</head>
-<body>
-    <button onclick="myFunction()">Show Snackbar</button>
-    <select id='type'>
-        <option value='warning'>Warning</option>
-        <option value='success'>Success</option>
-        <option value='error'>Error</option>
-    </select>
+    } 
+    /* Toast notifications END */
+</style>
+<script>
+    // Close the toast by clicking the X icon
+    function closeToast(){
+        snackbar.className = snackbar.className.replace("show", "");
+    }
+    // Create a toast notification
+    function toast(type, text, duration) {
+        let snackbar = document.getElementById("snackbar");
+        let snackbarText = document.getElementById("snackbarText");
+        let typeIcon = document.getElementById("typeIcon");
+        let typeText = document.getElementById("typeText");
 
-    <div>
-        <label for='text'>Text: </label>
-        <input id='text' type='text'></input>
-    </div>
+        // Changes the toast text to the input text
+        snackbarText.innerHTML = text;
 
-    <div>
-        <label for='duration'>Duration (s): </label>
-        <input id='duration' type='number'></input>
-    </div>
+        // Decides the type of toast (changes the type icon)
+        switch(type){
+            case "warning":
+                typeIcon.innerHTML = "warning";
+                typeText.innerHTML = "Warning";
+                break;
+            case "success":
+                typeIcon.innerHTML = "check_circle";
+                typeText.innerHTML = "Success";
+                break;
+            case "error":
+                typeIcon.innerHTML = "error";
+                typeText.innerHTML = "Error";
+                break;
+            default:
+                typeIcon.innerHTML = "";
+                typeText.innerHTML = "";
+                break;
+        }
 
+        snackbar.className = "show";
 
-    <div id='toastContainer'></div>
-    <div id='snackbar'>
-        <span class="material-symbols-outlined" id='typeIcon'>warning</span>
-        <span class="material-symbols-outlined" id='closeIcon' onclick='closeToast()'>close</span>
-        <p id='snackbarText'>Hellooo HIII</p>
-    </div> 
-</body>
-</html>
+        // Default duration
+        if(duration == "" || duration == null){
+            duration = 3;
+        }
+        // After x amount of seconds, remove the show class from DIV
+        setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, duration*1000);
+    } 
+</script>
+<!-- In the current version, Google icons needs to be linked in order for the icons to work -->
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> -->
+<!-- The structure of a toast notication -->
+<div id='snackbar'>
+    <span class="material-symbols-outlined" id='typeIcon'>warning</span>
+    <span id='typeText'></span>
+    <span class="material-symbols-outlined" id='closeIcon'>close</span>
+    <p id='snackbarText'></p>
+</div> 
