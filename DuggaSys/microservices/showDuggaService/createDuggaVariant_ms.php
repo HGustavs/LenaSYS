3984acc82f9395e58a047ec1a6353b1c33ea748f
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-19 12:35:17 +0200

    inserted an array

--- DuggaSys/microservices/showDuggaService/createDuggaVariant_ms.php
+++ DuggaSys/microservices/showDuggaService/createDuggaVariant_ms.php
@@ -7,8 +7,6 @@ include_once "../../../Shared/basic.php";
 include "../shared_microservices/getUid_ms.php";
 getUid();
 
-// ***Creation of retriveInformation_ms.php
-
 // Connect to database and start session
 pdoConnect();
 session_start();
@@ -38,6 +36,16 @@ $disabled = getOP('disabled');
 $param = getOP('param');
 $variantanswer = getOP('variananswer');
 
+$insertData = array(
+  'quizID' => $qid,
+  'creator' => $uid,
+  'disabled' => $disabled,
+  'param' => $param,
+  'variantanswer' => $variantanswer
+)
+
+//????echo json_encode(retrieveCourseedService($pdo, $ha, $debug, $writeAccess, $LastCourseCreated));
+
 
 // - quizID
 // - creator
@@ -50,25 +58,5 @@ $variantanswer = getOP('variananswer');
 
 ### createDuggaVariant
 // Uses service __insertIntoTablVariant__ to makes _inserts_ into the table __Variant__.
-
-
-
-
-//////////////////////////////////////////
-### loadDugga
-// Get information based on __hash__.
-// Uses service __selectFromTableUserAnswer__ to _get_ information it requires from __userAnswer__ and __variant__.
-// <br>
-
-// If _hash_ did not work, retrive all answeres for that __moment__. 
-// Uses service __selectFromUserAnswer__ to _get_ information it requires from __userAnswer__ and __variant__.
-
-// - hash: vid,variant.variantanswer __AS__ variantanswer,useranswer,param,cid,vers,quiz 
-// ```sql
-// SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE hash=:hash
-// ```
-// - moment: vid,variant.variantanswer __AS__ variantanswer,useranswer,param,cid,vers,quiz 
-// ```sql
-// SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE moment=:moment
-// ```
+``
 ?>
