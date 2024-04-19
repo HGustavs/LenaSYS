b4c8b518dc10a572a2d780260dce2c784561c4e0
Erkan <a22eriwi@student.his.se>
2024-04-19 16:13:40 +0200

    removed unnecessary code

--- DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
+++ DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
@@ -17,16 +17,8 @@ $query = $pdo->prepare("SELECT username FROM user WHERE uid = :uid");
 $query->bindParam(':uid', $userid);
 $query->execute();
 
-// This while is only performed if userid was set through _SESSION['uid'] check above, a guest will not have it's username set, USED FOR LOGGING
+
 if(checklogin()){
-	if(isset($_SESSION['uid'])){
-		$userid=$_SESSION['uid'];
-		$hasread=hasAccess($userid, $courseid, 'r');
-		$studentTeacher=hasAccess($userid, $courseid, 'st');
-		$haswrite=hasAccess($userid, $courseid, 'w');
-	}else{
-		$userid="guest";
-	}
 	while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 		$username = $row['username'];
 	}
