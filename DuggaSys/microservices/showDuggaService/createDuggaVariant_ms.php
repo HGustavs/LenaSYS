fa11132daff7431e06d6fd8c79e570a6e9b6e3ac
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-19 12:06:25 +0200

    start too write relevent code

--- DuggaSys/microservices/showDuggaService/createDuggaVariant_ms.php
+++ DuggaSys/microservices/showDuggaService/createDuggaVariant_ms.php
@@ -4,10 +4,32 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
+include "../shared_microservices/getUid_ms.php";
+getUid();
+
+// ***Creation of retriveInformation_ms.php
 
 // Connect to database and start session
 pdoConnect();
 session_start();
+
+function getUid(){
+  // Checks user id, if user has none a guest id is set
+  if(isset($_SESSION['uid'])){
+      $userid=$_SESSION['uid'];
+  }else{
+      $userid="1";
+  }
+
+  $log_uuid = getOP('log_uuid');
+  $log_timestamp = getOP('log_timestamp');
+
+  $log_uuid = getOP('log_uuid');
+  $info="opt: ".$opt." courseId: ".$courseId." courseVersion: ".$courseVersion." exampleName: ".$exampleName." sectionName: ".$sectionName." exampleId: ".$exampleId;
+  logServiceEvent($log_uuid, EventTypes::ServiceServerStart, "getUid_ms.php",$userid,$info);
+
+  return $log_uuid;
+}
 
 //Get the necessary parameters from the request
 $qid = getOP('qid');
