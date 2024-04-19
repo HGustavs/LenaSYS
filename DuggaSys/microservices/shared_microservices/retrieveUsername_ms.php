cedfe11ea821633d7236ada81c4dfca72dae3910
Erkan <a22eriwi@student.his.se>
2024-04-19 16:42:16 +0200

    Fixed directorys

--- DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
+++ DuggaSys/microservices/shared_microservices/retrieveUsername_ms.php
@@ -5,8 +5,8 @@
 date_default_timezone_set("Europe/Stockholm");
 
 // Include basic application services!
-include_once "../Shared/basic.php";
-include_once "../Shared/sessions.php";
+include_once "../../../Shared/basic.php";
+include_once "../../../Shared/sessions.php";
 include_once "getUid_ms.php";
 
 // Connect to database and start session
