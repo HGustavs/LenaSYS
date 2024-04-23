56343959315e3576d7843ca63bf57dcaeeb9dfe9
c22cargo <c22cargo@student.his.se>
2024-04-23 14:29:30 +0200

    Remade the MS

--- DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
+++ DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Handles creation of new items on a course, not just for code examples (as the name would suggest)
- * I presume that the title of the file is for consistency with the legacy system... Hey, I just work here....
- */
-
 date_default_timezone_set("Europe/Stockholm");
 
-// Include basic application services!
-include_once "../../../../coursesyspw.php";
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
 include_once "getUid_ms.php";
@@ -18,85 +11,24 @@ pdoConnect();
 session_start();
 
 // Global variables
-$opt = getOP('opt');
-$link = getOP('link');
 $courseid = getOP('courseid');
-$coursevers = getOP('coursevers');
+$examplename=getOP('examplename');
 $sectname = getOP('sectname');
-$kind = getOP('kind');
-$comments = getOP('comments');
-$visibility = getOP('visibility');
-$highscoremode = getOP('highscoremode');
-$pos = getOP('pos');
-$grptype = getOP('grptype');
-$gradesys=getOP('gradesys');
-$tabs=getOP('tabs');
+$coursevers = getOP('coursevers');
+$userid = getUid();
 
-getUid(); // call to microservice getUid_ms.php
-$userid = $_SESSION['uid'];
+$query = $pdo->prepare("INSERT INTO codeexample(cid, examplename, sectionname, uid, cversion) values (:cid, :ename, :sname, 1, :cversion);");
 
-if (strcmp($opt, "NEW") === 0) {
+$query->bindParam(':cid', $courseid);
+$query->bindParam(':ename', $examplename);
+$query->bindParam(':sname', $sectname);
+$query->bindParam(':cversion', $coursevers);
 
-	// Insert a new code example and update variables accordingly.
-	if ($link == -1) {
-		$query1 = $pdo->prepare("SELECT * FROM codeexample ORDER BY exampleid DESC LIMIT 1");
-		if (!$query1->execute()) {
-			$error = $query1->errorInfo();
-			$debug = "Error reading entries" . $error[2];
-		}
-
-		foreach ($query1->fetchAll() as $row) {
-			$sname = $row['exampleid'] + 1;
-		}
-
-		$sname = $sectname . $sname;
-		$query2 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion) values (:cid,:ename,:sname,1,:cversion);");
-
-		$query2->bindParam(':cid', $courseid);
-		$query2->bindParam(':cversion', $coursevers);
-		$query2->bindParam(':ename', $sectname);
-		$query2->bindParam(':sname', $sname);
-
-		if (!$query2->execute()) {
-			$error = $query2->errorInfo();
-			$debug = "Error updating entries" . $error[2];
-		}
-
-		$link = $pdo->lastInsertId();
-	}
-
-	$query = $pdo->prepare("INSERT INTO listentries (cid,vers, entryname, link, kind, pos, visible,creator,comments, gradesystem, highscoremode, groupKind) 
-												 VALUES(:cid,:cvs,:entryname,:link,:kind,:pos,:visible,:usrid,:comment, :gradesys, :highscoremode, :groupkind)");
-
-	if ($kind == 4) {
-		$query->bindParam(':gradesys', $gradesys);
-	} else {
-		$query->bindParam(':gradesys', $tabs);
-	}
-
-	$query->bindParam(':cid', $courseid);
-	$query->bindParam(':cvs', $coursevers);
-	$query->bindParam(':usrid', $userid);
-	$query->bindParam(':entryname', $sectname);
-	$query->bindParam(':link', $link);
-	$query->bindParam(':kind', $kind);
-	$query->bindParam(':comment', $comments);
-	$query->bindParam(':visible', $visibility);
-	$query->bindParam(':highscoremode', $highscoremode);
-	$query->bindParam(':pos', $pos);
-
-	if ($grptype != "UNK") {
-		$query->bindParam(':groupkind', $grptype);
-	} else {
-		$query->bindValue(':groupkind', null, PDO::PARAM_STR);
-
-		// Logging for newly added items
-		//$description = $sectname; // this isn't used
-		logUserEvent($userid, $username, EventTypes::SectionItems, $sectname);
-	}
-
-	if (!$query->execute()) {
-		$error = $query->errorInfo();
-		$debug = "Error updating entries" . $error[2];
-	}
+if (!$query->execute()) {
+	$error = $query->errorInfo();
+	$debug = "Error updating entries" . $error[2];
 }
+
+logUserEvent($userid, $username, EventTypes::SectionItems, $sectname);
+
+echo json_encode($debug);
