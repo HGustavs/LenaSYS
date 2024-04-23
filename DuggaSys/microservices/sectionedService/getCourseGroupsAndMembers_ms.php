246dec3b11a29224a7b2d2b0964012b2cafba486
Albin <aljo940616@gmail.com>
2024-04-23 10:07:41 +0200

    microservices now uses sharedMicroservices/getUid_ms instead of the old shared_microservices/getUid.php

--- DuggaSys/microservices/sectionedService/getCourseGroupsAndMembers_ms.php
+++ DuggaSys/microservices/sectionedService/getCourseGroupsAndMembers_ms.php
@@ -9,7 +9,6 @@ date_default_timezone_set("Europe/Stockholm");
 // Include basic application services!
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
-include_once "../../../../coursesyspw.php";
 
 // Connect to database and start session
 pdoConnect();
--- DuggaSys/microservices/sectionedService/updateListEntries_ms.php
+++ DuggaSys/microservices/sectionedService/updateListEntries_ms.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
-include_once "../shared_microservices/getUid_ms.php";
+include_once "../sharedMicroservices/getUid_ms.php";
 
 // Connect to database and start session.
 pdoConnect();
