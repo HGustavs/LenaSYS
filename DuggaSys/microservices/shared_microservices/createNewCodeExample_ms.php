9302f83a78a074de7e5f0b0e0e676abb06e550bd
a22marca <a22marca@student.his.se>
2024-04-23 15:31:58 +0200

    made createNewCodeExample into a function

--- DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
+++ DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
@@ -7,96 +7,27 @@
 date_default_timezone_set("Europe/Stockholm");
 
 // Include basic application services!
-include_once "../../../../coursesyspw.php";
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
 include_once "getUid_ms.php";
 include_once "retrieveUsername_ms.php";
 
-// Connect to database and start session
-pdoConnect();
-session_start();
 
-// Global variables
-$opt = getOP('opt');
-$link = getOP('link');
-$courseid = getOP('courseid');
-$coursevers = getOP('coursevers');
-$sectname = getOP('sectname');
-$kind = getOP('kind');
-$comments = getOP('comments');
-$visibility = getOP('visibility');
-$highscoremode = getOP('highscoremode');
-$pos = getOP('pos');
-$grptype = getOP('grptype');
-$gradesys=getOP('gradesys');
-$tabs=getOP('tabs');
-
-getUid(); // call to microservice getUid_ms.php
-$userid = $_SESSION['uid'];
-
-if (strcmp($opt, "NEW") === 0) {
-
-	// Insert a new code example and update variables accordingly.
-	if ($link == -1) {
-		$query1 = $pdo->prepare("SELECT * FROM codeexample ORDER BY exampleid DESC LIMIT 1");
-		if (!$query1->execute()) {
-			$error = $query1->errorInfo();
-			$debug = "Error reading entries" . $error[2];
-		}
-
-		foreach ($query1->fetchAll() as $row) {
-			$sname = $row['exampleid'] + 1;
-		}
-
-		$sname = $sectname . $sname;
-		$query2 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion) values (:cid,:ename,:sname,1,:cversion);");
-
-		$query2->bindParam(':cid', $courseid);
-		$query2->bindParam(':cversion', $coursevers);
-		$query2->bindParam(':ename', $sectname);
-		$query2->bindParam(':sname', $sname);
-
-		if (!$query2->execute()) {
-			$error = $query2->errorInfo();
-			$debug = "Error updating entries" . $error[2];
-		}
-
-		$link = $pdo->lastInsertId();
-	}
-
-	$query = $pdo->prepare("INSERT INTO listentries (cid,vers, entryname, link, kind, pos, visible,creator,comments, gradesystem, highscoremode, groupKind) 
-												 VALUES(:cid,:cvs,:entryname,:link,:kind,:pos,:visible,:usrid,:comment, :gradesys, :highscoremode, :groupkind)");
-
-	if ($kind == 4) {
-		$query->bindParam(':gradesys', $gradesys);
-	} else {
-		$query->bindParam(':gradesys', $tabs);
-	}
-
-	$query->bindParam(':cid', $courseid);
-	$query->bindParam(':cvs', $coursevers);
-	$query->bindParam(':usrid', $userid);
-	$query->bindParam(':entryname', $sectname);
-	$query->bindParam(':link', $link);
-	$query->bindParam(':kind', $kind);
-	$query->bindParam(':comment', $comments);
-	$query->bindParam(':visible', $visibility);
-	$query->bindParam(':highscoremode', $highscoremode);
-	$query->bindParam(':pos', $pos);
-
-	if ($grptype != "UNK") {
-		$query->bindParam(':groupkind', $grptype);
-	} else {
-		$query->bindValue(':groupkind', null, PDO::PARAM_STR);
-
-		// Logging for newly added items
-		//$description = $sectname; // this isn't used
-		logUserEvent($userid, $username, EventTypes::SectionItems, $sectname);
-	}
-
-	if (!$query->execute()) {
-		$error = $query->errorInfo();
+function createNewCodeExample($pdo, $exampleid, $courseid, $coursevers, $sectname, $link, $log_uuid){
+	$userid = getUid();
+	$sname = $sectname . ($exampleid + 1);
+	$query2 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion) values (:cid,:ename,:sname,1,:cversion);");
+	$query2->bindParam(':cid', $courseid);
+	$query2->bindParam(':cversion', $coursevers);
+	$query2->bindParam(':ename', $sectname);
+	$query2->bindParam(':sname', $sname);
+	if (!$query2->execute()) {
+		$error = $query2->errorInfo();
 		$debug = "Error updating entries" . $error[2];
 	}
+	// link used by createNewListentrie, should probably be returned?
+	$link = $pdo->lastInsertId();
+	// TODO: Add logUserEvent call
+	return $link;
 }
+
