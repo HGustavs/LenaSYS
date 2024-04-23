5a3aa78cba5a53563dce5422818d3b654da97ffb
SleepyBearIV <mattias.nicander.mn@gmail.com>
2024-04-23 09:58:56 +0200

    indentation on form fixed

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -143,11 +143,11 @@ if(isset($_SESSION['uid'])){
 				<button id="deleteCourseButton" onclick='openForm()'>Delete</button>
     			<input id='saveCourse' class='submit-button' type='button' value='Save' title='Save changes' onclick="validateForm('editCourse')" />
 					<div class="form-popup" id="myForm">
-					<form action="" class="form-container DarkModeBackgrounds">
-						<h1 Class="DarkModeText" style="font-size:24px; padding-bottom:50px; padding-top:60px; border-bottom: 2px solid #ccc;">Are you sure? </h1>
-							<button id="deleteCourseButtonYes" type="button" onclick="closeForm()">Yes</button>
-							<button id="deleteCourseButtonNo" type="button" onclick="closeForm()">No</button>
-					</form>
+						<form action="" class="form-container DarkModeBackgrounds">
+							<h1 Class="DarkModeText" style="font-size:24px; padding-bottom:50px; padding-top:60px; border-bottom: 2px solid #ccc;">Are you sure? </h1>
+								<button id="deleteCourseButtonYes" type="button" onclick="closeForm()">Yes</button>
+								<button id="deleteCourseButtonNo" type="button" onclick="closeForm()">No</button>
+						</form>
 					</div>
     		</div>
       </div>
