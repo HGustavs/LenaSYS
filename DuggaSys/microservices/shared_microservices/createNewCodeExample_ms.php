cefa43aa69c9cfe5ee987f05db0f794da9605fdc
a22marca <a22marca@student.his.se>
2024-04-23 16:24:24 +0200

    removed comments and unused code

--- DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
+++ DuggaSys/microservices/shared_microservices/createNewCodeExample_ms.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Handles creation of new items on a course, not just for code examples (as the name would suggest)
- * I presume that the title of the file is for consistency with the legacy system... Hey, I just work here....
- */
-
-date_default_timezone_set("Europe/Stockholm");
-
-// Include basic application services!
-include_once "../../../Shared/sessions.php";
-include_once "../../../Shared/basic.php";
-include_once "getUid_ms.php";
-include_once "retrieveUsername_ms.php";
-
 
 function createNewCodeExample($pdo, $exampleid, $courseid, $coursevers, $sectname, $link, $log_uuid){
 	$userid = getUid();
@@ -25,7 +12,6 @@ function createNewCodeExample($pdo, $exampleid, $courseid, $coursevers, $sectnam
 		$error = $query2->errorInfo();
 		$debug = "Error updating entries" . $error[2];
 	}
-	// link used by createNewListentrie, should probably be returned?
 	$link = $pdo->lastInsertId();
 	// TODO: Add logUserEvent call
 	return $link;
