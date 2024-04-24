4d6ea03f752ec23848fea5f1fc3678784a6a7d30
c22mohel <c22mohel@student.his.se>
2024-04-24 14:41:28 +0200

    login works now
    finnaly got login to pass

--- DuggaSys/tests/showDuggaservice_test.php
+++ DuggaSys/tests/showDuggaservice_test.php
@@ -112,31 +112,33 @@ $testsData = array(
     //         'none'
     //     )),
     // ),
-    // 'submit dugga' => array(
-    //     'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-    //     'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
-    //     'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES (9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
-    //     'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
-    //     'variables-query-before-test-4' => "moment",
-    //     'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
-    //     'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
-    //     'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
-    //     'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
-    //     'service-data' => serialize(array( // Data that service needs to execute function
-    //         'opt' => 'SAVDU',
-    //         'cid' => '9999',
-    //         'coursevers' => '52432',
-    //         'duggaid' => '1',
-    //         'moment' => '<!query-before-test-3!><*[0]["listentries"]*>',
-    //         'variant' => '3',
-    //         'hash' => 'ghj1ghj2',
-    //         'haspwd' => 'asddasdd',
-    //         'answer' => 'NULL'
-    //     )),
-    //     'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-    //         'none'
-    //     )),
-    // ),
+    'submit dugga' => array(
+        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
+        'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
+        'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES (9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
+        'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
+        'variables-query-before-test-4' => "moment",
+        'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
+        'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
+        'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+        'service-data' => serialize(array( // Data that service needs to execute function
+            'hash' => 'ghj1ghj2',
+            'hashpwd' => 'asddasdd',
+            'username' => 'stei',
+            'password' => 'password',
+            'opt' => 'SAVDU',
+            'cid' => '9999',
+            'coursevers' => '52432',
+            'duggaid' => '1',
+            'moment' => '<!query-before-test-3!><*[0]["listentries"]*>',
+            'variant' => '3',
+            'answer' => 'NULL'
+        )),
+        'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
+            'none'
+        )),
+    ),
 
     /* This test doesn't work.
     'super-view data from useranswer on hash (teacher)' => array(
