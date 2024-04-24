373f410eb51072d191a816ca895b8bb29a3a5890
a22eriwi <a22eriwi@student.his.se>
2024-04-24 14:20:14 +0200

    added function

--- DuggaSys/microservices/sharedMicroservices/retrieveUsername_ms.php
+++ DuggaSys/microservices/sharedMicroservices/retrieveUsername_ms.php
@@ -12,20 +12,18 @@ include_once "getUid_ms.php";
 // Connect to database and start session
 pdoConnect();
 session_start();
-
 $userid = getUid();
 
+function retrieveUsername($userid, $pdo){
 // Gets username based on uid
-$query = $pdo->prepare("SELECT username FROM user WHERE uid = :uid");
-$query->bindParam(':uid', $userid);
-$query->execute();
-
-
-if(checklogin() == true){
-	while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-		$username = $row['username'];
+	$query = $pdo->prepare("SELECT username FROM user WHERE uid = :uid");
+	$query->bindParam(':uid', $userid);
+	$query->execute();
+	
+	if(checklogin() == true){
+		while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+			$username = $row['username'];
+		}
 	}
+	return $username;
 }
-return $username;
-
-?>
