4ba64eb2c6b06f2f7d40034ed15e55548c30b095
c22emigr <c22emigr@student.his.se>
2024-04-24 16:52:31 +0200

    removed empty rows and end php tag

--- DuggaSys/microservices/sharedMicroservices/updateUserPassword_ms.php
+++ DuggaSys/microservices/sharedMicroservices/updateUserPassword_ms.php
@@ -4,13 +4,11 @@
 // profileService - handles password changes and challenge question
 //---------------------------------------------------------------------------------------------------------------
 
-
 date_default_timezone_set("Europe/Stockholm");
 
 // Include basic application services!
 include_once "../Shared/basic.php";
 include_once "../Shared/sessions.php";
-
 
 // Connect to database and start session
 pdoConnect();
@@ -21,7 +19,6 @@ if(isset($_SESSION['uid'])){
 }else{
 	$userid="1";		
 } 
-
 
 $password= getOP('password');
 $question = getOP('question');
@@ -106,4 +103,3 @@ echo json_encode(array(
 // Log the end event of this service query.
 logServiceEvent($log_uuid, EventTypes::ServiceServerEnd, __FILE__, $userid, $info);
 
-?>
