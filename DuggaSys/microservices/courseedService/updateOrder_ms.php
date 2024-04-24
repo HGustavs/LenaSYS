670fee3b670c74e8cdcd19e1268b727c83b5efc4
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-04-24 15:24:04 +0200

    included timezone and files

---
+++ DuggaSys/microservices/courseedService/updateOrder_ms.php
@@ -0,0 +1,8 @@
+<?php
+// Set the correct time zone
+date_default_timezone_set("Europe/Stockholm");
+
+// Basic includes for this ms to work
+include_once "../../../Shared/basic.php";
+include_once "../../../Shared/sessions.php";
+include_once "../shared_microservices/getUid_ms.php";
