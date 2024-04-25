20ee7122e4546986ea969fd0db128cf1d5778eab
a22eriwi <a22eriwi@student.his.se>
2024-04-25 15:45:52 +0200

    Formatted file

--- DuggaSys/microservices/sharedMicroservices/updateSecurityQuestion_ms.php
+++ DuggaSys/microservices/sharedMicroservices/updateSecurityQuestion_ms.php
@@ -14,13 +14,13 @@ include_once "../Shared/sessions.php";
 pdoConnect();
 session_start();
 
-if(isset($_SESSION['uid'])){
-	$userid=$_SESSION['uid'];
-}else{
-	$userid="1";		
-} 
+if (isset($_SESSION['uid'])) {
+	$userid = $_SESSION['uid'];
+} else {
+	$userid = "1";
+}
 
-$password= getOP('password');
+$password = getOP('password');
 $question = getOP('question');
 $answer = getOP('answer');
 $action = getOP('action');
@@ -32,57 +32,56 @@ $debug = "NONE!";
 
 // Create arguments for log
 $log_uuid = getOP('log_uuid'); // Cookie id or something.. 
-$info="action: ".$action." userid: ".$userid;
+$info = "action: " . $action . " userid: " . $userid;
 
 // Log the start event of this service query. This is logged in log.db (not MySQL)
 logServiceEvent($log_uuid, EventTypes::ServiceServerStart, __FILE__, $userid, $info);
 
 // Check if the user is logged in and block access if it is a super user
-if(checklogin()) {
+if (checklogin()) {
 	if (isSuperUser($userid)) {
 		$status = "teacher";
 	} else if ($action === "challenge" || $action === "password") {
 		// and fetch the password from the db
-		$querystring="SELECT password FROM user WHERE uid=:userid LIMIT 1";	
+		$querystring = "SELECT password FROM user WHERE uid=:userid LIMIT 1";
 		$stmt = $pdo->prepare($querystring);
 		$stmt->bindParam(':userid', $userid);
-		
-		if(!$stmt->execute()) {
-			$error = $stmt->errorInfo(); 
-			$debug = "Error finding user ".$error[2];
+
+		if (!$stmt->execute()) {
+			$error = $stmt->errorInfo();
+			$debug = "Error finding user " . $error[2];
 		} else {
 			$result = $stmt->fetch(PDO::FETCH_OBJ);
-			
+
 			// Check that the password matches
-			if(password_verify($password, $result->password)) {
+			if (password_verify($password, $result->password)) {
 				//Query that selects the user row if it is a superuser or a teacher
 				$accessString = "SELECT access FROM user_course WHERE uid=:userid AND access='W' LIMIT 1";
 				$query = $pdo->prepare($accessString);
 				$query->bindParam('userid', $userid);
-				
-				if(!$query->execute()) {
+
+				if (!$query->execute()) {
 					$error = $stmt->errorInfo();
-					$debug = "Error checking if user is teacher ".$error[2];
+					$debug = "Error checking if user is teacher " . $error[2];
 				} else {
-					if($query->rowCount() > 0) {
+					if ($query->rowCount() > 0) {
 						//If a row matches query, the user is a teacher/superuser and cannot change password/security questions
 						$status = "teacher";
 					} else {
 						//Action determines which form is being used
-						if($action == "challenge"){
-								//Update challenge question
-								$querystringz = "UPDATE user SET securityquestion=:SQ, securityquestionanswer=:answer WHERE uid=:userid";
-								$stmt = $pdo->prepare($querystringz);
-								$stmt->bindParam(':userid', $userid);
-								$stmt->bindParam(':SQ', $question);
-								$stmt->bindParam(':answer', $hashedAnswer);
-
-								if(!$stmt->execute()) {
-									$error=$stmt->errorInfo(); 
-								} else {
-									$success = true;
-								}
-						} 
+						if ($action == "challenge") {
+							//Update challenge question
+							$querystringz = "UPDATE user SET securityquestion=:SQ, securityquestionanswer=:answer WHERE uid=:userid";
+							$stmt = $pdo->prepare($querystringz);
+							$stmt->bindParam(':userid', $userid);
+							$stmt->bindParam(':SQ', $question);
+							$stmt->bindParam(':answer', $hashedAnswer);
+							if (!$stmt->execute()) {
+								$error = $stmt->errorInfo();
+							} else {
+								$success = true;
+							}
+						}
 					}
 				}
 			} else {
