95f6a79c983ab8ca3426e4a95c0ef560c94b7231
c22emigr <c22emigr@student.his.se>
2024-04-26 13:27:17 +0200

    imported code and comments from monolith file

--- DuggaSys/microservices/gitCommitService/clearGitFiles_ms.php
+++ DuggaSys/microservices/gitCommitService/clearGitFiles_ms.php
@@ -1,7 +1,47 @@
 <?php
 
+// -------------==============######## Setup ###########==============-------------
 
+// Variables for the refresh button, deadlines specified in seconds
+$shortdeadline = 300; // 300 = 5 minutes
+$longdeadline = 600; // 600 = 10 minutes
 
+// Used to display errors on screen since PHP doesn't do that automatically.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Include basic application services!
+include_once "../../../Shared/basic.php";
+include_once "../../../Shared/sessions.php";
+include_once "../../gitfetchService.php";
+
+global $pdo;
+
+//Get data from AJAX call in courseed.js and then runs the function getCourseID, refreshGithubRepo or updateGithubRepo depending on the action
+if(isset($_POST['action'])) 
+{
+    if($_POST['action'] == 'getCourseID') 
+    {
+        getCourseID($_POST['githubURL']);
+    }
+    else if($_POST['action'] == 'refreshGithubRepo') 
+    {
+        refreshGithubRepo($_POST['cid']);
+    }
+    else if($_POST['action'] == 'updateGithubRepo') 
+    {
+        updateGithubRepo($_POST['githubURL'], $_POST['cid']);
+    }
+    else if($_POST['action'] == 'directInsert'){
+        
+        insertIntoSQLite($_POST['githubURL'], $_POST['cid'], $_POST['token'],);
+    }
+};
+
+//--------------------------------------------------------------------------------------------------
+// clearGitFiles: Clear the gitFiles table in SQLite db when a course has been updated with a new github repo
+//--------------------------------------------------------------------------------------------------
 
 
 function clearGitFiles($cid) {
@@ -16,8 +56,5 @@ function clearGitFiles($cid) {
         echo $errorvar;
     }
 }
-
-
-
 
 ?>
