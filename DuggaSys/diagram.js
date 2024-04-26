ce1e5eb23734aee6e8c290c427983e2a9fe39e7a
a22ricni <a22ricni@student.his.se>
2024-04-26 14:22:56 +0200

    Added a new if statement
    Added a new if statement so that a line can be drawn between two ER entity.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -7813,6 +7813,12 @@ function addLine(fromElement, toElement, kind, stateMachineShouldSave = true, su
             fromElement = tempElement;
         }
     }
+    // Prevent a line to be drawn between two ER entity.
+    if (fromElement.kind == toElement.kind && fromElement.kind == elementTypesNames.EREntity && toElement.kind == elementTypesNames.EREntity){
+        displayMessage(messageTypes.ERROR, `Not possible to draw a line between: ${fromElement.name}- and ${toElement.name}-element`);
+        return;
+    }
+
     if (fromElement.id === toElement.id && !(fromElement.kind === elementTypesNames.SDEntity || toElement.kind === elementTypesNames.SDEntity)) {
         displayMessage(messageTypes.ERROR, `Not possible to draw a line between: ${fromElement.name} and ${toElement.name}, they are the same element`);
         return;
