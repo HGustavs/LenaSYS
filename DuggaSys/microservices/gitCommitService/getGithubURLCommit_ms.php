361264e5bb8972a0b3e457769cdfe03a3a896c37
c22emigr <c22emigr@student.his.se>
2024-04-29 15:38:06 +0200

    basic includes

---
+++ DuggaSys/microservices/gitCommitService/getGithubURLCommit_ms.php
@@ -0,0 +1,9 @@
+<?php
+
+// Include basic application services!
+include_once "../../../Shared/basic.php";
+include_once "../../../Shared/sessions.php";
+include_once "../../gitfetchService.php";
+
+global $pdo;
+
