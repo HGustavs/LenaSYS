f85b1cdcdc7e633976d23c1bc589402fdc2e9262
a22eriwi <a22eriwi@student.his.se>
2024-04-29 14:35:49 +0200

    Updated file

--- DuggaSys/microservices/gitCommitService/clearGitFiles_ms.php
+++ DuggaSys/microservices/gitCommitService/clearGitFiles_ms.php
@@ -3,7 +3,6 @@
 // Include basic application services!
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
-include_once "../../gitfetchService.php";
 
 global $pdo;
 
