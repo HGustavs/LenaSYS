f313af66d19ab6e78d4832d96422e4c1515548cf
a22marca <a22marca@student.his.se>
2024-04-29 16:47:15 +0200

    created the microservice from the function refreshCheck()

---
+++ DuggaSys/microservices/gitCommitService/refreshCheck_ms.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once "../../../Shared/sessions.php";
+include_once "../../../Shared/basic.php";
+include_once "./newUpdateTime_ms.php";
+
+function refreshCheck($cid, $user) {
+    global $shortdeadline, $longdeadline;
+    // Connect to database and start session
+    pdoConnect();
+    session_start();
+
+    // Fetching the latest update of the course from the MySQL database
+    global $pdo;
+    $query = $pdo->prepare('SELECT updated FROM course WHERE cid = :cid;');
+    $query->bindParam(':cid', $cid);
+    $query->execute();
+
+    // Save the result in a variable
+    $updated = "";
+    foreach($query->fetchAll(PDO::FETCH_ASSOC) as $row){
+        $updated = $row['updated'];
+    }
+
+
+    $currentTime = time(); // Get the current time as a Unix timestamp
+    $updateTime = strtotime($updated); // Format the update-time as Unix timestamp
+
+    $_SESSION["updatetGitReposCooldown"][$cid]=$updateTime;
+
+
+    $_SESSION["lastFetchTime"] = date("Y-m-d H:i:s", $currentTime);
+    $fethCooldown = $longdeadline - (time() - $updateTime);
+    if($fethCooldown<0){
+        $_SESSION["fetchCooldown"]=0;
+    }else{
+        $_SESSION["fetchCooldown"]=$fethCooldown;
+    }
+    // Check if the user has superuser priviliges
+    if($user == 1) { // 1 = superuser
+        if(($currentTime - $_SESSION["updatetGitReposCooldown"][$cid]) < $shortdeadline) { // If they to, use the short deadline
+
+            print "Too soon since last update, please wait.";
+            return false;
+        } else {
+            newUpdateTime($currentTime, $cid);
+            return true;
+        }
+    } else { 
+        if(($currentTime - $_SESSION["updatetGitReposCooldown"][$cid]) > $longdeadline) { // Else use the long deadline
+            newUpdateTime($currentTime, $cid);
+            return true;
+        } else {
+            print "Too soon since last update, please wait.";
+            return false;
+        }
+    }
+}
