1e38d4240b8c66bd0a1d8467ef8b0309fac00d2c
a22marca <a22marca@student.his.se>
2024-04-29 12:03:09 +0200

    removed unneeded query and only the specific course used in the test is removed from users in user_course

--- DuggaSys/tests/sectionedservice_Test.php
+++ DuggaSys/tests/sectionedservice_Test.php
@@ -26,10 +26,8 @@ $testsData = array(
     'getCourseGroupsAndMembers' => array(
         'expected-output' => '{"debug": "NONE!","coursevers": "1337","courseid": "1885","grpmembershp": "No_2","grplst": [["No_2",null,null,"Tester@student.his.se"],["No_2","Martin","Brobygge","martin.b@his.se"]]}',
         'query-before-test-1' => "INSERT INTO user_course (cid, vers, uid,groups,access) VALUES (1885,1337,3, 'No_2','W'), (1885,1337,101, 'No_2','W');",
-        'query-before-test-2' => "INSERT INTO listentries (cid,vers, entryname, kind, visible,creator, groupKind)  VALUES(1885,1337,'test group',6,1,101, 'No');",
-        'query-after-test-1' => "DELETE FROM user_course WHERE uid=3;",
-        'query-after-test-2' => "DELETE FROM user_course WHERE uid=101;",
-        'query-after-test-3' => "DELETE FROM listentries WHERE cid=1885 AND vers=1337 AND entryname='test group';",
+        'query-after-test-1' => "DELETE FROM user_course WHERE uid=3 AND cid=1885;",
+        'query-after-test-2' => "DELETE FROM user_course WHERE uid=101 AND cid=1885;",
         'service' => 'http://localhost/LenaSYS/DuggaSys/sectionedservice.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
