cd166af6c6e52012663546e9bdd74b3470a64c79
a22willi <a22willi@student.his.se>
2024-04-29 13:16:34 +0200

    Created frame for db_setup.php

---
+++ newinstaller/tools/db_setup.php
@@ -0,0 +1,53 @@
+<?php
+    class DBSetup {
+        private $pdo;
+        private $db_name;
+        private $db_user;
+
+        public function __construct(PDO $pdo, string $db_name, string $db_user) {
+            $this->pdo = $pdo;
+            $this->db_name = $db_name;
+            $this->db_user = $db_user;
+        }
+
+        /**
+         * function create_db
+         * Create db using class variables.
+         * Force first drops any existing database.
+         */
+        public function create_db(bool $force = false) {
+            // TODO: Implement function that creates a db.
+        }
+
+        /**
+         * function create_user
+         * Creates a new sql user.
+         * Force first removes user.
+         */
+        public function create_user (string $hostname = "%", bool $force = false) {
+            // TODO: Implement function that creates user using class variables.
+        }
+
+        /**
+         * function drop_db
+         * Drops an existing database. 
+         */
+        public function drop_db() { 
+            // TODO: implement function that drops db.
+        }
+
+        /**
+         * function drop_user
+         * removes an already existing user.
+         */
+        public function drop_user(string $hostname = hostname) {
+            // TODO: Implement function that removes a user.
+        }
+
+        /**
+         * Runs an sql file using the class variable pdo.
+         */
+        public function run_sql_file(string $file_name): bool {
+            // TODO: Implement function that runs an sql file.
+        }
+    }
