b59db49ba75d69155bcbecd0e86a5dceb851c547
a22melst <a22melst@student.his.se>
2024-04-29 14:38:37 +0200

    Removed nesting
    I removed the nested checks and added "NONE!" as the default value of debug. I also made it so logging is performed after the file and database has been updated, so that no log is produced if something were to fail. Add the current version of retrieveUsername_ms.php to test and replace the "echo json_encode($debug)" with "echo $debug".

--- DuggaSys/microservices/fileedService/updateFileLink_ms.php
+++ DuggaSys/microservices/fileedService/updateFileLink_ms.php
@@ -1,5 +1,11 @@
 <?php
 
+//---------------------------------------------------------------------------------------------------------------
+// updateFileLink_ms
+//---------------------------------------------------------------------------------------------------------------
+// Microservice that handles writing to files and updates filesize in fileLink
+//---------------------------------------------------------------------------------------------------------------
+
 date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/sessions.php";
@@ -20,6 +26,7 @@ $kind = getOP('kind');
 $contents = getOP('contents');
 $userid = getUid();
 $username = retrieveUsername($pdo);
+$debug = "NONE!";
 
 
 // Check access
@@ -29,85 +36,90 @@ if (hasAccess($userid, $cid, 'w') || hasAccess($userid, $cid, 'st') || isSuperUs
     $hasAccess = false;
 }
 
-// Service 
-if ((strcmp($opt, "SAVEFILE") === 0)) {
-    if (checklogin()) {
-        if ($hasAccess) {
-            if (!($kind == 2 && !(isSuperUser($userid)))) {
-                // Change path to file depending on filename and filekind
-                chdir("../../../");
-                $currcwd = getcwd();
-        
-                switch ($kind) {
-                    case 2:
-                        $currcwd .= "/courses/global/" . $filename;
-                        // Logging for global files
-                        $description="Global"." ".$filename;
-                        logUserEvent($userid, $username, EventTypes::EditFile, $description);
-                        break;
-                    case 3:
-                        $currcwd .= "/courses/" . $cid . "/" . $filename;
-                        // Logging for course local files
-                        $description="CourseLocal"." ".$filename;
-                        logUserEvent($userid, $username, EventTypes::EditFile, $description);            
-                        break;
-                    case 4:
-                        $currcwd .= "/courses/" . $cid . "/" . $coursevers . "/" . $filename;
-                        // Logging for version local files
-                        $description="VersionLocal"." ".$filename;
-                        logUserEvent($userid, $username, EventTypes::EditFile, $description);
-                        break;
-                }
-                    
-                // Check if file exists at set path
-                if (file_exists($currcwd)) {
-                    // Update the database if the save is successful
-                    if (file_put_contents($currcwd, html_entity_decode($contents))) {
-                        $fileSize = filesize($currcwd);
-        
-                        switch ($kind) {
-                            case 2:
-                                $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE kind=:kindid AND filename=:filename;");
-                                break;
-                            case 3:
-                                $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE cid=:cid AND kind=:kindid AND filename=:filename;");
-                                $query->bindParam(':cid', $cid);
-                                break;
-                            case 4:
-                                $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE vers=:vers AND cid=:cid AND kind=:kindid AND filename=:filename;");
-                                $query->bindParam(':cid', $cid);
-                                $query->bindParam(':vers', $coursevers);
-                                break;
-                        }
-        
-                        $query->bindParam(':filename', $filename);
-                        $query->bindParam(':filesize', $fileSize);
-                        $query->bindParam(':kindid', $kind);
-        
-                        if (!$query->execute()) {
-                            $error = $query->errorInfo();
-                            $debug = "Error updating filesize and uploaddate. Details: " . $error[2];
-                        }
-                    } else {
-                        $debug = "Something went wrong when updating the file";
-                    }
-                } else {
-                    $debug = "No such file exists";
-                }
-            } else {
-                $debug = "Access denied: Only superusers can update global files";
-            }
-        } else {
-            $debug = "Access denied";
-        }
-    } else {
-        $debug = "You need to be logged in to update a file";
-    }
-} else{
+if (strcmp($opt, "SAVEFILE") !== 0) {
     $debug = "You can only update a file through the file editor";
+    echo json_encode($debug);
+    return;
 }
 
-if (is_null($debug)) 
-    $debug = "File successfully updated";
+if (!checklogin()) {
+    $debug = "You need to be logged in to update a file";
+    echo json_encode($debug);
+    return;
+}
+
+if (!$hasAccess) {
+    $debug = "Access denied";
+    echo json_encode($debug);
+    return;
+}
+
+if ($kind == 2 && !(isSuperUser($userid))) {
+    $debug = "Access denied: Only superusers can update global files";
+    echo json_encode($debug);
+    return;
+}
+
+// Change path to file depending on filename and filekind
+chdir("../../../");
+$currcwd = getcwd();
+
+switch ($kind) {
+    case 2:
+        $currcwd .= "/courses/global/" . $filename;
+        $description="Global"." ".$filename;
+        break;
+    case 3:
+        $currcwd .= "/courses/" . $cid . "/" . $filename;
+        $description="CourseLocal"." ".$filename;     
+        break;
+    case 4:
+        $currcwd .= "/courses/" . $cid . "/" . $coursevers . "/" . $filename;
+        $description="VersionLocal"." ".$filename;
+        break;
+}
+
+// Check if file exists at set path
+if (!file_exists($currcwd)) {
+    $debug = "No such file exists";
+    echo json_encode($debug);
+    return;
+}
+
+// Try writing to file
+if (!file_put_contents($currcwd, html_entity_decode($contents))) {
+    $debug = "Something went wrong when updating the file";
+    echo json_encode($debug);
+    return;
+}  
+
+$fileSize = filesize($currcwd);
+
+// Update filesize
+switch ($kind) {
+    case 2:
+        $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE kind=:kindid AND filename=:filename;");
+        break;
+    case 3:
+        $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE cid=:cid AND kind=:kindid AND filename=:filename;");
+        $query->bindParam(':cid', $cid);
+        break;
+    case 4:
+        $query = $pdo->prepare("UPDATE fileLink SET filesize=:filesize, uploaddate=NOW() WHERE vers=:vers AND cid=:cid AND kind=:kindid AND filename=:filename;");
+        $query->bindParam(':cid', $cid);
+        $query->bindParam(':vers', $coursevers);
+        break;
+}
+
+$query->bindParam(':filename', $filename);
+$query->bindParam(':filesize', $fileSize);
+$query->bindParam(':kindid', $kind);
+
+if (!$query->execute()) {
+    $error = $query->errorInfo();
+    $debug = "Error updating filesize and uploaddate. Details: " . $error[2];
+}
+
+logUserEvent($userid, $username, EventTypes::EditFile, $description);
 
 echo json_encode($debug);
