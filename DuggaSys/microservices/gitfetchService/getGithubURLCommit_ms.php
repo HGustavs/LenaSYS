b5a84e1247f65601b1fd428cf85320f750fe8b60
a22eriwi <a22eriwi@student.his.se>
2024-04-30 13:26:40 +0200

    Added funktion

--- DuggaSys/microservices/gitfetchService/getGithubURLCommit_ms.php
+++ DuggaSys/microservices/gitfetchService/getGithubURLCommit_ms.php
@@ -7,3 +7,15 @@ include_once "../../gitfetchService.php";
 
 global $pdo;
 
+function getGitHubURLCommit($url)
+{
+    $urlParts = explode('/', $url);
+    // In normal GitHub Repo URL:s, the username is the third object separated by a slash
+    $username = $urlParts[3];
+    // In normal GitHub Repo URL:s, the repo is the fourth object separated by a slash
+    $repository = $urlParts[4];
+    // Translates the parts broken out of $url into the correct URL syntax for an API-URL 
+    $translatedURL = 'https://api.github.com/repos/'.$username.'/'.$repository.'/commits/master';
+    // bfs($translatedURL, "REFRESH");
+    return $translatedURL;
+}
