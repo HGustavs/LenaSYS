f65f105b9450eba75b31eb0f3a69aa65c27aef55
a22willi <a22willi@student.his.se>
2024-04-30 09:42:18 +0200

    Implemented get_permissions function.

--- newinstaller/tools/permissions.php
+++ newinstaller/tools/permissions.php
@@ -14,8 +14,45 @@
          * Get the permissions set on a file or directory.
          */
         public static function get_permissions(string $path): array {
-            
+            // Check if the file or directory exists
+            if (!file_exists($path)) {
+                return ['error' => 'File or directory does not exist'];
+            }
+        
+            // Get file permissions
+            $perms = fileperms($path);
+        
+            $info = [
+                'is_readable' => is_readable($path),
+                'is_writable' => is_writable($path),
+                'is_executable' => is_executable($path),
+                'permissions' => ''
+            ];
+        
+            // Owner permissions
+            $info['permissions'] .= (($perms & 0x0100) ? 'r' : '-');
+            $info['permissions'] .= (($perms & 0x0080) ? 'w' : '-');
+            $info['permissions'] .= (($perms & 0x0040) ? 
+                                    (($perms & 0x0800) ? 's' : 'x' ) :
+                                    (($perms & 0x0800) ? 'S' : '-'));
+        
+            // Group permissions
+            $info['permissions'] .= (($perms & 0x0020) ? 'r' : '-');
+            $info['permissions'] .= (($perms & 0x0010) ? 'w' : '-');
+            $info['permissions'] .= (($perms & 0x0008) ? 
+                                    (($perms & 0x0400) ? 's' : 'x' ) :
+                                    (($perms & 0x0400) ? 'S' : '-'));
+        
+            // Other permissions
+            $info['permissions'] .= (($perms & 0x0004) ? 'r' : '-');
+            $info['permissions'] .= (($perms & 0x0002) ? 'w' : '-');
+            $info['permissions'] .= (($perms & 0x0001) ? 
+                                    (($perms & 0x0200) ? 't' : 'x' ) :
+                                    (($perms & 0x0200) ? 'T' : '-'));
+        
+            return $info;
         }
+        
 
         /**
          * function get_owner
