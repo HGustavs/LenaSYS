7c59825898f9fa6fa030aece2bf6c5797f82a433
Ilirr <61836150+Ilirr@users.noreply.github.com>
2024-04-30 14:51:02 +0200

    Update codeViewerService_test.php

---
+++ DuggaSys/tests/codeViewerService_test.php
@@ -0,0 +1,15 @@
+/* 
+        Microservices to be included in the test
+    deleteCodeExample_ms.php
+
+    editBoxTitle_ms.php
+        
+    update paths to be consistent with the new filenames
+        
+
+    editCodeExample_ms.php
+        
+
+    editContentOfExample_ms.php
+
+*/
