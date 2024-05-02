6b15296f57b9ef94f418104482a9596a1fff4e39
Ilirr <61836150+Ilirr@users.noreply.github.com>
2024-05-02 11:30:05 +0200

    Can create course, but something broken

--- DuggaSys/tests/microservices/createNewCourse_ms_test.php
+++ DuggaSys/tests/microservices/createNewCourse_ms_test.php
@@ -9,17 +9,18 @@ $testsData = array(
     //------------------------------------------------------------------------------------------
     'insert_new_course' => array(
         'expected-output' => '1', // need to figure this one out.
+     //   'query-before-test-1' => "DELETE FROM course WHERE coursecode = 'TEST001';",
+     // 'query-after-test-1' => "DELETE FROM course WHERE coursecode = 'TEST001';",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/courseedService/createNewCourse_ms.php',
         'service-data' => serialize(
             array( 
-               // 'opt' => 'NEW',Is needed?
-               'opt' => 'NEW',
++               'opt' => 'NEW',
                 'username' => 'brom',
                 'password' => 'password',
                 'coursename' => 'Test Course',
                 'coursecode' => 'TEST001',
                 'courseGitURL' => 'http://localhost/LenaSYS/DuggaSys/sectioned.php?courseid=9999&coursename=Test%20Course&coursevers=8888',
-                'AUtoken' => 999
+                'AUtoken' => 1
             )
         ),
         'filter-output' => serialize(
