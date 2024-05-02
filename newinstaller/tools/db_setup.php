d760a473910ead4423a8a2458c124388268ae137
a22willi <a22willi@student.his.se>
2024-05-02 09:18:31 +0200

    Implemented function set_permissions.

--- newinstaller/tools/db_setup.php
+++ newinstaller/tools/db_setup.php
@@ -1,6 +1,6 @@
 <?php
     class DBSetup {
-        private $pdo;
+        private $pdo, $db_name, $db_user, $hostname = null;
 
         public function __construct(PDO $pdo) {
             $this->pdo = $pdo;
@@ -11,20 +11,24 @@
          * Create db using class variables.
          * Force first drops any existing database.
          */
-        public function create_db(string $db_name, bool $force = false): array {
+        public function create_db(string $db_name = null, bool $force = false): array {
             try {
+
+                $db_name = $db_name ?? $this->db_name;
+
                 if ($force) {
                     $this->pdo->exec("DROP DATABASE IF EXISTS `$db_name`");
                 }
 
                 $this->pdo->exec("CREATE DATABASE `$db_name`");
+                $this->db_name = $db_name;
 
             } catch (PDOException $e) { 
 
                 return [
                     'success' => false,
                     'message' => "Failed to create database {$db_name}. " . 
-                    (!$force ? "Try using force." : $e)
+                    (!$force ? "Try using force." : $e->getMessage())
                 ];
             }
 
@@ -39,20 +43,25 @@
          * Creates a new sql user.
          * Force first removes user.
          */
-        public function create_user(string $user_name, string $hostname = "%", bool $force = false): array {
+        public function create_user(string $user_name = null, string $hostname = "%", bool $force = false): array {
             try {
+
+                $user_name = $user_name ?? $this->db_user;
+
                 if ($force) {
                     $this->pdo->exec("DROP USER IF EXISTS `$user_name`");
                 }
 
                 $this->pdo->exec("CREATE USER `$user_name`");
+                $this->db_user = $user_name;
+                $this->hostname = $hostname;
 
             } catch (PDOException $e) { 
                 
                 return [
                     'success' => false,
                     'message' => "Failed to create user {$user_name}@{$hostname}. " . 
-                    (!$force ? "Try using force." : $e)
+                    (!$force ? "Try using force." : $e->getMessage())
                 ];
             }
 
@@ -66,8 +75,10 @@
          * function drop_db
          * Drops an existing database. 
          */
-        public function drop_db(string $db_name, bool $force = false): array { 
+        public function drop_db(string $db_name = null, bool $force = false): array { 
             try {
+
+                $db_name = $db_name ?? $this->db_name;
 
                 if ($force) {
                     $this->pdo->exec("DROP DATABASE `$db_name`");
@@ -82,7 +93,7 @@
             } catch (PDOException $e) {
                 return [
                     'success' => false,
-                    'message' => "Failed to remove database {$db_name}. {$e}"
+                    'message' => "Failed to remove database {$db_name}. {$e->getMessage()}"
                 ];
             }
         }
@@ -91,8 +102,11 @@
          * function drop_user
          * removes an already existing user.
          */
-        public function drop_user(string $user_name, string $hostname = "%", bool $force = false): array {
+        public function drop_user(string $user_name = null, string $hostname = "%", bool $force = false): array {
             try {
+
+                $user_name = $user_name ?? $this->db_user;
+
                 if ($force) {
                     $this->pdo->exec("DROP USER `$user_name`@`$hostname`");
                 } else {
@@ -115,11 +129,22 @@
          * function set_permissions
          * Set permission to access db for user.
          */
-        public function set_permissions(string $user_name, string $hostname, string $db_name) {
+        public function set_permissions(string $db_name = null, string $user_name = null, string $hostname = '%'): array {
             try {
-                $this->pdo->exec("GRANT ");
-            } catch (PDOException $e) {
+                $db_name = $db_name ?? $this->db_name;
+                $user_name = $user_name ?? $this->db_user;
 
+                $this->pdo->exec("GRANT ALL PRIVILEGES ON `$db_name`.* TO `$user_name`@`$hostname`");
+
+                return [
+                    'success' => true,
+                    'message' => "Successfully set permissions for {$user_name}@{$hostname} on {$db_name}."
+                ];
+            } catch (PDOException $e) {
+                return [
+                    'success' => false,
+                    'message' => "Failed to set permissions for {$user_name}@{$hostname} on {$db_name}: {$e->getMessage()}"
+                ];
             }
         }
 
