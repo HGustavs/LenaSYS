8b65e9eeb30c2f9cef573bfe9ef85ea26999f2e7
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-05-02 12:17:59 +0200

    fixed queries and link

--- DuggaSys/tests/microservices/showDuggaService/loadDugga_ms_test.php
+++ DuggaSys/tests/microservices/showDuggaService/loadDugga_ms_test.php
@@ -6,19 +6,18 @@ $testsData = array(
 
     'load dugga' => array(
         'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO variant (param, variantanswer) VALUES ('{testparam}','{Variant}');",
-        'query-before-test-2' => "INSERT INTO listentries (cid,vers, entryname, visible,creator) VALUES(1885,1337,'LDMOMENT',1,101);",
-        'query-before-test-3' => "SELECT MAX(vid) AS vid FROM variant",
-        'query-before-test-4' => "SELECT MAX(lid) AS moment FROM listentries",
-        'variables-query-before-test-5' => "vid, moment",
-        'query-before-test-5' => "INSERT INTO userAnswer (cid, variant, moment, hash) VALUES (1885, ?, ?, 'hnf3j58s');",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+        'query-before-test-1' => "INSERT INTO variant (vid,param, variantanswer) VALUES (991,'{testparam}','{Variant}');",
+        'query-before-test-2' => "INSERT INTO listentries (lid,cid,vers, entryname, visible,creator) VALUES(9991,1885,1337,'LDMOMENT',1,101);",
+        'query-before-test-3' => "INSERT INTO userAnswer (cid, variant, moment, hash) VALUES (1885, 991, 9991, 'hnf3j58s');",
+        'query-after-test-1' => "DELETE FROM userAnswer WHERE hash = 'hnf3j58s';",
+        'query-after-test-2' => "DELETE FROM variant WHERE vid = 991;",
+        'query-after-test-3' => "DELETE FROM listentries WHERE lid = 9991;",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/loadDugga_ms.php',
         'service-data' => serialize(array( // Data that service needs to execute function
             'username' => 'brom',
             'password' => 'password',
-            'hash' => 'PWVykkqQ',
-            'moment' => '2002',
-            //'moment' => '<!query-before-test-4!><*[0]["moment"]*>',
+            'hash' => 'hnf3j58s',
+            'moment' => '9991',
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'none'
