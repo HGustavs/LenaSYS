b9f8444e96dc90577cdcab619059c874ab1dfb11
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-05-02 12:31:28 +0200

    fixed expected output

--- DuggaSys/tests/microservices/showDuggaService/loadDugga_ms_test.php
+++ DuggaSys/tests/microservices/showDuggaService/loadDugga_ms_test.php
@@ -5,10 +5,10 @@ include "../../../../Shared/test.php";
 $testsData = array(
 
     'load dugga' => array(
-        'expected-output' => '{"debug":"[Guest] Missing hash\/password\/variant!","param":"{}","answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":0,"variants":[],"duggaTitle":"UNK","hash":"UNK","hashpwd":"UNK","opt":"UNK","link":"UNK"}',
-        'query-before-test-1' => "INSERT INTO variant (vid,param, variantanswer) VALUES (991,'{testparam}','{Variant}');",
+        'expected-output' => '{"variant":"991","answer":"useranswer","variantanswer":"{variantanswer}","param":"{testparam}"}',
+        'query-before-test-1' => "INSERT INTO variant (vid,param, variantanswer) VALUES (991,'{testparam}','{variantanswer}');",
         'query-before-test-2' => "INSERT INTO listentries (lid,cid,vers, entryname, visible,creator) VALUES(9991,1885,1337,'LDMOMENT',1,101);",
-        'query-before-test-3' => "INSERT INTO userAnswer (cid, variant, moment, hash) VALUES (1885, 991, 9991, 'hnf3j58s');",
+        'query-before-test-3' => "INSERT INTO userAnswer (cid, variant, moment,useranswer, hash) VALUES (1885, 991, 9991,'useranswer', 'hnf3j58s');",
         'query-after-test-1' => "DELETE FROM userAnswer WHERE hash = 'hnf3j58s';",
         'query-after-test-2' => "DELETE FROM variant WHERE vid = 991;",
         'query-after-test-3' => "DELETE FROM listentries WHERE lid = 9991;",
@@ -20,7 +20,11 @@ $testsData = array(
             'moment' => '9991',
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
-            'none'
+            'variant',
+            'answer',
+            'variantanswer',
+            'param',
+
         )),
     ),
 );
