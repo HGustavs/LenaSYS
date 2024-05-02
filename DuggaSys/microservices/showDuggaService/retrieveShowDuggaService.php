16f471aec3ef5249bf329d258ff504c036040016
Albin <aljo940616@gmail.com>
2024-05-02 13:43:52 +0200

    made a new file and populated it with the creation of the dataoutput array and echoed it

---
+++ DuggaSys/microservices/showDuggaService/retrieveShowDuggaService.php
@@ -0,0 +1,39 @@
+<?php
+
+$array = array(
+		"debug" => $debug,
+		"param" => $param,
+		"answer" => $answer,
+		"danswer" => $variantanswer,
+		"score" => $score,
+		"highscoremode" => $highscoremode,
+		"grade" => $grade,
+		"submitted" => $submitted,
+		"marked" => $marked,
+		"deadline" => $duggainfo['deadline'],
+		"release" => $duggainfo['qrelease'],
+		"files" => $files,
+		"userfeedback" => $userfeedback,
+		"feedbackquestion" => $feedbackquestion,
+		"variant" => $savedvariant,
+		"ishashindb" => $ishashindb,
+		"variantsize" => $variantsize,
+		"variantvalue" => $variantvalue,
+		"password" => $password,
+		"hashvariant" => $hashvariant,
+		"isFileSubmitted" => $isFileSubmitted,
+		"isTeacher" => $isTeacher, // isTeacher is true for both teachers and superusers
+		"variants" => $variants,
+		"duggaTitle" => $duggatitle,
+		"hash" => $hash,
+		"hashpwd" => $hashpwd,
+		"opt" => $opt,
+		"link" => $link,
+		"activeusers" => $active,
+
+	);
+if (strcmp($opt, "GRPDUGGA")==0) $array["group"] = $group;
+header('Content-Type: application/json');
+echo json_encode($array);
+
+?>
