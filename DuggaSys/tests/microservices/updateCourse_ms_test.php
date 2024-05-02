f6d25f7bb9298588e9536ded8260c5f5a96b265c
Ilirr <61836150+Ilirr@users.noreply.github.com>
2024-05-02 12:49:55 +0200

    Update updateCourse_ms_test.php

--- DuggaSys/tests/microservices/updateCourse_ms_test.php
+++ DuggaSys/tests/microservices/updateCourse_ms_test.php
@@ -9,18 +9,19 @@ $testsData = array(
     // Test for updateCourse_ms.php
     //------------------------------------------------------------------------------------------
     'update_course' => array(
-        'expected-output' => '1',  
-        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/courseedService/createNewCourse_ms.php',
+        'expected-output' => '1', 
+       // 'query-after-test-1' => "UPDATE course SET coursename='Testing-Course', coursecode='G1337', visibility=0, courseGitURL='http://localhost/LenaSYS/DuggaSys/sectioned.php?courseid=1885&coursename=Testing-Course&coursevers=1337' WHERE cid=1885;",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/courseedService/updateCourse_ms.php',
         'service-data' => serialize(
             array( 
                 'opt' => 'UPDATE',
                 'username' => 'brom',
                 'password' => 'password',
-                'cid' => 9999,
-                'coursename' => 'Updated Course',
+                'cid' => 1885,
+                'coursename' => 'Updated-Course',
                 'visibility' => 1,
-                'coursecode' => 'UPD001',
-                'courseGitURL' => 'http://localhost/LenaSYS/DuggaSys/sectioned.php?courseid=9999&coursename=Test%20Course&coursevers=1337',
+                'coursecode' => 'G1337',
+                'courseGitURL' => 'http://localhost/LenaSYS/DuggaSys/sectioned.php?courseid=1885&coursename=Updated-Course&coursevers=1337',
             )
         ),
         'filter-output' => serialize(
