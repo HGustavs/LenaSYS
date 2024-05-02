7cdc41d6a817c08671afdf8522c3e32b87a52d0c
c22emigr <c22emigr@student.his.se>
2024-05-02 14:11:07 +0200

    changed name of tests

--- DuggaSys/tests/microservices/updateUserCourse_ms_test.php
+++ DuggaSys/tests/microservices/updateUserCourse_ms_test.php
@@ -27,7 +27,7 @@ $testsData = array(
         ),
     ),
 
-    'updateActiveUsers_ms' => array(
+    'updateUserCourse_ms' => array(
         'expected-output' => '999',
         'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 999);",
         'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
@@ -48,7 +48,7 @@ $testsData = array(
         ),
     ),
 
-    'updateActiveUsers_ms' => array(
+    'updateUserCourse_ms' => array(
         'expected-output' => '999',
         'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 999);",
         'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
@@ -69,7 +69,7 @@ $testsData = array(
         ),
     ),
 
-    'updateActiveUsers_ms' => array(
+    'updateUserCourse_ms' => array(
         'expected-output' => '999',
         'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 999);",
         'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
