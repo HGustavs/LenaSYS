d603ca36371c91e25a9d63dd12b6a2361a54db1d
Ilirr <61836150+Ilirr@users.noreply.github.com>
2024-05-03 11:55:44 +0200

    Update deleteCodeExample_ms_test.php

---
+++ DuggaSys/tests/microservices/codeviewerService/deleteCodeExample_ms_test.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "../../../../Shared/test.php";
+
+$testsData = array(
+    'deleteCodeExample' => array(
+        'expected-output' => '"deleted": true',
+
+        'query-after-test-1' => "INSERT INTO improw (exampleid) VALUES (:exampleid);",
+        'query-after-test-2' => "INSERT INTO box (exampleid) VALUES (:exampleid);",
+        'query-after-test-3' => "INSERT INTO impwordlist (exampleid) VALUES (:exampleid);",
+        'query-after-test-4' => "INSERT INTO codeexample (exampleid) VALUES (:exampleid);",
+        'query-after-test-5' => "INSERT INTO listentries (lid) VALUES (:lid);",
+
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/deleteCodeExample_ms.php',
+        'service-data' => serialize(
+            array(
+                'opt' => 'DELETEEXAMPLE',
+                'exampleid' => '1001', 
+                'boxid' => '501',
+                'lid' => '301' 
+            )
+        ),
+        'filter-output' => serialize(
+            array(
+                'none'
+            )
+        ),
+    ),
+);
+
+testHandler($testsData, true); // 2nd argument (prettyPrint): true = pretty print (HTML), false = raw JSON
