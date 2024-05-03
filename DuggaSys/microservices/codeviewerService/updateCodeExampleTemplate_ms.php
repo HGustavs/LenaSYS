49df0f8a9603a3552d227e559f259e2599a08be2
Erkan <a22eriwi@student.his.se>
2024-05-03 13:58:58 +0200

    added timezone

--- DuggaSys/microservices/codeviewerService/updateCodeExampleTemplate_ms.php
+++ DuggaSys/microservices/codeviewerService/updateCodeExampleTemplate_ms.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set("Europe/Stockholm");
+
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
 include_once "../sharedMicroservices/getUid_ms.php";
