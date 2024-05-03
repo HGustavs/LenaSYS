41ce71bd1189e93380a681b6598fd5e98bc9c1e6
c22mohel <c22mohel@student.his.se>
2024-05-03 12:30:55 +0200

    inserting user before query

--- DuggaSys/tests/microservices/adduser_ms_test.php
+++ DuggaSys/tests/microservices/adduser_ms_test.php
@@ -8,15 +8,16 @@ $testsData = array(
     //------------------------------------------------------------------------------------------
     'updateActiveUsers_ms' => array(
         'expected-output' => '999',
-        'query-before-test-1' => "INSERT INTO course (coursecode,coursename,visibility,creator, hp, courseGitURL) VALUES('test','toBeDeleted', 0, 1, 7.5, null)",
-        'query-after-test-2' => "DELETE FROM user WHERE username = 'test';",
+        'query-before-test-1' => "INSERT INTO course (cid,coursecode,coursename,visibility,creator, hp, courseGitURL) VALUES(999,'test','toBeDeleted', 0, 1, 7.5, null)",
+        'query-before-test-2' => "INSERT INTO user (uid, username, email, firstname, lastname, ssn, password,addedtime, class) VALUES(9999 , 'hacker', 'hacker@student.his.se', 'tester', 'test', 00000000-0000, 'test',now(), WEBUG14h);",
+        'query-after-test-1' => "DELETE FROM user WHERE username = 'test';",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/addUser_ms.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
                 'opt' => 'ADDUSR',
                 'username' => 'brom',
                 'password' => 'password',
-                'courseid' => '2',
+                'courseid' => '999',
                 'coursevers' => '97732',
                 'hash' => 'hjk4ert6',
                 'AUtoken' => 999
