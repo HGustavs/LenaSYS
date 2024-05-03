e9e05b04807d83d951d03cf9b609838fd28d1428
c20juler <81637183+c20juler@users.noreply.github.com>
2024-05-03 14:56:51 +0200

    Update updateUser_ms_test.php
    fixed test 5 update-class

--- DuggaSys/tests/microservices/accessedService/updateUser_ms_test.php
+++ DuggaSys/tests/microservices/accessedService/updateUser_ms_test.php
@@ -101,58 +101,66 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
         )),
     ),
 
-    /*
+    
     //TEST #4
     //Update username
     'Update-username' => array(  
+        'expected-output' => '{"entries":[{"username":"{\"username\":\"testuser1\"}","username":"{\"username\":\"testuser2\"}"}]}',
         //Pre-values
-        'expected-output' => '{"output":"PLACEHOLDER"}',
-        'query-before-test-1' => "INSERT INTO user(username, password) VALUES ('testuser1', 'testpwd');",
-        'query-before-test-2' => "SELECT uid FROM user WHERE username = 'testuser1';",
-        'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1';",
-        'query-after-test-2' => "DELETE FROM user WHERE username = 'testuser2';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/accessedservice.php',
+        'query-before-test-1' => "INSERT INTO user(uid, username, password) VALUES (9997, 'testuser1', 'testpwd');",
+        'query-before-test-2' => "INSERT INTO user_course(uid, cid, access) VALUES (9997, 1885, 'R');",
+        'query-after-test-1' => "DELETE FROM user_course WHERE uid = '9997';",
+        'query-after-test-2' => "DELETE FROM user WHERE uid = '9997';",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/updateUsers_ms.php',
         'service-data' => serialize(
             array(
                 // Data that service needs to execute function
                 'username' => 'brom',
                 'password' => 'password',
+                'courseid' => '1885',
                 'opt' => 'UPDATE',
+                'uid' => '9997',
                 'prop' => 'username',
                 'val' => 'testuser2',
-                'uid' => '<!query-before-test-2!><*[0]["uid"]*>',
             )),
         'filter-output' => serialize(array(
                 // Filter what output to use in assert test, use none to use all ouput from service
-                'none'
-            )),
+                'entries' => array(
+                    'username'
+                ),
+        )),
     ),
 
+    
     //TEST #5
     //Update class
     'Update-class' => array(  
+        'expected-output' => '{"entries":[{"username":"{\"username\":\"testuser1\"}","class":"{\"class\":\"DVSUG\"}"}]}',
         //Pre-values
-        'expected-output' => '{"output":"PLACEHOLDER"}',
-        'query-before-test-1' => "INSERT INTO user(username, password) VALUES ('testuser1', 'testpwd');",
-        'query-before-test-2' => "SELECT uid FROM user WHERE username = 'testuser1';",
-        'variables-query-before-test-1' => "uid",
-        'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/accessedservice.php',
+        'query-before-test-1' => "INSERT INTO user(uid, username, password) VALUES (9997, 'testuser1', 'testpwd');",
+        'query-before-test-2' => "INSERT INTO user_course(uid, cid, access) VALUES (9997, 1885, 'R');",
+        'query-after-test-1' => "DELETE FROM user_course WHERE uid = '9997';",
+        'query-after-test-2' => "DELETE FROM user WHERE uid = '9997';",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/updateUsers_ms.php',
         'service-data' => serialize(
             array(
                 // Data that service needs to execute function
                 'username' => 'brom',
                 'password' => 'password',
+                'courseid' => '1885',
                 'opt' => 'UPDATE',
+                'uid' => '9997',
                 'prop' => 'class',
-                'val' => 'test',
-                'uid' => '<!query-before-test-2!><*[0]["uid"]*>',
+                'val' => 'DVSUG',
             )),
         'filter-output' => serialize(array(
                 // Filter what output to use in assert test, use none to use all ouput from service
-                'none'
-            )),
-    )*/
+                'entries' => array(
+                    'username',
+                    'class'
+                ),
+        )),
+    )
 );
 
 testHandler($testsData, true); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
