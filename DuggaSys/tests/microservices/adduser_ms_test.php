3ecab24c8e9e762fd0e547a974f69376d36c6dad
c22mohel <c22mohel@student.his.se>
2024-05-03 15:53:19 +0200

    removing after query

--- DuggaSys/tests/microservices/adduser_ms_test.php
+++ DuggaSys/tests/microservices/adduser_ms_test.php
@@ -7,10 +7,12 @@ $testsData = array(
     // This tests the microservice updateActiveUsers_ms 
     //------------------------------------------------------------------------------------------
     'updateActiveUsers_ms' => array(
-        'expected-output' => '999',
+        'expected-output' => '9999',
         'query-before-test-1' => "INSERT INTO course (cid,coursecode,coursename,visibility,creator, hp, courseGitURL) VALUES(999,'test','toBeDeleted', 0, 1, 7.5, null)",
         'query-before-test-2' => "INSERT INTO user (uid, username, email, firstname, lastname, ssn, password,addedtime, class) VALUES(9999 , 'hacker', 'hacker@student.his.se', 'tester', 'test', 00000000-0000, 'test',now(), WEBUG14h);",
-        'query-after-test-1' => "DELETE FROM user WHERE username = 'test';",
+        'query-after-test-1' => "DELETE FROM user WHERE uid = 9999;",
+        'query-after-test-2' => "DELETE FROM course WHERE cid = 999;",
+        'query-after-test-3' => "DELETE FROM user_course WHERE cid = 999;",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/addUser_ms.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
@@ -18,6 +20,7 @@ $testsData = array(
                 'username' => 'brom',
                 'password' => 'password',
                 'courseid' => '999',
+                'newusers' =Z 'hacker@student.his.se',
                 'coursevers' => '97732',
                 'hash' => 'hjk4ert6',
                 'AUtoken' => 999
@@ -30,5 +33,3 @@ $testsData = array(
         ),
     ),
 );
-
-testHandler($testsData, true);
