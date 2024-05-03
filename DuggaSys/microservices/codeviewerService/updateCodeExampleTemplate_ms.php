4aa725f528fbfd911321b752d642aeb7bf534a61
c22emigr <c22emigr@student.his.se>
2024-05-03 13:26:01 +0200

    checks userid

--- DuggaSys/microservices/codeviewerService/updateCodeExampleTemplate_ms.php
+++ DuggaSys/microservices/codeviewerService/updateCodeExampleTemplate_ms.php
@@ -2,9 +2,12 @@
 
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
+include_once "../sharedMicroservices/getUid_ms.php";
 
 pdoConnect();
 session_start();
+
+$userid = getUid();
 
 $opt=getOP('opt');
 $templateNumber=getOP('templateno');
