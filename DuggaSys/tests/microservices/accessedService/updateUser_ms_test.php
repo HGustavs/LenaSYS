6ba8ca1b6f202a00cb8d18232325a02b4ff7276a
c20juler <81637183+c20juler@users.noreply.github.com>
2024-05-03 14:39:12 +0200

    Update updateUser_ms_test.php
    fixed test 2 for update-lastname

--- DuggaSys/tests/microservices/accessedService/updateUser_ms_test.php
+++ DuggaSys/tests/microservices/accessedService/updateUser_ms_test.php
@@ -1,5 +1,4 @@
 <?php
-
 //------------------------------------------------------------------------------------------
 // Test for the microservice updateUser_ms
 // Tests that updating firstname, lastname, ssn, username and class works as expected
@@ -14,13 +13,13 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
     //TEST #1
     //Update firstname
     'Update-firstname' => array(  
-        'expected-output'   => '{"entries":[{"username":"{\"username\":\"testuser1\"}","firstname":"{\"firstname\":\"testuser2\"}"}]}',
+        'expected-output' => '{"entries":[{"username":"{\"username\":\"testuser1\"}","firstname":"{\"firstname\":\"testfirstname\"}"}]}',
         //Pre-values
         'query-before-test-1' => "INSERT INTO user(uid, username, password) VALUES (9997, 'testuser1', 'testpwd');",
         'query-before-test-2' => "INSERT INTO user_course(uid, cid, access) VALUES (9997, 1885, 'R');",
         'query-after-test-1' => "DELETE FROM user_course WHERE uid = '9997';",
         'query-after-test-2' => "DELETE FROM user WHERE uid = '9997';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/accessedservice.php',
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/updateUsers_ms.php',
         'service-data' => serialize(
             array(
                 // Data that service needs to execute function
@@ -30,7 +29,7 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
                 'opt' => 'UPDATE',
                 'uid' => '9997',
                 'prop' => 'firstname',
-                'val' => 'testuser2',
+                'val' => 'testfirstname',
             )),
         'filter-output' => serialize(array(
                 // Filter what output to use in assert test, use none to use all ouput from service
@@ -43,30 +42,35 @@ $testsData = array(   // Test-data is saved on this array that is then tested in
 
     //TEST #2
     //Update lastname
-    /*'Update-lastname' => array(
+    'Update-lastname' => array(
+        'expected-output' => '{"entries":[{"username":"{\"username\":\"testuser1\"}","lastname":"{\"lastname\":\"testlastname\"}"}]}',
         //Pre-values
-        'expected-output' => '{"output":"PLACEHOLDER"}',
-        'query-before-test-1' => "INSERT INTO user(username, password) VALUES ('testuser1', 'testpwd');",
-        'query-before-test-2' => "SELECT uid FROM user WHERE username = 'testuser1';",
-        'variables-query-before-test-1' => "uid",
-        'query-after-test-1' => "DELETE FROM user WHERE username = 'testuser1';",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/accessedservice.php',
+        'query-before-test-1' => "INSERT INTO user(uid, username, password) VALUES (9997, 'testuser1', 'testpwd');",
+        'query-before-test-2' => "INSERT INTO user_course(uid, cid, access) VALUES (9997, 1885, 'R');",
+        'query-after-test-1' => "DELETE FROM user_course WHERE uid = '9997';",
+        'query-after-test-2' => "DELETE FROM user WHERE uid = '9997';",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/updateUsers_ms.php',
         'service-data' => serialize(
             array(
                 // Data that service needs to execute function
                 'username' => 'brom',
                 'password' => 'password',
+                'courseid' => '1885',
                 'opt' => 'UPDATE',
+                'uid' => '9997',
                 'prop' => 'lastname',
-                'val' => 'test',
-                'uid' => '<!query-before-test-2!><*[0]["uid"]*>',
+                'val' => 'testlastname',
             )),
         'filter-output' => serialize(array(
                 // Filter what output to use in assert test, use none to use all ouput from service
-                'none'
+                'entries' => array(
+                    'username',
+                    'lastname'
+                ),
             )),
-    ),
+        ),
 
+    /*
     //TEST #3
     //Update ssn
     'Update-ssn' => array(  
