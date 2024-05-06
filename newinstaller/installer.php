db0436dfa0b3cc955de9b7cf1b1333ea8bdf22e8
William Lindholm <a22willi@student.his.se>
2024-05-06 07:58:14 +0200

    Change installer to route to backend or frontend.

--- newinstaller/installer.php
+++ newinstaller/installer.php
@@ -1,6 +1,9 @@
 <?php 
-    include_once("installer_ui.php");
-    foreach (glob("tools/*.php") as $tools) {
-        include_once $tools;
+    if (isset($_GET['stream'])) {
+        // Script that runs the installer
+        // Sends installation progress messages at: installer.php?stream=true
+        include_once("tools/install_engine.php");
+    } else {
+        // Displays installer UI
+        include_once("installer_ui.php");
     }
-?>
