e87233be9c664aafe6ccb73ffdf9b10aab3b82da
a22willi <a22willi@student.his.se>
2024-05-06 12:21:13 +0200

    Changed indent to tabs.

--- newinstaller/installer_ui.php
+++ newinstaller/installer_ui.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Installer Progress</title>
+	<meta charset="UTF-8">
+	<title>Installer Progress</title>
 </head>
 <body>
-    <h1>Installation Progress</h1>
-    <div>
+	<h1>Installation Progress</h1>
+	<div>
 		Progress: <span id="progressPercentage">0%</span>
 	</div>
-    <progress id="progressBar" value="0" max="100"></progress>
+	<progress id="progressBar" value="0" max="100"></progress>
 	<div style="height: 200px; overflow: scroll;">
 		<span id="progress"></span>
 	</div>
 
 	<script src="tools/sse_receiver.js"></script>
-    <script>
+	<script>
 		let progressInfo = document.getElementById("progress");
 		let progressPercentage = document.getElementById("progressPercentage");
-        let progressBar = document.getElementById("progressBar");
+		let progressBar = document.getElementById("progressBar");
 		const getCurrentValue = () => parseInt(progressBar.value);
 
 		fetch('installer.php', {
@@ -26,14 +26,14 @@
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded'
 			},
-			body: 'installation_settings=' + encodeURIComponent(JSON.stringify({ verbose: 'true', overwrite_db: 'true', overwrite_user: 'true'}))
+			body: 'installation_settings=' + encodeURIComponent(JSON.stringify({ verbose: 'false', overwrite_db: 'true', overwrite_user: 'true'}))
 		});
 
-        let sseReceiver = new SSEReceiver({
-            message: function(data) {
-                progressInfo.innerHTML = data + "<br>" + progressInfo.innerHTML;
-            },
-            updateProgress: function(data) {
+		let sseReceiver = new SSEReceiver({
+			message: function(data) {
+				progressInfo.innerHTML = data + "<br>" + progressInfo.innerHTML;
+			},
+			updateProgress: function(data) {
 				let targetValue = parseInt(data);
 				let distance = Math.abs(targetValue - getCurrentValue());
 				let speedFactor = Math.max(4, distance / 100);
@@ -54,7 +54,7 @@
 				
 				update();
 			}
-        });
-    </script>
+		});
+	</script>
 </body>
 </html>
