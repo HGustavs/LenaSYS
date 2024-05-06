23566d163f8322b5ca2f9cab472aa5a3bcffa453
c22mohel <c22mohel@student.his.se>
2024-05-06 16:14:51 +0200

    adding the right variable to newuser

--- DuggaSys/tests/microservices/adduser_ms_test.php
+++ DuggaSys/tests/microservices/adduser_ms_test.php
@@ -1,29 +1,27 @@
 <?php
 
-include "../../../Shared/test.php";
+
+$newusers_value = '[[null,"Help","He","Help@student.his.se",null,"HT-20",null]]';
 
 $testsData = array(
     //------------------------------------------------------------------------------------------
     // This tests the microservice addUser_ms 
     //------------------------------------------------------------------------------------------
     'addUser_ms' => array(
-        'expected-output' => '9999',
+        'expected-output' => '999',
         'query-before-test-1' => "INSERT INTO course (cid,coursecode,coursename,visibility,creator, hp, courseGitURL) VALUES(999,'test','toBeDeleted', 0, 1, 7.5, null)",
-        'query-before-test-2' => "INSERT INTO user (uid, username, email, firstname, lastname, ssn, password,addedtime, class) VALUES(9999 , 'hacker', 'hacker@student.his.se', 'tester', 'test', 00000000-0000, 'test',now(), WEBUG14h);",
-        'query-after-test-1' => "DELETE FROM user WHERE uid = 9999;",
-        'query-after-test-2' => "DELETE FROM course WHERE cid = 999;",
-        'query-after-test-3' => "DELETE FROM user_course WHERE cid = 999 AND uid = 9999;"
-        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/addUser_ms.php',
+        'query-after-test-1' => "DELETE FROM course WHERE cid = 999;",
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/addUser_ms.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
                 'opt' => 'ADDUSR',
                 'username' => 'brom',
                 'password' => 'password',
                 'courseid' => '999',
-                'newusers' =Z 'hacker@student.his.se',
                 'coursevers' => '97732',
-                'hash' => 'hjk4ert6'
-                // 'AUtoken' => 999
+                'newusers' => $newusers_value,,
+                'hash' => 'hjk4ert6',
+                'AUtoken' => 999
             )
         ),
         'filter-output' => serialize(
@@ -33,3 +31,18 @@ $testsData = array(
         ),
     ),
 );
+
+testHandler($testsData, true);
+
+
+// $opt = getOP('opt');
+// $cid = getOP('courseid');
+// $newusers = getOP('newusers');
+// $coursevers = getOP('coursevers');
+// $log_uuid = getOP('log_uuid');
+
+
+
+
+
+?>
