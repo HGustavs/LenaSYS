a046c28097a2ac1b07579d0b90bf949905184fd6
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-05-06 12:32:56 +0200

    made test for user

--- DuggaSys/tests/microservices/profileService/updateUserPassword_ms_test.php
+++ DuggaSys/tests/microservices/profileService/updateUserPassword_ms_test.php
@@ -3,12 +3,15 @@
 include "../../../../Shared/test.php";
 
 $testsData = array(
-    'update password' => array(
-        'expected-output' => '{"blabla}"}',
+    'update password (user)' => array(
+        'expected-output' => '{"success":true,"status":"","debug":"NONE!"}',
+        'query-before-test-1' => "INSERT INTO user(username, password) VALUES ( 'testuser1', '\$2y\$10\$qGYP1gTTr7SG5/WbRlxCm.1tPmmZonKSYlAJaLj00pBEAJxyeKtI2');",
+        'query-after-test-1' => "DELETE FROM user WHERE username ='testuser1';",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/profileService/updateUserPassword_ms.php',
         'service-data' => serialize(array( // Data that service needs to execute function
-            'username' => 'a99birol',
+            'username' => 'testuser1',
             'password' => 'password',
+            'action' => 'password',
             'newPassword' => 'Newpassword1',
         )),
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
@@ -18,5 +21,7 @@ $testsData = array(
         )),
     ),
 );
+
+
 
 testHandler($testsData, true); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
