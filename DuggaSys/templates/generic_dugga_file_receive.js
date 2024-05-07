ba50d4b83c39f45942026c1e8795c4863ba923ca
a22natth <a22natth@student.his.se>
2024-05-07 15:15:10 +0200

    Removed inline styling
    Removed inline styling and replaced it with classes.

--- DuggaSys/templates/generic_dugga_file_receive.js
+++ DuggaSys/templates/generic_dugga_file_receive.js
@@ -364,17 +364,17 @@ function createFileUploadArea(fileuploadfileds){
 		str += "<div class='instructionBox'>";
 		str += "<div class='submissionInstructions'>";
 		if (type === "pdf"){
-			str += "<h4>Pdf Submission and Preview</h4>";
+			str += "<h4 class='submissionBoxTitles'>Pdf Submission and Preview</h4>";
 		} else if (type === "link"){
 			str += "<h4>Link Submission and Preview</h4>";
 		} else if (type === "zip") {
-			str += "<h4>Zip / Rar file Upload</h4>";
+			str += "<h4 class='submissionBoxTitles'>Zip / Rar file Upload</h4>";
 		} else if (type === "multi"){
 			str += "<h4>Multiple file Upload</h4>";
 		} else if (type === "text"){
 			str += "<h4>Text Submission</h4>";
 			str += "</div>";
-      str +="<div id='"+fieldname+"Prev' style='min-height:100px;background:#f8f8ff;padding:10px;border-top:2px 2px solid #d3d3d3;border-bottom:2px 2px solid #d3d3d3;'><span style='font-style:italic;M'>Submission History</span></div>";
+      str +="<div id='"+fieldname+"Prev'<span style='font-style:italic;M'>Submission History</span></div>";
 			str += "<div style='padding:10px;'>";
 			str +="<table style='width:100%;'>";
 			str +="<tr>";
@@ -389,7 +389,7 @@ function createFileUploadArea(fileuploadfileds){
 		str += "</div>";
 		str += "<div>";
 		if (type !== "text"){
-            str +="<div id='"+fieldname+"Prev' style='min-height:100px;padding:10px;border-top:2px 2px solid #d3d3d3;border-bottom:2px 2px solid #d3d3d3;'><span style='font-stile:italic;'>Submission History</span></div>";
+            str +="<div id='"+fieldname+"Prev'><span style='font-stile:italic;'>Submission History</span></div>";
             str +="<div style='padding:10px;'>";
             str +="<h4>Instructions</h4>";
             str +="<div id='"+fieldname+"Instruction' style='font-style: italic;padding:0px;'></div>"
