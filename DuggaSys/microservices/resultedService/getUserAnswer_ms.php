533a9c1bf17304d4f24214091a35f47c7d2691c9
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-07 14:25:10 +0200

    changed back to fix later

--- DuggaSys/microservices/resultedService/getUserAnswer_ms.php
+++ DuggaSys/microservices/resultedService/getUserAnswer_ms.php
@@ -5,7 +5,7 @@ date_default_timezone_set("Europe/Stockholm");
 // Include basic application services!
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
-include_once "./retrieveResultedService_ms.php";
+include_once "retrieveResultedService_ms.php";
 
 // Connect to database and start session
 pdoConnect();
