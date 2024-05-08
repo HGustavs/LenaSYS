7764def39a8216a9c10b2f394207437fe3c97886
b22marah <b22marah@student.his.se>
2024-05-08 12:12:30 +0200

    Update installer_ui.php
    Added viewport meta-tag to support different screen sizes, to better fit smaller devices

--- newinstaller/installer_ui.php
+++ newinstaller/installer_ui.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Installer Progress</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<?php include 'tools/components.php'; ?>
