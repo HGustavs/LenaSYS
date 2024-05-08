b1c4fd6bf8ace27ec1a92dd7c7271c2aa4d2b111
f22linbe <f22linbe@student.his.se>
2024-05-08 15:09:17 +0200

    Changed uid to higher value

--- DuggaSys/tests/microservices/profileService/updateSecurityQuestion_ms_test.php
+++ DuggaSys/tests/microservices/profileService/updateSecurityQuestion_ms_test.php
@@ -4,16 +4,16 @@ include "../../../../Shared/test.php";
 // Hashed password for password verfication
 $testPassword = password_hash('secret', PASSWORD_BCRYPT);
 
-// TODO 'uid' should not be hard coded or manually inserted in USER table. Modify the testHandler function to keep same code structure.
+// TODO 'uid' should not be hard coded or manually inserted in USER table.
 
 $testdata = [
     'update security question' => [
         'expected-output' => '{"success":true,"status":"","debug":"NONE!"}', // Updating security question & answer was successful
-        'query-before-test' => "INSERT INTO user (uid, username, password, securityquestion, securityquestionanswer) VALUES (200000, 'testuser', '$testPassword', 'What is old question', 'old');", // Insert a user with old security question
-        'query-after-test' => "DELETE FROM user WHERE uid = 200000;", // Clean up after the test
+        'query-before-test' => "INSERT INTO user (uid, username, password, securityquestion, securityquestionanswer) VALUES (4000000, 'testuser', '$testPassword', 'What is old question', 'old');", // Insert a user with old security question
+        'query-after-test' => "DELETE FROM user WHERE uid = 4000000;", // Clean up after the test
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/profileService/updateSecurityQuestion_ms.php',
         'service-data' => serialize([ // Data that the service needs to execute the function
-            'userid' => 200000, 
+            'userid' => 4000000, 
             'username' => 'testuser',
             'password' => 'secret', 
             'action' => 'challenge',
