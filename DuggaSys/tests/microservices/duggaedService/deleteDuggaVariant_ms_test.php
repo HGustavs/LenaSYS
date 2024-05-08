6cfa187821eebeae13550c257a7d97144ddbf22f
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-05-08 15:56:31 +0200

    updated test

--- DuggaSys/tests/microservices/duggaedService/deleteDuggaVariant_ms_test.php
+++ DuggaSys/tests/microservices/duggaedService/deleteDuggaVariant_ms_test.php
@@ -4,11 +4,13 @@ include "../../../../Shared/test.php";
 
 $testsData = array(
     'delete variant' => array(
-        'expected-output' => '{"debug":"NONE!","writeaccess":true,"coursecode":"IT118G","coursename":"Webbutveckling - datorgrafik","variants":["{\"danswer\":\"00000010 0 2\"}","{\"danswer\":\"00000101 0 5\"}","{\"danswer\":\"00002 0 A\"}","{\"danswer\":\"00011001 1 9\"}","{\"danswer\":\"02111 5 7\"}","{\"danswer\":\"11000000 C 0\"}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","{Variant}","B","A","C","A","","",""]}',
+        'expected-output' => '{"variants":["othervariant"]}',
         'query-before-test-1' => "INSERT INTO quiz(id, cid,vers, qname) VALUES (333,1885,1337,'testQuiz')",
-        'query-before-test-2' => "INSERT INTO variant(quizID, param, variantanswer) VALUES (333,'param','delvariAnswer')",
-        'query-before-test-3' => "SELECT vid FROM variant WHERE variantanswer = 'delvariAnswer'",
-        'query-after-test-1' => "DELETE FROM quiz WHERE id = 333",
+        'query-before-test-2' => "INSERT INTO variant(quizID, param, variantanswer) VALUES (333,'param','deletevariant');",
+        'query-before-test-3' => "INSERT INTO variant(quizID, param, variantanswer) VALUES (333,'param','othervariant');",
+        'query-before-test-4' => "SELECT vid FROM variant WHERE variantanswer = 'deletevariant'",
+        'query-after-test-1' => "DELETE FROM variant WHERE variantanswer = 'othervariant'",
+        'query-after-test-2' => "DELETE FROM quiz WHERE id = 333",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/duggaedService/deleteDuggaVariant_ms.php',
         'service-data' => serialize(
             array(
@@ -18,16 +20,12 @@ $testsData = array(
                 'opt' => 'DELVARI',
                 'cid' => 1885,
                 'coursevers' => 1337,
-                'vid' => '<!query-before-test-3!><*[0]["vid"]*>',
+                'vid' => '<!query-before-test-4!><*[0]["vid"]*>',
             )
         ),
         'filter-output' => serialize(
             array(
                 // Filter what output to use in assert test, use none to use all ouput from service
-                'debug',
-                'writeaccess',
-                'coursename',
-                'coursecode',
                 'variants',
             )
         ),
