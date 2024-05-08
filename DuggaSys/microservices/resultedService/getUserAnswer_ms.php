31adaf990a9b888906521d2edc2c7845e71bc939
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-08 15:19:27 +0200

    fixed include in getUserAnswer_ms.php

--- DuggaSys/microservices/resultedService/getUserAnswer_ms.php
+++ DuggaSys/microservices/resultedService/getUserAnswer_ms.php
@@ -6,7 +6,7 @@ date_default_timezone_set("Europe/Stockholm");
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
 include_once "../sharedMicroservices/getUid_ms.php";
-include_once "retrieveResultedService_ms.php";
+include_once "./retrieveResultedService_ms.php";
 
 // Connect to database and start session
 pdoConnect();
