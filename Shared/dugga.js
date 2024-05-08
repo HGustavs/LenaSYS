3e8734bf143488ccd4e3800e5b70be5677b7e077
a22oscja <a22oscja@student.his.se>
2024-05-08 14:27:26 +0200

    logging out now moves you to courseed if in profile, pushnotifications should work better

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1680,7 +1680,12 @@ function processLogout() {
 		success:function(data) {
 			localStorage.removeItem("ls-security-question");
 			localStorage.removeItem("securitynotification");
-			reloadPage();
+			if(window.location.pathname == "/LenaSYS/DuggaSys/profile.php"){
+				window.location.href = "courseed.php";
+			}
+			else{
+				reloadPage();
+			}
 		},
 		error:function() {
 			console.log("error");
