93168b9822557b7002b2eb5862d09806e5bea4b8
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-08 15:25:11 +0200

    fixed same type of include in the folder sectionedService

--- DuggaSys/microservices/sectionedService/setVisibleListentries_ms.php
+++ DuggaSys/microservices/sectionedService/setVisibleListentries_ms.php
@@ -6,8 +6,8 @@ date_default_timezone_set("Europe/Stockholm");
 // Include basic application services!
 include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
-include ('../sharedMicroservices/getUid_ms.php');
-include_once "retrieveSectionedService_ms.php";
+include "../sharedMicroservices/getUid_ms.php";
+include_once "./retrieveSectionedService_ms.php";
 
 
 // Connect to database and start session
