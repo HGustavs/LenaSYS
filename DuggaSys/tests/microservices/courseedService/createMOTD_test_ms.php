2b760a64c96a6ddb7cbd4d2d51e499b3aef7d10d
a22marca <a22marca@student.his.se>
2024-05-10 11:18:17 +0200

    added a draft for the test

---
+++ DuggaSys/tests/microservices/courseedService/createMOTD_test_ms.php
@@ -0,0 +1,29 @@
+<?php
+include_once "../../../../Shared/test.php";
+
+$testsData = array(
+    'updateDugga_ms' => array(
+        'expected-output' => '{"debug":"NONE!","motd":"TESTING-MOTD-DELETE","readonly":"1"}',
+        'query-after-test-1' => 'DELETE FROM settings WHERE motd="TESTING-MOTD-DELETE"',
+        'service' => 'http://localhost/LenaSYS/DuggaSys/courseedservice.php',
+        'service-data' => serialize(
+            array(
+                'opt' => 'SETTINGS',
+                'motd' => 'TESTING-MOTD-DELETE',
+                'readonly' => 'readonly',
+                'username' => 'mestr',
+                'password' => 'password',
+            )
+        ),
+        'filter-output' => serialize(
+            array(
+                'debug',
+                'motd',
+                'readonly'
+            )
+        ),
+    ),
+);
+
+testHandler($testsData, true); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
+?>
