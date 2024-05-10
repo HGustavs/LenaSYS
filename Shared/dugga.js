efc5b80a925d3f2b38f50d8325725cc15f656186
Albin <aljo940616@gmail.com>
2024-05-10 13:25:58 +0200

    migrate showDuggaService to microservices by changing the service redirections in dugga.js

--- Shared/dugga.js
+++ Shared/dugga.js
@@ -1146,7 +1146,7 @@ function AJAXService(opt,apara,kind)
       dataType: "json",
       success: returnedGroups
     });
-  }else if(kind=="CONTRIBUTION"){
+  	}else if(kind=="CONTRIBUTION"){
 			$.ajax({
 				url: "contributionservice.php",
 				type: "POST",
@@ -1163,38 +1163,32 @@ function AJAXService(opt,apara,kind)
 				success: returnedSection
 			});
 	}else if(kind=="PDUGGA"){
-		//for testing
-		console.log("dugga.js (kind=='PDUGGA')\n"+"courseid="+querystring['courseid']+"\ndid="+querystring['did']+"\ncoursevers="+querystring['coursevers']+"\nmoment="+querystring['moment']+"\nsegment="+querystring['segment']);
+		let service_url = "../DuggaSys/microservices/showDuggaService/getShowDugga_ms.php";
+		switch (opt) {
+			case "SAVDU":
+				service_url = "../DuggaSys/microservices/showDuggaService/saveDugga_ms.php";
+				break;
+			case "UPDATEAU":
+				service_url = "../DuggaSys/microservices/showDuggaService/updateActiveUsers_ms.php";
+				break;
+			default:
+				break;
+		}
 		$.ajax({
-			url: "showDuggaservice.php",
+			url: service_url,
 			type: "POST",
-
-			//Something is wrong with the line below, restored to an older version for now.
-			//data: "courseid="+querystring['cid']+"&did="+querystring['did']+"&coursevers="+querystring['coursevers']+"&moment="+querystring['moment']+"&segment="+querystring['segment']+"&hash="+hash+"&password="+pwd+"&opt="+"&variant="+querystring['variantValue']+opt+para,
 			data: "courseid="+querystring['cid']+"&did="+querystring['did']+"&coursevers="+querystring['coursevers']+"&moment="+querystring['moment']+"&segment="+querystring['segment']+"&hash="+hash+"&password="+pwd+"&opt="+opt+para+"&variant="+variantValue,
 			datatype: "json",
 			success: returnedDugga
-			// success: function(data){
-			// 	console.log(data);
-			// 	var phpData = JSON.parse(data);
-			// 	isTeacher = phpData.isTeacher;
-			// 	isFileSubmitted = phpData.isFileSubmitted;
-			// 	canSaveController(); 
-			// 	localStorageHandler(phpData);
-			// 	returnedDugga(phpData);
-			// 	setPassword(phpData.password); 
-			// 	enableTeacherVariantChange(phpData);
-			// 	handleHash();	//Makes sure hash is unique.
-			// }
 		});
 	}else if(kind=="RESULT"){
-			$.ajax({
-				url: "resultedservice.php",
-				type: "POST",
-				data: "opt="+opt+para,
-				dataType: "json",
-				success: returnedResults
-			});
+		$.ajax({
+			url: "resultedservice.php",
+			type: "POST",
+			data: "opt="+opt+para,
+			dataType: "json",
+			success: returnedResults
+		});
 	}else if(kind=="GROUP"){
 			$.ajax({
 				url: "groupedservice.php",
