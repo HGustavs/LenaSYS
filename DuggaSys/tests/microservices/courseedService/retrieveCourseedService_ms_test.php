8e7f31493f29e25e79dfa428c84fbbe8d6615d1f
Ilirr <61836150+Ilirr@users.noreply.github.com>
2024-05-10 13:47:08 +0200

    Update retrieveCourseedService_ms_test.php

--- DuggaSys/tests/microservices/courseedService/retrieveCourseedService_ms_test.php
+++ DuggaSys/tests/microservices/courseedService/retrieveCourseedService_ms_test.php
@@ -10,14 +10,16 @@ $testsData = array(
     //TEST #1
     'Retrieve-public-course' => array(  
         'expected-output' => '{"entries":[{"cid":"1001","coursename":"Test Course","visibility":"1","registered":false}]}',
-        'query-before-test-1' => "INSERT INTO course(cid, coursename, visibility) VALUES (1001, 'Test Course', 1);",
-        'query-before-test-2' => "INSERT INTO user(uid, username) VALUES (9997, 'testuser1');",
+        //Pre-values
+        'query-before-test-1' => "INSERT INTO user(uid, username) VALUES (9997, 'testuser1');", // Ensure user exists first
+        'query-before-test-2' => "INSERT INTO course(cid, coursename, visibility, creator) VALUES (1001, 'Test Course', 1, 9997);", // Then insert course with reference to user
         'query-after-test-1' => "DELETE FROM course WHERE cid = '1001';",
         'query-after-test-2' => "DELETE FROM user WHERE uid = '9997';",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/retrieveCourseedService_ms.php',
         'service-data' => serialize(
             array(
-                'username' => 'testuser1',
+                'username' => 'brom',
+                'password' => 'password',
                 'cid' => '1001',
                 'opt' => 'RETRIEVE'
             )),
