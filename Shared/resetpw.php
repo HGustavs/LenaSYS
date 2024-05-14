14fec940fe2a5bd50f9c4735f3e0b59acf96b681
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-14 14:20:19 +0200

    fixed include in resetpw.php

--- Shared/resetpw.php
+++ Shared/resetpw.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once "../Shared/sessions.php";
-include_once "../Shared/basic.php";
+include_once "./sessions.php";
+include_once "./basic.php";
 
-include_once "../../coursesyspw.php";
+include_once "./coursesyspw.php";
 
 $opt=getOP('opt');
 
