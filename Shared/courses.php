65f1bf29846cc0bec9e4fe4996d0c021556a8ec8
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-14 14:11:32 +0200

    fixed include in courses.php

--- Shared/courses.php
+++ Shared/courses.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__) . '/../Shared/database.php');
+require_once(dirname(__FILE__) . './database.php');
 
 //---------------------------------------------------------------------------------------------------------------
 // courseexists - Checks if a cerain course exists or not
