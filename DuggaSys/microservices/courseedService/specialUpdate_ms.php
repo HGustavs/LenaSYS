9e851ef90afd4d12429e1d3c9c0d96d70b656a75
c22mohel <c22mohel@student.his.se>
2024-05-14 15:02:33 +0200

    adding the necessary code

--- DuggaSys/microservices/courseedService/specialUpdate_ms.php
+++ DuggaSys/microservices/courseedService/specialUpdate_ms.php
@@ -1,86 +1,66 @@
 <?php
 
 
-date_default_timezone_set("Europe/Stockholm");
+    date_default_timezone_set("Europe/Stockholm");
 
-// Include basic application services!
-include_once "../Shared/basic.php";
-include_once "../Shared/sessions.php";
+    // Include basic application services!
+    include_once "../../../Shared/sessions.php";
+    include_once "../../../Shared/basic.php";
+    include_once "../sharedMicroservices/getUid_ms.php";
 
-
-// Connect to database and start session
-pdoConnect();
-session_start();
- 
-$opt = getOP('opt');
-$cid = getOP('cid');
-$courseGitURL = getOP('courseGitURL'); // for github url 
-
-
-
-// checks that the user is a superuser and logged in
-if(checklogin() && isSuperUser(getUid()) == true) {
-    $userid = getUid();  
-
-    if (strcmp($opt, "SPECIALUPDATE") === 0) {
-
-        $query = $pdo->prepare("SELECT * from course WHERE cid=:cid;");
-        $query->bindParam(':cid', $cid);
+    // Connect to database and start session
+    pdoConnect();
+    session_start();
     
-    
-        if (!$query->execute()) {
-    
-            $error = $query->errorInfo();
-            $allOperationsSucceeded = false;
-            $debug = "Error finding course specifics\n" . $error[2];
-    
-        } else {
-            $momentlist = array();
-            foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode, courseGitURL=:courseGitURL WHERE cid=:cid;");
-    
-                print_r($row['coursename'] . $row['visibility'] . $row['coursecode']);
-                $query->bindParam(':cid', $cid);
-                $query->bindParam(':coursename', $row['coursename']);
-                $query->bindParam(':visibility', $row['visibility']);
-                $query->bindParam(':coursecode', $row['coursecode']);
-                $query->bindParam(':courseGitURL', $courseGitURL);
-                try{
-                    $query->execute();
-                }
-                catch(Exception $e){
-                    $query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode WHERE cid=:cid;");
+    $opt = getOP('opt');
+    $cid = getOP('cid');
+    $courseGitURL = getOP('courseGitURL');
+
+
+
+    // checks that the user is a superuser and logged in
+    if(checklogin() && isSuperUser(getUid()) == true) {
+        $userid = getUid();  
+
+        if (strcmp($opt, "SPECIALUPDATE") === 0) {
+
+            $query = $pdo->prepare("SELECT * from course WHERE cid=:cid;");
+            $query->bindParam(':cid', $cid);
+        
+            if (!$query->execute()) {
+                $error = $query->errorInfo();
+                $debug = "Error finding course specifics\n" . $error[2];
+            } else {
+                foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode, courseGitURL=:courseGitURL WHERE cid=:cid;");
+        
+                    print_r($row['coursename'] . $row['visibility'] . $row['coursecode']);
                     $query->bindParam(':cid', $cid);
                     $query->bindParam(':coursename', $row['coursename']);
                     $query->bindParam(':visibility', $row['visibility']);
                     $query->bindParam(':coursecode', $row['coursecode']);
-                    $query->execute();
+                    $query->bindParam(':courseGitURL', $courseGitURL);
+                    try{
+                        $query->execute();
+                    }
+                    catch(Exception $e){
+                        $query = $pdo->prepare("UPDATE course SET coursename=:coursename, visibility=:visibility, coursecode=:coursecode WHERE cid=:cid;");
+                        $query->bindParam(':cid', $cid);
+                        $query->bindParam(':coursename', $row['coursename']);
+                        $query->bindParam(':visibility', $row['visibility']);
+                        $query->bindParam(':coursecode', $row['coursecode']);
+                        $query->execute();
+                    }
+        
+                    if (!$query->execute()) {
+                        $error = $query->errorInfo();
+                        $debug = "Error updating entries\n" . $error[2];
+                    }
                 }
-    
-                if (!$query->execute()) {
-                    $error = $query->errorInfo();
-                    $debug = "Error updating entries\n" . $error[2];
-                }
+        
             }
-    
         }
+
     }
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
 
 ?>
