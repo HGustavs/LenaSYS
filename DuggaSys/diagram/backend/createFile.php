df1f253d2bad5b4d56cce33070bf38e7073c6540
a22jacka <a22jacka@student.his.se>
2024-05-14 09:35:33 +0200

    copy files away

--- DuggaSys/diagram/backend/createFile.php
+++ DuggaSys/diagram/backend/createFile.php
@@ -7,4 +7,13 @@ $file = fopen("diagram_".$_POST["id"].".".$_POST["extension"], "w") or die("fail
 fwrite($file, json_encode($_POST['diagram'])."\n");
 fclose($file);
 
+// copies the file out of the repo and onto the server
+// this is probably not a good solution but testing for now
+$source = "var/www/html/LenaSYS/DuggaSys/diagram/backend/diagram_".$_POST["id"].".".$_POST["extension"];
+$destination = "home/ubuntu/saved_diagrams/diagram_".$_POST["id"].".".$_POST["extension"];
+if (!copy($source, $destination)) {
+    echo "error";
+} else {    
+    echo "file copied";
+}
 ?>
