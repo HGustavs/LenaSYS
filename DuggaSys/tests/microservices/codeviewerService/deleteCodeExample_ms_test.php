fd2a8d058f91625e0f21cfffb31accb73e84f827
a22melst <a22melst@student.his.se>
2024-05-14 17:39:21 +0200

    Progress
    The deletions take place but it seems to be an issue with the retrieve-service not retrieving all the data. Further commits will come.

--- DuggaSys/tests/microservices/codeviewerService/deleteCodeExample_ms_test.php
+++ DuggaSys/tests/microservices/codeviewerService/deleteCodeExample_ms_test.php
@@ -4,44 +4,35 @@ include_once "../../../../Shared/test.php";
 
 $testsData = array(
     'deleteCodeExample' => array(
-        'expected-output' => '"{"entries":[],"debug":"NONE!","writeaccess":true,"studentteacher":false,"readaccess":false,"coursename":"UNK","coursevers":"124","coursecode":"UNK","courseid":"undefined","links":[{"fileid":-1,"filename":"---===######===---"},{"fileid":45,"filename":"diagram.json"},{"fileid":47,"filename":"helloWorld.html"},{"fileid":4,"filename":"HTML_Ex1.txt"},{"fileid":6,"filename":"HTML_Ex2.txt"},{"fileid":8,"filename":"HTML_Ex3.txt"},{"fileid":10,"filename":"HTML_Ex4.txt"},{"fileid":12,"filename":"HTML_Ex5.txt"},{"fileid":14,"filename":"HTML_Ex6.txt"},{"fileid":16,"filename":"HTML_Ex7.txt"},{"fileid":20,"filename":"HTML_Ex8.txt"},{"fileid":23,"filename":"JavaScript_Ex1.txt"},{"fileid":26,"filename":"JavaScript_Ex2.txt"},{"fileid":29,"filename":"JavaScript_Ex3.txt"},{"fileid":50,"filename":"mdTest.md"},{"fileid":31,"filename":"PHP_Ex1.txt"},{"fileid":33,"filename":"PHP_Ex2.txt"},{"fileid":35,"filename":"PHP_Ex3.txt"},{"fileid":38,"filename":"Shader_Ex1.txt"},{"fileid":41,"filename":"Shader_Ex2.txt"}],"duggor":[],"results":[],"versions":[{"cid":1,"coursecode":"DV12G","vers":"45656","versname":"HT15","coursename":"Webbprogrammering","coursenamealt":"UNK","startdate":"2014-12-29 00:00:00","enddate":"2015-03-08 00:00:00","motd":"Webbprogrammering - HT15"},{"cid":1,"coursecode":"DV12G","vers":"45657","versname":"HT16","coursename":"Webbprogrammering","coursenamealt":"UNK","startdate":"2015-12-29 00:00:00","enddate":"2016-03-08 00:00:00","motd":"Webbprogrammering - HT16"},{"cid":2,"coursecode":"IT118G","vers":"97731","versname":"HT14","coursename":"Webbutveckling - datorgrafik","coursenamealt":"UNK","startdate":"2014-12-29 00:00:00","enddate":"2015-03-08 00:00:00","motd":"Webbutveckling - datorgrafik - HT14"},{"cid":2,"coursecode":"IT118G","vers":"97732","versname":"HT15","coursename":"Webbutveckling - datorgrafik","coursenamealt":"UNK","startdate":"2014-12-29 00:00:00","enddate":"2015-03-08 00:00:00","motd":"Webbutveckling - datorgrafik - HT15"},{"cid":3,"coursecode":"IT500G","vers":"1337","versname":"HT15","coursename":"Datorns grunder","coursenamealt":"UNK","startdate":"2014-12-29 00:00:00","enddate":"2015-03-08 00:00:00","motd":"Datorns grunder - HT15"},{"cid":4,"coursecode":"IT301G","vers":"1338","versname":"HT15","coursename":"Software Engineering","coursenamealt":"UNK","startdate":"2014-12-29 00:00:00","enddate":"2015-03-08 00:00:00","motd":"Software Engineering - HT15"},{"cid":305,"coursecode":"IT308G","vers":"12305","versname":"HT15","coursename":"Objektorienterad programmering","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":307,"coursecode":"IT115G","vers":"12307","versname":"HT15","coursename":"Datorns grunder","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":308,"coursecode":"MA161G","vers":"12308","versname":"HT15","coursename":"Diskret matematik","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":309,"coursecode":"DA322G","vers":"12309","versname":"HT15","coursename":"Operativsystem","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":312,"coursecode":"IT326G","vers":"12312","versname":"HT15","coursename":"Distribuerade system","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":319,"coursecode":"DV736A","vers":"12319","versname":"HT15","coursename":"Examensarbete i datavetenskap","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":324,"coursecode":"IT108G","vers":"12324","versname":"HT15","coursename":"Webbutveckling - webbplatsdesign","coursenamealt":"UNK","startdate":null,"enddate":null,"motd":null},{"cid":1885,"coursecode":"G1337","vers":"1337","versname":"","coursename":"Testing-Course","coursenamealt":"Course for testing codeviewer","startdate":"2020-05-01 00:00:00","enddate":"2020-06-30 00:00:00","motd":"Code examples shows both templateid and boxid!"},{"cid":1894,"coursecode":"G420","vers":"52432","versname":"ST20","coursename":"Demo-Course","coursenamealt":"Chaos Theory - Conspiracy 64k Demo","startdate":"2020-05-01 00:00:00","enddate":"2020-06-30 00:00:00","motd":"Demo Course 2020 - All current duggas"}],"codeexamples":[{"exampleid":"-1","cid":"","examplename":"","sectionname":"New Example","runlink":"","cversion":""}],"unmarked":0,"startdate":"2024-05-06","enddate":"2024-05-16","groups":{"No":["1","2","3","4","5","6","7","8"],"Le":["A","B","C","D","E","F","G","H"],"Vi":["I","II","III","IV","V","VI","VII","VIII"]},"grpmembershp":"UNK","grplst":[],"userfeedback":[],"feedbackquestion":"UNK","avgfeedbackscore":0}',
+        'expected-output' => ':D',
 
-        'query-before-test-1' => "SELECT * FROM codeexample WHERE exampleid = ?;",
-        'query-before-test-2' => "SELECT * FROM box WHERE exampleid = ?;",
-        'query-before-test-3' => "SELECT * FROM impwordlist WHERE exampleid = ?;",
-        'query-before-test-4' => "SELECT * FROM improw WHERE exampleid = ?;",
-        'query-before-test-5' => "SELECT * FROM listentries WHERE lid = ?;",
+        'query-before-test-1' => "INSERT INTO codeexample (exampleid, examplename, cid, uid, cversion, templateid) VALUES (999996, 'TestCodeExample', 1885, 101, 1337, 10)",
+        'query-before-test-2' => "INSERT INTO box (boxid, exampleid, boxtitle) VALUES (1, 999996, 'TestBox')",
+        'query-before-test-3' => "INSERT INTO improw (boxid, exampleid, uid, istart, iend) VALUES (1, 999996, 101, 120, 220)",
+        'query-before-test-4' => "INSERT INTO impwordlist (exampleid, word, uid) VALUES (999996, 'TestWord', 101)",
+        'query-before-test-5' => "INSERT INTO listentries (cid, entryname, creator) VALUES (1885, 'TestListentry', 101)",
 
-        'query-after-test-1' => "INSERT INTO codeexample (exampleid) VALUES (1001);",
-        'query-after-test-2' => "INSERT INTO box (exampleid) VALUES (1001);",
-        'query-after-test-3' => "INSERT INTO impwordlist (exampleid) VALUES (1001);",
-        'query-after-test-4' => "INSERT INTO improw (exampleid) VALUES (1001);",
-        'query-after-test-5' => "INSERT INTO listentries (lid) VALUES (301);",
+        'query-before-test-6' => "SELECT MAX(lid) AS lid FROM listentries",
+        'variables-query-before-test-3' => "lid",
 
-        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/accessedService/deleteCodeExample_ms.php',
+        // Deletes for if MS fails
+        //'query-after-test-1' => "DELETE FROM listentries where cid = 1885 AND entryname = 'TestListentry' AND creator = 101",
+        //'query-after-test-2' => "DELETE FROM impwordlist WHERE exampleid = 999996",
+        //'query-after-test-3' => "DELETE FROM improw WHERE boxid = 1 AND exampleid = 999996",
+        //'query-after-test-4' => "DELETE FROM box WHERE boxid = 1 AND exampleid = 999996",
+        //'query-after-test-5' => "DELETE FROM codeexample WHERE exampleid = 999996",
+
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/codeviewerService/deleteCodeExample_ms.php',
         'service-data' => serialize(
             array(
-                'courseid' => array(
-                    "undefined",
-                    "undefined"
-                ),
-                'coursename' => array(
-                    "undefined",
-                    "undefined"
-                ),
-                'coursevers' => "124",
-                'comment' => "undefined",
-                'opt' => "UPDATEVRS",
-                'cid' => "undefined",
-                'versid' => "124",
-                'versname' => "ST13",
-                'coursecode' => "UNK",
-                'makeactive' => "2",
-                'startdate' => "2024-05-06",
-                'enddate' => "2024-05-16",
-                'motd' => "HELLO",
-                'log_uuid' => "AXoBAp7bWtNKzuD",
-                'hash' => "UNK"
+                'opt' => "DELEXAMPLE",
+                'exampleid' => 999996,
+                'boxid' => 1,
+                'lid' => '<!query-before-test-6!><*[0]["lid"]*>',
+                'username' => 'brom',
+                'password' => 'password',
+                'courseid' => 1885,
+                'cvers' => 1337,
             )
         ),
         'filter-output' => serialize(
