1ea0eb479149fc08c5afba1da42d609e546b549c
b21isabe <129263151+b21isabe@users.noreply.github.com>
2024-05-15 16:17:55 +0200

    fixed too large numbers
    They were causing "Numeric value out of range" PDO error

--- DuggaSys/tests/microservices/duggaedService/createDuggaVariant_ms_test.php
+++ DuggaSys/tests/microservices/duggaedService/createDuggaVariant_ms_test.php
@@ -6,9 +6,9 @@ $testsData = array(
     'createDuggaVariant_ms' => array(
         'expected-output' => '{"entries":[{"variants":[{"param":"{question\"What is the correct answer?: A\"Answer1: B\"Answer2: C\"Answer3}","variantanswer":"Answer3"}]}]}',
         
-        'query-before-test-1' => "INSERT INTO quiz (id, cid, qname, vers) VALUES (2147483645, 1885, 'createDuggaVariantTestQuiz', 1337)",
-        'query-after-test-1' => "DELETE FROM variant WHERE quizID = 2147483645",
-        'query-after-test-2' => "DELETE FROM quiz WHERE id = 2147483645",
+        'query-before-test-1' => "INSERT INTO quiz (id, cid, qname, vers) VALUES (99999, 1885, 'createDuggaVariantTestQuiz', 1337)",
+        'query-after-test-1' => "DELETE FROM variant WHERE quizID = 99999",
+        'query-after-test-2' => "DELETE FROM quiz WHERE id = 99999",
 
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/duggaedService/createDuggaVariant_ms.php',
         'service-data' => serialize(
@@ -16,7 +16,7 @@ $testsData = array(
                 // Data that service needs to execute function
                 'opt' => 'ADDVARI',
                 'cid' => 1885,
-                'qid' => 2147483645,
+                'qid' => 99999,
                 'disabled' => 0,
                 'parameter' =>'{question"What is the correct answer?: A"Answer1: B"Answer2: C"Answer3}',
                 'variantanswer' => 'Answer3',
