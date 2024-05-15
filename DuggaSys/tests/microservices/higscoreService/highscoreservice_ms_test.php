0fd50d91d1ed7e4debcbd3c8823a547c381ba2ef
a22melst <a22melst@student.his.se>
2024-05-15 20:51:43 +0200

    Changed test

--- DuggaSys/tests/microservices/higscoreService/highscoreservice_ms_test.php
+++ DuggaSys/tests/microservices/higscoreService/highscoreservice_ms_test.php
@@ -3,25 +3,31 @@
 include "../../../../Shared/test.php";
 
 $testsData = array(
-    'update password (user)' => array(
-        'expected-output' => '{}',
-        'query-before-test-1' => "INSERT INTO listentries(lid, entryname,cid,vers,creator) VALUES (9789,'TestEntry',1885,1337,101);",
-        'query-before-test-2' => "INSERT INTO quiz(id, cid,vers, qname) VALUES (333,1885,1337,'testQuiz')",
-		'query-before-test-3' => "INSERT INTO userAnswer(aid,cid,vers, quiz,moment, useranswer) VALUES (978,1885,1337,333,9789,'hsanswer');",
+    'highscoreservice_ms' => array(
+        
+        'expected-output' => '{"debug":"NONE!","highscores":[{"username":"a99joher","score":99},{"username":"mestr","score":99},{"username":"a99andni","score":99},{"username":"e93evkos","score":99},{"username":"a13siman","score":99},{"username":"brom","score":99},{"username":"a13andka","score":99},{"username":"a99perla","score":99},{"username":"Tester","score":99},{"username":"a99krila","score":99}],"user":[5]}',
+
+        'query-before-test-1' => "INSERT INTO listentries (lid, entryname,cid,vers,creator) VALUES (9789, 'TestEntry', 1885, 1337, 101)",
+        'query-before-test-2' => "INSERT INTO quiz (id, cid, vers, qname) VALUES (333, 1885, 1337, 'testQuiz')",
+		'query-before-test-3' => "INSERT INTO userAnswer (aid, cid, vers, quiz, moment, useranswer, score, grade) VALUES (978, 1885, 1337, 333, 9789, 'hsanswer', 99, 2)",
+
 		'query-after-test-1' => "DELETE FROM userAnswer WHERE aid = 978;",
 		'query-after-test-2' => "DELETE FROM quiz WHERE id = 333;",
 		'query-after-test-3' => "DELETE FROM listentries WHERE lid = 9789;",
+
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/highscoreService/highscoreService_ms.php',
+
         'service-data' => serialize(array( // Data that service needs to execute function
             'username' => 'brom',
             'password' => 'password',
-			//'opt' => '', doesnt seem to use an opt at the moment.
+			'opt' => 'GET', 
 			'courseid' => '1885',
-			'coursename' => 'Testing-Course',
+			'coursename' => 'Testing-Cours',
 			'coursevers' => '1337',
 			'did' => '333',
 			'lid' => '9789',
         )),
+
         'filter-output' => serialize(array( // Filter what output to use in assert test, use none to use all ouput from service
             'debug',
             'highscores',
