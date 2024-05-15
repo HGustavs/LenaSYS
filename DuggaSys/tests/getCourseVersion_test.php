5e0d6ff2f48cc33000059a852e4d5d42c1f3afff
a22fokol <a22fokol@student.his.se>
2024-05-15 11:18:38 +0200

    updated getCourseVersion_test.php

--- DuggaSys/tests/getCourseVersion_test.php
+++ DuggaSys/tests/getCourseVersion_test.php
@@ -7,12 +7,17 @@ $testsData = array(
         'expected-output' => '[{"cid":"4","coursecode":"IT301G","vers":"1338","versname":"HT15","coursename":"Software Engineering","coursenamealt":"UNK","startdate":"2024-01-01 00:00:00","enddate":"2025-12-31 00:00:00","motd":"Welcome to Software Engineering - HT15!"}]', 
         'query-after-test-1' => "DELETE FROM vers WHERE cid = 4;", 
         'service' => 'http://localhost/LenaSYS/DuggaSys/tests/getCourseVersion_test.php',
-        'service-data' => serialize(array( 
-           
 
+
+        'service-data' => serialize(array( 
+
+            'username' => 'mestr', 
+            'password' => 'password', 
         )),
+
         'filter-output' => serialize(array( 
             // Filter what output to use in assert test
+
             'entries' => array(
                 'cid',
                 'coursecode',
@@ -24,11 +29,10 @@ $testsData = array(
                 'enddate',
                 'motd'
             ),
-
         )),
     ),
-
 );
+
 
 testHandler($testsData, true); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
 
