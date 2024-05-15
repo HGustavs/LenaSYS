96b75bc5ea6ab1f2f6a3e1bf005002ce228c4748
f22linbe <f22linbe@student.his.se>
2024-05-15 11:29:49 +0200

    changed expected outputs and values on insert query

--- DuggaSys/tests/microservices/sectionedService/getUserDuggaFeedback_ms_test.php
+++ DuggaSys/tests/microservices/sectionedService/getUserDuggaFeedback_ms_test.php
@@ -3,8 +3,8 @@ include "../../../../Shared/test.php";
 
 $testdata = array(
     'get user dugga feedback' => array(
-        'expected-output' => '{"userfeedback":[],"avgfeedbackscore":[]}', // Updating security question & answer was successful
-        'query-before-test' => "INSERT INTO userduggafeedback (ufid, username, cid, lid, score, entryname) VALUES (1, 'brom', 1894, 3017, 3, NULL);",
+        'expected-output' => '{"userfeedback":[{"ufid":2,"username":"Tester","cid":1894,"lid":3017,"score":5,"entryname":"Feedback Dugga"}],"avgfeedbackscore":null}', // avgScore should be 3.0
+        'query-before-test' => "INSERT INTO userduggafeedback (ufid, username, cid, lid, score, entryname) VALUES (1, 'brom', 1894, 3017, 3, 'Feedback Dugga');",
         'query-after-test' => "DELETE FROM userduggafeedback WHERE ufid = 1 AND username='brom' AND lid = 3017;", // Clean up after the test
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/sectionedService/getUserDuggaFeedback_ms.php',
         'service-data' => serialize(array( // Data that the service needs to execute the function
