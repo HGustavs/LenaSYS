7ff6f1daac372db8e2adbaa3303cc53e78311140
a21sahfaa <129837581+a21sahfaa@users.noreply.github.com>
2024-05-16 15:32:53 +0200

    new test for UpdateActiveUsers
    we have created a new branch for this issue because of the week name so the new one is g2-2024-v7-issue#15363

--- DuggaSys/tests/microservices/showDuggaService/updateActiveUsers_ms_test.php
+++ DuggaSys/tests/microservices/showDuggaService/updateActiveUsers_ms_test.php
@@ -7,37 +7,26 @@ $testsData = array(
     // This tests the microservice updateActiveUsers_ms 
     //------------------------------------------------------------------------------------------
     'updateActiveUsers_ms' => array(
-        'expected-output' => '{"answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":1,"variants":[],"duggaTitle":null,"hash":"UNK","hashpwd":"UNK","opt":"UNK","link":null,"activeusers":1}',
-        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = '46dee064';",
+        
+        'expected-output' => '{"hash":"hjk4ert6"}',
+        
+        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
+
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/updateActiveUsers_ms.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
                 'opt' => 'UPDATEAU',
                 'username' => 'brom',
                 'password' => 'password',
-                'courseid' => '2',
-                'coursevers' => '97732',
+                'courseid' => 1885,
+                'coursevers' => 1337,
                 'hash' => 'hjk4ert6',
-                'AUtoken' => '999',
-                'danswer' => 'UNK',
-                'score' => '0',
-                'hash' => 'hjk4ert6',
-                'grade'=> 'UNK',
-                'submitted'=> '',
-                'marked'=> '',
-                'deadline'=> 'UNK',
-                'release'=> 'UNK',
-                'files'=> '[]',
-                'userfeedback'=> 'UNK',
-                'feedbackquestion'=> 'UNK',
-                'variant'=> 'UNK',
-                'activeusers'=> '1'
+                'AUtoken' => 999,
             )
         ),
         'filter-output' => serialize(
             array( // Filter what output to use in assert test, use none to use all ouput from service
-                             'none',
-
+                'hash'
             )
         ),
     ),
