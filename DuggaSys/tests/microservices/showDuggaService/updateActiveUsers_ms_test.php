75e65e3fa55d97d2e5ec33d29b18aa117fe8bee9
a21sahfaa <129837581+a21sahfaa@users.noreply.github.com>
2024-05-16 13:31:13 +0200

    Go over the test for updateActiveUsers_ms.php again #15363
    it does not look like the microservice is being used anywhere on lenasys, if you use a curl request in shell in xampp then atleast one record is created in groupdugga and you get an output.

--- DuggaSys/tests/microservices/showDuggaService/updateActiveUsers_ms_test.php
+++ DuggaSys/tests/microservices/showDuggaService/updateActiveUsers_ms_test.php
@@ -7,9 +7,8 @@ $testsData = array(
     // This tests the microservice updateActiveUsers_ms 
     //------------------------------------------------------------------------------------------
     'updateActiveUsers_ms' => array(
-        'expected-output' => '3',
-        'query-before-test-1' => "INSERT INTO groupdugga(hash,active_users) VALUES('hjk4ert6', 3);",
-        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = 'hjk4ert6';",
+        'expected-output' => '{"answer":"UNK","danswer":"UNK","score":0,"highscoremode":"","grade":"UNK","submitted":"","marked":"","deadline":"UNK","release":"UNK","files":[],"userfeedback":"UNK","feedbackquestion":"UNK","variant":"UNK","ishashindb":false,"variantsize":"UNK","variantvalue":"UNK","password":"UNK","hashvariant":"UNK","isFileSubmitted":"UNK","isTeacher":1,"variants":[],"duggaTitle":null,"hash":"UNK","hashpwd":"UNK","opt":"UNK","link":null,"activeusers":1}',
+        'query-after-test-1' => "DELETE FROM groupdugga WHERE hash = '46dee064';",
         'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/updateActiveUsers_ms.php',
         'service-data' => serialize(
             array( // Data that service needs to execute function
@@ -19,12 +18,26 @@ $testsData = array(
                 'courseid' => '2',
                 'coursevers' => '97732',
                 'hash' => 'hjk4ert6',
-                'AUtoken' => 3
+                'AUtoken' => '999',
+                'danswer' => 'UNK',
+                'score' => '0',
+                'hash' => 'hjk4ert6',
+                'grade'=> 'UNK',
+                'submitted'=> '',
+                'marked'=> '',
+                'deadline'=> 'UNK',
+                'release'=> 'UNK',
+                'files'=> '[]',
+                'userfeedback'=> 'UNK',
+                'feedbackquestion'=> 'UNK',
+                'variant'=> 'UNK',
+                'activeusers'=> '1'
             )
         ),
         'filter-output' => serialize(
             array( // Filter what output to use in assert test, use none to use all ouput from service
-                'none'
+                             'none',
+
             )
         ),
     ),
