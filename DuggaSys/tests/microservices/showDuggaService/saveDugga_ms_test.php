c3b46ce91b858270c91a461215b167a647ae9310
Albin <aljo940616@gmail.com>
2024-05-17 17:07:33 +0200

    changed some of the variables after harvesting the request from the real system

--- DuggaSys/tests/microservices/showDuggaService/saveDugga_ms_test.php
+++ DuggaSys/tests/microservices/showDuggaService/saveDugga_ms_test.php
@@ -1,5 +1,7 @@
 <?php
 
+include "../../../../Shared/test.php";
+
 $testsData = array(
 
     'Save new dugga' => array(
@@ -7,21 +9,23 @@ $testsData = array(
         'query-before-test-1' => "INSERT INTO course(cid, creator) VALUES (9999, 1);",
         'query-before-test-2' => "INSERT INTO listentries(cid,entryname,link,kind,pos,creator,visible,vers,gradesystem,highscoremode,feedbackenabled,feedbackquestion) VALUES (9999, 'Inserttobedeleted', 'UNK', 4, 12, 2, 1, 1337, 1, 1, 0, 'UNK');",
         'query-before-test-3' => "SELECT MAX(lid) AS listentries FROM listentries",
-        'variables-query-before-test-4' => "moment",
         'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
         'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
         'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/saveDugga_ms.php',
         'service-data' => serialize(array( //Data that service needs to execute function
+            'username' => 'a87antal',
+            'password' => 'password',
             'opt' => 'SAVDU',
-            'cid' => '9999',
+            'courseid' => '9999',
             'coursevers' => '52432',
             'duggaid' => '1',
             'moment' => '<!query-before-test-3!><*[0]["listentries"]*>',
             'variant' => '3',
-            'hash' => 'ghj1ghj2',
-            'haspwd' => 'asddasdd',
-            'answer' => 'NULL'
+            'hash' => 'UNK',
+            'password' => 'UNK',
+            'answer' => 'NULL',
+            'did' => 3
         )),
         'filter-output' => serialize(array( //Filter what output to use in assert test, use none to use all ouput from service
             'none'
@@ -38,11 +42,13 @@ $testsData = array(
         'query-after-test-1' => "DELETE FROM userAnswer ORDER BY aid DESC LIMIT 1;",
         'query-after-test-2' => "DELETE FROM listentries WHERE cid = 9999;",
         'query-after-test-3' => "DELETE FROM course WHERE cid = 9999;",
-        'service' => 'http://localhost/LenaSYS/DuggaSys/showDuggaservice.php',
+        'service' => 'http://localhost/LenaSYS/DuggaSys/microservices/showDuggaService/saveDugga_ms.php',
         'service-data' => serialize(array( // Data that service needs to execute function
+            'username' => 'a87antal',
+            'password' => 'password',
             'opt' => 'SAVDU',
             'hash' => 'dfg4zxc5',
-            'haspwd' => 'asdfasdf',
+            'password' => 'asdfasdf',
             'answer' => 'im updated',
             'moment' => '<!query-before-test-3!><*[0]["listentries"]*>'
         )),
@@ -50,6 +56,8 @@ $testsData = array(
             'none'
         )),
     ),
-)
+);
+
+testHandler($testsData, true); // 2nd argument (prettyPrint): true = prettyprint (HTML), false = raw JSON
 
 ?>
