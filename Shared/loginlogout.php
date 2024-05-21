4fb434d50eb613dcc9c44e4b30ac3cfbaa632cf8
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-21 05:43:07 +0200

    change to DIR

--- Shared/loginlogout.php
+++ Shared/loginlogout.php
@@ -1,9 +1,9 @@
 <?php
-include_once "./sessions.php";
+include_once(__DIR__ . "/sessions.php");
 session_start();
 
 // Includes
-include_once "./basic.php";
+include_once(__DIR__ . "/basic.php");
 include_once "../../coursesyspw.php";
 
 $opt = getOP('opt');
