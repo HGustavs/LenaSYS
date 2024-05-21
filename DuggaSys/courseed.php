698a8af34d16a39abe0ad1118bb9c766b30707cd
a20jonda <a20jonda@student.his.se>
2024-05-21 15:43:32 +0200

    Removed style.
    This style is unnecessary with the new convention

--- DuggaSys/courseed.php
+++ DuggaSys/courseed.php
@@ -184,8 +184,8 @@ if(isset($_SESSION['uid'])){
 					<span>Message of the day:</span>
 					<div class="dialogwrapper">
 						<input class='textinput' onkeyup="validateMOTD('motd','dialog51', 'dialog52', 'submitMotd')" type='text' id='motd' placeholder='Leave blank for no MOTD' />
-						<div class="formDialog" id="errorSymbol"><span id="dialog51" class="formDialogText">Prohibited symbols</span></div>
-						<div class="formDialog" id="error50"><span id="dialog52" class="formDialogText">Message can only contain a maximum of 50 symbols</span></div>
+						<div class="formDialog"><span id="dialog51" class="formDialogText">Prohibited symbols</span></div>
+						<div class="formDialog"><span id="dialog52" class="formDialogText">Message can only contain a maximum of 50 symbols</span></div>
 					</div>
 				</div>
 				<div class='inputwrapper'><span class="italic">Read Only:</span><input type="checkbox" name='readonly' id='readonly' title='Disables uploads/submits. Useful for active backup servers.'></select></div>	
