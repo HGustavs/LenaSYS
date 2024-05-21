c2268487d58df7e2e1402e1ca85232b928e68761
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-21 06:39:58 +0200

    fixed change request in courses.php

--- Shared/courses.php
+++ Shared/courses.php
@@ -1,5 +1,5 @@
 <?php
-require_once('database.php');require_once(dirname(__FILE__) . '/../Shared/database.php');
+require_once(dirname(__FILE__) . '/../Shared/database.php');
 
 //---------------------------------------------------------------------------------------------------------------
 // courseexists - Checks if a cerain course exists or not
