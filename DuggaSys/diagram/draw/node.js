cf7a3fbcba3c0a1dac219c3d744bf9bd265d54d0
a22alfna <a22alfna@student.his.se>
2024-05-21 11:01:40 +0200

    Added documentation for the "createNode" and "createCorner" functions in node.js

--- DuggaSys/diagram/draw/node.js
+++ DuggaSys/diagram/draw/node.js
@@ -8,9 +8,21 @@ function addNodes(element) {
     let nodeSize = defaultNodeSize * zoomfact;
     let nodes = "";
 
+    /**
+    * @description creates node using name and side
+    * @param {string} name - The name of the node.
+    * @param {string} side - The side the node is on.
+    */
     const createNode = (name, side) => {
         nodes += `<span id='${name}' class='node ${name}' style="height: ${nodeSize}px; width: ${nodeSize}px; ${side}: calc(50% - ${nodeSize / 2}px);"></span>`;
     };
+
+    /**
+    * @description creates corner node using name, sideA and sideB
+    * @param {string} name - The name of the node.
+    * @param {string} sideA - The side the node is on.
+    * @param {string} sideB - The side the node is on.
+    */
     const createCorner = (name, sideA, sideB) => {
         nodes += `<span id='${name}' class='node ${name}' style="height: ${nodeSize}px; width: ${nodeSize}px; ${sideA}: 0%; ${sideB}: 0%;"></span>`;
     };
