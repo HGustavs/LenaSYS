eca75ef16bf2a8a2a34e87408bf2977e01589f5c
Samue <b22samer@student.his.se>
2024-08-04 17:49:01 +0200

    Making the elements to stop jumping when snaptogrid is on

--- DuggaSys/diagram/theme.js
+++ DuggaSys/diagram/theme.js
@@ -11,16 +11,19 @@ function updateCSSForAllElements() {
             left -= deltaX;
             top -= deltaY;
         }
-
-        if (settings.grid.snapToGrid && useDelta) {
-            if (element.kind == elementTypesNames.EREntity) {
+        const includedElementTypes = [elementTypesNames.EREntity, elementTypesNames.IEEntity, elementTypesNames.UMLEntity, elementTypesNames.SDEntity, elementTypesNames.UMLRelation, 
+            elementTypesNames.IERelation, elementTypesNames.ERRelation, elementTypesNames.ERAttr, elementTypesNames.UMLInitialState, elementTypesNames.UMLFinalState, elementTypesNames.UMLSuperState, 
+            elementTypesNames.sequenceActor, elementTypesNames.sequenceObject, elementTypesNames.sequenceActivation, elementTypesNames.sequenceLoopOrAlt];
+       
+            if (settings.grid.snapToGrid && useDelta) {
+            if (includedElementTypes.includes(element.kind)) {
                 // The element coordinates with snap point
                 let objX = Math.round((elementData.x + elementData.width / 2 - (deltaX * (1.0 / zoomfact))) / (settings.grid.gridSize / 2)) * (settings.grid.gridSize / 2);
                 let objY = Math.round((elementData.y + elementData.height / 2 - (deltaY * (1.0 / zoomfact))) / (settings.grid.gridSize / 2)) * (settings.grid.gridSize / 2);
 
                 // Add the scroll values
                 left = Math.round(((objX - zoomOrigo.x) * zoomfact) + (scrollx * (1.0 / zoomfact)));
-                top = Math.round((((objY - zoomOrigo.y) - (settings.grid.gridSize / 2)) * zoomfact) + (scrolly * (1.0 / zoomfact)));
+                top = Math.round(((objY - zoomOrigo.y) * zoomfact) + (scrolly * (1.0 / zoomfact)));
 
                 // Set the new snap point to center of element
                 left -= ((elementData.width * zoomfact) / 2);
@@ -38,6 +41,7 @@ function updateCSSForAllElements() {
                 left -= ((elementData.width * zoomfact) / 2);
                 top -= ((elementData.height * zoomfact) / 2);
             }
+
         }
         divObject.style.left = left + "px";
         divObject.style.top = top + "px";
