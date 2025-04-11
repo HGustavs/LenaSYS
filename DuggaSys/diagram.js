4e5a16682c8102425a9501ca903d0d62f363488a
a23aleva <a23aleva@student.his.se>
2025-04-11 12:41:12 +0200

    Documentation
    Added old note to maintain good readability.

--- DuggaSys/diagram.js
+++ DuggaSys/diagram.js
@@ -2052,7 +2052,7 @@ function saveDiagramAs() {
 
     const currentDate = new Date();
     const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1) < 10 ? `0${currentDate.getMonth() + 1}` : currentDate.getMonth() + 1;
+    const month = (currentDate.getMonth() + 1) < 10 ? `0${currentDate.getMonth() + 1}` : currentDate.getMonth() + 1; // Note: January is month 0
     const day = currentDate.getDate() < 10 ? `0${currentDate.getDate()}` : currentDate.getDate();
     const hours = currentDate.getHours() < 10 ? `0${currentDate.getHours()}` : currentDate.getHours();
     const minutes = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : currentDate.getMinutes();
