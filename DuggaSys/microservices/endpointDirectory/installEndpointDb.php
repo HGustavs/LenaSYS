b1d638a53ac1b80119ec3bf8c5d759278f17b9ca
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-14 13:40:52 +0200

    Created the database

---
+++ DuggaSys/microservices/endpointDirectory/installEndpointDb.php
@@ -0,0 +1,23 @@
+<?
+// select where to create the database
+$dbFile = __DIR__ . 'endpointDirectory_db.sqlite';
+
+// create or open the database
+$db = new PDO('sqlite:' . $dbFile);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$db->exec("
+    CREATE TABLE IF NOT EXIST microservices (
+    id INT PRIMARY KEY AUTOINCREMENT,
+    ms_name TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    ms_path TEXT NOT NULL,
+    parameters TEXT NOT NULL,
+    documentation TEXT NOT NULL,
+    render TEXT NOT NULL,
+    )
+");
+
+echo "console.log('Microservice database has been created')";
+
+?>
