6926a8b3615c8107f704c48d26cb4db1496c01ca
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-14 14:11:42 +0200

    added variables for the folders

--- DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
+++ DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
@@ -8,5 +8,13 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // clear previous data
 $db->exec("DELETE FROM microservices");
 
+// points to microservice folder
+$basePath = realpath(__DIR__ . '/../');
+// points to project root
+$rootPath = realpath($basePath . '/../');
+
+$services = scandir($basePath);
+
+
 
 ?>
