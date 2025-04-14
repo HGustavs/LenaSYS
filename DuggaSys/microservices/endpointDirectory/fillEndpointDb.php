70488365950bc883db2ec75526f5402d9874c3df
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-14 14:06:50 +0200

    Added db connection and clearing the database before filling it

--- DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
+++ DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
@@ -1,5 +1,12 @@
 <?php
 
+// connect to the database
+$dbFile = __DIR__ . '/endpointDirectory_db.sqlite';
+$db = new PDO('sqlite:' . $dbFile);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// clear previous data
+$db->exec("DELETE FROM microservices");
 
 
 ?>
