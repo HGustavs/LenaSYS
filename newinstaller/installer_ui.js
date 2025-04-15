dfaea583e41923414c59d45de9db53fa6f2c38b1
a23dilel <164745156+a23dilel@users.noreply.github.com>
2025-04-15 13:10:41 +0200

    Added a button for go to course page when installer is finish to install lenasys

--- newinstaller/installer_ui.js
+++ newinstaller/installer_ui.js
@@ -212,6 +212,7 @@ function start_installer() {
 			if (targetValue === 100) {
 				setProgressBarWidth(100);
 				//progressPercentage.innerHTML = "100%";
+				addButton(error_occured);
 			}
 	
 			function update() {
@@ -225,7 +226,14 @@ function start_installer() {
 		},
 		error: function(data) {
 			error_occured = true;
+			addButton(error_occured);
 			alert(data);
 		}
 	});
+
+	function addButton(error) {
+		if (error) {
+			document.getElementById("pageButtonContainer").innerHTML = "<button class='defaultButton' onclick=location.href='../DuggaSys/courseed.php'>Go to course</button>";
+		}
+	}
 }
