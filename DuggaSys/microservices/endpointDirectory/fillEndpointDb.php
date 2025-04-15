5b5f7a30f6355b433984b7fa2f1b00e396766112
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-15 13:15:47 +0200

    started creating regex to divide the text within an md file

--- DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
+++ DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
@@ -20,6 +20,60 @@ foreach ($mdFiles as $mdFile) {
     $services = preg_split('/# Name of file\/service/i', $content);
 
     foreach ($services as $service) {
+        // skip empty sections
+        if (trim($section) === '') {
+            continue;
+        }
+
+        // microservice name
+        preg_match('/^\s*(.+)/', $section, $nameMatch);
+        if (isset($nameMatch[1])) {
+            $ms_name = trim($nameMatch[1]);
+        } else {
+            $ms_name = null;
+        }
+
+        // description
+        preg_match('/## Description\s*(.*?)\n(?:\n|#)/si', $section, $descMatch);
+        if (isset($descMatch[1])) {
+            $description = trim($descMatch[1]);
+        } else {
+            $description = null;
+        }
+
+        // parameter table
+        preg_match('/\| Parameter \| Type \| Description \|.*?\| (.+?) \| (.+?) \| (.+?) \|/si', $section, $paramMatch);
+        // parameter
+        if (isset($paramMatch[1])) {
+            $parameter = trim($paramMatch[1]);
+        } else {
+            $parameter = null;
+        }
+        // parameter type
+        if (isset($paramMatch[2])) {
+            $parameter_type = trim($paramMatch[2]);
+        } else {
+            $parameter_type = null;
+        }
+        // parameter description
+        if (isset($paramMatch[3])) {
+            $parameter_description = trim($paramMatch[3]);
+        } else {
+            $parameter_description = null;
+        }
+
+        // calling methods 
+        preg_match('/## Calling Methods\s*(GET|POST|PUT|DELETE)/i', $section, $callMatch);
+        if (isset($callMatch[1])) {
+            $calling_methods = $callMatch[1];
+        } else {
+            $calling_methods = null;
+        }
+
+        // output table
+        preg_match('/\| Output \| Type \| Description \|.*?\| (.+?) \| (.+?) \| (.+?) \|/si', $section, $outputMatch);
+
+
 
     }
 
