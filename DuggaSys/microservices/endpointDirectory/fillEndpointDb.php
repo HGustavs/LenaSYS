a2b542031c67dc7184d50d0e187e17e62bf94f05
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-15 12:59:13 +0200

    commented code that wont be used and started working on separating multiple microservices within one md file

--- DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
+++ DuggaSys/microservices/endpointDirectory/fillEndpointDb.php
@@ -10,63 +10,63 @@ $db->exec("DELETE FROM microservices");
 
 // points to microservice folder
 $basePath = realpath(__DIR__ . '/../');
-// points to project root
-// $rootPath = realpath($basePath . '/../');
 $mdFiles = glob($basePath . '/*/*.md');
 
 // loop through every .md-file
 foreach ($mdFiles as $mdFile) {
     $content = file_get_contents($mdFile);
 
-    // check if the markdown file includes correct headline
-    if (!preg_match('/### MICROSERVICE DOCUMENTATION ###/', $content)) {
-        continue;
+    // split section within the markdown file (the beginning of each microservice starts with "#Name of file service/service")
+    $services = preg_split('/# Name of file\/service/i', $content);
+
+    foreach ($services as $service) {
+
     }
 
-    // extract blocks with regex
-    $fields = [
-        'ms_name' => '/# MICROSERVICE NAME #\s*(.+)/i',
-        'ms_path' => '/# SEARCHPATH #\s*(.+)/i',
-        'file_name' => '/# FILENAME #\s*(.+)/i',
-        'description' => '/# DESCRIPTION #\s*(.+)/i',
-        'parameters' => '/# PARAMETERS #\s*(.+)/i',
-        'render' => '/# RENDER #\s*(.+)/i',
-    ];
+    // // extract blocks with regex
+    // $fields = [
+    //     'ms_name' => '/# MICROSERVICE NAME #\s*(.+)/i',
+    //     'ms_path' => '/# SEARCHPATH #\s*(.+)/i',
+    //     'file_name' => '/# FILENAME #\s*(.+)/i',
+    //     'description' => '/# DESCRIPTION #\s*(.+)/i',
+    //     'parameters' => '/# PARAMETERS #\s*(.+)/i',
+    //     'render' => '/# RENDER #\s*(.+)/i',
+    // ];
 
-    $values = [];
+    // $values = [];
 
-    foreach ($fields as $key => $regex) {
-        if (preg_match($regex, $content, $match)) {
-            $values[$key] = trim($match[1]);
-        } else {
-            // if something is missing
-            $values[$key] = null;
-        }
-    }
+    // foreach ($fields as $key => $regex) {
+    //     if (preg_match($regex, $content, $match)) {
+    //         $values[$key] = trim($match[1]);
+    //     } else {
+    //         // if something is missing
+    //         $values[$key] = null;
+    //     }
+    // }
 
-    // print what is inserted (for debugging)
-    echo "  Adding:<br>";
-    echo "  Microservice:  " . $values['ms_name'] . "<br>";
-    echo "  Filename:       " . $values['file_name'] . "<br>";
-    echo "  Path:        " . $values['ms_path'] . "<br>";
-    echo "  Description:   " . $values['description'] . "<br>";
-    echo "  Parameters:    " . $values['parameters'] . "<br>";
-    echo "  Render:        " . $values['render'] . "<br>";
+    // // print what is inserted (for debugging)
+    // echo "  Adding:<br>";
+    // echo "  Microservice:  " . $values['ms_name'] . "<br>";
+    // echo "  Filename:       " . $values['file_name'] . "<br>";
+    // echo "  Path:        " . $values['ms_path'] . "<br>";
+    // echo "  Description:   " . $values['description'] . "<br>";
+    // echo "  Parameters:    " . $values['parameters'] . "<br>";
+    // echo "  Render:        " . $values['render'] . "<br>";
 
-    // insert into database
-    $stmt = $db->prepare("
-        INSERT INTO microservices (ms_name, file_name, ms_path, parameters, description, render)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ");
+    // // insert into database
+    // $stmt = $db->prepare("
+    //     INSERT INTO microservices (ms_name, file_name, ms_path, parameters, description, render)
+    //     VALUES (?, ?, ?, ?, ?, ?)
+    // ");
 
-    $stmt->execute([
-        $values['ms_name'],
-        $values['file_name'],
-        $values['ms_path'],
-        $values['parameters'],
-        $values['description'],
-        $values['render']
-    ]);
+    // $stmt->execute([
+    //     $values['ms_name'],
+    //     $values['file_name'],
+    //     $values['ms_path'],
+    //     $values['parameters'],
+    //     $values['description'],
+    //     $values['render']
+    // ]);
 
 }
 
