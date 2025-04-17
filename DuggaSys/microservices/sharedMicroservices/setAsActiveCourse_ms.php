7c75e77d012ed52a0255fa366077ef9046035f9b
a20artje <coolvids.bruh@gmail.com>
2025-04-17 10:11:16 +0200

    SetActiveCourseVersion works
    The HTTP request works instead of the specific function call

--- DuggaSys/microservices/sharedMicroservices/setAsActiveCourse_ms.php
+++ DuggaSys/microservices/sharedMicroservices/setAsActiveCourse_ms.php
@@ -3,43 +3,30 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/basic.php";
 include_once "../Shared/sessions.php";
-/*
-function setAsActiveCourse($pdo, $cid, $versid)
-{
-	$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
-	$query->bindParam(':cid', $cid);
-	$query->bindParam(':vers', $versid);
 
-	if (!$query->execute()) {
-		$error = $query->errorInfo();
-		$debug = "Error updating entries\n" . $error[2];
-		echo json_encode($debug);
-	}
-}
+pdoConnect();
+session_start();
 
-*/
+header("Content-Type: application/json");
 
-function setAsActiveCourse()
-{
-	pdoConnect();
-	session_start();
-	file_put_contents("../../../log/log.json", "SetAsActiveCourse GLORY\n", FILE_APPEND);
-    header("Content-Type: application/json");
-    //need to fix _SERVER
+//get values from post
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if (isset($_POST['cid'], $_POST['versid'])) {
 		file_put_contents("../../../log/log.json", "POST ISSET GLORY\n", FILE_APPEND);
 		$cid = $_POST['cid'];
 		$versid = $_POST['versid'];
 	}
-
-    $query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
-    $query->bindParam(':cid', $cid);
-    $query->bindParam(':vers', $versid);
-
-
-    if (!$query->execute()) {
-        $error = $query->errorInfo();
-        $debug = "Error updating entries\n" . $error[2];
-        echo json_encode($debug);
-    }
 }
+
+//set active course in database
+$query = $pdo->prepare("UPDATE course SET activeversion=:vers WHERE cid=:cid");
+$query->bindParam(':cid', $cid);
+$query->bindParam(':vers', $versid);
+
+//error handling
+if (!$query->execute()) {
+	$error = $query->errorInfo();
+	$debug = "Error updating entries\n" . $error[2];
+	echo json_encode($debug);
+}
+
