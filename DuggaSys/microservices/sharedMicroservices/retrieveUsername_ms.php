fb69b65a8a331ab9c0e6b9129b38cda8d2be5eeb
d22marwi <d22marwi@student.his.se>
2025-04-24 10:32:29 +0200

    Re-engineer microservice createNewListEntry #16827
    Updated retrieveUsername

--- DuggaSys/microservices/sharedMicroservices/retrieveUsername_ms.php
+++ DuggaSys/microservices/sharedMicroservices/retrieveUsername_ms.php
@@ -3,8 +3,7 @@
 
 include_once "./getUid_ms.php";
 
-function retrieveUsername($pdo)
-{
+
 	date_default_timezone_set("Europe/Stockholm");
 
 	$userid = getUid();
@@ -19,5 +18,5 @@ function retrieveUsername($pdo)
 		}
 	}
 
-	return $username;
-}
+	header('Content-Type: application/json');
+	echo json_encode($data);
