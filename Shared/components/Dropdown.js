9e62a9ae42c84517ca44427ba47a86f0a1e7663a
b23sebth <164857526+b23sebth@users.noreply.github.com>
2025-04-25 21:58:34 +0200

    Updated dropdown component and related styling.

--- Shared/components/Dropdown.js
+++ Shared/components/Dropdown.js
@@ -1,8 +1,8 @@
-const Dropdown = ({ children, dropdownName, onClick, className = "" }) => {
+const Dropdown = ({ children, dropdownName, onClick, className = "", id="" }) => {
     return (
-        <div className={`dropdown ${className}`}>
+        <div className={`dropdown dropdown-content-hide ${className}`}>
             <button onClick={onClick} className="dropButton">{dropdownName}</button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id={id}>
                 {children}
             </div>
         </div>
