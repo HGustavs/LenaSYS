8078686d670ed3f8976683a4e1dfb881d5f47d29
a23axehe <a23axehe@student.his.se>
2025-04-25 11:34:01 +0200

    changed post reception
    changed post reception according to new standard in documentation

--- DuggaSys/microservices/sharedMicroservices/createNewCodeExample_ms.php
+++ DuggaSys/microservices/sharedMicroservices/createNewCodeExample_ms.php
@@ -2,79 +2,48 @@
 include_once "./getUid_ms.php";
 include_once "./retrieveUsername_ms.php";
 
-//Here we add a http call for post requests
+// Connect to database and start session
+pdoConnect();
+session_start();
+
+
 header('Content-Type: application/json');
 
-// This makes the function only accept POST
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo json_encode(['error' => 'Only POST allowed']);
-    exit;
-}
-// Error handling regarding the six values needed
-// These are required for the post call to work
-$required = [
-    'exampleid','courseid', 'coursevers', 'sectionname',
-    'link', 'log_uuid', 'templateNumber'
-];
-//If any value is missing the function will return error code 400 and a error message
-foreach ($required as $f) {
-    if (! isset($_POST[$f])) {
-        http_response_code(400);
-        echo json_encode([
-            'error' => "Missing parameter: $f"
-        ]);
-        exit;
+
+//get values from post
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['exampleid'], $_POST['courseid'], $_POST['coursevers'], $_POST['sectname'], $_POST['link'], $_POST['log_uuid'], $_POST['templatenumber'])) {
+		$exampleid = $_POST['exampleid'];
+		$courseid = $_POST['courseid'];
+		$coursevers = $_POST['coursevers'];
+		$sectname = $_POST['sectname'];
+		$link = $_POST['link'];
+		$log_uuid  = $_POST['log_uuid'];
+		$templateNumber = $_POST['templatenumber'];
     }
 }
-// Turn post values into locals
-$exampleid = $_POST['exampleid'];
-$courseid = $_POST['courseid'];
-$coursevers = $_POST['coursevers'];
-$sectionname = $_POST['sectionname'];
-$link = $_POST['link'];
-$log_uuid = $_POST['log_uuid'];
-$templateNumber = $_POST['templateNumber'];
 
-// Here we call the original function
-$result = createNewCodeExample(
-    $pdo,
-    $exampleid,
-    $courseid,
-    $coursevers,
-    $sectionname,
-    $link,
-    $log_uuid,
-    $templateNumber
-);
-
-// Return the result as JSON
-echo json_encode($result);
-exit;
-
-//Here is where the microservice function starts
-function createNewCodeExample($pdo, $exampleid, $courseid, $coursevers, $sectname, $link, $log_uuid, $templateNumber=0){
-	if (!is_null($exampleid)){
-		$sname = $sectname . ($exampleid + 1);
-	}else{
-		$sname= $sectname;
-	}
-	$debug="NONE!";
-	$query2 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (:cid,:ename,:sname,1,:cversion, :templateid);");
-	$query2->bindParam(':cid', $courseid);
-	$query2->bindParam(':cversion', $coursevers);
-	$query2->bindParam(':ename', $sectname);
-	$query2->bindParam(':sname', $sname);
-	$query2->bindParam(":templateid", $templateNumber);
-	if (!$query2->execute()) {
-		$error = $query2->errorInfo();
-		$debug = "Error updating entries" . $error[2];
-	}
-	$link = $pdo->lastInsertId();
-
-	$userid = getUid();
-	$username = retrieveUsername($pdo);
-	logUserEvent($userid, $username, EventTypes::SectionItems, $sectname);
-
-	return array('debug'=>$debug,'link'=>$link);
+if (!is_null($exampleid)){
+	$sname = $sectname . ($exampleid + 1);
+}else{
+	$sname= $sectname;
 }
+$debug="NONE!";
+$query2 = $pdo->prepare("INSERT INTO codeexample(cid,examplename,sectionname,uid,cversion,templateid) values (:cid,:ename,:sname,1,:cversion, :templateid);");
+$query2->bindParam(':cid', $courseid);
+$query2->bindParam(':cversion', $coursevers);
+$query2->bindParam(':ename', $sectname);
+$query2->bindParam(':sname', $sname);
+$query2->bindParam(":templateid", $templateNumber);
+if (!$query2->execute()) {
+	$error = $query2->errorInfo();
+	$debug = "Error updating entries" . $error[2];
+}
+$link = $pdo->lastInsertId();
+
+$userid = getUid();
+$username = retrieveUsername($pdo);
+logUserEvent($userid, $username, EventTypes::SectionItems, $sectname);
+
+return array('debug'=>$debug,'link'=>$link);
+
