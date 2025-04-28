cf8a3c7045eb8181515ce5632635b00dc9978315
b23sebth <164857526+b23sebth@users.noreply.github.com>
2025-04-28 17:17:26 +0200

    Added hamburger menu icon to dropdown component.

--- Shared/components/Dropdown.js
+++ Shared/components/Dropdown.js
@@ -1,7 +1,10 @@
 const Dropdown = ({ children, dropdownName, onClick, className = "", id="" }) => {
     return (
         <div className={`dropdown ${className}`}>
-            <button onClick={onClick} className="dropButton submit-button">{dropdownName}</button>
+            <button onClick={onClick} className="dropButton submit-button">
+                <img src="/LenaSYS/Shared/icons/hamburger_menu_google_fonts.svg"/>
+                {dropdownName}
+            </button>
             <div className="dropdown-content dropdown-content-hide" id={id}>
                 {children}
             </div>
