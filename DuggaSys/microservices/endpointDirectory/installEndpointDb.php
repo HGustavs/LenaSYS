4e3a6f6974c2c2ae1d308058ed250ff425727f5c
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-04-28 12:43:30 +0200

    Added remove database if it already exists

--- DuggaSys/microservices/endpointDirectory/installEndpointDb.php
+++ DuggaSys/microservices/endpointDirectory/installEndpointDb.php
@@ -2,7 +2,13 @@
 // select where to create the database
 $dbFile = __DIR__ . '/endpointDirectory_db.sqlite';
 
-// create or open the database
+// remove old database if it exists
+if (file_exists($dbFile)) {
+    unlink($dbFile);
+    echo "Removed database";
+}
+
+// create the database
 $db = new PDO('sqlite:' . $dbFile);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
