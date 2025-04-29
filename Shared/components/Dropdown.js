4b3e689fbb44cdcc7554b9839abe2c9b3a5bda91
b23sebth <164857526+b23sebth@users.noreply.github.com>
2025-04-29 12:10:27 +0200

    Added description to the Dropdown component.

--- Shared/components/Dropdown.js
+++ Shared/components/Dropdown.js
@@ -1,3 +1,10 @@
+/**
+ * Animated reusable dropdown component.
+ * It takes a number of children which then acts as the rows. The onClick props should use the function
+ * showDropdown inside of sectioned.js. The id of the dropdown should be passed as a parameter to the function.
+ * The dropdownName props is the text that will be displayed on the button that opens the dropdown. 
+ */
+
 const Dropdown = ({ children, dropdownName, onClick, className = "", id="" }) => {
     return (
         <div className={`dropdown ${className}`}>
