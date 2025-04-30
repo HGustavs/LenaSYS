09ed943dc52de8c9ce9d34e03287cc20bb3708f2
a23dilel <164745156+a23dilel@users.noreply.github.com>
2025-04-30 12:14:09 +0200

    Copy all modal files from G2-2025-v4-#16039

--- newinstaller/tools/modal.php
+++ newinstaller/tools/modal.php
@@ -1,14 +1,20 @@
 <?php
 
 class Modal {
+    private $id;
     private $header;
     private $body;
     private $buttons;
 
-    public function __construct($header = '', $body = '', $buttons = []) {
+    public function __construct($id = '', $header = '', $body = '', $buttons = []) {
+        $this->id = $id;
         $this->header = $header;
         $this->body = $body;
         $this->buttons = $buttons;
+    }
+
+    public function setID($id) {
+        $this->id = $id;
     }
 
     public function setHeader($header) {
@@ -24,7 +30,7 @@ class Modal {
     }
 
     public function render() {
-        echo '<div class="modal" id="genericModal">';
+        echo '<div class="modal" id="' . $this->id . '">';
         echo '  <div class="modal-content">';
         echo '      <span class="close-btn" id="closeModal">&times;</span>';
         echo '      <div id="modalHeader" class="modal-header">' . $this->header . '</div>';
@@ -40,55 +46,93 @@ class Modal {
         echo '</div>';
     }
 
-    public static function setDbConnectionErrorModal() {
+    public static function setDbConnectionErrorModal($id) {
         $modal = new self(
+            $id,
             'Database Connection Failed',
-            'Unable to connect to the database. Please check your root username, password, and hostname',
+            '
+                Unable to connect to the database. Please check your root username, password, and hostname.
+                Press the retry-button if you want to return to the installer at the step where it failed or 
+                change settings root user credentials and hostname here:
+
+                <div>
+                    <div class="input-grid">
+                        <div class="input-field">
+                            <label for="changeRootUsername">Root username:</label>
+                            <input id="changeRootUsername" type="text"">
+                        </div>
+
+                        <div class="input-field">
+                            <label for="changeRootPassword">Root password:</label>
+                            <input id="changeRootPassword" type="password">
+                        </div>
+
+                        <div class="input-field">
+                            <label for="changeHostname">Hostname:</label>
+                            <input id="changeHostname" type="text">
+                        </div>
+                    </div>
+                </div> 
+            ',
             [
-                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'retryInstaller()'],
-                ['text' => 'Change Settings', 'progressButton' => 'settings-button', 'onclick' => 'changeDbSettings()'],
-                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'closeModal()']
+                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'Window.retryInstaller()'],
+                ['text' => 'Change Settings', 'class' => 'progressButton', 'onclick' => 'Window.changeDbSettings()'],
+                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'Window.closeModal()']
             ]
         );
 
         $modal->render();
     }
 
-    public static function showFilePermissionErrorModal() {
+    public static function showFilePermissionErrorModal($id) {
         $modal = new self(
+            $id,
             'File Permission Error',
-            'Failed to copy course files. Please run the following command: <code>chmod -R 755 /LenaSys/install/courses</code>',
+            '
+                Failed to copy course files. Please run the following command: <code>chmod -R 755 /LenaSys/install/courses</code>
+                Press the retry-button if you want to return to the installer at the step where it failed.
+            ',
             [
-                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'retryInstaller()'],
-                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'closeModal()']
+                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'Window.retryInstaller()'],
+                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'Window.closeModal()']
             ]
         );
 
         $modal->render();
     }
 
-    public static function showDbCreationErrorModal() {
+    public static function showDbCreationErrorModal($id) {
         $modal = new self(
+            $id,
             'Database Creation Failed',
-            'Failed to create the database or database user. Please ensure you have the necessary permissions.',
+            '
+                Failed to create the database or database user. Please ensure you have the necessary permissions. 
+                Press the force-button if you want overwrite database and database user or 
+                press the retry-button if you want to return to the installer at the step where it failed.
+            ',
             [
-                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'closeModal()'],
-                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'retryInstaller()'],
-                ['text' => 'Force', 'class' => 'progressButton', 'onclick' => 'forceCreateDb()']
+                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'Window.retryInstaller()'],
+                ['text' => 'Force', 'class' => 'progressButton', 'onclick' => 'Window.forceCreateDb()'],
+                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'Window.closeModal()']
             ]
         );
 
         $modal->render();
     }
 
-    public static function showSqlExecutionErrorModal() {
+    public static function showSqlExecutionErrorModal($id) {
         $modal = new self(
+            $id,
             'SQL Execution Failed',
-            'An error occurred while executing SQL files. Please check the SQL syntax and try again.',
+            '
+                An error occurred while executing SQL files. Please check the SQL syntax and try again.
+                Press the retry-button if you want to return to the installer at the step where it failed or 
+                press the restart installer-button to restart installer and reset all values.
+            ',
             [
-                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'retryInstaller()'],
-                ['text' => 'Restart Installer', 'progressButton' => 'restart-button', 'onclick' => 'restartInstaller()'],
-                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'closeModal()']
+                ['text' => 'Retry', 'class' => 'progressButton', 'onclick' => 'Window.retryInstaller()'],
+                ['text' => 'Restart Installer', 'class' => 'progressButton', 'onclick' => 'Window.restartInstaller()'],
+                ['text' => 'Cancel', 'class' => 'backButton', 'onclick' => 'Window.closeModal()']
             ]
         );
 
