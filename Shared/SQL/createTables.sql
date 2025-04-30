448deb91fd96d0887e3742653c7f3911584724ae
gustafssonjohanna <b23johgu@student.his.se>
2025-04-30 14:01:17 +0200

    Update createTables.sql
    Created a table to be used for filenames

--- Shared/SQL/createTables.sql
+++ Shared/SQL/createTables.sql
@@ -4,3 +4,11 @@ CREATE TABLE groupdugga (
      active_users INT(3) UNSIGNED,
      PRIMARY KEY(hash)
 )CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB;
+
+/* Table for filenames in courses and duggor associated with issue #16987 */
+CREATE TABLE duggaFiles(
+	filename				VARCHAR(100) NOT NULL UNIQUE,
+	quizID                  VARCHAR(100) NOT NULL UNIQUE,
+	course_id				VARCHAR(45) NULL UNIQUE
+	PRIMARY KEY (filename)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB;
