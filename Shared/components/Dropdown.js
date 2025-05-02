925fdb53778a84e5d5b18147e946658b06a1efd7
c22isako <164880812+c22isako@users.noreply.github.com>
2025-05-02 10:43:25 +0200

    Removed inline-styling
    Moved styling to be nested within the dropButton.

--- Shared/components/Dropdown.js
+++ Shared/components/Dropdown.js
@@ -9,7 +9,7 @@ const Dropdown = ({ children, dropdownName, onClick, className = "", id="" }) =>
     return (
         <div className={`dropdown ${className}`}>
             <button onClick={onClick} className="dropButton submit-button">
-                <img alt="dropdownIcon" src="../Shared/icons/hamburger_menu_google_fonts.svg" style={{pointerEvents: 'none'}}/>
+                <img alt="dropdownIcon" src="../Shared/icons/hamburger_menu_google_fonts.svg" />
                 {dropdownName}
             </button>
             <div className="dropdown-content dropdown-content-hide" id={id}>
