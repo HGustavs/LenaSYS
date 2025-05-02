437f2469ac7b73ba1589bbb6248e39bdd5e0fb63
gustafssonjohanna <b23johgu@student.his.se>
2025-05-02 12:00:36 +0200

    duggaFiles-table are created in the right file
    Removed table from createTables.sql and put it in init_db.sql.
    
    Changed the names for the columns quiz and cid (course id) to be more consistent to the naming of the same things in the other tables.

--- Shared/SQL/createTables.sql
+++ Shared/SQL/createTables.sql
@@ -4,11 +4,3 @@ CREATE TABLE groupdugga (
      active_users INT(3) UNSIGNED,
      PRIMARY KEY(hash)
 )CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB;
-
-/* Table for filenames in courses and duggor associated with issue #16987 */
-CREATE TABLE duggaFiles(
-	filename				VARCHAR(100) NOT NULL UNIQUE,
-	quizID                  INT(11) NOT NULL,
-	course_id				INT(10) NOT NULL
-	PRIMARY KEY (filename)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB;
