e4be3d3892eb8cee30c056dc9a5203502168a926
a20artje <coolvids.bruh@gmail.com>
2025-05-06 10:52:53 +0200

    Change return option to true

--- DuggaSys/microservices/curlService.php
+++ DuggaSys/microservices/curlService.php
@@ -41,7 +41,7 @@ function callMicroserviceGET($microservicePath){
     $url = $baseURL . $path;
   
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
 
