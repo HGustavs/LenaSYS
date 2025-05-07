520ba8c2e2d6528ffc2395e7887b5de56406187a
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-05-07 13:31:53 +0200

    Added a table with the dependencies

--- DuggaSys/microservices/endpointDirectory/microserviceUI.php
+++ DuggaSys/microservices/endpointDirectory/microserviceUI.php
@@ -187,7 +187,23 @@ if (isset($_GET['id'])) {
         <?php } else { ?>
             <p>No outputs</p>
         <?php } ?>
-
+        <?php 
+            echo "<h3>Dependencies</h3>";
+        if (!empty($dependencies)) {
+            echo "<table>";
+            echo $microservice['ms_name'] . " depends on";
+            echo "<tr><th>Microservice</th><th>Path</th></tr>";
+            foreach ($dependencies as $dependency) {
+                echo '<tr>';
+                echo '<td>' . $dependency['depends_on'] . '</td>';
+                echo '<td>' . $dependency['path'] . '</td>';
+                echo '</tr>';
+            }
+        } else {
+            echo "<p>No dependencies</p>";
+        }
+        ?>
+        </table>
         <div style="display: flex; gap: 5px;">
             <form method="get">
                 <input type="hidden" name="id" value="<?php echo $microservice['id']; ?>">
