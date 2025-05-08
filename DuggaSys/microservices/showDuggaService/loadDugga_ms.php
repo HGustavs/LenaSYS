9533d986a0587ef89b700fd4ac5b56b57b1d8d74
a23danme <a23danme@student.his.se>
2025-05-08 15:18:13 +0200

    loadDugga changed
    trying to fix #17264

--- DuggaSys/microservices/showDuggaService/loadDugga_ms.php
+++ DuggaSys/microservices/showDuggaService/loadDugga_ms.php
@@ -3,19 +3,18 @@ date_default_timezone_set("Europe/Stockholm");
 
 include_once "../../../Shared/sessions.php";
 include_once "../../../Shared/basic.php";
-include_once "retrieveShowDuggaService_ms.php";
+include_once "../curlService.php";
 
 // Connect to database and start session
 pdoConnect();
 session_start();
-
 if(isset($_SESSION['uid'])){
 	$userid=$_SESSION['uid'];
 	$loginname=$_SESSION['loginname'];
 	$lastname=$_SESSION['lastname'];
 	$firstname=$_SESSION['firstname'];
 }else{
-	$userid="student";		
+$userid="student";		
 } 	
 
 $hash=getOP('hash');
@@ -30,30 +29,48 @@ $newcoursevers=array();
 $newduggaid=array();
 
 if($hash!="UNK"){
-    $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE hash=:hash";
-    $query = $pdo->prepare($sql);
-    $query->bindParam(':hash', $hash);
-    $result = $query->execute();
-    $rows = $query->fetchAll();
-
-    //if the hash didn't work then retrive all answers for that moment
-    if($rows == NULL){
-        //changed WHERE key to moment instead of hash since hash isn't working correctly. It appears to work so long as their is an entry for that moment in userAnswer
-        $sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE moment=:moment";
-        $query = $pdo->prepare($sql);
-        $query->bindParam(':moment', $moment);
-        $query->execute();
-        $rows = $query->fetchAll();
-    }
-
-    foreach($rows as $row){
-        $variant=$row['vid'];
-        $answer=$row['useranswer'];
-        $variantanswer=html_entity_decode($row['variantanswer']);
-        $param=html_entity_decode($row['param']);
-        $newcourseid=$row['cid'];
-        $newcoursevers=$row['vers'];
-        $newduggaid=$row['quiz'];
-    }
-
+$sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE hash=:hash";
+$query = $pdo->prepare($sql);
+$query->bindParam(':hash', $hash);
+$result = $query->execute();
+$rows = $query->fetchAll();
 }
+
+//if the hash didn't work then retrive all answers for that moment
+if($rows == NULL){
+	//changed WHERE key to moment instead of hash since hash isn't working correctly. It appears to work so long as their is an entry for that moment in userAnswer
+	$sql="SELECT vid,variant.variantanswer AS variantanswer,useranswer,param,cid,vers,quiz FROM userAnswer LEFT JOIN variant ON userAnswer.variant=variant.vid WHERE moment=:moment";
+	$query = $pdo->prepare($sql);
+	$query->bindParam(':moment', $moment);
+	$query->execute();
+	$rows = $query->fetchAll();
+}
+
+foreach($rows as $row){
+	$variant=$row['vid'];
+	$answer=$row['useranswer'];
+	$variantanswer=html_entity_decode($row['variantanswer']);
+	$param=html_entity_decode($row['param']);
+	$newcourseid=$row['cid'];
+	$newcoursevers=$row['vers'];
+	$newduggaid=$row['quiz'];
+}
+
+
+// Microservice for POST
+$response = callMicroservicePOST(
+	"showDuggaService/retrieveShowDuggaService_ms.php",
+	[
+		'moment' => $moment,
+		'courseid' => $courseid,
+		'hash' => $hash,
+		'password' => $hashpwd,
+		'coursevers' => $coursevers,
+		'duggaid' => $duggaid,
+		'opt' => $opt
+	],
+	true
+);
+
+header("Content-Type: application/json");
+echo $response;
