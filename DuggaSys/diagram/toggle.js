b08285eedf3ca79bf97c012ba3c42ead7e22e74c
elinaxelsson <165275449+ElinAxelsson@users.noreply.github.com>
2025-05-13 17:03:50 +0200

    Updated diagram.css and toggle.js

--- DuggaSys/diagram/toggle.js
+++ DuggaSys/diagram/toggle.js
@@ -455,13 +455,9 @@ function toggleColorMenu(buttonID) {
             }
         }
         // Menu position relative to button
-        menu.style.width = width + "px";
-        const buttonWidth = button.offsetWidth;
-        const offsetWidth = window.innerWidth - button.getBoundingClientRect().x - (buttonWidth);
-        const offsetHeight = button.getBoundingClientRect().y;
-        menu.style.top = (offsetHeight - 50) + "px";
-        const menuOffset = window.innerWidth - menu.getBoundingClientRect().x - (width);
-        menu.style.left = (menu.style.left + menuOffset) - (offsetWidth + buttonWidth) + "px";
+        menu.style.maxHeight = "600px";
+        menu.style.overflowY = "auto";
+        menu.style.overflowX = "hidden";
     } else {    // if the color menu's inner html is not empty, remove the content
         menu = button.children[0];
         menu.innerHTML = "";
