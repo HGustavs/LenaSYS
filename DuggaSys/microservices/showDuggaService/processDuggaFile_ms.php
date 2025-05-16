57fa30edf89c627beea263476779e0e20cce820a
a22wilas <a22wilas@student.his.se>
2025-05-16 16:46:06 +0200

    Update processDuggaFile_ms.php

--- DuggaSys/microservices/showDuggaService/processDuggaFile_ms.php
+++ DuggaSys/microservices/showDuggaService/processDuggaFile_ms.php
@@ -9,7 +9,7 @@ include_once "../../../Shared/basic.php";
 include_once "../../../Shared/sessions.php";
 
 // Connect to database
-pdoConnect(); 
+pdoConnect();
 
 $courseid;
 $coursevers;
@@ -20,116 +20,117 @@ $moment;
 
 // Handle incoming POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['courseid'], $_POST['coursevers'])) {
-        $courseid = $_POST['courseid'];
-        $coursevers = $_POST['coursevers'];
+	if (isset($_POST['courseid'], $_POST['coursevers'])) {
+		$courseid = $_POST['courseid'];
+		$coursevers = $_POST['coursevers'];
 		$duggaid = $_POST['duggaid'];
 		$duggainfo = $_POST['duggainfo'];
 		$moment = $_POST['moment'];
 	}
 }
 
-	$files= array();
+$files = array();
 
-	if(	isset($_SESSION["submission-$courseid-$coursevers-$duggaid-$moment"]) && 
-	isset($_SESSION["submission-password-$courseid-$coursevers-$duggaid-$moment"]) && 
-	isset($_SESSION["submission-variant-$courseid-$coursevers-$duggaid-$moment"])){
+if (
+	isset($_SESSION["submission-$courseid-$coursevers-$duggaid-$moment"]) &&
+	isset($_SESSION["submission-password-$courseid-$coursevers-$duggaid-$moment"]) &&
+	isset($_SESSION["submission-variant-$courseid-$coursevers-$duggaid-$moment"])
+) {
 
-	$tmphash=$_SESSION["submission-$courseid-$coursevers-$duggaid-$moment"];
-	
-	$query = $pdo->prepare("SELECT subid,vers,did,fieldnme,filename,extension,mime,updtime,kind,filepath,seq,segment,hash from submission WHERE hash=:hash ORDER BY subid,fieldnme,updtime asc;");  
+	$tmphash = $_SESSION["submission-$courseid-$coursevers-$duggaid-$moment"];
+
+	$query = $pdo->prepare("SELECT subid,vers,did,fieldnme,filename,extension,mime,updtime,kind,filepath,seq,segment,hash from submission WHERE hash=:hash ORDER BY subid,fieldnme,updtime asc;");
 	$query->bindParam(':hash', $tmphash);
 	$query->execute();
 	$rows = $query->fetchAll();
-	
+
 	//if the hash didn't work and the user is a superuser then retrive all submissions
-	if(isSuperUser($_SESSION['uid']) && $rows == NULL){
-		$query = $pdo->prepare("SELECT subid,vers,did,fieldnme,filename,extension,mime,updtime,kind,filepath,seq,segment,hash from submission WHERE segment=:moment ORDER BY subid,fieldnme,updtime asc;");  
+	if (isSuperUser($_SESSION['uid']) && $rows == NULL) {
+		$query = $pdo->prepare("SELECT subid,vers,did,fieldnme,filename,extension,mime,updtime,kind,filepath,seq,segment,hash from submission WHERE segment=:moment ORDER BY subid,fieldnme,updtime asc;");
 		$query->bindParam(':moment', $moment);
 		$query->execute();
 		$rows = $query->fetchAll();
 	}
 	// Store current day in string
 	$today = date("Y-m-d H:i:s");
-	
-	foreach($rows as $row) {
-			$content = "UNK";
-			$feedback = "UNK";
-			$zipdir = "";
-			$zip = new ZipArchive;
-			
 
-			$ziptemp = $row['filepath'].$row['filename'].$row['seq'].".".$row['extension'];
-			
-			if(!file_exists($ziptemp)) {
-				$isFileSubmitted = false;
-				$zipdir="UNK";
-			}else{	
-				$isFileSubmitted = true; 		
-				if ($zip->open($ziptemp) == TRUE) {
-					for ($i = 0; $i < $zip->numFiles; $i++) {
-						$zipdir .= $zip->getNameIndex($i).'<br />';
-					}
+	foreach ($rows as $row) {
+		$content = "UNK";
+		$feedback = "UNK";
+		$zipdir = "";
+		$zip = new ZipArchive;
+
+
+		$ziptemp = $row['filepath'] . $row['filename'] . $row['seq'] . "." . $row['extension'];
+
+		if (!file_exists($ziptemp)) {
+			$isFileSubmitted = false;
+			$zipdir = "UNK";
+		} else {
+			$isFileSubmitted = true;
+			if ($zip->open($ziptemp) == TRUE) {
+				for ($i = 0; $i < $zip->numFiles; $i++) {
+					$zipdir .= $zip->getNameIndex($i) . '<br />';
 				}
 			}
-						
-			$fedbname=$row['filepath'].$row['filename'].$row['seq']."_FB.txt";				
-			if(!file_exists($fedbname)) {
-					$feedback="UNK";
-			} else {
-				if($today > $duggainfo['qrelease']  || is_null($duggainfo['qrelease'])){
-					$feedback=file_get_contents($fedbname);				
-				}
-			}			
-			
-			if($row['kind']=="3"){
-					// Read file contents
-					$movname=$row['filepath']."/".$row['filename'].$row['seq'].".".$row['extension'];
-
-					if(!file_exists($movname)) {
-							$content="UNK!";
-					} else {
-							$content=file_get_contents($movname);
-					}
-			}	else if($row['kind']=="2"){
-					// File content is an URL
-					$movname=$row['filepath']."/".$row['filename'].$row['seq'];
-	
-					if(!file_exists($movname)) {
-							$content="UNK URL!";
-					} else {
-							$content=file_get_contents($movname);
-					}
-			}else{
-					$content="Not a text-submit or URL";
-			}
-			
-			$entry = array(
-				'subid' => $row['subid'],
-				'vers' => $row['vers'],
-				'did' => $row['did'],
-				'fieldnme' => $row['fieldnme'],
-				'filename' => $row['filename'],	
-				'filepath' => $row['filepath'],	
-				'extension' => $row['extension'],
-				'mime' => $row['mime'],
-				'updtime' => $row['updtime'],
-				'kind' => $row['kind'],	
-				'seq' => $row['seq'],	
-				'segment' => $row['segment'],	
-				'content' => $content,
-				'feedback' => $feedback,
-				'username' => $tmphash,
-				'zipdir' => $zipdir
-			);
-			file_put_contents('example.txt', print_r($entry, true), FILE_APPEND);
-			// If the filednme key isn't set, create it now
-			 if (!isset($files[$row['segment']])) $files[$row['segment']] = array();
-	
-			array_push($files[$row['segment']], $entry);	
 		}
 
-	}
-	if (sizeof($files) === 0) {$files = (object)array();} // Force data type to be object
-    echo json_encode([$files]);
+		$fedbname = $row['filepath'] . $row['filename'] . $row['seq'] . "_FB.txt";
+		if (!file_exists($fedbname)) {
+			$feedback = "UNK";
+		} else {
+			if ($today > $duggainfo['qrelease']  || is_null($duggainfo['qrelease'])) {
+				$feedback = file_get_contents($fedbname);
+			}
+		}
 
+		if ($row['kind'] == "3") {
+			// Read file contents
+			$movname = $row['filepath'] . "/" . $row['filename'] . $row['seq'] . "." . $row['extension'];
+
+			if (!file_exists($movname)) {
+				$content = "UNK!";
+			} else {
+				$content = file_get_contents($movname);
+			}
+		} else if ($row['kind'] == "2") {
+			// File content is an URL
+			$movname = $row['filepath'] . "/" . $row['filename'] . $row['seq'];
+
+			if (!file_exists($movname)) {
+				$content = "UNK URL!";
+			} else {
+				$content = file_get_contents($movname);
+			}
+		} else {
+			$content = "Not a text-submit or URL";
+		}
+
+		$entry = array(
+			'subid' => $row['subid'],
+			'vers' => $row['vers'],
+			'did' => $row['did'],
+			'fieldnme' => $row['fieldnme'],
+			'filename' => $row['filename'],
+			'filepath' => $row['filepath'],
+			'extension' => $row['extension'],
+			'mime' => $row['mime'],
+			'updtime' => $row['updtime'],
+			'kind' => $row['kind'],
+			'seq' => $row['seq'],
+			'segment' => $row['segment'],
+			'content' => $content,
+			'feedback' => $feedback,
+			'username' => $tmphash,
+			'zipdir' => $zipdir
+		);
+		// If the filednme key isn't set, create it now
+		if (!isset($files[$row['segment']])) $files[$row['segment']] = array();
+
+		array_push($files[$row['segment']], $entry);
+	}
+}
+if (sizeof($files) === 0) {
+	$files = (object)array();
+} // Force data type to be object
+echo json_encode([$files]);
