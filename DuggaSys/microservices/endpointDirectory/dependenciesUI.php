80d0a50e205f4cca335e0cd4ed9d2dec9bea07c1
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2025-05-16 08:31:03 +0200

    Added form and database query

--- DuggaSys/microservices/endpointDirectory/dependenciesUI.php
+++ DuggaSys/microservices/endpointDirectory/dependenciesUI.php
@@ -12,30 +12,50 @@ $db = new PDO('sqlite:endpointDirectory_db.sqlite');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $services = $db->query("SELECT * FROM microservices")->fetchAll();
 
+// get the id of the searched microservice
+if (isset($_GET['ms_name'])) {
+    $ms_name = $_GET['ms_name'];
 
+    $stmt = $db->prepare("SELECT id FROM microservices WHERE ms_name LIKE ?");
+    $stmt->execute([$ms_name]);
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        $id = $result['id'];
+    } 
+}
 
 } catch (PDOException $e) {
-    $dbError = "Database is not installed. Press the button below to create a database. ";
+    $dbError = "Database is not installed.";
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Microservice Directory</title>
+    <title>Dependencies Directory</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>    
     <div class='line'>
         <h1>Microservice Directory</h1>
     </div>
-    <a href="microserviceUI.php" class="a-button">Back to microserviceUI</button>
+    <a href="microserviceUI.php" class="a-button">Back to microserviceUI</a>
     <?php    
     if (isset($dbError)) {
         echo "<p class='error_message'>" . $dbError . "</p>";
-    } else {
-        if (!isset($microservice)) { 
-
+    } else { ?>
+        <form method="GET" action="">
+            <label for="ms_name">Enter microservice name</label>
+            <input type="text" id="ms_name" name="ms_name">
+            <input type="submit" value="Submit">
+        </form>
+        <?php
+        if ($id) { 
+            echo $id;
+        } else {
+            echo "No results found";
         }
     }
     ?>
