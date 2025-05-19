6108cfc0588ee44630ff64660cb59ee370895a67
a23arvme <a23arvme@student.his.se>
2025-05-19 11:57:00 +0200

    fixed ssl generator

--- newinstaller/tools/ssl/ssl_generator.php
+++ newinstaller/tools/ssl/ssl_generator.php
@@ -1,44 +1,43 @@
 <?php
-# create SSL for only local use( localhost )
-$hostname = "localhost";
-# a number is needed, so we pick:
-$validDays = 420;
 
-# generate a 2048 bit RSA private key
-$privateKey = openssl_pkey_new([
-	"private_key_bits" => 2048,
-	"private_key_type" => OPENSSL_KEYTYPE_RSA,
-]);
+function generateSSL()
+{
+	# create SSL for only local use( localhost )
+	$hostname = "localhost";
+	# a number is needed, so we pick:
+	$validDays = 420;
 
-# SSL keys requires some metadata, even though it is local
-$csr = openssl_csr_new([
-	"CN" => $hostname,
-	"O" => "dev",
-	"OU" => "dev",
-	"L" => "Locality",
-	"ST" => "State",
-	"C" => "SE",
-],$privateKey);
+	# generate a 2048 bit RSA private key
+	$privateKey = openssl_pkey_new([
+		"private_key_bits" => 2048,
+		"private_key_type" => OPENSSL_KEYTYPE_RSA,
+	]);
 
-# Sign the certificate!
+	# SSL keys requires some metadata, even though it is local
+	$csr = openssl_csr_new([
+		"CN" => $hostname,
+		"O" => "dev",
+		"OU" => "dev",
+		"L" => "Locality",
+		"ST" => "State",
+		"C" => "SE",
+	], $privateKey);
 
-$certificate = openssl_csr_sign(
-	$csr,
-	null,
-	$privateKey,
-	$validDays,
-	["digest_alg" => "sha256"],
-);
+	# Sign the certificate!
 
-# export the key and certificate
-openssl_pkey_export($privateKey, $privateKeyOut);
-openssl_x509_export($certificate, $certificateOut);
+	$certificate = openssl_csr_sign(
+		$csr,
+		null,
+		$privateKey,
+		$validDays,
+		["digest_alg" => "sha256"],
+	);
 
-# write the output to files
-file_put_contents("localhost.key", $privateKeyOut);
-file_put_contents("localhost.crt", $certificateOut);
+	# export the key and certificate
+	openssl_pkey_export($privateKey, $privateKeyOut);
+	openssl_x509_export($certificate, $certificateOut);
 
-
-
-
-
+	# write the output to files
+	file_put_contents(__DIR__ . "/localhost.key", $privateKeyOut);
+	file_put_contents(__DIR__ . "/localhost.crt", $certificateOut);
+}
