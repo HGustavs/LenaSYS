cd49fd41458835c44b7f7323c0282fe6795cfa56
a23louma <a23louma@student.his.se>
2025-05-20 10:06:29 +0200

    Removed header, changed URL, removed global pdo #17708

--- DuggaSys/microservices/sectionedService/createListEntry_ms.php
+++ DuggaSys/microservices/sectionedService/createListEntry_ms.php
@@ -34,9 +34,6 @@ $templateNumber = getOP('templateNumber');
 $exampleid = getOP('exampleid');
 $debug = "NONE!";
 
-global $pdo;
-
-
 // Insert a new code example and update variables accordingly.
 if ($link == -1) {
     $queryz2 = $pdo->prepare("SELECT * FROM codeexample ORDER BY exampleid DESC LIMIT 1");
@@ -49,9 +46,7 @@ if ($link == -1) {
     }
 
     //set url for createNewCodeExample.php path
-    header("Content-Type: application/json");
-    $baseURL = "https://" . $_SERVER['HTTP_HOST'];
-    $url = $baseURL . "/LenaSYS/DuggaSys/microservices/sharedMicroservices/createNewCodeExample_ms.php";
+    $url = "sharedMicroservices/createNewCodeExample_ms.php";
 
     $dataToSend = [
         'exampleid' => $exampleid,
