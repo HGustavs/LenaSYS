d2fe44c9125e59436d5e71c78e9d7746fd9c8e79
d22marwi <d22marwi@student.his.se>
2025-05-22 17:18:26 +0200

    create a test for profileService/retrieveProfileService_ms.php
    Updated retrieveProfileService_ms.php to make the test failure pass

--- DuggaSys/microservices/profileService/retrieveProfileService_ms.php
+++ DuggaSys/microservices/profileService/retrieveProfileService_ms.php
@@ -1,5 +1,14 @@
 <?php
 
+function pg($name, $default = null) {
+    return $_POST[$name] ?? $_GET[$name] ?? $default;
+}
+
+$successRaw = pg('success', false);                 
+$success     = filter_var($successRaw);
+$status      = pg('status', '');
+$debug       = pg('debug',  '');
+
 function retrieveProfileService($debug, $success, $status){
 	$array = array(
         "success" => $success,
@@ -9,5 +18,8 @@ function retrieveProfileService($debug, $success, $status){
     return $array;
 }
 
-header("Content-Type: application/json");
-echo json_encode($array);
+$response = retrieveProfileService($debug, $success, $status);
+
+header('Content-Type: application/json');
+echo json_encode($response);
+exit;
