088dc3f370f0f42a56ed58c8340dcb5eeb8d3b02
a23dilel <a23dilel@student.his.se>
2025-05-22 16:24:49 +0200

    Added command for add user and groups, and change file permission

--- dockerenv/setup.sh
+++ dockerenv/setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+mkdir -p ./githubMetadata
+
 # Make sure that coursesyspw.php exists before Docker mounts it
 if [ ! -f ./coursesyspw.php ]; then
   echo "Creating coursesyspw.php from template"
   cp ./coursesyspw.php.template ./coursesyspw.php
 fi
+
+# Add user and group if not exists
+if ! id "www-data" &>/dev/null; then
+  sudo groupadd -g 33 www-data
+  sudo useradd -u 33 -g www-data -s /bin/false www-data
+fi
+
+# Set appropriate user rights
+sudo chown -R www-data:www-data ./githubMetadata
+sudo chown www-data:www-data ./coursesyspw.php
+sudo chmod -R 777 ../../LenaSYS/
+
+# Prevent showing changing the file permissions recursively
+git config core.fileMode false
 
 # start building Docker
 docker compose up --build
