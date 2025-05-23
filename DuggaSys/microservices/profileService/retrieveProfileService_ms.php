8d45fab1add098d4faa740129a01b2046cd14e9c
d22marwi <d22marwi@student.his.se>
2025-05-23 11:51:36 +0200

    create a test for profileService/retrieveProfileService_ms.php
    Removed the function that hardcodes values.

--- DuggaSys/microservices/profileService/retrieveProfileService_ms.php
+++ DuggaSys/microservices/profileService/retrieveProfileService_ms.php
@@ -1,14 +1,5 @@
 <?php
 
-function pg($name, $default = null) {
-    return $_POST[$name] ?? $_GET[$name] ?? $default;
-}
-
-$successRaw = pg('success', false);                 
-$success     = filter_var($successRaw);
-$status      = pg('status', '');
-$debug       = pg('debug',  '');
-
 function retrieveProfileService($debug, $success, $status){
 	$array = array(
         "success" => $success,
@@ -17,9 +8,3 @@ function retrieveProfileService($debug, $success, $status){
 		);
     return $array;
 }
-
-$response = retrieveProfileService($debug, $success, $status);
-
-header('Content-Type: application/json');
-echo json_encode($response);
-exit;
