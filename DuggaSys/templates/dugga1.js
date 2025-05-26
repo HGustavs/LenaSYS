dfcd7d69dd4a25ff1b5327f833cbe06718103b5d
a22maxda <a22maxda@student.his.se>
2025-05-26 19:52:08 +0200

    feedback removed & animation
    Removed the feedback function as it is depreciated. Also created an animation that sort of works. Will refine it later.

--- DuggaSys/templates/dugga1.js
+++ DuggaSys/templates/dugga1.js
@@ -361,10 +361,25 @@ function resetBitstring() {
 //----------------------------------------------------------------------------------
 function toggleInstructions()
 {
-    $(".instructions-content").slideToggle("slow");
-}
+	let instructionBox = document.querySelector(".instructions-content");
+	if(!instructionBox.classList.contains("vis")){
+		instructionBox.classList.add("vis");
+		instructionBox.style.height="auto";
 
-function toggleFeedback()
-{
-    $(".feedback-content").slideToggle("slow");
+		let height = instructionBox.clientHeight + "px";
+
+		instructionBox.style.height="0px";
+
+		setTimeout(function(){
+			instructionBox.style.height=height
+		}, 0);
+	}
+	else{
+		instructionBox.style.height="0px";
+		instructionBox.addEventListener("transitionend", function(){
+			instructionBox.classList.remove("vis");
+		}, {
+			once :true
+		});
+	}
 }
