44fb253750e78ff41fcbac1bd6ba61b2aea19319
a23dilel <164745156+a23dilel@users.noreply.github.com>
2025-05-27 16:16:31 +0200

    Added chown and chmod command

--- dockerenv/setup.bat
+++ dockerenv/setup.bat
@@ -7,4 +7,15 @@ IF NOT EXIST coursesyspw.php (
 )
 
 REM start building Docker
-docker compose up --build
+docker compose up --build -d
+
+REM Make sure the containers are running
+echo Please wait a bit to make sure the containers are running
+timeout /t 10
+
+REM Run chown and chmod command in Apache container
+echo Beginning to change ownership and permissions of html folder
+docker exec -u root apache-php chown -R www-data:www-data /var/www/html/
+docker exec -u root apache-php chmod -R 777 /var/www/html/
+
+echo Now you can use the website!
