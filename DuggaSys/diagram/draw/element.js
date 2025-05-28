b0fac7dba54a4ee27932ee3b3d6d0778a2b0b3b9
b20isato <102606510+b20isato@users.noreply.github.com>
2025-05-28 13:43:23 +0000

    Update element.js

--- DuggaSys/diagram/draw/element.js
+++ DuggaSys/diagram/draw/element.js
@@ -696,6 +696,7 @@ function drawElementUMLRelation(element, boxw, boxh, linew) {
 function drawElementSelfCall(element, boxw, boxh, linew) {
   const strokeColor = "black";
   const fillColor   = "none";
+    console.log("hello from draw");
   // Determine square size so it fits with a linew-wide margin
   const size = Math.min(boxw, boxh) - 2 * linew;
   // Center it
